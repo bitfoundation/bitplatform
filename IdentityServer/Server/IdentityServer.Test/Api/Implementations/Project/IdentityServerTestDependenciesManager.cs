c82a98b7c2769c1b5537cc5d05df7809b7a70634
Yasser Moradi <ysmoradi@outlook.com>
2016-10-21 21:00:46 +0330

    Changed bit-identity-server ioc config based on recent bit-framework changes

--- IdentityServer/Server/IdentityServer.Test/Api/Implementations/Project/IdentityServerTestDependenciesManager.cs
+++ IdentityServer/Server/IdentityServer.Test/Api/Implementations/Project/IdentityServerTestDependenciesManager.cs
@@ -2,17 +2,19 @@
 using Foundation.Api.Contracts.Metadata;
 using Foundation.Api.Implementations;
 using Foundation.Api.Implementations.Metadata;
-using Foundation.Api.Metadata;
+using Foundation.Api.Implementations.Project;
 using Foundation.Api.Middlewares;
 using Foundation.Core.Contracts;
 using Foundation.Core.Contracts.Project;
 using Foundation.Core.Implementations;
 using Foundation.Test.Api.Implementations;
+using Foundation.Test.Api.Middlewares;
 using IdentityServer.Api.Contracts;
 using IdentityServer.Api.Implementations;
 using IdentityServer.Api.Metadata.Views;
 using IdentityServer.Api.Middlewares;
 using IdentityServer3.Core.Services;
+using System.Reflection;
 
 namespace IdentityServer.Test.Api.Implementations.Project
 {
@@ -22,11 +24,11 @@ namespace IdentityServer.Test.Api.Implementations.Project
         {
             #region Foundation
 
+            dependencyManager.RegisterMinimalDependencies();
+
             dependencyManager.RegisterInstance(DefaultAppEnvironmentProvider.Current);
-            dependencyManager.RegisterInstance(DefaultDependenciesManagerProvider.Current);
             dependencyManager.RegisterInstance(DefaultJsonContentFormatter.Current);
             dependencyManager.RegisterInstance(DefaultPathProvider.Current);
-            dependencyManager.RegisterInstance(DefaultDependencyManager.Current);
 
             dependencyManager.Register<ILogger, DefaultLogger>();
             dependencyManager.Register<ILogStore, TestLogStore>();
@@ -35,17 +37,19 @@ namespace IdentityServer.Test.Api.Implementations.Project
 
             dependencyManager.Register<IDateTimeProvider, DefaultDateTimeProvider>(lifeCycle: DepepdencyLifeCycle.SingleInstance);
             dependencyManager.Register<ICertificateProvider, DefaultCertificateProvider>(lifeCycle: DepepdencyLifeCycle.SingleInstance);
+            dependencyManager.Register<IExceptionToHttpErrorMapper, DefaultExceptionToHttpErrorMapper>(lifeCycle: DepepdencyLifeCycle.SingleInstance);
 
             dependencyManager.RegisterAppEvents<RazorViewEngineConfiguration>();
 
+            dependencyManager.RegisterOwinMiddleware<OwinCompressionMiddlewareConfiguration>();
             dependencyManager.RegisterOwinMiddleware<StaticFilesMiddlewareConfiguration>();
             dependencyManager.RegisterOwinMiddleware<AutofacDependencyInjectionMiddlewareConfiguration>();
             dependencyManager.RegisterOwinMiddleware<OwinExceptionHandlerMiddlewareConfiguration>();
             dependencyManager.RegisterOwinMiddleware<LogRequestInformationMiddlewareConfiguration>();
             dependencyManager.RegisterOwinMiddleware<MetadataMiddlewareConfiguration>();
 
-            dependencyManager.Register<IAppMetadataProvider, DefaultAppMetadataProvider>();
-            dependencyManager.Register<IMetadataBuilder, FoundationMetadataBuilder>(overwriteExciting: false);
+            dependencyManager.Register<IAppMetadataProvider, DefaultAppMetadataProvider>(lifeCycle: DepepdencyLifeCycle.SingleInstance);
+            dependencyManager.RegisterMetadata(typeof(FoundationEdmModelProvider).GetTypeInfo().Assembly, typeof(LoginViewMetadataBuilder).GetTypeInfo().Assembly);
 
             #endregion
 
@@ -54,7 +58,6 @@ namespace IdentityServer.Test.Api.Implementations.Project
             dependencyManager.Register<IScopesProvider, DefaultScopesProvider>(lifeCycle: DepepdencyLifeCycle.SingleInstance);
             dependencyManager.Register<IdentityServer3.Core.Logging.ILogProvider, DefaultIdentityServerLogProvider>(lifeCycle: DepepdencyLifeCycle.SingleInstance);
             dependencyManager.RegisterOwinMiddleware<IdentityServerMiddlewareConfiguration>();
-            dependencyManager.Register<IMetadataBuilder, LoginViewMetadataBuilder>(overwriteExciting: false);
             dependencyManager.Register<IViewService, DefaultViewService>(lifeCycle: DepepdencyLifeCycle.SingleInstance);
             dependencyManager.Register<ISsoPageHtmlProvider, RazorSsoHtmlPageProvider>(lifeCycle: DepepdencyLifeCycle.SingleInstance);
             dependencyManager.Register<ISSOPageModelProvider, DefaultSSOPageModelProvider>(lifeCycle: DepepdencyLifeCycle.SingleInstance);
