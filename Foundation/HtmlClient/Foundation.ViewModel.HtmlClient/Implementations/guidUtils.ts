b6e5987179b7b888a2f7fa21e0f5cfb00033f196
Yasser Moradi <ysmoradi@outlook.com>
2016-11-06 16:34:07 +0330

    Added guid utils

---
+++ Foundation/HtmlClient/Foundation.ViewModel.HtmlClient/Implementations/guidUtils.ts
@@ -0,0 +1,11 @@
+﻿module Foundation.ViewModel.Implementations {
+    @Core.ObjectDependency({ name: "GuidUtils" })
+    export class GuidUtils {
+        @Core.Log()
+        public newGuid(): string {
+
+            return $data.Guid['NewGuid']().value.toLowerCase();
+            
+        }
+    }
+}
