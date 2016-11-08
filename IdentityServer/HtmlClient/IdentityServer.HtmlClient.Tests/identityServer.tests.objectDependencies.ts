206aaf55a8c5699697bfb9e3bad052052e7ba93f
Yasser Moradi <ysmoradi@outlook.com>
2016-11-08 13:34:39 +0330

    Changes based on recent foundation changes

--- IdentityServer/HtmlClient/IdentityServer.HtmlClient.Tests/identityServer.tests.objectDependencies.ts
+++ IdentityServer/HtmlClient/IdentityServer.HtmlClient.Tests/identityServer.tests.objectDependencies.ts
@@ -6,21 +6,21 @@ module IdentityServerTests {
 
     let dependencyManager = Foundation.Core.DependencyManager.getCurrent();
 
-    dependencyManager.registerObjectDependency({ name: "ModelProvider", classCtor: IdentityServer.ViewModel.Implementations.DefaultModelProvider });
+    dependencyManager.registerObjectDependency({ name: "ModelProvider", class: IdentityServer.ViewModel.Implementations.DefaultModelProvider });
 
-    dependencyManager.registerObjectDependency({ name: "AppEvent", classCtor: IdentityServer.ViewModel.Implementations.IdentityServerDefaultAngularAppInitialization });
+    dependencyManager.registerObjectDependency({ name: "AppEvent", class: IdentityServer.ViewModel.Implementations.IdentityServerDefaultAngularAppInitialization });
 
-    dependencyManager.registerObjectDependency({ name: "Logger", classCtor: Foundation.ViewModel.Implementations.DefaultLogger });
+    dependencyManager.registerObjectDependency({ name: "Logger", class: Foundation.ViewModel.Implementations.DefaultLogger });
 
-    dependencyManager.registerObjectDependency({ name: "AppStartup", classCtor: Foundation.ViewModel.Implementations.DefaultAppStartup });
+    dependencyManager.registerObjectDependency({ name: "AppStartup", class: Foundation.ViewModel.Implementations.DefaultAppStartup });
 
-    dependencyManager.registerObjectDependency({ name: "MetadataProvider", classCtor: Foundation.ViewModel.Implementations.DefaultMetadataProvider });
+    dependencyManager.registerObjectDependency({ name: "MetadataProvider", class: Foundation.ViewModel.Implementations.DefaultMetadataProvider });
 
-    dependencyManager.registerObjectDependency({ name: "AngularConfiguration", classCtor: Foundation.ViewModel.Implementations.DefaultAngularTranslateConfiguration });
+    dependencyManager.registerObjectDependency({ name: "AngularConfiguration", class: Foundation.ViewModel.Implementations.DefaultAngularTranslateConfiguration });
 
     dependencyManager.registerInstanceDependency({ name: 'ClientAppProfileManager', instance: Foundation.Core.ClientAppProfileManager.getCurrent() });
 
-    dependencyManager.registerObjectDependency({ name: "DateTimeService", classCtor: Foundation.ViewModel.Implementations.DefaultDateTimeService });
+    dependencyManager.registerObjectDependency({ name: "DateTimeService", class: Foundation.ViewModel.Implementations.DefaultDateTimeService });
 
-    dependencyManager.registerObjectDependency({ name: "EntityContextProvider", classCtor: Foundation.ViewModel.Implementations.DefaultEntityContextProvider });
+    dependencyManager.registerObjectDependency({ name: "EntityContextProvider", class: Foundation.ViewModel.Implementations.DefaultEntityContextProvider });
 }
