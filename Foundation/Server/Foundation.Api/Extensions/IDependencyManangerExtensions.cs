a39ebc50e206ab787c95595b3995ceb83f98eb7b
Yasser Moradi <ysmoradi@outlook.com>
2016-11-15 12:45:41 +0330

    Set CreateMissingTypeMaps to true for auto mapper by default

--- Foundation/Server/Foundation.Api/Extensions/IDependencyManangerExtensions.cs
+++ Foundation/Server/Foundation.Api/Extensions/IDependencyManangerExtensions.cs
@@ -266,6 +266,7 @@ namespace Foundation.Core.Contracts
 
                 MapperConfiguration mapperConfig = new MapperConfiguration(cfg =>
                 {
+                    cfg.CreateMissingTypeMaps = true;
                     configs.ToList().ForEach(c => c.Configure(cfg));
                 });
 
