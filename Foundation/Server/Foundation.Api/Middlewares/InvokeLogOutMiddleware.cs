a977a184d1f9aaa643628bd91c62a4c72d46b3b9
Yasser Moradi <ysmoradi@outlook.com>
2016-11-19 20:42:00 +0330

    Invoke logout middleware to signout from single sign on server

---
+++ Foundation/Server/Foundation.Api/Middlewares/InvokeLogOutMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Foundation.Core.Contracts;
+using Foundation.Core.Models;
+using Microsoft.Owin;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foundation.Api.Middlewares
+{
+    public class InvokeLogOutMiddleware : OwinMiddleware
+    {
+        public InvokeLogOutMiddleware(OwinMiddleware next)
+            : base(next)
+        {
+        }
+
+        private string _baseRedirectUri = null;
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            IDependencyResolver dependencyResolver = context.GetDependencyResolver();
+
+            if (_baseRedirectUri == null)
+            {
+                IAppEnvironmentProvider appEnvironmentProvider = dependencyResolver
+                    .Resolve<IAppEnvironmentProvider>();
+
+                AppEnvironment activEnvironment = appEnvironmentProvider.GetActiveAppEnvironment();
+
+                _baseRedirectUri = $"{activEnvironment.Security.SSOServerUrl}connect/endsession?post_logout_redirect_uri={activEnvironment.GetConfig("ClientHostBaseUri", context.Request.Host.Value)}{activEnvironment.GetConfig("ClientHostVirtualPath", "/")}SignOut";
+            }
+
+            context.Response.Redirect(_baseRedirectUri + "&id_token_hint=" + context.Request.Query["id_token"]);
+
+            context.Authentication.SignOut("custom", "Barear");
+        }
+    }
+}
---
+++ Foundation/Server/Foundation.Api/Middlewares/InvokeLogOutMiddlewareConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Foundation.Api.Contracts;
+using System;
+using Owin;
+using Microsoft.Owin;
+
+namespace Foundation.Api.Middlewares
+{
+    public class InvokeLogOutMiddlewareConfiguration : IOwinMiddlewareConfiguration
+    {
+        public virtual void Configure(IAppBuilder owinApp)
+        {
+            if (owinApp == null)
+                throw new ArgumentNullException(nameof(owinApp));
+
+            owinApp.Map("/InvokeLogout", innerApp =>
+            {
+                innerApp.Use<InvokeLogOutMiddleware>();
+            });
+        }
+    }
+}
