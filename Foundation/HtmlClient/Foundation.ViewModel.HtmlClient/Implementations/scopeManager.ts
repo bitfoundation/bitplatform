9e86baabd79460bc5d64066a8c35e1024e3c753b
ysmoradi <ysmoradi@outlook.com>
2016-11-29 00:36:02 +0330

    call $applyAsync in setTimeout of 0

--- Foundation/HtmlClient/Foundation.ViewModel.HtmlClient/Implementations/scopeManager.ts
+++ Foundation/HtmlClient/Foundation.ViewModel.HtmlClient/Implementations/scopeManager.ts
@@ -5,8 +5,11 @@
 
         public static update$scope = ($scope: ng.IScope): void => {
 
-            if (ScopeManager.Use$ApplyAsync == true)
-                $scope.$applyAsync();
+            if (ScopeManager.Use$ApplyAsync == true) {
+                window.setTimeout(() => {
+                    $scope.$applyAsync();
+                }, 0);
+            }
             else if ($scope.$$phase != "$apply" && $scope.$$phase != "$digest" && $scope.$root.$$phase != "$apply" && $scope.$root.$$phase != "$digest")
                 $scope.$apply();
 
