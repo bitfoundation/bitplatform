09fa9538a81ef1a95dfe208a704b4ee171295a76
AliKhll <ali.khll.1994@gmail.com>
2016-12-04 12:38:03 +0000

    added emptyGuid method to guid utils

--- Foundation/HtmlClient/Foundation.ViewModel.HtmlClient/Implementations/guidUtils.ts
+++ Foundation/HtmlClient/Foundation.ViewModel.HtmlClient/Implementations/guidUtils.ts
@@ -7,5 +7,11 @@
             return $data.Guid['NewGuid']().value.toLowerCase();
             
         }
+
+        public emptyGuid(): string {
+
+            return "00000000-0000-0000-0000-000000000000";
+
+        }
     }
 }
