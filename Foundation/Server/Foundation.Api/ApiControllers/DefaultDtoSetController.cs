23c5013c7bd9e608b0d25e53c61c1043f8068f9c
ymoradi <ysmoradi@outlook.com>
2016-12-07 16:23:22 +0330

    Added DefaultDtoSetController for dto & models with default guid key

---
+++ Foundation/Server/Foundation.Api/ApiControllers/DefaultDtoSetController.cs
@@ -0,0 +1,23 @@
+﻿using Foundation.DataAccess.Contracts;
+using Foundation.Model.Contracts;
+using System;
+
+namespace Foundation.Api.ApiControllers
+{
+    public class DefaultDtoSetController<TModel, TDto> : DtoSetController<TModel, TDto, Guid>
+        where TDto : class, IDtoWithDefaultGuidKey
+        where TModel : class, IEntityWithDefaultGuidKey
+    {
+        protected DefaultDtoSetController()
+            : base()
+        {
+
+        }
+
+        public DefaultDtoSetController(IEntityWithDefaultGuidKeyRepository<TModel> repository)
+            : base(repository)
+        {
+
+        }
+    }
+}
