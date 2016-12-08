eadb61ef3dd81ff3fde111def2e955c572933870
ymoradi <ysmoradi@outlook.com>
2016-12-08 17:50:25 +0330

    Removed bower-babel packages and gulp tasks from project

--- IdentityServer/HtmlClient/IdentityServer.HtmlClient/gulpfile.js
+++ IdentityServer/HtmlClient/IdentityServer.HtmlClient/gulpfile.js
@@ -1,18 +1,6 @@
 ﻿/// <binding AfterBuild='less' />
 var gulp = require("gulp");
-var sourcemaps = require("gulp-sourcemaps");
-var babel = require("gulp-babel");
 var less = require("gulp-less");
-
-gulp.task("ts-babel", function () {
-    return gulp.src("identityServer.htmlClient.js")
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(babel({
-            presets: ["es2015-loose"]
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./'));
-});
 
 gulp.task("less", function () {
     gulp.src(["./view/contents/styles/*.less"])
