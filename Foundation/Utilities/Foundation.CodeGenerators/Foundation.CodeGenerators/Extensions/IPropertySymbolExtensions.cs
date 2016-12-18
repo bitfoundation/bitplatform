30b2cf080fd362f4930a26f73bb70f0a8dfab953
ymoradi <ysmoradi@outlook.com>
2016-12-18 19:19:11 +0330

    Generate $ViewType based on System.ComponentModel.DataAnnotations.DataType attribute

--- Foundation/Utilities/Foundation.CodeGenerators/Foundation.CodeGenerators/Extensions/IPropertySymbolExtensions.cs
+++ Foundation/Utilities/Foundation.CodeGenerators/Foundation.CodeGenerators/Extensions/IPropertySymbolExtensions.cs
@@ -21,12 +21,36 @@ namespace Microsoft.CodeAnalysis
 
         private static bool IsKeyByConvention(this IPropertySymbol prop)
         {
+            if (prop == null)
+                throw new ArgumentNullException(nameof(prop));
+
             return string.Compare(prop.Name, "Id", StringComparison.InvariantCultureIgnoreCase) == 0 || string.Compare(prop.Name, (prop.ContainingType?.Name + "Id"), StringComparison.InvariantCultureIgnoreCase) == 0;
         }
 
         private static bool IsKeyByConfiguration(this IPropertySymbol prop)
         {
+            if (prop == null)
+                throw new ArgumentNullException(nameof(prop));
+
             return prop.GetAttributes().Any(att => att.AttributeClass.Name == "KeyAttribute");
+        }
+
+        public static string GetViewType(this IPropertySymbol prop)
+        {
+            if (prop == null)
+                throw new ArgumentNullException(nameof(prop));
+
+            AttributeData dataTypeAtt = prop.GetAttributes()
+                .SingleOrDefault(att => att.AttributeClass.Name == "DataTypeAttribute");
+
+            if (dataTypeAtt == null)
+                return null;
+
+            int value = Convert.ToInt32(dataTypeAtt.ConstructorArguments.Single().Value);
+
+            return dataTypeAtt.ConstructorArguments.Single().Type.GetMembers()
+                .OfType<IFieldSymbol>()
+                .Single(f => Convert.ToInt32(f.ConstantValue) == value).Name;
         }
     }
 }
