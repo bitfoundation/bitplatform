75dcfd9d368109ede1ef70620e24386ea82af5ad
ymoradi <ysmoradi@outlook.com>
2016-12-21 13:47:53 +0330

    Created a promise for metadata provider init method to make sure that it's body will be executed only once.

--- Foundation/HtmlClient/Foundation.ViewModel.HtmlClient/Implementations/defaultMetadataProvider.ts
+++ Foundation/HtmlClient/Foundation.ViewModel.HtmlClient/Implementations/defaultMetadataProvider.ts
@@ -3,26 +3,44 @@ module Foundation.ViewModel.Implementations {
     @Core.Injectable()
     export class DefaultMetadataProvider implements Contracts.IMetadataProvider {
 
-        private appMetadata: Contracts.AppMetadata = null;
-
         public constructor( @Core.Inject("ClientAppProfileManager") public clientAppProfileManager: Core.ClientAppProfileManager) {
 
         }
 
+        private getMetadataPromise: Promise<Contracts.AppMetadata> = null;
+
         @Core.Log()
         public async getMetadata(): Promise<Contracts.AppMetadata> {
 
-            if (this.appMetadata == null) {
-                let response = await fetch(`Metadata/V${this.clientAppProfileManager.getClientAppProfile().version}`, { credentials: 'include' });
-                if (response.ok) {
-                    this.appMetadata = await response.json() as any;
-                }
-                else {
-                    throw new Error("Error retriving metadata");
-                }
+            if (this.getMetadataPromise == null) {
+
+                this.getMetadataPromise = new Promise<Contracts.AppMetadata>(async (resolve, reject) => {
+
+                    let appMetadata: Contracts.AppMetadata = null
+
+                    try {
+
+                        let response = await fetch(`Metadata/V${this.clientAppProfileManager.getClientAppProfile().version}`, { credentials: 'include' });
+                        if (response.ok) {
+                            appMetadata = await response.json() as any;
+                        }
+                        else {
+                            reject("Error retriving metadata");
+                        }
+                    }
+                    catch (e) {
+                        reject(e);
+                        throw e;
+                    }
+
+                    resolve(appMetadata);
+
+                });
+
             }
 
-            return this.appMetadata;
+            return this.getMetadataPromise;
+
         }
     }
 }
