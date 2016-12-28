27403ab9c18fdfe4b817c0f2666a361e0293ccb3
ymoradi <ysmoradi@outlook.com>
2016-12-28 13:36:33 +0330

    Ef core implementation for IAsyncQueryableExecuter

---
+++ Foundation/Server/Foundation.DataAccess/Implementations/EntityFrameworkCore/EfCoreAsyncQueryableExecuter.cs
@@ -0,0 +1,48 @@
+﻿using Foundation.DataAccess.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace Foundation.DataAccess.Implementations.EntityFrameworkCore
+{
+    public class EfCoreAsyncQueryableExecuter : IAsyncQueryableExecuter
+    {
+        public Task<T> FirstOrDefaultAsync<T>(IQueryable<T> source, CancellationToken cancellationToken)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source.FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public virtual Task<long> LongCountAsync<T>(IQueryable<T> source, CancellationToken cancellationToken)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source
+                .LongCountAsync(cancellationToken);
+        }
+
+        public bool SupportsAsyncExecution<T>(IQueryable source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source is EntityQueryable<T>;
+        }
+
+        public virtual Task<List<T>> ToListAsync<T>(IQueryable<T> source, CancellationToken cancellationToken)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
