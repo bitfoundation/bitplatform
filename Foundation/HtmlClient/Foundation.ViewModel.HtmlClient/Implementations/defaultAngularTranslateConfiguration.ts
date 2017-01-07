73a30480008f59e02641241a48d15290226966f5
ymoradi <ysmoradi@outlook.com>
2017-01-07 10:36:51 +0330

    Add messages of dto metadata to their static members instead of $translate. See https://github.com/bit-foundation/Announcements/issues/2

--- Foundation/HtmlClient/Foundation.ViewModel.HtmlClient/Implementations/defaultAngularTranslateConfiguration.ts
+++ Foundation/HtmlClient/Foundation.ViewModel.HtmlClient/Implementations/defaultAngularTranslateConfiguration.ts
@@ -11,19 +11,35 @@
         @Core.Log()
         public async configure(app: ng.IModule): Promise<void> {
 
+            const currentCulture = this.clientAppProfileManager.getClientAppProfile().culture;
+
             const metadata = await this.metadataProvider.getMetadata();
 
             const translates = {};
 
-            metadata.Dtos.forEach(dto => dto.MembersMetadata
-                .forEach(member => {
-                    member.Messages.forEach(m => {
-                        translates[m.Name] = translates[m.Name] || {};
-                        m.Values.forEach(v => {
-                            translates[m.Name][v.Name] = v.Title;
+            metadata.Dtos.forEach(dto => {
+
+                const parts = dto.DtoType.split(".");
+                let jayDataDtoType: any = window;
+                for (let part of parts) {
+                    jayDataDtoType = jayDataDtoType[part];
+                }
+
+                dto.MembersMetadata
+                    .forEach(member => {
+
+                        let dtoMemberMetadata = jayDataDtoType[member.DtoMemberName] = jayDataDtoType[member.DtoMemberName] || {};
+                        dtoMemberMetadata = dtoMemberMetadata || {};
+
+                        member.Messages.forEach(m => {
+                            if (m.Name == currentCulture) {
+                                m.Values.forEach(v => {
+                                    dtoMemberMetadata[v.Name] = v.Title;
+                                });
+                            }
                         });
                     });
-                }));
+            });
 
             metadata.Projects.forEach(proj => proj.Messages
                 .forEach(m => {
@@ -57,7 +73,7 @@
                     }
                 }
 
-                $translateProvider.preferredLanguage(this.clientAppProfileManager.getClientAppProfile().culture);
+                $translateProvider.preferredLanguage(currentCulture);
 
                 $translateProvider.useSanitizeValueStrategy("escape");
 
