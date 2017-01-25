97e3d8e270973e9953691cbc4207f34eb5dd6b33
Alikhll <ysmoradi@outlook.com>
2017-01-25 18:41:20 +0330

    Added BitTools.sln (BitTools.Core | BitCodeAnalyzer | BitCodeAnalyzer.Test) projects

--- BitTools/BitCodeAnalyzer/Properties/AssemblyInfo.cs
+++ BitTools/BitCodeAnalyzer/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Foundation.CSharpAnalyzers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bit Code Analyzer")]
+[assembly: AssemblyDescription("Bit Code Analyzer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bit Foundation")]
-[assembly: AssemblyProduct("Foundation.CSharpAnalyzers")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("BitCodeAnalyzer")]
+[assembly: AssemblyCopyright("Copyright © Bit Foundation 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("0.0.9.0")]
+[assembly: AssemblyFileVersion("0.0.9.0")]
--- BitTools/BitTools.Core/Properties/AssemblyInfo.cs
+++ BitTools/BitTools.Core/Properties/AssemblyInfo.cs
@@ -1,22 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Foundation.CSharpAnalyzers.Test")]
+[assembly: AssemblyTitle("BitTools.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Foundation.CSharpAnalyzers.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Bit Foundation")]
+[assembly: AssemblyProduct("BitTools.Core")]
+[assembly: AssemblyCopyright("Copyright © Bit Foundation 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.9.0")]
+[assembly: AssemblyFileVersion("0.0.9.0")]
