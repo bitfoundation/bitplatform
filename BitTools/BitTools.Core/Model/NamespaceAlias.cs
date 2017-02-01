d6ab0dd2677a196992d1904f652690c01344ccfb
ysmoradi <ysmoradi@outlook.com>
2017-02-01 17:39:01 +0330

    Moved Namespace aliases to BitConfig

--- BitTools/BitTools.Core/Model/NamespaceAlias.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BitTools.Core.Model
-{
-    public class NamespaceAlias
-    {
-        public virtual string Namespace { get; set; }
-
-        public virtual string Alias { get; set; }
-    }
-}
