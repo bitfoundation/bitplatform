1bba443e5d6b4630caa0a422ee8d18d34e2c2f0d
ymoradi <ysmoradi@outlook.com>
2017-02-02 10:31:43 +0330

    Foundation.DataAccess was renamed to Bit.Data.EntityFrameworkCore

--- Foundation/Server/Bit.Data.EntityFrameworkCore/Properties/AssemblyInfo.cs
+++ Foundation/Server/Bit.Data.EntityFrameworkCore/Properties/AssemblyInfo.cs
@@ -1,18 +1,18 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Foundation.DataAccess")]
+[assembly: AssemblyTitle("Bit.Data.EntityFrameworkCore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bit Foundation")]
-[assembly: AssemblyProduct("Bit Framework")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bit.Data.EntityFrameworkCore")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("022f3e55-04c4-4541-82fb-488bbd1cbad3")]
+[assembly: Guid("795411d0-0471-47ea-b461-5a5235752c5a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
