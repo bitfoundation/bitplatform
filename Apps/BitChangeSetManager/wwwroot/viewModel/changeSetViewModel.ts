9b9b61c1d08562ca69aacb44793f6aff2af8e697
ysmoradi <ysmoradi@outlook.com>
2017-03-06 12:51:34 +0330

    change sets rad grid toolbar & edit template + enhanced view template

---
+++ Apps/BitChangeSetManager/wwwroot/viewModel/changeSetViewModel.ts
@@ -0,0 +1,29 @@
+﻿module BitChangeSetManager.ViewModels {
+
+    @DtoRulesDependency({ name: "ChangeSetRules" })
+    export class ChangeSetRules extends DtoRules<BitChangeSetManagerModel.ChangeSetDto> {
+
+        public validateMember(memberName: keyof BitChangeSetManagerModel.ChangeSetDto, newValue: any, oldValue: any): void {
+            super.validateMember(memberName, newValue, oldValue);
+        }
+
+    }
+
+    @DtoViewModelDependency({
+        name: "ChangeSetViewModel",
+        templateUrl: `view/changeSetView.html`
+    })
+    export class ChangeSetViewModel extends DtoViewModel<BitChangeSetManagerModel.ChangeSetDto, ChangeSetRules> {
+
+        public constructor( @Inject("ChangeSetRules") public rules: ChangeSetRules) {
+            super();
+        }
+
+        @Command()
+        public async $onInit(): Promise<void> {
+
+        }
+
+    }
+
+}
