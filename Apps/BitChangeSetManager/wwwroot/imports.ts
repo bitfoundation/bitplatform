1e11aa5026d630e7d8691dd56f1d3d023c2d985e
ysmoradi <ysmoradi@outlook.com>
2017-03-06 12:51:54 +0330

    Bit change set manager updated to latest enhancements of bit

--- Apps/BitChangeSetManager/wwwroot/imports.ts
+++ Apps/BitChangeSetManager/wwwroot/imports.ts
@@ -3,11 +3,15 @@ import FoundationVM = Foundation.ViewModel;
 import FoundationView = Foundation.View;
 
 import FormViewModelDependency = FoundationCore.FormViewModelDependency;
+import DtoViewModelDependency = FoundationCore.DtoViewModelDependency;
+import DtoRulesDependency = FoundationCore.DtoRulesDependency;
 import Inject = FoundationCore.Inject;
 import IMessageReceiver = FoundationCore.Contracts.IMessageReceiver;
 import ObjectDependency = FoundationCore.ObjectDependency;
 import ClientAppProfileManager = FoundationCore.ClientAppProfileManager;
 
 import FormViewModel = FoundationVM.ViewModels.FormViewModel;
+import DtoViewModel = FoundationVM.ViewModels.DtoViewModel;
+import DtoRules = FoundationVM.Implementations.DtoRules;
 import Command = FoundationVM.Command;
 import IEntityContextProvider = FoundationVM.Contracts.IEntityContextProvider;
