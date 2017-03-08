9f3aa1d455a1b9b3bd78441a62c7740baa3e04a6
ysmoradi <ysmoradi@outlook.com>
2017-03-08 22:00:50 +0330

    Bit change set manager user model & service

---
+++ Apps/BitChangeSetManager/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using Foundation.Model.Contracts;
+using System;
+
+namespace BitChangeSetManager.Model
+{
+    public class User : IEntityWithDefaultGuidKey
+    {
+        public virtual Guid Id { get; set; }
+
+        public virtual string UserName { get; set; }
+
+        public virtual string Password { get; set; }
+    }
+}
---
+++ Apps/BitChangeSetManager/Security/BitChangeSetManagerUserService.cs
@@ -0,0 +1,151 @@
+﻿using BitChangeSetManager.DataAccess;
+using BitChangeSetManager.Model;
+using Foundation.Api.Exceptions;
+using Foundation.Core.Contracts;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services.Default;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitChangeSetManager.Security
+{
+    public class BitChangeSetManagerUserService : UserServiceBase
+    {
+        private readonly IDependencyManager _dependencyManager;
+
+        public BitChangeSetManagerUserService(IDependencyManager dependencyManager)
+        {
+            _dependencyManager = dependencyManager;
+        }
+
+        public override async Task AuthenticateLocalAsync(LocalAuthenticationContext context)
+        {
+            try
+            {
+                string username = context.UserName;
+                string password = context.Password;
+
+                if (string.IsNullOrEmpty(username))
+                    throw new ArgumentException(nameof(username));
+
+                if (string.IsNullOrEmpty(password))
+                    throw new ArgumentException(nameof(password));
+
+                using (MD5 md5Hash = MD5.Create())
+                {
+                    byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                    StringBuilder sBuilder = new StringBuilder();
+
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        sBuilder.Append(data[i].ToString("x2"));
+                    }
+
+                    password = sBuilder.ToString();
+                }
+
+                username = username.ToLower();
+
+                User user = null;
+
+                using (IDependencyResolver resolver = _dependencyManager.CreateChildDependencyResolver())
+                {
+                    IBitChangeSetManagerRepository<User> usersRepository = resolver.Resolve<IBitChangeSetManagerRepository<User>>();
+
+                    user = usersRepository.GetAll()
+                         .SingleOrDefault(u => u.UserName.ToLower() == username && u.Password == password);
+                }
+
+                if (user == null)
+                    throw new InvalidOperationException("LoginFailed");
+
+                string userId = user.Id.ToString();
+
+                List<Claim> claims = new List<Claim>
+                {
+                    new Claim("sub",userId),
+                    new Claim("primary_sid", userId),
+                    new Claim("upn", userId),
+                    new Claim("name", userId),
+                    new Claim("given_name", userId)
+                };
+
+                AuthenticateResult result = new AuthenticateResult(userId, userId,
+                    claims,
+                    authenticationMethod: "custom");
+
+                context.AuthenticateResult = result;
+
+                await base.AuthenticateLocalAsync(context);
+            }
+            catch
+            {
+                AuthenticateResult result = new AuthenticateResult("LoginFailed");
+
+                context.AuthenticateResult = result;
+            }
+        }
+
+        public override async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            User user = null;
+
+            using (IDependencyResolver resolver = _dependencyManager.CreateChildDependencyResolver())
+            {
+                IBitChangeSetManagerRepository<User> usersRepository = resolver.Resolve<IBitChangeSetManagerRepository<User>>();
+
+                Guid userId = Guid.Parse(context.Subject.Identity.Name);
+
+                user = usersRepository.GetAll()
+                     .SingleOrDefault(u => u.Id == userId);
+            }
+
+            if (user != null)
+            {
+                string userId = user.Id.ToString();
+
+                List<Claim> claims = new List<Claim>
+                {
+                    new Claim("sub", userId),
+                    new Claim("primary_sid", userId),
+                    new Claim("upn", userId),
+                    new Claim("name", userId),
+                    new Claim("given_name", userId)
+                };
+
+                context.IssuedClaims = claims;
+
+                await base.GetProfileDataAsync(context);
+            }
+            else
+            {
+                throw new ResourceNotFoundException("User could not be found");
+            }
+        }
+
+        public override async Task IsActiveAsync(IsActiveContext context)
+        {
+            User user = null;
+
+            using (IDependencyResolver resolver = _dependencyManager.CreateChildDependencyResolver())
+            {
+                IBitChangeSetManagerRepository<User> usersRepository = resolver.Resolve<IBitChangeSetManagerRepository<User>>();
+
+                Guid userId = Guid.Parse(context.Subject.Identity.Name);
+
+                user = usersRepository.GetAll()
+                     .SingleOrDefault(u => u.Id == userId);
+            }
+
+            context.IsActive = user != null;
+
+            await base.IsActiveAsync(context);
+        }
+    }
+}
