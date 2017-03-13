d8c6028ef13fe44c2b918e8f296c4ba8a27fbf27
ysmoradi <ysmoradi@outlook.com>
2017-03-13 21:37:15 +0330

    Temporary fix for null reference exception when anonymous person tries to access jobs dashboard

--- Foundation/Server/Foundation.Api/Middlewares/JobScheduler/Implementations/DefaultJobsDashboardAuthorizationFilter.cs
+++ Foundation/Server/Foundation.Api/Middlewares/JobScheduler/Implementations/DefaultJobsDashboardAuthorizationFilter.cs
@@ -10,7 +10,14 @@ namespace Foundation.Api.Middlewares.JobScheduler.Implementations
         public virtual bool Authorize([NotNull] DashboardContext context)
         {
             IOwinContext owinContext = new OwinContext(context.GetOwinEnvironment());
-            return owinContext.GetDependencyResolver().Resolve<IUserInformationProvider>().IsAuthenticated();
+            bool isAuthenticated = owinContext.GetDependencyResolver().Resolve<IUserInformationProvider>().IsAuthenticated();
+            if (isAuthenticated == false && owinContext.Authentication?.User == null)
+            {
+                // https://github.com/HangfireIO/Hangfire/issues/833
+                owinContext.Authentication.User = new System.Security.Claims.ClaimsPrincipal { };
+                owinContext.Authentication.User.AddIdentity(new System.Security.Claims.ClaimsIdentity { });
+            };
+            return isAuthenticated;
         }
     }
 }
