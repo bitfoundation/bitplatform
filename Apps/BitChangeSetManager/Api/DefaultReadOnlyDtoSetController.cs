0f857e73afc9cec85f1340f38ab864b9c5133491
ysmoradi <ysmoradi@outlook.com>
2017-03-18 17:30:16 +0330

    Added default read only dto set controller

---
+++ Apps/BitChangeSetManager/Api/DefaultReadOnlyDtoSetController.cs
@@ -0,0 +1,40 @@
+﻿using BitChangeSetManager.DataAccess;
+using BitChangeSetManager.Metadata;
+using Foundation.Api.ApiControllers;
+using Foundation.Api.Exceptions;
+using Foundation.Model.Contracts;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.OData;
+
+namespace BitChangeSetManager.Api
+{
+    public class DefaultReadOnlyDtoSetController<TModel, TDto> : DefaultDtoSetController<TModel, TDto>
+        where TDto : class, IDtoWithDefaultGuidKey
+        where TModel : class, IEntityWithDefaultGuidKey
+    {
+        private readonly IBitChangeSetManagerRepository<TModel> _repository;
+
+        public DefaultReadOnlyDtoSetController(IBitChangeSetManagerRepository<TModel> repository)
+            : base(repository)
+        {
+            _repository = repository;
+        }
+
+        public override Task<TDto> Insert(TDto dto, CancellationToken cancellationToken)
+        {
+            throw new AppException(BitChangeSetManagerMetadata.InsertIsDeined);
+        }
+
+        public override Task<TDto> Update([FromODataUri] Guid key, Delta<TDto> modifiedDtoDelta, CancellationToken cancellationToken)
+        {
+            throw new AppException(BitChangeSetManagerMetadata.UpdateIsDeined);
+        }
+
+        public override Task Delete([FromODataUri] Guid key, CancellationToken cancellationToken)
+        {
+            throw new AppException(BitChangeSetManagerMetadata.DeleteIsDeined);
+        }
+    }
+}
--- Apps/BitChangeSetManager/Metadata/BitChangeSetManagerMetadata.cs
+++ Apps/BitChangeSetManager/Metadata/BitChangeSetManagerMetadata.cs
@@ -8,6 +8,10 @@ namespace BitChangeSetManager.Metadata
 {
     public class BitChangeSetManagerMetadata : DefaultProjectMetadataBuilder
     {
+        public const string InsertIsDeined = nameof(InsertIsDeined);
+        public const string UpdateIsDeined = nameof(UpdateIsDeined);
+        public const string DeleteIsDeined = nameof(DeleteIsDeined);
+
         public override async Task<IEnumerable<ObjectMetadata>> BuildMetadata()
         {
             AddProjectMetadata(new ProjectMetadata
@@ -23,7 +27,10 @@ namespace BitChangeSetManager.Metadata
                             new EnvironmentCultureValue { Name = "Yes", Title = "Yes" },
                             new EnvironmentCultureValue { Name = "No", Title = "No" },
                             new EnvironmentCultureValue { Name = "Ok", Title = "Ok" },
-                            new EnvironmentCultureValue { Name = "Commands", Title = "Commands" }
+                            new EnvironmentCultureValue { Name = "Commands", Title = "Commands" },
+                            new EnvironmentCultureValue { Name = nameof(InsertIsDeined), Title = "Insert is denied" },
+                            new EnvironmentCultureValue { Name = nameof(UpdateIsDeined), Title = "Update is denied" },
+                            new EnvironmentCultureValue { Name = nameof(DeleteIsDeined), Title = "Delete is denied" }
                         }
                     }
                 }
