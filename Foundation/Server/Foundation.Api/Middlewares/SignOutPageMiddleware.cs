be79df2b3db60ff27b503bc97b24b34453327a1b
ysmoradi <ysmoradi@outlook.com>
2017-04-12 20:27:26 +0430

    Clear cookies in apps under virtual directories on signout is now working

--- Foundation/Server/Foundation.Api/Middlewares/SignOutPageMiddleware.cs
+++ Foundation/Server/Foundation.Api/Middlewares/SignOutPageMiddleware.cs
@@ -42,7 +42,7 @@ namespace Foundation.Api.Middlewares
                 var eqPos = cookie.indexOf('=');
                 var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
                 if(name == 'access_token' || name == 'token_type')
-                    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+                    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path={defaultPath}';
             }}
             location = '{defaultPath}';
         </script>
