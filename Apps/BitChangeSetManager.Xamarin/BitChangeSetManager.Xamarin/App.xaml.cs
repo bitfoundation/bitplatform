c8827ff774e2c8790d1f167f1c7db64a732d0ad8
ysmoradi <ysmoradi@outlook.com>
2017-04-26 17:24:16 +0430

    Xamarin mobile client of bit change set manager app

---
+++ Apps/BitChangeSetManager.Xamarin/BitChangeSetManager.Xamarin/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace BitChangeSetManager.Xamarin
+{
+    public partial class App
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+	}
+}
---
+++ Apps/BitChangeSetManager.Xamarin/BitChangeSetManager.Xamarin/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using BitChangeSetManager.Xamarin.BitChangeSetManager;
+using BitChangeSetManager.Xamarin.BitChangeSetManager.Dto;
+using IdentityModel.Client;
+using Microsoft.OData.Client;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitChangeSetManager.Xamarin
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public const string baseAddress = "http://192.168.21.42/BitChangeSetManager";
+
+        private TokenResponse _tokenResponse = null;
+
+        private async void Login_Clicked(object sender, EventArgs e)
+        {
+            using (TokenClient identityClient = new TokenClient($"{baseAddress}/core/connect/token", "BitChangeSetManager-ResOwner", "secret"))
+            {
+                _tokenResponse = await identityClient.RequestResourceOwnerPasswordAsync(UserName.Text, Password.Text, "openid profile user_info");
+
+                if (_tokenResponse.IsError)
+                {
+                    LoadData.IsEnabled = false;
+                }
+                else
+                {
+                    LoadData.IsEnabled = true;
+                }
+            }
+        }
+
+        private async void LoadData_Clicked(object sender, EventArgs e)
+        {
+            BitChangeSetManagerContext context = new BitChangeSetManagerContext(new Uri($"{baseAddress}/odata/BitChangeSetManager/"));
+
+            context.SetBearerToken(_tokenResponse);
+
+            DataServiceCollection<ChangeSetDto> changeSets = new DataServiceCollection<ChangeSetDto>(context);
+
+            ChangeSetsListView.ItemsSource = changeSets;
+
+            await changeSets.LoadQueryAsync(context.ChangeSets);
+        }
+    }
+
+    public static class Extensions
+    {
+        public static void SetBearerToken(this DataServiceContext context, TokenResponse token)
+        {
+            context.BuildingRequest += (sender, e) =>
+            {
+                if (!e.Headers.ContainsKey("Authorization"))
+                    e.Headers.Add("Authorization", $"{token.TokenType} {token.AccessToken}");
+            };
+        }
+
+        public static Task LoadQueryAsync<T>(this DataServiceCollection<T> collection, IQueryable<T> query)
+        {
+            TaskCompletionSource<object> taskCompletationSource = new TaskCompletionSource<object>();
+
+            EventHandler<LoadCompletedEventArgs> onCompleted = null;
+
+            onCompleted = new EventHandler<LoadCompletedEventArgs>((e, sender) =>
+            {
+                collection.LoadCompleted -= onCompleted;
+
+                if (sender.Cancelled)
+                    taskCompletationSource.SetCanceled();
+                else if (sender.Error != null)
+                    taskCompletationSource.SetException(sender.Error);
+                else
+                    taskCompletationSource.SetResult(null);
+            });
+
+            collection.LoadCompleted += onCompleted;
+
+            collection.LoadAsync(query);
+
+            return taskCompletationSource.Task;
+        }
+    }
+}
