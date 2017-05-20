84bc1560619830e2ca7682db03df1abdfa1550d0
ysmoradi <ysmoradi@outlook.com>
2017-05-20 15:20:27 +0430

    Simplified repositories in bit change set manager

--- Apps/BitChangeSetManager/DataAccess/BitChangeSetManagerEfRepository.cs
+++ Apps/BitChangeSetManager/DataAccess/BitChangeSetManagerEfRepository.cs
@@ -1,8 +1,5 @@
 ﻿using Bit.Data.EntityFramework.Implementations;
 using Foundation.Model.Contracts;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Collections.Generic;
 
 namespace BitChangeSetManager.DataAccess
 {
@@ -12,77 +9,6 @@ namespace BitChangeSetManager.DataAccess
         public BitChangeSetManagerEfRepository(BitChangeSetManagerDbContext dbContext)
             : base(dbContext)
         {
-        }
-
-        public override TEntity Add(TEntity entityToAdd)
-        {
-            OnAddingAsync(new[] { entityToAdd }).Wait();
-
-            return base.Add(entityToAdd);
-        }
-
-        public override async Task<TEntity> AddAsync(TEntity entityToAdd, CancellationToken cancellationToken)
-        {
-            await OnAddingAsync(new[] { entityToAdd });
-
-            return await base.AddAsync(entityToAdd, cancellationToken);
-        }
-
-        public override IEnumerable<TEntity> AddRange(IEnumerable<TEntity> entitiesToAdd)
-        {
-            OnAddingAsync(entitiesToAdd).Wait();
-
-            return base.AddRange(entitiesToAdd);
-        }
-
-        public override async Task<IEnumerable<TEntity>> AddRangeAsync(IEnumerable<TEntity> entitiesToAdd, CancellationToken cancellationToken)
-        {
-            await OnAddingAsync(entitiesToAdd);
-
-            return await base.AddRangeAsync(entitiesToAdd, cancellationToken);
-        }
-
-        protected virtual async Task OnAddingAsync(IEnumerable<TEntity> entities)
-        {
-
-        }
-
-        public override TEntity Update(TEntity entityToUpdate)
-        {
-            OnUpdatingAsync(new[] { entityToUpdate }).Wait();
-
-            return base.Update(entityToUpdate);
-        }
-
-        public override async Task<TEntity> UpdateAsync(TEntity entityToUpdate, CancellationToken cancellationToken)
-        {
-            await OnUpdatingAsync(new[] { entityToUpdate });
-
-            return await base.UpdateAsync(entityToUpdate, cancellationToken);
-        }
-
-        protected virtual async Task OnUpdatingAsync(IEnumerable<TEntity> entities)
-        {
-
-        }
-
-        public override TEntity Delete(TEntity entityToDelete)
-        {
-            OnDeletingAsync(new[] { entityToDelete }).Wait();
-
-            return base.Delete(entityToDelete);
-        }
-
-        public override async Task<TEntity> DeleteAsync(TEntity entityToDelete, CancellationToken cancellationToken)
-        {
-            await OnDeletingAsync(new[] { entityToDelete });
-
-            return await base.DeleteAsync(entityToDelete, cancellationToken);
-        }
-
-        protected virtual async Task OnDeletingAsync(IEnumerable<TEntity> entities)
-        {
-
         }
     }
 }
--- Apps/BitChangeSetManager/DataAccess/ChangeSetRepository.cs
+++ Apps/BitChangeSetManager/DataAccess/ChangeSetRepository.cs
@@ -1,6 +1,9 @@
 ﻿using BitChangeSetManager.Model;
 using Foundation.Core.Contracts;
-using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BitChangeSetManager.DataAccess
@@ -13,21 +16,37 @@ namespace BitChangeSetManager.DataAccess
     public class ChangeSetRepository : BitChangeSetManagerEfRepository<ChangeSet>, IChangeSetRepository
     {
         private readonly IDateTimeProvider _dateTimeProvider;
+        private readonly BitChangeSetManagerDbContext _dbContext;
 
         public ChangeSetRepository(IDateTimeProvider dateTimeProvider, BitChangeSetManagerDbContext dbContext)
             : base(dbContext)
         {
             _dateTimeProvider = dateTimeProvider;
+            _dbContext = dbContext;
         }
 
-        protected override async Task OnAddingAsync(IEnumerable<ChangeSet> entities)
+        public override void SaveChanges()
         {
-            foreach (ChangeSet changeSet in entities)
-            {
-                changeSet.CreatedOn = _dateTimeProvider.GetCurrentUtcDateTime();
-            }
+            SetCreatedOnValue();
 
-            await base.OnAddingAsync(entities);
+            base.SaveChanges();
+        }
+
+        public override async Task SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            SetCreatedOnValue();
+
+            await base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void SetCreatedOnValue()
+        {
+            _dbContext.ChangeTracker.DetectChanges();
+
+            foreach (DbEntityEntry<ChangeSet> changeSet in _dbContext.ChangeTracker.Entries<ChangeSet>().Where(e => e.State == EntityState.Added))
+            {
+                changeSet.Entity.CreatedOn = _dateTimeProvider.GetCurrentUtcDateTime();
+            }
         }
     }
 }
