36c3c9109dfeb3270acc0272a85cf3313daab109
ysmoradi <ysmoradi@outlook.com>
2017-05-29 20:22:19 +0430

    using System.Net; added to DefaultWebApiODataControllerActionSelector

--- Foundation/Server/Bit.Api/Middlewares/WebApi/OData/Implementations/DefaultWebApiODataControllerActionSelector.cs
+++ Foundation/Server/Bit.Api/Middlewares/WebApi/OData/Implementations/DefaultWebApiODataControllerActionSelector.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using Bit.Api.ApiControllers;
+using System.Net;
 
 namespace Bit.Api.Middlewares.WebApi.OData.Implementations
 {
