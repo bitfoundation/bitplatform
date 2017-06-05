9128fd08b8d4dc955c72ebedeeaf8db3b82a83a7
ysmoradi <ysmoradi@outlook.com>
2017-06-05 15:41:41 +0430

    Refactored color viewer directive

--- Foundation/HtmlClient/Foundation.View.HtmlClient/directives/defaultColorViewerDirective.ts
+++ Foundation/HtmlClient/Foundation.View.HtmlClient/directives/defaultColorViewerDirective.ts
@@ -2,17 +2,29 @@
 /// <reference path="../../foundation.viewmodel.htmlclient/foundation.viewmodel.d.ts" />
 
 module Foundation.View.Directives {
-    @Core.DirectiveDependency({ name: "colorViewer", usesOldStyle: true })
-    export class DefaultColorViewerDirective implements ViewModel.Contracts.IDirective {
-        public getDirectiveFactory(): ng.IDirectiveFactory {
-            return () => ({
-                scope: false,
-                replace: true,
-                terminal: true,
-                template: ($element: JQuery, $attrs: ng.IAttributes) => {
-                    return `<div style='background-color:${$attrs["color"]}; color:transparent;'>color</div>`;
-                }
-            });
+    @Core.DirectiveDependency({
+        name: "ColorViewer",
+        scope: true,
+        bindToController: {
+            color: "<"
+        },
+        controllerAs: "colorViewer",
+        template: ($element: JQuery, $attrs: ng.IAttributes & { kButtons: string, kPreview: string, kInput: string }) => {
+            return `<div></div>`;
+        },
+        replace: true,
+        terminal: true,
+        restrict: "E"
+    })
+    export class DefaultColorViewerDirective {
+
+        public constructor( @Core.Inject("$element") public $element: JQuery) {
+
+        }
+
+        public $onChanges(changes: { color: { currentValue: string } }) {
+            if (changes.color != null)
+                this.$element.css("background-color", changes.color.currentValue);
         }
     }
 }
