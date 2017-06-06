47146e8c1eb7cbb19d2c0488fcb7bc2606fd1ce0
ysmoradi <ysmoradi@outlook.com>
2017-06-06 15:33:18 +0430

    Don't use types from System.Web assembly analyzer + tests

---
+++ BitTools/BitCodeAnalyzer.Test/SystemAnalyzers/WebAnalyzers/DoNotUseSystemWebAssemblyAnalyzerTests.cs
@@ -0,0 +1,58 @@
+﻿using BitCodeAnalyzer.SystemAnalyzers;
+using BitCodeAnalyzer.SystemAnalyzers.WebAnalyzers;
+using BitCodeAnalyzer.SystemCodeFixes;
+using BitCodeAnalyzer.Test.Helpers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+using System.Web.Hosting;
+
+namespace BitCodeAnalyzer.Test.SystemAnalyzers
+{
+    [TestClass]
+    public class DoNotUseSystemWebAssemblyAnalyzerTests : CodeFixVerifier
+    {
+        [TestMethod]
+        [TestCategory("Analyzer")]
+        public void FindSystemWebUsages()
+        {
+            typeof(HostingEnvironment).GetTypeInfo();
+
+            const string sourceCodeWithSystemWebUsage = @"
+    using System;
+
+    namespace MyNamespace
+    {
+        class MyClass
+        {   
+            public MyClass()
+            {
+                bool isHosted = System.Web.Hosting.HostingEnvironment.IsHosted;
+            }
+        }
+    }";
+            DiagnosticResult firstSystemWebUsage = new DiagnosticResult
+            {
+                Id = nameof(DoNotUseSystemWebAssemblyAnalyzer),
+                Message = DoNotUseSystemWebAssemblyAnalyzer.Message,
+                Severity = DiagnosticSeverity.Error,
+                Locations = new[] { new DiagnosticResultLocation(10, 52) }
+            };
+
+            VerifyCSharpDiagnostic(sourceCodeWithSystemWebUsage, firstSystemWebUsage);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new DoNotUseSystemWebAssemblyAnalyzer();
+        }
+    }
+}
---
+++ BitTools/BitCodeAnalyzer/SystemAnalyzers/WebAnalyzers/DoNotUseSystemWebAssemblyAnalyzer.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace BitCodeAnalyzer.SystemAnalyzers.WebAnalyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DoNotUseSystemWebAssemblyAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = nameof(DoNotUseSystemWebAssemblyAnalyzer);
+
+        public const string Title = "Don not use System.Web assembly types in your codes";
+        public const string Message = "Most System.Web Types won't work in ASP.NET Core.";
+        public const string Description = Message;
+        private const string Category = "System";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeSyntax, SyntaxKind.IdentifierName);
+        }
+
+        private void AnalyzeSyntax(SyntaxNodeAnalysisContext context)
+        {
+            SyntaxNode root = context.Node;
+
+            if (!(root is IdentifierNameSyntax))
+                return;
+
+            root = (IdentifierNameSyntax)context.Node;
+
+            ISymbol usageSymbol = context.SemanticModel.GetSymbolInfo(root).Symbol;
+
+            if (usageSymbol == null)
+                return;
+
+            if (usageSymbol.Kind == SymbolKind.NamedType && usageSymbol.ContainingAssembly?.Name == "System.Web")
+            {
+                Diagnostic diagn = Diagnostic.Create(Rule, root.GetLocation(), Message);
+
+                context.ReportDiagnostic(diagn);
+            }
+        }
+    }
+}
