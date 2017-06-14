7580f716406535e3ca344773437d9f75d3711c53
ysmoradi <ysmoradi@outlook.com>
2017-06-14 22:50:23 +0430

    Removed default metadata middleware

--- src/Server/Bit.Owin/Middlewares/MetadataMiddlewareConfiguration.cs
+++ src/Server/Bit.Owin/Middlewares/MetadataMiddlewareConfiguration.cs
@@ -39,14 +39,6 @@ namespace Bit.Owin.Middlewares
                 innerApp.UseXDownloadOptions();
                 innerApp.Use<MetadatMiddleware>();
             });
-
-            owinApp.Map("/Metadata", innerApp =>
-            {
-                innerApp.Use<OwinNoCacheResponseMiddleware>();
-                innerApp.UseXContentTypeOptions();
-                innerApp.UseXDownloadOptions();
-                innerApp.Use<MetadatMiddleware>();
-            });
         }
     }
 }
