69f5bf537b707f337960ecd0298a909fd23777f7
ysmoradi <ysmoradi@outlook.com>
2017-06-21 15:10:22 +0430

    Added debug log store

---
+++ src/Server/Bit.Owin/Implementations/DebugLogStore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bit.Core.Contracts;
+using Bit.Core.Models;
+using System.Diagnostics;
+
+namespace Bit.Owin.Implementations
+{
+    public class DebugLogStore : ILogStore
+    {
+        private readonly IContentFormatter _formatter;
+
+        public DebugLogStore(IContentFormatter formatter)
+        {
+            _formatter = formatter;
+        }
+
+        protected DebugLogStore()
+        {
+
+        }
+
+        public virtual void SaveLog(LogEntry logEntry)
+        {
+            Debug.WriteLine(_formatter.Serialize(logEntry) + Environment.NewLine);
+        }
+
+        public virtual async Task SaveLogAsync(LogEntry logEntry)
+        {
+            Debug.WriteLine(_formatter.Serialize(logEntry) + Environment.NewLine);
+        }
+    }
+}
