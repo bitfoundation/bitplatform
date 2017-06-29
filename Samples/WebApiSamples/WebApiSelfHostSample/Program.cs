7e71370071be9b20f1ddf62157ef3048b5f4d288
ysmoradi <ysmoradi@outlook.com>
2017-06-29 13:57:05 +0430

    Simple web api sample

---
+++ Samples/WebApiSamples/WebApiSelfHostSample/Program.cs
@@ -0,0 +1,118 @@
+﻿using Bit.Core;
+using Bit.Core.Contracts;
+using Bit.Owin;
+using Bit.Owin.Contracts;
+using Bit.Owin.Implementations;
+using Bit.Owin.Middlewares;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiSelfHostSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<AppStartup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+
+        }
+    }
+
+    public class ValuesController : ApiController
+    {
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5 
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class AppStartup : OwinAppStartup, IDependenciesManager, IDependenciesManagerProvider
+    {
+        public override void Configuration(IAppBuilder owinApp)
+        {
+            DefaultDependenciesManagerProvider.Current = this;
+
+            base.Configuration(owinApp);
+        }
+
+        public IEnumerable<IDependenciesManager> GetDependenciesManagers()
+        {
+            yield return this;
+        }
+
+        public void ConfigureDependencies(IDependencyManager dependencyManager)
+        {
+            AssemblyContainer.Current.Init();
+
+            dependencyManager.RegisterMinimalDependencies();
+
+            dependencyManager.RegisterInstance(DefaultAppEnvironmentProvider.Current);
+            dependencyManager.RegisterInstance(DefaultJsonContentFormatter.Current);
+            dependencyManager.RegisterInstance(DefaultPathProvider.Current);
+
+            dependencyManager.Register<IRequestInformationProvider, DefaultRequestInformationProvider>();
+
+            dependencyManager.Register<ILogger, DefaultLogger>();
+
+            if (DefaultAppEnvironmentProvider.Current.GetActiveAppEnvironment().DebugMode == true)
+                dependencyManager.RegisterLogStore<DebugLogStore>();
+            dependencyManager.RegisterLogStore<ConsoleLogStore>();
+
+            dependencyManager.RegisterDefaultOwinApp();
+
+            dependencyManager.RegisterOwinMiddleware<AutofacDependencyInjectionMiddlewareConfiguration>();
+            dependencyManager.RegisterOwinMiddleware<OwinExceptionHandlerMiddlewareConfiguration>();
+            dependencyManager.RegisterOwinMiddleware<LogRequestInformationMiddlewareConfiguration>();
+
+            dependencyManager.RegisterDefaultWebApiConfiguration();
+
+            dependencyManager.RegisterUsing<IOwinMiddlewareConfiguration>(() =>
+            {
+                return dependencyManager.CreateChildDependencyResolver(childDependencyManager =>
+                {
+                    childDependencyManager.RegisterWebApiMiddlewareUsingDefaultConfiguration("WebApi");
+                }).Resolve<IOwinMiddlewareConfiguration>("WebApi");
+
+            }, lifeCycle: DependencyLifeCycle.SingleInstance, overwriteExciting: false);
+        }
+    }
+}
