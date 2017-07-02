2fb1c003216b082562e48943923c904f52e9ef9b
ysmoradi <ysmoradi@outlook.com>
2017-07-02 17:18:28 +0430

    Added ODataPushContentStream & ODataContentStream classes

--- src/Server/Bit.OData/Contents/ODataStreamContent.cs
+++ src/Server/Bit.OData/Contents/ODataStreamContent.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Bit.OData.Contents
 {
-    public class ODataStreamContent : PushStreamContent
+    public class ODataStreamContent : StreamContent
     {
-        public ODataStreamContent(Func<Stream, Task> onStreamAvailable, CancellationToken cancellationToken)
-            : base(async (stream, content, transport) =>
-            {
-                if (onStreamAvailable == null)
-                    throw new ArgumentNullException(nameof(onStreamAvailable));
-
-                using (stream)
-                {
-                    await onStreamAvailable(stream);
-                    await stream.FlushAsync(cancellationToken);
-                }
-
-            }, "application/json")
+        public ODataStreamContent(Stream content)
+            : base(content)
         {
+            Headers.ContentType.MediaType = "application/json";
+            Headers.ContentType.Parameters.Add(new NameValueHeaderValue("odata.metadata", "minimal"));
+        }
+
+        public ODataStreamContent(Stream content, int bufferSize)
+            : base(content, bufferSize)
+        {
+            Headers.ContentType.MediaType = "application/json";
             Headers.ContentType.Parameters.Add(new NameValueHeaderValue("odata.metadata", "minimal"));
         }
     }
