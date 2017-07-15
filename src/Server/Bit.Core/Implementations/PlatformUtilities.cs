e1935f1a8f1462b3bcbd534ec7fedda2528f4447
ysmoradi <ysmoradi@outlook.com>
2017-07-15 13:45:52 +0430

    Added initial platform utilities helper class

---
+++ src/Server/Bit.Core/Implementations/PlatformUtilities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Bit.Core.Implementations
+{
+    public class PlatformUtilities
+    {
+        private static readonly Lazy<bool> _isRunningMono = new Lazy<bool>(() =>
+        {
+            try
+            {
+                return Type.GetType("Mono.Runtime") != null;
+            }
+            catch
+            {
+                return false;
+            }
+        });
+
+        public static bool IsRunningOnMono
+        {
+            get
+            {
+                return _isRunningMono.Value;
+            }
+        }
+
+        public static bool IsRunningOnDotNetCore
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
