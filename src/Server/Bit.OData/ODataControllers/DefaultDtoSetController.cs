5a418f3d1e8b980d933029fd6f7c466b84261834
ysmoradi <ysmoradi@outlook.com>
2017-07-22 12:56:50 +0430

    Overrides methods in default dto set controller

--- src/Server/Bit.OData/ODataControllers/DefaultDtoSetController.cs
+++ src/Server/Bit.OData/ODataControllers/DefaultDtoSetController.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
 using Bit.Data.Contracts;
 using Bit.Model.Contracts;
 
@@ -18,6 +23,42 @@ namespace Bit.OData.ODataControllers
             : base(repository)
         {
 
+        }
+
+        [Create]
+        public override Task<TDto> Create(TDto dto, CancellationToken cancellationToken)
+        {
+            return base.Create(dto, cancellationToken);
+        }
+
+        [Update]
+        public override Task<TDto> Update(Guid key, TDto dto, CancellationToken cancellationToken)
+        {
+            return base.Update(key, dto, cancellationToken);
+        }
+
+        [PartialUpdate]
+        public override Task<TDto> PartialUpdate(Guid key, Delta<TDto> modifiedDtoDelta, CancellationToken cancellationToken)
+        {
+            return base.PartialUpdate(key, modifiedDtoDelta, cancellationToken);
+        }
+
+        [Get]
+        public override Task<SingleResult<TDto>> Get(Guid key, CancellationToken cancellationToken)
+        {
+            return base.Get(key, cancellationToken);
+        }
+
+        [Get]
+        public override Task<IQueryable<TDto>> GetAll(CancellationToken cancellationToken)
+        {
+            return base.GetAll(cancellationToken);
+        }
+
+        [Delete]
+        public override Task Delete(Guid key, CancellationToken cancellationToken)
+        {
+            return base.Delete(key, cancellationToken);
         }
     }
 }
