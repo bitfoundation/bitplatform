f58b6c987cffc25fef3db95b08a1dabcb4bfe2f2
ysmoradi <ysmoradi@outlook.com>
2017-07-25 21:07:54 +0430

    DefaultWebApiODataControllerActionSelector now handles Get|Creatre|Update|PartialUpdate|Delete attribute better

--- src/Server/Bit.OData/Implementations/DefaultWebApiODataControllerActionSelector.cs
+++ src/Server/Bit.OData/Implementations/DefaultWebApiODataControllerActionSelector.cs
@@ -1,19 +1,30 @@
-﻿using System;
+﻿using Bit.OData.ODataControllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Reflection;
 using System.Threading.Tasks;
-using System.Web.Http;
 using System.Web.Http.Controllers;
-using Bit.OData.ODataControllers;
 
 namespace Bit.OData.Implementations
 {
     public class DefaultWebApiODataControllerActionSelector : ApiControllerActionSelector, IHttpActionSelector
     {
-        private readonly FieldInfo _actionNameFieldName = typeof(ReflectedHttpActionDescriptor).GetTypeInfo().GetField("_actionName",
-            BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly FieldInfo _cacheKeyField;
+
+        private static readonly FieldInfo _actionNameFieldName;
+
+        private static readonly TypeInfo cacheType;
+
+        private static readonly FieldInfo _actionParameterNamesField;
+
+        static DefaultWebApiODataControllerActionSelector()
+        {
+            _cacheKeyField = typeof(ApiControllerActionSelector).GetTypeInfo().GetField("_cacheKey", BindingFlags.NonPublic | BindingFlags.Instance);
+            cacheType = typeof(ApiControllerActionSelector).GetTypeInfo().Assembly.GetType("System.Web.Http.Controllers.ApiControllerActionSelector+ActionSelectorCacheItem").GetTypeInfo();
+            _actionParameterNamesField = cacheType.GetField("_actionParameterNames", BindingFlags.NonPublic | BindingFlags.Instance);
+            _actionNameFieldName = typeof(ReflectedHttpActionDescriptor).GetTypeInfo().GetField("_actionName", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
 
         private class HttpActionDescriptorAndKey
         {
@@ -25,45 +36,30 @@ namespace Bit.OData.Implementations
         {
             ILookup<string, HttpActionDescriptor> actionMappings = base.GetActionMapping(controllerDescriptor);
 
+            if (controllerDescriptor.Properties.Any() && !controllerDescriptor.Properties.TryGetValue("ApplyBitCRUDAttributesCalled", out object applyBitCRUDAttributesCalled))
+            {
+                object cacheKey = _cacheKeyField.GetValue(this);
+
+                if (cacheKey != null && controllerDescriptor.Properties.TryGetValue(cacheKey, out object cache))
+                {
+                    IDictionary<HttpActionDescriptor, string[]> actions = (IDictionary<HttpActionDescriptor, string[]>)_actionParameterNamesField.GetValue(cache);
+
+                    foreach (HttpActionDescriptor action in actions.Keys)
+                    {
+                        ApplyBitCRUDAttributes(action.ActionName, action);
+                    }
+
+                    controllerDescriptor.Properties.TryAdd("ApplyBitCRUDAttributesCalled", true);
+                }
+            }
+
             List<HttpActionDescriptorAndKey> actionMappingsList = new List<HttpActionDescriptorAndKey>();
 
             foreach (IGrouping<string, HttpActionDescriptor> aMaps in actionMappings)
             {
                 foreach (HttpActionDescriptor httpActionDescriptor in aMaps)
                 {
-                    IActionHttpMethodProvider actionHttpMethodProvider =
-                        httpActionDescriptor.GetCustomAttributes<IActionHttpMethodProvider>()
-                        .FirstOrDefault();
-
-                    string actionName = aMaps.Key;
-
-                    if (actionHttpMethodProvider != null)
-                    {
-                        if (actionHttpMethodProvider is GetAttribute)
-                            actionName = "Get";
-                        else if (actionHttpMethodProvider is PartialUpdateAttribute)
-                        {
-                            actionName = "Patch";
-                            if (httpActionDescriptor.ReturnType.GetTypeInfo() == typeof(void).GetTypeInfo() || httpActionDescriptor.ReturnType.GetTypeInfo() == typeof(Task).GetTypeInfo())
-                                throw new InvalidOperationException("Patch | PartialUpdate must have a return type other than Task or void");
-                        }
-                        else if (actionHttpMethodProvider is CreateAttribute)
-                        {
-                            actionName = "Post";
-                            if (httpActionDescriptor.ReturnType.GetTypeInfo() == typeof(void).GetTypeInfo() || httpActionDescriptor.ReturnType == typeof(Task).GetTypeInfo())
-                                throw new InvalidOperationException("Post | Create must have a return type other than Task or void");
-                        }
-                        else if (actionHttpMethodProvider is UpdateAttribute)
-                        {
-                            actionName = "Put";
-                            if (httpActionDescriptor.ReturnType.GetTypeInfo() == typeof(void).GetTypeInfo() || httpActionDescriptor.ReturnType == typeof(Task).GetTypeInfo())
-                                throw new InvalidOperationException("Put | Update must have a return type other than Task or void");
-                        }
-                        else if (actionHttpMethodProvider is DeleteAttribute)
-                            actionName = "Delete";
-
-                        _actionNameFieldName.SetValue(httpActionDescriptor, actionName);
-                    }
+                    string actionName = ApplyBitCRUDAttributes(aMaps.Key, httpActionDescriptor);
 
                     actionMappingsList.Add(new HttpActionDescriptorAndKey
                     {
@@ -76,39 +72,42 @@ namespace Bit.OData.Implementations
             return actionMappingsList.ToLookup(httpActionDescriptorAndKey => httpActionDescriptorAndKey.Key, httpActionDescriptorAndKey => httpActionDescriptorAndKey.HttpActionDescriptor);
         }
 
-        public override HttpActionDescriptor SelectAction(HttpControllerContext controllerContext)
+        private string ApplyBitCRUDAttributes(string actionName, HttpActionDescriptor httpActionDescriptor)
         {
-            try
-            {
-                return base.SelectAction(controllerContext);
-            }
-            catch (HttpResponseException ex) when (ex.Response?.StatusCode == HttpStatusCode.NotFound)
-            {
-                object action = null;
+            string newActionName = actionName;
 
-                if (controllerContext?.RouteData?.Values?.TryGetValue("action", out action) == true)
+            IActionHttpMethodProvider actionHttpMethodProvider = httpActionDescriptor.GetCustomAttributes<IActionHttpMethodProvider>().FirstOrDefault();
+
+            if (actionHttpMethodProvider != null)
+            {
+                if (actionHttpMethodProvider is GetAttribute)
+                    newActionName = "Get";
+                else if (actionHttpMethodProvider is PartialUpdateAttribute)
                 {
-                    string actionName = Convert.ToString(action);
-
-                    if (actionName == "Patch" || actionName == "Put" || actionName == "Post")
-                    {
-                        if (actionName == "Patch")
-                            actionName = "PartialUpdate";
-                        else if (actionName == "Put")
-                            actionName = "Update";
-                        else if (actionName == "Post")
-                            actionName = "Create";
-                        else if (actionName == "Get")
-                            actionName = "GetAll";
-
-                        controllerContext.RouteData.Values["action"] = actionName;
-
-                        return base.SelectAction(controllerContext);
-                    }
+                    newActionName = "Patch";
+                    if (httpActionDescriptor.ReturnType.GetTypeInfo() == typeof(void).GetTypeInfo() || httpActionDescriptor.ReturnType.GetTypeInfo() == typeof(Task).GetTypeInfo())
+                        throw new InvalidOperationException("Patch | PartialUpdate must have a return type other than Task or void");
                 }
+                else if (actionHttpMethodProvider is CreateAttribute)
+                {
+                    newActionName = "Post";
+                    if (httpActionDescriptor.ReturnType.GetTypeInfo() == typeof(void).GetTypeInfo() || httpActionDescriptor.ReturnType == typeof(Task).GetTypeInfo())
+                        throw new InvalidOperationException("Post | Create must have a return type other than Task or void");
+                }
+                else if (actionHttpMethodProvider is UpdateAttribute)
+                {
+                    newActionName = "Put";
+                    if (httpActionDescriptor.ReturnType.GetTypeInfo() == typeof(void).GetTypeInfo() || httpActionDescriptor.ReturnType == typeof(Task).GetTypeInfo())
+                        throw new InvalidOperationException("Put | Update must have a return type other than Task or void");
+                }
+                else if (actionHttpMethodProvider is DeleteAttribute)
+                    newActionName = "Delete";
 
-                throw;
+                if (newActionName != actionName)
+                    _actionNameFieldName.SetValue(httpActionDescriptor, newActionName);
             }
+
+            return newActionName;
         }
     }
 }
