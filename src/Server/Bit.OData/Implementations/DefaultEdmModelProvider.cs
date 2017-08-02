5b17a4ceaef93520d60993392586bc86e7677022
ysmoradi <ysmoradi@outlook.com>
2017-08-02 21:06:37 +0430

    Added DefaultEdmModelProvider

---
+++ src/Server/Bit.OData/Implementations/DefaultEdmModelProvider.cs
@@ -0,0 +1,34 @@
+﻿using Bit.Core;
+using Bit.OData.Contracts;
+using System.Reflection;
+using System.Web.OData.Builder;
+
+namespace Bit.OData.Implementations
+{
+    public abstract class DefaultEdmModelProvider : IEdmModelProvider
+    {
+#if DEBUG
+        protected DefaultEdmModelProvider()
+        {
+
+        }
+#endif
+
+        private readonly IAutoEdmBuilder _autoEdmBuilder;
+
+        public DefaultEdmModelProvider(IAutoEdmBuilder autoEdmBuilder)
+        {
+            _autoEdmBuilder = autoEdmBuilder;
+        }
+
+        public virtual void BuildEdmModel(ODataModelBuilder edmModelBuilder)
+        {
+            foreach (Assembly appAssembly in AssemblyContainer.Current.AssembliesWithDefaultAssemblies())
+            {
+                _autoEdmBuilder.AutoBuildEdmFromAssembly(appAssembly, edmModelBuilder);
+            }
+        }
+
+        public abstract string GetEdmName();
+    }
+}
