55980b80bf7c14cc83a91b20352f9aa6bb924777
ysmoradi <ysmoradi@outlook.com>
2017-08-29 13:43:48 +0430

    Added initial .net core sample app

---
+++ src/Server/DotNetCoreTestApp/AppStartup.cs
@@ -0,0 +1,192 @@
+﻿using AutoMapper;
+using Bit.Core;
+using Bit.Core.Contracts;
+using Bit.Data.Contracts;
+using Bit.Data.EntityFrameworkCore.Contracts;
+using Bit.Data.EntityFrameworkCore.Implementations;
+using Bit.Model.Contracts;
+using Bit.Model.Implementations;
+using Bit.OData.Contracts;
+using Bit.OData.Implementations;
+using Bit.OData.ODataControllers;
+using Bit.Owin.Implementations;
+using Bit.OwinCore;
+using Bit.OwinCore.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.Application;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.OData.Builder;
+
+namespace DotNetCoreTestApp
+{
+    public class AppStartup : AutofacAspNetCoreAppStartup, IAspNetCoreDependenciesManager, IDependenciesManagerProvider
+    {
+        public AppStartup(IServiceProvider serviceProvider)
+            : base(serviceProvider)
+        {
+
+        }
+
+        public override IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            DefaultDependenciesManagerProvider.Current = this;
+
+            return base.ConfigureServices(services);
+        }
+
+        public IEnumerable<IDependenciesManager> GetDependenciesManagers()
+        {
+            yield return this;
+        }
+
+        public virtual void ConfigureDependencies(IServiceProvider serviceProvider, IServiceCollection services, IDependencyManager dependencyManager)
+        {
+            AssemblyContainer.Current.Init();
+
+            dependencyManager.RegisterMinimalDependencies();
+
+            dependencyManager.RegisterDefaultLogger(typeof(DebugLogStore).GetTypeInfo(), typeof(ConsoleLogStore).GetTypeInfo());
+
+            dependencyManager.RegisterDefaultOwinCoreApp();
+
+            dependencyManager.RegisterMinimalOwinCoreMiddlewares();
+
+            dependencyManager.RegisterDefaultWebApiAndODataConfiguration();
+
+            dependencyManager.RegisterWebApiMiddleware(webApiDependencyManager =>
+            {
+                webApiDependencyManager.RegisterGlobalWebApiActionFiltersUsing(httpConfiguration =>
+                {
+                    httpConfiguration.EnableSwagger(c =>
+                    {
+                        c.SingleApiVersion("v1", "Test-Api");
+                        c.ApplyDefaultApiConfig(httpConfiguration);
+                    }).EnableSwaggerUi();
+                });
+
+                webApiDependencyManager.RegisterWebApiMiddlewareUsingDefaultConfiguration();
+            });
+
+            dependencyManager.RegisterODataMiddleware(odataDependencyManager =>
+            {
+                odataDependencyManager.RegisterODataServiceBuilder<BitODataServiceBuilder>();
+                odataDependencyManager.RegisterODataServiceBuilder<TestODataServiceBuilder>();
+
+                odataDependencyManager.RegisterGlobalWebApiActionFiltersUsing(httpConfiguration =>
+                {
+                    httpConfiguration.EnableSwagger(c =>
+                    {
+                        c.SingleApiVersion("v1", "Test-Api");
+                        c.ApplyDefaultODataConfig(httpConfiguration);
+                    }).EnableSwaggerUi();
+                });
+
+                odataDependencyManager.RegisterWebApiODataMiddlewareUsingDefaultConfiguration();
+            });
+
+            dependencyManager.RegisterSignalRMiddlewareUsingDefaultConfiguration();
+
+            dependencyManager.RegisterGeneric(typeof(IRepository<>).GetTypeInfo(), typeof(TestEfRepository<>).GetTypeInfo());
+
+            dependencyManager.RegisterEfCoreDbContext<TestDbContext, InMemoryDbContextObjectsProvider>();
+
+            dependencyManager.RegisterDtoEntityMapper();
+
+            dependencyManager.RegisterDtoEntityMapperConfiguration<DefaultDtoEntityMapperConfiguration>();
+            dependencyManager.RegisterDtoEntityMapperConfiguration<TestDtoEntityMapperConfiguration>();
+        }
+    }
+
+    public class TestODataServiceBuilder : IODataServiceBuilder
+    {
+        public IAutoODataModelBuilder AutoODataModelBuilder { get; set; }
+
+        public virtual void BuildModel(ODataModelBuilder oDataModelBuilder)
+        {
+            AutoODataModelBuilder.AutoBuildODatModelFromAssembly(typeof(TestODataServiceBuilder).GetTypeInfo().Assembly, oDataModelBuilder);
+        }
+
+        public virtual string GetODataRoute()
+        {
+            return "Test";
+        }
+    }
+
+    public class TestDbContext : EfCoreDbContextBase
+    {
+        public TestDbContext()
+            : base(new DbContextOptionsBuilder<TestDbContext>().UseInMemoryDatabase("test").Options)
+        {
+
+        }
+
+        public TestDbContext(IDbContextObjectsProvider dbContextCreationOptionsProvider)
+              : base("test", dbContextCreationOptionsProvider)
+        {
+
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+    }
+
+    public class Customer : IEntityWithDefaultKey<int>
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string FirstName { get; set; }
+
+        public virtual string LastName { get; set; }
+    }
+
+    public class CustomerDto : IDtoWithDefaultKey<int>
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string FullName { get; set; }
+    }
+
+    public class TestDtoEntityMapperConfiguration : IDtoEntityMapperConfiguration
+    {
+        public virtual void Configure(IMapperConfigurationExpression mapperConfigExpression)
+        {
+            mapperConfigExpression.CreateMap<Customer, CustomerDto>()
+                .ForMember(c => c.FullName, cnfg => cnfg.MapFrom(c => c.FirstName + " " + c.LastName));
+        }
+    }
+
+    public class TestEfRepository<TEntity> : EfCoreRepository<TEntity>
+        where TEntity : class, IEntity
+    {
+        public TestEfRepository(TestDbContext context)
+            : base(context)
+        {
+
+        }
+    }
+
+    public class ValuesController : ApiController
+    {
+        public string[] Get()
+        {
+            return new[] { "1", "2", "3" };
+        }
+    }
+
+    public class CustomersController : DtoSetController<CustomerDto, Customer, int>
+    {
+        public CustomersController(IRepository<Customer> repository)
+            : base(repository)
+        {
+        }
+
+        [Function]
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
---
+++ src/Server/DotNetCoreTestApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace DotNetCoreTestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.AddServerHeader = false;
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseWebRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<AppStartup>()
+                .CaptureStartupErrors(true)
+                .Build();
+
+            host.Run();
+        }
+
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.StartsWith("System.Data.Linq"))
+                return Assembly.LoadFile(@"C:\Program Files\Mono\lib\mono\2.0-api\System.Data.Linq.dll");
+            return null;
+        }
+    }
+}
