39521d7366bbee4445635341e0a797efed2a691c
ysmoradi <ysmoradi@outlook.com>
2017-09-03 17:01:15 +0430

    Add location header to create | post responses. This closes #80

--- src/Server/Bit.OData/ODataControllers/DtoController.cs
+++ src/Server/Bit.OData/ODataControllers/DtoController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection;
 using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
 using System.Web.OData;
 using System.Web.OData.Extensions;
 using System.Web.OData.Query;
@@ -54,7 +56,7 @@ namespace Bit.OData.ODataControllers
     }
 
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-    public sealed class CreateAttribute : Attribute, IActionHttpMethodProvider
+    public sealed class CreateAttribute : ActionFilterAttribute, IActionHttpMethodProvider
     {
         private static readonly Collection<HttpMethod> SupportedMethods = new Collection<HttpMethod>(new[]
         {
@@ -62,6 +64,30 @@ namespace Bit.OData.ODataControllers
         });
 
         public Collection<HttpMethod> HttpMethods => SupportedMethods;
+
+        private static readonly Lazy<MethodInfo> GenerateODataLinkMethod = new Lazy<MethodInfo>(() =>
+        {
+            return typeof(MetadataController).GetTypeInfo()
+                    .Assembly.GetType("System.Web.OData.Results.ResultHelpers")
+                    .GetMethod("GenerateODataLink");
+        });
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (!actionExecutedContext.Response.Headers.Contains(nameof(HttpResponseHeaders.Location))
+                && actionExecutedContext.Response?.Content is ObjectContent
+                && actionExecutedContext.Response.IsSuccessStatusCode == true)
+            {
+                ObjectContent objContent = ((ObjectContent)(actionExecutedContext.Response.Content));
+
+                if (objContent.Value == null)
+                    return;
+
+                actionExecutedContext.Response.Headers.Location = (Uri)GenerateODataLinkMethod.Value.Invoke(null, new object[] { actionExecutedContext.Request, objContent.Value, false });
+            }
+
+            base.OnActionExecuted(actionExecutedContext);
+        }
     }
 
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
