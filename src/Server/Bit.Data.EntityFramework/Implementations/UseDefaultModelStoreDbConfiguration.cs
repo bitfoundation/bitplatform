9e3fe8ef57d42b4d3ac2f71e12f4cd16462a405e
ysmoradi <ysmoradi@outlook.com>
2017-11-03 20:24:44 +0330

    Added UseDefaultModelStoreDbConfiguration based on ef 6.2 release

---
+++ src/Server/Bit.Data.EntityFramework/Implementations/UseDefaultModelStoreDbConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+
+namespace Bit.Data.EntityFramework.Implementations
+{
+    public class UseDefaultModelStoreDbConfiguration : DbConfiguration
+    {
+        public UseDefaultModelStoreDbConfiguration()
+        {
+            SetModelStore(new DefaultDbModelStore(Directory.GetCurrentDirectory()));
+        }
+    }
+}
