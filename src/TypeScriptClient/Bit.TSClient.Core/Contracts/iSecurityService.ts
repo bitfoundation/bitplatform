1e015a19c92b9768060c7e9864c0359a7606412d
ysmoradi@outlook.com <ysmoradi@outlook.com>
2018-02-14 23:05:25 +0330

    Added loginWithCredentials to ts client security service contract

--- src/TypeScriptClient/Bit.TSClient.Core/Contracts/iSecurityService.ts
+++ src/TypeScriptClient/Bit.TSClient.Core/Contracts/iSecurityService.ts
@@ -3,5 +3,6 @@
         isLoggedIn(): boolean;
         login(state?: any): void;
         logout(): void;
+        loginWithCredentials(username: string, password: string, client_id: string, client_secret: string, scopes: string[], saveToken: boolean): Promise<{ access_token: string, expires_in: number, token_type: string }>;
     }
 }
