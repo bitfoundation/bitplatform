947118528b334e9b6e24e7e92c5bc73bc50ad523
ysmoradi@outlook.com <ysmoradi@outlook.com>
2018-02-14 23:02:04 +0330

    cs client config provider contrct

---
+++ src/CSharpClient/Bit.CSharpClient.All/ViewModel/Contracts/IConfigProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bit.ViewModel.Contracts
+{
+    public interface IConfigProvider
+    {
+        Uri HostUri { get; }
+
+        string OAuthImplicitFlowClientId { get; }
+
+        Uri OAuthImplicitFlowRedirectUri { get; }
+
+        string OAuthResourceOwnerFlowClientId { get; }
+
+        string OAuthResourceOwnerFlowSecret { get; }
+
+        string AppName { get; }
+    }
+}
