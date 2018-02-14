1a51828ee4d779f373ce4c8f7d15d4d8af5dbf7e
ysmoradi@outlook.com <ysmoradi@outlook.com>
2018-02-14 22:58:49 +0330

    cs client boolean converter

---
+++ src/CSharpClient/Bit.CSharpClient.All/View/Converters/BooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Bit.View.Converters
+{
+    public class BooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return !boolValue;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return !boolValue;
+            return false;
+        }
+    }
+}
