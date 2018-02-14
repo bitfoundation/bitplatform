99220ee91b9fa8282ac298f8729082dd7d79916d
ysmoradi@outlook.com <ysmoradi@outlook.com>
2018-02-14 23:02:35 +0330

    cs client dictionary extensions

---
+++ src/CSharpClient/Bit.CSharpClient.All/ViewModel/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Navigation;
+
+namespace System.Collections.Generic
+{
+    public static class DictionaryExtensions
+    {
+        public static NavigationParameters ToNavParams(this IDictionary<string, object> parameters)
+        {
+            NavigationParameters result = new NavigationParameters();
+
+            foreach (KeyValuePair<string, object> param in parameters)
+            {
+                result.Add(param.Key, param.Value);
+            }
+
+            return result;
+        }
+    }
+}
