58d3b03808a397895843cd26d157f9c2b0d459c0
ysmoradi@outlook.com <ysmoradi@outlook.com>
2018-02-20 19:11:28 +0330

    Added ext method to read claims easier

---
+++ src/Server/Bit.IdentityServer/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IdentityServer3.Core.Models
+{
+    public static class ClaimsExtensions
+    {
+        public static string GetClaimValue(this IEnumerable<System.Security.Claims.Claim> claims, string claimType)
+        {
+            if (claims == null)
+                throw new ArgumentNullException(nameof(claims));
+
+            if (claimType == null)
+                throw new ArgumentNullException(nameof(claimType));
+
+            System.Security.Claims.ClaimsIdentity subject = claims.FirstOrDefault()?.Subject;
+
+            return subject?.FindFirst(claimType)?.Value ?? claims.ExtendedSingleOrDefault($"Finding claim: {claimType}", c => c.Type == claimType)?.Value;
+        }
+    }
+}
