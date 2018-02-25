2a4165eb6aa50922f10788775b85357f6232dba6
ysmoradi@outlook.com <ysmoradi@outlook.com>
2018-02-25 15:56:08 +0330

    Removed ununsed namespaces

--- src/CSharpClient/Bit.CSharpClientSample/Bit.CSharpClientSample/Views/MainView.xaml.cs
+++ src/CSharpClient/Bit.CSharpClientSample/Bit.CSharpClientSample/Views/MainView.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace Bit.CSharpClientSample.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainView : ContentPage
+    public partial class MainView : ContentPage
 	{
 		public MainView ()
 		{
