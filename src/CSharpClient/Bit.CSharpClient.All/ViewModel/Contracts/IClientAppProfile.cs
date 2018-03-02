2fafefd132fec971fe460b0b350eb4903ff7e6d7
ysmoradi@outlook.com <ysmoradi@outlook.com>
2018-03-02 20:22:50 +0330

    Renamed IConfigProvider to IClientAppProfile in cs client

---
+++ src/CSharpClient/Bit.CSharpClient.All/ViewModel/Contracts/IClientAppProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bit.ViewModel.Contracts
+{
+    public interface IClientAppProfile
+    {
+        Uri HostUri { get; }
+
+        Uri OAuthRedirectUri { get; }
+
+        string AppName { get; }
+    }
+}
