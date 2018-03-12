79de72bb7bd98f98ad1611330da21ab661bbecda
Yaser Moradi <ysmoradi@outlook.com>
2018-03-12 16:51:58 +0330

    Updated to latest ts client npm package of bit

--- Apps/BitChangeSetManager/AspNet/wwwroot/security/bitChangeSetManagerSecurity.objectDependencies.ts
+++ Apps/BitChangeSetManager/AspNet/wwwroot/security/bitChangeSetManagerSecurity.objectDependencies.ts
@@ -4,7 +4,7 @@ module BitChangeSetManagerSecurity {
 
     const dependencyManager = DependencyManager.getCurrent();
 
-    dependencyManager.registerObjectDependency({ name: "ModelProvider", type: Bit.Implementations.Identity.DefaultModelProvider });
+    dependencyManager.registerObjectDependency({ name: "LoginModelProvider", type: Bit.Implementations.Identity.DefaultLoginModelProvider });
 
     dependencyManager.registerObjectDependency({ name: "AppEvent", type: Bit.Implementations.Identity.IdentityServerDefaultAngularAppInitialization });
 
