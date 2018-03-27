c6134277f772e785637c6351cd87802a304391ae
Yaser Moradi <ysmoradi@outlook.com>
2018-03-27 10:46:58 +0430

    Moved declarations in ts - angular js client

--- src/TypeScriptClient/Bit.TSClient.AngularJS.Tests/typings.d.ts
+++ src/TypeScriptClient/Bit.TSClient.AngularJS.Tests/typings.d.ts
@@ -3,5 +3,5 @@
 /// <reference path="../Bit.TSClient.Core/bit.model.context.d.ts" />
 /// <reference path="../Bit.TSClient.Core/declarations.d.ts" />
 /// <reference path="../Bit.TSClient.AngularJS/bit.ng1.d.ts" />
-/// <reference path="../Bit.TSClient.AngularJS/typings/declarations.d.ts" />
+/// <reference path="../Bit.TSClient.AngularJS/declarations.d.ts" />
 /// <reference path="../Bit.TSClient.AngularJS.Test/bit.ng1.test.d.ts" />
