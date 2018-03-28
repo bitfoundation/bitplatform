bfc07473bd8d1dfaf2c77a7d688949871d7d022c
Yaser Moradi <ysmoradi@outlook.com>
2018-03-29 00:54:37 +0430

    Added ios version of cs client sample app

---
+++ src/CSharpClient/Bit.CSharpClientSample.iOS/AppDelegate.cs
@@ -0,0 +1,32 @@
+﻿using Bit.iOS;
+using Bit.ViewModel.Contracts;
+using Bit.ViewModel.Implementations;
+using Foundation;
+using Prism;
+using Prism.Ioc;
+using UIKit;
+using Xamarin.Forms;
+
+namespace Bit.CSharpClientSample.iOS
+{
+    [Register(nameof(AppDelegate))]
+    public partial class AppDelegate : BitFormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Forms.Init();
+
+            LoadApplication(new App(new SampleAppiOSInitializer()));
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+
+    public class SampleAppiOSInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<IBrowserService, DefaultBrowserService>();
+        }
+    }
+}
