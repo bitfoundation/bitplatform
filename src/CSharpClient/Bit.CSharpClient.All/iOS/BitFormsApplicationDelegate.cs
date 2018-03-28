9a514c722b10302b1a74f06ea62473a583a6f8d7
Yaser Moradi <ysmoradi@outlook.com>
2018-03-29 00:54:49 +0430

    Initial version of BitFormsApplicationDelegate

---
+++ src/CSharpClient/Bit.CSharpClient.All/iOS/BitFormsApplicationDelegate.cs
@@ -0,0 +1,22 @@
+﻿#if iOS 
+using Bit.ViewModel.Implementations;
+using Foundation;
+using System;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+namespace Bit.iOS
+{
+    public class BitFormsApplicationDelegate : FormsApplicationDelegate
+    {
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+            Uri uri = new Uri(url.AbsoluteString);
+
+            DefaultSecurityService.Current.OnSsoLoginLogoutRedirectCompleted(uri);
+
+            return true;
+        }
+    }
+}
+#endif
