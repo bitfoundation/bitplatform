b4d416e63c20d54862ccb0a34b2ee0c7c8d9bd3e
Yaser Moradi <ysmoradi@outlook.com>
2018-03-29 20:47:59 +0430

    Added BitPlatformInitializer to cs client

---
+++ src/CSharpClient/Bit.CSharpClient.All/ViewModel/Implementations/BitPlatformInitializer.cs
@@ -0,0 +1,68 @@
+﻿#if iOS
+
+using Bit.ViewModel.Contracts;
+using Prism;
+using Prism.Ioc;
+
+namespace Bit.ViewModel.Implementations
+{
+    public class BitPlatformInitializer : IPlatformInitializer
+    {
+        public virtual void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<IBrowserService, DefaultBrowserService>();
+        }
+    }
+}
+
+#elif Android
+
+using Android.App;
+using Android.Content;
+using Bit.ViewModel.Contracts;
+using Prism;
+using Prism.Ioc;
+using System;
+
+namespace Bit.ViewModel.Implementations
+{
+    public class BitPlatformInitializer : IPlatformInitializer
+    {
+        private readonly Activity _activity;
+
+        public BitPlatformInitializer(Activity activity)
+        {
+            if (activity == null)
+                throw new ArgumentNullException(nameof(activity));
+
+            _activity = activity;
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterInstance<Activity>(_activity);
+            containerRegistry.RegisterInstance<Context>(_activity);
+
+            containerRegistry.Register<IBrowserService, DefaultBrowserService>();
+        }
+    }
+}
+
+#else
+
+using Bit.ViewModel.Contracts;
+using Prism;
+using Prism.Ioc;
+
+namespace Bit.ViewModel.Implementations
+{
+    public class BitPlatformInitializer : IPlatformInitializer
+    {
+        public virtual void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<IBrowserService, DefaultBrowserService>();
+        }
+    }
+}
+
+#endif
