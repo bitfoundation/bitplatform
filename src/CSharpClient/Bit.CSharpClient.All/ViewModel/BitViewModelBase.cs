5ff9ff75d68738669b92bf174391a12d5cadea13
Yaser Moradi <ysmoradi@outlook.com>
2018-03-29 14:11:01 +0430

    Enhanced view model base for cs client

--- src/CSharpClient/Bit.CSharpClient.All/ViewModel/BitViewModelBase.cs
+++ src/CSharpClient/Bit.CSharpClient.All/ViewModel/BitViewModelBase.cs
@@ -1,8 +1,39 @@
 ﻿using Prism.Mvvm;
+using Prism.Navigation;
+using System.Threading.Tasks;
 
 namespace Bit.ViewModel
 {
-    public class BitViewModelBase : BindableBase
+    public class BitViewModelBase : BindableBase, INavigatedAware, INavigatingAware, INavigationAware, IDestructible, IConfirmNavigation, IConfirmNavigationAsync
     {
+        public virtual bool CanNavigate(NavigationParameters parameters)
+        {
+            return true;
+        }
+
+        public virtual Task<bool> CanNavigateAsync(NavigationParameters parameters)
+        {
+            return Task.FromResult(true);
+        }
+
+        public virtual void Destroy()
+        {
+
+        }
+
+        public virtual void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
     }
 }
