475d7ed6ad38492af3ecf07f0324d88c4686e752
Yaser Moradi <ysmoradi@outlook.com>
2018-03-31 17:38:27 +0430

    cs client's IDictionary<string,object> to Dto methods

---
+++ src/CSharpClient/Bit.CSharpClient.All/ViewModel/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Bit.Model.Contracts;
+using Prism.Navigation;
+using System.Reflection;
+
+namespace System.Collections.Generic
+{
+    public static class IDictionaryExtensions
+    {
+        public static NavigationParameters ToNavParams(this IDictionary<string, object> parameters)
+        {
+            NavigationParameters result = new NavigationParameters();
+
+            foreach (KeyValuePair<string, object> param in parameters)
+            {
+                result.Add(param.Key, param.Value);
+            }
+
+            return result;
+        }
+
+        public static IDto ToDto(this IDictionary<string, object> unTypedDto, TypeInfo dtoType)
+        {
+            if (unTypedDto == null)
+                throw new ArgumentNullException(nameof(unTypedDto));
+
+            if (dtoType == null)
+                throw new ArgumentNullException(nameof(dtoType));
+
+            IDto dto = (IDto)Activator.CreateInstance(dtoType);
+
+            Dictionary<string, object> extraProps = null;
+
+            if (dto is IOpenType openTypeDto)
+                extraProps = openTypeDto.Properties = (openTypeDto.Properties ?? new Dictionary<string, object>());
+
+            foreach (KeyValuePair<string, object> KeyVal in unTypedDto)
+            {
+                PropertyInfo prop = dtoType.GetProperty(KeyVal.Key);
+
+                object val = KeyVal.Value;
+
+                if (prop.PropertyType.IsEnum && val != null)
+                    val = Enum.Parse(prop.PropertyType, KeyVal.Value.ToString());
+
+                if (prop != null)
+                    prop.SetValue(dto, val);
+                else if (extraProps != null)
+                    extraProps.Add(KeyVal.Key, val);
+            }
+
+            return dto;
+        }
+
+        public static TDto ToDto<TDto>(this IDictionary<string, object> unTypedDto)
+            where TDto : class, IDto
+        {
+            return (TDto)unTypedDto.ToDto(typeof(TDto).GetTypeInfo());
+        }
+    }
+}
