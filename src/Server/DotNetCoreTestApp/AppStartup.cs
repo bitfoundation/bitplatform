7c4e62926251d4a49c3ad11dd4b78b6540d96686
Behzad Abbasi <behzad888@users.noreply.github.com>
2018-06-09 17:35:54 +0430

    Depricated IAspNetCoreAppModule

--- src/Server/DotNetCoreTestApp/AppStartup.cs
+++ src/Server/DotNetCoreTestApp/AppStartup.cs
@@ -29,7 +29,7 @@ using System.Web.Http;
 
 namespace DotNetCoreTestApp
 {
-    public class AppStartup : AutofacAspNetCoreAppStartup, IAspNetCoreAppModule, IAppModulesProvider
+    public class AppStartup : AutofacAspNetCoreAppStartup, IAppModule, IAppModulesProvider
     {
         public AppStartup(IServiceProvider serviceProvider)
             : base(serviceProvider)
@@ -49,7 +49,7 @@ namespace DotNetCoreTestApp
             yield return this;
         }
 
-        public virtual void ConfigureDependencies(IServiceProvider serviceProvider, IServiceCollection services, IDependencyManager dependencyManager)
+        public virtual void ConfigureDependencies(IServiceCollection services, IDependencyManager dependencyManager)
         {
             AssemblyContainer.Current.Init();
 
