6765eb16720bcdcce9a5e273c663dc516b9db4e9
Yaser Moradi <ysmoradi@outlook.com>
2018-06-25 18:30:50 +0430

    Fixed gulp less task

--- src/TypeScriptClient/Bit.TSClient.AngularJS/gulpfile.js
+++ src/TypeScriptClient/Bit.TSClient.AngularJS/gulpfile.js
@@ -1,9 +1,12 @@
-﻿/// <binding AfterBuild='less' />
+﻿/// <binding AfterBuild="less" />
 var gulp = require("gulp");
 var less = require("gulp-less");
-
-gulp.task("less", function less() {
-    gulp.src(["./contents/styles/*.less"])
-        .pipe(less())
-        .pipe(gulp.dest("./contents/styles/"));
+var path = require("path");
+ 
+gulp.task("less", function () {
+  return gulp.src("./contents/styles/*.less")
+    .pipe(less({
+      paths: [ path.join(__dirname, "less", "includes") ]
+    }))
+    .pipe(gulp.dest("./contents/styles/"));
 });
