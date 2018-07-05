052de87e1402c476c37421110e5700906e74400f
Yaser Moradi <ysmoradi@outlook.com>
2018-07-05 14:05:12 +0430

    Use SubscribeAsync instead of Subscribe code analyzer & test

---
+++ BitTools/BitCodeAnalyzer.Test/BitAnalyzers/ViewModel/SubscribeAsyncTestProj/Tests.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Events;
+using System;
+using System.Reactive.Subjects;
+
+namespace SubscribeAsyncTestProj
+{
+    public class Tests
+    {
+        public void Test()
+        {
+            new Subject<string>().Subscribe((arg) => { });
+
+            new EventAggregator().GetEvent<TestEvent>().Subscribe(arg => { });
+        }
+    }
+
+    public class TestEvent : PubSubEvent<TestEvent> { }
+}
---
+++ BitTools/BitCodeAnalyzer.Test/BitAnalyzers/ViewModel/UseSubscribeAsyncInsteadOfSubscribeAnalyzerTests.cs
@@ -0,0 +1,61 @@
+using BitCodeAnalyzer.BitAnalyzers.Data.EntityFramework;
+using BitCodeAnalyzer.BitAnalyzers.ViewModel;
+using BitCodeAnalyzer.Test.Helpers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitCodeAnalyzer.Test.BitAnalyzers.ViewModel
+{
+    [TestClass]
+    public class UseSubscribeAsyncInsteadOfSubscribeAnalyzerTests : DiagnosticVerifier
+    {
+        [TestMethod]
+        [TestCategory("Analyzer")]
+        public async Task UseSubscribeAsyncInsteadOfSubscribeAnalyzerTest()
+        {
+            DiagnosticResult subscribeUsage1 = new DiagnosticResult
+            {
+                Id = nameof(UseSubscribeAsyncInsteadOfSubscribeAnalyzer),
+                Message = UseSubscribeAsyncInsteadOfSubscribeAnalyzer.Message,
+                Severity = DiagnosticSeverity.Error,
+                Locations = new[] { new DiagnosticResultLocation(Path.Combine(basePath, @"Tests.cs"), 11, 13) }
+            };
+
+            DiagnosticResult subscribeUsage2 = new DiagnosticResult
+            {
+                Id = nameof(UseSubscribeAsyncInsteadOfSubscribeAnalyzer),
+                Message = UseSubscribeAsyncInsteadOfSubscribeAnalyzer.Message,
+                Severity = DiagnosticSeverity.Error,
+                Locations = new[] { new DiagnosticResultLocation(Path.Combine(basePath, @"Tests.cs"), 13, 13) }
+            };
+
+            await VerifyCSharpDiagnostic(subscribeUsage1, subscribeUsage2);
+        }
+
+        private readonly string basePath = new DirectoryInfo(Path.Combine(Environment.CurrentDirectory, @"..\..\..\BitAnalyzers\ViewModel\SubscribeAsyncTestProj")).FullName;
+
+        public override async Task<Project> CreateProject(string[] sources, string language = LanguageNames.CSharp)
+        {
+            MSBuildWorkspace workspace = MSBuildWorkspace.Create(new Dictionary<string, string>()
+            {
+                { "TargetFramework", "net461" }
+            });
+
+            await workspace.OpenSolutionAsync(Path.Combine(basePath, "SubscribeAsyncTestProj.sln"));
+
+            return workspace.CurrentSolution.Projects.Single();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new UseSubscribeAsyncInsteadOfSubscribeAnalyzer();
+        }
+    }
+}
