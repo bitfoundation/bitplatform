ff36a4b9858fc2ed62dd138ad3278b0dbb65f6d6
Yaser Moradi <ysmoradi@outlook.com>
2018-09-06 00:14:10 +0430

    Removed bit rg popups integration due prism integration with that library

--- src/CSharpClient/Bit.CSharpClient.All/ViewModel/Extensions/IContainerRegistryExtensions.cs
+++ src/CSharpClient/Bit.CSharpClient.All/ViewModel/Extensions/IContainerRegistryExtensions.cs
@@ -1,13 +1,9 @@
 ﻿using Autofac;
-using Bit.ViewModel;
 using Bit.ViewModel.Contracts;
 using Bit.ViewModel.Implementations;
 using IdentityModel.Client;
 using Prism.Autofac;
 using Prism.Events;
-using Rg.Plugins.Popup.Contracts;
-using Rg.Plugins.Popup.Pages;
-using Rg.Plugins.Popup.Services;
 using Simple.OData.Client;
 using System;
 using System.Net.Http;
@@ -113,35 +109,6 @@ namespace Prism.Ioc
             containerBuilder
                 .Register(c => new ODataBatch(c.Resolve<IODataClient>(), reuseSession: true))
                 .PreserveExistingDefaults();
-
-            return containerRegistry;
-        }
-
-        public static IContainerRegistry RegisterPopupService(this IContainerRegistry containerRegistry)
-        {
-            if (containerRegistry == null)
-                throw new ArgumentNullException(nameof(containerRegistry));
-
-            ContainerBuilder containerBuilder = containerRegistry.GetBuilder();
-
-            containerBuilder
-                .Register<IPopupNavigation>(c => PopupNavigation.Instance)
-                .PreserveExistingDefaults();
-
-            containerBuilder.RegisterType<DefaultPopupNavigationService>().As<IPopupNavigationService>().SingleInstance().PreserveExistingDefaults();
-
-            return containerRegistry;
-        }
-
-        public static IContainerRegistry RegisterForPopup<TView, TViewModel>(this IContainerRegistry containerRegistry, string name)
-            where TView : PopupPage
-            where TViewModel : BitViewModelBase
-        {
-            if (containerRegistry == null)
-                throw new ArgumentNullException(nameof(containerRegistry));
-
-            containerRegistry.Register<TViewModel>();
-            containerRegistry.Register<PopupPage, TView>(name);
 
             return containerRegistry;
         }
