b76c9bf57362b796506478d57b52c6a7f6acc4b1
Yaser Moradi <ysmoradi@outlook.com>
2018-09-18 22:39:00 +0430

    Added UnTyped Binding | View Binding | ViewModel Binding

---
+++ src/CSharpClient/Bit.CSharpClient.All/View/UnTypedBindingExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bit.View
+{
+    // https://github.com/xamarin/Xamarin.Forms/issues/3606
+    [ContentProperty(nameof(Path))]
+    public class UnTypedBindingExtension : IMarkupExtension<BindingBase>
+    {
+        public virtual string Path { get; set; } = ".";
+        public BindingMode Mode { get; set; } = BindingMode.Default;
+        public IValueConverter Converter { get; set; }
+        public object ConverterParameter { get; set; }
+        public string StringFormat { get; set; }
+        public object Source { get; set; }
+        public string UpdateSourceEventName { get; set; }
+        public object TargetNullValue { get; set; }
+        public object FallbackValue { get; set; }
+
+        BindingBase IMarkupExtension<BindingBase>.ProvideValue(IServiceProvider serviceProvider)
+        {
+            Source = Source ?? GetSource(serviceProvider);
+
+            return new Binding(Path, Mode, Converter, ConverterParameter, StringFormat, Source)
+            {
+                UpdateSourceEventName = UpdateSourceEventName,
+                FallbackValue = FallbackValue,
+                TargetNullValue = TargetNullValue
+            };
+        }
+
+        protected virtual object GetSource(IServiceProvider serviceProvider)
+        {
+            return null;
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return (this as IMarkupExtension<BindingBase>).ProvideValue(serviceProvider);
+        }
+    }
+}
---
+++ src/CSharpClient/Bit.CSharpClient.All/View/ViewBindingExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Xaml.Internals;
+
+namespace Bit.View
+{
+    [ContentProperty(nameof(Path))]
+    public class ViewBindingExtension : UnTypedBindingExtension
+    {
+        private static readonly FieldInfo _objectAndParentsField = typeof(SimpleValueTargetProvider).GetField("objectAndParents", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        protected override object GetSource(IServiceProvider serviceProvider)
+        {
+            SimpleValueTargetProvider providedValueTarget = (SimpleValueTargetProvider)serviceProvider.GetService<IReferenceProvider>();
+
+            Page parentPage = ((object[])_objectAndParentsField.GetValue(providedValueTarget))
+                .OfType<Page>()
+                .LastOrDefault();
+
+            return parentPage;
+        }
+    }
+}
