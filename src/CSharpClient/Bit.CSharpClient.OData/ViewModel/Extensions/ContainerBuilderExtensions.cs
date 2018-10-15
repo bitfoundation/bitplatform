8586fd1f6d1099f6a1b7c18271b0bcc53c1cb145
Yaser Moradi <ysmoradi@outlook.com>
2018-10-15 18:02:40 +0330

    bit cs client odata

---
+++ src/CSharpClient/Bit.CSharpClient.OData/ViewModel/Extensions/ContainerBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using Bit.ViewModel.Contracts;
+using Simple.OData.Client;
+using System;
+using System.Net.Http;
+
+namespace Prism.Ioc
+{
+    public static class ContainerBuilderExtensions
+    {
+        public static ContainerBuilder RegisterODataClient(this ContainerBuilder containerBuilder)
+        {
+            if (containerBuilder == null)
+                throw new ArgumentNullException(nameof(containerBuilder));
+
+            Simple.OData.Client.V4Adapter.Reference();
+
+            containerBuilder.Register(c =>
+            {
+                HttpMessageHandler authenticatedHttpMessageHandler = c.ResolveNamed<HttpMessageHandler>(ContractKeys.AuthenticatedHttpMessageHandler);
+
+                IClientAppProfile clientAppProfile = c.Resolve<IClientAppProfile>();
+
+                IODataClient odataClient = new ODataClient(new ODataClientSettings(new Uri(clientAppProfile.HostUri, clientAppProfile.ODataRoute))
+                {
+                    RenewHttpConnection = false,
+                    OnCreateMessageHandler = () => authenticatedHttpMessageHandler
+                });
+
+                return odataClient;
+            }).PreserveExistingDefaults();
+
+            containerBuilder
+                .Register(c => new ODataBatch(c.Resolve<IODataClient>(), reuseSession: true))
+                .PreserveExistingDefaults();
+
+            return containerBuilder;
+        }
+    }
+}
