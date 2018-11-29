d2d9cae622c045acb3e920b7e0c5df518b5eadc3
Yaser Moradi <ysmoradi@outlook.com>
2018-11-29 19:06:02 +0330

    Renamed BooleanConverter to BooleanInverseConverter

--- src/CSharpClient/Bit.CSharpClient.Controls/Converters/BooleanInverseConverter.cs
+++ src/CSharpClient/Bit.CSharpClient.Controls/Converters/BooleanInverseConverter.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Bit
 {
-    public class BooleanConverter : IValueConverter
+    public class BooleanInverseConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
