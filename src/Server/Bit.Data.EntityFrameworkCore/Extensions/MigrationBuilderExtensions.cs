88f97c84655abe66fc1d549490cb3d63d00d1216
Yaser Moradi <ysmoradi@outlook.com>
2018-12-01 12:45:18 +0330

    ef core migration for sql server json log store required table

---
+++ src/Server/Bit.Data.EntityFrameworkCore/Extensions/MigrationBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Bit.Data.Implementations;
+using System;
+
+namespace Microsoft.EntityFrameworkCore.Migrations
+{
+    public static class MigrationBuilderExtensions
+    {
+        /// <summary>
+        /// <seealso cref="SqlServerJsonLogStore"/>
+        /// </summary>
+        public static void CreateSqlServerJsonLogStoreTable(this MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "NEWSEQUENTIALID()"),
+                    Contents = table.Column<string>(nullable: false),
+                    Date = table.Column<DateTimeOffset>(nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                });
+        }
+    }
+}
