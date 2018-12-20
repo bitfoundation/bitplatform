4beaa4bab75685e8fb60519c8a2a67e5527bd359
Yaser Moradi <ysmoradi@outlook.com>
2018-12-20 15:47:25 +0330

    Renamed converter to value converter

--- src/CSharpClient/Bit.CSharpClient.Prism/View/ValueConverter.cs
+++ src/CSharpClient/Bit.CSharpClient.Prism/View/ValueConverter.cs
@@ -5,14 +5,14 @@ using Xamarin.Forms.Xaml;
 
 namespace Bit.View
 {
-    public class Converter<TSource, TTarget> : IMarkupExtension, IValueConverter
+    public class ValueConverter<TSource, TTarget, TParameter> : IMarkupExtension, IValueConverter
     {
-        protected virtual TTarget Convert(TSource value, Type targetType, object parameter, CultureInfo culture)
+        protected virtual TTarget Convert(TSource value, Type targetType, TParameter parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
 
-        protected virtual TSource ConvertBack(TTarget value, Type targetType, object parameter, CultureInfo culture)
+        protected virtual TSource ConvertBack(TTarget value, Type targetType, TParameter parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
@@ -22,7 +22,10 @@ namespace Bit.View
             if (value is null && !typeof(TSource).IsClass && Nullable.GetUnderlyingType(typeof(TSource)) == null)
                 throw new NotSupportedException();
 
-            return Convert((TSource)value, targetType, parameter, culture);
+            if (parameter is null && !typeof(TParameter).IsClass && Nullable.GetUnderlyingType(typeof(TParameter)) == null)
+                throw new NotSupportedException();
+
+            return Convert((TSource)value, targetType, (TParameter)parameter, culture);
         }
 
         object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -30,12 +33,20 @@ namespace Bit.View
             if (value is null && !typeof(TTarget).IsClass && Nullable.GetUnderlyingType(typeof(TTarget)) == null)
                 throw new NotSupportedException();
 
-            return ConvertBack((TTarget)value, targetType, parameter, culture);
+            if (parameter is null && !typeof(TParameter).IsClass && Nullable.GetUnderlyingType(typeof(TParameter)) == null)
+                throw new NotSupportedException();
+
+            return ConvertBack((TTarget)value, targetType, (TParameter)parameter, culture);
         }
 
         public object ProvideValue(IServiceProvider serviceProvider)
         {
             return this;
         }
+    }
+
+    public class ValueConverter<TSource, TTarget> : ValueConverter<TSource, TTarget, object>
+    {
+
     }
 }
