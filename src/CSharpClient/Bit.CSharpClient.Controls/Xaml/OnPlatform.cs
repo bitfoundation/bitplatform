a94810af5762ae3b467c0f10840ee25aff001b41
Yaser Moradi <ysmoradi@outlook.com>
2019-01-22 18:39:39 +0330

    Added on platform to bit view

---
+++ src/CSharpClient/Bit.CSharpClient.Controls/Xaml/OnPlatform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bit.View
+{
+    public class OnPlatform<T>
+    {
+        public T Value { get; set; }
+
+        public static implicit operator T(OnPlatform<T> value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            return value.Value;
+        }
+    }
+}
