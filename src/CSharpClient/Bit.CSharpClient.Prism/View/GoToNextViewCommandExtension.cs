adaad7cb01b4b0d24d0021a89ce831bb3daf4d75
ysmoradi <ysmoradi@outlook.com>
2019-02-20 18:15:56 +0330

    Go to next view command in cs client

---
+++ src/CSharpClient/Bit.CSharpClient.Prism/View/GoToNextViewCommandExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms.Xaml;
+
+namespace Bit.View
+{
+    public class GoToNextViewCommandExtension : ICommand, IMarkupExtension<ICommand>
+    {
+        public Xamarin.Forms.View Next { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return Next != null;
+        }
+
+        public void Execute(object parameter)
+        {
+            Next?.Focus();
+        }
+
+        public ICommand ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
