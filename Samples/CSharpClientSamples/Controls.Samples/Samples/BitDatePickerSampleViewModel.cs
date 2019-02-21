f714fa9ade1e854ccf0379b7be4c47d4b2967f77
ysmoradi <ysmoradi@outlook.com>
2019-02-21 18:21:27 +0330

    Updated samples to bit 1.9.0

--- Samples/CSharpClientSamples/Controls.Samples/Samples/BitDatePickerSampleViewModel.cs
+++ Samples/CSharpClientSamples/Controls.Samples/Samples/BitDatePickerSampleViewModel.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Bit.CSharpClient.Controls.Samples
 {
     public class BitDatePickerSampleViewModel : INotifyPropertyChanged
     {
-        public DateTime? SelectedDate { get; set; }
+        private DateTime _SelectedDate;
+
+        public DateTime SelectedDate
+        {
+            get => _SelectedDate;
+            set
+            {
+                _SelectedDate = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedDate)));
+            }
+        }
+
+        public ICommand ChangeSelecedDate { get; set; }
 
         public BitDatePickerSampleViewModel()
         {
             SelectedDate = DateTime.Now;
+            ChangeSelecedDate = new Command(() =>
+            {
+                SelectedDate = SelectedDate.AddDays(3);
+            });
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
