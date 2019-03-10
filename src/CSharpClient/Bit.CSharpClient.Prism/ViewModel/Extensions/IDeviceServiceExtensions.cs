4f8c805807f542c40b8dd9bed55182f04aa972cc
ysmoradi <ysmoradi@outlook.com>
2019-03-10 20:11:13 +0330

    Added BeginInvokeOnMainThreadAsync to IDeviceService

---
+++ src/CSharpClient/Bit.CSharpClient.Prism/ViewModel/Extensions/IDeviceServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Bit.ViewModel;
+using System;
+using System.Threading.Tasks;
+
+namespace Prism.Services
+{
+    public static class IDeviceServiceExtensions
+    {
+        public static void BeginInvokeOnMainThreadAsync(this IDeviceService deviceService, Func<Task> func)
+        {
+            deviceService.BeginInvokeOnMainThread(async () =>
+            {
+                try
+                {
+                    await func();
+                }
+                catch (Exception exp)
+                {
+                    BitExceptionHandler.Current.OnExceptionReceived(exp);
+                }
+            });
+        }
+    }
+}
