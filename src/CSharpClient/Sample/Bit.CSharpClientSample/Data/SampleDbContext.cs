3f327838cb8828e0dd9d15806beabb0d4e8a2ec9
ysmoradi <ysmoradi@outlook.com>
2019-03-10 20:12:06 +0330

    As db context does not support property injection, use ctor injection in cs client samples' db context

--- src/CSharpClient/Sample/Bit.CSharpClientSample/Data/SampleDbContext.cs
+++ src/CSharpClient/Sample/Bit.CSharpClientSample/Data/SampleDbContext.cs
@@ -10,7 +10,10 @@ namespace Bit.CSharpClientSample.Data
 {
     public class SampleDbContext : EfCoreDbContextBase
     {
-        public IDeviceService DeviceService { get; set; }
+        public SampleDbContext(IDeviceService deviceService)
+        {
+            DeviceService = deviceService;
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -32,5 +35,7 @@ namespace Bit.CSharpClientSample.Data
         }
 
         public virtual DbSet<TestCustomerDto> TestCustomers { get; set; }
+
+        public virtual IDeviceService DeviceService { get; }
     }
 }
