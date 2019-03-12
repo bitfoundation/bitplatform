23593f2b8ff72763ce3c3ecf23a0e98ffb2fb147
ysmoradi <ysmoradi@outlook.com>
2019-03-12 17:40:33 +0330

    Enhanced device service's BeginInvokeOnMainThreadAsync

--- src/CSharpClient/Bit.CSharpClient.Prism/ViewModel/Extensions/IDeviceServiceExtensions.cs
+++ src/CSharpClient/Bit.CSharpClient.Prism/ViewModel/Extensions/IDeviceServiceExtensions.cs
@@ -1,24 +1,28 @@
-﻿using Bit.ViewModel;
-using System;
+﻿using System;
 using System.Threading.Tasks;
 
 namespace Prism.Services
 {
     public static class IDeviceServiceExtensions
     {
-        public static void BeginInvokeOnMainThreadAsync(this IDeviceService deviceService, Func<Task> func)
+        public static Task BeginInvokeOnMainThreadAsync(this IDeviceService deviceService, Func<Task> func)
         {
+            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+
             deviceService.BeginInvokeOnMainThread(async () =>
             {
                 try
                 {
                     await func();
+                    tcs.SetResult(null);
                 }
                 catch (Exception exp)
                 {
-                    BitExceptionHandler.Current.OnExceptionReceived(exp);
+                    tcs.SetException(exp);
                 }
             });
+
+            return tcs.Task;
         }
     }
 }
