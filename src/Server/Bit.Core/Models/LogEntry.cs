06e0a59439ef08ecadbcfb4aebebb84978a9473b
ysmoradi <ysmoradi@outlook.com>
2019-04-01 22:30:54 +0430

    Log entry to dictionary method

--- src/Server/Bit.Core/Models/LogEntry.cs
+++ src/Server/Bit.Core/Models/LogEntry.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Bit.Core.Models
 {
@@ -70,6 +71,40 @@ namespace Bit.Core.Models
         public override string ToString()
         {
             return $"{nameof(Message)}: {Message}, {nameof(Severity)}: {Severity}";
+        }
+
+        public virtual Dictionary<string, object> ToDictionary()
+        {
+            Dictionary<string, object> values = new Dictionary<string, object>
+            {
+                { nameof(Id), Id },
+                { nameof(Severity), Severity },
+                { nameof(AppServerDateTime), AppServerDateTime },
+                { nameof(Message), Message },
+
+                { nameof(AppServerProcessId), AppServerProcessId },
+                { nameof(AppServerThreadId), AppServerThreadId },
+
+                { nameof(ApplicationName), ApplicationName },
+                { nameof(AppEnvironmentName), AppEnvironmentName },
+                { nameof(AppVersion), AppVersion },
+                { nameof(AppWasInDebugMode), AppWasInDebugMode },
+
+                { nameof(AppServerAppDomainName), AppServerAppDomainName },
+                { nameof(AppServerName), AppServerName },
+                { nameof(AppServerOSVersion), AppServerOSVersion },
+                { nameof(AppServerUserAccountName), AppServerUserAccountName },
+                { nameof(AppServerWas64Bit), AppServerWas64Bit },
+                { nameof(AppWas64Bit), AppWas64Bit },
+                { nameof(MemoryUsage), MemoryUsage }
+            };
+
+            foreach (IGrouping<string, LogData> logData in LogData.GroupBy(ld => ld.Key))
+            {
+                values.Add(logData.Key, logData.First().Value);
+            }
+
+            return values;
         }
     }
 }
