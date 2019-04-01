a41e6e84bc199d57ca34b18d580970c2d1581c36
ysmoradi <ysmoradi@outlook.com>
2019-04-01 21:06:59 +0430

    Support for border witdh in bit frame of cs client

--- src/CSharpClient/Bit.CSharpClient.Controls/BitFrame/BitFrameRenderer.ios.cs
+++ src/CSharpClient/Bit.CSharpClient.Controls/BitFrame/BitFrameRenderer.ios.cs
@@ -2,6 +2,7 @@
 
 using Bit.CSharpClient.Controls;
 using Bit.CSharpClient.Controls.iOS;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
@@ -11,9 +12,23 @@ namespace Bit.CSharpClient.Controls.iOS
 {
     public class BitFrameRenderer : FrameRenderer
     {
+        protected override void OnElementChanged(ElementChangedEventArgs<Frame> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                double borderWidth = ((BitFrame)e.NewElement).BorderWidth;
+                if (borderWidth != (double)BitFrame.BorderWidthProperty.DefaultValue)
+                {
+                    Layer.BorderWidth = (nfloat)borderWidth;
+                }
+            }
+        }
+
         public static void Init()
         {
-            
+
         }
     }
 }
