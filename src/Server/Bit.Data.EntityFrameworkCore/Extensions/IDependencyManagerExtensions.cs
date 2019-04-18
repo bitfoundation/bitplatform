bd0dad4fe6b4933488f953aa0fdec75f04b5723f
ysmoradi <ysmoradi@outlook.com>
2019-04-18 10:41:38 +0430

    services.AddDbContext integration in Bit.Data.EfCore while there is a support for implicit unit work

--- src/Server/Bit.Data.EntityFrameworkCore/Extensions/IDependencyManagerExtensions.cs
+++ src/Server/Bit.Data.EntityFrameworkCore/Extensions/IDependencyManagerExtensions.cs
@@ -1,23 +1,74 @@
-﻿using System;
-using Bit.Data.Contracts;
-using Bit.Data.EntityFrameworkCore.Contracts;
+﻿using Bit.Data.Contracts;
 using Bit.Data.EntityFrameworkCore.Implementations;
 using Bit.Data.Implementations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Data.Common;
+using System.Linq;
 using System.Reflection;
 
 namespace Bit.Core.Contracts
 {
     public static class IDependencyManagerExtensions
     {
-        public static IDependencyManager RegisterEfCoreDbContext<TDbContext, TDbContextObjectsProvider>(this IDependencyManager dependencyManager)
-            where TDbContext : EfCoreDbContextBase
-            where TDbContextObjectsProvider : class, IDbContextObjectsProvider
+        public static IDependencyManager RegisterEfCoreDbContext<TDbContext>(this IDependencyManager dependencyManager, Action<IServiceProvider, DbContextOptionsBuilder> optionsAction)
+            where TDbContext : DbContext
         {
             if (dependencyManager == null)
                 throw new ArgumentNullException(nameof(dependencyManager));
 
-            dependencyManager.Register<IDbContextObjectsProvider, TDbContextObjectsProvider>(overwriteExciting: false);
-            dependencyManager.Register(new[] { typeof(EfCoreDbContextBase).GetTypeInfo(), typeof(TDbContext).GetTypeInfo() }, typeof(TDbContext).GetTypeInfo(), overwriteExciting: false);
+            if (optionsAction == null)
+                throw new ArgumentNullException(nameof(optionsAction));
+
+            IServiceCollection services = ((IServiceCollectionAccessor)dependencyManager).ServiceCollection;
+
+            services.AddDbContext<TDbContext>((sp, optionsBuilder) =>
+            {
+                optionsAction.Invoke(sp, optionsBuilder);
+
+                if (!optionsBuilder.IsConfigured)
+                    throw new InvalidOperationException("optionsBuilder is not configured.");
+
+                optionsBuilder.ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
+            });
+
+            services.Remove(services.ExtendedSingle($"Finding {typeof(TDbContext).Name}'s service descriptor", s => s.ServiceType == typeof(TDbContext)));
+
+            dependencyManager.RegisterUsing(c =>
+            {
+                DbContextOptions options = c.Resolve<DbContextOptions>();
+
+                string connectionString = null;
+                DbConnection dbConnectionFromDbContextOptionsBuilder = null;
+
+                foreach (RelationalOptionsExtension ext in options.Extensions.OfType<RelationalOptionsExtension>())
+                {
+                    dbConnectionFromDbContextOptionsBuilder = dbConnectionFromDbContextOptionsBuilder ?? ext.Connection;
+                    connectionString = connectionString ?? dbConnectionFromDbContextOptionsBuilder?.ConnectionString;
+                }
+
+                TDbContext dbContext = (TDbContext)ActivatorUtilities.CreateInstance(c, typeof(TDbContext).GetTypeInfo());
+
+                if (connectionString != null) // It's not a relation db, for example UseInMemoryDatabase
+                {
+                    DbTransaction transaction = c.Resolve<IDbConnectionProvider>().GetDbTransaction(connectionString);
+
+                    if (transaction != null) // When DbConnectionProvider has failed to open a connection to db due connection open timeout or any other reasons such as database does not exist, it won't provide any transaction too.
+                    {
+                        if (transaction.Connection != dbConnectionFromDbContextOptionsBuilder) // You're using a db connection other than db connection created by bit.
+                            throw new InvalidOperationException("You've to configure optionsBuilder by calling (serviceProvider, optionsBuilder) => { optionsBuilder.UseSqlServer /* or UseSqlite, UseInMemoryDatabase, ... */ (serviceProvider.GetService<IDbConnectionProvider>().GetDbConnection(\"YOUR_CONNECTION_STRING_FROM_CONFIG_FOR_EXAMPLE\", rollbackOnScopeStatusFailure: true)) }.");
+                        if (dbContext.Database.CurrentTransaction != null) // You're using a db transaction other than one provided by bit.
+                            throw new InvalidOperationException("DbContext.Database.CurrentTransaction is not null");
+                        dbContext.Database.UseTransaction(transaction);
+                    }
+                }
+
+                return dbContext;
+            }, servicesType: new[] { typeof(EfCoreDbContextBase).GetTypeInfo(), typeof(TDbContext).GetTypeInfo() }, overwriteExciting: false);
+
             dependencyManager.Register<IDataProviderSpecificMethodsProvider, EfCoreDataProviderSpecificMethodsProvider>(lifeCycle: DependencyLifeCycle.SingleInstance, overwriteExciting: false);
             dependencyManager.Register<EfCoreDataProviderSpecificMethodsProvider, EfCoreDataProviderSpecificMethodsProvider>(lifeCycle: DependencyLifeCycle.SingleInstance, overwriteExciting: false);
             dependencyManager.Register<IUnitOfWork, DefaultUnitOfWork>(overwriteExciting: false);
--- src/Server/Bit.Data.EntityFrameworkCore/Implementations/EfCoreDbContextBase.cs
+++ src/Server/Bit.Data.EntityFrameworkCore/Implementations/EfCoreDbContextBase.cs
@@ -1,56 +1,22 @@
-﻿using Bit.Data.EntityFrameworkCore.Contracts;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using System;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Bit.Data.EntityFrameworkCore.Implementations
 {
     public class EfCoreDbContextBase : DbContext
     {
-        protected EfCoreDbContextBase()
+        public EfCoreDbContextBase()
         {
         }
 
         public EfCoreDbContextBase(DbContextOptions options)
             : base(options)
         {
-            ApplyDefaultConfig();
-        }
-
-        public EfCoreDbContextBase(string connectionString, IDbContextObjectsProvider dbContextCreationOptionsProvider)
-            : base(dbContextCreationOptionsProvider.GetDbContextOptions(connectionString).Options)
-        {
-            if (dbContextCreationOptionsProvider == null)
-                throw new ArgumentNullException(nameof(dbContextCreationOptionsProvider));
-
-            if (connectionString == null)
-                throw new ArgumentNullException(nameof(connectionString));
-
-            DbContextObjects dbContextObjects = dbContextCreationOptionsProvider.GetDbContextOptions(connectionString);
-
-            if (dbContextObjects.Transaction != null /* We're going to use a relational database */)
-                UseTransactionForRelationalDatabases(dbContextObjects);
-
-            ApplyDefaultConfig();
+            
         }
 
         public virtual bool ChangeTrackingEnabled()
         {
             return true;
-        }
-
-        protected virtual void ApplyDefaultConfig()
-        {
-            if (ChangeTrackingEnabled() == false)
-                ChangeTracker.AutoDetectChangesEnabled = false;
-        }
-
-        protected virtual void UseTransactionForRelationalDatabases(DbContextObjects dbContextObjects)
-        {
-            // Although Database is in EntityFramework.Core.dll, but it has a dependency on EntityFramework.Relational.dll
-            // We're going to call this method, only in case of relation database usage.
-            // So this property has been used in separated method call stack.
-            Database.UseTransaction(dbContextObjects.Transaction);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -59,8 +25,6 @@ namespace Bit.Data.EntityFrameworkCore.Implementations
             {
                 optionsBuilder = optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             }
-
-            optionsBuilder.ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
 
             base.OnConfiguring(optionsBuilder);
         }
