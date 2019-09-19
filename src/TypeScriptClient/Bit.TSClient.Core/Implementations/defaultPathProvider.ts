69a8e663c7634c712348c1648924fd0102c6fc0a
Yaser Moradi <ysmoradi@outlook.com>
2019-09-19 10:05:34 +0430

    Enhanced url detection in ts client

--- src/TypeScriptClient/Bit.TSClient.Core/Implementations/defaultPathProvider.ts
+++ src/TypeScriptClient/Bit.TSClient.Core/Implementations/defaultPathProvider.ts
@@ -2,7 +2,7 @@
 
     export class PathUtils {
 
-        private static urlRegex = new RegExp("^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$", "i");
+        private static urlRegex = new RegExp("^([a-z]+://|//)", "i"); // https://stackoverflow.com/a/19692053/2720104
 
         public static isUrlPath(path: string): boolean {
 
