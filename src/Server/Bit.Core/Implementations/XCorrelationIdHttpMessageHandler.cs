55f0b89989ac62f671b8604dc2c97d2d2b633254
ysmoradi <ysmoradi@outlook.com>
2020-01-21 15:25:51 +0330

    Added XCorrelationIdHttpMessageHandler

---
+++ src/Server/Bit.Core/Implementations/XCorrelationIdHttpMessageHandler.cs
@@ -0,0 +1,31 @@
+﻿using Bit.Core.Contracts;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bit.Core.Implementations
+{
+    public class XCorrelationIdHttpMessageHandler : DelegatingHandler
+    {
+        private readonly IRequestInformationProvider _requestInformationProvider;
+
+        public XCorrelationIdHttpMessageHandler(IRequestInformationProvider requestInformationProvider)
+        {
+            _requestInformationProvider = requestInformationProvider;
+        }
+
+        public XCorrelationIdHttpMessageHandler(IRequestInformationProvider requestInformationProvider, HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+            _requestInformationProvider = requestInformationProvider;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!request.Headers.Contains("X-Correlation-ID"))
+                request.Headers.Add("X-Correlation-ID", _requestInformationProvider.XCorrelationId);
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
