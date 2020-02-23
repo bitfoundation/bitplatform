82e4c847acd9c943c0c4a7db52c77049c1002b25
ysmoradi <ysmoradi@outlook.com>
2020-02-23 18:52:25 +0330

    Added Inline data template selector

---
+++ src/CSharpClient/Bit.CSharpClient.Prism/View/InlineDataTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Bit.View
+{
+    [ContentProperty(nameof(Template))]
+    public class InlineDataTemplate
+    {
+        public Type ItemType { get; set; }
+
+        public DataTemplate Template { get; set; }
+    }
+
+    public class InlineDataTemplateSelector : DataTemplateSelector
+    {
+        public List<InlineDataTemplate> Templates { get; set; } = new List<InlineDataTemplate> { };
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            foreach (InlineDataTemplate inlineDataTemplate in Templates)
+            {
+                if (item.GetType() == inlineDataTemplate.ItemType)
+                    return inlineDataTemplate.Template;
+            }
+
+            throw new InvalidOperationException($"{nameof(InlineDataTemplate)} could not be found for item with type: {item.GetType().FullName}");
+        }
+    }
+}
