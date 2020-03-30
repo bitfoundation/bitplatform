15ffef9c8f1fd8ddbfd74cb07d9d0b81e46acf5e
ysmoradi <ysmoradi@outlook.com>
2020-03-30 22:50:28 +0430

    Moved BooleanInverseConverter from controls to prism

---
+++ src/CSharpClient/Bit.CSharpClient.Prism/View/Converters/BooleanInverseConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bit.View
+{
+    public class BooleanInverseConverter : ValueConverter<bool, bool>
+    {
+        protected override bool Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !value;
+        }
+
+        protected override bool ConvertBack(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !value;
+        }
+    }
+}
