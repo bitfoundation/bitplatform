b90f36305a81d22ea0ae03794df38b4b1ff21dc8
ysmoradi <ysmoradi@outlook.com>
2020-04-03 14:00:02 +0430

    Bit.Extensions.Caching.EasyCaching

---
+++ src/Extensions/Bit.Extensions.Caching.EasyCaching/Implementations/EasyCachingOptionsBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Bit.Extensions.Caching.EasyCaching.Implementations
+{
+    public abstract class EasyCachingOptionsBase
+    {
+        public TimeSpan DefaultExpiration { get; set; } = TimeSpan.FromMinutes(5);
+    }
+}
