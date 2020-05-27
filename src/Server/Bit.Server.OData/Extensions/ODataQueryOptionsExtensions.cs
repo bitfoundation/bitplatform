132e350ec6326968be26d08353e848fba665bea0
Yaser Moradi <ysmoradi@outlook.com>
2020-05-27 23:36:15 +0430

    enabled nested odata expand in GetExpandedProperties of ODataQueryOptionsExtensions

--- src/Server/Bit.Server.OData/Extensions/ODataQueryOptionsExtensions.cs
+++ src/Server/Bit.Server.OData/Extensions/ODataQueryOptionsExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.OData.UriParser;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Microsoft.AspNet.OData.Query
@@ -11,8 +12,34 @@ namespace Microsoft.AspNet.OData.Query
             if (oDataQueryOptions == null)
                 throw new ArgumentNullException(nameof(oDataQueryOptions));
 
-            return oDataQueryOptions.SelectExpand?.SelectExpandClause
-                ?.SelectedItems?.OfType<ExpandedReferenceSelectItem>()?.SelectMany(i => i.PathToNavigationProperty)?.Select(i => i.Identifier)?.ToArray() ?? Array.Empty<string>();
+            static void GetEntirePath(ExpandedNavigationSelectItem expandItem, ref List<ExpandedNavigationSelectItem> expandItems)
+            {
+                expandItems.Add(expandItem);
+
+                var nestedExpandItems = expandItem.SelectAndExpand?.SelectedItems?.OfType<ExpandedNavigationSelectItem>()?.ToList();
+
+                if (nestedExpandItems != null)
+                {
+                    foreach (var nestedExpandItem in nestedExpandItems)
+                    {
+                        GetEntirePath(nestedExpandItem, ref expandItems);
+                    }
+                }
+            }
+
+            var finalResult = new List<string> { };
+
+            foreach (ExpandedNavigationSelectItem expandItem in oDataQueryOptions.SelectExpand?.SelectExpandClause
+                ?.SelectedItems?.OfType<ExpandedNavigationSelectItem>() ?? new ExpandedNavigationSelectItem[] { })
+            {
+                var expandItems = new List<ExpandedNavigationSelectItem>();
+                GetEntirePath(expandItem, ref expandItems);
+                var expandItemNames = expandItems.SelectMany(i => i.PathToNavigationProperty.Select(i => i.Identifier)).ToArray();
+                var result = string.Join(".", expandItemNames);
+                finalResult.Add(result);
+            }
+
+            return finalResult.ToArray();
         }
     }
 }
