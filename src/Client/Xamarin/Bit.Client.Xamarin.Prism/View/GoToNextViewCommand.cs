3ed515e0cb5aec9074fe1d1cac63e84a1c080afe
Yaser Moradi <ysmoradi@outlook.com>
2020-07-14 10:52:46 +0430

    Enhanced GoToNextViewCommand

---
+++ src/Client/Xamarin/Bit.Client.Xamarin.Prism/View/GoToNextViewCommand.cs
@@ -0,0 +1,25 @@
+﻿using Bit.ViewModel;
+
+namespace Bit.View
+{
+    public class GoToNextViewCommand : BitDelegateCommand
+    {
+        public GoToNextViewCommand() 
+            : base(async () => { })
+        {
+
+        }
+
+        protected override bool CanExecute(object parameter)
+        {
+            return Next != null;
+        }
+
+        protected override void Execute(object parameter)
+        {
+            Next!.Focus();
+        }
+
+        public Xamarin.Forms.View? Next { get; set; }
+    }
+}
