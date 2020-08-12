262e7d1f4d8f9d07c0f805240e4a506bb1f32240
Yaser Moradi <ysmoradi@outlook.com>
2020-08-12 15:12:12 +0430

    added bit global styles + sample

---
+++ src/Client/Xamarin/Bit.Client.Xamarin.Controls/Implementations/BitGlobalStyles.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Bit.View
+{
+    public partial class BitGlobalStyles
+    {
+        public BitGlobalStyles()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Client/Xamarin/Sample/Bit.CSharpClientSample/Views/GlobalStyles.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Bit.CSharpClientSample.Views
+{
+    public partial class GlobalStyles
+    {
+        public GlobalStyles()
+        {
+            InitializeComponent();
+        }
+    }
+}
