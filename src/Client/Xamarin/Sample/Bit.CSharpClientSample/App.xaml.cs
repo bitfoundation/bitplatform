618c00c5a2db666f54d94d5696b3d51e49ee53af
Yaser Moradi <ysmoradi@outlook.com>
2020-09-21 15:02:24 +0330

    Renamed UnauthorizedRequestEvent to UnauthorizedResponseEvent

--- src/Client/Xamarin/Sample/Bit.CSharpClientSample/App.xaml.cs
+++ src/Client/Xamarin/Sample/Bit.CSharpClientSample/App.xaml.cs
@@ -66,7 +66,7 @@ namespace Bit.CSharpClientSample
 
             IEventAggregator eventAggregator = Container.Resolve<IEventAggregator>();
 
-            eventAggregator.GetEvent<UnauthorizedRequestEvent>()
+            eventAggregator.GetEvent<UnauthorizedResponseEvent>()
                 .SubscribeAsync(async tokenExpiredEvent => await NavigationService.NavigateAsync("/Nav/Login"), ThreadOption.UIThread);
 
             await base.OnInitializedAsync();
