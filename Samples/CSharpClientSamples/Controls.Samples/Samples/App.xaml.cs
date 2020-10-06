a8be8e5af53e035f2f82777caa0dcbb14cbff5f3
Yaser Moradi <ysmoradi@outlook.com>
2020-10-06 16:15:18 +0330

    Updated samples to bit 6.1.0

--- Samples/CSharpClientSamples/Controls.Samples/Samples/App.xaml.cs
+++ Samples/CSharpClientSamples/Controls.Samples/Samples/App.xaml.cs
@@ -3,6 +3,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Vazir-Light-FD-WOL.ttf", Alias = "Vazir FD-WOL")]
 
 namespace Bit.CSharpClient.Controls.Samples
 {
