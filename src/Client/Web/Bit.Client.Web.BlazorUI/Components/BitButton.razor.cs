0be453cc46cc382b2543bfd280cdf4fcbe2bdd8e
Saleh Yusefnejad <msynk@outlook.com>
2021-01-30 00:35:25 +0330

    enhanced components structure

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/BitButton.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/BitButton.razor.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
-namespace Bit.Client.Web.BlazorUI
+namespace Bit.Client.Web.BlazorUI.Components
 {
     public partial class BitButton
     {
