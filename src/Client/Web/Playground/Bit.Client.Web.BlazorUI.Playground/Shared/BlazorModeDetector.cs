37d6263b6336db0ce697f1ef57f5b35071643bce
Saleh Yusefnejad <msynk@outlook.com>
2021-01-30 00:56:33 +0330

    enhanced BlazorModeDetector

---
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Shared/BlazorModeDetector.cs
@@ -0,0 +1,36 @@
+﻿namespace Bit.Client.Web.BlazorUI.Playground.Shared
+{
+    public class BlazorModeDetector
+    {
+        public static BlazorModeDetector Current { get; set; } = new BlazorModeDetector();
+
+        public virtual bool IsServer()
+        {
+            return Mode == BlazorMode.Server;
+        }
+
+        public virtual bool IsClient()
+        {
+            return Mode == BlazorMode.Client;
+        }
+
+        public virtual BlazorMode Mode
+        {
+            get
+            {
+#if BlazorClient
+                return BlazorMode.Client;
+#else
+                return BlazorMode.Server;
+#endif
+            }
+        }
+    }
+
+    public enum BlazorMode
+    {
+        Server,
+        Client
+    }
+
+}
