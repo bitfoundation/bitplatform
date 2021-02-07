5468eb7e89347cf4dfcd7d52691a3ee9726710bc
Hamed Moghadasi <h4lmed@gmail.com>
2021-02-07 23:24:48 +0330

    update unit-tests of bit-label

--- src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Label/BitLabelTests.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Label/BitLabelTests.cs
@@ -1,10 +1,5 @@
 ﻿using Bunit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bit.Client.Web.BlazorUI.Tests.Label
 {
