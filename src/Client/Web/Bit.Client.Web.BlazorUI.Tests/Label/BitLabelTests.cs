9a6702980335a94489da7b337c62cc1f7a9d46a0
Hamed Moghadasi <h4lmed@gmail.com>
2021-02-07 16:59:12 +0330

    add bit-label component

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Label/BitLabelTests.cs
@@ -0,0 +1,21 @@
+﻿using Bunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit.Client.Web.BlazorUI.Tests.Label
+{
+    [TestClass]
+    public class BitLabelTests: BunitTestContext
+    {
+        [DataTestMethod, DataRow(true, "required")]
+        public void IsRequired_HasRedAsterisk_True(bool isRequired, string requiredclassName)
+        {
+            var component = RenderComponent<BitLabelTest>(parameters => parameters.Add(p => p.IsRequired, isRequired));
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/BitLabel.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit.Client.Web.BlazorUI.Components
+{
+    public partial class BitLabel
+    {
+        [Parameter] public string Text { get; set; }
+        [Parameter] public bool IsRequired { get; set; }
+        [Parameter] public string componentRef { get; set; }
+
+        public string RequiredClass => IsRequired ? "required" : "";
+
+        public override Task SetParametersAsync(ParameterView parameters)
+        {
+            foreach (ParameterValue parameter in parameters)
+            {
+                switch (parameter.Name)
+                {
+                    case nameof(Text):
+                        Text = (string)parameter.Value;
+                        break;
+                    case nameof(IsRequired):
+                        IsRequired = (bool)parameter.Value;
+                        break;
+                    case nameof(componentRef):
+                        componentRef = (string)parameter.Value;
+                        break;
+                }
+            }
+
+            return base.SetParametersAsync(parameters);
+        }
+
+    }
+}
