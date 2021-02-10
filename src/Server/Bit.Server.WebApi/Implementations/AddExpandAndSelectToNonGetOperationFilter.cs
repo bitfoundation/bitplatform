bab45ae2ed33cfede04f4f85cd4bc05110bb015e
Yaser Moradi <ysmoradi@outlook.com>
2021-02-10 16:39:25 +0330

    Fixed odata swagger get by id (key) issue

--- src/Server/Bit.Server.WebApi/Implementations/AddExpandAndSelectToNonGetOperationFilter.cs
+++ src/Server/Bit.Server.WebApi/Implementations/AddExpandAndSelectToNonGetOperationFilter.cs
@@ -1,6 +1,7 @@
 ﻿using Swashbuckle.Swagger;
 using System;
-using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Description;
 
@@ -19,8 +20,10 @@ namespace Bit.WebApi.Implementations
             if (apiDescription == null)
                 throw new ArgumentNullException(nameof(apiDescription));
 
-            if (typeof(SingleResult).IsAssignableFrom(apiDescription.ResponseType()) && apiDescription.HttpMethod != HttpMethod.Get)
+            if (typeof(SingleResult).IsAssignableFrom(apiDescription.ResponseType()) && !apiDescription.ParameterDescriptions.Any(p => p.Name == "$select" || p.Name == "$expand"))
             {
+                operation.parameters = operation.parameters ?? new List<Parameter>();
+
                 operation.parameters.Add(new Parameter { name = "$expand", @in = "query", description = "Expands related entities inline.", type = "string", required = false });
                 operation.parameters.Add(new Parameter { name = "$select", @in = "query", description = "Selects which properties to include in the response.", type = "string", required = false });
             }
