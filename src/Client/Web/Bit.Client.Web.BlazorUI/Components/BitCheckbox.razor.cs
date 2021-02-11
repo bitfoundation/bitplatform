cbf1b150af138befe0c3d9f07072c960a6eefe01
Hamed Moghadasi <h4lmed@gmail.com>
2021-02-11 03:36:29 +0330

    Added "BoxSide" attribute to component.

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/BitCheckbox.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/BitCheckbox.razor.cs
@@ -10,10 +10,10 @@ namespace Bit.Client.Web.BlazorUI.Components
 {
     public partial class BitCheckbox
     {
-
         [Parameter] public string Label { get; set; }
         [Parameter] public bool IsChecked { get; set; }
         [Parameter] public bool IsIndeterminate { get; set; }
+        [Parameter] public string BoxSide { get; set; }
         [Parameter] public EventCallback<MouseEventArgs> OnCheckboxClick { get; set; }
         [Parameter] public Action<bool> OnCheckboxChange { get; set; }
 
@@ -51,6 +51,9 @@ namespace Bit.Client.Web.BlazorUI.Components
                         break;
                     case nameof(IsIndeterminate):
                         IsIndeterminate = (bool)parameter.Value;
+                        break;
+                    case nameof(BoxSide):
+                        BoxSide = (string)parameter.Value;
                         break;
                     case nameof(OnCheckboxChange):
                         OnCheckboxChange = (Action<bool>)parameter.Value;
