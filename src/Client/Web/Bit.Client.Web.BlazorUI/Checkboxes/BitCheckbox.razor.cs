3a8931c8a6479fbf918b0a3d22cf51049ce98a82
Hamed Moghadasi <h4lmed@gmail.com>
2021-02-13 17:24:03 +0330

    Added Indeterminate functionality.
     + Complete User Interface
     + Create seprate folder for each stylesheet file.

--- src/Client/Web/Bit.Client.Web.BlazorUI/Checkboxes/BitCheckbox.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Checkboxes/BitCheckbox.razor.cs
@@ -1,40 +1,69 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using System.Threading.Tasks;
+using Microsoft.JSInterop;
 using System;
 
 namespace Bit.Client.Web.BlazorUI.Checkboxes
 {
     public partial class BitCheckbox
     {
+        [Inject] public IJSRuntime JSRuntime { get; set; }
+
         [Parameter] public string Label { get; set; }
         [Parameter] public string CustomLabel { get; set; }
-        [Parameter] public bool IsChecked { get; set; }
         [Parameter] public bool IsIndeterminate { get; set; }
+        [Parameter] public bool IsChecked { get; set; } = false;
         [Parameter] public string BoxSide { get; set; }
         [Parameter] public EventCallback<MouseEventArgs> OnCheckboxClick { get; set; }
         [Parameter] public Action<bool> OnCheckboxChange { get; set; }
 
 
-        public string CheckedClass => IsChecked ? "checked" : string.Empty;
+        public ElementReference TargetCheckbox { get; set; }
         public string IndeterminateClass => IsIndeterminate ? "indeterminate" : string.Empty;
+        public string CheckedClass => IsChecked ? "checked" : string.Empty;
         public bool HasCustomLabel => CustomLabel.HasValue() ? true : false;
 
         public string InputUniqueIdentifier = Guid.NewGuid().ToString("n");
 
+        protected void HandleIndeterminate() {
+            if (IsIndeterminate) {
+                IsIndeterminate = false;
+                _ = JSRuntime.SetElementProperty(TargetCheckbox, "indeterminate", IsIndeterminate);
+                OnCheckboxChange?.Invoke(IsChecked);
+                return;
+            }
+        }
         protected void HandleCheckboxClick()
         {
             if (IsEnabled)
             {
-                if (IsChecked)
+                if (IsIndeterminate)
                 {
-                    IsChecked = false;
+                    HandleIndeterminate();
                 }
-                else {
-                    IsChecked = true;
+                else
+                {
+                    if (IsChecked)
+                    {
+                        IsChecked = false;
+                    }
+                    else
+                    {
+                        IsChecked = true;
+                    }
                 }
-                OnCheckboxChange?.Invoke(IsChecked);
             }
+            OnCheckboxChange?.Invoke(IsChecked);
+        }
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender) 
+            {
+                _ = JSRuntime.SetElementProperty(TargetCheckbox, "indeterminate", IsIndeterminate);
+            }
+            
+            return base.OnAfterRenderAsync(firstRender);
         }
         public override Task SetParametersAsync(ParameterView parameters)
         {
--- src/Client/Web/Bit.Client.Web.BlazorUI/wwwroot/scripts/bit.client.web.ts
+++ src/Client/Web/Bit.Client.Web.BlazorUI/wwwroot/scripts/bit.client.web.ts
@@ -1,1 +1,9 @@
 ﻿
+function setElementProperty(element: HTMLElement, property: string, value: object) {
+    const _element: { [key: string]: string | any } = element;
+    _element[property] = value;
+}
+
+function getElementProperty(element: HTMLElement, property: string): string | null {
+    return element.getAttribute(property);
+}
