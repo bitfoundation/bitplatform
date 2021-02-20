ccb76606ede4fe67c0a185dcab6a14e4b3e83966
artav <artav.work@outlook.com>
2021-02-21 00:00:58 +0330

    Add BitMessageBar

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Notifications/BitMessageBar.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI.Notifications
+{
+    public partial class BitMessageBar
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        public override Task SetParametersAsync(ParameterView parameters)
+        {
+            foreach (ParameterValue parameter in parameters)
+            {
+                switch (parameter.Name)
+                {
+                    case nameof(ChildContent):
+                        ChildContent = (RenderFragment)parameter.Value;
+                        break;
+                }
+            }
+            return base.SetParametersAsync(parameters);
+        }
+    }
+}
