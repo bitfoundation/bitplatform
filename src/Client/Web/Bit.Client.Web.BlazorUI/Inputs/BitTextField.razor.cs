0280619b17f880b935edfe87f81028bf0d80d2d5
artav <artav.work@outlook.com>
2021-03-01 12:03:24 +0330

    Add Textfield

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Inputs/BitTextField.razor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI.Inputs
+{
+    public partial class BitTextField
+    {
+        [Parameter] public string Value { get; set; }
+        [Parameter] public string Placeholder { get; set; }
+        [Parameter] public bool IsReadonly { get; set; } = false;
+        public string ReadonlyClass => IsReadonly ? "no-text-select" : "";
+        public override Task SetParametersAsync(ParameterView parameters)
+        {
+            foreach (ParameterValue parameter in parameters)
+            {
+                switch (parameter.Name)
+                {
+                    case nameof(Value):
+                        Value = (string)parameter.Value;
+                        break;
+                    case nameof(Placeholder):
+                        Placeholder = (string)parameter.Value;
+                        break;
+                }
+            }
+            return base.SetParametersAsync(parameters);
+        }
+    }
+}
