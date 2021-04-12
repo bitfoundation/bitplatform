c49294e61dd121f28ad7d5c2930a2bbc78a4e604
Hamed Moghadasi <h4lmed@gmail.com>
2021-04-12 07:39:39 +0430

    Update bit.client.web.ts

--- src/Client/Web/Bit.Client.Web.BlazorUI/wwwroot/scripts/bit.client.web.ts
+++ src/Client/Web/Bit.Client.Web.BlazorUI/wwwroot/scripts/bit.client.web.ts
@@ -1,13 +1,13 @@
 ﻿class Bit {
-  static setProperty(
-    element: { [key: string]: any },
-    property: string,
-    value: any
-  ): void {
-    element[property] = value;
-  }
+    static setProperty(
+        element: { [key: string]: any },
+        property: string,
+        value: any
+    ): void {
+        element[property] = value;
+    }
 
-  static getProperty(element: HTMLElement, property: string): string | null {
-    return element.getAttribute(property);
-  }
+    static getProperty(element: HTMLElement, property: string): string | null {
+        return element.getAttribute(property);
+    }
 }
