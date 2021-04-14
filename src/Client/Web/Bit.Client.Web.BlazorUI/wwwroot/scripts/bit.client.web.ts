4cc8618bdc3e60cb52729ecc1480c46f03f0bb6b
Hamed Moghadasi <h4lmed@gmail.com>
2021-04-14 17:05:41 +0430

    Refactor Get/SetProperty method in TS file.

--- src/Client/Web/Bit.Client.Web.BlazorUI/wwwroot/scripts/bit.client.web.ts
+++ src/Client/Web/Bit.Client.Web.BlazorUI/wwwroot/scripts/bit.client.web.ts
@@ -1,11 +1,9 @@
 ﻿class Bit {
     static setProperty(element: { [key: string]: any },property: string,value: any): void {
-        const _element: { [key: string]: any } = element;
-        _element[property] = value;
+        element[property] = value;
     }
 
-    static getProperty(element: HTMLElement, property: string): string | null {
-        const _element: { [key: string]: string | any } = element;
-        return _element[property];
+    static getProperty(element: { [key: string]: any }, property: string): string | null {
+        return element[property];
     }
 }
