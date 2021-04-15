c7617222814323aea581fa408621847df022ec7c
Hamed Moghadasi <h4lmed@gmail.com>
2021-04-15 18:23:47 +0430

    add search-box component (#237)
    add search-box component (#237)

--- src/Client/Web/Bit.Client.Web.BlazorUI/wwwroot/scripts/bit.client.web.ts
+++ src/Client/Web/Bit.Client.Web.BlazorUI/wwwroot/scripts/bit.client.web.ts
@@ -1,9 +1,9 @@
 ﻿class Bit {
-    static setProperty(element: { [key: string]: any }, property: string, value: any): void {
+    static setProperty(element: { [key: string]: any },property: string,value: any): void {
         element[property] = value;
     }
 
-    static getProperty(element: HTMLElement, property: string): string | null {
-        return element.getAttribute(property);
+    static getProperty(element: { [key: string]: any }, property: string): string | null {
+        return element[property];
     }
 }
