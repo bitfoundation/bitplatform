02a449b6d8010e3eacf67a7f820112893b892e7f
Hamed Moghadasi <h4lmed@gmail.com>
2021-04-19 19:17:13 +0430

    Initial spinner buttun component

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/SpinnerButtons/BitSpinnerButton.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public partial class BitSpinnerButton
+    {
+        protected override string GetElementClass()
+        {
+            return base.GetElementClass();
+        }
+
+        public override Task SetParametersAsync(ParameterView parameters)
+        {
+            return base.SetParametersAsync(parameters);
+        }
+    }
+}
