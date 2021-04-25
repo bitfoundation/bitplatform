d63856a32032eeaae81c5114f75519f33e094223
Cyrus <cyrus.sushiant@hotmail.com>
2021-04-25 17:52:16 +0430

    Add source generator for set parameters

---
+++ src/Tooling/Bit.Tooling.SourceGenerators/BitSyntaxReceiver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Bit.Tooling.SourceGenerators
+{
+    public class BitSyntaxReceiver : ISyntaxContextReceiver
+    {
+        public List<IPropertySymbol> Properties { get; } = new List<IPropertySymbol>();
+
+        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+        {
+            if (context.Node is PropertyDeclarationSyntax propertyDeclarationSyntax
+                    && propertyDeclarationSyntax.AttributeLists.Count > 0)
+            {
+
+                ClassDeclarationSyntax classDeclarationSyntax = (ClassDeclarationSyntax)propertyDeclarationSyntax.Parent;
+
+                if (!classDeclarationSyntax.Modifiers.Any(k => k.IsKind(SyntaxKind.PartialKeyword)))
+                    return;
+
+                IPropertySymbol propertySymbol = context.SemanticModel.GetDeclaredSymbol(propertyDeclarationSyntax);
+
+                if (propertySymbol.GetAttributes().Any(ad => ad.AttributeClass.ToDisplayString() == "Microsoft.AspNetCore.Components.ParameterAttribute"))
+                {
+                    Properties.Add(propertySymbol);
+                }
+            }
+        }
+    }
+}
