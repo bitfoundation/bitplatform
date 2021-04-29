b3f4f42af031fcf3fcda089411a24e30b4a62b20
mirza-developer <43365143+mirza-developer@users.noreply.github.com>
2021-04-29 18:37:42 +0430

    Add expand class

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/Inputs/BitDropDown.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Inputs/BitDropDown.razor.cs
@@ -1,18 +1,78 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace Bit.Client.Web.BlazorUI
 {
     public partial class BitDropDown
     {
+        private string focusClass = "";
+        private string expandClass = "";
+
         [Parameter] public List<DropDownItem> Items { get; set; } = new List<DropDownItem>();
         [Parameter] public string Placeholder { get; set; }
+        [Parameter] public string Value { get; set; }
+        [Parameter] public EventCallback<FocusEventArgs> OnFocus { get; set; }
+        [Parameter] public EventCallback<FocusEventArgs> OnFocusIn { get; set; }
+        [Parameter] public EventCallback<FocusEventArgs> OnFocusOut { get; set; }
+
+        public string FocusClass
+        {
+            get => focusClass;
+            set
+            {
+                focusClass = value;
+                ClassBuilder.Reset();
+            }
+        }
+        public string ExpandClass
+        {
+            get => expandClass;
+            set
+            {
+                expandClass = value;
+                ClassBuilder.Reset();
+            }
+        }
 
         protected override string RootElementClass => "bit-drp";
         protected override void RegisterComponentClasses()
         {
             //ClassBuilder.Register(() => IsChecked is true ?
             //    $"{RootElementClass}-checked-{VisualClassRegistrar()}" : string.Empty);
+            ClassBuilder.Register(() => string.IsNullOrWhiteSpace(FocusClass)
+                ? string.Empty
+                : $"{RootElementClass}-{FocusClass}-{VisualClassRegistrar()}");
+
+            ClassBuilder.Register(() => string.IsNullOrWhiteSpace(ExpandClass)
+                ? string.Empty
+                : $"{RootElementClass}-{ExpandClass}-{VisualClassRegistrar()}");
+        }
+
+        protected virtual async Task HandleFocus(FocusEventArgs e)
+        {
+            if (IsEnabled)
+            {
+                FocusClass = "focused";
+                await OnFocus.InvokeAsync(e);
+            }
+        }
+        protected virtual async Task HandleFocusIn(FocusEventArgs e)
+        {
+            if (IsEnabled)
+            {
+                FocusClass = "focused";
+                await OnFocus.InvokeAsync(e);
+            }
+        }
+        protected virtual async Task HandleFocusOut(FocusEventArgs e)
+        {
+            if (IsEnabled)
+            {
+                FocusClass = "";
+                await OnFocusOut.InvokeAsync(e);
+            }
         }
     }
 }
