65edf8432e47b96fdf7fa51f39341b85116bfd46
Somaye Ebrahimi <s.ebrahimi67@hotmail.com>
2021-04-29 19:02:13 +0430

    Add spinner component

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Spinner/BitSpinnerTests.cs
@@ -0,0 +1,89 @@
+﻿using Bunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bit.Client.Web.BlazorUI.Tests.Spinner
+{
+    [TestClass]
+    public class BitSpinnerTests : BunitTestContext
+    {
+        [DataTestMethod, DataRow(SpinnerSize.Large, true)]
+        public void BitSpinnerShouldRespectLargeSize(SpinnerSize size, bool expectedResult)
+        {
+            var component = RenderComponent<BitSpinner>(parameters => parameters.Add(p => p.Size, size));
+
+            var bitLabel = component.Find(".bit-spinner");
+
+            Assert.AreEqual(expectedResult, bitLabel.ClassList.Contains("bit-spinner-large-fluent"));
+        }
+
+        [DataTestMethod, DataRow(null, true), DataRow(SpinnerSize.Medium, true)]
+        public void BitSpinnerShouldRespectMediumSize(SpinnerSize size, bool expectedResult)
+        {
+            var component = RenderComponent<BitSpinner>(parameters => parameters.Add(p => p.Size, size));
+
+            var bitLabel = component.Find(".bit-spinner");
+
+            Assert.AreEqual(expectedResult, bitLabel.ClassList.Contains("bit-spinner-medium-fluent"));
+        }
+
+        [DataTestMethod, DataRow(SpinnerSize.Small, true)]
+        public void BitSpinnerShouldRespectSmallSize(SpinnerSize size, bool expectedResult)
+        {
+            var component = RenderComponent<BitSpinner>(parameters => parameters.Add(p => p.Size, size));
+
+            var bitLabel = component.Find(".bit-spinner");
+
+            Assert.AreEqual(expectedResult, bitLabel.ClassList.Contains("bit-spinner-small-fluent"));
+        }
+
+        [DataTestMethod, DataRow(SpinnerSize.XSmall, true)]
+        public void BitSpinnerShouldRespectXSmallSize(SpinnerSize size, bool expectedResult)
+        {
+            var component = RenderComponent<BitSpinner>(parameters => parameters.Add(p => p.Size, size));
+
+            var bitLabel = component.Find(".bit-spinner");
+
+            Assert.AreEqual(expectedResult, bitLabel.ClassList.Contains("bit-spinner-xSmall-fluent"));
+        }
+
+        [DataTestMethod, DataRow(SpinnerLabelPosition.Bottom, true)]
+        public void BitSpinnerShouldRespectBottomPosition(SpinnerLabelPosition position, bool expectedResult)
+        {
+            var component = RenderComponent<BitSpinner>(parameters => parameters.Add(p => p.LabelPosition, position));
+
+            var bitLabel = component.Find(".bit-spinner");
+
+            Assert.AreEqual(expectedResult, bitLabel.ClassList.Contains("bit-spinner-bottom-fluent"));
+        }
+
+        [DataTestMethod, DataRow(SpinnerLabelPosition.Top, true)]
+        public void BitSpinnerShouldRespectTopPosition(SpinnerLabelPosition position, bool expectedResult)
+        {
+            var component = RenderComponent<BitSpinner>(parameters => parameters.Add(p => p.LabelPosition, position));
+
+            var bitLabel = component.Find(".bit-spinner");
+
+            Assert.AreEqual(expectedResult, bitLabel.ClassList.Contains("bit-spinner-top-fluent"));
+        }
+
+        [DataTestMethod, DataRow(SpinnerLabelPosition.Right, true)]
+        public void BitSpinnerShouldRespectRightPosition(SpinnerLabelPosition position, bool expectedResult)
+        {
+            var component = RenderComponent<BitSpinner>(parameters => parameters.Add(p => p.LabelPosition, position));
+
+            var bitLabel = component.Find(".bit-spinner");
+
+            Assert.AreEqual(expectedResult, bitLabel.ClassList.Contains("bit-spinner-right-fluent"));
+        }
+
+        [DataTestMethod, DataRow(SpinnerLabelPosition.Left, true)]
+        public void BitSpinnerShouldRespectLeftPosition(SpinnerLabelPosition position, bool expectedResult)
+        {
+            var component = RenderComponent<BitSpinner>(parameters => parameters.Add(p => p.LabelPosition, position));
+
+            var bitLabel = component.Find(".bit-spinner");
+
+            Assert.AreEqual(expectedResult, bitLabel.ClassList.Contains("bit-spinner-left-fluent"));
+        }
+    }
+}
---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Spinner/BitSpinner.razor.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public partial class BitSpinner
+    {
+        [Parameter] public SpinnerLabelPosition LabelPosition { get; set; }
+        [Parameter] public SpinnerSize Size { get; set; }
+        [Parameter] public string Label { get; set; }
+
+        protected override string RootElementClass => "bit-spinner";
+
+        protected override void RegisterComponentClasses()
+        {
+            ClassBuilder.Register(() => GetClassSize());
+            ClassBuilder.Register(() => GetClassLabelPosition());
+        }
+
+        private string GetClassSize()
+        {
+            string classSize = $"{RootElementClass}-medium-{VisualClassRegistrar()}";
+
+            switch (Size)
+            {
+                case SpinnerSize.XSmall:
+                    classSize = $"{RootElementClass}-xSmall-{VisualClassRegistrar()}";
+                    break;
+                case SpinnerSize.Small:
+                    classSize = $"{RootElementClass}-small-{VisualClassRegistrar()}";
+                    break;
+                case SpinnerSize.Large:
+                    classSize = $"{RootElementClass}-large-{VisualClassRegistrar()}";
+                    break;
+            }
+
+            return classSize;
+        }
+
+        private string GetClassLabelPosition()
+        {
+            string classLabelPosition = $"{RootElementClass}-bottom-{VisualClassRegistrar()}";
+
+            switch (LabelPosition)
+            {
+                case SpinnerLabelPosition.Top:
+                    classLabelPosition = $"{RootElementClass}-top-{VisualClassRegistrar()}";
+                    break;
+                case SpinnerLabelPosition.Left:
+                    classLabelPosition = $"{RootElementClass}-left-{VisualClassRegistrar()}";
+                    break;
+                case SpinnerLabelPosition.Right:
+                    classLabelPosition = $"{RootElementClass}-right-{VisualClassRegistrar()}";
+                    break;
+            }
+
+            return classLabelPosition;
+        }
+    }
+}
