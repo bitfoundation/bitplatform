34edf2b8cc119bc8adca9bdf9fcd93b2c941387b
mirza-developer <43365143+mirza-developer@users.noreply.github.com>
2021-04-29 14:53:34 +0430

    Add drop down component

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Inputs/BitDropDown.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public partial class BitDropDown
+    {
+        [Parameter] public List<DropDownItem> Items { get; set; } = new List<DropDownItem>();
+        [Parameter] public string Placeholder { get; set; }
+
+        protected override string RootElementClass => "bit-drp";
+        protected override void RegisterComponentClasses()
+        {
+            //ClassBuilder.Register(() => IsChecked is true ?
+            //    $"{RootElementClass}-checked-{VisualClassRegistrar()}" : string.Empty);
+        }
+    }
+}
---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Inputs/DropDownItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Bit.Client.Web.BlazorUI
+{
+    public class DropDownItem
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+        public bool IsDisabled { get; set; } = false;
+        public DropDownItemType ItemType { get; set; } = DropDownItemType.Normal;
+    }
+}
