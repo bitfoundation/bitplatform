257d89521752edc0d825207cacc6a5e31266d644
Somaye Ebrahimi <s.ebrahimi67@hotmail.com>
2021-05-08 10:43:48 +0430

    Add icon button component with tooltip (#371)

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Buttons/BitIconButton.razor.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public partial class BitIconButton
+    {
+        [Parameter] public string AriaDescription { get; set; }
+        [Parameter] public bool AriaHidden { get; set; }
+        [Parameter] public string AriaLabel { get; set; }
+        [Parameter] public string IconName { get; set; }
+        [Parameter] public string ToolTip { get; set; }
+        [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        protected override string RootElementClass => "bit-ico-btn";
+
+        protected override void RegisterComponentClasses()
+        {
+            ClassBuilder.Register(() => IsEnabled is false
+                                        ? string.Empty
+                                        : $"{RootElementClass}-default-{VisualClassRegistrar()}");
+        }
+
+        protected virtual async Task HandleOnClick(MouseEventArgs e)
+        {
+            if (IsEnabled)
+            {
+                await OnClick.InvokeAsync(e);
+            }
+        }
+    }
+}
