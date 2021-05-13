e8510663a44525ccfb795ff26d178234fde6e980
kh.p1999 <kh.p1999@yahoo.com>
2021-05-14 02:18:28 +0430

    Create files

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Pivot/BitPivot.razor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public partial class BitPivot
+    {
+        protected override string RootElementClass => "bit-pvt";
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        public RenderFragment Content { get; set; }
+
+
+        private List<BitPivotItem> _items = new List<BitPivotItem>();
+
+        private int _activeItemIndex = 0;
+
+        public int ActiveTabIndex { get => _activeItemIndex; set => _activeItemIndex = value; }
+
+        internal void ItemClicked(BitPivotItem item)
+        {
+            _activeItemIndex = _items.IndexOf(item);
+        }
+
+        internal void RegisterOption(BitPivotItem item)
+        {
+            if (IsEnabled is false)
+            {
+                item.IsEnabled = false;
+            }
+            _items.Add(item);
+        }
+
+        internal void UnregisterOption(BitPivotItem item)
+        {
+            _items.Remove(item);
+        }
+    }
+}
---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Pivot/BitPivotItem.razor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public partial class BitPivotItem : IDisposable
+    {
+        protected override string RootElementClass => "bit-pvt-itm";
+
+        [CascadingParameter] protected BitPivot Pivot { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            if (Pivot is not null)
+            {
+                //Pivot.RegisterOption(this);
+                //if (string.IsNullOrEmpty(Name))
+                //{
+                //    Name = ChoiceGroup.Name;
+                //}
+            }
+
+            return base.OnInitializedAsync();
+        }
+
+        public void Dispose()
+        {
+            if (Pivot is null) return;
+
+            Pivot.UnregisterOption(this);
+        }
+    }
+}
