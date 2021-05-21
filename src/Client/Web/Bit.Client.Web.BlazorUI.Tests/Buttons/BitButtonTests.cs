03638bc60444a14065d7286eaef7637fbd7f1a32
Somaye Ebrahimi <s.ebrahimi67@hotmail.com>
2021-05-22 00:21:30 +0430

    Check tabindex attribute value on bitbutton test (#379)

--- src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Buttons/BitButtonTests.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Buttons/BitButtonTests.cs
@@ -63,7 +63,7 @@ namespace Bit.Client.Web.BlazorUI.Tests.Buttons
 
             var bitButton = com.Find(".bit-btn");
 
-            Assert.AreEqual(bitButton.HasAttribute("tabindex"), expectedResult);
+            Assert.AreEqual(bitButton.HasAttribute("tabindex") ? bitButton.GetAttribute("tabindex").Equals("-1") : expectedResult, expectedResult);
 
             return Task.CompletedTask;
         }
@@ -107,7 +107,7 @@ namespace Bit.Client.Web.BlazorUI.Tests.Buttons
 
             Assert.AreEqual(bitButton.GetAttribute("title"), title);
 
-            Assert.AreEqual(bitButton.GetAttribute("target") , target);
+            Assert.AreEqual(bitButton.GetAttribute("target"), target);
 
             return Task.CompletedTask;
         }
