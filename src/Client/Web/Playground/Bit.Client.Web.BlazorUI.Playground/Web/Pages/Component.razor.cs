d6ade8def08a52c9e18c3d6252868c83cc52c7e8
khashayar-pakkhesal <46280312+khashayar-pakkhesal@users.noreply.github.com>
2021-06-03 10:19:09 +0430

    pivot component (#422)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Component.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Component.razor.cs
@@ -191,6 +191,26 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages
             }
         };
 
+        #region PivotSamples
+
+        public string OverridePivotSelectedKey { get; set; } = "1";
+        public string SelectedPivotItemKey { get; set; } = "1";
+        public BitPivotItem BitPivotItem { get; set; }
+        public ComponentVisibility PivotItemVisibility { get; set; }
+        public BitPivotItem SelectedPivotKey { get; set; } = new BitPivotItem { ItemKey = "Foo" };
+
+        public void PivotSelectedKeyChanged(string key)
+        {
+            OverridePivotSelectedKey = key;
+        }
+
+        public void TogglePivotItemVisobility()
+        {
+            PivotItemVisibility = PivotItemVisibility == ComponentVisibility.Visible ? ComponentVisibility.Collapsed : ComponentVisibility.Visible;
+        }
+
+        #endregion 
+
         private void HideMessageBar(MouseEventArgs args)
 
         {
