5ae90fd3b141056c92c1ff13608fcc08ff9e243f
artav <artav.work@outlook.com>
2021-06-06 18:09:47 +0430

    fix dropdown tests problems #258

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Inputs/BitDropDownTests.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bit.Client.Web.BlazorUI.Tests.Inputs
+{
+    [TestClass]
+    public class BitDropDownTests : BunitTestContext
+    {
+        [DataTestMethod,
+         DataRow(true, 1, true, "enabled"), 
+         DataRow(false, 0, false, "disabled")
+        ]
+        public async Task BitDropDownShouldRespectIsEnabled(bool isEnabled, int count, bool isCalloutOpen, string className)
+        {
+            var items = new List<DropDownItem>
+            {
+                new() {ItemType = DropDownItemType.Header, IsEnabled = isEnabled},
+                new() {ItemType = DropDownItemType.Normal, IsEnabled = isEnabled},
+                new() {ItemType = DropDownItemType.Normal, IsEnabled = isEnabled},
+                new() {ItemType = DropDownItemType.Divider, IsEnabled = isEnabled}
+            };
+            var com = RenderComponent<BitDropDownTest>(
+                parameters =>
+                {
+                    parameters.Add(p => p.Items, items);
+                    parameters.Add(p => p.IsEnabled, isEnabled);
+                    parameters.Add(p => p.Value, count.ToString());
+                });
+            var bitChoiceGroup = com.Find(".bit-drp");
+            bitChoiceGroup.Children.First().Click();
+
+            Assert.AreEqual(count, com.Instance.CurrentCount);
+            Assert.IsTrue(bitChoiceGroup.ClassList.Contains($"bit-drp-{className}-fluent"));
+            Assert.IsTrue(bitChoiceGroup.Children.Count().Equals(isCalloutOpen ? 3 : 2));
+        }
+
+        [DataTestMethod,
+         DataRow(true, 2),
+         DataRow(false, 1)
+        ]
+        public async Task BitDropDownItemsShouldRespectIsEnabled(bool isEnabled, int count)
+        {
+            var items = new List<DropDownItem>
+            {
+                new() {ItemType = DropDownItemType.Header, IsEnabled = isEnabled},
+                new() {ItemType = DropDownItemType.Normal, IsEnabled = isEnabled},
+                new() {ItemType = DropDownItemType.Normal, IsEnabled = isEnabled},
+                new() {ItemType = DropDownItemType.Divider, IsEnabled = isEnabled}
+            };
+            var com = RenderComponent<BitDropDownTest>(
+                parameters =>
+                {
+                    parameters.Add(p => p.Items, items);
+                    parameters.Add(p => p.ItemIsEnabled, isEnabled);
+                    parameters.Add(p => p.Value, count.ToString());
+                });
+            var bitChoiceGroup = com.Find(".bit-drp");
+            bitChoiceGroup.Children.First().Click();
+            bitChoiceGroup.Children[2].Children[1].Click();
+            Assert.AreEqual(count, com.Instance.CurrentCount);
+        }
+
+        [DataTestMethod,
+         DataRow(true, 2),
+         DataRow(false, 1)
+        ]
+        public async Task BitDropDownMultiSelectShouldRespectIsEnabled(bool isEnabled, int count)
+        {
+            Context.JSInterop.Mode = JSRuntimeMode.Loose;
+            var items = new List<DropDownItem>
+            {
+                new() {ItemType = DropDownItemType.Header, IsEnabled = isEnabled},
+                new() {ItemType = DropDownItemType.Normal, IsEnabled = isEnabled},
+                new() {ItemType = DropDownItemType.Normal, IsEnabled = isEnabled},
+                new() {ItemType = DropDownItemType.Divider, IsEnabled = isEnabled}
+            };
+            var com = RenderComponent<BitDropDownTest>(
+                parameters =>
+                {
+                    parameters.Add(p => p.Items, items);
+                    parameters.Add(p => p.IsMultiSelect, true);
+                    parameters.Add(p => p.ItemIsEnabled, isEnabled);
+                    parameters.Add(p => p.Value, count.ToString());
+                });
+            var bitChoiceGroup = com.Find(".bit-drp");
+            bitChoiceGroup.Children.First().Click();
+            var checkboxItems = com.FindAll(".bit-chb-fluent > div > div");
+            for (int index = 0; index < checkboxItems.Count; index++)
+            {
+                checkboxItems[index].Click();
+            }
+            Assert.AreEqual(count, com.Instance.CurrentCount);
+        }
+    }
+}
