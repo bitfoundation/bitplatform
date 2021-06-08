2200914d9a792e44b5f5737997e935eeb07aada2
Somaye Ebrahimi <s.ebrahimi67@hotmail.com>
2021-06-09 01:16:28 +0430

    Add BitButton component demo page (#439)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/NavMenu.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/NavMenu.razor.cs
@@ -13,15 +13,15 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components
                 IsExpanded = true,
                 Links = new List<NavLink>
                 {
-                     new NavLink{Name= "Button", Key= "Button",Url="/component/button" },
-                     new NavLink{Name= "Checkbox", Key= "Checkbox",Url="#" },
-                     new NavLink{Name= "Choice", Key= "Choice",Url="#" },
-                     new NavLink{Name= "Label", Key= "Label",Url="#" },
-                     new NavLink{Name= "Link", Key= "Link",Url="#" },
-                     new NavLink{Name= "Rating", Key= "Rating",Url="#" },
-                     new NavLink{Name= "SearchBox", Key= "SearchBox",Url="#" },
-                     new NavLink{Name= "TextField", Key= "TextField",Url="#" },
-                     new NavLink{Name= "Toggle", Key= "Toggle",Url="#" }
+                     new NavLink { Name= "Button", Key = "Button", Url = "/components/buttons", TargetType = NavLinkTargetType.Self },
+                     new NavLink { Name= "Checkbox", Key = "Checkbox", Url = "#" },
+                     new NavLink { Name= "Choice", Key = "Choice", Url = "#" },
+                     new NavLink { Name= "Label", Key = "Label", Url = "#" },
+                     new NavLink { Name= "Link", Key = "Link", Url = "#" },
+                     new NavLink { Name= "Rating", Key = "Rating", Url = "#" },
+                     new NavLink { Name= "SearchBox", Key = "SearchBox", Url = "#" },
+                     new NavLink { Name= "TextField", Key = "TextField", Url = "#" },
+                     new NavLink { Name= "Toggle", Key = "Toggle", Url = "#" }
                 }
             },
             new NavLink
@@ -30,8 +30,8 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components
                 Key = "Navigations",
                 Links = new List<NavLink>
                 {
-                    new NavLink{Name= "Nav", Key= "Nav",Url="#" },
-                    new NavLink{Name= "Pivot", Key= "Pivot",Url="#" },
+                    new NavLink { Name = "Nav", Key = "Nav", Url = "#" },
+                    new NavLink { Name = "Pivot", Key = "Pivot", Url = "#" },
                 }
             },
             new NavLink
@@ -40,7 +40,7 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components
                 Key = "Notifications",
                 Links = new List<NavLink>
                 {
-                    new NavLink{Name= "MessageBar", Key= "MessageBar",Url="#" }
+                    new NavLink { Name = "MessageBar", Key = "MessageBar",Url="#" }
                 }
             },
             new NavLink
@@ -49,7 +49,7 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components
                 Key = "Progress",
                 Links = new List<NavLink>
                 {
-                    new NavLink{Name= "Spinner", Key= "Spinner",Url="#" }
+                    new NavLink{ Name = "Spinner", Key = "Spinner", Url = "#" }
                 }
             },
         };
