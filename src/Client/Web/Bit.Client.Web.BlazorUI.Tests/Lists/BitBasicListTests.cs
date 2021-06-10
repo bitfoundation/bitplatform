3a9c89d053db181227bc0256af7087cc69163499
Z_Shobbar <zoha.shobbar@outlook.com>
2021-06-10 11:09:41 +0430

    fix some issues BitBasicList(#269)

--- src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Lists/BitBasicListTests.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Lists/BitBasicListTests.cs
@@ -11,20 +11,67 @@ namespace Bit.Client.Web.BlazorUI.Tests.Lists
     public class BitBasicListTests : BunitTestContext
     {
 
-        [DataTestMethod, DataRow(Visual.Fluent, true, 1000), DataRow(Visual.Fluent, true, 1000)]
+        [DataTestMethod,
+            DataRow(Visual.Fluent, true, 1000),
+            DataRow(Visual.Fluent, false, 100),
+            DataRow(Visual.Cupertino, true, 1000),
+            DataRow(Visual.Cupertino, false, 100),
+            DataRow(Visual.Material, true, 1000),
+            DataRow(Visual.Material, false, 100),
+            ]
         //When we use virtualization, two additional parts are created,
         //that's why this method use itemCount +2
-        public void BitBasicListShoudRenderExpectedChildElements(Visual visual, int itemCount)
+        public void BitBasicListShoudRenderExpectedChildElements(Visual visual, bool virtualize, int itemCount)
         {
             var component = RenderComponent<BitBasicListTest>(parameters =>
             {
                 parameters.Add(p => p.Visual, visual);
+                parameters.Add(p => p.Virtualize, virtualize);
                 parameters.Add(p => p.Items, GetTestData(itemCount));
             });
+
             var bitList = component.Find(".bit-bsc-lst");
             int comItemCount = bitList.ChildElementCount;
 
-            Assert.AreEqual(comItemCount + 2, itemCount + 2);
+            if (virtualize)
+            {
+                Assert.AreEqual(comItemCount, itemCount + 2);
+            }
+            else
+            {
+                Assert.AreEqual(comItemCount, itemCount);
+            }
+
+            var visualClass = visual == Visual.Cupertino ? "cupertino" : visual == Visual.Material ? "material" : "fluent";
+            Assert.IsTrue(bitList.ClassList.Contains($"bit-bsc-lst-{visualClass}"));
+        }
+
+        [DataTestMethod,
+            DataRow(Visual.Fluent, 1000, true, 1),
+            DataRow(Visual.Fluent, 100, false, 1),
+            ]
+        public void BitBasicListRenderExtraItemsUsingOverScanCount(Visual visual,
+             int itemCount, bool virtualize, int overScanCount)
+        {
+            var component = RenderComponent<BitBasicListTest>(parameters =>
+            {
+                parameters.Add(p => p.Visual, visual);
+                parameters.Add(p => p.Virtualize, virtualize);
+                parameters.Add(p => p.Items, GetTestData(itemCount));
+                parameters.Add(p => p.OverscanCount, overScanCount);
+            });
+
+            var bitList = component.Find(".bit-bsc-lst");
+            int comItemCount = bitList.ChildElementCount;
+
+            if (virtualize)
+            {
+                Assert.AreEqual(comItemCount, itemCount + (overScanCount + 2));
+            }
+            else
+            {
+                Assert.AreEqual(comItemCount, itemCount);
+            }
         }
 
         [DataTestMethod, DataRow(Visual.Fluent, 1000, "RoleItem", true)]
--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/Lists/BitBasicList.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Lists/BitBasicList.razor.cs
@@ -7,13 +7,35 @@ namespace Bit.Client.Web.BlazorUI
 {
     public partial class BitBasicList<TItem>
     {
-        [Parameter] public ICollection<TItem> Items { get; set; } = Array.Empty<TItem>();
-        [Parameter] public string Role { get; set; } = "list";
-        [Parameter] public bool Virtualize { get; set; } = true;
+        /// <summary>
+        /// list of items that want to render
+        /// </summary>
+        [Parameter] public ICollection<TItem> Items { get; set; } = Array.Empty<TItem>();     
+        /// <summary>
+        /// virtualize rendering the list
+        /// UI rendering to just the parts that are currently visible
+        /// defualt is false
+        /// </summary>
+        [Parameter] public bool Virtualize { get; set; } = false;
+        /// <summary>
+        /// a placeholder to display content until the item data is available.
+        /// </summary>
         [Parameter] public string? LoadingText { get; set; }
+        /// <summary>
+        /// determines how many additional items are rendered before and after the visible region
+        /// defualt is 3
+        /// </summary>
         [Parameter] public int OverscanCount { get; set; } = 3;
+        /// <summary>
+        /// The height of each item in pixels, defualt is 50
+        /// </summary>
         [Parameter] public int ItemSize { get; set; } = 50;
+        /// <summary>
+        /// content of each item
+        /// it should determin with context attribute
+        /// </summary>
         [Parameter] public RenderFragment<TItem>? RowTemplate { get; set; }
+        [Parameter] public string Role { get; set; } = "list";
 
         protected override string RootElementClass => "bit-bsc-lst";
         public override Task SetParametersAsync(ParameterView parameters)
