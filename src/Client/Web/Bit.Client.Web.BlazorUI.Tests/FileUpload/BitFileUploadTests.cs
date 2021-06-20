d55d13a9f05fc02a8a7c587a0e3687f737324749
hamed hajiloo <hamedhajiloo@hotmail.com>
2021-06-20 14:11:39 +0430

    Create Test (#394)

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/FileUpload/BitFileUploadTests.cs
@@ -0,0 +1,44 @@
+﻿using Bunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bit.Client.Web.BlazorUI.Tests.FileUpload
+{
+    [TestClass]
+    public class BitFileUploadTests : BunitTestContext
+    {
+        [DataTestMethod,
+           DataRow(Visual.Fluent),
+           DataRow(Visual.Cupertino),
+           DataRow(Visual.Material),
+        ]
+        public void BitUploadFile_BaseTest(Visual visual)
+        {
+            var com = RenderComponent<BitFileUploadTest>(parameters =>
+            {
+                parameters.Add(p => p.Visual, visual);
+            });
+
+            var bitFileUpload = com.Find(".bit-fu");
+            var visualClass = visual == Visual.Cupertino ? "cupertino" : visual == Visual.Material ? "material" : "fluent";
+
+            Assert.IsTrue(bitFileUpload.ClassList.Contains($"bit-fu-{visualClass}") && bitFileUpload.ClassList.Contains($"bit-fu"));
+        }
+
+        [DataTestMethod,
+           DataRow(true),
+           DataRow(false)
+        ]
+        public void BitUploadFile_MultipleTest(bool isMultiFile)
+        {
+            var com = RenderComponent<BitFileUploadTest>(parameters =>
+            {
+                parameters.Add(p => p.IsMultiFile, isMultiFile);
+            });
+
+            var bitFileUpload = com.Find("input[type=file]");
+            var attribute = bitFileUpload.GetAttribute("multiple");
+            var isMultiFileStr = isMultiFile == true ? "" : null;
+            Assert.AreEqual(isMultiFileStr, attribute);
+        }
+    }
+}
