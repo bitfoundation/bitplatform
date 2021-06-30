78cfe222a996443963dc6b2fe70ca8c2fc6bd7ce
Hamed Moghadasi <h4lmed@gmail.com>
2021-06-30 18:42:14 +0430

    complete indeterminate progress indicator

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/ProgressIndicator/BitProgressIndicator.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/ProgressIndicator/BitProgressIndicator.razor.cs
@@ -9,8 +9,28 @@ namespace Bit.Client.Web.BlazorUI
 {
     public partial class BitProgressIndicator
     {
+        private double percentComplete;
         [Parameter] public string Label { get; set; } = string.Empty;
         [Parameter] public string Description { get; set; } = string.Empty;
+        [Parameter]
+        public double PercentComplete
+        {
+            get => percentComplete;
+            set
+            {
+                if (value == percentComplete) return;
+                percentComplete = value;
+                _ = PercentCompleteChanged.InvokeAsync(value);
+            }
+        }
+
+        [Parameter] public EventCallback<double> PercentCompleteChanged { get; set; }
         protected override string RootElementClass => "bit-pi";
+        protected override void RegisterComponentClasses()
+        {
+            ClassBuilder.Register(() => PercentCompleteChanged.HasDelegate
+                                                ? string.Empty
+                                                : $"{RootElementClass}-indeterminate-{VisualClassRegistrar()}");
+        }
     }
 }
