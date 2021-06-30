39febde6ce72113aec132f869869da6856b4467f
Hamed Moghadasi <h4lmed@gmail.com>
2021-06-30 17:54:44 +0430

    init progress indicator component

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/ProgressIndicator/BitProgressIndicator.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public partial class BitProgressIndicator
+    {
+        [Parameter] public string Label { get; set; } = string.Empty;
+        [Parameter] public string Description { get; set; } = string.Empty;
+        protected override string RootElementClass => "bit-pi";
+    }
+}
