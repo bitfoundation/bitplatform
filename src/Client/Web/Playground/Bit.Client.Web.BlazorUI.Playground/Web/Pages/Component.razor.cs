d45ffee5ddb02b39caeba2af1a51bbe1a40fd7d5
Hamed Moghadasi <h4lmed@gmail.com>
2021-07-01 18:07:48 +0430

    remove useless codes ! (#267)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Component.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Component.razor.cs
@@ -6,28 +6,6 @@ using Microsoft.AspNetCore.Components.Web;
 
 namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages
 {
-
-    public static class Interval
-    {
-        public static System.Timers.Timer Set(Action action, int interval)
-        {
-            var timer = new System.Timers.Timer(interval);
-            timer.Elapsed += (s, e) => {
-                timer.Enabled = false;
-                action();
-                timer.Enabled = true;
-            };
-            timer.Enabled = true;
-            return timer;
-        }
-
-        public static void Stop(System.Timers.Timer timer)
-        {
-            timer.Stop();
-            timer.Dispose();
-        }
-    }
-
     public partial class Component
     {
         private double precentComplete;
