346e290a49c4c222fff9c29f53eecb3fe54996e5
Maryam Heydarian <maryamhdr76@gmail.com>
2021-07-15 01:49:35 +0430

    render anchor tag if href has value & IsEnabled is true #593

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/Buttons/BitCompoundButton.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Buttons/BitCompoundButton.razor.cs
@@ -41,6 +41,21 @@ namespace Bit.Client.Web.BlazorUI
         [Parameter] public string? SecondaryText { get; set; }
 
         /// <summary>
+        /// URL the link points to, if provided, button renders as an anchor
+        /// </summary>
+        [Parameter] public string? Href { get; set; }
+
+        /// <summary>
+        /// If Href provided, specifies how to open the link
+        /// </summary>
+        [Parameter] public string? Target { get; set; }
+
+        /// <summary>
+        /// The title to show when the mouse is placed on the button
+        /// </summary>
+        [Parameter] public string? Title { get; set; }
+
+        /// <summary>
         /// The style of compound button, Possible values: Primary | Standard
         /// </summary>
         [Parameter]
