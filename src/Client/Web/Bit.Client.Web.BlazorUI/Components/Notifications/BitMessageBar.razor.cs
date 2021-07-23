79f4182670e14fe13b2d6e7133912960843a0041
kh.p1999 <kh.p1999@yahoo.com>
2021-07-24 03:31:53 +0430

    Resolve Pr Comments

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/Notifications/BitMessageBar.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Notifications/BitMessageBar.razor.cs
@@ -7,38 +7,40 @@ namespace Bit.Client.Web.BlazorUI
 {
     public partial class BitMessageBar
     {
+        private BitMessageBarType messageBarType = BitMessageBarType.Info;
+
+
+        [Parameter] public bool IsMultiline { get; set; } = true;
+
+        [Parameter]
+        public BitMessageBarType MessageBarType
+        {
+            get => messageBarType;
+            set
+            {
+                ClassBuilder.Reset();
+                messageBarType = value;
+            }
+        }
+        [Parameter] public string DismissIconName { get; set; } = "Clear";
+
+        [Parameter] public bool Truncated { get; set; }
+
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+
+        [Parameter] public RenderFragment? Actions { get; set; }
+
+        [Parameter] public string? DismissButtonAriaLabel { get; set; }
+
+        [Parameter] public string? OverflowButtonAriaLabel { get; set; }
+
+        [Parameter] public EventCallback OnDismiss { get; set; }
+
+        private bool HasDismiss { get => (OnDismiss.HasDelegate); }
+
+        private bool ExpandSingleLine { get; set; }
+
         protected override string RootElementClass => "bit-msg-bar";
-
-        [Parameter]
-        public bool IsMultiline { get; set; } = true;
-
-        [Parameter]
-        public BitMessageBarType MessageBarType { get; set; } = BitMessageBarType.Info;
-
-        [Parameter]
-        public string DismissIconName { get; set; } = "Clear";
-
-        [Parameter]
-        public bool Truncated { get; set; }
-
-        [Parameter]
-        public RenderFragment? ChildContent { get; set; }
-
-        [Parameter]
-        public RenderFragment? Actions { get; set; }
-
-        [Parameter]
-        public string? DismissButtonAriaLabel { get; set; }
-
-        [Parameter]
-        public string? OverflowButtonAriaLabel { get; set; }
-
-        [Parameter]
-        public EventCallback OnDismiss { get; set; }
-
-        protected bool HasDismiss { get => (OnDismiss.HasDelegate); }
-
-        protected bool ExpandSingelLine { get; set; }
 
         protected override void RegisterComponentClasses()
         {
@@ -52,12 +54,12 @@ namespace Bit.Client.Web.BlazorUI
                                         : $"{RootElementClass}-success-{VisualClassRegistrar()}");
         }
 
-        protected void Truncate()
+        private void ToggleExpandSingleLine()
         {
-            ExpandSingelLine = !ExpandSingelLine;
+            ExpandSingleLine = !ExpandSingleLine;
         }
 
-        public static Dictionary<BitMessageBarType, string> IconMap = new()
+        private static Dictionary<BitMessageBarType, string> IconMap = new()
         {
             [BitMessageBarType.Info] = "Info",
             [BitMessageBarType.Warning] = "Info",
