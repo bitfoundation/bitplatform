168aaaf61fafc7ac7a1ab3ad2841a9edf6fbe82f
khashayar-pakkhesal <46280312+khashayar-pakkhesal@users.noreply.github.com>
2021-07-30 04:22:01 +0430

    Create ColoPicker (#621)

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/ColorPicker/Hex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public class Hex
+    {
+        public string ColorCode { get; set; }
+
+        public RGB ToRGB()
+        {
+            return new RGB()
+            {
+                Red = int.Parse(ColorCode.Substring(0, 2), NumberStyles.AllowHexSpecifier),
+                Green = int.Parse(ColorCode.Substring(2, 2), NumberStyles.AllowHexSpecifier),
+                Blue = int.Parse(ColorCode.Substring(4, 2), NumberStyles.AllowHexSpecifier)
+            };
+        }
+    }
+}
