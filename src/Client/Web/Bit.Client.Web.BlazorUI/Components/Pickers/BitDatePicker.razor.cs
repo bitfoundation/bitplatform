e81e950cc73f5fbad62038e56ad9f5fd7168dd40
artav <artav.work@outlook.com>
2021-08-01 22:34:03 +0430

    add date picker component #263

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Pickers/BitDatePicker.razor.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public partial class BitDatePicker
+    {
+        private bool isOpen;
+        private Calendar calendar;
+        private int[,] monthWeeks = new int[6, 7];
+        private int currentYear;
+        private int currentMonth;
+        private string monthTitle = "July 2021";
+
+        [Parameter]
+        public bool IsOpen
+        {
+            get => isOpen;
+            set
+            {
+                isOpen = value;
+                ClassBuilder.Reset();
+            }
+        }
+
+        [Parameter] public string GoToToday { get; set; } = "Go to today";
+        [Parameter] public CalendarType CalendarType { get; set; } = CalendarType.Gregorian;
+
+        [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+        [Parameter] public EventCallback<FocusEventArgs> OnFocusIn { get; set; }
+        [Parameter] public EventCallback<FocusEventArgs> OnFocusOut { get; set; }
+        [Parameter] public EventCallback<int> OnMonthChange { get; set; }
+        [Parameter] public EventCallback<int> OnYearChange { get; set; }
+
+        protected override string RootElementClass { get; } = "bit-dtp";
+
+        protected override void RegisterComponentClasses()
+        {
+            ClassBuilder.Register(() => IsEnabled is false
+                ? $"{RootElementClass}-disabled-{VisualClassRegistrar()}" : string.Empty);
+
+            ClassBuilder.Register(() => IsOpen is false
+                ? $"{RootElementClass}-open-{VisualClassRegistrar()}" : string.Empty);
+        }
+
+        protected async override Task OnInitializedAsync()
+        {
+            if (CalendarType == CalendarType.Gregorian)
+            {
+                calendar = new GregorianCalendar();
+            }
+            else if (CalendarType == CalendarType.Persian)
+            {
+                calendar = new PersianCalendar();
+            }
+            CreateMonthCalendar();
+        }
+
+        public async Task HandleClick(MouseEventArgs eventArgs)
+        {
+            IsOpen = true;
+            await OnClick.InvokeAsync(eventArgs);
+        }
+        public async Task HandleFocusIn(FocusEventArgs eventArgs)
+        {
+            await OnFocusIn.InvokeAsync(eventArgs);
+        }
+        public async Task HandleFocusOut(FocusEventArgs eventArgs)
+        {
+            //   IsOpen = false;
+            await OnFocusOut.InvokeAsync(eventArgs);
+        }
+
+        public async Task HandleMonthChanged(bool nextMonth)
+        {
+            if (nextMonth)
+            {
+                if (currentMonth + 1 == 0)
+                {
+                    currentYear++;
+                    currentMonth = 1;
+                }
+                else
+                {
+                    currentMonth++;
+                }
+            }
+            else
+            {
+                if (currentMonth - 1 == 0)
+                {
+                    currentYear--;
+                    currentMonth = 12;
+                }
+                else
+                {
+                    currentMonth--;
+                }
+            }
+            CreateMonthCalendar(currentYear, currentMonth);
+            await OnMonthChange.InvokeAsync(currentMonth);
+        }
+
+        public async Task HandleYearChanged(bool nextYear)
+        {
+            if (nextYear)
+            {
+                currentYear++;
+            }
+            else
+            {
+                currentYear--;
+            }
+            CreateMonthCalendar(currentYear, currentMonth);
+            await OnYearChange.InvokeAsync(currentYear);
+        }
+
+        private void CreateMonthCalendar()
+        {
+            currentMonth = calendar.GetMonth(DateTime.Now);
+            currentYear = calendar.GetYear(DateTime.Now);
+            CreateMonthCalendar(currentYear, currentMonth);
+        }
+
+        private void CreateMonthCalendar(int year, int month)
+        {
+            var daysCount = calendar.GetDaysInMonth(year, month);
+            var firstDay = calendar.ToDateTime(year, month, 1, 0, 0, 0, 0);
+            var currentDay = 1;
+            var dayOfWeekDifference = CalendarType == CalendarType.Persian ? -1 : 0;
+            var isCalendarEnded = false;
+            for (int weekIndex = 0; weekIndex < monthWeeks.GetLength(0); weekIndex++)
+            {
+                for (int dayIndex = 0; dayIndex < monthWeeks.GetLength(1); dayIndex++)
+                {
+                    if (weekIndex == 0
+                        && currentDay == 1
+                        && (int)firstDay.DayOfWeek > dayIndex + dayOfWeekDifference)
+                    {
+                        var previousMonthDaysCount = 0;
+                        if (month - 1 == 0)
+                        {
+                            previousMonthDaysCount = calendar.GetDaysInMonth(year - 1, 12);
+                        }
+                        else
+                        {
+                            previousMonthDaysCount = calendar.GetDaysInMonth(year, month - 1);
+                        }
+                        monthWeeks[weekIndex, dayIndex] = previousMonthDaysCount - ((int)firstDay.DayOfWeek - (dayIndex + dayOfWeekDifference)) + 1;
+                    }
+                    else
+                    {
+                        monthWeeks[weekIndex, dayIndex] = currentDay;
+                        currentDay++;
+                    }
+                    if (currentDay > daysCount && dayIndex != 6)
+                    {
+                        currentDay = 1;
+                        isCalendarEnded = true;
+                    }
+                    if (dayIndex == 6)
+                    {
+                        break;
+                    }
+                }
+                if (isCalendarEnded)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Pickers/CalendarExtensions.cs
@@ -0,0 +1,136 @@
+﻿using System.Globalization;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public static class CalendarExtensions
+    {
+        public static string GetMonthName(this Calendar calendar, int month)
+        {
+            if (calendar is GregorianCalendar)
+            {
+                switch (month)
+                {
+                    case 1:
+                        return "January";
+                    case 2:
+                        return "February";
+                    case 3:
+                        return "March";
+                    case 4:
+                        return "April";
+                    case 5:
+                        return "May";
+                    case 6:
+                        return "June";
+                    case 7:
+                        return "July";
+                    case 8:
+                        return "August";
+                    case 9:
+                        return "September";
+                    case 10:
+                        return "October";
+                    case 11:
+                        return "November";
+                    case 12:
+                        return "December";
+                }
+            }
+            if (calendar is PersianCalendar)
+            {
+                switch (month)
+                {
+                    case 1:
+                        return "Farvardin";
+                    case 2:
+                        return "Ordibehesht";
+                    case 3:
+                        return "Khordad";
+                    case 4:
+                        return "Tir";
+                    case 5:
+                        return "Mordad";
+                    case 6:
+                        return "Shahrivar";
+                    case 7:
+                        return "Mehr";
+                    case 8:
+                        return "Aban";
+                    case 9:
+                        return "Azar";
+                    case 10:
+                        return "Dey";
+                    case 11:
+                        return "Bahman";
+                    case 12:
+                        return "Esfand";
+                }
+            }
+            return "";
+        }
+        public static string GetMonthShortName(this Calendar calendar, int month)
+        {
+            if (calendar is GregorianCalendar)
+            {
+                switch (month)
+                {
+                    case 1:
+                        return "Jan";
+                    case 2:
+                        return "Feb";
+                    case 3:
+                        return "Mar";
+                    case 4:
+                        return "Apr";
+                    case 5:
+                        return "May";
+                    case 6:
+                        return "Jun";
+                    case 7:
+                        return "Jul";
+                    case 8:
+                        return "Aug";
+                    case 9:
+                        return "Sep";
+                    case 10:
+                        return "Oct";
+                    case 11:
+                        return "Nov";
+                    case 12:
+                        return "Dec";
+                }
+            }
+            if (calendar is PersianCalendar)
+            {
+                switch (month)
+                {
+                    case 1:
+                        return "Far";
+                    case 2:
+                        return "Ord";
+                    case 3:
+                        return "Khr";
+                    case 4:
+                        return "Tir";
+                    case 5:
+                        return "Mrd";
+                    case 6:
+                        return "Shr";
+                    case 7:
+                        return "Mhr";
+                    case 8:
+                        return "Abn";
+                    case 9:
+                        return "Azr";
+                    case 10:
+                        return "Dey";
+                    case 11:
+                        return "Bah";
+                    case 12:
+                        return "Esf";
+                }
+            }
+            return "";
+        }
+    }
+}
