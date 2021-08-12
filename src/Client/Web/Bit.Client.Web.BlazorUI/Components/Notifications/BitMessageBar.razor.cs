5684bd5c1ea51cfa93da3bf085fbba58684400c3
Somaye Ebrahimi <46419023+SomayeEbrahimi@users.noreply.github.com>
2021-08-12 11:11:49 +0430

    Add BitMessageBar component Documentation Comments (#577)

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/Notifications/BitMessageBar.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Notifications/BitMessageBar.razor.cs
@@ -9,9 +9,14 @@ namespace Bit.Client.Web.BlazorUI
     {
         private BitMessageBarType messageBarType = BitMessageBarType.Info;
 
-
+        /// <summary>
+        /// Determines if the message bar is multi lined. If false, and the text overflows over buttons or to another line, it is clipped
+        /// </summary>
         [Parameter] public bool IsMultiline { get; set; } = true;
 
+        /// <summary>
+        /// The type of message bar to render
+        /// </summary>
         [Parameter]
         public BitMessageBarType MessageBarType
         {
@@ -22,18 +27,40 @@ namespace Bit.Client.Web.BlazorUI
                 messageBarType = value;
             }
         }
+
+        /// <summary>
+        /// Custom Fabric icon name to replace the dismiss icon. If unset, default will be the Fabric Clear icon
+        /// </summary>
         [Parameter] public string DismissIconName { get; set; } = "Clear";
 
+        /// <summary>
+        /// Determines if the message bar text is truncated. If true, a button will render to toggle between a single line view and multiline view. This parameter is for single line message bars with no buttons only in a limited space scenario
+        /// </summary>
         [Parameter] public bool Truncated { get; set; }
 
+        /// <summary>
+        /// The content of message bar
+        /// </summary>
         [Parameter] public RenderFragment? ChildContent { get; set; }
 
+        /// <summary>
+        /// The content of the action to show on the message bar
+        /// </summary>
         [Parameter] public RenderFragment? Actions { get; set; }
 
+        /// <summary>
+        /// Aria label on dismiss button if onDismiss is defined
+        /// </summary>
         [Parameter] public string? DismissButtonAriaLabel { get; set; }
 
+        /// <summary>
+        /// Aria label on overflow button if truncated is true
+        /// </summary>
         [Parameter] public string? OverflowButtonAriaLabel { get; set; }
 
+        /// <summary>
+        /// Whether the message bar has a dismiss button and its callback. If null, dismiss button won't show
+        /// </summary>
         [Parameter] public EventCallback OnDismiss { get; set; }
 
         private bool HasDismiss { get => (OnDismiss.HasDelegate); }
