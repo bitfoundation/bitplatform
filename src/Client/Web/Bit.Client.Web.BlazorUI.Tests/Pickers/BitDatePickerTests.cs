2db30490d8b7754ee7cf481c45169ad0448d0e2c
artav <artav.work@outlook.com>
2021-08-21 14:01:10 +0430

    fix commit merge problems #263

--- src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Pickers/BitDatePickerTests.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Pickers/BitDatePickerTests.cs
@@ -1,4 +1,6 @@
-﻿using Bunit;
+﻿using System;
+using AngleSharp.Dom;
+using Bunit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bit.Client.Web.BlazorUI.Tests.Pickers
@@ -48,10 +50,10 @@ namespace Bit.Client.Web.BlazorUI.Tests.Pickers
         }
 
         [DataTestMethod,
-          DataRow(true),
-          DataRow(false),
+          DataRow(true, 1),
+          DataRow(false, 0)
       ]
-        public void BitDatePickerShouldRespondToClickEvent(bool isEnabled)
+        public void BitDatePickerShouldHandleOnClickEvent(bool isEnabled, int count)
         {
             var component = RenderComponent<BitDatePickerTest>(
                 parameters =>
@@ -59,144 +61,31 @@ namespace Bit.Client.Web.BlazorUI.Tests.Pickers
                     parameters.Add(p => p.IsEnabled, isEnabled);
                 });
 
-            var bitDatePicker = component.Find(".bit-dtp");
-            bitDatePicker.Click();
+            var bitDatePickerInput = component.Find(".bit-txt-fluent>div input");
+            bitDatePickerInput.Click();
+
+            Assert.AreEqual(component.Instance.ClickedValue, count);
         }
-        /*
-                [DataTestMethod,
-                   DataRow(true, "value1"),
-                   DataRow(false, "value2")
-                ]
-                public void BitChoiceOptionMustRespondToTheClickAndChangeValueEvent(bool isEnabled, string value)
-                {
-                    var component = RenderComponent<BitChoiceGroupTest>(
-                        parameters =>
-                        {
-                            parameters.Add(p => p.Value, value);
-                            parameters.Add(p => p.ChoiceGroupIsEnabled, isEnabled);
-                        });
 
-                    var input = component.Find(".bit-chg .bit-cho input");
-
-                    input.Click();
-
-                    //TODO: bypassed - BUnit onchange event issue
-                    //Assert.AreEqual(isEnabled ? 1 : 0, component.Instance.ChoiceOptionClickedValue);
-                    //Assert.AreEqual(isEnabled ? value : "", component.Instance.ChoiceGroupChangedValue);
-                }
-
-                [DataTestMethod, DataRow("test value")]
-                public void BitChoiceOptionShouldTakeCorrectValue(string value)
-                {
-                    var component = RenderComponent<BitChoiceGroupTest>(
-                        parameters =>
-                        {
-                            parameters.Add(p => p.Value, value);
-                        });
-
-                    var input = component.Find(".bit-cho input");
-
-                    Assert.IsTrue(input.HasAttribute("value"));
-                    Assert.AreEqual(value, input.GetAttribute("value"));
-                }
-
-                [DataTestMethod, DataRow("hello world")]
-                public void BitChoiceOptionShouldUseTextInLabel(string text)
-                {
-                    var component = RenderComponent<BitChoiceGroupTest>(
-                        parameters =>
-                        {
-                            parameters.Add(p => p.Text, text);
-                        });
-
-                    var bitChoiceOptionLabelText = component.Find(".bit-cho label span");
-
-                    Assert.AreEqual(text, bitChoiceOptionLabelText.TextContent);
-                }
-
-                [DataTestMethod,
-                  DataRow(true),
-                  DataRow(false)
-                ]
-                public void BitChoiceOptionShouldAcceptGivenCheckStatus(bool isChecked)
-                {
-                    var component = RenderComponent<BitChoiceGroupTest>(
-                        parameters =>
-                        {
-                            parameters.Add(p => p.IsChecked, isChecked);
-                        });
-
-                    var input = component.Find(".bit-cho input");
-
-                    Assert.AreEqual(isChecked, input.HasAttribute("checked"));
-
-                }
-
-                [DataTestMethod,
-                   DataRow(true, true),
-                   DataRow(true, false),
-                   DataRow(false, true),
-                   DataRow(false, false),
-                ]
-                public void BitChoiceOptionMustRespondToTheClickEvent(bool groupIsEnabled, bool optionIsEnabled)
-                {
-                    var component = RenderComponent<BitChoiceGroupTest>(
-                        parameters =>
-                        {
-                            parameters.Add(p => p.ChoiceGroupIsEnabled, groupIsEnabled);
-                            parameters.Add(p => p.ChoiceOptionIsEnabled, optionIsEnabled);
-                        });
-
-                    var input1 = component.Find(".bit-chg .bit-cho:nth-child(1) input");
-                    input1.Click();
-
-                    if (groupIsEnabled && optionIsEnabled)
-                    {
-                        //TODO: bypassed - BUnit onchange event issue
-                        //Assert.IsTrue(input1.HasAttribute("checked"));
-                    }
-
-                    var input2 = component.Find($".bit-chg .bit-cho:nth-child(2) input");
-                    input2.Click();
-
-                    if (groupIsEnabled && optionIsEnabled)
-                    {
-                        //TODO: bypassed - BUnit onchange event issue
-                        //Assert.IsTrue(input2.HasAttribute("checked"));
-                    }
-
-                    //TODO: bypassed - BUnit onchange event issue
-                    //Assert.AreEqual(groupIsEnabled && optionIsEnabled ? 1 : 0, component.Instance.ChoiceOptionClickedValue);
-                }
-
-                [DataTestMethod,
-                  DataRow(true, true, 2),
-                   DataRow(true, false, 2),
-                   DataRow(false, true, 2),
-                   DataRow(false, false, 2),
-               ]
-                public void BitChoiceOptionMustRespondToTheChangeEvent(bool groupIsEnabled, bool optionIsEnabled, int count)
-                {
-                    var component = RenderComponent<BitChoiceGroupTest>(
-                        parameters =>
-                        {
-                            parameters.Add(p => p.ChoiceGroupIsEnabled, groupIsEnabled);
-                            parameters.Add(p => p.ChoiceOptionIsEnabled, optionIsEnabled);
-                        });
-
-                    var input = component.Find(".bit-cho input");
-
-                    input.Click();
-
-                    if (groupIsEnabled && optionIsEnabled)
-                    {
-                        //TODO: bypassed - BUnit onchange event issue
-                        //Assert.IsTrue(input.HasAttribute("checked"));
-                    }
-
-                    //TODO: bypassed - BUnit onchange event issue
-                    //Assert.AreEqual(groupIsEnabled && optionIsEnabled ? count.ToString() : null, component.Instance.Value);
-                    //Assert.AreEqual(groupIsEnabled && optionIsEnabled ? 1 : 0, component.Instance.ChoiceOptionChangedValue);
-                }*/
+        [DataTestMethod,
+          DataRow(true, 1),
+          DataRow(false, 0)
+      ]
+        public void BitDatePickerCalendarItemsShouldRespectIsEnabled(bool isEnabled,int count)
+        {
+            var component = RenderComponent<BitDatePickerTest>(
+               parameters =>
+               {
+                   parameters.Add(p => p.IsOpen, true);
+                   parameters.Add(p => p.IsEnabled, isEnabled);
+               });
+          
+            var dateItems = component.FindAll(".bit-dtp>div:nth-child(2)>div>div>div:nth-child(2)>div:nth-child(2)>div:nth-child(2)>table>tbody tr:not([role]):nth-child(n+1) td");
+            
+            Random random = new();
+            int randomNumber = random.Next(0, dateItems.Count -1);
+            dateItems[randomNumber].Click();
+            Assert.AreEqual(component.Instance.SelectedDateValue, count);
+        }
     }
 }
