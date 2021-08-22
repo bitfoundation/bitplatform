720010439ee5f54d99dc4b34a391decfee5a4fc9
kh.p1999 <kh.p1999@yahoo.com>
2021-08-22 19:22:29 +0430

    Fix BitNav data binding

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/Navs/BitNav.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Navs/BitNav.razor.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
 
 namespace Bit.Client.Web.BlazorUI
 {
-    public partial class BitNav
+    public partial class BitNav : IDisposable
     {
         /// <summary>
         /// The key of the nav item selected by caller
         /// </summary>
-        [Parameter] public string? SelectedKey { get; set; }
+        private string? selectedKey;
+
+        [Inject] private NavigationManager navigationManager { get; set; }
+
+        public bool SelectedKeyHasBeenSet { get; set; }
+
+        [Parameter]
+        public string SelectedKey
+        {
+            get => selectedKey;
+            set
+            {
+                if (value == selectedKey) return;
+                selectedKey = value;
+                SelectedKeyChanged.InvokeAsync(selectedKey);
+
+                var currentUrl = NavLinkItems.Where(nli => nli.Links != null).SelectMany(sli => sli.Links).FirstOrDefault(sli => sli.Key.Contains(selectedKey))?.Key ?? string.Empty;
+
+                if (!string.IsNullOrEmpty(currentUrl) && navigationManager.Uri.Contains(currentUrl))
+                {
+                    navigationManager.NavigateTo(currentUrl);
+                }
+            }
+        }
+
+        [Parameter] public EventCallback<string> SelectedKeyChanged { get; set; }
 
         /// <summary>
         /// Indicates whether the navigation component renders on top of other content in the UI
@@ -33,6 +60,29 @@ namespace Bit.Client.Web.BlazorUI
         [Parameter] public RenderFragment<BitNavLinkItem>? HeaderTemplate { get; set; }
 
         protected override string RootElementClass => "bit-nav";
+
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            navigationManager.LocationChanged += OnLocationChanged;
+
+
+            await base.OnInitializedAsync();
+        }
+
+        private void OnLocationChanged(object sender, LocationChangedEventArgs args)
+        {
+            var currentPage = navigationManager.Uri.Replace(navigationManager.BaseUri, string.Empty);
+
+            string currentPageKey = NavLinkItems.Where(nli => nli.Links != null).SelectMany(nli => nli.Links).FirstOrDefault(l => l.Url.ToLower().Contains(currentPage.ToLower()))?.Key ?? string.Empty;
+
+
+            if (string.IsNullOrEmpty(currentPageKey)) return;
+
+            SelectedKey = currentPageKey;
+            StateHasChanged();
+        }
 
         private async Task Toggle(BitNavLinkItem navLink)
         {
@@ -59,13 +109,18 @@ namespace Bit.Client.Web.BlazorUI
             var hasUrlClass = navLink.Url.HasNoValue() ? "nourl" : "hasurl";
 
             var mainStyle = $"bit-nav-link-{enabledClass}-{hasUrlClass}-{VisualClassRegistrar()}";
-            var selected = navLink.Key == SelectedKey ? $"bit-nav-selected-{VisualClassRegistrar()}" : "";
+            var selectedClass = navLink.Key == SelectedKey ? $"bit-nav-selected-{VisualClassRegistrar()}" : "";
             var hasIcon = navLink.Icon.HasNoValue()
                             ? $"bit-nav-has-not-icon-{VisualClassRegistrar()}"
                             : $"bit-nav-has-icon-{VisualClassRegistrar()}";
             var hasChildren = navLink.Links?.Any() ?? false ? $"bit-nav-haschildren-{VisualClassRegistrar()}" : "";
 
-            return $"{mainStyle} {selected} {hasIcon} {hasChildren}";
+            return $"{mainStyle} {selectedClass} {hasIcon} {hasChildren}";
+        }
+
+        public void Dispose()
+        {
+            navigationManager.LocationChanged -= OnLocationChanged;
         }
     }
 }
