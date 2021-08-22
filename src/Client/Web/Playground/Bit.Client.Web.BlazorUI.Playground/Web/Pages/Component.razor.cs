3c7d845366c217cc2c5977dd09c84452fb86bd65
Mahsa Mohammadi <mohammadi.it91@yahoo.com>
2021-08-22 18:14:28 +0430

    fix some issues

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Component.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Component.razor.cs
@@ -23,8 +23,8 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages
         private int RatingReadOnlyValue = 5;
         private int RatingOutsideValue = 5;
 
-        private readonly string UploadUrl = new("/FileUpload/UploadStreamedFile");
-        private readonly string RemoveUrl = new("/FileUpload/RemoveFile");
+        private readonly string UploadUrl = "/FileUpload/UploadStreamedFile";
+        private readonly string RemoveUrl = "/FileUpload/RemoveFile";
 
         private double BasicSpinButtonValue = 5;
         private double BasicSpinButtonDisableValue = 20;
