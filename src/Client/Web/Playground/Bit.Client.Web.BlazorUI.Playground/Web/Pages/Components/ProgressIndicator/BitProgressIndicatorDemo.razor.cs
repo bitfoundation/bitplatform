a42fb0cc04f7fc51b154f7bf43bf5c3e8e3649e2
Somaye Ebrahimi <46419023+SomayeEbrahimi@users.noreply.github.com>
2021-08-26 18:03:11 +0430

    Add BitProgressIndicator component demo page (#717)

---
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/ProgressIndicator/BitProgressIndicatorDemo.razor.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.ProgressIndicator
+{
+    public partial class BitProgressIndicatorDemo
+    {
+        private string description = "Push button to start!";
+
+        public int CompletedPercent { get; set; }
+
+        private async Task StartProgress()
+        {
+            CompletedPercent = 0;
+
+            while (CompletedPercent <= 100)
+            {
+                if (CompletedPercent == 100)
+                {
+                    description = $"Completed !";
+                    break;
+                }
+                else
+                {
+                    CompletedPercent++;
+                    description = $"{CompletedPercent}%";
+                }
+
+                StateHasChanged();
+                await Task.Delay(100);
+            }
+        }
+    }
+}
