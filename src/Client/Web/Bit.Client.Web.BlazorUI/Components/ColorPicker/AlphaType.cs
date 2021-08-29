ca229b5530c7d09ceccfc39147a0093a8b39fe04
Somaye Ebrahimi <s.ebrahimi67@gmail.com>
2021-08-29 16:35:30 +0430

    Remove BitColorPicker enum summaries

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/ColorPicker/AlphaType.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/ColorPicker/AlphaType.cs
@@ -2,19 +2,8 @@
 {
     public enum AlphaType
     {
-        /// <summary>
-        /// Display a slider and text field for editing alpha values
-        /// </summary>
         Alpha,
-
-        /// <summary>
-        /// Displays a slider and text field for editing transparency values
-        /// </summary>
         Transparency,
-
-        /// <summary>
-        /// Hides slider and text field for editing values
-        /// </summary>
         None
     }
 }
