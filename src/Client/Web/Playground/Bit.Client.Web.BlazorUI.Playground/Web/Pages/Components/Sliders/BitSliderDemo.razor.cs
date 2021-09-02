01c7051bf2fe6c3cd3737a324d775d88767dc9e7
RezaKargar <r.kargar.2014@gmail.com>
2021-09-02 19:13:09 +0430

    Add BitSlider demo page (#711)
    This commit solves the issue #711 by adding demo page for `BitSlider`
    component on playground and also updates the navigation menu to
    add link for new demo page

---
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Sliders/BitSliderDemo.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Sliders
+{
+    public partial class BitSliderDemo
+    {
+        private double? BitSliderHorizontalValue = 2;
+        private double? BitSliderRangedLowerValue = 0;
+        private double? BitSliderRangedUpperValue = 0;
+
+        private Dictionary<string, object>? BitSliderRangedSliderBoxHtmlAttributes = new()
+        {
+            { "custom-attribute", "demo" }
+        };
+
+        private void ChangeBitSliderRangedValues()
+        {
+            BitSliderRangedLowerValue = 3;
+            BitSliderRangedUpperValue = 7;
+        }
+    }
+}
