04d2374f2ca37dde612998a39bedd5f4073353c5
Maryam Heydarian <maryamhdr76@gmail.com>
2021-09-07 16:54:49 +0430

    add test method for correct Visual class #746

--- src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Inputs/BitDropDownTests.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Inputs/BitDropDownTests.cs
@@ -9,6 +9,30 @@ namespace Bit.Client.Web.BlazorUI.Tests.Inputs
     [TestClass]
     public class BitDropDownTests : BunitTestContext
     {
+        [DataTestMethod,
+          DataRow(Visual.Fluent, true),
+          DataRow(Visual.Fluent, false),
 
+          DataRow(Visual.Cupertino, true),
+          DataRow(Visual.Cupertino, false),
+
+          DataRow(Visual.Material, true),
+          DataRow(Visual.Material, false)
+        ]
+        public void BitDropDownShouldTakeCorrectVisual(Visual visual, bool isEnabled)
+        {
+            var component = RenderComponent<BitDropDownTest>(parameters =>
+            {
+                parameters.Add(p => p.Visual, visual);
+                parameters.Add(p => p.IsEnabled, isEnabled);
+            });
+
+            var bitDrp = component.Find(".bit-drp");
+
+            var enabledClass = isEnabled ? "enabled" : "disabled";
+            var visualClass = visual == Visual.Cupertino ? "cupertino" : visual == Visual.Material ? "material" : "fluent";
+
+            Assert.IsTrue(bitDrp.ClassList.Contains($"bit-drp-{enabledClass}-{visualClass}"));
+        }
     }
 }
