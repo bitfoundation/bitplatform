d551c8ce755ba5ae93118dda86d55726e4357838
Maryam Heydarian <48528003+maryamhdr@users.noreply.github.com>
2021-09-13 18:26:20 +0430

    Add BitDropDown component RenderFragment parameters (#786)

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/Inputs/BitDropDownItem.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Inputs/BitDropDownItem.cs
@@ -26,5 +26,25 @@
         /// The type of this item, Refers to the dropdown separator
         /// </summary>
         public BitDropDownItemType ItemType { get; set; } = BitDropDownItemType.Normal;
+
+        /// <summary>
+        /// Title attribute (built in tooltip) for a given item
+        /// </summary>
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Defines whether the item is hidden or not
+        /// </summary>
+        public bool IsHidden { get; set; }
+
+        /// <summary>
+        /// The aria label for the dropdown item. If not present, the `text` will be used
+        /// </summary>
+        public string? AriaLabel { get; set; }
+
+        /// <summary>
+        /// Data available to custom templates
+        /// </summary>
+        public object? Data { get; set; }
     }
 }
