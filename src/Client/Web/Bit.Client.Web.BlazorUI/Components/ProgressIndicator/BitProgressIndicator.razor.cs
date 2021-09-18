ca59a5b13860717943e93d884435603a664fd4c7
Maryam Heydarian <48528003+maryamhdr@users.noreply.github.com>
2021-09-18 12:07:48 +0430

    Modify BitProgressIndicator component features (#808)

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/ProgressIndicator/BitProgressIndicator.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/ProgressIndicator/BitProgressIndicator.razor.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 
@@ -9,8 +6,7 @@ namespace Bit.Client.Web.BlazorUI
 {
     public partial class BitProgressIndicator
     {
-        private double percentComplete;
-        private bool PercentCompleteHasBeenSet;
+        private double? percentComplete;
 
         /// <summary>
         /// Label to display above the component
@@ -26,52 +22,54 @@ namespace Bit.Client.Web.BlazorUI
         /// Percentage of the operation's completeness, numerically between 0 and 100. If this is not set, the indeterminate progress animation will be shown instead
         /// </summary>
         [Parameter]
-        public double PercentComplete
+        public double? PercentComplete
         {
             get => percentComplete;
-            set => percentComplete = Normalize(value);
+            set
+            {
+                percentComplete = value is not null ? Normalize(value) : null;
+            }
         }
 
         /// <summary>
         /// Text describing or supplementing the operation
         /// </summary>
-        [Parameter]
-        public string Description { get; set; } = string.Empty;
+        [Parameter] public string Description { get; set; } = string.Empty;
 
-        public override async Task SetParametersAsync(ParameterView parameters)
+        /// <summary>
+        /// Text alternative of the progress status, used by screen readers for reading the value of the progress
+        /// </summary>
+        [Parameter] public string AriaValueText { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Whether or not to hide the progress state
+        /// </summary>
+        [Parameter] public bool IsProgressHidden { get; set; }
+
+        /// <summary>
+        /// A custom template for progress track
+        /// </summary>
+        [Parameter] public RenderFragment<BitProgressIndicator>? ProgressTemplate { get; set; }
+
+        public string? LabelId { get; set; } = string.Empty;
+        public string? DescriptionId { get; set; } = string.Empty;
+
+        protected override async Task OnParametersSetAsync()
         {
-            PercentCompleteHasBeenSet = false;
+            LabelId = Label.HasValue() ? $"progress-indicator{UniqueId}-label" : null;
+            DescriptionId = Description.HasValue() ? $"progress-indicator{UniqueId}-description" : null;
 
-            var parametersDictionary = parameters.ToDictionary() as Dictionary<string, object>;
-            foreach (var parameter in parametersDictionary!)
-            {
-                switch (parameter.Key)
-                {
-                    case nameof(PercentComplete):
-                        PercentCompleteHasBeenSet = true;
-                        PercentComplete = (double)parameter.Value;
-                        break;
-                    case nameof(Label):
-                        Label = (string)parameter.Value;
-                        break;
-                    case nameof(BarHeight):
-                        BarHeight = (int)parameter.Value;
-                        break;
-                    case nameof(Description):
-                        Description = (string)parameter.Value;
-                        break;
-                }
-            }
-            await base.SetParametersAsync(parameters);
+            await base.OnParametersSetAsync();
         }
 
         protected override string RootElementClass => "bit-pi";
+
         protected override void RegisterComponentClasses()
         {
-            ClassBuilder.Register(() => PercentCompleteHasBeenSet ? string.Empty
+            ClassBuilder.Register(() => PercentComplete is not null ? string.Empty
                                                 : $"{RootElementClass}-indeterminate-{VisualClassRegistrar()}");
         }
 
-        private static double Normalize(double value) => Math.Clamp(value, 0, 100);
+        private static double Normalize(double? value) => Math.Clamp(value.Value, 0, 100);
     }
 }
