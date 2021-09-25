be64a488b8ea533405de56bbe6c142270d2e2bf5
Maryam Heydarian <48528003+maryamhdr@users.noreply.github.com>
2021-09-25 17:07:36 +0330

    Modify project structure (#669)

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/SpinButtons/BitSpinButtonAction.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/SpinButtons/BitSpinButtonAction.cs
@@ -2,7 +2,14 @@
 {
     public enum BitSpinButtonAction
     {
+        /// <summary>
+        /// Increase the value of spin button, based on step value parameter
+        /// </summary>
         Increment,
+
+        /// <summary>
+        /// Decrease the value of spin button, based on step value parameter
+        /// </summary>
         Decrement
     }
 }
