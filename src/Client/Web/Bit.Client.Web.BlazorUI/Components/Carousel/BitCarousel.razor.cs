1e279f33cd2dbd54f2bbccbfb0b0ddd6bedcfb54
Z_Shobbar <zoha.shobbar@outlook.com>
2021-10-11 00:47:30 +0330

    add crousel component base files #863

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Carousel/BitCarousel.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI.Components.Carousel
+{
+    public partial class BitCarousel
+    {
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+
+        protected override string RootElementClass => "bit-crsl";
+    }
+}
