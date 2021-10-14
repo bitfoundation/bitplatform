7c754bda6c628c9af2bd24b8ca42533b72e540fb
Z_Shobbar <zoha.shobbar@outlook.com>
2021-10-15 00:53:05 +0330

    add styles to next and previus button #863

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/Carousel/BitCarousel.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Carousel/BitCarousel.razor.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
 
 namespace Bit.Client.Web.BlazorUI
 {
     public partial class BitCarousel
     {
+        private List<BitCarouselItem> AllCarouselItems = new();
+
         [Parameter] public RenderFragment? ChildContent { get; set; }
 
         protected override string RootElementClass => "bit-crsl";
+
+
+        internal void RegisterItem(BitCarouselItem carouselItem)
+        {
+            if (IsEnabled is false)
+            {
+                carouselItem.IsEnabled = false;
+            }
+            AllCarouselItems.Add(carouselItem);
+        }
+
+        internal void UnregisterItem(BitCarouselItem carouselItem)
+        {
+            AllCarouselItems.Remove(carouselItem);
+        }
     }
 }
---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Carousel/BitCarouselItem.razor.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public partial class BitCarouselItem
+    {
+        internal bool IsCurrent;
+        
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+        
+        [CascadingParameter] protected BitCarousel? Carousel { get; set; }
+
+
+        protected override Task OnInitializedAsync()
+        {
+            if (Carousel is not null)
+            {
+                Carousel.RegisterItem(this);
+            }
+
+            return base.OnInitializedAsync();
+        }
+
+        protected override string RootElementClass => "bit-crslitm";
+    }
+}
