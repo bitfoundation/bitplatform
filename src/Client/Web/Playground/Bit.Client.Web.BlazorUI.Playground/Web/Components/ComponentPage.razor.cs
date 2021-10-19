4605383ec10476be3cc2d6c5c32a2aece839621d
Somaye Ebrahimi <s.ebrahimi67@hotmail.com>
2021-10-19 19:05:53 +0330

    Add base component for demo pages

---
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Components/ComponentPage.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Bit.Client.Web.BlazorUI.Playground.Web.Models;
+using Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.ComponentDemoBase;
+using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI.Playground.Web.Components
+{
+    public partial class ComponentPage
+    {
+        [Parameter] public string ComponentName { get; set; }
+        [Parameter] public string OverviewDesc { get; set; }
+        [Parameter] public RenderFragment Sample { get; set; }
+        [Parameter] public List<EnumParameter> EnumParameters { get; set; }
+        [Parameter] public List<ComponentParameter> ComponentParameters { get; set; }
+
+        private void HandleClick()
+        {
+            // = "siderail-selected";
+            StateHasChanged();
+        }
+    }
+}
---
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Models/ComponentParameter.cs
@@ -0,0 +1,10 @@
+﻿namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.ComponentDemoBase
+{
+    public class ComponentParameter
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string DefaultValue { get; set; }
+        public string Description { get; set; }
+    }
+}
