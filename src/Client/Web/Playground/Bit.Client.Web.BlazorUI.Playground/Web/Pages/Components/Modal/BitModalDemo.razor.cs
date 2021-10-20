e39e0885be159c06830d508690f823a61862a110
Saleh Yusefnejad <msynk@outlook.com>
2021-10-20 14:14:17 +0330

    changed IsOpen to a 2-way bound parameter

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Modal/BitModalDemo.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Modal/BitModalDemo.razor.cs
@@ -1,7 +1,12 @@
-﻿namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Modal
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Modal
 {
     public partial class BitModalDemo
     {
+        private int DismissCount = 0;
+
         private bool IsOpen = false;
     }
 }
