2f7c531b6b39fac0e1f8323dfc1e55c43ba2eae8
Somaye Ebrahimi <46419023+SomayeEbrahimi@users.noreply.github.com>
2021-10-23 15:29:27 +0330

    Add BitActionButton component demo page #903 (#904)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Components/ComponentPage.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Components/ComponentPage.razor.cs
@@ -2,7 +2,6 @@
 using Bit.Client.Web.BlazorUI.Playground.Web.Models;
 using Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.ComponentDemoBase;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 
 namespace Bit.Client.Web.BlazorUI.Playground.Web.Components
 {
@@ -11,8 +10,7 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Components
         [Parameter] public string ComponentName { get; set; }
         [Parameter] public string OverviewDesc { get; set; }
         [Parameter] public RenderFragment ChildContent { get; set; }
-        [Parameter] public List<EnumParameter> EnumParameters { get; set; }
-        [Parameter] public List<ComponentParameter> ComponentParameters { get; set; }
-
+        [Parameter] public List<EnumParameter> EnumParameters { get; set; } = new();
+        [Parameter] public List<ComponentParameter> ComponentParameters { get; set; } = new();
     }
 }
