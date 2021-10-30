46970dc844e83766b88c406e86b4de7134652a50
Somaye Ebrahimi <s.ebrahimi67@gmail.com>
2021-10-30 09:24:57 +0330

    Add BitBasicList component demo page (#962)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/List/BitBasicListDemo.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/List/BitBasicListDemo.razor.cs
@@ -1,10 +1,57 @@
 ﻿using System.Collections.Generic;
+using Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.ComponentDemoBase;
 
 namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.List
 {
     public partial class BitBasicListDemo
     {
         private readonly List<Person>[] People = new List<Person>[2];
+
+        private readonly List<ComponentParameter> componentParameters = new()
+        {
+            new ComponentParameter()
+            {
+                Name = "Items",
+                Type = "ICollection<TItem>",
+                DefaultValue = "",
+                Description = "List of items want to render.",
+            },
+            new ComponentParameter()
+            {
+                Name = "ItemSize",
+                Type = "int",
+                DefaultValue = "50",
+                Description = "The height of each item in pixels.",
+            },
+            new ComponentParameter()
+            {
+                Name = "OverscanCount",
+                Type = "int",
+                DefaultValue = "3",
+                Description = "determines how many additional items are rendered before and after the visible region.",
+            },
+            new ComponentParameter()
+            {
+                Name = "Role",
+                Type = "string",
+                DefaultValue = "list",
+                Description = "Role of the BasicList.",
+            },
+            new ComponentParameter()
+            {
+                Name = "RowTemplate",
+                Type = "RenderFragment<TItem>",
+                DefaultValue = "",
+                Description = "content of each item. it should determin with context attribute.",
+            },
+            new ComponentParameter()
+            {
+                Name = "Virtualize",
+                Type = "bool",
+                DefaultValue = "false",
+                Description = "virtualize rendering the list, UI rendering to just the parts that are currently visible",
+            },
+        };
 
         protected override void OnInitialized()
         {
