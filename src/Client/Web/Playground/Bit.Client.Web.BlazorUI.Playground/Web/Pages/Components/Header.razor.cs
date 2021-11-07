91168093438064bb57659bc76828970d519d18e7
Somaye Ebrahimi <s.ebrahimi67@gmail.com>
2021-11-07 09:59:33 +0330

    Add header functionality in playground project (#999)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Header.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Header.razor.cs
@@ -1,6 +1,46 @@
-﻿namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components
 {
     public partial class Header
     {
+        private string homeLinkClass;
+        private string demoLinkClass;
+        private string getstartedLinkClass;
+
+        [Parameter] public PageType ActivePage { get; set; }
+
+        protected override void OnInitialized()
+        {
+            string activeClassName = "active";
+
+            switch (ActivePage)
+            {
+                case PageType.Demo:
+                {
+                    demoLinkClass = activeClassName;
+                    break;
+                }
+                case PageType.GetStarted:
+                {
+                    getstartedLinkClass = activeClassName;
+                    break;
+                }
+                default:
+                {
+                    homeLinkClass = activeClassName;
+                    break;
+                }
+            }
+
+            base.OnInitialized();
+        }
+    }
+
+    public enum PageType
+    {
+        Home,
+        Demo,
+        GetStarted
     }
 }
