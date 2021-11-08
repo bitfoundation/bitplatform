41e8925d67acb1a2e825ba3fdd40a7e7c5b50114
Somaye Ebrahimi <s.ebrahimi67@gmail.com>
2021-11-08 10:32:51 +0330

    Change code sample style and add code for copy in BitActionButton (#1000)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Buttons/BitActionButtonDemo.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Buttons/BitActionButtonDemo.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.ComponentDemoBase;
 
 namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Buttons
@@ -9,67 +10,95 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Buttons
         {
             new ComponentParameter()
             {
-                Name = "allowDisabledFocus",
+                Name = "AllowDisabledFocus",
                 Type = "bool",
                 DefaultValue = "false",
                 Description = "Whether the action button can have focus in disabled mode.",
             },
             new ComponentParameter()
             {
-                Name = "ariaDescription",
+                Name = "AriaDescription",
                 Type = "string",
                 DefaultValue = "",
                 Description = "Detailed description of the button for the benefit of screen readers.",
             },
             new ComponentParameter()
             {
-                Name = "ariaHidden",
+                Name = "AriaHidden",
                 Type = "bool",
                 DefaultValue = "false",
                 Description = "If true, add an aria-hidden attribute instructing screen readers to ignore the element.",
             },
             new ComponentParameter()
             {
-                Name = "childContent",
+                Name = "ChildContent",
                 Type = "RenderFragment",
                 DefaultValue = "",
                 Description = "The content of action button, It can be Any custom tag or a text.",
             },
             new ComponentParameter()
             {
-                Name = "href",
+                Name = "Href",
                 Type = "string",
                 DefaultValue = "",
                 Description = "URL the link points to, if provided, action button renders as an anchor.",
             },
             new ComponentParameter()
             {
-                Name = "iconName",
+                Name = "IconName",
                 Type = "string",
                 DefaultValue = "",
                 Description = "The icon name for the icon shown in the action button.",
             },
             new ComponentParameter()
             {
-                Name = "onClick",
+                Name = "OnClick",
                 Type = "EventCallback<MouseEventArgs>",
                 DefaultValue = "",
                 Description = "Callback for when the action button clicked.",
             },
             new ComponentParameter()
             {
-                Name = "target",
+                Name = "Target",
                 Type = "string",
                 DefaultValue = "",
                 Description = "If Href provided, specifies how to open the link.",
             },
             new ComponentParameter()
             {
-                Name = "title",
+                Name = "Title",
                 Type = "string",
                 DefaultValue = "",
                 Description = "The title to show when the mouse is placed on the action button.",
             },
         };
+
+        private readonly string actionButtonSampleCode = $"<BitActionButton IconName='AddFriend'>Create account</BitActionButton>{Environment.NewLine}" +
+             $"<BitActionButton IconName='UserRemove' AllowDisabledFocus='false' IsEnabled='false'>Remove user</BitActionButton>{Environment.NewLine}";
+
+        // Change style
+        private readonly string styleAndClassSampleCode = $"<BitActionButton Style='color:red'>Styled Action Button</BitActionButton>{Environment.NewLine}" +
+                 $"<BitActionButton Class='custom-button'>Classed Action Button</BitActionButton>{Environment.NewLine}" +
+                 $"<style>{Environment.NewLine}" +
+                 $".custom-button {{ {Environment.NewLine}" +
+                 $"height:40px;{Environment.NewLine}" +
+                 $"width:166px;{Environment.NewLine}" +
+                 $"font-family:Axiforma;{Environment.NewLine}" +
+                 $" font-size:16px;{Environment.NewLine}" +
+                 $"background-color:#2F455A;{Environment.NewLine}" +
+                 $"border-color:#2F455A;{Environment.NewLine}" +
+                 $"}} {Environment.NewLine}" +
+                 $"</style>{Environment.NewLine}";
+
+        private readonly string visibilitySampleCode = $"<BitActionButton Visibility='ComponentVisibility.Visible' IconName='AddEvent'>Add Event</BitActionButton>{Environment.NewLine}" +
+                 $"<BitActionButton Hidden='ComponentHidden.Hidden'>Hidden Action Button</BitActionButton>{Environment.NewLine}" +
+                 $"<BitActionButton Visibility='ComponentVisibility.Collapsed'>Collapsed Action Button</BitActionButton>";
+
+        private readonly string ariasSampleCode = $"<BitActionButton AriaDescription='Detailed description used for screen reader.'  IconName='Library'>Action Button with Aria Description</BitActionButton>{Environment.NewLine}" +
+                 $"<BitActionButton AriaHidden='true' IconName='Library'>Action Button with Aria Hidden</BitActionButton>{Environment.NewLine}";
+
+        private readonly string actionButtonLikeAnchorSampleCode = $"<BitActionButton IconName='Website' Target='_blank' Href='https://github.com/bitfoundation/bitframework'>Open Bit Foundation In New Tab</BitActionButton>{Environment.NewLine}" +
+                 $"<BitActionButton IconName='Website' ButtonStyle='ButtonStyle.Standard' Href='https://github.com/bitfoundation/bitframework'>Go To Bit Foundation</BitActionButton>{Environment.NewLine}" +
+                 $"<BitActionButton Target='_self' IconName='Website' Href='https://github.com/bitfoundation/bitframework' IsEnabled='false'<span>Bit Foundation From Span<span></BitActionButton>{Environment.NewLine}";
     }
 }
