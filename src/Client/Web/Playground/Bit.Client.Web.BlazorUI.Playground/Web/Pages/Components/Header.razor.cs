b6498abec35b87f2e35a400c7340e4f5946aee5a
Somaye Ebrahimi <s.ebrahimi67@gmail.com>
2021-11-09 12:50:58 +0330

    Fix issues in headers (#999)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Header.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Header.razor.cs
@@ -4,16 +4,13 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components
 {
     public partial class Header
     {
-        private readonly string demoPath = "components";
-        private readonly string getStartedPath = "get-started";
-
         private string currentUri;
 
         [Inject] public NavigationManager NavigationManager { get; set; }
 
         protected override void OnInitialized()
         {
-            currentUri = NavigationManager.Uri;
+            currentUri = NavigationManager.ToBaseRelativePath(NavigationManager.Uri);
 
             base.OnInitialized();
         }
