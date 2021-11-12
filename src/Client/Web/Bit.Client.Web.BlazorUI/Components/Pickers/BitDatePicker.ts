e3bb86f5970a95da306968021a89467dc67b5af2
Maryam Heydarian <maryamhdr76@gmail.com>
2021-11-12 17:17:01 +0330

    fix date picker callout position #1047

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Pickers/BitDatePicker.ts
@@ -0,0 +1,51 @@
+﻿class BitDatePicker {
+    static toggleDatePickerCallout(datePickerId: string,
+        datePickerCalloutId: string,
+        datePickerOverlayId: string,
+        isOpen: boolean) {
+        const datePicker = document.getElementById(datePickerId);
+        const datePickerCallout = document.getElementById(datePickerCalloutId);
+        const datePickerOverlay = document.getElementById(datePickerOverlayId);
+
+        if (!datePickerCallout || !datePickerOverlay || !datePicker) return;
+
+        if (isOpen) {
+            datePickerCallout.style.display = "none";
+            datePickerOverlay.style.display = "none";
+        } else {
+            datePickerCallout.style.display = "block";
+            datePickerOverlay.style.display = "block";
+            var datePickerCalloutHeight = datePickerCallout.offsetHeight;
+            var datePickerCalloutWidth = datePickerCallout.offsetWidth;
+            var datePickerHeight = datePicker.offsetHeight;
+            var datePickerWidth = datePicker.offsetWidth;
+            var datePickerX = datePicker.getBoundingClientRect().x;
+            var datePickerY = datePicker.getBoundingClientRect().y;
+            var datePickerTop = datePicker.getBoundingClientRect().y;
+            var datePickerWrapperBottom = window.innerHeight - (datePickerHeight + datePickerY);
+            var datePickerWrapperRight = window.innerWidth - (datePickerWidth + datePickerX);
+
+            if (datePickerWrapperBottom >= datePickerCalloutHeight) {
+                datePickerCallout.style.top = datePickerHeight + 1 + "px";
+                datePickerCallout.style.left = "0px";
+                datePickerCallout.style.right = "unset";
+                datePickerCallout.style.bottom = "unset";
+            } else if (datePickerTop >= datePickerCalloutHeight) {
+                datePickerCallout.style.right = "unset";
+                datePickerCallout.style.top = "unset";
+                datePickerCallout.style.bottom = datePickerHeight + 1 + "px";
+                datePickerCallout.style.left = "0px";
+            } else if (datePickerWrapperRight >= datePickerCalloutWidth) {
+                datePickerCallout.style.left = datePickerWidth + 1 + "px";
+                datePickerCallout.style.bottom = 0 - datePickerWrapperBottom + "px";
+                datePickerCallout.style.right = "unset";
+                datePickerCallout.style.top = "unset";
+            } else {
+                datePickerCallout.style.left = "unset";
+                datePickerCallout.style.top = "unset"
+                datePickerCallout.style.right = datePickerWidth + "px";
+                datePickerCallout.style.bottom = 0 - datePickerWrapperBottom + "px";
+            }
+        }
+    }
+}
