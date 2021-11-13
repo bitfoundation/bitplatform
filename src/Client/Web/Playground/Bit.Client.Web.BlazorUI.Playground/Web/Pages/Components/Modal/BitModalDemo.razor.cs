6eb7c503114d7513dde6b1843fcd540cb0671e20
Somaye Ebrahimi <s.ebrahimi67@hotmail.com>
2021-11-13 21:56:46 +0330

    Improve BitModal component demo page (#1056)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Modal/BitModalDemo.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Modal/BitModalDemo.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.ComponentDemoBase;
 
 namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Modal
@@ -11,60 +12,111 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Modal
         {
             new ComponentParameter()
             {
-                Name = "isAlert",
-                Type = "bool?",
-                DefaultValue = "",
-                Description = "Determines the ARIA role of the dialog (alertdialog/dialog). If this is set, it will override the ARIA role determined by IsBlocking and IsModeless.",
-            },
-            new ComponentParameter()
-            {
-                Name = "isBlocking",
-                Type = "bool",
-                DefaultValue = "false",
-                Description = "Whether the dialog can be light dismissed by clicking outside the dialog (on the overlay).",
-            },
-            new ComponentParameter()
-            {
-                Name = "isModeless",
-                Type = "bool",
-                DefaultValue = "false",
-                Description = "Whether the dialog should be modeless (e.g. not dismiss when focusing/clicking outside of the dialog). if true: IsBlocking is ignored, there will be no overlay.",
-            },
-            new ComponentParameter()
-            {
-                Name = "isOpen",
-                Type = "bool",
-                DefaultValue = "false",
-                Description = "Whether the dialog is displayed.",
-            },
-            new ComponentParameter()
-            {
-                Name = "childContent",
+                Name = "ChildContent",
                 Type = "RenderFragment",
                 DefaultValue = "",
                 Description = "The content of Modal, It can be Any custom tag or a text.",
             },
             new ComponentParameter()
             {
-                Name = "onDismiss",
+                Name = "IsAlert",
+                Type = "bool?",
+                DefaultValue = "",
+                Description = "Determines the ARIA role of the dialog (alertdialog/dialog). If this is set, it will override the ARIA role determined by IsBlocking and IsModeless.",
+            },
+            new ComponentParameter()
+            {
+                Name = "IsBlocking",
+                Type = "bool",
+                DefaultValue = "false",
+                Description = "Whether the dialog can be light dismissed by clicking outside the dialog (on the overlay).",
+            },
+            new ComponentParameter()
+            {
+                Name = "IsModeless",
+                Type = "bool",
+                DefaultValue = "false",
+                Description = "Whether the dialog should be modeless (e.g. not dismiss when focusing/clicking outside of the dialog). if true: IsBlocking is ignored, there will be no overlay.",
+            },
+            new ComponentParameter()
+            {
+                Name = "IsOpen",
+                Type = "bool",
+                DefaultValue = "false",
+                Description = "Whether the dialog is displayed.",
+            },
+            new ComponentParameter()
+            {
+                Name = "OnDismiss",
                 Type = "EventCallback<MouseEventArgs>",
                 DefaultValue = "",
                 Description = "A callback function for when the Modal is dismissed light dismiss, before the animation completes.",
             },
             new ComponentParameter()
             {
-                Name = "subtitleAriaId",
+                Name = "SubtitleAriaId",
                 Type = "string",
-                DefaultValue = "string.Empty",
+                DefaultValue = "",
                 Description = "ARIA id for the subtitle of the Modal, if any.",
             },
             new ComponentParameter()
             {
-                Name = "titleAriaId",
+                Name = "TitleAriaId",
                 Type = "string",
-                DefaultValue = "string.Empty",
+                DefaultValue = "",
                 Description = "ARIA id for the title of the Modal, if any.",
             },
         };
+
+        private readonly string modalSampleCode = $"<BitButton OnClick=@(()=>IsOpen=true)>Open Modal</BitButton>{Environment.NewLine}" +
+              $"<BitModal @bind-IsOpen=IsOpen>{Environment.NewLine}" +
+              $"<div class='modal-header'>{Environment.NewLine}" +
+              $"<span>Lorem Ipsum</span>{Environment.NewLine}" +
+              $"<BitIconButton OnClick=@(()=>IsOpen=false) IconName='ChromeClose' Title='Close' />{Environment.NewLine}" +
+              $"</div>{Environment.NewLine}" +
+              $"<div class='modal-body'>{Environment.NewLine}" +
+              $"<p>{Environment.NewLine}" +
+              $"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sitamet, vulputate in leo.Maecenas vulputate congue sapien eu tincidunt.Etiam eu sem turpis.Fusce temporsagittis nunc, ut interdum ipsum vestibulum non." +
+              $"Proin dolor elit, aliquam eget tincidunt non, vestibulum ut turpis.In hac habitasse platea dictumst.In a odio eget enim porttitor maximus.Aliquam nulla nibh,ullamcorper aliquam placerat eu, viverra et dui.Phasellus ex lectus, maximus in mollis ac, luctus vel eros." +
+              $"Vivamus ultrices, turpis sed malesuada gravida, eros ipsum venenatis elit, et volutpat eros dui et ante. Quisque ultricies mi nec leo ultricies mollis.Vivamus egestas volutpat lacinia. Quisque pharetra eleifend fficitur.{ Environment.NewLine}" +
+              $"</p>{Environment.NewLine}" +
+              $"<p>{Environment.NewLine}" +
+              $"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sitamet, vulputate in leo.Maecenas vulputate congue sapien eu tincidunt.Etiam eu sem turpis.Fusce temporsagittis nunc, ut interdum ipsum vestibulum non." +
+              $"Proin dolor elit, aliquam eget tincidunt non, vestibulum ut turpis.In hac habitasse platea dictumst.In a odio eget enim porttitor maximus.Aliquam nulla nibh,ullamcorper aliquam placerat eu, viverra et dui.Phasellus ex lectus, maximus in mollis ac, luctus vel eros." +
+              $"Vivamus ultrices, turpis sed malesuada gravida, eros ipsum venenatis elit, et volutpat eros dui et ante. Quisque ultricies mi nec leo ultricies mollis.Vivamus egestas volutpat lacinia. Quisque pharetra eleifend fficitur.{ Environment.NewLine}" +
+              $"</p>{Environment.NewLine}" +
+              $"<p>{Environment.NewLine}" +
+              $"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sitamet, vulputate in leo.Maecenas vulputate congue sapien eu tincidunt.Etiam eu sem turpis.Fusce temporsagittis nunc, ut interdum ipsum vestibulum non." +
+              $"Proin dolor elit, aliquam eget tincidunt non, vestibulum ut turpis.In hac habitasse platea dictumst.In a odio eget enim porttitor maximus.Aliquam nulla nibh,ullamcorper aliquam placerat eu, viverra et dui.Phasellus ex lectus, maximus in mollis ac, luctus vel eros." +
+              $"Vivamus ultrices, turpis sed malesuada gravida, eros ipsum venenatis elit, et volutpat eros dui et ante. Quisque ultricies mi nec leo ultricies mollis.Vivamus egestas volutpat lacinia. Quisque pharetra eleifend fficitur.{ Environment.NewLine}" +
+              $"</p>{Environment.NewLine}" +
+              $"<p>{Environment.NewLine}" +
+              $"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sitamet, vulputate in leo.Maecenas vulputate congue sapien eu tincidunt.Etiam eu sem turpis.Fusce temporsagittis nunc, ut interdum ipsum vestibulum non." +
+              $"Proin dolor elit, aliquam eget tincidunt non, vestibulum ut turpis.In hac habitasse platea dictumst.In a odio eget enim porttitor maximus.Aliquam nulla nibh,ullamcorper aliquam placerat eu, viverra et dui.Phasellus ex lectus, maximus in mollis ac, luctus vel eros." +
+              $"Vivamus ultrices, turpis sed malesuada gravida, eros ipsum venenatis elit, et volutpat eros dui et ante. Quisque ultricies mi nec leo ultricies mollis.Vivamus egestas volutpat lacinia. Quisque pharetra eleifend fficitur.{ Environment.NewLine}" +
+              $"</p>{Environment.NewLine}" +
+              $"<p>{Environment.NewLine}" +
+              $"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sitamet, vulputate in leo.Maecenas vulputate congue sapien eu tincidunt.Etiam eu sem turpis.Fusce temporsagittis nunc, ut interdum ipsum vestibulum non." +
+              $"Proin dolor elit, aliquam eget tincidunt non, vestibulum ut turpis.In hac habitasse platea dictumst.In a odio eget enim porttitor maximus.Aliquam nulla nibh,ullamcorper aliquam placerat eu, viverra et dui.Phasellus ex lectus, maximus in mollis ac, luctus vel eros." +
+              $"Vivamus ultrices, turpis sed malesuada gravida, eros ipsum venenatis elit, et volutpat eros dui et ante. Quisque ultricies mi nec leo ultricies mollis.Vivamus egestas volutpat lacinia. Quisque pharetra eleifend fficitur.{ Environment.NewLine}" +
+              $"</p>{Environment.NewLine}" +
+              $"</div>{Environment.NewLine}" +
+              $"</BitModal>{Environment.NewLine}" +
+              $"<style>{Environment.NewLine}" +
+              $".modal-header {{ {Environment.NewLine}" +
+              $"display: flex;{Environment.NewLine}" +
+              $"align-items: center;{Environment.NewLine}" +
+              $"font-size: 24px;{Environment.NewLine}" +
+              $"font-weight: 600;{Environment.NewLine}" +
+              $"border-top: 4px solid #5C2D91;{Environment.NewLine}" +
+              $"justify-content: space-between;{Environment.NewLine}" +
+              $"padding: 12px 12px 14px 24px;{Environment.NewLine}" +
+              $"}} {Environment.NewLine}" +
+              $".modal-body {{ {Environment.NewLine}" +
+              $"overflow-y: hidden;{Environment.NewLine}" +
+              $"line-height: 20px;{Environment.NewLine}" +
+              $"padding: 0 24px 24px;{Environment.NewLine}" +
+              $"}} {Environment.NewLine}" +
+              $"</style>{Environment.NewLine}";
     }
 }
