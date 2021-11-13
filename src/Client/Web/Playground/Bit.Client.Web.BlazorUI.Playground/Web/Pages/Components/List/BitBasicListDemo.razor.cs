ca991b6d91b5ba3958ee8161fd0e1266f8a7da09
Somaye Ebrahimi <s.ebrahimi67@gmail.com>
2021-11-13 15:43:15 +0330

    Improve BitBasicList component demo page (#1052)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/List/BitBasicListDemo.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/List/BitBasicListDemo.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.ComponentDemoBase;
 
 namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.List
@@ -52,6 +53,110 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.List
                 Description = "virtualize rendering the list, UI rendering to just the parts that are currently visible",
             },
         };
+
+        private static string listCodeBehinde = $"@code {{ {Environment.NewLine}" +
+             $"private readonly List<Person>[] People = new List<Person>[2];{Environment.NewLine}" +
+             $"protected override void OnInitialized(){Environment.NewLine}" +
+             $"{{ {Environment.NewLine}" +
+             $"Person person = new();{Environment.NewLine}" +
+             $"People[0] = person.GetPeople(8000);{Environment.NewLine}" +
+             $"People[1] = person.GetPeople(100);{Environment.NewLine}" +
+             $"base.OnInitialized();{Environment.NewLine}" +
+             $"}} {Environment.NewLine}" +
+             $"public class Person(){Environment.NewLine}" +
+             $"{{ {Environment.NewLine}" +
+             $"public int Id {{ get;  set; }}{Environment.NewLine}" +
+             $"public int FirstName {{ get;  set; }}{Environment.NewLine}" +
+             $"public int LastName {{ get;  set; }}{Environment.NewLine}" +
+             $"public int Job {{ get;  set; }}{Environment.NewLine}" +
+             $"}} {Environment.NewLine}" +
+             $"public List<Person> GetPeople(int itemCount){Environment.NewLine}" +
+             $"{{ {Environment.NewLine}" +
+             $"List<Person> person = new(); {Environment.NewLine}" +
+             $"for (int i = 0; i < itemCount; i++){Environment.NewLine}" +
+             $"{{ {Environment.NewLine}" +
+             $"people.Add(new Person {Environment.NewLine}" +
+             $"{{ {Environment.NewLine}" +
+             $"Id = i + 1,{Environment.NewLine}" +
+             $"FirstName = $'Person {{i + 1}}',{Environment.NewLine}" +
+             $"LastName = $'Person Family {{ i + 1}}',{Environment.NewLine}" +
+             $"Job = $'Programmer {{i + 1}}'{Environment.NewLine}" +
+             $"}});{Environment.NewLine}" +
+             $"}} {Environment.NewLine}" +
+             $"}} {Environment.NewLine}" +
+             $"}}";
+
+        private readonly string basicListSampleCode = $"<BitBasicList Items='People[0]'{Environment.NewLine}" +
+             $"Virtualize='true'{Environment.NewLine}" +
+             $"Style='border: 1px #a19f9d solid; border-radius: 3px;'>{Environment.NewLine}" +
+             $"<RowTemplate Context='person'>{Environment.NewLine}" +
+             $"<div style='margin-left:3%; display: inline-block;'>{Environment.NewLine}" +
+             $"<img src='https://picsum.photos/100/100?random=@(person.Id)'>{Environment.NewLine}" +
+             $"<div style='margin-left:3%; display: inline-block;'>{Environment.NewLine}" +
+             $"<p>Id: <strong>@person.Id</strong></p>{Environment.NewLine}" +
+             $"<p>Full Name: <strong>@person.FirstName @person.LastName</strong></p>{Environment.NewLine}" +
+             $"<p>Job: <strong>@person.Job</strong></p>{Environment.NewLine}" +
+             $"</div>{Environment.NewLine}" +
+             $"</div>{Environment.NewLine}" +
+             $"</RowTemplate>{Environment.NewLine}" +
+             $"</BitBasicList>{Environment.NewLine}" + listCodeBehinde;
+
+        private readonly string basicListWithoutVirtualizationSampleCode = $"<BitBasicList Items='People[1]'{Environment.NewLine}" +
+            $"Virtualize='false'{Environment.NewLine}" +
+            $"Style='border: 1px #a19f9d solid; border-radius: 3px;'>{Environment.NewLine}" +
+            $"<RowTemplate Context='person'>{Environment.NewLine}" +
+            $"<div style='margin-left:3%; display: inline-block;'>{Environment.NewLine}" +
+            $"<img src='https://picsum.photos/100/100?random=@(person.Id)'>{Environment.NewLine}" +
+            $"<div style='margin-left:3%; display: inline-block;'>{Environment.NewLine}" +
+            $"<p>Id: <strong>@person.Id</strong></p>{Environment.NewLine}" +
+            $"<p>Full Name: <strong>@person.FirstName @person.LastName</strong></p>{Environment.NewLine}" +
+            $"<p>Job: <strong>@person.Job</strong></p>{Environment.NewLine}" +
+            $"</div>{Environment.NewLine}" +
+            $"</div>{Environment.NewLine}" +
+            $"</RowTemplate>{Environment.NewLine}" +
+            $"</BitBasicList>{Environment.NewLine}" + listCodeBehinde;
+
+        private readonly string basicListWithCustomRoleSampleCode = $"<BitBasicList Items='People[0]'{Environment.NewLine}" +
+            $"Virtualize='true'{Environment.NewLine}" +
+            $"Role='list'{Environment.NewLine}" +
+            $"Class='lst-custom-style'{Environment.NewLine}" +
+            $"Style='border: 1px #a19f9d solid; border-radius: 3px;'>{Environment.NewLine}" +
+            $"<RowTemplate Context='person'>{Environment.NewLine}" +
+            $"<div class='lst3-list-item'>{Environment.NewLine}" +
+            $"<span>Id: <strong>@person.Id</strong></span>{Environment.NewLine}" +
+            $"<span>Full Name: <strong>@person.FirstName @person.LastName</strong></span>{Environment.NewLine}" +
+            $"<span>Job: <strong>@person.Job</strong></span>{Environment.NewLine}" +
+            $"</div>{Environment.NewLine}" +
+            $"</RowTemplate>{Environment.NewLine}" +
+            $"</BitBasicList>{Environment.NewLine}" + listCodeBehinde +
+            $"<style>{Environment.NewLine}" +
+            $".lst-custom-style {{ {Environment.NewLine}" +
+            $".lst3-list-item {{ {Environment.NewLine}" +
+            $"padding :16px 20px;{Environment.NewLine}" +
+            $"background-color : #f2f2f2;{Environment.NewLine}" +
+            $"margin : 10px 10px;{Environment.NewLine}" +
+            $"width : 20%;{Environment.NewLine}" +
+            $"height : 143px;{Environment.NewLine}" +
+            $"display : inline-grid;{Environment.NewLine}" +
+            $"justify-content : center;{Environment.NewLine}" +
+            $"align-items : center;{Environment.NewLine}" +
+            $"}} {Environment.NewLine}" +
+            $"}} {Environment.NewLine}" +
+            $"</style>";
+
+        private readonly string basicListWithoverscanSampleCode = $"<BitBasicList Items='People[0]'{Environment.NewLine}" +
+            $"Virtualize='true'{Environment.NewLine}" +
+            $"OverscanCount='5'{Environment.NewLine}" +
+            $"ItemSize='300'{Environment.NewLine}" +
+            $"Style='border: 1px #a19f9d solid; border-radius: 3px;'>{Environment.NewLine}" +
+            $"<RowTemplate Context='person'>{Environment.NewLine}" +
+            $"<div style='border-bottom: 1px #8a8886 solid; padding: 5px 20px; margin: 10px;'>{Environment.NewLine}" +
+            $"<span>Id: <strong>@person.Id</strong></span>{Environment.NewLine}" +
+            $"<span>Full Name: <strong>@person.FirstName @person.LastName</strong></span>{Environment.NewLine}" +
+            $"<span>Job: <strong>@person.Job</strong></span>{Environment.NewLine}" +
+            $"</div>{Environment.NewLine}" +
+            $"</RowTemplate>{Environment.NewLine}" +
+            $"</BitBasicList>{Environment.NewLine}" + listCodeBehinde;
 
         protected override void OnInitialized()
         {
