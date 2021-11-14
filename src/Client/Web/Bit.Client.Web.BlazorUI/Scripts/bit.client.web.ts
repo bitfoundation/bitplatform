62458cc802d76de86d260f282c850401c11f3ba8
Maryam Heydarian <48528003+maryamhdr@users.noreply.github.com>
2021-11-14 09:29:28 -0800

    Fix callout closing issue #1067 (#1068)

--- src/Client/Web/Bit.Client.Web.BlazorUI/Scripts/bit.client.web.ts
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Scripts/bit.client.web.ts
@@ -1,4 +1,23 @@
-﻿class Bit {
+﻿class CalloutComponent {
+    calloutId: string;
+    overlayId: string;
+    objRef: any;
+
+    constructor() {
+        this.calloutId = "";
+        this.overlayId = "";
+    }
+
+    update(calloutId: string, overlayId: string, obj: any) {
+        this.calloutId = calloutId;
+        this.overlayId = overlayId;
+        this.objRef = obj;
+    }
+}
+
+class Bit {
+    static currentCallout: CalloutComponent = new CalloutComponent();
+
     static setProperty(element: { [key: string]: any }, property: string, value: any): void {
         element[property] = value;
     }
@@ -13,5 +32,21 @@
 
     static getClientHeight(element: { [key: string]: any }): string {
         return element.clientHeight;
+    }
+
+    static closeCurrentCalloutIfExists(calloutId: string, overlayId: string, obj: any) {
+        if (Bit.currentCallout.calloutId.length === 0 || Bit.currentCallout.overlayId.length === 0) {
+            Bit.currentCallout.update(calloutId, overlayId, obj);
+            return;
+        }
+
+        if (calloutId !== Bit.currentCallout.calloutId && overlayId !== Bit.currentCallout.overlayId) {
+            var callout = document.getElementById(Bit.currentCallout.calloutId) ?? new HTMLElement();
+            var overlay = document.getElementById(Bit.currentCallout.overlayId) ?? new HTMLElement();
+            callout.style.display = "none";
+            overlay.style.display = "none";
+            Bit.currentCallout.objRef.invokeMethodAsync("CloseCallout");
+            Bit.currentCallout.update(calloutId, overlayId, obj);
+        }
     }
 }
