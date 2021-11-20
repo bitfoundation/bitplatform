aca4e927d45cabda0dffb5b21515f01d809de784
Somaye Ebrahimi <46419023+SomayeEbrahimi@users.noreply.github.com>
2021-11-20 14:37:27 +0330

    Improve overview page #1085 (#1087)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Overview.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Overview.razor.cs
@@ -1,6 +1,15 @@
-﻿namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components
+﻿using Bit.Client.Web.BlazorUI.Playground.Web.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components
 {
     public partial class Overview
     {
+        [Inject] public NavManuService NavManuService { get; set; }
+
+        private void ToggleMenu()
+        {
+            NavManuService.ToggleMenu();
+        }
     }
 }
