3cbee3d7ca1a0e9dedf77182cdaa63c54424775c
Maryam Heydarian <maryamhdr76@gmail.com>
2021-11-23 23:10:20 +0330

    implement popular components section #1106

---
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Models/PopularComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Bit.Client.Web.BlazorUI.Playground.Web.Models
+{
+    public class PopularComponent
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string CssClassName { get; set; }
+    }
+}
