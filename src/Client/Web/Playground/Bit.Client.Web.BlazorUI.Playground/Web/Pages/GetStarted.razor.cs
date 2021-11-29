38d4531d2bdd74a3f823d26189c2b3a7bfd871af
Maryam Heydarian <maryamhdr76@gmail.com>
2021-11-29 09:04:18 -0800

    modify get started page content #1118 (#1119)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/GetStarted.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/GetStarted.razor.cs
@@ -10,47 +10,27 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages
             new SideRailItem()
             {
                 Title = "Getting Started",
-                Id = "get-started-section"
+                Id = "getSartedSection"
             },
             new SideRailItem()
             {
-                Title = "First Local Development",
-                Id = "first-development-section"
+                Title = "Install",
+                Id = "installationSection"
             },
             new SideRailItem()
             {
-                Title = "Installation",
-                Id = "installation-section"
+                Title = "Import the namespace",
+                Id = "importNamespaceSection"
             },
             new SideRailItem()
             {
-                Title = "Create a New Blazor Web",
-                Id = "web-assembly-section"
+                Title = "Include styles",
+                Id = "includeStyleSection"
             },
             new SideRailItem()
             {
-                Title = "Development & Debugging",
-                Id = "debugging-section"
-            },
-            new SideRailItem()
-            {
-                Title = "Building & Deployment",
-                Id = "building-section"
-            },
-            new SideRailItem()
-            {
-                Title = "Install Bit Design Blazor",
-                Id = "install-blazor-section"
-            },
-            new SideRailItem()
-            {
-                Title = "Register Dependencies",
-                Id = "register-dependency-section"
-            },
-            new SideRailItem()
-            {
-                Title = "Import Styles",
-                Id = "import-style-section"
+                Title = "Include source file",
+                Id = "includeSourceSection"
             }
         };
     }
