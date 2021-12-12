7a8dafd35a6b906c43fce45c637981f7003df79e
Maryam Heydarian <maryamhdr76@gmail.com>
2021-12-13 00:49:19 +0330

    implemen example box com #1165

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Components/ComponentDemo.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Components/ComponentDemo.razor.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using Bit.Client.Web.BlazorUI.Playground.Web.Models;
 using Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.ComponentDemoBase;
-using Bit.Client.Web.BlazorUI.Playground.Web.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace Bit.Client.Web.BlazorUI.Playground.Web.Components
@@ -27,18 +26,12 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Components
             }
         };
 
-        [Inject] public NavManuService NavManuService { get; set; }
-
         [Parameter] public string ComponentName { get; set; }
+        [Parameter] public string ComponentDescription { get; set; }
         [Parameter] public string OverviewDesc { get; set; }
         [Parameter] public RenderFragment ChildContent { get; set; }
         [Parameter] public List<EnumParameter> EnumParameters { get; set; } = new();
         [Parameter] public List<ComponentParameter> ComponentParameters { get; set; } = new();
         [Parameter] public List<ComponentSubParameter> ComponentSubParameters { get; set; } = new();
-
-        private void ToggleMenu()
-        {
-            NavManuService.ToggleMenu();
-        }
     }
 }
---
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Components/ComponentExampleBox.razor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Bit.Client.Web.BlazorUI.Playground.Web.Components
+{
+    public partial class ComponentExampleBox
+    {
+        private bool showCode { get; set; }
+        private string exampleId = Guid.NewGuid().ToString();
+
+        [Inject] public IJSRuntime JSRuntime { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        [Parameter] public string Title { get; set; }
+        [Parameter] public string Description { get; set; }
+        [Parameter] public string CodeSampleContentForCopy { get; set; }
+        [Parameter] public RenderFragment SampleContent { get; set; }
+        [Parameter] public RenderFragment CodeSampleContent { get; set; }
+
+        private async Task CopyCodeToClipboard()
+        {
+            await JSRuntime.CopyToClipboard(CodeSampleContentForCopy);
+        }
+
+        private async Task CopyLinkToClipboard()
+        {
+            var currentUrl = NavigationManager.Uri.Replace(NavigationManager.BaseUri, "/", StringComparison.Ordinal);
+            var exampleUrl = $"{currentUrl}#{exampleId}";
+            await JSRuntime.CopyToClipboard(exampleUrl);
+        }
+    }
+}
