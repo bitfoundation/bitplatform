9d1cc07fede77ee9a412d4328d0dd5e062a88ed5
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2021-12-13 21:46:08 +0330

    Improve logic BitDatePicker

--- src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Pickers/BitDatePickerTests.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/Pickers/BitDatePickerTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Linq;
-using AngleSharp.Dom;
+using System.Globalization;
+using Bit.Client.Web.BlazorUI.Components.Helper;
 using Bunit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -18,7 +18,7 @@ namespace Bit.Client.Web.BlazorUI.Tests.Pickers
 
           DataRow(Visual.Material, true),
           DataRow(Visual.Material, false)
-      ]
+        ]
         public void BitDatePickerShouldTakeCorrectVisual(Visual visual, bool isEnabled)
         {
             var component = RenderComponent<BitDatePickerTest>(
@@ -53,7 +53,7 @@ namespace Bit.Client.Web.BlazorUI.Tests.Pickers
         [DataTestMethod,
           DataRow(true, 1),
           DataRow(false, 0)
-      ]
+        ]
         public void BitDatePickerShouldHandleOnClickEvent(bool isEnabled, int count)
         {
             Context.JSInterop.Mode = JSRuntimeMode.Loose;
@@ -72,8 +72,8 @@ namespace Bit.Client.Web.BlazorUI.Tests.Pickers
         [DataTestMethod,
           DataRow(true, 1),
           DataRow(false, 0)
-      ]
-        public void BitDatePickerCalendarItemsShouldRespectIsEnabled(bool isEnabled,int count)
+        ]
+        public void BitDatePickerCalendarItemsShouldRespectIsEnabled(bool isEnabled, int count)
         {
             Context.JSInterop.Mode = JSRuntimeMode.Loose;
             var component = RenderComponent<BitDatePickerTest>(
@@ -82,13 +82,54 @@ namespace Bit.Client.Web.BlazorUI.Tests.Pickers
                    parameters.Add(p => p.IsOpen, true);
                    parameters.Add(p => p.IsEnabled, isEnabled);
                });
-          
+
             var dateItems = component.FindAll(".day-btn");
-            
+
             Random random = new();
-            int randomNumber = random.Next(0, dateItems.Count -1);
+            int randomNumber = random.Next(0, dateItems.Count - 1);
             dateItems[randomNumber].Click();
             Assert.AreEqual(component.Instance.SelectedDateValue, count);
+        }
+
+        [DataTestMethod]
+        public void BitDatePickerCalendarSelectTodayDate()
+        {
+            Context.JSInterop.Mode = JSRuntimeMode.Loose;
+            var component = RenderComponent<BitDatePicker>(
+               parameters =>
+               {
+                   parameters.Add(p => p.IsOpen, true);
+                   parameters.Add(p => p.IsEnabled, true);
+               });
+
+            Assert.IsNull(component.Instance.Value);
+
+            var today = component.Find(".date-cell--today button.day-btn");
+            today.Click();
+
+            Assert.IsNotNull(component.Instance.Value);
+            Assert.AreEqual(component.Instance.Value.Value.Date, DateTimeOffset.Now.Date);
+            Assert.AreEqual(component.Instance.Value.Value.Offset, DateTimeOffset.Now.Offset);
+        }
+
+        [DataTestMethod]
+        public void BitDatePickerCalendarWithCustomCultureInfo()
+        {
+            Context.JSInterop.Mode = JSRuntimeMode.Loose;
+            var component = RenderComponent<BitDatePickerTest>(
+               parameters =>
+               {
+                   parameters.Add(p => p.IsOpen, true);
+                   parameters.Add(p => p.Culture, CultureInfoHelper.GetPersianCaltureByFinglishNames());
+               });
+
+            var monthButtons = component.FindAll(".month-picker-wrapper .grid-container .btn-row button");
+
+            var index = 0;
+            foreach (var button in monthButtons)
+            {
+                Assert.AreEqual(button.FirstElementChild.TextContent, component.Instance.Culture.DateTimeFormat.AbbreviatedMonthNames[index++]);
+            }
         }
     }
 }
