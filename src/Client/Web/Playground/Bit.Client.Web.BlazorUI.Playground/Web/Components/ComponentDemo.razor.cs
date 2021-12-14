71580401ff4e2a3c2a62ffd8d3ed94f92f643be4
Maryam Heydarian <maryamhdr76@gmail.com>
2021-12-14 18:43:25 +0330

    modify date picker demo page #1165

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Components/ComponentDemo.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Components/ComponentDemo.razor.cs
@@ -28,7 +28,6 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Components
 
         [Parameter] public string ComponentName { get; set; }
         [Parameter] public string ComponentDescription { get; set; }
-        [Parameter] public string OverviewDesc { get; set; }
         [Parameter] public RenderFragment ChildContent { get; set; }
         [Parameter] public List<EnumParameter> EnumParameters { get; set; } = new();
         [Parameter] public List<ComponentParameter> ComponentParameters { get; set; } = new();
