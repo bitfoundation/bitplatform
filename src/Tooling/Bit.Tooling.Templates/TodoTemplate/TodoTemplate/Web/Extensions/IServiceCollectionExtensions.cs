92925cf731d7befef87bf6cba7125b66c5bf7387
sajad kardel <sajadkardel@gmail.com>
2021-12-27 17:51:21 +0330

    remove contact viewer business code

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/TodoTemplate/Web/Extensions/IServiceCollectionExtensions.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/TodoTemplate/Web/Extensions/IServiceCollectionExtensions.cs
@@ -7,18 +7,6 @@ public static class IServiceCollectionExtensions
         services.AddScoped<IToastService, ToastService>();
 
 
-#if Android
-        services.AddSingleton<IContactsService, TodoTemplate.App.Platforms.Android.Implementations.ContactsService>();
-#elif iOS
-        services.AddSingleton<IContactsService, TodoTemplate.App.Platforms.iOS.Implementations.ContactsService>();
-#elif Windows
-        services.AddSingleton<IContactsService, TodoTemplate.App.Platforms.Windows.Implementations.ContactsService>();
-#elif Mac
-        services.AddSingleton<IContactsService, TodoTemplate.App.Platforms.MacCatalyst.Implementations.ContactsService>();
-#else
-        services.AddSingleton<IContactsService, TodoTemplate.App.Platforms.Web.Implementations.ContactsService>();
-#endif
-
         return services;
     }
 }
--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/TodoTemplate/Web/Pages/Index.razor.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/TodoTemplate/Web/Pages/Index.razor.cs
@@ -2,25 +2,5 @@
 
 public partial class Index
 {
-    [Inject] public IContactsService ContactsService { get; set; }
-    [Inject] public IToastService ToastService { get; set; }
-
-    public List<ContactInfo> Contacts { get; set; }
-
-    public async Task Reload()
-    {
-        Contacts = await ContactsService.GetContacts();
-        await ToastService.ShowToast("Done!");
-    }
-
-    protected async override Task OnInitializedAsync()
-    {
-#if DEBUG
-        await Task.Delay(TimeSpan.FromSeconds(2));
-#endif
-
-        Contacts = await ContactsService.GetContacts();
-
-        await base.OnInitializedAsync();
-    }
+   
 }
