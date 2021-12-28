52e8a440b2c2bf2511e02f6494ad12de71141e8b
ysmoradi <ysmoradi@outlook.com>
2021-12-28 21:26:38 +0330

    Refactor components gallery project. This closes #1241

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/App/mklink.bat
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/App/mklink.bat
@@ -6,6 +6,11 @@ rd Scripts
 rd Services
 rd Shared
 rd Styles
+del compilerconfig.json
+del compilerconfig.json.defaults
+del _Imports.razor
+del Main.razor
+del appsettings.json
 mklink /j "Components" "../Web/Components"
 mklink /j "Extensions" "../Web/Extensions"
 mklink /j "Models" "../Web/Models"
@@ -14,6 +19,11 @@ mklink /j "Scripts" "../Web/Scripts"
 mklink /j "Services" "../Web/Services"
 mklink /j "Shared" "../Web/Shared"
 mklink /j "Styles" "../Web/Styles"
+mklink /h "compilerconfig.json" "../Web/compilerconfig.json"
+mklink /h "compilerconfig.json.defaults" "../Web/compilerconfig.json.defaults"
+mklink /h "_Imports.razor" "../Web/_Imports.razor"
+mklink /h "Main.razor" "../Web/App.razor"
+mklink /h "appsettings.json" "../Web/appsettings.json"
 cd wwwroot
 rd fonts
 rd images
