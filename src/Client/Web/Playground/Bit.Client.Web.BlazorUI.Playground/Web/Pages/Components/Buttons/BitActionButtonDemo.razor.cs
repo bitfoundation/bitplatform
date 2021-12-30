220bef169f4cf66d1036af6d386cbf486b6a72ff
Maryam Heydarian <maryamhdr76@gmail.com>
2021-12-30 14:09:18 +0330

    fix issue #1206

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Buttons/BitActionButtonDemo.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Buttons/BitActionButtonDemo.razor.cs
@@ -90,7 +90,7 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Buttons
                  $"</style>";
 
         private readonly string visibilitySampleCode = $"<BitActionButton Visibility='BitComponentVisibility.Visible' IconName='BitIcon.AddEvent'>Add Event</BitActionButton>{Environment.NewLine}" +
-                 $"<BitActionButton Hidden='ComponentHidden.Hidden'>Hidden Action Button</BitActionButton>{Environment.NewLine}" +
+                 $"<BitActionButton Visibility='BitComponentVisibility.Hidden'>Hidden Action Button</BitActionButton>{Environment.NewLine}" +
                  $"<BitActionButton Visibility='BitComponentVisibility.Collapsed'>Collapsed Action Button</BitActionButton>";
 
         private readonly string ariasSampleCode = $"<BitActionButton AriaDescription='Detailed description used for screen reader.'  IconName='BitIcon.Library'>Action Button with Aria Description</BitActionButton>{Environment.NewLine}" +
