a8b6e87fa089e8e3895393502ade62b52e81f4d3
Yaser Moradi <ysmoradi@outlook.com>
2022-01-06 19:22:21 +0330

    Enable nullable ref types for project template. This closes #1298 (#1301)

---
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Api/Data/Models/Account/User.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoTemplate.Api.Data.Models.Account;
+
+public class User : IdentityUser<int>
+{
+    public override int Id { get; set; }
+    public override string? UserName { get; set; }
+    public override string? NormalizedUserName { get; set; }
+    public override string? Email { get; set; }
+    public override string? NormalizedEmail { get; set; }
+    public override bool EmailConfirmed { get; set; }
+    public override string? PasswordHash { get; set; }
+    public override string? SecurityStamp { get; set; }
+    public override string? ConcurrencyStamp { get; set; }
+    public override string? PhoneNumber { get; set; }
+    public override bool PhoneNumberConfirmed { get; set; }
+    public override bool TwoFactorEnabled { get; set; }
+    public override DateTimeOffset? LockoutEnd { get; set; }
+    public override bool LockoutEnabled { get; set; }
+    public override int AccessFailedCount { get; set; }
+}
