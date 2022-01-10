ecc93c8d89ac2aa8bcbb8f095aa251b988db0e08
Maryam Heydarian <maryamhdr76@gmail.com>
2022-01-10 16:45:32 +0330

    imlement nav menu UI #1264

---
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Components/NavMenu.razor.cs
@@ -0,0 +1,31 @@
+﻿using Bit.Client.Web.BlazorUI;
+
+namespace TodoTemplate.App.Components
+{
+    public partial class NavMenu
+    {
+        private readonly List<BitNavLinkItem> NavLinks = new()
+        {
+            new BitNavLinkItem
+            {
+                Name = "Home",
+                Url = "/",
+                Icon = "Home",
+                Key = "Home"
+            },
+            new BitNavLinkItem
+            {
+                Name = "To do",
+                Url = "/to-do",
+                Icon = "TaskLogo",
+                Key = "ToDo"
+            },
+            new BitNavLinkItem
+            {
+                Name = "Log out",
+                Icon = "ReleaseGate",
+                Key = "LogOut"
+            }
+        };
+    }
+}
