77061bd2336fda3f891a7905cce59ec842016654
Saeed <61748672+saeedhalab@users.noreply.github.com>
2022-01-25 18:02:16 +0330

    Implement Todo Page UI #1270 (#1354)

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Pages/Todo.razor.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Pages/Todo.razor.cs
@@ -2,4 +2,21 @@
 
 public partial class Todo
 {
+    List<TodoModel> todolist = new();
+    protected override void OnInitialized()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            todolist.Add(new TodoModel
+            {
+                Date = "January 3, 2022, Monday",
+                Title = "Project name"
+            });
+        }
+    }
+}
+public class TodoModel
+{
+    public string? Title { get; set; }
+    public string? Date { get; set; }
 }
