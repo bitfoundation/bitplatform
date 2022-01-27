8b13c2a356df9335ec0bcded0337beb8225ee0ec
sajad kardel <sajadkardel@gmail.com>
2022-01-27 15:19:55 +0330

    add some functionality on SignUp Page

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Pages/SignUp.razor.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Pages/SignUp.razor.cs
@@ -1,7 +1,28 @@
-﻿namespace TodoTemplate.App.Pages;
+﻿using TodoTemplate.Shared.Dtos.Account;
+
+namespace TodoTemplate.App.Pages;
 
 public partial class SignUp
 {
+    public string? Email { get; set; }
 
+    public string? Password { get; set; }
+
+    public bool IsAcceptPrivacy { get; set; }
+
+    [Inject]
+    public HttpClient HttpClient { get; set; } = default!;
+
+    private async Task OnClickSignUp()
+    {
+        var response = await HttpClient.PostAsync("User/Create",JsonContent.Create(new UserDto
+        {
+            UserName = Email,
+            Email = Email,
+            Password = Password
+        }));
+
+        var user = await response.Content.ReadFromJsonAsync<RoleDto>();
+    }
 }
 
