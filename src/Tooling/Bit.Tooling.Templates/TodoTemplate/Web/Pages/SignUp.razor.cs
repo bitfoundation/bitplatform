b0f5ef6003953f5a18a1f03b36c27f8f617867dd
sajad kardel <sajadkardel@gmail.com>
2022-01-28 17:40:47 +0330

    End of Initial implementation SignUp  page Functionality #1278

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Pages/SignUp.razor.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Pages/SignUp.razor.cs
@@ -1,28 +1,41 @@
 ﻿using TodoTemplate.Shared.Dtos.Account;
-
 namespace TodoTemplate.App.Pages;
 
 public partial class SignUp
 {
     public string? Email { get; set; }
 
+    public string? EmailError { get; set; }
+
     public string? Password { get; set; }
 
+    public string? PasswordError { get; set; }
+
     public bool IsAcceptPrivacy { get; set; }
+
+    public bool HasMessageBar { get; set; }
+
+    public HttpResponseMessage OnClickSignUpResponse { get; set; } = new();
 
     [Inject]
     public HttpClient HttpClient { get; set; } = default!;
 
     private async Task OnClickSignUp()
     {
-        var response = await HttpClient.PostAsync("User/Create",JsonContent.Create(new UserDto
+        EmailError = Email is null ? "Please enter your email" : null;
+
+        PasswordError = Password is null ? "Please enter your password" : null;
+
+        if (EmailError is not null || PasswordError is not null) return;
+
+        OnClickSignUpResponse = await HttpClient.PostAsync("User/Create", JsonContent.Create(new UserDto
         {
             UserName = Email,
             Email = Email,
             Password = Password
         }));
 
-        var user = await response.Content.ReadFromJsonAsync<RoleDto>();
+        HasMessageBar = true;
     }
 }
 
