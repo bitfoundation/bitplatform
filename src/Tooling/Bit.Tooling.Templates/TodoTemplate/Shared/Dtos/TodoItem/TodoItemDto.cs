b4a20725a4c4f32ab5294be2ae2104b36bcae91d
SaeedHalab <Halab.saeed@gmail.com>
2022-01-30 19:54:39 +0330

    Added Id To TodoItemDto #1360

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Shared/Dtos/TodoItem/TodoItemDto.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Shared/Dtos/TodoItem/TodoItemDto.cs
@@ -3,8 +3,9 @@ namespace TodoTemplate.Shared.Dtos.TodoItem
 {
     public class TodoItemDto
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public DateTime Date { get; set; }
-        public bool IsDone{ get; set; }
+        public bool IsDone { get; set; }
     }
 }
