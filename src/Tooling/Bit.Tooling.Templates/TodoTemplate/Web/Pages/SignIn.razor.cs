4612361bc361113068cf71044079132bd2009830
SaeedHalab <Halab.saeed@gmail.com>
2022-02-07 14:48:48 +0330

    Implement  user sign in process #1282

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Pages/SignIn.razor.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Pages/SignIn.razor.cs
@@ -1,7 +1,52 @@
-﻿namespace TodoTemplate.App.Pages;
+﻿using TodoTemplate.Shared.Dtos.Account;
+namespace TodoTemplate.App.Pages;
 
 public partial class SignIn
 {
+    public string? Email { get; set; }
 
+    public string? EmailError { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? PasswordError { get; set; }
+
+    public bool HasMessageBar { get; set; }
+
+    public bool IsSuccessMessageBar { get; set; }
+
+    public string? MessageBarText { get; set; }
+
+    [Inject]
+    public HttpClient HttpClient { get; set; } = default!;
+
+    [Inject]
+    public ITodoTemplateAuthenticationService TodoTemplateAuthenticationService { get; set; } = default!;
+
+    private async Task OnClickSignIn()
+    {
+        EmailError = string.IsNullOrEmpty(Email) ? "Please enter your email" : null;
+        PasswordError = string.IsNullOrEmpty(Password) ? "Please enter your password" : null;
+
+        if (EmailError is not null || PasswordError is not null)
+            return;
+
+        try
+        {
+            await TodoTemplateAuthenticationService.SignIn(new RequestTokenDto
+            {
+                UserName = Email,
+                Password = Password
+            });
+
+            IsSuccessMessageBar = true;
+            MessageBarText = "Sign-up successfully";
+        }
+        catch (Exception e)
+        {
+            MessageBarText = e.Message;
+        }
+        HasMessageBar = true;
+    }
 }
 
