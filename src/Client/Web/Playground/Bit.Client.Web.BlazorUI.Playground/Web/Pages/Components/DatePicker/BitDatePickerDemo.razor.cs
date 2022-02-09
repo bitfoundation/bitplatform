4f1a854a526e534bc9347b384fe14aafce90d7e9
SaeedHalab <Halab.saeed@gmail.com>
2022-02-09 18:35:08 +0330

    Implement Visiblity data in Parameter Section Demo Page for BitDatePicker#1141

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/DatePicker/BitDatePickerDemo.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/DatePicker/BitDatePickerDemo.razor.cs
@@ -160,6 +160,15 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.DatePicker
                 DefaultValue = "",
                 Description = "Callback for when DatePicker value changed.",
             },
+            new ComponentParameter()
+            {
+                Name = "Visibility",
+                Type = "BitComponentVisibility",
+                LinkType = LinkType.Link,
+                Href = "#component-visibility-enum",
+                DefaultValue = "BitComponentVisibility.Visible",
+                Description = "Whether the component is Visible,Hidden,Collapsed.",
+            },
         };
 
         private readonly List<EnumParameter> enumParameters = new()
@@ -182,6 +191,33 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.DatePicker
                         Name= "Persian",
                         Description="Show DatePicker in Persian calendar.",
                         Value="1",
+                    }
+                }
+            },
+            new EnumParameter()
+            {
+                Id = "component-visibility-enum",
+                Title = "BitComponentVisibility Enum",
+                Description = "",
+                EnumList = new List<EnumItem>()
+                {
+                    new EnumItem()
+                    {
+                        Name= "Visible",
+                        Description="Show content of the component.",
+                        Value="0",
+                    },
+                    new EnumItem()
+                    {
+                        Name= "Hidden",
+                        Description="Hide content of the component,though the space it takes on the page remains.",
+                        Value="1",
+                    },
+                    new EnumItem()
+                    {
+                        Name= "Collapsed",
+                        Description="Hide content of the component,though the space it takes on the page gone.",
+                        Value="2",
                     }
                 }
             }
