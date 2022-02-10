d7adeb03ea918e1d16a1b4b549aa7941a075eae3
Z_Shobbar <zoha.shobbar@outlook.com>
2022-02-10 20:10:20 +0330

    fix selected key issue #1392

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/ChoiceGroup/BitChoiceGroup.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/ChoiceGroup/BitChoiceGroup.razor.cs
@@ -54,6 +54,7 @@ namespace Bit.Client.Web.BlazorUI
             set
             {
                 if (value == selectedKey) return;
+                selectedKey = value;
                 SelectOptionByKey(value);
             }
         }
