0238c723f8d24958cba89bb2ba50055d9d6b0c28
sajad kardel <sajadkardel@gmail.com>
2022-02-13 20:29:40 +0330

    display the user info in the menu #1286

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Components/NavMenu.razor.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Components/NavMenu.razor.cs
@@ -1,4 +1,6 @@
-﻿namespace TodoTemplate.App.Components;
+﻿using TodoTemplate.Shared.Dtos.Account;
+
+namespace TodoTemplate.App.Components;
 
 public partial class NavMenu
 {
@@ -37,4 +39,19 @@ public partial class NavMenu
     }
 
     public List<BitNavLinkItem> NavLinks { get; set; }
+
+    public UserDto? User { get; set; } = new();
+
+    [Inject]
+    public HttpClient HttpClient { get; set; } = default!;
+
+    [Inject]
+    public IStateService StateService { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        User = await StateService.GetValue(nameof(User), async () => await HttpClient.GetFromJsonAsync($"User/GetCurrentUser", ToDoTemplateJsonContext.Default.UserDto));
+
+        await base.OnInitializedAsync();
+    }
 }
