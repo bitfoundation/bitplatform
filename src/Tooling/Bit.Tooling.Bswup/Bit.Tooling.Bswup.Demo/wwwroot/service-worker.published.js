623d70ff83dc4671c1a81cf52ecb79a30fcbf650
Saleh Yusefnejad <msynk@outlook.com>
2022-02-17 18:25:14 +0330

    add assetsUrl setting (#1467)

--- src/Tooling/Bit.Tooling.Bswup/Bit.Tooling.Bswup.Demo/wwwroot/service-worker.published.js
+++ src/Tooling/Bit.Tooling.Bswup/Bit.Tooling.Bswup.Demo/wwwroot/service-worker.published.js
@@ -3,6 +3,7 @@ self.assetsInclude = [];
 self.assetsExclude = [/\.scp\.css$/, /weather\.json$/];
 self.defaultUrl = "index.html";
 self.prohibitedUrls = [];
+self.assetsUrl = '/service-worker-assets.js';
 
 self.importScripts('_content/Bit.Tooling.Bswup/bit-bswup.sw.js');
 
