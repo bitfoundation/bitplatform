b040d806b3e9cdf537d1610a5e4d90f673546dd5
Saleh Yusefnejad <msynk@outlook.com>
2022-02-19 20:59:21 +0330

    add externalAssets setting #1462 (#1474)

--- src/Tooling/Bit.Tooling.Bswup/Bit.Tooling.Bswup.Demo/wwwroot/service-worker.js
+++ src/Tooling/Bit.Tooling.Bswup/Bit.Tooling.Bswup.Demo/wwwroot/service-worker.js
@@ -9,4 +9,17 @@ self.defaultUrl = 'index.html';
 self.prohibitedUrls = [];
 self.assetsUrl = '/service-worker-assets.js';
 
+// more about SRI (Subresource Integrity) here: https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity
+// online tool to generate integrity hash: https://www.srihash.org/   or   https://laysent.github.io/sri-hash-generator/
+// using only js to generate hash: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+self.externalAssets = [
+    {
+        "hash": "sha256-lDAEEaul32OkTANWkZgjgs4sFCsMdLsR5NJxrjVcXdo=",
+        "url": "css/app.css"
+    },
+    {
+        "url": "/"
+    },
+];
+
 self.importScripts('_content/Bit.Tooling.Bswup/bit-bswup.sw.js');
