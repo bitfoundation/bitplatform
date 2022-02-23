2b2187a4f1d4975c6a0be64d00a219d5f06d5831
Maryam Heydarian <maryamhdr76@gmail.com>
2022-02-23 17:27:19 +0330

    implement toggle menu funct #1312

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Components/Header.razor.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Components/Header.razor.cs
@@ -2,7 +2,7 @@
 
 public partial class Header : IAsyncDisposable
 {
-    public bool IsUserAuthenticated { get; set; }
+    [Parameter] public EventCallback MenuToggled { get; set; }
 
     [Inject]
     public IStateService StateService { get; set; } = default!;
@@ -12,6 +12,8 @@ public partial class Header : IAsyncDisposable
 
     [Inject]
     public IExceptionHandler ExceptionHandler { get; set; } = default!;
+
+    public bool IsUserAuthenticated { get; set; }
 
     protected async override Task OnInitAsync()
     {
@@ -36,6 +38,11 @@ public partial class Header : IAsyncDisposable
         {
             StateHasChanged();
         }
+    }
+
+    private async Task ToggleMenu()
+    {
+        await MenuToggled.InvokeAsync();
     }
 
     public async ValueTask DisposeAsync()
