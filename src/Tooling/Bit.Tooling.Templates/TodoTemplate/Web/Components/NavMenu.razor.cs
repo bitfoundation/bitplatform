bc47ce1063c49631754368016c0e44806df67218
Sajad Kardel <sajadkardel@gmail.com>
2022-03-02 19:36:43 +0330

    fix the user info in the nav menu #1538 (#1564)

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Components/NavMenu.razor.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Components/NavMenu.razor.cs
@@ -1,34 +1,17 @@
-﻿namespace TodoTemplate.App.Components;
+﻿using TodoTemplate.Shared.Dtos.Account;
+
+namespace TodoTemplate.App.Components;
 
 public partial class NavMenu
 {
     private bool IsMenuOpenHasBeenSet;
     private bool isMenuOpen;
 
-    [CascadingParameter]
-    public Task<AuthenticationState> AuthenticationStateTask { get; set; } = default!;
-
-    [Parameter] 
-    public bool IsMenuOpen
-    {
-        get => isMenuOpen;
-        set
-        {
-            if (value == isMenuOpen) return;
-            isMenuOpen = value;
-            _ = IsMenuOpenChanged.InvokeAsync(value);
-        }
-    }
-
-    [Parameter] public EventCallback<bool> IsMenuOpenChanged { get; set; }
-
-    [Inject] public HttpClient HttpClient { get; set; } = default!;
-
-    [Inject] public IStateService StateService { get; set; } = default!;
-
     public List<BitNavLinkItem> NavLinks { get; set; }
 
-    public string? UserName { get; set; }
+    public UserDto? User { get; set; } = new();
+
+    public string? ProfileImageUrl { get; set; }
 
     public bool IsSignOutModalOpen { get; set; }
 
@@ -64,6 +47,33 @@ public partial class NavMenu
         };
     }
 
+    [CascadingParameter]
+    public Task<AuthenticationState> AuthenticationStateTask { get; set; } = default!;
+
+    [Parameter] 
+    public bool IsMenuOpen
+    {
+        get => isMenuOpen;
+        set
+        {
+            if (value == isMenuOpen) return;
+            isMenuOpen = value;
+            _ = IsMenuOpenChanged.InvokeAsync(value);
+        }
+    }
+
+    [Parameter] public EventCallback<bool> IsMenuOpenChanged { get; set; }
+
+    [Inject] public HttpClient HttpClient { get; set; } = default!;
+
+    [Inject] public IStateService StateService { get; set; } = default!;
+
+    [Inject] public IAuthTokenProvider AuthTokenProvider { get; set; } = default!;
+
+#if BlazorServer || BlazorHybrid
+    [Inject] public IConfiguration Configuration { get; set; } = default!;
+#endif
+
     private void CloseMenu()
     {
         if (IsMenuOpenHasBeenSet && IsMenuOpenChanged.HasDelegate is false) return;
@@ -73,7 +83,18 @@ public partial class NavMenu
 
     protected override async Task OnInitAsync()
     {
-        UserName = await StateService.GetValue(nameof(UserName), async () => (await AuthenticationStateTask).User.GetUserName());
+        User = await StateService.GetValue(nameof(User), async () =>
+            await HttpClient.GetFromJsonAsync("User/GetCurrentUser", ToDoTemplateJsonContext.Default.UserDto));
+
+        var access_token = await StateService.GetValue("access_token", async () =>
+            await AuthTokenProvider.GetAcccessToken());
+
+        ProfileImageUrl = $"api/Attachment/GetProfileImage?access_token={access_token}";
+
+#if BlazorServer || BlazorHybrid
+        var serverUrl = Configuration.GetValue<string>("ApiServerAddress");
+        ProfileImageUrl = $"{serverUrl}{ProfileImageUrl}";
+#endif
 
         await base.OnInitAsync();
     }
