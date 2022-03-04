4343b67e7cce5bfd1b60c8ec0656e5604dd96eba
RezaKargar <r.kargar.2014@gmail.com>
2022-03-04 22:22:12 +0330

    Use WebApplication in Program.cs of Playground.Web
    This resolves #1579 by changing Program.cs of Playground.Web to
    use `WebApplication` rather than `HostBuilder`

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Program.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Program.cs
@@ -1,47 +1,79 @@
-﻿#if BlazorWebAssembly
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.IO.Compression;
+using System.Linq;
 using System.Net.Http;
+#if BlazorWebAssembly
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 #elif BlazorServer
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 #endif
-using System;
-using System.Threading.Tasks;
-
-namespace Bit.Client.Web.BlazorUI.Playground.Web
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-#if BlazorWebAssembly || BlazorServer
-            await CreateHostBuilder(args)
-                .Build()
-                .RunAsync();
-#else
-            Console.WriteLine("You're in blazor hybrid mode, please run app project isntead of web project.");
-#endif
-        }
+using Microsoft.Extensions.DependencyInjection;
 
 #if BlazorWebAssembly
-        public static WebAssemblyHostBuilder CreateHostBuilder(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault();
+        
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            new Startup().ConfigureServices(builder.Services);
+builder.Services.AddPlaygroundServices();
 
-            return builder;
-        }
+var app = builder.Build();
+await app.RunAsync();
+
 #elif BlazorServer
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-#endif
-    }
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddPlaygroundServices();
+
+builder.Services.AddHttpClient("ApiHttpClient", (serviceProvider, httpClient) =>
+{
+    httpClient.BaseAddress = new Uri(serviceProvider.GetRequiredService<IConfiguration>()["ApiServerAddress"]);
+});
+builder.Services.AddTransient(c => c.GetRequiredService<IHttpClientFactory>().CreateClient("ApiHttpClient"));
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddResponseCompression(opts =>
+    {
+        opts.EnableForHttps = true;
+        opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Where(m => m != "text/html").Concat(new[] { "application/octet-stream" }).ToArray();
+        opts.Providers.Add<BrotliCompressionProvider>();
+        opts.Providers.Add<GzipCompressionProvider>();
+    })
+    .Configure<BrotliCompressionProviderOptions>(opt => opt.Level = CompressionLevel.Fastest)
+    .Configure<GzipCompressionProviderOptions>(opt => opt.Level = CompressionLevel.Fastest);
+
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseHttpsRedirection();
+app.UseResponseCompression();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+});
+
+await app.RunAsync();
+
+#else
+
+Console.WriteLine("You're in blazor hybrid mode, please run app project isntead of web project.");
+
+#endif
