bac594d44cb8ef9ffce56edcced11be590a145b7
Saleh Yusefnejad <msynk@outlook.com>
2022-03-09 18:58:37 +0330

    update BitFileUpload demo page

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/FileUpload/BitFileUploadStatus.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/FileUpload/BitFileUploadStatus.cs
@@ -3,47 +3,47 @@
     public enum BitFileUploadStatus
     {
         /// <summary>
-        /// File uploading progress is pended because the server cannot be contacted
+        /// File uploading progress is pended because the server cannot be contacted.
         /// </summary>
         Pending,
 
         /// <summary>
-        /// File uploading is in progress
+        /// File uploading is in progress.
         /// </summary>
         InProgress,
 
         /// <summary>
-        /// File uploading progress is paused by the user
+        /// File uploading progress is paused by the user.
         /// </summary>
         Paused,
 
         /// <summary>
-        /// File uploading progress is canceled by the user
+        /// File uploading progress is canceled by the user.
         /// </summary>
         Canceled,
 
         /// <summary>
-        /// The file is successfully uploaded
+        /// The file is successfully uploaded.
         /// </summary>
         Completed,
 
         /// <summary>
-        /// The file has a problem and progress is failed
+        /// The file has a problem and progress is failed.
         /// </summary>
         Failed,
 
         /// <summary>
-        /// The uploaded file removed by the user
+        /// The uploaded file removed by the user.
         /// </summary>
         Removed,
 
         /// <summary>
-        /// The file removal failed
+        /// The file removal failed.
         /// </summary>
         RemoveFailed,
 
         /// <summary>
-        /// The type of uploaded file is not allowed
+        /// The type of uploaded file is not allowed.
         /// </summary>
         NotAllowed
     }
