c71905027545852b4a9bdca2f1244c68b3ba29ab
ysmoradi <ysmoradi@outlook.com>
2022-03-10 11:35:31 +0330

    Updated nuget packages

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/wwwroot/service-worker.js
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/wwwroot/service-worker.js
@@ -10,9 +10,19 @@ self.assetsExclude = [
     /bit\.blazorui\.fluent\.min\.css$/,
     /bit\.blazorui\.fluent\.css$/,
     /bit\.blazorui\.material\.min\.css$/,
-    /bit\.blazorui\.material\.css$/
+    /bit\.blazorui\.material\.css$/,
+    /todoTemplate\.es5\.js/,
+    /app\.css/
+];
+
+self.externalAssets = [
+    {
+        "url": "/"
+    },
 ];
 
 self.defaultUrl = "/";
+
+self.caseInsensitiveUrl = true;
 
 self.importScripts('_content/Bit.Tooling.Bswup/bit-bswup.sw.js');
