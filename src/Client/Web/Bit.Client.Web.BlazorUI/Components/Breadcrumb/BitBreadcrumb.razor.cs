50cdfe6ce19431905c5700bfe7a84f30f197a770
Marzie Sangtarash <m.sangtarash@outlook.com>
2022-03-13 19:58:32 +0330

    Fix functionality of MaxDisplayedItems and overflow properties in Breadcrumb #1588 (#1661)

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/Breadcrumb/BitBreadcrumb.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Breadcrumb/BitBreadcrumb.razor.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,17 +10,17 @@ namespace Bit.Client.Web.BlazorUI
     public partial class BitBreadcrumb
     {
         protected override string RootElementClass => "bit-brc";
-        
+
         /// <summary>
         /// Collection of breadcrumbs to render
         /// </summary>
-        [Parameter] public List<BitBreadcrumbItem> Items { get; set; } = new List<BitBreadcrumbItem>();
-        
+        [Parameter] public List<BitBreadcrumbItem> Items { get; set; } = new();
+
         /// <summary>
         /// The maximum number of breadcrumbs to display before coalescing.
         /// If not specified, all breadcrumbs will be rendered.
         /// </summary>
-        [Parameter] public byte? MaxDisplayedItems { get; set; }
+        [Parameter] public int MaxDisplayedItems { get; set; }
 
         /// <summary>
         /// Aria label for the overflow button.
@@ -28,13 +30,47 @@ namespace Bit.Client.Web.BlazorUI
         /// <summary>
         /// Optional index where overflow items will be collapsed.
         /// </summary>
-        [Parameter] public byte? OverflowIndex { get; set; }
-        
+        [Parameter] public int OverflowIndex { get; set; }
+
         /// <summary>
         /// Render a custom divider in place of the default chevron >
         /// </summary>
         [Parameter] public string DividerAs { get; set; } = "bit-icon--ChevronRight";
 
         [Parameter] public string OnRenderOverflowIcon { get; set; } = "bit-icon--More";
+
+
+        private List<BitBreadcrumbItem> _overflowItems = new();
+        private List<BitBreadcrumbItem> _itemsToShowInBreadcrumb = new();
+
+        private List<BitBreadcrumbItem> GetBreadcrumbItemsToShow()
+        {
+            if (MaxDisplayedItems == 0 || MaxDisplayedItems >= Items.Count)
+            {
+                return _itemsToShowInBreadcrumb = Items;
+            }
+
+            if (OverflowIndex >= MaxDisplayedItems)
+                OverflowIndex = 0;
+
+            var overflowItemsCount = Items.Count - MaxDisplayedItems;
+
+            foreach ((BitBreadcrumbItem item, int index) item in Items.Select((item, index) => (item, index)))
+            {
+                if (OverflowIndex <= item.index && item.index < overflowItemsCount + OverflowIndex)
+                {
+                    if (item.index == OverflowIndex)
+                        _itemsToShowInBreadcrumb.Add(item.item);
+
+                    _overflowItems.Add(item.item);
+                }
+                else
+                {
+                    _itemsToShowInBreadcrumb.Add(item.item);
+                }
+            }
+
+            return _itemsToShowInBreadcrumb;
+        }
     }
 }
