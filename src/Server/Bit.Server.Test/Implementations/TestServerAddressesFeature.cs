84979f53c26d699e1b10c00f8d7919afd3b68aaf
Yaser Moradi <ysmoradi@outlook.com>
2022-03-25 00:08:13 +0430

    server address feature in fake server impl + test #1547 (#1759)

---
+++ src/Server/Bit.Server.Test/Implementations/TestServerAddressesFeature.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+
+namespace Bit.Test.Implementations
+{
+    public class TestServerAddressesFeature : IServerAddressesFeature
+    {
+        private readonly string _hostUri;
+
+        public TestServerAddressesFeature(string hostUri)
+        {
+            _hostUri = hostUri;
+        }
+
+        public ICollection<string> Addresses => new[] { _hostUri };
+
+        public bool PreferHostingUrls { get; set; } = true;
+    }
+}
