12e1d98e65107721ff1bf36874b92e6d8eb3323d
Maryam Heydarian <maryamhdr76@gmail.com>
2022-03-30 00:13:42 +0430

    create icon page #1412

---
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/IconPage.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages
+{
+    public partial class IconPage
+    {
+        private List<string> FilteredIcons;
+
+        protected override void OnInitialized()
+        {
+            FilteredIcons = Enum.GetValues(typeof(BitIconName))
+                .Cast<BitIconName>()
+                .Select(v => v.GetName())
+                .ToList();
+            base.OnInitialized();
+        }
+
+        private void SearchIcon()
+        {
+
+        }
+    }
+}
