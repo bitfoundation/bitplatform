68d51c24d44c99d6ec5fb28cc39fbc0a443f4456
sajad kardel <sajadkardel@gmail.com>
2022-04-04 05:44:17 +0430

    Add validation for SignIn #1792

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Pages/SignIn.razor.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Pages/SignIn.razor.cs
@@ -1,22 +1,17 @@
-﻿using TodoTemplate.Shared.Dtos.Account;
+﻿using TodoTemplate.App.Models;
+using TodoTemplate.Shared.Dtos.Account;
 
 namespace TodoTemplate.App.Pages;
 
 public partial class SignIn
 {
-    public string? Email { get; set; }
-    public string? Password { get; set; }
+    public SignInModel SignInModel { get; set; } = new();
 
-    public string? EmailErrorMessage { get; set; }
-    public string? PasswordErrorMessage { get; set; }
-
-    public bool IsSignInButtonEnabled { get; set; }
     public bool IsLoading { get; set; }
 
     public BitMessageBarType SignInMessageType { get; set; }
-    public string? SignInMessage { get; set; }
 
-    [Inject] public HttpClient HttpClient { get; set; } = default!;
+    public string? SignInMessage { get; set; }
 
     [Inject] public NavigationManager NavigationManager { get; set; } = default!;
 
@@ -29,47 +24,17 @@ public partial class SignIn
     [SupplyParameterFromQuery]
     public string? RedirectUrl { get; set; }
 
-    private void CheckSignInButtonEnabled()
-    {
-        if (string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password))
-        {
-            IsSignInButtonEnabled = false;
-            return;
-        }
-        IsSignInButtonEnabled = true;
-    }
-
-    private bool ValidateSignIn()
-    {
-        EmailErrorMessage = string.IsNullOrEmpty(Email)
-            ? "Please enter your email."
-            : null;
-
-        PasswordErrorMessage = string.IsNullOrEmpty(Password)
-            ? "Please enter your password."
-            : null;
-
-        return string.IsNullOrEmpty(EmailErrorMessage) is not false &&
-               string.IsNullOrEmpty(PasswordErrorMessage) is not false;
-    }
-
     private async Task DoSignIn()
-    {
-        if (IsLoading || ValidateSignIn() is false)
-        {
-            return;
-        }
-        
+    {        
         IsLoading = true;
-        IsSignInButtonEnabled = false;
         SignInMessage = null;
         
         try
         {
             await TodoTemplateAuthenticationService.SignIn(new SignInRequestDto
             {
-                UserName = Email, 
-                Password = Password
+                UserName = SignInModel.Email, 
+                Password = SignInModel.Password
             });
 
             NavigationManager.NavigateTo(RedirectUrl ?? "/");
@@ -78,12 +43,11 @@ public partial class SignIn
         {
             SignInMessageType = BitMessageBarType.Error;
 
-            SignInMessage = ErrorStrings.ResourceManager.Translate(e.Message, Email!);
+            SignInMessage = ErrorStrings.ResourceManager.Translate(e.Message, SignInModel.Email!);
         }
         finally
         {
             IsLoading = false;
-            IsSignInButtonEnabled = true;
         }
     }
 
