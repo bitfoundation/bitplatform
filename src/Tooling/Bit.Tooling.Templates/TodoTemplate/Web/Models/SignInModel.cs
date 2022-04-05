09437b2bf60566364c26a332bc89d41f7b32b95e
Sajad Kardel <sajadkardel@gmail.com>
2022-04-05 16:22:42 +0430

    feat(templates): apply bit input components validation in the TodoTemplate project #1792 (#1829)

---
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Models/SignInModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoTemplate.App.Models
+{
+    public class SignInModel
+    {
+        [Required(ErrorMessage = "Please enter your email.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password.")]
+        public string? Password { get; set; }
+    }
+}
