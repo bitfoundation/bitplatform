d145f398857e4084d26d9caf11fb9a67d520e61e
sajad kardel <sajadkardel@gmail.com>
2022-04-07 20:16:26 +0430

    undo BitChoiceGroup.razor.cs #1837

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/ChoiceGroup/BitChoiceGroup.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/ChoiceGroup/BitChoiceGroup.razor.cs
@@ -58,16 +58,6 @@ namespace Bit.Client.Web.BlazorUI
         /// </summary>
         [Parameter] public RenderFragment? LabelFragment { get; set; }
 
-        /// <summary>
-        /// Callback that is called when the ChoiceGroup selected value has changed.
-        /// </summary>
-        [Parameter] public EventCallback<ChangeEventArgs> OnChange { get; set; }
-
-        /// <summary>
-        /// Callback for when focus moves into the ChoiceGroup option
-        /// </summary>
-        [Parameter] public EventCallback<FocusEventArgs> OnFocus { get; set; }
-
         protected override string RootElementClass => "bit-chg";
 
         protected override void RegisterComponentClasses()
