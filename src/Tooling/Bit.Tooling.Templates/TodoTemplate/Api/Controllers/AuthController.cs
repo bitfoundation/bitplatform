41d737d3ae6e22ea6b8638d4120b2716371e48c5
M.Mohabbati <mohabbati@gmail.com>
2022-04-18 11:54:59 +0300

    Added send confirmation email overload #1866

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Api/Controllers/AuthController.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Api/Controllers/AuthController.cs
@@ -71,7 +71,7 @@ public class AuthController : ControllerBase
             throw new ResourceValidationException(result.Errors.Select(e => e.Code).ToArray());
         }
 
-        await SendConfirmationEmail(new() { Email = userToAdd.Email }, cancellationToken);
+        await SendConfirmationEmail(new() { Email = userToAdd.Email }, userToAdd, cancellationToken);
     }
 
     [HttpPost("[action]")]
@@ -79,18 +79,19 @@ public class AuthController : ControllerBase
     {
         var user = await _userManager.FindByEmailAsync(sendConfirmationEmailRequest.Email);
 
+        await SendConfirmationEmail(sendConfirmationEmailRequest, user, cancellationToken);
+    }
+
+    private async Task SendConfirmationEmail(SendConfirmationEmailRequestDto sendConfirmationEmailRequest, User user, CancellationToken cancellationToken)
+    {
         if (user is null)
             throw new BadRequestException(nameof(ErrorStrings.UserNameNotFound));
 
         if (await _userManager.IsEmailConfirmedAsync(user))
-        {
             throw new BadRequestException(nameof(ErrorStrings.EmailAlreadyConfirmed));
-        }
 
         if ((DateTimeOffset.Now - user.ConfirmationEmailRequestedOn) < _appSettings.IdentitySettings.ConfirmationEmailResendDelay)
-        {
             throw new TooManyRequestsExceptions(nameof(ErrorStrings.WaitForConfirmationEmailResendDelay));
-        }
 
         var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
