41cfe0c5673e3e0766ad49e6efe6d69b50c62060
SomayeEbrahimi <s.ebrahimi67@gmail.com>
2022-04-19 22:19:17 +0430

    Add BitRadioButtonList tests (#1389)

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/RadioButtonList/BitRadioButtonListTestModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bit.Client.Web.BlazorUI.Tests.RadioButtonList
+{
+    public class BitRadioButtonListTestModel
+    {
+        [Required]
+        [Range(1,int.MaxValue)]
+        public int Value { get; set; }
+    }
+}
