a40dc770c43716425cd394b85195546e62ff0bc7
Saleh Yusefnejad <msynk@outlook.com>
2022-04-19 12:12:33 +0430

    add TodoTemplateErrorBoundary

---
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Web/Shared/TodoTemplateErrorBoundary.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+
+namespace TodoTemplate.App.Shared;
+
+public class TodoTemplateErrorBoundary : ErrorBoundaryBase
+{
+    [Inject] private IExceptionHandler ExceptionHandler { get; set; } = default!;
+
+    protected override Task OnErrorAsync(Exception exception)
+    {
+        ExceptionHandler.Handle(exception);
+
+        return Task.CompletedTask;
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.AddContent(0, ChildContent);
+    }
+}
