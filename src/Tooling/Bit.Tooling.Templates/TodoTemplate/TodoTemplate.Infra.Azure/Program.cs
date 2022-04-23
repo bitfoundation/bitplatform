f6525122e88921293751e5373dec147c8cb57770
m.sangtarash <m.sangtarash@outlook.com>
2022-04-23 05:41:20 +0430

    Initial infra project for template project

---
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/TodoTemplate.Infra.Azure/Program.cs
@@ -0,0 +1,20 @@
+﻿using Pulumi;
+using TodoTemplate.Infra.Azure;
+
+public class Program
+{
+    static Task<int> Main() => Deployment.RunAsync<StackSelector>();
+}
+
+public class StackSelector : Stack
+{
+    public StackSelector()
+    {
+        string stackName = Deployment.Instance.StackName;
+
+        if (stackName == "cd")
+            new CdStack(); // azure devops agent
+        else
+            new TdStack(); // test / prod
+    }
+}
