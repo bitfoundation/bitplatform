4037c8046823715fc3a9b435e01ac47ab454a1de
saeed halab <halab.saeed@gmail.com>
2022-04-25 16:56:56 +0430

    Add validations  in the Sign-In page

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Shared/Dtos/Account/SignInRequestDto.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Shared/Dtos/Account/SignInRequestDto.cs
@@ -3,8 +3,10 @@
 public class SignInRequestDto
 {
     [Required(ErrorMessage = "Please enter your email.")]
+    [EmailAddress(ErrorMessage = "The email address format is incorrect.")]
     public string? UserName { get; set; }
 
     [Required(ErrorMessage = "Please enter your password.")]
+    [MinLength(6, ErrorMessage = "The password must have at least 6 characters.")]
     public string? Password { get; set; }
 }
