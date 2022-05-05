692c695800e1765e443a37e078a7b8b83794684f
msynk <msynk@outlook.com>
2022-05-05 14:18:42 +0430

    implement the position feature for the BitModal (#1944)

--- src/Client/Web/Bit.Client.Web.BlazorUI/Components/Modal/BitModal.razor.cs
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Modal/BitModal.razor.cs
@@ -24,7 +24,7 @@ namespace Bit.Client.Web.BlazorUI
         /// <summary>
         /// Whether the dialog can be light dismissed by clicking outside the dialog (on the overlay).
         /// </summary>
-        
+
         [Parameter] public bool IsBlocking { get; set; }
 
         /// <summary>
@@ -56,7 +56,12 @@ namespace Bit.Client.Web.BlazorUI
         /// <summary>
         /// A callback function for when the Modal is dismissed light dismiss, before the animation completes.
         /// </summary>
-        [Parameter] public EventCallback<MouseEventArgs> OnDismiss { get; set; }
+        [Parameter] public EventCallback OnDismiss { get; set; }
+
+        /// <summary>
+        /// Position of the modal on the screen.
+        /// </summary>
+        [Parameter] public BitModalPosition Position { get; set; } = BitModalPosition.Center;
 
         /// <summary>
         /// ARIA id for the subtitle of the Modal, if any.
@@ -71,15 +76,31 @@ namespace Bit.Client.Web.BlazorUI
         protected override string RootElementClass => "bit-mdl";
         private bool IsAlertRole { get; set; }
 
-        private void CloseCallout(MouseEventArgs e)
+        private void CloseCallout()
         {
             if (IsOpenHasBeenSet && IsOpenChanged.HasDelegate is false) return;
 
             if (IsBlocking == false)
             {
                 IsOpen = false;
-                _ = OnDismiss.InvokeAsync(e);
+                _ = OnDismiss.InvokeAsync();
             }
         }
+
+        private string GetPositionClass() => Position switch
+        {
+            BitModalPosition.Center => $"bit-mdl-position-center",
+
+            BitModalPosition.TopLeft => $"bit-mdl-position-topleft",
+            BitModalPosition.TopCenter => $"bit-mdl-position-topcenter",
+            BitModalPosition.TopRight => $"bit-mdl-position-topright",
+
+            BitModalPosition.CenterLeft => $"bit-mdl-position-centerleft",
+            BitModalPosition.CenterRight => $"bit-mdl-position-centerright",
+
+            BitModalPosition.BottomLeft => $"bit-mdl-position-bottomleft",
+            BitModalPosition.BottomCenter => $"bit-mdl-position-bottomcenter",
+            BitModalPosition.BottomRight => $"bit-mdl-position-bottomright",
+        };
     }
 }
---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/Modal/BitModalPosition.cs
@@ -0,0 +1,13 @@
+namespace Bit.Client.Web.BlazorUI;
+public enum BitModalPosition
+{
+    Center,
+    TopLeft,
+    TopCenter,
+    TopRight,
+    CenterLeft,
+    CenterRight,
+    BottomLeft,
+    BottomCenter,
+    BottomRight
+}
