1b9fe1b1f872483f0b5f5a55a87aeb2506566088
Sajad Kardel <sajadkardel@outlook.com>
2022-05-23 11:06:21 +0430

    feat(components): implement 404 error page in the Playground project #1891 (#2060)

---
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Components/NotFoundComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bit.Client.Web.BlazorUI.Playground.Web.Components
+{
+    public partial class NotFoundComponent
+    {
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        private void BackToHome()
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
