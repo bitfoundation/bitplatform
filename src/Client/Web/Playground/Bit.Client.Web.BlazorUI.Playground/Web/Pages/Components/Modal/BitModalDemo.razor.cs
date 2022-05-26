d6f65209507cf7acd28c368724c6a3dc41086772
sajad kardel <sajadkardel@outlook.com>
2022-05-26 16:57:53 +0430

    add more options for the position demo #1984

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Modal/BitModalDemo.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Modal/BitModalDemo.razor.cs
@@ -7,7 +7,8 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Modal
     public partial class BitModalDemo
     {
         private bool IsOpen = false;
-        private bool IsOpenInPosition = false;
+        private bool IsOpenInTopRight = false;
+        private bool IsOpenInBottomLeft = false;
 
         private readonly List<ComponentParameter> componentParameters = new()
         {
@@ -224,11 +225,61 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Modal
 </BitModal>";
 
         private readonly string example2HTMLCode = @"
-<BitButton OnClick=@(()=>IsOpen=true)>Open Modal in the top and right position</BitButton>
-<BitModal @bind-IsOpen=""IsOpen"" Position=""BitModalPosition.TopRight"">
+<BitButton OnClick=@(()=>IsOpenInTopRight=true)>Open Modal in the top and right position</BitButton>
+<BitModal @bind-IsOpen=""IsOpenInTopRight"" Position=""BitModalPosition.TopRight"">
     <div class=""modal-header"">
         <span>Lorem Ipsum</span>
-        <BitIconButton OnClick=@(()=>IsOpen=false) IconName=""BitIconName.ChromeClose"" Title=""Close"" />
+        <BitIconButton OnClick=@(()=>IsOpenInTopRight=false) IconName=""BitIconName.ChromeClose"" Title=""Close"" />
+    </div>
+    <div class=""modal-body"">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sit
+            amet, vulputate in leo. Maecenas vulputate congue sapien eu tincidunt. Etiam eu sem turpis. Fusce tempor
+            sagittis nunc, ut interdum ipsum vestibulum non. Proin dolor elit, aliquam eget tincidunt non, vestibulum ut
+            turpis. In hac habitasse platea dictumst. In a odio eget enim porttitor maximus. Aliquam nulla nibh,
+            ullamcorper aliquam placerat eu, viverra et dui. Phasellus ex lectus, maximus in mollis ac, luctus vel eros.
+            Vivamus ultrices, turpis sed malesuada gravida, eros ipsum venenatis elit, et volutpat eros dui et ante.
+            Quisque ultricies mi nec leo ultricies mollis. Vivamus egestas volutpat lacinia. Quisque pharetra eleifend
+            efficitur.
+        </p>
+        <p>
+            Mauris at nunc eget lectus lobortis facilisis et eget magna. Vestibulum venenatis augue sapien, rhoncus
+            faucibus magna semper eget. Proin rutrum libero sagittis sapien aliquet auctor. Suspendisse tristique a
+            magna at facilisis. Duis rhoncus feugiat magna in rutrum. Suspendisse semper, dolor et vestibulum lacinia,
+            nunc felis malesuada ex, nec hendrerit justo ex et massa. Quisque quis mollis nulla. Nam commodo est ornare,
+            rhoncus odio eu, pharetra tellus. Nunc sed velit mi.
+        </p>
+        <p>
+            Sed condimentum ultricies turpis convallis pharetra. Sed sagittis quam pharetra luctus porttitor. Cras vel
+            consequat lectus. Sed nec fringilla urna, a aliquet libero. Aenean sed nisl purus. Vivamus vulputate felis
+            et odio efficitur suscipit. Ut volutpat dictum lectus, ac rutrum massa accumsan at. Sed pharetra auctor
+            finibus. In augue libero, commodo vitae nisi non, sagittis convallis ante. Phasellus malesuada eleifend
+            mollis. Curabitur ultricies leo ac metus venenatis elementum.
+        </p>
+        <p>
+            Aenean egestas quam ut erat commodo blandit. Mauris ante nisl, pellentesque sed venenatis nec, aliquet sit
+            amet enim. Praesent vitae diam non diam aliquet tristique non ut arcu. Pellentesque et ultrices eros. Fusce
+            diam metus, mattis eu luctus nec, facilisis vel erat. Nam a lacus quis tellus gravida euismod. Nulla sed sem
+            eget tortor cursus interdum. Sed vehicula tristique ultricies. Aenean libero purus, mollis quis massa quis,
+            eleifend dictum massa. Fusce eu sapien sit amet odio lacinia placerat. Mauris varius risus sed aliquet
+            cursus. Aenean lectus magna, tincidunt sit amet sodales a, volutpat ac leo. Morbi nisl sapien, tincidunt sit
+            amet mauris quis, sollicitudin auctor est.
+        </p>
+        <p>
+            Nam id mi justo. Nam vehicula vulputate augue, ac pretium enim rutrum ultricies. Sed aliquet accumsan
+            varius. Quisque ac auctor ligula. Fusce fringilla, odio et dignissim iaculis, est lacus ultrices risus,
+            vitae condimentum enim urna eu nunc. In risus est, mattis non suscipit at, mattis ut ante. Maecenas
+            consectetur urna vel erat maximus, non molestie massa consequat. Duis a feugiat nibh. Sed a hendrerit diam,
+            a mattis est. In augue dolor, faucibus vel metus at, convallis rhoncus dui.
+        </p>
+    </div>
+</BitModal>
+
+<BitButton OnClick=@(()=>IsOpenInBottomLeft=true)>Open Modal in the bottom and left position</BitButton>
+<BitModal @bind-IsOpen=""IsOpenInBottomLeft"" Position=""BitModalPosition.BottomLeft"">
+    <div class=""modal-header"">
+        <span>Lorem Ipsum</span>
+        <BitIconButton OnClick=@(()=>IsOpenInBottomLeft=false) IconName=""BitIconName.ChromeClose"" Title=""Close"" />
     </div>
     <div class=""modal-body"">
         <p>
@@ -274,6 +325,11 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Modal
     </div>
 </BitModal>";
 
-        private readonly string example1CSharpCode = @"private bool IsOpen = false;";
+        private readonly string example1CSharpCode = @"
+private bool IsOpen = false;";
+
+        private readonly string example2CSharpCode = @"
+private bool IsOpenInTopRight = false;
+private bool IsOpenInBottomLeft = false;";
     }
 }
