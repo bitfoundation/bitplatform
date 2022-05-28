a1a0e610ee34ad9c9470a02e8f3a3dc5528049e0
Mohammad Aminsafaei <arc13.net@hotmail.com>
2022-05-29 00:21:43 +0430

    Add BitNumericTextField component (#1846)

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/NumericTextField/BitNumericTextFieldChangeValue.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace Bit.Client.Web.BlazorUI
+{
+    public class BitNumericTextFieldChangeValue<T>
+    {
+        public T? Value { get; set; }
+        public MouseEventArgs? MouseEventArgs { get; set; }
+        public KeyboardEventArgs? KeyboardEventArgs { get; set; }
+    }
+}
---
+++ src/Client/Web/Bit.Client.Web.BlazorUI/Components/NumericTextField/BitNumericTextFieldLabelPosition.cs
@@ -0,0 +1,15 @@
+﻿namespace Bit.Client.Web.BlazorUI
+{
+    public enum BitNumericTextFieldLabelPosition
+    {
+        /// <summary>
+        /// The label shows on the top of the numeric text field
+        /// </summary>
+        Top,
+
+        /// <summary>
+        /// The label shows on the left side of the numeric text field
+        /// </summary>
+        Left,
+    }
+}
