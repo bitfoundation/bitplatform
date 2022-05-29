c416f6d9f0a6a6fb0924cfc6cbb96b69eb39e143
Sajad Kardel <sajadkardel@outlook.com>
2022-05-29 14:31:39 +0430

    feat(components): update demo page and tests of the BitModal component for the new Position parameter #1984 (#2087)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Modal/BitModalDemo.razor.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Web/Pages/Components/Modal/BitModalDemo.razor.cs
@@ -7,6 +7,8 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Modal
     public partial class BitModalDemo
     {
         private bool IsOpen = false;
+        private bool IsOpenInPosition = false;
+        private BitModalPosition position;
 
         private readonly List<ComponentParameter> componentParameters = new()
         {
@@ -75,6 +77,15 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Modal
                 DefaultValue = "BitComponentVisibility.Visible",
                 Description = "Whether the component is Visible,Hidden,Collapsed.",
             },
+            new ComponentParameter()
+            {
+                Name = "Position",
+                Type = "BitModalPosition",
+                LinkType = LinkType.Link,
+                Href = "#component-position-enum",
+                DefaultValue = "BitModalPosition.Center",
+                Description = "Position of the modal on the screen.",
+            }
         };
 
         private readonly List<EnumParameter> enumParameters = new()
@@ -105,10 +116,71 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Modal
                         Value="2",
                     }
                 }
+            },
+            new EnumParameter()
+            {
+                Id = "component-position-enum",
+                Title = "BitModalPosition Enum",
+                Description = "",
+                EnumList = new List<EnumItem>()
+                {
+                    new EnumItem()
+                    {
+                        Name= "Center",
+                        Value="0"
+                    },
+                    new EnumItem()
+                    {
+                        Name= "TopLeft",
+                        Value="1"
+                    },
+                    new EnumItem()
+                    {
+                        Name= "TopCenter",
+                        Value="2"
+                    },
+                    new EnumItem()
+                    {
+                        Name= "TopRight",
+                        Value="3"
+                    },
+                    new EnumItem()
+                    {
+                        Name= "CenterLeft",
+                        Value="4"
+                    },
+                    new EnumItem()
+                    {
+                        Name= "CenterRight",
+                        Value="5"
+                    },
+                    new EnumItem()
+                    {
+                        Name= "BottomLeft",
+                        Value="6"
+                    },
+                    new EnumItem()
+                    {
+                        Name= "BottomCenter",
+                        Value="7"
+                    },
+                    new EnumItem()
+                    {
+                        Name= "BottomRight",
+                        Value="8"
+                    }
+                }
             }
         };
 
-        private readonly string example1HTMLCode = @"<BitButton OnClick=@(()=>IsOpen=true)>Open Modal</BitButton>
+        private void OpenModalInPosition(BitModalPosition positionValue)
+        {
+            IsOpenInPosition = true;
+            position = positionValue;
+        }
+
+        private readonly string example1HTMLCode = @"
+<BitButton OnClick=@(()=>IsOpen=true)>Open Modal</BitButton>
 <BitModal @bind-IsOpen=IsOpen>
     <div class=""modal-header"">
         <span>Lorem Ipsum</span>
@@ -158,7 +230,72 @@ namespace Bit.Client.Web.BlazorUI.Playground.Web.Pages.Components.Modal
     </div>
 </BitModal>";
 
+        private readonly string example2HTMLCode = @"
+<BitButton OnClick=""() => OpenModalInPosition(BitModalPosition.TopRight)"">Open Modal (top and right)</BitButton>
+<BitButton OnClick=""() => OpenModalInPosition(BitModalPosition.TopLeft)"">Open Modal (top and left)</BitButton>
+<BitButton OnClick=""() => OpenModalInPosition(BitModalPosition.BottomRight)"">Open Modal (bottom and right)</BitButton>
+<BitButton OnClick=""() => OpenModalInPosition(BitModalPosition.BottomLeft)"">Open Modal (bottom and left)</BitButton>
+<BitModal @bind-IsOpen=""IsOpenInPosition"" Position=""position"">
+    <div class=""modal-header"">
+        <span>Lorem Ipsum</span>
+        <BitIconButton OnClick=@(()=>IsOpenInPosition=false) IconName=""BitIconName.ChromeClose"" Title=""Close"" />
+    </div>
+    <div class=""modal-body"">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sit
+            amet, vulputate in leo. Maecenas vulputate congue sapien eu tincidunt. Etiam eu sem turpis. Fusce tempor
+            sagittis nunc, ut interdum ipsum vestibulum non. Proin dolor elit, aliquam eget tincidunt non, vestibulum ut
+            turpis. In hac habitasse platea dictumst. In a odio eget enim porttitor maximus. Aliquam nulla nibh,
+            ullamcorper aliquam placerat eu, viverra et dui. Phasellus ex lectus, maximus in mollis ac, luctus vel eros.
+            Vivamus ultrices, turpis sed malesuada gravida, eros ipsum venenatis elit, et volutpat eros dui et ante.
+            Quisque ultricies mi nec leo ultricies mollis. Vivamus egestas volutpat lacinia. Quisque pharetra eleifend
+            efficitur.
+        </p>
+        <p>
+            Mauris at nunc eget lectus lobortis facilisis et eget magna. Vestibulum venenatis augue sapien, rhoncus
+            faucibus magna semper eget. Proin rutrum libero sagittis sapien aliquet auctor. Suspendisse tristique a
+            magna at facilisis. Duis rhoncus feugiat magna in rutrum. Suspendisse semper, dolor et vestibulum lacinia,
+            nunc felis malesuada ex, nec hendrerit justo ex et massa. Quisque quis mollis nulla. Nam commodo est ornare,
+            rhoncus odio eu, pharetra tellus. Nunc sed velit mi.
+        </p>
+        <p>
+            Sed condimentum ultricies turpis convallis pharetra. Sed sagittis quam pharetra luctus porttitor. Cras vel
+            consequat lectus. Sed nec fringilla urna, a aliquet libero. Aenean sed nisl purus. Vivamus vulputate felis
+            et odio efficitur suscipit. Ut volutpat dictum lectus, ac rutrum massa accumsan at. Sed pharetra auctor
+            finibus. In augue libero, commodo vitae nisi non, sagittis convallis ante. Phasellus malesuada eleifend
+            mollis. Curabitur ultricies leo ac metus venenatis elementum.
+        </p>
+        <p>
+            Aenean egestas quam ut erat commodo blandit. Mauris ante nisl, pellentesque sed venenatis nec, aliquet sit
+            amet enim. Praesent vitae diam non diam aliquet tristique non ut arcu. Pellentesque et ultrices eros. Fusce
+            diam metus, mattis eu luctus nec, facilisis vel erat. Nam a lacus quis tellus gravida euismod. Nulla sed sem
+            eget tortor cursus interdum. Sed vehicula tristique ultricies. Aenean libero purus, mollis quis massa quis,
+            eleifend dictum massa. Fusce eu sapien sit amet odio lacinia placerat. Mauris varius risus sed aliquet
+            cursus. Aenean lectus magna, tincidunt sit amet sodales a, volutpat ac leo. Morbi nisl sapien, tincidunt sit
+            amet mauris quis, sollicitudin auctor est.
+        </p>
+        <p>
+            Nam id mi justo. Nam vehicula vulputate augue, ac pretium enim rutrum ultricies. Sed aliquet accumsan
+            varius. Quisque ac auctor ligula. Fusce fringilla, odio et dignissim iaculis, est lacus ultrices risus,
+            vitae condimentum enim urna eu nunc. In risus est, mattis non suscipit at, mattis ut ante. Maecenas
+            consectetur urna vel erat maximus, non molestie massa consequat. Duis a feugiat nibh. Sed a hendrerit diam,
+            a mattis est. In augue dolor, faucibus vel metus at, convallis rhoncus dui.
+        </p>
+    </div>
+</BitModal>
+";
+
         private readonly string example1CSharpCode = @"
 private bool IsOpen = false;";
+
+        private readonly string example2CSharpCode = @"
+private bool IsOpenInPosition = false;
+private BitModalPosition position;
+
+private void OpenModalInPosition(BitModalPosition positionValue)
+{
+    IsOpenInPosition = true;
+    position = positionValue;
+}";
     }
 }
