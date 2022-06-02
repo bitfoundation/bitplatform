b8508865eac44e1482135b810c936f19b6b14b26
ysmoradi <ysmoradi@outlook.com>
2022-06-02 16:29:56 +0430

    Simplify jwt claim types

--- src/Tooling/Bit.Tooling.Templates/TodoTemplate/Shared/Extensions/ClaimsPrincipalExtensions.cs
+++ src/Tooling/Bit.Tooling.Templates/TodoTemplate/Shared/Extensions/ClaimsPrincipalExtensions.cs
@@ -4,11 +4,11 @@ public static class ClaimsPrincipalExtensions
 {
     public static int GetUserId(this ClaimsPrincipal claimsPrincipal)
     {
-        return int.Parse((claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier) ?? claimsPrincipal.FindFirst("nameid"))!.Value);
+        return int.Parse(claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)!.Value);
     }
 
     public static string GetUserName(this ClaimsPrincipal claimsPrincipal)
     {
-        return (claimsPrincipal.FindFirst(ClaimTypes.Name) ?? claimsPrincipal.FindFirst("name"))!.Value;
+        return claimsPrincipal.FindFirst(ClaimTypes.Name)!.Value;
     }
 }
