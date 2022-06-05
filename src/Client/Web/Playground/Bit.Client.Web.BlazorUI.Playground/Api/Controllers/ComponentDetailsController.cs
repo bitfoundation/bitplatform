50d37fd3c35fe0fe38165f93ef923bbe296e0f04
Saleh Yusefnejad <msynk@outlook.com>
2022-06-05 20:08:32 +0430

    feat(components): enhance the component-details api to return only parameters #2136 (#2146)

--- src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Api/Controllers/ComponentDetailsController.cs
+++ src/Client/Web/Playground/Bit.Client.Web.BlazorUI.Playground/Api/Controllers/ComponentDetailsController.cs
@@ -1,117 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.Xml.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using Bit.Client.Web.BlazorUI.Playground.Shared.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using Bit.Client.Web.BlazorUI.Playground.Shared.Dtos;
 
-namespace Bit.Client.Web.BlazorUI.Playground.Api.Controllers
+namespace Bit.Client.Web.BlazorUI.Playground.Api.Controllers;
+
+[ApiController]
+[Route("component-details")]
+public class ComponentDetailsController : ControllerBase
 {
-    [ApiController]
-    [Route("component-details")]
-    public class ComponentDetailsController : ControllerBase
+    private static XDocument SummariesXmlDocument = null;
+    private static readonly Assembly ComponentsAssembly = Assembly.GetAssembly(typeof(BitButton));
+
+    [HttpGet("properties")]
+    public async Task<ActionResult<List<ComponentPropertyDetailsDto>>> GetProperties(string name)
     {
-        private static readonly Assembly _bitComponentAssembly = Assembly.GetAssembly(typeof(BitToggleButton));
-       
-        [HttpGet("properties")]
-        public async Task<ActionResult<List<ComponentPropertyDetailsDto>>>
-            GetProperties(string componentName)
+        if (string.IsNullOrWhiteSpace(name))
+            return BadRequest("Component Name is empty.");
+
+        var componentType = ComponentsAssembly.ExportedTypes
+                                              .Where(c => c.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase))
+                                              .FirstOrDefault();
+
+        if (componentType == null)
+            return NotFound("No component type found.");
+
+        if (SummariesXmlDocument == null)
         {
-            if(string.IsNullOrWhiteSpace(componentName))
-                return BadRequest("Component Name is empty!");
-
-            var classType = GetComponentType(componentName);
-            
-            if (classType == null) 
-                return NotFound("No component type found!");
-
-            var result = new List<ComponentPropertyDetailsDto>();
-
-            var xmlFile = await LoadXmlFileAsync();
-            var instance = Activator.CreateInstance(classType);
-
-            foreach (var item in classType.GetProperties())
-            {
-                string propertyAttribute = $"{classType.FullName}.{item.Name}";
-                var xmlProperty = xmlFile?.Descendants().Attributes()
-                    .Where(attr => attr.Value.Contains(propertyAttribute))
-                    .FirstOrDefault();
-
-                var componentDetail = new ComponentPropertyDetailsDto
-                {
-                    Name = item.Name,
-                    Type = item.PropertyType.Name,
-                    Summary = xmlProperty?.Parent.Element("summary")?.Value.Trim(),
-                    DefaultValue = item.GetValue(instance)?.ToString(),
-                };
-                result.Add(componentDetail);
-            }
-
-            return Ok(result);
+            SummariesXmlDocument = await LoadSummariesXmlDocumentAsync();
         }
 
-        [HttpGet("methods")]
-        public async Task<ActionResult<List<ComponentMethodDetailsDto>>>
-            GetMethods(string componentName)
-        {
-            if (string.IsNullOrWhiteSpace(componentName))
-                return BadRequest("Component Name is empty!");
+        var componentInstance = Activator.CreateInstance(componentType);
 
-            var classType = GetComponentType(componentName);
+        var prefix = $"{componentType.FullName}.";
+        return Ok(componentType.GetProperties()
+                              .Where(p => Attribute.IsDefined(p, typeof(Microsoft.AspNetCore.Components.ParameterAttribute)))
+                              .Select(p =>
+                              {
+                                  var xmlProperty = SummariesXmlDocument?.Descendants()
+                                                            .Attributes()
+                                                            .Where(a => a.Value.Contains(prefix + p.Name))
+                                                            .FirstOrDefault();
 
-            if (classType == null)
-                return NotFound("No component type found!");
-
-            var result = new List<ComponentMethodDetailsDto>();
-            
-            var xmlFile = await LoadXmlFileAsync();
-
-            var methodList = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public)
-                        .Where(m => !m.IsSpecialName).ToList();
-
-            foreach (var item in methodList)
-            {
-                string propertyAttribute = $"{classType.FullName}.{item.Name}";
-                var xmlProperty = xmlFile?.Descendants().Attributes()
-                    .Where(attr => attr.Value.Contains(propertyAttribute))
-                    .FirstOrDefault();
-
-                var parameters = item.GetParameters();
-                
-                var componentDetail = new ComponentMethodDetailsDto
-                {
-                    Name = item.Name,
-                    Parameters = parameters.Length > 0 ?
-                        parameters.Select(v=> v.ParameterType.FullName).Aggregate((a,b)=> $"{a},{b}") : null,
-                    Output = item.ReturnType.ToString(),
-                    Summary = xmlProperty?.Parent.Element("summary")?.Value.Trim(),
-                };
-                result.Add(componentDetail);
-            }
-
-            return Ok(result);
-        }
-
-        private static async Task<XDocument> LoadXmlFileAsync()
-        {
-            string path = Path.Combine(AppContext.BaseDirectory, "Bit.Client.Web.BlazorUI.xml");
-
-            if (System.IO.File.Exists(path) == false)
-                return null;
-
-            var stream = System.IO.File.OpenRead(path);
-            return await XDocument.LoadAsync(stream, LoadOptions.None, default);
-        }
-
-        private static Type GetComponentType(string name)
-        {
-            return _bitComponentAssembly.ExportedTypes
-                .Where(c => c.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase))
-                .FirstOrDefault();
-        }
+                                  return new
+                                  {
+                                      p.Name,
+                                      Type = p.PropertyType.Name,
+                                      DefaultValue = p.GetValue(componentInstance)?.ToString(),
+                                      Description = xmlProperty?.Parent.Element("summary")?.Value.Trim(),
+                                  };
+                              }));
     }
 
+    private static async Task<XDocument> LoadSummariesXmlDocumentAsync()
+    {
+        string path = Path.Combine(AppContext.BaseDirectory, $"{ComponentsAssembly.GetName().Name}.xml");
+
+        if (System.IO.File.Exists(path) == false) return null;
+
+        var stream = System.IO.File.OpenRead(path);
+        return await XDocument.LoadAsync(stream, LoadOptions.None, default);
+    }
 }
+
