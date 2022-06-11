35a798e73079ba0ab9ab001126310a1a5a3fbc90
m.sangtarash <m.sangtarash@outlook.com>
2022-06-11 14:53:00 +0430

    Add header & hero baner

--- src/WebSite/Bit.Platform.WebSite/Web/Components/Header.razor.cs
+++ src/WebSite/Bit.Platform.WebSite/Web/Components/Header.razor.cs
@@ -5,7 +5,29 @@ using Microsoft.AspNetCore.Components.Routing;
 
 namespace Bit.Platform.WebSite.Web.Components
 {
-    public partial class Header
+    public partial class Header : IDisposable
     {
+        private string CurrentUrl = string.Empty;
+
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            CurrentUrl = NavigationManager.Uri.Replace(NavigationManager.BaseUri, "/", StringComparison.Ordinal);
+            NavigationManager.LocationChanged += OnLocationChanged;
+
+            base.OnInitialized();
+        }
+
+        private void OnLocationChanged(object sender, LocationChangedEventArgs args)
+        {
+            CurrentUrl = NavigationManager.Uri.Replace(NavigationManager.BaseUri, "/", StringComparison.Ordinal);
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            NavigationManager.LocationChanged -= OnLocationChanged;
+        }
     }
 }
