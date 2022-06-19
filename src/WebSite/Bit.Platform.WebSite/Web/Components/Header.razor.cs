f64f94da346588da5479d257d0e9650606952a5d
m.sangtarash <m.sangtarash@outlook.com>
2022-06-19 14:47:11 +0430

    add header

--- src/WebSite/Bit.Platform.WebSite/Web/Components/Header.razor.cs
+++ src/WebSite/Bit.Platform.WebSite/Web/Components/Header.razor.cs
@@ -1,11 +1,48 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Bit.Platform.WebSite.Web.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
 
 namespace Bit.Platform.WebSite.Web.Components
 {
-    public partial class Header
+    public partial class Header : IDisposable
     {
+        private string CurrentUrl = string.Empty;
+
+        public ElementReference HeaderElement { get; internal set; }
+
+        [Inject] public NavigationManager NavigationManager { get; set; }
+        [Inject] public IJSRuntime JSRuntime { get; set; }
+
+        protected override void OnInitialized()
+        {
+            CurrentUrl = NavigationManager.Uri.Replace(NavigationManager.BaseUri, "/", StringComparison.Ordinal);
+            NavigationManager.LocationChanged += OnLocationChanged;
+
+            base.OnInitialized();
+        }
+
+        private void OnLocationChanged(object sender, LocationChangedEventArgs args)
+        {
+            CurrentUrl = NavigationManager.Uri.Replace(NavigationManager.BaseUri, "/", StringComparison.Ordinal);
+            StateHasChanged();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSRuntime.ChangeHeaderByScrolling(HeaderElement);
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        public void Dispose()
+        {
+            NavigationManager.LocationChanged -= OnLocationChanged;
+        }
     }
 }
