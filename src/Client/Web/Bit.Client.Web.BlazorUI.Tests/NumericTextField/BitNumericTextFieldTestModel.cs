14f6265a906492b6659d3fe42e6723893013ceed
Milad Karimifard <karimifard.milad@gmail.com>
2022-06-23 20:36:07 +0430

    Writing tests for BitNumericTextField component complete

---
+++ src/Client/Web/Bit.Client.Web.BlazorUI.Tests/NumericTextField/BitNumericTextFieldTestModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bit.Client.Web.BlazorUI.Tests.NumericTextField
+{
+    public class BitNumericTextFieldTestModel
+    {
+        [Range(6, 18)]
+        public double? Value { get; set; }
+    }
+}
