42277a579f16638b68fe23a08c9cd97070bbfe61
m.sangtarash <m.sangtarash@outlook.com>
2022-06-28 08:57:50 +0430

    some improvments

--- src/WebSite/Bit.Platform.WebSite/Web/Components/GettingStarted.razor.cs
+++ src/WebSite/Bit.Platform.WebSite/Web/Components/GettingStarted.razor.cs
@@ -6,15 +6,5 @@ namespace Bit.Platform.WebSite.Web.Components
 {
     public partial class GettingStarted
     {
-        [Inject] public IJSRuntime JSRuntime { get; set; }
-
-        public async Task OnScrollTo(string id)
-        {
-            await JSRuntime.ScrollToElement(id);
-        }
-
-        public string Code1 = "<section class=\"getting-started-list-section\">\r\n    <div class=\"item\" onclick=\"@OnScrollTo(\"step-number-1\")\">Instal</div>\r\n    <div class=\"item selected-item\" onclick=\"@OnScrollTo(\"step-number-2\")\">Development prerequisites</div>\r\n    <div class=\"item\" onclick=\"@OnScrollTo(\"step-number-3\")\">Prepare The Project</div>\r\n</section>";
-
-        public string Code2 = "public async Task OnScrollTo(string id)\r\n{\r\n     await JSRuntime.ScrollToElement(id);\r\n}";
     }
 }
