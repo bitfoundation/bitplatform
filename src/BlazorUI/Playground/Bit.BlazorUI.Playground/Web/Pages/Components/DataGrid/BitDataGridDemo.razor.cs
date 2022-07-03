47733a8a40e4229a7ba034a71049bda02dfa8c1c
MeysamBahadori <bahadori.meysam@outlook.com>
2022-07-03 13:35:45 +0430

    Add styling sample and create properties and enums

--- src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/DataGrid/BitDataGridDemo.razor.cs
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/DataGrid/BitDataGridDemo.razor.cs
@@ -10,8 +10,281 @@ namespace Bit.BlazorUI.Playground.Web.Pages.Components.DataGrid;
 
 public partial class BitDataGridDemo
 {
-    private readonly List<ComponentParameter> componentParameters = new();
-    private readonly List<EnumParameter> enumParameters = new();
+    private readonly List<ComponentParameter> componentParameters = new()
+    {
+
+         new ComponentParameter()
+        {
+            Name = "Items",
+            Type = "IQueryable<TGridItem>",
+            DefaultValue = "",
+            Description = @"A queryable source of data for the grid.
+                            This could be in-memory data converted to queryable using the
+                            System.Linq.Queryable.AsQueryable(System.Collections.IEnumerable) extension method,
+                            or an EntityFramework DataSet or an IQueryable derived from it.
+                            You should supply either Items or ItemsProvider, but not both.
+                            ",
+        },
+         new ComponentParameter()
+        {
+            Name = "ItemsProvider",
+            Type = "BitDataGridItemsProvider<TGridItem>",
+            DefaultValue = "",
+            Description = @"A callback that supplies data for the rid.
+                            You should supply either Items or ItemsProvider, but not both.
+                           ",
+        },
+
+          new ComponentParameter()
+        {
+            Name = "Class",
+            Type = "string",
+            DefaultValue = "",
+            Description = "An optional CSS class name. If given, this will be included in the class attribute of the rendered table.",
+        },
+
+          new ComponentParameter()
+        {
+            Name = "Theme",
+            Type = "IQueryable<TGridItem>",
+            DefaultValue = "default",
+            Description = @"A theme name, with default value ""default"". This affects which styling rules match the table.",
+        },
+
+        new ComponentParameter()
+        {
+            Name = "ChildContent",
+            Type = "RenderFragment",
+            DefaultValue = "",
+            Description = "Defines the child components of this instance. For example, you may define columns by adding components derived from the BitDataGridColumnBase<TGridItem>",
+        },
+
+         new ComponentParameter()
+        {
+            Name = "Virtualize",
+            Type = "bool",
+            DefaultValue = "false",
+            Description = @"If true, the grid will be rendered with virtualization. This is normally used in conjunction with
+                            scrolling and causes the grid to fetch and render only the data around the current scroll viewport.
+                            This can greatly improve the performance when scrolling through large data sets.",
+        },
+
+         new ComponentParameter()
+        {
+            Name = "ItemSize",
+            Type = "float",
+            DefaultValue = "50",
+            Description = @"This is applicable only when using Virtualize. It defines an expected height in pixels for
+                            each row, allowing the virtualization mechanism to fetch the correct number of items to match the display
+                            size and to ensure accurate scrolling.",
+        },
+
+         new ComponentParameter()
+        {
+            Name = "ResizableColumns",
+            Type = "bool",
+            DefaultValue = "",
+            Description = @"If true, renders draggable handles around the column headers, allowing the user to resize the columns
+                            manually. Size changes are not persisted.
+                           ",
+        },
+
+        new ComponentParameter()
+        {
+            Name = "ItemKey",
+            Type = "Func<TGridItem, object>",
+            DefaultValue = "x => x!",
+            Description = @"Optionally defines a value for @key on each rendered row. Typically this should be used to specify a
+                            unique identifier, such as a primary key value, for each data item.
+                            This allows the grid to preserve the association between row elements and data items based on their
+                            unique identifiers, even when the TGridItem instances are replaced by new copies (for example, after a new query against the underlying data store).
+                            If not set, the @key will be the TGridItem instance itself.",
+        },
+
+        new ComponentParameter()
+        {
+            Name = "Pagination",
+            Type = "BitDataGridPaginationState",
+            DefaultValue = "",
+            Description = @"Optionally links this BitDataGrid<TGridItem> instance with a BitDataGridPaginationState model,
+                            causing the grid to fetch and render only the current page of data.
+                            This is normally used in conjunction with a Paginator component or some other UI logic
+                            that displays and updates the supplied BitDataGridPaginationState instance.",
+        },
+
+
+    };
+    private readonly List<ComponentSubParameter> componentSubParameter = new()
+    {
+         new ComponentSubParameter()
+        {
+            Id="BitDataGridColumnBase",
+            Title = "BitDataGridColumnBase",
+            Parameters=new List<ComponentParameter>()
+            {
+                 new ComponentParameter()
+                    {
+                        Name = "Title",
+                        Type = "string",
+                        DefaultValue = "",
+                        Description = @"Title text for the column. This is rendered automatically if HeaderTemplate is not used.",
+                    },
+                 new ComponentParameter()
+                    {
+                        Name = "Class",
+                        Type = "string",
+                        DefaultValue = "",
+                        Description = @"An optional CSS class name. If specified, this is included in the class attribute of table header and body cells
+                                        for this column.
+                                        ",
+                    },
+                 new ComponentParameter()
+                    {
+                        Name = "Align",
+                        Type = "BitDataGridAlign",
+                        DefaultValue = "",
+                        Description = @"If specified, controls the justification of table header and body cells for this column.",
+                    },
+                  new ComponentParameter()
+                    {
+                        Name = "HeaderTemplate",
+                        Type = "RenderFragment<BitDataGridColumnBase<TGridItem>>",
+                        DefaultValue = "",
+                        Description = @"An optional template for this column's header cell. If not specified, the default header template
+                                        includes the Title along with any applicable sort indicators and options buttons.
+                                        ",
+                    },
+                   new ComponentParameter()
+                    {
+                        Name = "ColumnOptions",
+                        Type = "RenderFragment<BitDataGridColumnBase<TGridItem>>",
+                        DefaultValue = "",
+                        Description = @"If specified, indicates that this column has this associated options UI. A button to display this
+                                        UI will be included in the header cell by default.
+                                        If HeaderTemplate is used, it is left up to that template to render any relevant
+                                        ""show options"" UI and invoke the grid's BitDataGrid<TGridItem>.ShowColumnOptions(BitDataGridColumnBase<TGridItem>)).
+                                        ",
+                    },
+                   new ComponentParameter()
+                    {
+                        Name = "Sortable",
+                        Type = "bool",
+                        DefaultValue = "",
+                        Description = @"Indicates whether the data should be sortable by this column.
+                                        The default value may vary according to the column type (for example, a BitDataGridTemplateColumn<TGridItem>
+                                        is sortable by default if any BitDataGridTemplateColumn<TGridItem>.SortBy parameter is specified).
+                                        ",
+                    },
+                   new ComponentParameter()
+                    {
+                        Name = "IsDefaultSort",
+                        Type = "BitDataGridSortDirection",
+                        DefaultValue = "",
+                        Description = @"If specified and not null, indicates that this column represents the initial sort order
+                                        for the grid. The supplied value controls the default sort direction.
+                                        ",
+                    },
+                   new ComponentParameter()
+                    {
+                        Name = "PlaceholderTemplate",
+                        Type = "RenderFragment<PlaceholderContext>",
+                        DefaultValue = "",
+                        Description = @"If specified, virtualized grids will use this template to render cells whose data has not yet been loaded.",
+                    },
+            },
+            Description=@"BitDataGrid has two built-in column types, BitDataGridPropertyColumn and BitDataGridTemplateColumn. You can also create your own column types by subclassing ColumnBase
+                          The BitDataGridColumnBase type, which all column must derive from, offers some common parameters",
+
+        },
+
+         new ComponentSubParameter()
+        {
+            Id="BitDataGridPropertyColumn",
+            Title = "BitDataGridPropertyColumn",
+            Parameters=new List<ComponentParameter>()
+            {
+                 new ComponentParameter()
+                    {
+                        Name = "Property",
+                        Type = "Expression<Func<TGridItem, TProp>>",
+                        DefaultValue="",
+                        Description = @"Defines the value to be displayed in this column's cells.",
+                    },
+                  new ComponentParameter()
+                    {
+                        Name = "Format",
+                        Type = "string",
+                        DefaultValue="",
+                        Description = @"Optionally specifies a format string for the value.
+                                        Using this requires the TProp type to implement IFormattable.
+                                       ",
+                    },
+            },
+            Description=@"It is for displaying a single value specified by the parameter Property. This column infers sorting rules automatically,
+                          and uses the property's name as its title if not otherwise set.",
+
+        },
+
+          new ComponentSubParameter()
+        {
+            Id="BitDataGridTemplateColumn",
+            Title = "BitDataGridTemplateColumn",
+            Parameters=new List<ComponentParameter>()
+            {
+                 new ComponentParameter()
+                    {
+                        Name = "ChildContent",
+                        Type = "RenderFragment<TGridItem>",
+                        DefaultValue="",
+                        Description = @"Specifies the content to be rendered for each row in the table.",
+                    },
+                  new ComponentParameter()
+                    {
+                        Name = "SortBy",
+                        Type = "BitDataGridSort<TGridItem>",
+                        DefaultValue="",
+                        Description = @"Optionally specifies sorting rules for this column.",
+                    },
+            },
+            Description=@"It uses arbitrary Razor fragments to supply contents for its cells.
+                          It can't infer the column's title or sort order automatically. 
+                          also it's possible to add arbitrary Blazor components to your table cells.
+                          Remember that rendering many components, or many event handlers, can impact the performance of your grid. One way to mitigate this issue is by paginating or virtualizing your grid",
+
+        },
+
+    };
+    private readonly List<EnumParameter> enumParameters = new()
+    {
+        new EnumParameter()
+        {
+             Id="BitDataGridAlign",
+             Title="BitDataGridAlign Enum",
+             Description="Describes alignment for a BitDataGrid<TGridItem> column.",
+             EnumList=new List<EnumItem>()
+             {
+                 new EnumItem()
+                 {
+                      Name="Left",
+                      Value="0",
+                      Description="Justifies the content against the start of the container."
+                 },
+                 new EnumItem()
+                 {
+                      Name="Center",
+                      Value="1",
+                      Description="Justifies the content at the center of the container."
+                 },
+                 new EnumItem()
+                 {
+                      Name="Right",
+                      Value="2",
+                      Description="Justifies the content at the end of the container."
+                 },
+
+             }
+        },
+    };
     private readonly string example1HTMLCode = @"
 <style scoped>
 .grid {
@@ -234,8 +507,58 @@ public class Medals
 }
 ";
     private readonly string example3HTMLCode = @"
+<style scoped>
+::deep .bitdatagrid[theme=redskin] {
+    font-style :italic;
+    color: red;
+}
+
+::deep .bitdatagrid[theme=redskin] .col-title {
+    gap: 0.4rem; /* Separate the sort indicator from title text */
+    font-weight: bold;
+    text-transform: uppercase;
+    background-color:red;
+    color:white;
+}
+
+::deep .bitdatagrid[theme=redskin] .sort-indicator {
+    color: white;
+}
+</style>
+
+<div class=""grid"">
+     <BitDataGrid Items = ""@data""  Theme=""redskin"">
+         <BitDataGridPropertyColumn Property = ""@(c => c.Name)"" Sortable=""true"" Class=""column1"">
+         </BitDataGridPropertyColumn>
+         <BitDataGridPropertyColumn Property = ""@(c => c.Medals.Gold)"" Sortable=""true"" Align=""BitDataGridAlign.Right"" />
+         <BitDataGridPropertyColumn Property = ""@(c => c.Medals.Silver)"" Sortable=""true"" Align=""BitDataGridAlign.Right"" />
+         <BitDataGridPropertyColumn Property = ""@(c => c.Medals.Bronze)"" Sortable=""true"" Align=""BitDataGridAlign.Right"" />
+         <BitDataGridPropertyColumn Property = ""@(c => c.Medals.Total)"" Sortable=""true"" Align=""BitDataGridAlign.Right"" />
+    </BitDataGrid>
+</div>
 ";
     private readonly string example3CSharpCode = @"
+IQueryable<Country> data;
+protected override async Task OnInitializedAsync()
+{
+    data= (await Get7CountriesAsync()).Items.AsQueryable();
+}
+private async Task<BitDataGridItemsProviderResult<Country>> Get7CountriesAsync()
+{
+     var Countries = _countries.Take(7).AsQueryable();
+     return BitDataGridItemsProviderResult.From(Countries.ToArray(), Countries.Count());
+}
+private readonly static Country[] _countries = new[]
+{
+    new Country { Code = ""AR"", Name=""Argentina"", Medals = new Medals { Gold = 0, Silver = 1, Bronze = 2 } },
+    new Country { Code = ""AM"", Name=""Armenia"", Medals = new Medals { Gold = 0, Silver = 2, Bronze = 2 } },
+    new Country { Code = ""AU"", Name = ""Australia"", Medals = new Medals { Gold = 17, Silver = 7, Bronze = 22 } },
+    new Country { Code = ""AT"", Name = ""Austria"", Medals = new Medals { Gold = 1, Silver = 1, Bronze = 5 } },
+    new Country { Code = ""AZ"", Name = ""Azerbaijan"", Medals = new Medals { Gold = 0, Silver = 3, Bronze = 4 } },
+    new Country { Code = ""BS"", Name = ""Bahamas"", Medals = new Medals { Gold = 2, Silver = 0, Bronze = 0 } },
+    new Country { Code = ""BH"", Name = ""Bahrain"", Medals = new Medals { Gold = 0, Silver = 1, Bronze = 0 } },
+    ...
+};
 ";
 
     BitDataGridPaginationState pagination = new() { ItemsPerPage = 15 };
@@ -251,7 +574,7 @@ public class Medals
     protected override async Task OnInitializedAsync()
     {
         items = (await GetCountriesAsync(0, null, null, true, CancellationToken.None)).Items.AsQueryable();
-        data= (await Get10CountriesAsync()).Items.AsQueryable();
+        data = (await Get7CountriesAsync()).Items.AsQueryable();
 
     }
 
@@ -279,13 +602,12 @@ public class Medals
 
         return BitDataGridItemsProviderResult.From(result.ToArray(), ordered.Count());
     }
-  
 
-    private async Task<BitDataGridItemsProviderResult<Country>> Get10CountriesAsync()
+    private async Task<BitDataGridItemsProviderResult<Country>> Get7CountriesAsync()
     {
         await Task.Delay(1000);
 
-        var Countries = _countries.Take(10).AsQueryable();
+        var Countries = _countries.Take(7).AsQueryable();
         return BitDataGridItemsProviderResult.From(Countries.ToArray(), Countries.Count());
     }
 
