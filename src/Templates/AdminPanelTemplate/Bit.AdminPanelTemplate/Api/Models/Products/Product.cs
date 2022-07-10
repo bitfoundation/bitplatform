4ea7234263ed26459ec7619c1f288d6cef4d677e
Miki <Miki@DESKTOP-FQG9ECI>
2022-07-10 16:03:04 +0430

    implement Dashboard Apis

--- src/Templates/AdminPanelTemplate/Bit.AdminPanelTemplate/Api/Models/Products/Product.cs
+++ src/Templates/AdminPanelTemplate/Bit.AdminPanelTemplate/Api/Models/Products/Product.cs
@@ -6,21 +6,34 @@ namespace AdminPanelTemplate.Api.Models.Products;
 [Table("Products")]
 public class Product
 {
+    public Product()
+    {
+        this.CreateDate = DateTime.Now;
+    }
+
     public int Id { get; set; }
 
     [NotNull]
     [MaxLength(64)]
     public string? Name { get; set; }
 
+
     [Column(TypeName = "money")]
-    public decimal? Price { get; set; }
+    [Range(0, Double.MaxValue)]
+    public decimal Price { get; set; }
 
     [MaxLength(512)]
     public string? Desc { get; set; }
+
+    [NotNull]
+    [Column(TypeName ="Date")]
+    public DateTime CreateDate { get; set; }
+
 
     [ForeignKey(nameof(CategoryId))]
     public Category? Category { get; set; }
 
     public int? CategoryId { get; set; }
+
 
 }
---
+++ src/Templates/AdminPanelTemplate/Bit.AdminPanelTemplate/Shared/Dtos/Dashboard/AnalyticsStatsDataDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanelTemplate.Shared.Dtos.Dashboard;
+
+public class AnalyticsStatsDataDto
+{
+    public int SeasonProductCount { get; set; }
+
+    public int TotalProducts { get; set; }
+
+    public int TotalCategories { get; set; }
+
+}
