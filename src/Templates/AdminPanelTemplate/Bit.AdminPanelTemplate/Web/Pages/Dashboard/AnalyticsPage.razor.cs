ddb70014d7e34fdaea9e11c7e0aca45ae7800e6e
Miki <Miki@DESKTOP-FQG9ECI>
2022-07-10 13:19:31 +0430

    Dashboard page and layout

---
+++ src/Templates/AdminPanelTemplate/Bit.AdminPanelTemplate/Web/Pages/Dashboard/AnalyticsPage.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace AdminPanelTemplate.App.Pages.Dashboard;
+
+public partial class AnalyticsPage
+{
+    [AutoInject] private HttpClient httpClient = default!;
+
+    [AutoInject] private IStateService stateService = default!;
+
+    public bool IsLoading { get; set; }
+
+
+    protected override async Task OnInitAsync()
+    {
+        await base.OnInitAsync();
+    }
+
+
+
+
+}
