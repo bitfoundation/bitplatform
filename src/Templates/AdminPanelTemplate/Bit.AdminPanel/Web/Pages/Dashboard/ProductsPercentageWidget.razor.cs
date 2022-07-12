ac0c6624bc2846f5609310e852da502ba336dda6
MeysamBahadori <bahadori.meysam@outlook.com>
2022-07-12 12:19:53 +0430

    Replace color input element with BitColorPicker and improve ProductPercentageChart

--- src/Templates/AdminPanelTemplate/Bit.AdminPanel/Web/Pages/Dashboard/ProductsPercentageWidget.razor.cs
+++ src/Templates/AdminPanelTemplate/Bit.AdminPanel/Web/Pages/Dashboard/ProductsPercentageWidget.razor.cs
@@ -28,8 +28,16 @@ public partial class ProductsPercentageWidget
         {
             IsLoading = true;
             var Data = await stateService.GetValue($"{nameof(AnalyticsPage)}-{nameof(ProductsPercentageWidget)}", async () => await httpClient.GetFromJsonAsync($"Dashboard/GetProductsPercentagePerCategoryStats", AppJsonContext.Default.ListProductPercentagePerCategoryDto));
-            Series = Data.Select(d => 
-                    new PieSeries<double> { Values = new double[] { d.ProductPercentage }, Name = d.CategoryName }).ToArray();
+            Series = Data!.Select(d =>
+                    new PieSeries<float> { 
+                        Values = new float[] { d.ProductPercentage },
+                        Name = d.CategoryName,
+                        Fill = new SolidColorPaint(SKColor.Parse(d.CategoryColor)),
+                        DataLabelsPosition = PolarLabelsPosition.Start,
+                        DataLabelsFormatter = point => point.PrimaryValue.ToString("N1"),
+                        DataLabelsPaint = new SolidColorPaint(SKColors.Black)
+                       
+                    }).ToArray();
         }
         finally
         {
