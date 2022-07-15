ac9f16fa3e78cfbffa865f89ae7bded2ff35fcab
ysmoradi <ysmoradi@outlook.com>
2022-07-15 20:39:33 +0430

    bit data grid odata sample

---
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Shared/Dtos/DataGridDemo/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Bit.BlazorUI.Playground.Shared.Dtos.DataGridDemo;
+
+public class ProductDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+}
