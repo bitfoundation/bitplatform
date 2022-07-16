ada9080c38ba493daf1ae9ffd83e8953a909f8d9
Mohammad Mohabbati <mohabbati@live.com>
2022-07-16 22:44:38 +0300

    Added two extensions for string and enum #2086

---
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Bit.BlazorUI.Playground;
+
+public static class StringExtensions
+{
+    public static bool HasValue(this string? value, bool ignoreWhiteSpace = true)
+    {
+        return ignoreWhiteSpace
+            ? string.IsNullOrWhiteSpace(value) is false
+            : string.IsNullOrEmpty(value) is false;
+    }
+
+    public static bool HasNoValue(this string? value, bool ignoreWhiteSpace = true)
+    {
+        return ignoreWhiteSpace
+            ? string.IsNullOrWhiteSpace(value)
+            : string.IsNullOrEmpty(value);
+    }
+}
