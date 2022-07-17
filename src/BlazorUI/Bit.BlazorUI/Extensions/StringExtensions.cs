c8b9909320963235cfe8836ebc34cf42b12e0e09
M. Mohabbati <mohabbati@live.com>
2022-07-17 11:17:20 -0400

    feat(components): change the public access modifier to internal in all extension classes of the BlazorUI #2086 (#2572)

--- src/BlazorUI/Bit.BlazorUI/Extensions/StringExtensions.cs
+++ src/BlazorUI/Bit.BlazorUI/Extensions/StringExtensions.cs
@@ -1,15 +1,17 @@
-﻿namespace Bit.BlazorUI;
+﻿using System.Runtime.CompilerServices;
 
-public static class StringExtensions
+namespace Bit.BlazorUI;
+
+internal static class StringExtensions
 {
-    public static bool HasValue(this string? value, bool ignoreWhiteSpace = true)
+    internal static bool HasValue(this string? value, bool ignoreWhiteSpace = true)
     {
         return ignoreWhiteSpace
             ? string.IsNullOrWhiteSpace(value) is false
             : string.IsNullOrEmpty(value) is false;
     }
 
-    public static bool HasNoValue(this string? value, bool ignoreWhiteSpace = true)
+    internal static bool HasNoValue(this string? value, bool ignoreWhiteSpace = true)
     {
         return ignoreWhiteSpace
             ? string.IsNullOrWhiteSpace(value)
