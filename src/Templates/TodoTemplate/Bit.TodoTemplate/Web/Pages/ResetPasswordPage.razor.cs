3b7488783f9cc9c694b9c4228839eae1a3550d22
Milad Karimifard <46561982+milad-karimifard@users.noreply.github.com>
2022-07-17 14:18:02 +0430

    fix(templates): resolve reset password issue in the TodoTemplate #2496 (#2579)

--- src/Templates/TodoTemplate/Bit.TodoTemplate/Web/Pages/ResetPasswordPage.razor.cs
+++ src/Templates/TodoTemplate/Bit.TodoTemplate/Web/Pages/ResetPasswordPage.razor.cs
@@ -45,9 +45,6 @@ public partial class ResetPasswordPage
 
         try
         {
-            ResetPasswordModel.Email = Email;
-            ResetPasswordModel.Token = Token;
-
             await httpClient.PostAsJsonAsync("Auth/ResetPassword", ResetPasswordModel, AppJsonContext.Default.ResetPasswordRequestDto);
 
             ResetPasswordMessageType = BitMessageBarType.Success;
@@ -72,6 +69,14 @@ public partial class ResetPasswordPage
         {
             IsLoading = false;
         }
+    }
+
+    protected override void OnInitialized()
+    {
+        ResetPasswordModel.Email = Email;
+        ResetPasswordModel.Token = Token;
+
+        base.OnInitialized();
     }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
