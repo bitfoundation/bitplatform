f48dd3178fe4c266074573e2ed9256cd0e10dee0
Sajad Kardel <sajadkardel@outlook.com>
2022-07-18 13:35:34 +0430

    fix(components): fix svg hover issue in the popular components section of the BlazorUI demo project #2483 (#2552)

---
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Components/PopularComponentsIcon.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bit.BlazorUI.Playground.Web.Components;
+
+public partial class PopularComponentsIcon
+{
+    [Parameter] public string ComponentName { get; set; }
+}
