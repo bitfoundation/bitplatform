0ae6c5972da211bcba8de211eeb21f0d2b7aad65
Saleh Yusefnejad <msynk@outlook.com>
2022-07-23 23:15:07 +0430

    feat(components): add the new EnableChunkedUpload parameter to the BitFileUpload #2670 (#2710)

--- src/BlazorUI/Bit.BlazorUI/Extensions/JsRuntimeExtension.cs
+++ src/BlazorUI/Bit.BlazorUI/Extensions/JsRuntimeExtension.cs
@@ -21,26 +21,6 @@ internal static class JsRuntimeExtension
     {
         return await jsRuntime.InvokeAsync<int>("Bit.getClientHeight", element);
     }
-    internal static async Task<IJSObjectReference?> InitUploaderDropZone(this IJSRuntime jsRuntime, ElementReference dragDropZoneElement, ElementReference inputFileElement)
-    {
-        return await jsRuntime.InvokeAsync<IJSObjectReference?>("BitFileUploader.initDropZone", dragDropZoneElement, inputFileElement);
-    }
-    internal static async Task<BitFileInfo[]?> InitUploader(this IJSRuntime jsRuntime, ElementReference element, DotNetObjectReference<BitFileUpload>? dotnetObjectReference, string uploadAddress, IReadOnlyDictionary<string, string> uploadRequestHttpHeaders)
-    {
-        if (uploadAddress.HasNoValue() || dotnetObjectReference is null) return null;
-
-        return await jsRuntime.InvokeAsync<BitFileInfo[]>("BitFileUploader.init", element, dotnetObjectReference, uploadAddress, uploadRequestHttpHeaders);
-    }
-
-    internal static async Task UploadFile(this IJSRuntime jsRuntime, long to, long from, int index = -1)
-    {
-        await jsRuntime.InvokeVoidAsync("BitFileUploader.upload", index, to, from);
-    }
-
-    internal static async Task PauseFile(this IJSRuntime jsRuntime, int index = -1)
-    {
-        await jsRuntime.InvokeVoidAsync("BitFileUploader.pause", index);
-    }
 
     internal static async Task<BoundingClientRect> GetBoundingClientRect(this IJSRuntime jsRuntime, ElementReference element)
     {
