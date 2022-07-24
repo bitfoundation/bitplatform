867c5ee836a8b8d7490106d7f9d58218e266a316
Mahsa Mohammadi <mohammadi.it91@yahoo.com>
2022-07-24 16:41:01 +0430

    feat(components): add unit test for the Placeholder parameter of the NumericTextField component #2244 (#2676)

---
+++ src/BlazorUI/Bit.BlazorUI.Tests/NumericTextField/BitNumericTextFieldGeneralTests.cs
@@ -0,0 +1,29 @@
+﻿using Bunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bit.BlazorUI.Tests.NumericTextField;
+
+[TestClass]
+public class BitNumericTextFieldGeneralTests : BunitTestContext
+{
+    [TestInitialize]
+    public void SetupJsInteropMode()
+    {
+        Context.JSInterop.Mode = JSRuntimeMode.Loose;
+    }
+
+    [DataTestMethod,
+     DataRow("The placeholder")]
+    public void BitNumericTextFieldShouldHaveCorrectPlaceholder(string placeholder)
+    {
+        var component = RenderComponent<BitNumericTextField<byte>>(parameters =>
+        {
+            parameters.Add(p => p.Placeholder, placeholder);
+        });
+
+        var numericTextFieldPlaceholder = component.Find(".bit-ntf-input");
+
+        Assert.IsTrue(numericTextFieldPlaceholder.HasAttribute("placeholder"));
+        Assert.AreEqual(numericTextFieldPlaceholder.GetAttribute("placeholder"), placeholder);
+    }
+}
