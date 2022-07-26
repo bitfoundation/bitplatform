1a0729335c876763ed27d86f846a236c645d19a4
Yaser Moradi <ysmoradi@outlook.com>
2022-07-26 11:22:48 +0430

    fix: resolve api url issues in the bit platform projects #2757 (#2758)

--- src/BlazorUI/Playground/Bit.BlazorUI.Playground/Api/Startup/Services.cs
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Api/Startup/Services.cs
@@ -24,7 +24,7 @@ public static class Services
             // you can inject HttpClient and call Bit.BlazorUI.Playground.Api api controllers in blazor pages.
             // for other usages of http client, for example calling 3rd party apis, please use services.AddHttpClient("NamedHttpClient"), 
             // then inject IHttpClientFactory and use its CreateClient("NamedHttpClient") method.
-            return new HttpClient { BaseAddress = new Uri(c.GetRequiredService<NavigationManager>().BaseUri) };
+            return new HttpClient { BaseAddress = new Uri($"{c.GetRequiredService<NavigationManager>().BaseUri}api/") };
         });
         services.AddScoped<Microsoft.AspNetCore.Components.WebAssembly.Services.LazyAssemblyLoader>();
         services.AddRazorPages();
