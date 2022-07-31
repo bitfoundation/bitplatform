8976237337fbd085b9fdd5e706c58b4ef4f17887
Saleh Yusefnejad <msynk@outlook.com>
2022-07-31 17:00:49 +0430

    feat(components): add the AutoPlayInterval parameter to the BitCarousel component #2832 (#2835)

--- src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/Carousel/BitCarouselDemo.razor.cs
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/Carousel/BitCarouselDemo.razor.cs
@@ -63,6 +63,13 @@ public partial class BitCarouselDemo
             Type = "bool",
             DefaultValue = "false",
             Description = "Enables/disables the auto scrolling of the slides."
+        },
+        new()
+        {
+            Name = "AutoPlayInterval",
+            Type = "double",
+            DefaultValue = "2000",
+            Description = "Sets the interval of the auto scrolling in milliseconds (the default value is 2000)."
         }
     };
 
@@ -367,7 +374,7 @@ private void GoTo()
     }
 </style>
 
-<BitCarousel ShowNextPrev=""false"" @ref=""carousel"">
+<BitCarousel ShowNextPrev=""false"" AutoPlay=""true"" AutoPlayInterval=""2500"">
     <BitCarouselItem>
         <div class=""numbertext"">1 / 4</div>
         <img class=""image"" src=""/images/carousel/img1.jpg"" />
