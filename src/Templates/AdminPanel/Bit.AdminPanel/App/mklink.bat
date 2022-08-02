baf653a2a9904a2d6177474f00f0ff4df9a79b8c
Yaser Moradi <ysmoradi@outlook.com>
2022-08-03 00:17:05 +0430

    fix: resolve solo file code reuse issue between app and web #2863 (#2864)

--- src/Templates/AdminPanel/Bit.AdminPanel/App/mklink.bat
+++ src/Templates/AdminPanel/Bit.AdminPanel/App/mklink.bat
@@ -6,16 +6,16 @@ if not exist "Services/." mklink /j "Services" "../Web/Services"
 if not exist "Components/." mklink /j "Components" "../Web/Components"
 if not exist "Extensions/." mklink /j "Extensions" "../Web/Extensions"
 
-if not exist Main.razor mklink /h "Main.razor" "../Web/App.razor"
-if not exist tsconfig.json mklink /h "tsconfig.json" "../Web/tsconfig.json"
-if not exist _Imports.razor mklink /h "_Imports.razor" "../Web/_Imports.razor"
-if not exist appsettings.json mklink /h "appsettings.json" "../Web/appsettings.json"
-if not exist compilerconfig.json mklink /h "compilerconfig.json" "../Web/compilerconfig.json"
-if not exist compilerconfig.json.defaults mklink /h "compilerconfig.json.defaults" "../Web/compilerconfig.json.defaults"
+if not exist Main.razor mklink "Main.razor" "%cd%/../Web/App.razor"
+if not exist tsconfig.json mklink "tsconfig.json" "%cd%/../Web/tsconfig.json"
+if not exist _Imports.razor mklink "_Imports.razor" "%cd%/../Web/_Imports.razor"
+if not exist appsettings.json mklink "appsettings.json" "%cd%/../Web/appsettings.json"
+if not exist compilerconfig.json mklink "compilerconfig.json" "%cd%/../Web/compilerconfig.json"
+if not exist compilerconfig.json.defaults mklink "compilerconfig.json.defaults" "%cd%/../Web/compilerconfig.json.defaults"
 
 cd wwwroot
 
 if not exist "images/." mklink /j "images" "../../Web/wwwroot/images"
 
-if not exist service-worker.js mklink /h "service-worker.js" "../../Web/wwwroot/service-worker.js"
-if not exist service-worker.published.js mklink /h "service-worker.published.js" "../../Web/wwwroot/service-worker.published.js"
+if not exist service-worker.js mklink "service-worker.js" "%cd%/../../Web/wwwroot/service-worker.js"
+if not exist service-worker.published.js mklink "service-worker.published.js" "%cd%/../../Web/wwwroot/service-worker.published.js"
