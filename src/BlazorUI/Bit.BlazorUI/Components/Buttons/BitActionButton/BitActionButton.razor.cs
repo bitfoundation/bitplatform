fac1724049f3b06936c66e2cb8f8af77fdc972ac
Steven Giesel <stgiesel35@gmail.com>
2022-08-03 08:13:33 +0200

    fix: Button inside form defaults to submit

--- src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitActionButton/BitActionButton.razor.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitActionButton/BitActionButton.razor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace Bit.BlazorUI;
@@ -51,7 +52,12 @@ public partial class BitActionButton
     /// <summary>
     /// The type of the button
     /// </summary>
-    [Parameter] public BitButtonType ButtonType { get; set; } = BitButtonType.Button;
+    [Parameter] public BitButtonType? ButtonType { get; set; }
+
+    /// <summary>
+    /// The EditContext, which is set if the button is inside an <see cref="EditForm"/>
+    /// </summary>
+    [CascadingParameter] public EditContext? EditContext { get; set; }
 
     /// <summary>
     /// Callback for when the button clicked
@@ -66,6 +72,8 @@ public partial class BitActionButton
         {
             tabIndex = AllowDisabledFocus ? null : -1;
         }
+        
+        ButtonType ??= EditContext is null ? BitButtonType.Button : BitButtonType.Submit;
 
         await base.OnInitializedAsync();
     }
