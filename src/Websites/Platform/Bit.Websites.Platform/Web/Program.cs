3387f03fe75bf3cbb3225fe6a84326cda8b10617
Yaser Moradi <ysmoradi@outlook.com>
2022-08-04 17:09:06 +0430

    feat: Enhance debug experience of the bit platform projects outside of Visual Studio #2894 (#2895)

--- src/Websites/Platform/Bit.Websites.Platform/Web/Program.cs
+++ src/Websites/Platform/Bit.Websites.Platform/Web/Program.cs
@@ -35,7 +35,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
 #if DEBUG
-        builder.WebHost.UseUrls("https://*:4001", "http://*:4000");
+        builder.WebHost.UseUrls("https://localhost:4001", "http://localhost:4000", "https://*:4001", "http://*:4000");
 #endif
 
         Startup.Services.Add(builder.Services, builder.Configuration);
--- src/Websites/Sales/Bit.Websites.Sales/Api/Program.cs
+++ src/Websites/Sales/Bit.Websites.Sales/Api/Program.cs
@@ -2,7 +2,7 @@
 
 #if DEBUG
 // The following line (using the * in the URL), allows the emulators and mobile devices to access the app using the host IP address.
-builder.WebHost.UseUrls("https://*:5001", "http://*:5000");
+builder.WebHost.UseUrls("https://localhost:5001", "http://localhost:5000", "https://*:5001", "http://*:5000");
 #endif
 
 Bit.Websites.Sales.Api.Startup.Services.Add(builder.Services, builder.Environment, builder.Configuration);
