b74c7dbb0846c9028e1869db1527b79b0e31a56f
ysmoradi <ysmoradi@outlook.com>
2022-08-06 23:07:58 +0430

    Applied  code review

---
+++ src/Templates/AdminPanel/Bit.AdminPanel/Web/Components/NotAuthorizedComponent.razor.cs
@@ -0,0 +1,43 @@
+﻿namespace AdminPanel.App.Components;
+
+public partial class NotAuthorizedComponent
+{
+    [AutoInject] private IAuthenticationService authenticationService = default!;
+
+    [CascadingParameter] public Task<AuthenticationState> AuthenticationState { get; set; } = default!;
+
+    public ClaimsPrincipal User { get; set; } = default!;
+
+    async Task SignIn()
+    {
+        await authenticationService.SignOut();
+
+        RedirectToSignInPage();
+    }
+
+    protected async override Task OnParamsSetAsync()
+    {
+        User = (await AuthenticationState).User;
+
+        await base.OnParamsSetAsync();
+    }
+
+    void RedirectToSignInPage()
+    {
+        var redirectUrl = navigationManager.ToBaseRelativePath(navigationManager.Uri);
+        navigationManager.NavigateTo($"/sign-in?redirectUrl={redirectUrl}");
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender)
+        {
+            if (User.Identity?.IsAuthenticated is false)
+            {
+                RedirectToSignInPage();
+            }
+        }
+
+        base.OnAfterRender(firstRender);
+    }
+}
