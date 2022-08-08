3cc3e23aa526b8a62a6e7dac2e3db0b2231fdf16
Maryam Heydarian <maryamhdr76@gmail.com>
2022-08-08 19:08:45 +0430

    implement header #2680

--- src/Templates/AdminPanel/Bit.AdminPanel/Web/Components/Header.razor.cs
+++ src/Templates/AdminPanel/Bit.AdminPanel/Web/Components/Header.razor.cs
@@ -1,24 +1,61 @@
-﻿namespace AdminPanel.App.Components;
+﻿using AdminPanel.Shared.Dtos.Account;
+
+namespace AdminPanel.App.Components;
 
 public partial class Header : IAsyncDisposable
 {
+    [AutoInject] private HttpClient httpClient = default!;
+
+#if BlazorServer || BlazorHybrid
+    [AutoInject] private IConfiguration configuration = default!;
+#endif
+
+    [AutoInject] private IAuthTokenProvider authTokenProvider = default!;
+
     [AutoInject] private IStateService stateService = default!;
 
     [AutoInject] private AppAuthenticationStateProvider authStateProvider = default!;
 
     [AutoInject] private IExceptionHandler exceptionHandler = default!;
 
+    [AutoInject] private NavigationManager navigationManager = default!;
+
     [Parameter] public EventCallback OnToggleMenu { get; set; }
+
+    public UserDto? User { get; set; } = new();
+
+    public string? ProfileImageUrl { get; set; }
 
     public bool IsUserAuthenticated { get; set; }
 
+    public bool IsHeaderDrpDownOpen { get; set; }
+
+    public bool IsSignOutModalOpen { get; set; }
+
     protected override async Task OnInitAsync()
     {
+        User = await stateService.GetValue($"{nameof(NavMenu)}-{nameof(User)}", async () =>
+            await httpClient.GetFromJsonAsync("User/GetCurrentUser", AppJsonContext.Default.UserDto));
+
+        var access_token = await stateService.GetValue($"{nameof(NavMenu)}-access_token", async () =>
+            await authTokenProvider.GetAcccessToken());
+
+        ProfileImageUrl = $"{GetBaseUrl()}Attachment/GetProfileImage?access_token={access_token}&file={User!.ProfileImageName}";
+
         authStateProvider.AuthenticationStateChanged += VerifyUserIsAuthenticatedOrNot;
 
         IsUserAuthenticated = await stateService.GetValue($"{nameof(Header)}-{nameof(IsUserAuthenticated)}", authStateProvider.IsUserAuthenticated);
 
         await base.OnInitAsync();
+    }
+
+    string GetBaseUrl()
+    {
+#if BlazorWebAssembly
+        return "/api/";
+#else
+        return configuration.GetValue<string>("ApiServerAddress");
+#endif
     }
 
     async void VerifyUserIsAuthenticatedOrNot(Task<AuthenticationState> task)
@@ -40,6 +77,23 @@ public partial class Header : IAsyncDisposable
     private async Task ToggleMenu()
     {
         await OnToggleMenu.InvokeAsync();
+    }
+
+    private void OpenSignOutModal()
+    {
+        IsSignOutModalOpen = true;
+        StateHasChanged();
+    }
+
+    private void ToggleHeaderDrpDown()
+    {
+        IsHeaderDrpDownOpen = !IsHeaderDrpDownOpen;
+    }
+
+    private void OpenEditProfilePage()
+    {
+        ToggleHeaderDrpDown();
+        navigationManager.NavigateTo("/edit-profile");
     }
 
     public async ValueTask DisposeAsync()
