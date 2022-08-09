2d27f3cd44d3fcee52a1830b86d9610788ff9712
Maryam Heydarian <maryamhdr76@gmail.com>
2022-08-09 12:19:02 +0430

    finalize category pages #2680

--- src/Templates/AdminPanel/Bit.AdminPanel/Api/Data/Configurations/Category/CategoryConfiguration.cs
+++ src/Templates/AdminPanel/Bit.AdminPanel/Api/Data/Configurations/Category/CategoryConfiguration.cs
@@ -7,11 +7,11 @@ public class CategoryConfiguration : IEntityTypeConfiguration<Category>
     public void Configure(EntityTypeBuilder<Category> builder)
     {
         builder.HasData(
-            new Category() { Id = 1, Name = "Ford", Color = "#ff0000" },
-            new Category() { Id = 2, Name = "Nissan", Color = "#0300ff" },
-            new Category() { Id = 3, Name = "Benz", Color = "#00f800" },
-            new Category() { Id = 4, Name = "BMW", Color = "#fefe00" },
-            new Category() { Id = 5, Name = "Tesla", Color = "#fe04fe" });
+            new Category() { Id = 1, Name = "Ford", Color = "#FFCD56" },
+            new Category() { Id = 2, Name = "Nissan", Color = "#FF6384" },
+            new Category() { Id = 3, Name = "Benz", Color = "#4BC0C0" },
+            new Category() { Id = 4, Name = "BMW", Color = "#FF9124" },
+            new Category() { Id = 5, Name = "Tesla", Color = "#2B88D8" });
     }
 }
 
