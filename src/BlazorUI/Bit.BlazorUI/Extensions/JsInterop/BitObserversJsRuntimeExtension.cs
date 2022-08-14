9ba8638c2e5c603b9164ca147e20b7d053f611dd
Saleh Yusefnejad <msynk@outlook.com>
2022-08-14 13:13:48 +0430

    feat(components): handle resize in the BitCarousel component #2793 (#2991)

---
+++ src/BlazorUI/Bit.BlazorUI/Extensions/JsInterop/BitObserversJsRuntimeExtension.cs
@@ -0,0 +1,16 @@
+﻿using Bit.BlazorUI;
+
+namespace Microsoft.JSInterop;
+
+internal static class BitObserversJsRuntimeExtension
+{
+    internal static async Task<string> RegisterResizeObserver<T>(this IJSRuntime jsRuntime, ElementReference element, DotNetObjectReference<T> obj, string method) where T : class
+    {
+        return await jsRuntime.InvokeAsync<string>("BitObservers.resize", element, obj, method);
+    }
+
+    internal static async Task UnregisterResizeObserver(this IJSRuntime jsRuntime, ElementReference element, string id)
+    {
+        await jsRuntime.InvokeVoidAsync("BitObservers.unresize", element, id);
+    }
+}
---
+++ src/BlazorUI/Bit.BlazorUI/Scripts/general.ts
@@ -0,0 +1,38 @@
+﻿interface DotNetObject {
+    invokeMethod<T>(methodIdentifier: string, ...args: any[]): T;
+    invokeMethodAsync<T>(methodIdentifier: string, ...args: any[]): Promise<T>;
+}
+
+class BitCalloutComponent {
+    calloutId: string;
+    overlayId: string;
+    objRef: DotNetObject | null;
+
+    constructor() {
+        this.calloutId = "";
+        this.overlayId = "";
+        this.objRef = null;
+    }
+
+    update(calloutId: string, overlayId: string, obj: DotNetObject | null) {
+        this.calloutId = calloutId;
+        this.overlayId = overlayId;
+        this.objRef = obj;
+    }
+}
+
+window.addEventListener('load', e => {
+    Bit.init();
+})
+
+window.addEventListener('scroll', (e: any) => {
+    const minimumWidthForDropDownNormalOpen = 640;
+    if ((Bit.currentDropDownCalloutId && window.innerWidth < minimumWidthForDropDownNormalOpen && Bit.currentDropDownCalloutResponsiveModeIsEnabled) ||
+        (e.target.id && Bit.currentDropDownCalloutId === e.target.id)) return;
+
+    Bit.closeCurrentCalloutIfExists("", "", null);
+}, true);
+
+window.addEventListener('resize', (e: any) => {
+    Bit.closeCurrentCalloutIfExists("", "", null);
+}, true);
