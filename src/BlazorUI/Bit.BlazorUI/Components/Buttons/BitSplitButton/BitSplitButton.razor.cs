3a123598a31ac104b770ad326be8486f41692f4c
sajad kardel <sajadkardel@outlook.com>
2022-08-16 17:35:30 +0430

    Implement of basic stracture #2972

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitSplitButton/BitSplitButton.razor.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Bit.BlazorUI;
+
+public partial class BitSplitButton
+{
+    private BitSplitButtonItem Item = new();
+
+    private BitButtonStyle buttonStyle = BitButtonStyle.Primary;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter] public IEnumerable<BitSplitButtonItem> Items { get; set; } = new List<BitSplitButtonItem>();
+
+    /// <summary>
+    /// The style of button, Possible values: Primary | Standard
+    /// </summary>
+    [Parameter]
+    public BitButtonStyle ButtonStyle
+    {
+        get => buttonStyle;
+        set
+        {
+            buttonStyle = value;
+            ClassBuilder.Reset();
+        }
+    }
+
+    protected override string RootElementClass => "splt-btn";
+
+    protected override async Task OnInitializedAsync()
+    {
+        var item = Items.FirstOrDefault(i => i.DefaultIsSelected);
+
+        if (item is not null)
+        {
+            Item = item;
+        }
+        else
+        {
+            Item = Items.First();
+        }
+
+        await base.OnInitializedAsync();
+    }
+
+    private void HandleItemOnClick(BitSplitButtonItem item)
+    {
+        if (item.IsEnabled is false || IsEnabled is false) return;
+
+        Item = item;
+
+        if (item.OnClick is not null)
+        {
+            item.OnClick.Invoke();
+        }
+    }
+}
---
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitSplitButton/BitSplitButtonItem.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Bit.BlazorUI;
+
+public class BitSplitButtonItem
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public BitIconName? IconName { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? Text { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? Value { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public Action? OnClick { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public RenderFragment<BitSplitButtonItem>? ItemTemplate { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool DefaultIsSelected { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool IsEnabled { get; set; } = true;
+}
