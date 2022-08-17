106fabc29a4a4831ff8a510e165200b116e49359
Milad Karimifard <karimifard.milad@gmail.com>
2022-08-17 13:03:19 +0430

    Apply improvement to code analysis

--- src/CodeAnalyzers/Bit.CodeAnalyzers/SystemAnalyzers/AsyncVoidMethodAnalyzer.cs
+++ src/CodeAnalyzers/Bit.CodeAnalyzers/SystemAnalyzers/AsyncVoidMethodAnalyzer.cs
@@ -10,15 +10,21 @@ namespace Bit.CodeAnalyzers.SystemAnalyzers;
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public class AsyncVoidMethodAnalyzer : DiagnosticAnalyzer
 {
-    private const string DiagnosticId = nameof(AsyncVoidMethodAnalyzer);
-    private const string Title = "Avoid using 'async void'";
-    private const string Message = "Either change the return type to 'Task' or use try/catch to handle the exceptions.";
-    private const string Description = Message;
     private const string Category = "System";
+    private const string Title = "Avoid using 'async void'";
 
-    private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Message, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+    private const string DefaultRuleDiagnosticId = nameof(AsyncVoidMethodAnalyzer) + "1";
+    private const string DefaultMessage = "Either change the return type to 'Task' or use try/catch to handle the exceptions.";
+    private const string DefaultDescription = DefaultMessage;
+    private static readonly DiagnosticDescriptor DefaultRule = new DiagnosticDescriptor(DefaultRuleDiagnosticId, Title, DefaultMessage, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: DefaultDescription);
 
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+    private const string CorrenctUsageOfTryStatementRuleDiagnosticId = nameof(AsyncVoidMethodAnalyzer) + "2";
+    private const string CorrenctUsageOfTryStatementMessage = "The try/catch block must contain whole body to capable of handling error";
+    private const string CorrenctUsageOfTryStatementDescription = CorrenctUsageOfTryStatementMessage;
+    private static readonly DiagnosticDescriptor CorrenctUsageOfTryStatementRule = new DiagnosticDescriptor(CorrenctUsageOfTryStatementRuleDiagnosticId, Title, CorrenctUsageOfTryStatementMessage, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: CorrenctUsageOfTryStatementDescription);
+
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(DefaultRule, CorrenctUsageOfTryStatementRule);
 
 
     public override void Initialize(AnalysisContext context)
@@ -51,11 +57,28 @@ public class AsyncVoidMethodAnalyzer : DiagnosticAnalyzer
         if (returnType.Keyword.IsKind(SyntaxKind.VoidKeyword) is false)
             return;
 
-        if (methodDec.Body?.Statements.FirstOrDefault()?.IsKind(SyntaxKind.TryStatement) is true)
+        if (methodDec.Body?.Statements.FirstOrDefault()?.IsKind(SyntaxKind.TryStatement) is false)
+        {
+            RepoertError(context, root, DefaultRule);
+            return;
+        }
+
+        if (IsTryStatementUsedCorrectly(methodDec) is true)
             return;
 
-        Diagnostic diagnostic = Diagnostic.Create(Rule, root.GetLocation(), Message);
+        RepoertError(context, root, CorrenctUsageOfTryStatementRule);
+    }
 
+    private bool IsTryStatementUsedCorrectly(MethodDeclarationSyntax methodDeclaration)
+    {
+        return
+            methodDeclaration.Body?.Statements.FirstOrDefault()?.IsKind(SyntaxKind.TryStatement) is true &&
+            methodDeclaration.Body?.Statements.Count == 1;
+    }
+
+    private void RepoertError(SyntaxNodeAnalysisContext context, SyntaxNode node, DiagnosticDescriptor rule)
+    {
+        Diagnostic diagnostic = Diagnostic.Create(rule, node.GetLocation());
         context.ReportDiagnostic(diagnostic);
     }
 }
