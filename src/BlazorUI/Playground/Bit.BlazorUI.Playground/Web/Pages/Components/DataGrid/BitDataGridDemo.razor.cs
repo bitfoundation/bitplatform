38778a9943eedaa761727082c99ff35eb678c292
m.sangtarash <m.sangtarash@outlook.com>
2022-08-21 14:05:44 +0430

    Fix grid responsive issue in mobile size

--- src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/DataGrid/BitDataGridDemo.razor.cs
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/DataGrid/BitDataGridDemo.razor.cs
@@ -298,6 +298,10 @@ public partial class BitDataGridDemo
   vertical-align: middle;
 }
 
+.grid-container {
+    overflow: auto;
+}
+
 .grid ::deep .column--large {
   width: 220px;
 }
@@ -446,22 +450,24 @@ public partial class BitDataGridDemo
 </style>
 
 <div class=""grid"">
-    <BitDataGrid Items=""@FilteredItems"" ResizableColumns=""true"" Pagination=""@pagination"">                
-        <BitDataGridPropertyColumn Class=""column--large"" Property=""@(c => c.Name)"" Sortable=""true"" IsDefaultSort=""BitDataGridSortDirection.Ascending"">
-            <ColumnOptions>
-                <BitSearchBox @bind-Value=""typicalSampleNameFilter""
-                                Placeholder=""Search on Name""
-                                InputHtmlAttributes=""@(new Dictionary<string, object> {{""autofocus"", true}})"" />
-            </ColumnOptions>
-        </BitDataGridPropertyColumn>
-            <BitDataGridTemplateColumn Title=""Flag"" Align=""BitDataGridAlign.Center"" >
-            <img class=""flag"" src=""images/flags/@(context.Code).png"" />
-        </BitDataGridTemplateColumn>
-        <BitDataGridPropertyColumn Property=""@(c => c.Medals.Gold)"" Sortable=""true"" />
-        <BitDataGridPropertyColumn Property=""@(c => c.Medals.Silver)"" Sortable=""true"" />
-        <BitDataGridPropertyColumn Property=""@(c => c.Medals.Bronze)"" Sortable=""true"" />
-        <BitDataGridPropertyColumn Property=""@(c => c.Medals.Total)"" Sortable=""true"" />
-    </BitDataGrid>
+    <div class=""grid-container"">
+        <BitDataGrid Items=""@FilteredItems"" ResizableColumns=""true"" Pagination=""@pagination"">                
+            <BitDataGridPropertyColumn Class=""column--large"" Property=""@(c => c.Name)"" Sortable=""true"" IsDefaultSort=""BitDataGridSortDirection.Ascending"">
+                <ColumnOptions>
+                    <BitSearchBox @bind-Value=""typicalSampleNameFilter""
+                                    Placeholder=""Search on Name""
+                                    InputHtmlAttributes=""@(new Dictionary<string, object> {{""autofocus"", true}})"" />
+                </ColumnOptions>
+            </BitDataGridPropertyColumn>
+                <BitDataGridTemplateColumn Title=""Flag"" Align=""BitDataGridAlign.Center"" >
+                <img class=""flag"" src=""images/flags/@(context.Code).png"" />
+            </BitDataGridTemplateColumn>
+            <BitDataGridPropertyColumn Property=""@(c => c.Medals.Gold)"" Sortable=""true"" />
+            <BitDataGridPropertyColumn Property=""@(c => c.Medals.Silver)"" Sortable=""true"" />
+            <BitDataGridPropertyColumn Property=""@(c => c.Medals.Bronze)"" Sortable=""true"" />
+            <BitDataGridPropertyColumn Property=""@(c => c.Medals.Total)"" Sortable=""true"" />
+        </BitDataGrid>
+    </div>
     <BitDataGridPaginator Value=""@pagination"" />
 </div>";
     private readonly string example1CSharpCode = @"
