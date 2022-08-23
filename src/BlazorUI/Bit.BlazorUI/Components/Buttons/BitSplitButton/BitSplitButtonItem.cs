d906b750888b5415eb4e8274c71ea6c40061f96d
Sajad Kardel <sajadkardel@outlook.com>
2022-08-23 12:49:14 +0430

    feat(components): add BitSplitButton component #2972 (#3045)

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitSplitButton/BitSplitButtonItem.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Bit.BlazorUI;
+
+public class BitSplitButtonItem
+{
+    /// <summary>
+    /// Name of an icon to render next to the item text
+    /// </summary>
+    public BitIconName? IconName { get; set; }
+
+    /// <summary>
+    /// Text to render in the item
+    /// </summary>
+    public string? Text { get; set; }
+
+    /// <summary>
+    /// A unique value to use as a key of the item
+    /// </summary>
+    public string? key { get; set; }
+
+    /// <summary>
+    /// The content inside the item can be customized.
+    /// </summary>
+    public RenderFragment<BitSplitButtonItem>? ItemTemplate { get; set; }
+
+    /// <summary>
+    /// Whether or not the item is enabled.
+    /// </summary>
+    public bool IsEnabled { get; set; } = true;
+}
