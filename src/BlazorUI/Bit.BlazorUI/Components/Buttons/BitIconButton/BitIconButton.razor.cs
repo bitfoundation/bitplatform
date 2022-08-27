794169946f7b39047c35a1eecc8d7d9bd5170cf8
Sajad Kardel <sajadkardel@outlook.com>
2022-08-27 14:27:19 +0430

    fix(components): resolve issues of the BitIconButton component #3067 (#3099)

--- src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitIconButton/BitIconButton.razor.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitIconButton/BitIconButton.razor.cs
@@ -1,13 +1,10 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Forms;
 
 namespace Bit.BlazorUI;
 
 public partial class BitIconButton
 {
-    private int? tabIndex;
+    private int? _tabIndex;
 
     /// <summary>
     /// Whether the icon button can have focus in disabled mode
@@ -25,31 +22,6 @@ public partial class BitIconButton
     [Parameter] public bool AriaHidden { get; set; }
 
     /// <summary>
-    /// The icon name for the icon shown in the button
-    /// </summary>
-    [Parameter] public BitIconName IconName { get; set; }
-
-    /// <summary>
-    /// The tooltip to show when the mouse is placed on the icon button
-    /// </summary>
-    [Parameter] public string? Title { get; set; }
-
-    /// <summary>
-    /// Callback for when the button clicked
-    /// </summary>
-    [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
-
-    /// <summary>
-    /// URL the link points to, if provided, button renders as an anchor
-    /// </summary>
-    [Parameter] public string? Href { get; set; }
-
-    /// <summary>
-    /// If Href provided, specifies how to open the link
-    /// </summary>
-    [Parameter] public string? Target { get; set; }
-
-    /// <summary>
     /// The type of the button
     /// </summary>
     [Parameter] public BitButtonType? ButtonType { get; set; }
@@ -59,13 +31,38 @@ public partial class BitIconButton
     /// </summary>
     [CascadingParameter] public EditContext? EditContext { get; set; }
 
-    protected override string RootElementClass => "bit-ico-btn";
+    /// <summary>
+    /// URL the link points to, if provided, button renders as an anchor
+    /// </summary>
+    [Parameter] public string? Href { get; set; }
+
+    /// <summary>
+    /// The icon name for the icon shown in the button
+    /// </summary>
+    [Parameter] public BitIconName IconName { get; set; }
+
+    /// <summary>
+    /// Callback for when the button clicked
+    /// </summary>
+    [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    /// <summary>
+    /// The tooltip to show when the mouse is placed on the icon button
+    /// </summary>
+    [Parameter] public string? Title { get; set; }
+
+    /// <summary>
+    /// If Href provided, specifies how to open the link
+    /// </summary>
+    [Parameter] public string? Target { get; set; }
+
+    protected override string RootElementClass => "bit-icob";
 
     protected override async Task OnInitializedAsync()
     {
-        if (!IsEnabled)
+        if (IsEnabled is false)
         {
-            tabIndex = AllowDisabledFocus ? null : -1;
+            _tabIndex = AllowDisabledFocus ? null : -1;
         }
         
         ButtonType ??= EditContext is null ? BitButtonType.Button : BitButtonType.Submit;
