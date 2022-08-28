676a6621d0655db545e043cb69287aae6af47fd7
sajad kardel <sajadkardel@outlook.com>
2022-08-28 22:29:22 +0430

    Add BitAccordion component #3035

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Accordion/BitAccordion.razor.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Bit.BlazorUI;
+
+public partial class BitAccordion
+{
+    private IEnumerable<BitAccordionItem> _items = default!;
+
+    [Parameter] public IEnumerable<BitAccordionItem> Items { get; set; } = new List<BitAccordionItem>();
+
+    [Parameter] public bool DefaultIsExpanded { get; set; }
+
+    [Parameter] public bool OnlyOneIsExpand { get; set; }
+
+    [Parameter] public EventCallback<BitAccordionItem> OnClick { get; set; }
+
+    protected override string RootElementClass => "bit-acd";
+
+    protected override async Task OnInitializedAsync()
+    {
+        _items = Items;
+
+        if (DefaultIsExpanded)
+        {
+            if (OnlyOneIsExpand)
+            {
+                _items.First().IsExpanded = true;
+            }
+            else
+            {
+                foreach (var item in _items)
+                {
+                    item.IsExpanded = true;
+                }
+            }
+        }
+
+        await base.OnInitializedAsync();
+    }
+
+    private async Task HandleOnItemClick(BitAccordionItem item)
+    {
+        if (IsEnabled is false || item.IsEnabled is false) return;
+
+        if (OnlyOneIsExpand && _items.Any(i => i != item && i.IsExpanded))
+        {
+            _items.First(i => i != item && i.IsExpanded).IsExpanded = false;
+        }
+
+        _items.First(i => i == item).IsExpanded = !(_items.First(i => i == item).IsExpanded);
+
+        await OnClick.InvokeAsync(item);
+
+        StateHasChanged();
+    }
+}
---
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/Accordion/BitAccordionDemo.razor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Bit.BlazorUI.Playground.Web.Pages.Components.Accordion;
+
+public partial class BitAccordionDemo
+{
+    public List<BitAccordionItem> BitAccordionItems { get; set; } = new List<BitAccordionItem>
+    {
+        new BitAccordionItem
+        {
+            Title = "title-1",
+            Description = "a simple description",
+            Text = "BitDataGrid is a robust way to display an information-rich collection of items, and allow people to sort, and filter the content. Use a data-grid when information density is critical."
+        },
+        new BitAccordionItem
+        {
+            Title = "title-2",
+            Text = "BitDataGrid is a robust way to display an information-rich collection of items, and allow people to sort, and filter the content. Use a data-grid when information density is critical.",
+            IsEnabled = false,
+        },
+        new BitAccordionItem
+        {
+            Title = "title-3",
+            Text = "BitDataGrid is a robust way to display an information-rich collection of items, and allow people to sort, and filter the content. Use a data-grid when information density is critical.",
+            IsExpanded = true
+        },
+        new BitAccordionItem
+        {
+            Title = "title-4",
+            Text = "BitDataGrid is a robust way to display an information-rich collection of items, and allow people to sort, and filter the content. Use a data-grid when information density is critical."
+        },
+    };
+}
