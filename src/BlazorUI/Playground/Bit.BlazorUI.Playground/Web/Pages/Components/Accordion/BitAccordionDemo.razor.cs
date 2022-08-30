cdc2021c84108ab7e51089f9f4566bb11cd45360
sajad kardel <sajadkardel@outlook.com>
2022-08-30 17:16:59 +0430

    Improment #3035

--- src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/Accordion/BitAccordionDemo.razor.cs
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/Accordion/BitAccordionDemo.razor.cs
@@ -4,6 +4,6 @@ namespace Bit.BlazorUI.Playground.Web.Pages.Components.Accordion;
 
 public partial class BitAccordionDemo
 {
-    private bool isExpanded_1;
-    private bool isExpanded_2;
+    private bool basicAccordionIsExpanded;
+    private bool templateedAccordionIsExpanded;
 }
