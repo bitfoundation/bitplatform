f18db5e416f68c65898eb12bdbb17fe472a26890
ysmoradi <ysmoradi@outlook.com>
2022-09-07 17:11:52 +0430

    Fix android release issues

--- src/BlazorUI/Playground/Bit.BlazorUI.Playground/App/Platforms/Android/MainApplication.cs
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/App/Platforms/Android/MainApplication.cs
@@ -1,8 +1,11 @@
 ﻿using Android.App;
 using Android.Runtime;
+using Microsoft.AspNetCore.Components.WebView.Maui;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 using System;
+
+[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 
 namespace Bit.BlazorUI.Playground.Web
 {
@@ -13,9 +16,29 @@ namespace Bit.BlazorUI.Playground.Web
     )]
     public class MainApplication : MauiApplication
     {
+        static MainApplication()
+        {
+            BlazorWebViewHandler.BlazorWebViewMapper.AppendToMapping("CustomBlazorWebViewMapper", (handler, view) =>
+            {
+                Android.Webkit.WebSettings settings = handler.PlatformView.Settings;
+
+                settings.AllowFileAccessFromFileURLs =
+                    settings.AllowUniversalAccessFromFileURLs =
+                    settings.AllowContentAccess =
+                    settings.AllowFileAccess =
+                    settings.DatabaseEnabled =
+                    settings.JavaScriptCanOpenWindowsAutomatically =
+                    settings.DomStorageEnabled = true;
+
+                settings.BlockNetworkLoads =
+                    settings.BlockNetworkImage = false;
+            });
+        }
+
         public MainApplication(IntPtr handle, JniHandleOwnership ownership)
             : base(handle, ownership)
         {
+
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram
