a81b76232be757e1f99878fadc949d8a6f35b582
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2022-09-10 20:40:27 +0430

    Fix tests

--- src/BlazorUI/Bit.BlazorUI.Tests/Inputs/BitDropDownTests.cs
+++ src/BlazorUI/Bit.BlazorUI.Tests/Inputs/BitDropDownTests.cs
@@ -270,12 +270,15 @@ public class BitDropDownTests : BunitTestContext
 
         selectedItems.ForEach(i =>
         {
-            if (expectedText.HasValue())
+            if (i.IsSelected && i.ItemType == BitDropDownItemType.Normal)
             {
-                expectedText += component.Instance.MultiSelectDelimiter;
-            }
+                if (expectedText.HasValue())
+                {
+                    expectedText += component.Instance.MultiSelectDelimiter;
+                }
 
-            expectedText += i.Text;
+                expectedText += i.Text;
+            }
         });
 
         Assert.AreEqual(expectedText, textSpan.InnerHtml);
