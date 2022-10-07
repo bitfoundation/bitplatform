faa5b3d88d2fe6c72616d17f8ebb3d05cfba9f14
sajad kardel <sajadkardel@outlook.com>
2022-10-08 01:01:54 +0330

    Remove unused namespace #3204

--- src/BlazorUI/Bit.BlazorUI.Tests/OtpInput/BitOtpInputTests.cs
+++ src/BlazorUI/Bit.BlazorUI.Tests/OtpInput/BitOtpInputTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using AngleSharp.Css.Dom;
-using AngleSharp.Diffing.Extensions;
-using AngleSharp.Dom;
-using Bunit;
+﻿using Bunit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bit.BlazorUI.Tests.OtpInput;
