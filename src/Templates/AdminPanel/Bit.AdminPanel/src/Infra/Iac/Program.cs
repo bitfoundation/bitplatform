ddb41010517812ada0737ccb9ce9a201c1bc2523
Yaser Moradi <ysmoradi@outlook.com>
2022-10-31 15:05:22 +0330

    Initial azure devops pipelines

--- src/Templates/AdminPanel/Bit.AdminPanel/src/Infra/Iac/Program.cs
+++ src/Templates/AdminPanel/Bit.AdminPanel/src/Infra/Iac/Program.cs
@@ -3,18 +3,5 @@ using AdminPanel.Iac;
 
 public class Program
 {
-    static Task<int> Main() => Deployment.RunAsync<StackSelector>();
-}
-
-public class StackSelector : Stack
-{
-    public StackSelector()
-    {
-        string stackName = Deployment.Instance.StackName;
-
-        if (stackName == "cd")
-            new CdStack(); // azure devops agent
-        else
-            new AdStack(); // test / prod
-    }
+    static Task<int> Main() => Deployment.RunAsync<AdStack>();
 }
