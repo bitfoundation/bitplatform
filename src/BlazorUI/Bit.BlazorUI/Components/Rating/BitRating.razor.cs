b430bf7402b14e3fd179619c25122a1484d1d4be
sajad kardel <sajadkardel@outlook.com>
2022-11-23 01:00:34 +0330

    Resolve issues of the BitRating component #3558

--- src/BlazorUI/Bit.BlazorUI/Components/Rating/BitRating.razor.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Rating/BitRating.razor.cs
@@ -1,13 +1,34 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace Bit.BlazorUI;
 
 public partial class BitRating
 {
     private bool isReadOnly;
+    private double _min;
+
+    /// <summary>
+    /// Allow the initial rating value be 0. Note that a value of 0 still won't be selectable by mouse or keyboard
+    /// </summary>
+    [Parameter] public bool AllowZeroStars { get; set; }
+
+    /// <summary>
+    /// Optional label format for each individual rating star (not the rating control as a whole) that will be read by screen readers. 
+    /// Placeholder {0} is the current rating and placeholder {1} is the max: for example, 
+    /// "Select {0} of {1} stars". (To set the label for the control as a whole, use getAriaLabel or aria-label.)
+    /// </summary>
+    [Parameter] public string? AriaLabelFormat { get; set; }
+
+    /// <summary>
+    /// Default value. Must be a number between _min and max. 
+    /// Only provide this if the CurrentValue is an uncontrolled component; otherwise, use the Value property.
+    /// </summary>
+    [Parameter] public double? DefaultValue { get; set; }
+
+    /// <summary>
+    /// Optional callback to set the aria-label for rating control in readOnly mode. Also used as a fallback aria-label if ariaLabel prop is not provided.
+    /// </summary>
+    [Parameter] public Func<double, double, string>? GetAriaLabel { get; set; }
 
     /// <summary>
     /// A flag to mark rating control as readOnly
@@ -24,37 +45,19 @@ public partial class BitRating
     }
 
     /// <summary>
-    /// Allow the initial rating value be 0. Note that a value of 0 still won't be selectable by mouse or keyboard
-    /// </summary>
-    [Parameter] public bool AllowZeroStars { get; set; }
-
-    /// <summary>
-    /// Optional label format for each individual rating star (not the rating control as a whole) that will be read by screen readers. 
-    /// Placeholder {0} is the current rating and placeholder {1} is the max: for example, 
-    /// "Select {0} of {1} stars". (To set the label for the control as a whole, use getAriaLabel or aria-label.)
-    /// </summary>
-    [Parameter] public string? AriaLabelFormat { get; set; }
-
-    /// <summary>
-    /// Maximum rating. Must be >= min (0 if AllowZeroStars is true, 1 otherwise)
-    /// </summary>
-    [Parameter] public int Max { get; set; } = 5;
-
-    /// <summary>
     /// Custom icon name for selected rating elements, If unset, default will be the FavoriteStarFill icon
     /// </summary>
     [Parameter] public BitIconName Icon { get; set; } = BitIconName.FavoriteStarFill;
 
     /// <summary>
-    /// Custom icon name for unselected rating elements, If unset, default will be the FavoriteStar icon
+    /// Maximum rating. Must be >= Min (0 if AllowZeroStars is true, 1 otherwise)
     /// </summary>
-    [Parameter] public BitIconName UnselectedIcon { get; set; } = BitIconName.FavoriteStar;
+    [Parameter] public int Max { get; set; } = 5;
 
     /// <summary>
-    /// Default value. Must be a number between min and max. 
-    /// Only provide this if the CurrentValue is an uncontrolled component; otherwise, use the Value property.
+    /// Callback that is called when the rating has changed
     /// </summary>
-    [Parameter] public double? DefaultValue { get; set; }
+    [Parameter] public EventCallback<double> OnChange { get; set; }
 
     /// <summary>
     /// Size of rating
@@ -62,30 +65,25 @@ public partial class BitRating
     [Parameter] public BitRatingSize Size { get; set; }
 
     /// <summary>
-    /// Optional callback to set the aria-label for rating control in readOnly mode. Also used as a fallback aria-label if ariaLabel prop is not provided.
+    /// Custom icon name for unselected rating elements, If unset, default will be the FavoriteStar icon
     /// </summary>
-    [Parameter] public Func<double, double, string>? GetAriaLabel { get; set; }
-
-    /// <summary>
-    /// Callback that is called when the rating has changed
-    /// </summary>
-    [Parameter] public EventCallback<double> OnChange { get; set; }
+    [Parameter] public BitIconName UnselectedIcon { get; set; } = BitIconName.FavoriteStar;
 
     protected override async Task OnInitializedAsync()
     {
         OnValueChanged += HandleOnValueChanged;
 
+        _min = AllowZeroStars ? 0 : 1;
+
         if (CurrentValue == default && DefaultValue.HasValue)
         {
-            CurrentValue = DefaultValue.Value;
+            CurrentValue = DefaultValue.Value > Max ? Max : DefaultValue.Value < _min ? _min : DefaultValue.Value;
         }
-
-        CurrentValue = Math.Min(Math.Max(CurrentValue, (AllowZeroStars ? 0 : 1)), Max);
 
         await base.OnInitializedAsync();
     }
 
-    protected override string RootElementClass => "bit-rating";
+    protected override string RootElementClass => "bit-rtg";
 
     protected override void RegisterComponentClasses()
     {
@@ -102,22 +100,22 @@ public partial class BitRating
                                             : string.Empty);
     }
 
-    private double GetPercentageOf(int starNumber)
+    private void HandleOnValueChanged(object? sender, EventArgs args) => ClassBuilder.Reset();
+
+    private double GetPercentageOf(int index)
     {
         double fullRating = Math.Ceiling(CurrentValue);
-        double fullStar = 100;
 
-        if (starNumber == CurrentValue)
+        double fullStar = 0;
+
+        if (index < fullRating)
         {
             fullStar = 100;
         }
-        else if (starNumber == fullRating)
+        else if(index == fullRating)
         {
-            fullStar = 100 * (CurrentValue % 1);
-        }
-        else if (starNumber > fullRating)
-        {
-            fullStar = 0;
+            var decimalValue = CurrentValue % 1;
+            fullStar = decimalValue == 0 ? 100 : (decimalValue * 100);
         }
 
         return fullStar;
@@ -125,16 +123,16 @@ public partial class BitRating
 
     private async Task HandleClick(int index)
     {
-        if ((AllowZeroStars is false && index == 0) ||
+        if (index > Max ||
+            index < _min ||
             IsReadOnly is true ||
             IsEnabled is false ||
             ValueChanged.HasDelegate is false) return;
 
         CurrentValue = index;
+
         await OnChange.InvokeAsync(CurrentValue);
     }
-
-    private void HandleOnValueChanged(object? sender, EventArgs args) => ClassBuilder.Reset();
 
     /// <inheritdoc />
     protected override bool TryParseValueFromString(string? value, out double result, [NotNullWhen(false)] out string? validationErrorMessage)
