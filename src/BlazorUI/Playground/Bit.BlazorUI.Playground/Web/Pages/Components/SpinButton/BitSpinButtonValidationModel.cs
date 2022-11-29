605c843eac487ad26e563d58da2e27c019cf6bc8
sajad kardel <sajadkardel@outlook.com>
2022-11-29 16:50:24 +0330

    update demo #3586

--- src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/SpinButton/BitSpinButtonValidationModel.cs
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/SpinButton/BitSpinButtonValidationModel.cs
@@ -5,6 +5,6 @@ namespace Bit.BlazorUI.Playground.Web.Pages.Components.SpinButton;
 public class BitSpinButtonValidationModel
 {
     [Required(ErrorMessage = "Enter an age")]
-    [Range(1, 200, ErrorMessage = "Nobody is that old")]
+    [Range(1, 150, ErrorMessage = "Nobody is that old")]
     public double AgeInYears { get; set; }
 }
