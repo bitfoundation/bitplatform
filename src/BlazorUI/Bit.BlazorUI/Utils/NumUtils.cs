46a5415ce94be513a99dbc21ae06ec9e77c29fd3
Saleh Yusefnejad <msynk@outlook.com>
2022-12-04 23:50:55 +0330

    feat(components): resolve number formatting issues in the BitCarousel component #3588 (#3593)

---
+++ src/BlazorUI/Bit.BlazorUI/Utils/NumUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit.BlazorUI;
+
+public class NumUtils
+{
+    public static string ToInvariantString(double value)
+    {
+        return value.ToString(System.Globalization.CultureInfo.InvariantCulture);
+    }
+}
