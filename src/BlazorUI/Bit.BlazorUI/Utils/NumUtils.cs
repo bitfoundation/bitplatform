7402c01ea138bd09bf4f8bcfb0841709df0f4361
msynk <msynk@outlook.com>
2022-12-04 23:53:51 +0330

    Resolve number formatting issues in the BitSwiper component #3594

--- src/BlazorUI/Bit.BlazorUI/Utils/NumUtils.cs
+++ src/BlazorUI/Bit.BlazorUI/Utils/NumUtils.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bit.BlazorUI;
+﻿namespace Bit.BlazorUI;
 
 public class NumUtils
 {
