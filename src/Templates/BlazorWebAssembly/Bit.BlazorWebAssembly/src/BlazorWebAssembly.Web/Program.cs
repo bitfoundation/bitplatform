2701090f68f60d6a8f2dbf5f7538c9b1714b29ed
Saleh Yusefnejad <msynk@outlook.com>
2023-01-16 18:48:38 +0330

    add Bit Blazor WebAssembly template project #3736

---
+++ src/Templates/BlazorWebAssembly/Bit.BlazorWebAssembly/src/BlazorWebAssembly.Web/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
