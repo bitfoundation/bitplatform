20927408373abf1fb8f0570c5375e7316b2de314
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2023-01-21 02:18:35 +0330

    Fix issue

--- src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitMenuButton/BitMenuButton.razor.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitMenuButton/BitMenuButton.razor.cs
@@ -5,11 +5,24 @@ namespace Bit.BlazorUI;
 public partial class BitMenuButton
 {
     protected override bool UseVisual => false;
+
     private BitButtonStyle buttonStyle = BitButtonStyle.Primary;
-    private bool isOpenCallout;
+    private bool isCalloutOpen;
     private string? _menuButtonId;
     private string? _menuButtonCalloutId;
     private string? _menuButtonOverlayId;
+
+    private bool _isCalloutOpen
+    {
+        get => isCalloutOpen;
+        set
+        {
+            if (isCalloutOpen == value) return;
+
+            isCalloutOpen = value;
+            ClassBuilder.Reset();
+        }
+    }
 
     [Inject] private IJSRuntime _js { get; set; } = default!;
 
@@ -79,24 +92,12 @@ public partial class BitMenuButton
     /// <summary>
     /// The EditContext, which is set if the button is inside an <see cref="EditForm"/>
     /// </summary>
-    [CascadingParameter] public EditContext? EditContext { get; set; }
-
-    public bool IsOpenCallout
-    {
-        get => isOpenCallout;
-        set
-        {
-            if (isOpenCallout == value) return;
-
-            isOpenCallout = value;
-            ClassBuilder.Reset();
-        }
-    }
+    [CascadingParameter] private EditContext? _editContext { get; set; }
 
     [JSInvokable("CloseCallout")]
     public void CloseCalloutBeforeAnotherCalloutIsOpened()
     {
-        IsOpenCallout = false;
+        _isCalloutOpen = false;
     }
 
     protected override string RootElementClass => "bit-mnb";
@@ -112,7 +113,7 @@ public partial class BitMenuButton
 
     protected override Task OnParametersSetAsync()
     {
-        ButtonType ??= EditContext is null
+        ButtonType ??= _editContext is null
             ? BitButtonType.Button
             : BitButtonType.Submit;
 
@@ -127,7 +128,7 @@ public partial class BitMenuButton
                                            ? "primary"
                                            : "standard");
 
-        ClassBuilder.Register(() => IsOpenCallout
+        ClassBuilder.Register(() => _isCalloutOpen
                                        ? "open-menu"
                                        : string.Empty);
     }
@@ -137,8 +138,8 @@ public partial class BitMenuButton
         if (IsEnabled is false) return;
 
         var obj = DotNetObjectReference.Create(this);
-        await _js.InvokeVoidAsync("BitMenuButton.toggleMenuButtonCallout", obj, UniqueId, _menuButtonId, _menuButtonCalloutId, _menuButtonOverlayId, IsOpenCallout);
-        IsOpenCallout = true;
+        await _js.InvokeVoidAsync("BitMenuButton.toggleMenuButtonCallout", obj, UniqueId, _menuButtonId, _menuButtonCalloutId, _menuButtonOverlayId, _isCalloutOpen);
+        _isCalloutOpen = true;
 
         await OnClick.InvokeAsync(e);
     }
@@ -148,8 +149,8 @@ public partial class BitMenuButton
         if (IsEnabled is false || item.IsEnabled is false) return;
 
         var obj = DotNetObjectReference.Create(this);
-        await _js.InvokeVoidAsync("BitMenuButton.toggleMenuButtonCallout", obj, UniqueId, _menuButtonId, _menuButtonCalloutId, _menuButtonOverlayId, IsOpenCallout);
-        IsOpenCallout = false;
+        await _js.InvokeVoidAsync("BitMenuButton.toggleMenuButtonCallout", obj, UniqueId, _menuButtonId, _menuButtonCalloutId, _menuButtonOverlayId, _isCalloutOpen);
+        _isCalloutOpen = false;
 
         await OnItemClick.InvokeAsync(item);
     }
@@ -157,7 +158,7 @@ public partial class BitMenuButton
     private async Task CloseCallout()
     {
         var obj = DotNetObjectReference.Create(this);
-        await _js.InvokeVoidAsync("BitMenuButton.toggleMenuButtonCallout", obj, UniqueId, _menuButtonId, _menuButtonCalloutId, _menuButtonOverlayId, IsOpenCallout);
-        IsOpenCallout = false;
+        await _js.InvokeVoidAsync("BitMenuButton.toggleMenuButtonCallout", obj, UniqueId, _menuButtonId, _menuButtonCalloutId, _menuButtonOverlayId, _isCalloutOpen);
+        _isCalloutOpen = false;
     }
 }
