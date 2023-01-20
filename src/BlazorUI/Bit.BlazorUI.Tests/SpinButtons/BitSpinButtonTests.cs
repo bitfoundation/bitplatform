498bc961a78a5da33266d87e75319ab06a925915
Saleh Yusefnejad <msynk@outlook.com>
2023-01-20 21:58:48 +0330

    add Ignore attribute

--- src/BlazorUI/Bit.BlazorUI.Tests/SpinButtons/BitSpinButtonTests.cs
+++ src/BlazorUI/Bit.BlazorUI.Tests/SpinButtons/BitSpinButtonTests.cs
@@ -656,51 +656,53 @@ public class BitSpinButtonTests : BunitTestContext
         spbLabelChild.MarkupMatches(labelTemplate);
     }
 
-    //[DataTestMethod,
-    //    DataRow(3, 1, 100, 475),
-    //    DataRow(3, 1, 100, 550)
-    //]
-    //public void BitSpinButtonContinuousIncrementOnMouseDownTest(double defaultValue, double step, double max, int timeout)
-    //{
-    //    var component = RenderComponent<BitSpinButton>(parameters =>
-    //    {
-    //        parameters.Add(p => p.Step, step);
-    //        parameters.Add(p => p.Max, max);
-    //        parameters.Add(p => p.DefaultValue, defaultValue);
-    //    });
+    [Ignore]
+    [DataTestMethod,
+        DataRow(3, 1, 100, 475),
+        DataRow(3, 1, 100, 550)
+    ]
+    public void BitSpinButtonContinuousIncrementOnMouseDownTest(double defaultValue, double step, double max, int timeout)
+    {
+        var component = RenderComponent<BitSpinButton>(parameters =>
+        {
+            parameters.Add(p => p.Step, step);
+            parameters.Add(p => p.Max, max);
+            parameters.Add(p => p.DefaultValue, defaultValue);
+        });
 
-    //    var input = component.Find("input");
-    //    var incrementButton = component.FindAll("button")[0];
-    //    var initialIncrementCount = timeout / 400;
-    //    var continuousIncrementCount = timeout >= 400 ? (timeout - 400) / 75 : 0;
-    //    var expectedResult = defaultValue + step * (initialIncrementCount + continuousIncrementCount);
-    //    incrementButton.MouseDown();
+        var input = component.Find("input");
+        var incrementButton = component.FindAll("button")[0];
+        var initialIncrementCount = timeout / 400;
+        var continuousIncrementCount = timeout >= 400 ? (timeout - 400) / 75 : 0;
+        var expectedResult = defaultValue + step * (initialIncrementCount + continuousIncrementCount);
+        incrementButton.MouseDown();
 
-    //    component.WaitForAssertion(() => Assert.AreEqual(expectedResult.ToString(), input.GetAttribute("value")), TimeSpan.FromMilliseconds(timeout));
-    //}
+        component.WaitForAssertion(() => Assert.AreEqual(expectedResult.ToString(), input.GetAttribute("value")), TimeSpan.FromMilliseconds(timeout));
+    }
 
-    //[DataTestMethod,
-    //    DataRow(50, 1, 0, 475),
-    //    DataRow(50, 1, 0, 550)
-    //]
-    //public void BitSpinButtonContinuousDecrementOnMouseDownTest(double defaultValue, double step, double min, int timeout)
-    //{
-    //    var component = RenderComponent<BitSpinButton>(parameters =>
-    //    {
-    //        parameters.Add(p => p.Step, step);
-    //        parameters.Add(p => p.Min, min);
-    //        parameters.Add(p => p.DefaultValue, defaultValue);
-    //    });
+    [Ignore]
+    [DataTestMethod,
+        DataRow(50, 1, 0, 475),
+        DataRow(50, 1, 0, 550)
+    ]
+    public void BitSpinButtonContinuousDecrementOnMouseDownTest(double defaultValue, double step, double min, int timeout)
+    {
+        var component = RenderComponent<BitSpinButton>(parameters =>
+        {
+            parameters.Add(p => p.Step, step);
+            parameters.Add(p => p.Min, min);
+            parameters.Add(p => p.DefaultValue, defaultValue);
+        });
 
-    //    var input = component.Find("input");
-    //    var incrementButton = component.FindAll("button")[1];
-    //    var initialDecrementCount = timeout / 400;
-    //    var continuousDecrementCount = timeout >= 400 ? (timeout - 400) / 75 : 0;
-    //    var expectedResult = defaultValue - step * (initialDecrementCount + continuousDecrementCount);
-    //    incrementButton.MouseDown();
+        var input = component.Find("input");
+        var incrementButton = component.FindAll("button")[1];
+        var initialDecrementCount = timeout / 400;
+        var continuousDecrementCount = timeout >= 400 ? (timeout - 400) / 75 : 0;
+        var expectedResult = defaultValue - step * (initialDecrementCount + continuousDecrementCount);
+        incrementButton.MouseDown();
 
-    //    component.WaitForAssertion(() => Assert.AreEqual(expectedResult.ToString(), input.GetAttribute("value")), TimeSpan.FromMilliseconds(timeout));
-    //}
+        component.WaitForAssertion(() => Assert.AreEqual(expectedResult.ToString(), input.GetAttribute("value")), TimeSpan.FromMilliseconds(timeout));
+    }
 
     [DataTestMethod,
         DataRow(4),
