d95660d655b591dc8eccfede7e026a7bc8dd8164
ysmoradi <ysmoradi@outlook.com>
2023-01-20 20:18:08 +0000

    bup

--- src/Templates/AdminPanel/Bit.AdminPanel/src/Client/Shared/Shared/LoadingComponent.razor.cs
+++ src/Templates/AdminPanel/Bit.AdminPanel/src/Client/Shared/Shared/LoadingComponent.razor.cs
@@ -3,18 +3,5 @@
 public partial class LoadingComponent
 {
     [Parameter] public string Color { get; set; } = "#123456";
-    [Parameter] public bool AutoHide { get; set; }
-
-
-    private bool _showLoading = true;
-
-    protected override void OnAfterRender(bool firstRender)
-    {
-        base.OnAfterRender(firstRender);
-        if (firstRender is false || AutoHide is false) return;
-
-        _showLoading = false;
-        StateHasChanged();
-    }
 }
 
