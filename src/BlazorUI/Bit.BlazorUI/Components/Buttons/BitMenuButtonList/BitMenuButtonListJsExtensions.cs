bca09f6de306418dee07db4b8b1b333a00bc5f17
sajad kardel <sajadkardel@outlook.com>
2023-02-06 23:43:50 +0330

    Code review #3870

--- src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitMenuButtonList/BitMenuButtonListJsExtensions.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitMenuButtonList/BitMenuButtonListJsExtensions.cs
@@ -3,13 +3,14 @@ namespace Bit.BlazorUI;
 
 internal static class BitMenuButtonListJsExtensions
 {
-    internal static async Task ToggleMenuButtonListCallout<T>(this IJSRuntime jsRuntime, DotNetObjectReference<BitMenuButtonList<T>> component,
-        string uniqueId, 
-        string menuButtonId,
-        string menuButtonCalloutId, 
-        string menuButtonOverlayId,
-        bool isCalloutOpen)
+    internal static async Task ToggleMenuButtonListCallout<T>(this IJSRuntime jsRuntime,
+                                                              DotNetObjectReference<BitMenuButtonList<T>> dotNetObjRef,
+                                                              string uniqueId, 
+                                                              string menuButtonId,
+                                                              string menuButtonCalloutId, 
+                                                              string menuButtonOverlayId,
+                                                              bool isCalloutOpen)
     {
-        await jsRuntime.InvokeVoidAsync("BitMenuButtonList.toggleMenuButtonListCallout", component, uniqueId, menuButtonId, menuButtonCalloutId, menuButtonOverlayId, isCalloutOpen);
+        await jsRuntime.InvokeVoidAsync("BitMenuButtonList.toggleMenuButtonListCallout", dotNetObjRef, uniqueId, menuButtonId, menuButtonCalloutId, menuButtonOverlayId, isCalloutOpen);
     }
 }
