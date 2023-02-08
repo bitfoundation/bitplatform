b4ba65c427cacc44ecd45df6ec402d7792c5394c
sajad kardel <sajadkardel@outlook.com>
2023-02-08 18:58:49 +0330

    Add BitSplitButtonList component #3895

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitSplitButtonList/BitSplitButtonList.ts
@@ -0,0 +1,62 @@
+﻿class BitSplitButtonList {
+    static toggleSplitButtonListCallout(dotnetObjReference: DotNetObject,
+        splitButtonWrapperId: string,
+        splitButtonId: string,
+        splitButtonCalloutId: string,
+        splitButtonOverlayId: string,
+        isOpenMenu: boolean) {
+
+        const splitButtonWrapper = document.getElementById(splitButtonWrapperId);
+        if (splitButtonWrapper == null)
+            return;
+
+        const splitButton = document.getElementById(splitButtonId);
+        if (splitButton == null)
+            return;
+
+        const splitButtonCallout = document.getElementById(splitButtonCalloutId);
+        if (splitButtonCallout == null)
+            return;
+
+        const splitButtonOverlay = document.getElementById(splitButtonOverlayId);
+        if (splitButtonOverlay == null)
+            return;
+
+        if (isOpenMenu) {
+            splitButtonCallout.style.display = "none";
+            splitButtonOverlay.style.display = "none";
+            Bit.currentCallout.update("", "", null);
+        } else {
+            Bit.closeCurrentCalloutIfExists(splitButtonCalloutId, splitButtonOverlayId, dotnetObjReference);
+            splitButtonCallout.style.display = "block";
+            splitButtonOverlay.style.display = "block";
+
+            const splitButtonCalloutHeight = splitButtonCallout.offsetHeight;
+            const splitButtonCalloutWidth = splitButtonCallout.offsetWidth;
+
+            const splitButtonHeight = splitButton.offsetHeight;
+            const splitButtonTop = splitButton.getBoundingClientRect().y;
+
+            const splitButtonWrapperWidth = splitButtonWrapper.offsetWidth;
+            const splitButtonWrapperHeight = splitButtonWrapper.offsetHeight;
+
+            const splitButtonWrapperX = splitButtonWrapper.getBoundingClientRect().x;
+            const splitButtonWrapperY = splitButtonWrapper.getBoundingClientRect().y;
+
+            const splitButtonWrapperBottom = window.innerHeight - (splitButtonWrapperHeight + splitButtonWrapperY);
+            const splitButtonWrapperRight = window.innerWidth - (splitButtonWrapperWidth + splitButtonWrapperX);
+
+            splitButtonCallout.style.left = splitButtonWrapperX + "px";
+            splitButtonCallout.style.right = "unset";
+
+            if (splitButtonWrapperBottom >= splitButtonCalloutHeight) {
+                splitButtonCallout.style.top = splitButtonWrapperY + splitButtonWrapperHeight + 1 + "px";
+                splitButtonCallout.style.bottom = "unset";
+
+            } else {
+                splitButtonCallout.style.bottom = splitButtonWrapperBottom + splitButtonHeight + 1 + "px";
+                splitButtonCallout.style.top = "unset";
+            }
+        }
+    }
+}
---
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/Buttons/BitSplitButtonListDemo.razor.cs
@@ -0,0 +1,372 @@
+﻿using Bit.BlazorUI.Playground.Web.Models;
+using Bit.BlazorUI.Playground.Web.Pages.Components.ComponentDemoBase;
+using System.Collections.Generic;
+
+namespace Bit.BlazorUI.Playground.Web.Pages.Components.Buttons;
+
+public partial class BitSplitButtonListDemo
+{
+    private readonly List<ComponentParameter> componentParameters = new()
+    {
+        new ComponentParameter
+        {
+            Name = "AriaDescription",
+            Type = "string?",
+            Description = "Detailed description of the button for the benefit of screen readers."
+        },
+        new ComponentParameter
+        {
+            Name = "AriaHidden",
+            Type = "bool",
+            DefaultValue = "false",
+            Description = "If true, add an aria-hidden attribute instructing screen readers to ignore the element."
+        },
+        new ComponentParameter
+        {
+            Name = "ButtonStyle",
+            Type = "BitButtonStyle",
+            LinkType = LinkType.Link,
+            Href = "#button-style-enum",
+            DefaultValue = "BitButtonStyle.Primary",
+            Description = "The style of button, Possible values: Primary | Standard."
+        },
+        new ComponentParameter
+        {
+            Name = "ButtonType",
+            Type = "BitButtonType",
+            LinkType = LinkType.Link,
+            Href = "#button-type-enum",
+            DefaultValue = "BitButtonType.Button",
+            Description = "The type of the button."
+        },
+        new ComponentParameter
+        {
+            Name = "Items",
+            Type = "List<SplitActionItem>",
+            LinkType = LinkType.Link,
+            Href = "#split-button-items",
+            DefaultValue = "new List<SplitActionItem>()",
+            Description = "List of Item, each of which can be a Button with different action in the SplitButton."
+        },
+        new ComponentParameter
+        {
+            Name = "IsSticky",
+            Type = "bool",
+            DefaultValue = "false",
+            Description = "If true, the current item is going to be change selected item."
+        },
+        new ComponentParameter()
+        {
+            Name = "ItemTemplate",
+            Type = "RenderFragment<SplitActionItem>?",
+            Description = "The content inside the item can be customized.",
+        },
+        new ComponentParameter
+        {
+            Name = "OnClick",
+            Type = "EventCallback<SplitActionItem>",
+            Description = "The callback is called when the button or button item is clicked."
+        }
+    };
+    private readonly List<ComponentSubParameter> componentSubParameters = new()
+    {
+        new ComponentSubParameter()
+        {
+            Id = "split-button-items",
+            Title = "SplitActionItem",
+            Parameters = new List<ComponentParameter>()
+            {
+               new ComponentParameter()
+               {
+                   Name = "IconName",
+                   Type = "BitIconName?",
+                   Description = "Name of an icon to render next to the item text.",
+               },
+               new ComponentParameter()
+               {
+                   Name = "IsEnabled",
+                   Type = "bool",
+                   DefaultValue = "true",
+                   Description = "Whether or not the item is enabled.",
+               },
+               new ComponentParameter()
+               {
+                   Name = "Key",
+                   Type = "string?",
+                   Description = "A unique value to use as a key of the item.",
+               },
+               new ComponentParameter()
+               {
+                   Name = "Text",
+                   Type = "string?",
+                   Description = "Text to render in the item.",
+               }
+            }
+        }
+    };
+    private readonly List<EnumParameter> enumParameters = new()
+    {
+        new EnumParameter()
+        {
+            Id = "button-style-enum",
+            Title = "BitButtonStyle Enum",
+            Description = "",
+            EnumList = new List<EnumItem>()
+            {
+                new EnumItem()
+                {
+                    Name= "Primary",
+                    Description="The button with white text on a blue background.",
+                    Value="0",
+                },
+                new EnumItem()
+                {
+                    Name= "Standard",
+                    Description="The button with black text on a white background.",
+                    Value="1",
+                }
+            }
+        },
+        new EnumParameter()
+        {
+            Id = "button-type-enum",
+            Title = "BitButtonType Enum",
+            Description = "",
+            EnumList = new List<EnumItem>()
+            {
+                new EnumItem()
+                {
+                    Name= "Button",
+                    Description="The button is a clickable button.",
+                    Value="0",
+                },
+                new EnumItem()
+                {
+                    Name= "Submit",
+                    Description="The button is a submit button (submits form-data).",
+                    Value="1",
+                },
+                new EnumItem()
+                {
+                    Name= "Reset",
+                    Description="The button is a reset button (resets the form-data to its initial values).",
+                    Value="2",
+                }
+            }
+        },
+        new EnumParameter()
+        {
+            Id = "component-visibility-enum",
+            Title = "BitComponentVisibility Enum",
+            Description = "",
+            EnumList = new List<EnumItem>()
+            {
+                new EnumItem()
+                {
+                    Name= "Visible",
+                    Description="Show content of the component.",
+                    Value="0",
+                },
+                new EnumItem()
+                {
+                    Name= "Hidden",
+                    Description="Hide content of the component,though the space it takes on the page remains.",
+                    Value="1",
+                },
+                new EnumItem()
+                {
+                    Name= "Collapsed",
+                    Description="Hide content of the component,though the space it takes on the page gone.",
+                    Value="2",
+                }
+            }
+        }
+    };
+
+
+    private string BasicPrimarySelectedItem;
+    private string BasicStandardSelectedItem;
+    private string StickyPrimarySelectedItem;
+    private string StickyStandardSelectedItem;
+    private string DisabledPrimarySelectedItem;
+    private string DisabledStandardSelectedItem;
+    private string TemplatePrimarySelectedItem;
+    private string TemplateStandardSelectedItem;
+
+    private List<SplitActionItem> example1Items = new()
+    {
+        new SplitActionItem()
+        {
+            Name = "Item A",
+            Id = "A"
+        },
+        new SplitActionItem()
+        {
+            Name = "Item B",
+            Id = "B",
+            Icon = BitIconName.Emoji
+        },
+        new SplitActionItem()
+        {
+            Name = "Item C",
+            Id = "C",
+            Icon = BitIconName.Emoji2
+        }
+    };
+    private List<SplitActionItem> example2Items = new()
+    {
+        new SplitActionItem()
+        {
+            Name = "Add",
+            Id = "add-key",
+            Icon = BitIconName.Add
+        },
+        new SplitActionItem()
+        {
+            Name = "Edit",
+            Id = "edit-key",
+            Icon = BitIconName.Edit
+        },
+        new SplitActionItem()
+        {
+            Name = "Delete",
+            Id = "delete-key",
+            Icon = BitIconName.Delete
+        }
+    };
+    private List<SplitActionItem> example3Items = new()
+    {
+        new SplitActionItem()
+        {
+            Name = "Item A",
+            Id = "A",
+            Icon = BitIconName.Emoji,
+            IsEnabled = false
+        },
+        new SplitActionItem()
+        {
+            Name = "Item B",
+            Id = "B",
+            Icon = BitIconName.Emoji2
+        },
+        new SplitActionItem()
+        {
+            Name = "Item C",
+            Id = "C",
+            Icon = BitIconName.Emoji,
+            IsEnabled = false
+        },
+        new SplitActionItem()
+        {
+            Name = "Item D",
+            Id = "D",
+            Icon = BitIconName.Emoji2
+        }
+    };
+    private List<SplitActionItem> example4Items = new()
+    {
+        new SplitActionItem()
+        {
+            Name = "Add",
+            Id = "add-key"
+        },
+        new SplitActionItem()
+        {
+            Name = "Edit",
+            Id = "edit-key"
+        },
+        new SplitActionItem()
+        {
+            Name = "Delete",
+            Id = "delete-key"
+        }
+    };
+
+    private readonly string example1HTMLCode = @"
+<style>
+    .example-content {
+        display: flex;
+        flex-flow: row wrap;
+        gap: 4rem;
+        width: fit-content;
+    }
+
+    .selected-item {
+        margin-top: 0.5rem;
+        font-weight: 600;
+    }
+</style>
+
+";
+    private readonly string example1CSharpCode = @"
+private string BasicPrimarySelectedItem;
+private string BasicStandardSelectedItem;
+
+";
+
+    private readonly string example2HTMLCode = @"
+<style>
+    .example-content {
+        display: flex;
+        flex-flow: row wrap;
+        gap: 4rem;
+        width: fit-content;
+    }
+
+    .selected-item {
+        margin-top: 0.5rem;
+        font-weight: 600;
+    }
+</style>
+
+";
+    private readonly string example2CSharpCode = @"
+private string StickyPrimarySelectedItem;
+private string StickyStandardSelectedItem;
+
+";
+
+    private readonly string example3HTMLCode = @"
+<style>
+    .example-content {
+        display: flex;
+        flex-flow: row wrap;
+        gap: 4rem;
+        width: fit-content;
+    }
+
+    .selected-item {
+        margin-top: 0.5rem;
+        font-weight: 600;
+    }
+</style>
+
+";
+    private readonly string example3CSharpCode = @"
+private string DisabledPrimarySelectedItem;
+private string DisabledStandardSelectedItem;
+
+";
+
+    private readonly string example4HTMLCode = @"
+<style>
+    .example-content {
+        display: flex;
+        flex-flow: row wrap;
+        gap: 4rem;
+        width: fit-content;
+    }
+
+    .selected-item {
+        margin-top: 0.5rem;
+        font-weight: 600;
+    }
+</style>
+
+";
+    private readonly string example4CSharpCode = @"
+private string TemplatePrimarySelectedItem;
+private string TemplateStandardSelectedItem;
+
+";
+}
