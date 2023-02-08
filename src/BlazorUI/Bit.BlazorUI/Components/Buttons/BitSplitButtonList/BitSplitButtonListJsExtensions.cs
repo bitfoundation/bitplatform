4200e5470fdc0fcbebd7dd746cad189f88b1af92
Sajad Kardel <sajadkardel@outlook.com>
2023-02-08 22:38:05 +0330

    feat(components): add BitSplitButtonList component #3895 (#3903)

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitSplitButtonList/BitSplitButtonListJsExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Bit.BlazorUI;
+
+internal static class BitSplitButtonListJsExtensions
+{
+    internal static async Task ToggleSplitButtonListCallout<T>(this IJSRuntime jsRuntime,
+                                                              DotNetObjectReference<BitSplitButtonList<T>> dotNetObjRef,
+                                                              string uniqueId, 
+                                                              string menuButtonId,
+                                                              string menuButtonCalloutId, 
+                                                              string menuButtonOverlayId,
+                                                              bool isCalloutOpen) where T : class
+    {
+        await jsRuntime.InvokeVoidAsync("BitSplitButtonList.toggleSplitButtonListCallout", dotNetObjRef, uniqueId, menuButtonId, menuButtonCalloutId, menuButtonOverlayId, isCalloutOpen);
+    }
+}
