70b100a38ac4fb16c1b08ffd8ab5cf5711bb014b
Saleh Yusefnejad <msynk@outlook.com>
2023-02-13 13:34:52 +0330

    feat(components): add the new Theme infrastructure for the BitMessageBar component #3935 (#3936)

--- src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/Notification/BitMessageBarDemo.razor.cs
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/Notification/BitMessageBarDemo.razor.cs
@@ -6,57 +6,44 @@ namespace Bit.BlazorUI.Playground.Web.Pages.Components.Notification;
 
 public partial class BitMessageBarDemo
 {
-    private bool IsMessageBarHidden1 = false;
-    private bool IsMessageBarHidden2 = false;
-
-    private void HideMessageBar1()
-    {
-        IsMessageBarHidden1 = true;
-    }
-
-    private void HideMessageBar2()
-    {
-        IsMessageBarHidden2 = true;
-    }
-
     private readonly List<ComponentParameter> componentParameters = new()
     {
-        new ComponentParameter()
+        new()
         {
             Name = "Actions",
             Type = "RenderFragment",
             DefaultValue = "",
             Description = "The content of the action to show on the message bar.",
         },
-        new ComponentParameter()
+        new()
         {
             Name = "ChildContent",
             Type = "RenderFragment",
             DefaultValue = "",
             Description = "The content of message bar.",
         },
-        new ComponentParameter()
+        new()
         {
             Name = "DismissButtonAriaLabel",
             Type = "string",
             DefaultValue = "",
             Description = "Aria label on dismiss button if onDismiss is defined.",
         },
-        new ComponentParameter()
+        new()
         {
             Name = "DismissIconName",
             Type = "BitIconName",
             DefaultValue = "BitIconName.Clear",
             Description = "Custom Fabric icon name to replace the dismiss icon. If unset, default will be the Fabric Clear icon.",
         },
-        new ComponentParameter()
+        new()
         {
             Name = "IsMultiline",
             Type = "bool",
             DefaultValue = "false",
             Description = "Determines if the message bar is multi lined. If false, and the text overflows over buttons or to another line, it is clipped.",
         },
-        new ComponentParameter()
+        new()
         {
             Name = "MessageBarType",
             Type = "BitMessageBarType",
@@ -65,35 +52,35 @@ public partial class BitMessageBarDemo
             DefaultValue = "BitMessageBarType.Info",
             Description = "The type of message bar to render.",
         },
-        new ComponentParameter()
+        new()
         {
             Name = "OnDismiss",
             Type = "EventCallback",
             DefaultValue = "",
             Description = "Whether the message bar has a dismiss button and its callback. If null, dismiss button won't show.",
         },
-        new ComponentParameter()
+        new()
         {
             Name = "OverflowButtonAriaLabel",
             Type = "string",
             DefaultValue = "",
             Description = "Aria label on overflow button if truncated is true.",
         },
-        new ComponentParameter()
+        new()
         {
             Name = "Role",
             Type = "string",
             DefaultValue = "Browse",
             Description = "Custom role to apply to the message bar.",
         },
-        new ComponentParameter()
+        new()
         {
             Name = "Truncated",
             Type = "bool",
             DefaultValue = "false",
             Description = "Determines if the message bar text is truncated. If true, a button will render to toggle between a single line view and multiline view. This parameter is for single line message bars with no buttons only in a limited space scenario.",
         },
-        new ComponentParameter()
+        new()
         {
             Name = "Visibility",
             Type = "BitComponentVisibility",
@@ -106,121 +93,185 @@ public partial class BitMessageBarDemo
 
     private readonly List<EnumParameter> enumParameters = new()
     {
-        new EnumParameter()
+        new()
         {
             Id = "messageBarType-enum",
             Title = "BitMessageBarType Enum",
             Description = "",
             EnumList = new List<EnumItem>()
             {
-                new EnumItem()
+                new()
                 {
-                    Name= "Info",
-                    Description="Info styled MessageBar.",
-                    Value="0",
+                    Name = "Info",
+                    Description = "Info styled MessageBar.",
+                    Value = "0",
                 },
-                new EnumItem()
+                new()
                 {
-                    Name= "Warning",
-                    Description="Warning styled MessageBar.",
-                    Value="1",
+                    Name = "Warning",
+                    Description = "Warning styled MessageBar.",
+                    Value = "1",
                 },
-                new EnumItem()
+                new()
                 {
-                    Name= "Error",
-                    Description="Error styled MessageBar.",
-                    Value="2",
+                    Name = "Error",
+                    Description = "Error styled MessageBar.",
+                    Value = "2",
                 },
-                new EnumItem()
+                new()
                 {
-                    Name= "Blocked",
-                    Description="Blocked styled MessageBar.",
-                    Value="3",
+                    Name = "Blocked",
+                    Description = "Blocked styled MessageBar.",
+                    Value = "3",
                 },
-                new EnumItem()
+                new()
                 {
-                    Name= "SevereWarning",
-                    Description="SevereWarning styled MessageBar.",
-                    Value="4",
+                    Name = "SevereWarning",
+                    Description = "SevereWarning styled MessageBar.",
+                    Value = "4",
                 },
-                new EnumItem()
+                new()
                 {
-                    Name= "Success",
-                    Description="Success styled MessageBar.",
-                    Value="5",
+                    Name = "Success",
+                    Description = "Success styled MessageBar.",
+                    Value = "5",
                 },
-            }
-        },
-        new EnumParameter()
-        {
-            Id = "component-visibility-enum",
-            Title = "BitComponentVisibility Enum",
-            Description = "",
-            EnumList = new List<EnumItem>()
-            {
-                new EnumItem()
-                {
-                    Name= "Visible",
-                    Description="Show content of the component.",
-                    Value="0",
-                },
-                new EnumItem()
-                {
-                    Name= "Hidden",
-                    Description="Hide content of the component,though the space it takes on the page remains.",
-                    Value="1",
-                },
-                new EnumItem()
-                {
-                    Name= "Collapsed",
-                    Description="Hide content of the component,though the space it takes on the page gone.",
-                    Value="2",
-                }
             }
         }
     };
 
-    private readonly string example1HTMLCode = @"<div>
-    <BitMessageBar MessageBarType=""@BitMessageBarType.Success"" IsMultiline=""false"" Truncated=""true"" IsEnabled=""false"">
-        <Actions>
-            <BitButton>Ok</BitButton>
-        </Actions>
-        <ChildContent>
-            Action completed! This is a sample of message bar with dismiss ability
-            Action completed! This is a sample of message bar with dismiss ability
-            Action completed! This is a sample of message bar with dismiss ability
-            Action completed! This is a sample of message bar with dismiss ability
-            Action completed! This is a sample of message bar with dismiss ability
-        </ChildContent>
-    </BitMessageBar>
-    @if (IsMessageBarHidden2 is false)
+
+    private bool IsMessageBarHidden1 = false;
+    private bool IsMessageBarHidden2 = false;
+    private bool IsMessageBarHidden3 = false;
+
+    private void HideMessageBar1()
     {
-        <div class=""m-b-10"">
-            <BitMessageBar MessageBarType=""@BitMessageBarType.Success"" OnDismiss=""HideMessageBar2"" IsMultiline=""false"" DismissIconName=""BitIconName.SkypeCheck"" DismissButtonAriaLabel=""close"">
-                Action completed! This is a sample of message bar with dismiss ability and custom dismiss icon
-            </BitMessageBar>
-        </div>
+        IsMessageBarHidden1 = true;
     }
-    <BitMessageBar MessageBarType=""@BitMessageBarType.Success"" MessageBarIconName=""BitIconName.Emoji2"">
-        Action completed! This is a sample of message bar with dismiss ability and custom dismiss icon
+
+    private void HideMessageBar2()
+    {
+        IsMessageBarHidden2 = true;
+    }
+
+    private void HideMessageBar3()
+    {
+        IsMessageBarHidden3 = true;
+    }
+
+    private void Reset()
+    {
+        IsMessageBarHidden1 = false;
+        IsMessageBarHidden2 = false;
+        IsMessageBarHidden3 = false;
+    }
+
+
+    private readonly string example1HTMLCode = @"
+<div class=""example-box"">
+    <BitMessageBar MessageBarType=""@BitMessageBarType.Info"">
+        Info (default) MessageBar. <BitLink Href=""https://bitplatform.dev"">Visit our website.</BitLink>
     </BitMessageBar>
 </div>
-@if (IsMessageBarHidden1 is false)
-{
-    <div class=""m-b-10"">
-        <BitMessageBar MessageBarType=""@BitMessageBarType.Success"" OnDismiss=""HideMessageBar1"" IsMultiline=""false"" Truncated=""true"" OverflowButtonAriaLabel=""see more"">
-            Action completed! This is a sample of message bar with dismiss ability
-            Action completed! This is a sample of message bar with dismiss ability
-            Action completed! This is a sample of message bar with dismiss ability
-            Action completed! This is a sample of message bar with dismiss ability
-            Action completed! This is a sample of message bar with dismiss ability
-        </BitMessageBar>
-    </div>
-}";
+<div class=""example-box"">
+    <BitMessageBar MessageBarType=""@BitMessageBarType.Error"">
+        Error MessageBar. <BitLink Href=""https://bitplatform.dev"">Visit our website.</BitLink>
+    </BitMessageBar>
+</div>
+<div class=""example-box"">
+    <BitMessageBar MessageBarType=""@BitMessageBarType.Blocked"">
+        Blocked MessageBar. <BitLink Href=""https://bitplatform.dev"">Visit our website.</BitLink>
+    </BitMessageBar>
+</div>
+<div class=""example-box"">
+    <BitMessageBar MessageBarType=""@BitMessageBarType.SevereWarning"">
+        SevereWarning MessageBar. <BitLink Href=""https://bitplatform.dev"">Visit our website.</BitLink>
+    </BitMessageBar>
+</div>
+<div class=""example-box"">
+    <BitMessageBar MessageBarType=""@BitMessageBarType.Success"">
+        Success MessageBar. <BitLink Href=""https://bitplatform.dev"">Visit our website.</BitLink>
+    </BitMessageBar>
+</div>
+<div class=""example-box"">
+    <BitMessageBar MessageBarType=""@BitMessageBarType.Warning"">
+        Warning MessageBar. <BitLink Href=""https://bitplatform.dev"">Visit our website.</BitLink>
+    </BitMessageBar>
+</div>";
 
-    private readonly string example1CSharpCode = @"
+    private readonly string example2HTMLCode = @"
+<style>
+    .example-box {
+        margin-bottom: 10px;
+    }
+</style>
+<div class=""example-box"">
+    @if (IsMessageBarHidden1 is false)
+    {
+        <BitMessageBar MessageBarType=""@BitMessageBarType.Info"" OnDismiss=""HideMessageBar1"">
+            Dismiss option enabled by adding <strong>OnDismiss</strong> parameter.
+        </BitMessageBar>
+    }
+</div>
+<div class=""example-box"">
+    @if (IsMessageBarHidden2 is false)
+    {
+        <BitMessageBar MessageBarType=""@BitMessageBarType.Error"" OnDismiss=""HideMessageBar2"" Truncated=""true"" IsMultiline=""false"">
+            single line, with dismiss button and truncated text.
+            Truncation is not available if you use action buttons or multiline and should be used sparingly.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi luctus, purus a lobortis tristique, odio augue pharetra metus, ac placerat nunc mi nec dui. Vestibulum aliquam et nunc semper scelerisque. Curabitur vitae orci nec quam condimentum porttitor et sed lacus. Vivamus ac efficitur leo. Cras faucibus mauris libero, ac placerat erat euismod et. Donec pulvinar commodo odio sit amet faucibus. In hac habitasse platea dictumst. Duis eu ante commodo, condimentum nibh pellentesque, laoreet enim. Fusce massa lorem, ultrices eu mi a, fermentum suscipit magna. Integer porta purus pulvinar, hendrerit felis eget, condimentum mauris.
+        </BitMessageBar>
+    }
+</div>
+<div class=""example-box"">
+    <BitMessageBar MessageBarType=""@BitMessageBarType.Blocked"">
+        <Actions>
+            <div style=""width:100%;text-align:right"">
+                <BitButton ButtonStyle=""BitButtonStyle.Standard"">Yes</BitButton>
+                <BitButton ButtonStyle=""BitButtonStyle.Standard"">No</BitButton>
+            </div>
+        </Actions>
+        <ChildContent>
+            MessageBar with action buttons which defaults to multiline.
+        </ChildContent>
+    </BitMessageBar>
+</div>
+<div class=""example-box"">
+    <BitMessageBar MessageBarType=""@BitMessageBarType.Success"" IsMultiline=""false"">
+        <Actions>
+            <div style=""display:flex;align-items:center"">
+                <BitButton ButtonStyle=""BitButtonStyle.Standard"" Style=""padding:0"">Yes</BitButton>
+                <BitButton ButtonStyle=""BitButtonStyle.Standard"" Style=""padding:0"">No</BitButton>
+            </div>
+        </Actions>
+        <ChildContent>
+            MessageBar with single line and action buttons.MessageBar with single line and action buttons.
+        </ChildContent>
+    </BitMessageBar>
+</div>
+<div class=""example-box"">
+    @if (IsMessageBarHidden3 is false)
+    {
+        <BitMessageBar MessageBarType=""@BitMessageBarType.Warning"" OnDismiss=""HideMessageBar3"">
+            <ChildContent>
+                MessageBar defaults to multiline.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi luctus, purus a lobortis tristique, odio augue pharetra metus, ac placerat nunc mi nec dui. Vestibulum aliquam et nunc semper scelerisque. Curabitur vitae orci nec quam condimentum porttitor et sed lacus. Vivamus ac efficitur leo. Cras faucibus mauris libero, ac placerat erat euismod et. Donec pulvinar commodo odio sit amet faucibus. In hac habitasse platea dictumst. Duis eu ante commodo, condimentum nibh pellentesque, laoreet enim. Fusce massa lorem, ultrices eu mi a, fermentum suscipit magna. Integer porta purus pulvinar, hendrerit felis eget, condimentum mauris.
+            </ChildContent>
+            <Actions>
+                <div style=""width:100%;text-align:right"">
+                    <BitButton ButtonStyle=""BitButtonStyle.Standard"" Style=""padding:0"">Yes</BitButton>
+                    <BitButton ButtonStyle=""BitButtonStyle.Standard"" Style=""padding:0"">No</BitButton>
+                </div>
+            </Actions>
+        </BitMessageBar>
+    }
+</div>";
+    private readonly string example2CSharpCode = @"
 private bool IsMessageBarHidden1 = false;
 private bool IsMessageBarHidden2 = false;
+private bool IsMessageBarHidden3 = false;
+
 private void HideMessageBar1()
 {
     IsMessageBarHidden1 = true;
@@ -229,31 +280,10 @@ private void HideMessageBar1()
 private void HideMessageBar2()
 {
     IsMessageBarHidden2 = true;
+}
+
+private void HideMessageBar3()
+{
+    IsMessageBarHidden3 = true;
 }";
-
-    private readonly string example2HTMLCode = @"<BitMessageBar MessageBarType=""@BitMessageBarType.Error"" Truncated=""false"">
-    <ChildContent>
-        This is an error message bar with the ability to truncate your text Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </ChildContent>
-    <Actions>
-        <BitButton>Ok</BitButton>
-    </Actions>
-</BitMessageBar>";
-
-    private readonly string example3HTMLCode = @"<BitMessageBar MessageBarType=""@BitMessageBarType.Blocked"">
-    Blocked MessageBar - single line Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</BitMessageBar>";
-
-    private readonly string example4HTMLCode = @"<div>
-    <BitMessageBar MessageBarType=""@BitMessageBarType.Warning"" IsMultiline=""false"">
-        Caution! Action may takes long and also this message bar shown multiline messages time Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </BitMessageBar>
-</div>
-<BitMessageBar MessageBarType=""@BitMessageBarType.SevereWarning"">
-    Cannot connect to server
-</BitMessageBar>";
-
-    private readonly string example5HTMLCode = @"<BitMessageBar MessageBarType=""@BitMessageBarType.Info"">
-    Visit repository <BitLink HasUnderline=""true"">the link is rendered as a button</BitLink>
-</BitMessageBar>";
 }
