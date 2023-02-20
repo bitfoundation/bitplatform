3304cc5b0cc6bfb9a5f7c28091929eb049339161
Sajad Kardel <sajadkardel@outlook.com>
2023-02-20 11:04:50 +0330

    feat(components): merge different implementations of the BitBreadcrumb component #3961 (#3963)

--- src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/Breadcrumb/PageInfoModel.cs
+++ src/BlazorUI/Playground/Bit.BlazorUI.Playground/Web/Pages/Components/Breadcrumb/PageInfoModel.cs
@@ -1,16 +1,16 @@
-﻿namespace Bit.BlazorUI.Playground.Web.Pages.Components.BreadList;
+﻿namespace Bit.BlazorUI.Playground.Web.Pages.Components.Breadcrumb;
 
 public class PageInfoModel
 {
     public string Name { get; set; }
 
-    public string Href { get; set; }
+    public string Address { get; set; }
 
     public string HtmlClass { get; set; }
 
     public string HtmlStyle { get; set; }
 
-    public bool IsSelected { get; set; }
+    public bool IsCurrent { get; set; }
 
     public bool IsEnabled { get; set; } = true;
 }
