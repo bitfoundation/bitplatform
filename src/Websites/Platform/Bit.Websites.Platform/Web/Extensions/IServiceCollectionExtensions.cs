a568b216c9da3f2a728b69451ee587344d9de983
Saleh Yusefnejad <msynk@outlook.com>
2023-03-03 00:15:04 +0330

    update ci solutions

--- src/Websites/Platform/Bit.Websites.Platform/Web/Extensions/IServiceCollectionExtensions.cs
+++ src/Websites/Platform/Bit.Websites.Platform/Web/Extensions/IServiceCollectionExtensions.cs
@@ -4,7 +4,7 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class IServiceCollectionExtensions
 {
-    public static IServiceCollection AddPlaygroundServices(this IServiceCollection services)
+    public static IServiceCollection AddBitPlatformServices(this IServiceCollection services)
     {
         services.AddScoped<NavManuService>();
         return services;
