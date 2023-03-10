4d3050467b1c2a45662f81558ccecf6f4c0b4568
ysmoradi <ysmoradi@outlook.com>
2023-03-10 19:10:02 +0000

    Fixed small issues

--- src/Websites/Platform/Bit.Websites.Platform/Web/Program.cs
+++ src/Websites/Platform/Bit.Websites.Platform/Web/Program.cs
@@ -27,7 +27,17 @@ public class Program
         var builder = WebAssemblyHostBuilder.CreateDefault();
         builder.Configuration.AddJsonStream(typeof(MainLayout).Assembly.GetManifestResourceStream("Bit.Websites.Platform.Web.appsettings.json"));
 
-        builder.Services.AddSingleton(sp => new HttpClient(sp.GetRequiredService<AppHttpClientHandler>()) { BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress}api/") });
+        if (Uri.TryCreate(builder.Configuration.GetApiServerAddress(), UriKind.RelativeOrAbsolute, out var apiServerAddress) is false)
+        {
+            throw new InvalidOperationException("Aoi server address is invalid");
+        }
+
+        if (apiServerAddress.IsAbsoluteUri is false)
+        {
+            apiServerAddress = new Uri($"{builder.HostEnvironment.BaseAddress}{apiServerAddress}");
+        }
+
+        builder.Services.AddSingleton(sp => new HttpClient(sp.GetRequiredService<AppHttpClientHandler>()) { BaseAddress = apiServerAddress });
         builder.Services.AddScoped<Microsoft.AspNetCore.Components.WebAssembly.Services.LazyAssemblyLoader>();
 
         builder.Services.AddSharedServices();
