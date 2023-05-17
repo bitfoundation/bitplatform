d80d7a013d1646dba5760069089ae02a1e723856
Yaser Moradi <ysmoradi@outlook.com>
2023-05-17 17:49:07 +0000

    feat(templates): use Google Play store links for demo instead of app center #4365 (#4371)

--- src/Websites/Platform/Bit.Websites.Platform/Web/Shared/Urls.cs
+++ src/Websites/Platform/Bit.Websites.Platform/Web/Shared/Urls.cs
@@ -23,13 +23,13 @@ public static class Urls
     public const string TodoTemplate = "/todo-template/overview";
     public const string TodoTemplateSSR = "https://todo.bitplatform.dev/";
     public const string TodoTemplatePWA = "https://todo-app.bitplatform.dev/";
-    public const string TodoTemplateAppCenter = "https://install.appcenter.ms/orgs/bitfoundation/apps/todo/distribution_groups/testers";
+    public const string TodoTemplateGooglePlay = "https://play.google.com/store/apps/details?id=com.bitplatform.Todo.Template";
     public const string TodoTemplateFigma = "https://www.figma.com/file/PHDEnipl4PCWIoh5dCRpLd/Todo-Template?node-id=2022%3A5223";
     public const string TodoTemplateNuget = "https://www.nuget.org/packages/Bit.TodoTemplate/";
 
     public const string AdminPanel = "/admin-panel/overview";
     public const string AdminPanelMultiLingual = "https://adminpanel.bitplatform.dev/";
-    public const string AdminPanelAppCenter = "https://install.appcenter.ms/orgs/bitfoundation/apps/adminpanel/distribution_groups/testers";
+    public const string AdminPanelGooglePlay = "https://play.google.com/store/apps/details?id=com.bitplatform.AdminPanel.Template";
     public const string AdminPanelFigma = "https://www.figma.com/community/file/1161030350747233731";
     public const string AdminPanelNuget = "https://www.nuget.org/packages/Bit.AdminPanel";
 
