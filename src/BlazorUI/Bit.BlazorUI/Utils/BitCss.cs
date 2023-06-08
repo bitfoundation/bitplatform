302ebb2d9f06f196c18de26a859307cb524f6aca
Saleh Yusefnejad <msynk@outlook.com>
2023-06-08 16:49:04 +0330

    feat(components): revisit background colors of the Theme #4519 (#4521)

--- src/BlazorUI/Bit.BlazorUI/Utils/BitCss.cs
+++ src/BlazorUI/Bit.BlazorUI/Utils/BitCss.cs
@@ -8,42 +8,30 @@ public static class BitCss
         public static class Primary
         {
             public const string Main = "bit-css-color-primary-main";
-            public const string Light = "bit-css-color-primary-light";
-            public const string Lighter = "bit-css-color-primary-lighter";
             public const string Dark = "bit-css-color-primary-dark";
-            public const string Darker = "bit-css-color-primary-darker";
+            public const string Light = "bit-css-color-primary-light";
 
         }
         public static class Secondary
         {
             public const string Main = "bit-css-color-secondary-main";
-            public const string Light = "bit-css-color-secondary-light";
-            public const string Lighter = "bit-css-color-secondary-lighter";
             public const string Dark = "bit-css-color-secondary-dark";
-            public const string Darker = "bit-css-color-secondary-darker";
+            public const string Light = "bit-css-color-secondary-light";
 
         }
     }
 
-    public static class Text
+    public static class Foreground
     {
-        public const string Primary = "bit-css-txt-primary";
-        public const string Primary2 = "bit-css-txt-primary2";
-        public const string Primary3 = "bit-css-txt-primary3";
-        public const string Secondary = "bit-css-txt-secondary";
-        public const string Secondary2 = "bit-css-txt-secondary2";
-        public const string Secondary3 = "bit-css-txt-secondary3";
-        public const string Disabled = "bit-css-txt-disabled";
+        public const string Primary = "bit-css-fg-primary";
+        public const string Secondary = "bit-css-fg-secondary";
+        public const string Disabled = "bit-css-fg-disabled";
     }
 
-    public static class BgColor
+    public static class Background
     {
-        public const string Primary = "bit-css-bgcolor-primary";
-        public const string Primary2 = "bit-css-bgcolor-primary2";
-        public const string Primary3 = "bit-css-bgcolor-primary3";
-
-        public const string Secondary = "bit-css-bgcolor-secondary";
-        public const string Secondary2 = "bit-css-bgcolor-secondary2";
-        public const string Secondary3 = "bit-css-bgcolor-secondary3";
+        public const string Primary = "bit-css-bg-primary";
+        public const string Secondary = "bit-css-bg-secondary";
+        public const string Disabled = "bit-css-bg-disabled";
     }
 }
