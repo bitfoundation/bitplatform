15e80c9b71878568347b967886f45138d9337aa7
Saleh Yusefnejad <msynk@outlook.com>
2023-06-13 01:33:25 +0330

    2nd commit

--- src/BlazorUI/Bit.BlazorUI/Theme/BitTheme.cs
+++ src/BlazorUI/Bit.BlazorUI/Theme/BitTheme.cs
@@ -8,14 +8,45 @@ public class BitTheme
 
 public class BitThemeColors
 {
-    public BitThemeColorVariants Primary { get; set; } = new BitThemeColorVariants();
-    public BitThemeColorVariants Secondary { get; set; } = new BitThemeColorVariants();
+    public BitThemeColorVariants Primary { get; private set; } = new BitThemeColorVariants();
+    public BitThemeColorVariants Secondary { get; private set; } = new BitThemeColorVariants();
+    public BitThemeGeneralColorVariants Foreground { get; private set; } = new BitThemeGeneralColorVariants();
+    public BitThemeBackgroundColorVariants Background { get; private set; } = new BitThemeBackgroundColorVariants();
+    public BitThemeGeneralColorVariants Border { get; private set; } = new BitThemeGeneralColorVariants();
+    public BitThemeActionColorVariants Action { get; private set; } = new BitThemeActionColorVariants();
+
 }
 public class BitThemeColorVariants
 {
     public string? Main { get; set; }
-    public string? High { get; set; }
-    public string? Higher { get; set; }
-    public string? Low { get; set; }
-    public string? Lower { get; set; }
+    public string? Dark { get; set; }
+    public string? Light { get; set; }
+    public string? Text { get; set; }
+}
+
+public class BitThemeGeneralColorVariants
+{
+    public string? Primary { get; set; }
+    public string? Secondary { get; set; }
+    public string? Disabled { get; set; }
+}
+
+public class BitThemeBackgroundColorVariants : BitThemeGeneralColorVariants
+{
+    public string? Overlay { get; set; }
+}
+
+public class BitThemeActionColorVariants
+{
+
+}
+
+public class BitThemeActionHoverColorVariants
+{
+    public string? Primary { get; set; }
+    public string? PrimaryDark { get; set; }
+    public string? PrimaryLight { get; set; }
+    public string? Secondary { get; set; }
+    public string? SecondaryDark { get; set; }
+    public string? SecondaryLight { get; set; }
 }
