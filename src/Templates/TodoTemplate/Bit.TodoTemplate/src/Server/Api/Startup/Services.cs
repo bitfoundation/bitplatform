12fb12c412ba544a1a79f8f5f82ba9d53712457b
Yaser Moradi <ysmoradi@outlook.com>
2023-06-17 19:26:37 +0200

    fix(templates): add missing service registration for blazor wasm ssr #4570 (#4571)

--- src/Templates/TodoTemplate/Bit.TodoTemplate/src/Server/Api/Startup/Services.cs
+++ src/Templates/TodoTemplate/Bit.TodoTemplate/src/Server/Api/Startup/Services.cs
@@ -29,6 +29,7 @@ public static class Services
 #if BlazorWebAssembly
         services.AddTransient<IAuthTokenProvider, ServerSideAuthTokenProvider>();
         services.AddClientSharedServices();
+        services.AddClientWebServices();
 
         // In the Pre-Rendering mode, the configured HttpClient will use the access_token provided by the cookie in the request, so the pre-rendered content would be fitting for the current user.
         services.AddHttpClient("WebAssemblyPreRenderingHttpClient")
