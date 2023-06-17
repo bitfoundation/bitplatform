a520baa75ddef9856a626ff4414008ed4b9300b3
Saleh Yusefnejad <msynk@outlook.com>
2023-06-17 22:32:32 +0330

    fix z-index & and ClassStyles parameter #4573

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Modal/BitModalClassStyles.cs
@@ -0,0 +1,9 @@
+﻿namespace Bit.BlazorUI;
+
+public class BitModalClassStyles
+{
+    /// <summary>
+    /// Custom CSS classes/styles for the modal container.
+    /// </summary>
+    public BitClassStylePair? Container { get; set; }
+}
