7bad4b393174e84de74cb8e98e0b5a7e410afbcf
ysmoradi <ysmoradi@outlook.com>
2023-06-30 13:59:30 +0200

    refactor clean.bat files

--- src/Templates/BlazorDual/Bit.BlazorDual/src/Clean.bat
+++ src/Templates/BlazorDual/Bit.BlazorDual/src/Clean.bat
@@ -1,16 +1,18 @@
-:: This batch file can completely clean the source code and remove the extra files (even better than the Clean command of the Visual Studio).
-:: Close the IDE (Visual Studio, and ...) before running it.
-:: These commands work only on Windows.
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S node_modules') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S Packages') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S .vs') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S TestResults') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S AppPackages') DO RMDIR /S /Q "%%G"
-DEL /Q /F /S "Resource.designer.cs"
-DEL /Q /F /S "*.csproj.user"
-DEL /Q /F /S "*.razor.css"
-DEL /Q /F /S "*.razor.min.css"
-DEL /Q /F /S "*.map"
-powershell.exe "& Get-ChildItem -Recurse | Where-Object { $_.Attributes -match 'ReparsePoint' } | Remove-Item -Confirm:$false -Force -Recurse "
+:: This batch script is designed for comprehensive cleaning of your project by deleting unnecessary files.
+:: It's crucial to close any Integrated Development Environment (IDE), such as Visual Studio, etc., before executing this script to prevent any conflicts or loss of unsaved data.
+:: Please note that the commands included in this script are specifically tailored for the Windows
+
+:: Delete css,js and source maps files if not tracked in git
+powershell -Command "[string]$trackedFiles = git ls-files; Get-ChildItem -Include *.css,*.min.css,*.js,*.min.js,*.map -Recurse | ForEach-Object { if ($trackedFiles -NotMatch $_.Name) { Remove-Item -Recurse -Path $_ -Confirm:$false -Force }}"
+
+:: Runs dotnet clean for each csproj file
+powershell -Command "Get-ChildItem -Include *.csproj -Recurse | ForEach-Object { dotnet clean $_.FullName }"
+
+@REM :: Delete specified directories
+@REM powershell -Command "Get-ChildItem -Include bin,obj,node_modules,Packages,.vs,TestResults,AppPackages -Recurse -Directory | Remove-Item -Recurse -Confirm:$false -Force"
+
+@REM :: Delete specified files
+@REM powershell -Command "Get-ChildItem -Include *.csproj.user,Resources.designer.cs -Recurse | Remove-Item -Confirm:$false -Force"
+
+@REM :: Delete empty directories
+@REM powershell -Command "Get-ChildItem -Recurse | Where-Object { $_.PSIsContainer -and @(Get-ChildItem -Lit $_.FullName).Count -eq 0 } | Remove-Item -Confirm:$false -Force"
