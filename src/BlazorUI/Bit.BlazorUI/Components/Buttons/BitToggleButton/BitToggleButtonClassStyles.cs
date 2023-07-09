f468aa8b95b1a485c72a0893cd012daf2b9516f0
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-07-09 15:27:26 +0330

    Add BitClassStylePair parameter to BitToggleButton #4746

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitToggleButton/BitToggleButtonClassStyles.cs
@@ -0,0 +1,19 @@
+﻿namespace Bit.BlazorUI;
+
+public class BitToggleButtonClassStyles
+{
+    /// <summary>
+    /// Custom CSS classes/styles for the icon element.
+    /// </summary>
+    public BitClassStylePair? Icon { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the label and icon container.
+    /// </summary>
+    public BitClassStylePair? Container { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the label element.
+    /// </summary>
+    public BitClassStylePair? Label { get; set; }
+}
