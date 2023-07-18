a7c1bafdc819299c64f1574a73dc3e3a3ded09d4
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-07-18 14:22:55 +0330

    Fix Bit BlazorUI demo PWA theme_color responding to current theme #4783

--- src/BlazorUI/Demo/Client/Core/Scripts/app.ts
+++ src/BlazorUI/Demo/Client/Core/Scripts/app.ts
@@ -36,10 +36,6 @@ function goToTop() {
     window.scrollTo({ top: 0 });
 }
 
-function toggleBitTheme(isDark: boolean) {
-    document.documentElement.setAttribute('bit-theme', isDark ? 'dark' : 'light');
-}
-
 function applyBodyElementClasses(cssClasses: string[], cssVariables: any) {
     cssClasses?.forEach(c => document.body.classList.add(c));
     Object.keys(cssVariables).forEach(key => document.body.style.setProperty(key, cssVariables[key]));
@@ -53,9 +49,11 @@ BitTheme.init({
         if (newTheme === 'dark') {
             document.body.classList.add('bit-theme-dark');
             document.body.classList.remove('bit-theme-light');
+            document.querySelector("meta[name=theme-color]")!.setAttribute('content','#0d1117');
         } else {
             document.body.classList.add('bit-theme-light');
             document.body.classList.remove('bit-theme-dark');
+            document.querySelector("meta[name=theme-color]")!.setAttribute('content', '#ffffff');
         }
     }
 });
