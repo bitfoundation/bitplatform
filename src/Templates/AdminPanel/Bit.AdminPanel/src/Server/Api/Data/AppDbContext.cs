4550c283c01baa09714c979da76c05d630686b1c
Yaser Moradi <ysmoradi@outlook.com>
2023-07-21 23:22:31 +0200

    feat(templates): add different database option to the template projects #4668 (#4797)

--- src/Templates/AdminPanel/Bit.AdminPanel/src/Server/Api/Data/AppDbContext.cs
+++ src/Templates/AdminPanel/Bit.AdminPanel/src/Server/Api/Data/AppDbContext.cs
@@ -1,6 +1,8 @@
-﻿using AdminPanel.Server.Api.Models.Account;
+﻿//+:cnd:noEmit
+using AdminPanel.Server.Api.Models.Account;
 using AdminPanel.Server.Api.Models.Categories;
 using AdminPanel.Server.Api.Models.Products;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AdminPanel.Server.Api.Data;
 
@@ -46,6 +48,14 @@ public class AppDbContext : IdentityDbContext<User, Role, int>
             throw new ConflictException(nameof(AppStrings.UpdateConcurrencyException), exception);
         }
     }
+
+    //#if (database == "Sqlite")
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<DateTimeOffset>().HaveConversion<DateTimeOffsetToBinaryConverter>();
+        configurationBuilder.Properties<DateTimeOffset?>().HaveConversion<DateTimeOffsetToBinaryConverter>();
+    }
+    //#endif
 
     private void ConfigIdentityTables(ModelBuilder builder)
     {
