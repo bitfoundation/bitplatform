b6e45a145fd4a4d67b56349fcad3aea0a71248cc
Saleh Yusefnejad <msynk@outlook.com>
2023-08-01 21:49:18 +0330

    update bswup & bup usages

--- src/Templates/TodoTemplate/Bit.TodoTemplate/src/Client/Web/wwwroot/service-worker.published.js
+++ src/Templates/TodoTemplate/Bit.TodoTemplate/src/Client/Web/wwwroot/service-worker.published.js
@@ -1,20 +1,7 @@
-﻿// https://github.com/bitfoundation/bitplatform/tree/develop/src/Bswup#service-worker
+﻿// https://github.com/bitfoundation/bitplatform/tree/develop/src/Bswup
 
-self.assetsInclude = [
-    /\.svg$/,
-    /\.webp/,
-    /FabExMDL*/
-];
-
-self.assetsExclude = [
-    /bit\.blazorui\.cupertino\.min\.css$/,
-    /bit\.blazorui\.cupertino\.css$/,
-    /bit\.blazorui\.fluent\.min\.css$/,
-    /bit\.blazorui\.fluent\.css$/,
-    /bit\.blazorui\.material\.min\.css$/,
-    /bit\.blazorui\.material\.css$/
-];
-
+self.assetsInclude = [];
+self.assetsExclude = [];
 self.externalAssets = [
     {
         "url": "/"
@@ -33,9 +20,7 @@ self.serverHandledUrls = [
 ];
 
 self.defaultUrl = "/";
-
 self.caseInsensitiveUrl = true;
-
 self.noPrerenderQuery = 'no-prerender=true';
 
 self.importScripts('_content/Bit.Bswup/bit-bswup.sw.js');
