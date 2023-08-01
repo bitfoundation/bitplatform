5b13a568419efac26a51d8812a10269dd932005e
Saleh Yusefnejad <msynk@outlook.com>
2023-08-01 18:30:29 +0330

    apply Bswup passive in BlazorUI demo #4904

--- src/BlazorUI/Demo/Client/Web/wwwroot/service-worker.js
+++ src/BlazorUI/Demo/Client/Web/wwwroot/service-worker.js
@@ -1,22 +1,9 @@
-// https://github.com/bitfoundation/bitplatform/tree/develop/src/Bswup#service-worker
+// https://github.com/bitfoundation/bitplatform/tree/develop/src/Bswup
 
 // Make sure to apply all changes you make here to the service-worker.published.js file too (if required).
 
-self.assetsInclude = [
-    /\.svg$/,
-    /\.webp/,
-    /FabExMDL*/
-];
-
-self.assetsExclude = [
-    /bit\.blazorui\.cupertino\.min\.css$/,
-    /bit\.blazorui\.cupertino\.css$/,
-    /bit\.blazorui\.fluent\.min\.css$/,
-    /bit\.blazorui\.fluent\.css$/,
-    /bit\.blazorui\.material\.min\.css$/,
-    /bit\.blazorui\.material\.css$/
-];
-
+self.assetsInclude = [];
+self.assetsExclude = [];
 self.externalAssets = [
     {
         "url": "/"
@@ -38,10 +25,9 @@ self.serverHandledUrls = [
     /\/api.fda.gov/
 ];
 
+self.isPassive = true;
 self.defaultUrl = "/";
-
 self.caseInsensitiveUrl = true;
-
 self.noPrerenderQuery = 'no-prerender=true';
 
 self.importScripts('_content/Bit.Bswup/bit-bswup.sw.js');
