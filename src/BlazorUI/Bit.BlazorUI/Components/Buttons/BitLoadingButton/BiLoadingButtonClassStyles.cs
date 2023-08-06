ea4ed86320c5917443fe4f323aa44fca8edc0464
Saleh Yusefnejad <msynk@outlook.com>
2023-08-06 17:08:11 +0330

    add classstyles

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitLoadingButton/BiLoadingButtonClassStyles.cs
@@ -0,0 +1,19 @@
+﻿namespace Bit.BlazorUI;
+
+public class BiLoadingButtonClassStyles
+{
+    /// <summary>
+    /// Custom CSS classes/styles for the internal container of the BitLoadingButton.
+    /// </summary>
+    public BitClassStylePair? Container { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the Spinner of the BitLoadingButton.
+    /// </summary>
+    public BitClassStylePair? Spinner { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the Label of the BitLoadingButton.
+    /// </summary>
+    public BitClassStylePair? Label { get; set; }
+}
