b8ed99779541e32e88145f26bcf7300742da286b
Saleh Yusefnejad <msynk@outlook.com>
2023-08-06 20:22:33 +0330

    feat(blazorui): add ClassStyles parameter to the BitLoadingButton #4882 (#4942)

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Buttons/BitLoadingButton/BitLoadingButtonClassStyles.cs
@@ -0,0 +1,19 @@
+﻿namespace Bit.BlazorUI;
+
+public class BitLoadingButtonClassStyles
+{
+    /// <summary>
+    /// Custom CSS classes/styles for the internal container of the BitLoadingButton.
+    /// </summary>
+    public BitClassStylePair? LoadingContainer { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the Spinner of the BitLoadingButton.
+    /// </summary>
+    public BitClassStylePair? Spinner { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the Label of the BitLoadingButton.
+    /// </summary>
+    public BitClassStylePair? LoadingLabel { get; set; }
+}
