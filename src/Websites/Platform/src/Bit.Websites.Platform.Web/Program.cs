5026c5d6f5ddaf95a0e1576d5c83ce2cb6b5fec5
Saleh Yusefnejad <msynk@outlook.com>
2023-08-20 00:11:53 +0330

    feat(websites): improve content of new docs #5093 (#5094)

--- src/Websites/Platform/src/Bit.Websites.Platform.Web/Program.cs
+++ src/Websites/Platform/src/Bit.Websites.Platform.Web/Program.cs
@@ -5,7 +5,7 @@ public partial class Program
     public static async Task Main(string[] args)
     {
 #if !BlazorWebAssembly && !BlazorServer
-        throw new InvalidOperationException("Please switch to either blazor webassembly or server as described in readme.md");
+        throw new InvalidOperationException("Please switch to either Blazor WebAssembly or Server as described in readme.md");
 #else
         await CreateHostBuilder(args)
             .RunAsync();
