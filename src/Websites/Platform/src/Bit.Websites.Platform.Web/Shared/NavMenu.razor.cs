600baf9266ab607072b721b5970df0913e744afc
Saleh Yusefnejad <msynk@outlook.com>
2023-08-28 15:29:54 +0330

    feat(websites): consider and handle old doc urls #5168 (#5177)

--- src/Websites/Platform/src/Bit.Websites.Platform.Web/Shared/NavMenu.razor.cs
+++ src/Websites/Platform/src/Bit.Websites.Platform.Web/Shared/NavMenu.razor.cs
@@ -10,21 +10,21 @@ public partial class NavMenu : IDisposable
     private List<BitNavItem> _filteredNavItems = default!;
     private readonly List<BitNavItem> _allNavItems = new()
     {
-        new BitNavItem { Text = "Overview", Url = "/templates/overview"},
-        new BitNavItem { Text = "Development prerequisites", Url = "/templates/development-prerequisites"},
-        new BitNavItem { Text = "Create project", Url = "/templates/create-project"},
-        new BitNavItem { Text = "Project structure", Url = "/templates/project-structure"},
-        new BitNavItem { Text = "Database", Url = "/templates/database"},
-        new BitNavItem { Text = "Run", Url = "/templates/run"},
-        new BitNavItem { Text = "Hosting models", Url = "/templates/hosting-models"},
-        new BitNavItem { Text = "Deployment type", Url = "/templates/deployment-type"},
-        new BitNavItem { Text = "Settings", Url = "/templates/settings"},
-        new BitNavItem { Text = "Exception handling", Url = "/templates/exception-handling"},
-        new BitNavItem { Text = "Cache mechanism", Url = "/templates/cache-mechanism"},
-        new BitNavItem { Text = "Multilingualism", Url = "/templates/multilingualism"},
-        new BitNavItem { Text = "DevOps", Url = "/templates/devops"},
-        new BitNavItem { Text = "Platform integration", Url = "/templates/platform-integration"},
-        new BitNavItem { Text = "Contribute", Url = "/templates/contribute"}
+        new BitNavItem { Text = "Overview", Url = "/templates/overview", AdditionalUrls = new string[] { "/admin-panel/overview", "/todo-template/overview" } },
+        new BitNavItem { Text = "Development prerequisites", Url = "/templates/development-prerequisites", AdditionalUrls = new string[] { "/admin-panel/development-prerequisites", "/todo-template/development-prerequisites" } },
+        new BitNavItem { Text = "Create project", Url = "/templates/create-project", AdditionalUrls = new string[] { "/admin-panel/create-project", "/todo-template/create-project" } },
+        new BitNavItem { Text = "Project structure", Url = "/templates/project-structure", AdditionalUrls = new string[] { "/admin-panel/project-structure", "/todo-template/project-structure" } },
+        new BitNavItem { Text = "Database", Url = "/templates/database", AdditionalUrls = new string[] { "/admin-panel/database", "/todo-template/database" } },
+        new BitNavItem { Text = "Run", Url = "/templates/run", AdditionalUrls = new string[] { "/admin-panel/run", "/todo-template/run" } },
+        new BitNavItem { Text = "Hosting models", Url = "/templates/hosting-models", AdditionalUrls = new string[] { "/admin-panel/hosting-models", "/todo-template/hosting-models" } },
+        new BitNavItem { Text = "Deployment type", Url = "/templates/deployment-type", AdditionalUrls = new string[] { "/admin-panel/deployment-type", "/todo-template/deployment-type" } },
+        new BitNavItem { Text = "Settings", Url = "/templates/settings", AdditionalUrls = new string[] { "/admin-panel/settings", "/todo-template/settings" } },
+        new BitNavItem { Text = "Exception handling", Url = "/templates/exception-handling", AdditionalUrls = new string[] { "/admin-panel/exception-handling", "/todo-template/exception-handling" } },
+        new BitNavItem { Text = "Cache mechanism", Url = "/templates/cache-mechanism", AdditionalUrls = new string[] { "/admin-panel/cache-mechanism", "/todo-template/cache-mechanism" } },
+        new BitNavItem { Text = "Multilingualism", Url = "/templates/multilingualism", AdditionalUrls = new string[] { "/admin-panel/multilingualism", "/todo-template/multilingualism" } },
+        new BitNavItem { Text = "DevOps", Url = "/templates/devops", AdditionalUrls = new string[] { "/admin-panel/devops", "/todo-template/devops" } },
+        new BitNavItem { Text = "Platform integration", Url = "/templates/platform-integration", AdditionalUrls = new string[] { "/admin-panel/platform-integration", "/todo-template/platform-integration" } },
+        new BitNavItem { Text = "Contribute", Url = "/templates/contribute", AdditionalUrls = new string[] { "/admin-panel/contribute", "/todo-template/contribute" } },
     };
 
 
