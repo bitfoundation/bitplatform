65afe47481c3e4da9e08406bba8e870df872575e
Saleh Yusefnejad <msynk@outlook.com>
2023-08-30 11:01:45 +0330

    feat(prerelease): v-5.5.0-pre-07 #5197 (#5199)

--- src/BlazorUI/Demo/Client/Web/wwwroot/service-worker.published.js
+++ src/BlazorUI/Demo/Client/Web/wwwroot/service-worker.published.js
@@ -1,4 +1,4 @@
-﻿// bit version: 5.5.0-pre-06
+﻿// bit version: 5.5.0-pre-07
 // https://github.com/bitfoundation/bitplatform/tree/develop/src/Bswup
 
 self.assetsExclude = [
--- src/Bswup/Bit.Bswup.Demo/wwwroot/service-worker.js
+++ src/Bswup/Bit.Bswup.Demo/wwwroot/service-worker.js
@@ -1,4 +1,4 @@
-// bit version: 5.5.0-pre-06
+// bit version: 5.5.0-pre-07
 
 self.assetsExclude = [/\.scp\.css$/, /weather\.json$/];
 self.caseInsensitiveUrl = true;
