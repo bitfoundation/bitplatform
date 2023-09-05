611ec49e718e2182314953b1314b5fe263159cb3
Mohammad Hossein Rastegarinia <mh.rastegari@outlook.com>
2023-09-05 17:36:38 +0330

    feat(blazorui): add ClassStyles to BitSearchBox #5248 (#5250)

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Inputs/SearchBox/BitSearchBoxClassStyles.cs
@@ -0,0 +1,38 @@
+﻿namespace Bit.BlazorUI;
+
+public class BitSearchBoxClassStyles
+{
+    /// <summary>
+    /// Custom CSS classes/styles for the search box's clear button.
+    /// </summary>
+    public string? ClearButton { get; set; }
+    /// <summary>
+    /// Custom CSS classes/styles for the search box's clear button container.
+    /// </summary>
+    public string? ClearButtonContainer { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the search box's clear button icon.
+    /// </summary>
+    public string? ClearButtonIcon { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the search box's clear button icon container.
+    /// </summary>
+    public string? ClearButtonIconContainer { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the search box's input.
+    /// </summary>
+    public string? Input { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the search box's search icon.
+    /// </summary>
+    public string? SearchIcon { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the search box's search icon container.
+    /// </summary>
+    public string? SearchIconContainer { get; set; }
+}
