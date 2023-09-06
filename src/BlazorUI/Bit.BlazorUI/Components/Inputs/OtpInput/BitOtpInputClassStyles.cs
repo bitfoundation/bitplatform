363493901c6089d461bc03d5a6d673cd5e68539b
Mohammad Hossein Rastegarinia <mh.rastegari@outlook.com>
2023-09-06 15:27:06 +0330

    feat(blazorui): add ClassStyles to BitOtpInput #5252 (#5253)

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Inputs/OtpInput/BitOtpInputClassStyles.cs
@@ -0,0 +1,9 @@
+﻿namespace Bit.BlazorUI;
+
+public class BitOtpInputClassStyles
+{
+    /// <summary>
+    /// Custom CSS classes/styles for each input in otp input.
+    /// </summary>
+    public string? Input { get; set; }
+}
