04e514e7ce29bc804f3ea394c839ceabb181c23d
Saleh Yusefnejad <msynk@outlook.com>
2023-09-09 20:13:42 +0330

    feat(blazorui): add Data and Description parameters to BitNav items #5265 (#5267)

--- src/BlazorUI/Demo/Client/Core/Pages/Components/Nav/CarMenu.cs
+++ src/BlazorUI/Demo/Client/Core/Pages/Components/Nav/CarMenu.cs
@@ -3,11 +3,12 @@
 public class CarMenu
 {
     public string Name { get; set; } = string.Empty;
-    public string Tooltip { get; set; }
-    public string PageUrl { get; set; }
-    public string UrlTarget { get; set; }
-    public string ExpandedAriaLabel { get; set; }
-    public string CollapsedAriaLabel { get; set; }
+    public string? Tooltip { get; set; }
+    public string? PageUrl { get; set; }
+    public string? UrlTarget { get; set; }
+    public string? ExpandedAriaLabel { get; set; }
+    public string? CollapsedAriaLabel { get; set; }
     public bool IsExpandedParent { get; set; }
     public List<CarMenu> Links { get; set; } = new();
+    public string? Comment { get; set; }
 }
