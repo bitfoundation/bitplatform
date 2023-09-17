a9d84ce33dbc26ad83cb173dd2217eae1d99bd85
ysmoradi <ysmoradi@outlook.com>
2023-09-17 22:56:23 +0200

    test

--- src/BlazorUI/Demo/Client/App/MauiProgram.cs
+++ src/BlazorUI/Demo/Client/App/MauiProgram.cs
@@ -8,44 +8,52 @@ public static class MauiProgram
 {
     public static MauiAppBuilder CreateMauiAppBuilder()
     {
+        try
+        {
 #if Android || iOS || Mac || Windows
-        string appSecret = OperatingSystem.IsWindows() ? "a206e212-6427-414f-b4f3-83fa5eec4f1d"
-                         : OperatingSystem.IsAndroid() ? "c87802e3-0fa5-4938-b539-086b06d40726" : "f76345b1-9069-4477-afbe-a2be2a2ed46d";
-        Microsoft.AppCenter.AppCenter.Start(appSecret, typeof(Microsoft.AppCenter.Crashes.Crashes));
+            string appSecret = OperatingSystem.IsWindows() ? "a206e212-6427-414f-b4f3-83fa5eec4f1d"
+                             : OperatingSystem.IsAndroid() ? "c87802e3-0fa5-4938-b539-086b06d40726" : "f76345b1-9069-4477-afbe-a2be2a2ed46d";
+            Microsoft.AppCenter.AppCenter.Start(appSecret, typeof(Microsoft.AppCenter.Crashes.Crashes));
 #endif
 
 #if !BlazorHybrid
-        throw new InvalidOperationException("Please switch to blazor hybrid as described in readme.md");
+            throw new InvalidOperationException("Please switch to blazor hybrid as described in readme.md");
 #endif
 
-        var builder = MauiApp.CreateBuilder();
-        var assembly = typeof(MainLayout).GetTypeInfo().Assembly;
+            var builder = MauiApp.CreateBuilder();
+            var assembly = typeof(MainLayout).GetTypeInfo().Assembly;
 
-        builder
-            .UseMauiApp<App>()
-            .Configuration.AddJsonFile(new EmbeddedFileProvider(assembly), "appsettings.json", optional: false, false);
+            builder
+                .UseMauiApp<App>()
+                .Configuration.AddJsonFile(new EmbeddedFileProvider(assembly), "appsettings.json", optional: false, false);
 
-        var services = builder.Services;
+            var services = builder.Services;
 
-        services.AddMauiBlazorWebView();
+            services.AddMauiBlazorWebView();
 #if DEBUG
-        services.AddBlazorWebViewDeveloperTools();
+            services.AddBlazorWebViewDeveloperTools();
 #endif
 
-        services.AddScoped(sp =>
-        {
-            HttpClient httpClient = new(sp.GetRequiredService<AppHttpClientHandler>())
+            services.AddScoped(sp =>
             {
-                BaseAddress = new Uri(sp.GetRequiredService<IConfiguration>().GetApiServerAddress())
-            };
+                HttpClient httpClient = new(sp.GetRequiredService<AppHttpClientHandler>())
+                {
+                    BaseAddress = new Uri(sp.GetRequiredService<IConfiguration>().GetApiServerAddress())
+                };
 
-            return httpClient;
-        });
+                return httpClient;
+            });
 
-        services.AddSharedServices();
-        services.AddClientSharedServices();
-        services.AddClientAppServices();
+            services.AddSharedServices();
+            services.AddClientSharedServices();
+            services.AddClientAppServices();
 
-        return builder;
+            return builder;
+        }
+        catch (Exception ex)
+        {
+            Microsoft.AppCenter.Crashes.Crashes.TrackError(ex);
+            throw;
+        }
     }
 }
