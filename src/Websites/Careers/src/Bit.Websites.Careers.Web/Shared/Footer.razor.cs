749b1d0ab0477269507584627c1cc9db9b77651f
Saleh Yusefnejad <msynk@outlook.com>
2023-09-19 05:01:11 +0330

    apply BitDropdown changes to all projects #5303

--- src/Websites/Careers/src/Bit.Websites.Careers.Web/Shared/Footer.razor.cs
+++ src/Websites/Careers/src/Bit.Websites.Careers.Web/Shared/Footer.razor.cs
@@ -2,23 +2,9 @@
 
 public partial class Footer
 {
-    [Inject] IJSRuntime JsRuntime { get; set; }
+
     private async Task BackToTop()
     {
-        await JsRuntime.InvokeVoidAsync("App.backToTop");
+        await JSRuntime.InvokeVoidAsync("App.backToTop");
     }
-
-    private string? SelectedCulture;
-
-    private async Task OnCultureChanged()
-    {
-        var cultureCookie = $"c={SelectedCulture}|uic={SelectedCulture}";
-
-        await JSRuntime.InvokeVoidAsync("window.App.setCookie", ".AspNetCore.Culture", cultureCookie, 30 * 24 * 3600);
-
-        NavigationManager.ForceReload();
-    }
-
-    private static List<BitDropdownItem> GetCultures() =>
-        CultureInfoManager.SupportedCultures.Select(sc => new BitDropdownItem { Value = sc.code, Text = sc.name }).ToList();
 }
