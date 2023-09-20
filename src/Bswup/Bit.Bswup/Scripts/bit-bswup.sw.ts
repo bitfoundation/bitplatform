a883332df964dd32439edcd21fb936b1874bf633
Saleh Yusefnejad <msynk@outlook.com>
2023-09-21 02:28:40 +0330

    release v-5.6.1 #5321

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw.ts
@@ -1,4 +1,4 @@
-﻿self['bit-bswup.sw version'] = '5.6.1-pre-01';
+﻿self['bit-bswup.sw version'] = '5.6.1';
 
 interface Window {
     clients: any
--- src/Bswup/FullDemo/Client/wwwroot/service-worker.published.js
+++ src/Bswup/FullDemo/Client/wwwroot/service-worker.published.js
@@ -1,4 +1,4 @@
-// bit version: 5.6.1-pre-01
+// bit version: 5.6.1
 
 self.assetsInclude = [];
 self.assetsExclude = [/\.scp\.css$/, /weather\.json$/];
