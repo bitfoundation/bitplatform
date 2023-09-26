9dfcc238e68da4cc773f903a3e6231099ca78f3f
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-09-26 12:40:32 +0330

    Improve Copy link and Copy code buttons UX #4805

--- src/BlazorUI/Demo/Client/Core/Components/ComponentExampleBox.razor.cs
+++ src/BlazorUI/Demo/Client/Core/Components/ComponentExampleBox.razor.cs
@@ -15,6 +15,9 @@ public partial class ComponentExampleBox
         await JSRuntime.InvokeVoidAsync("highlightSnippet");
     }
 
+    private bool isCodeCopied = false;
+    private string codeIcon = BitIconName.Copy;
+    private string copyCodeMessage = "Copy code";
     private async Task CopyCodeToClipboard()
     {
         var code = string.IsNullOrEmpty(CsharpCode) is false
@@ -22,6 +25,19 @@ public partial class ComponentExampleBox
             : "";
 
         await JSRuntime.CopyToClipboard(RazorCode.Trim() + code);
+
+        codeIcon = BitIconName.CheckMark;
+        copyCodeMessage = "Code copied!";
+        isCodeCopied = true;
+
+        StateHasChanged();
+
+        await Task.Delay(1000);
+        isCodeCopied = false;
+        codeIcon = BitIconName.Copy;
+        copyCodeMessage = "Copy code";
+
+        StateHasChanged();
     }
 
     private string AppendCodePhraseToCsharpCode(string cSharpSourceCode)
@@ -32,11 +48,27 @@ public partial class ComponentExampleBox
         return code;
     }
 
+    private bool isLinkCopied = false;
+    private string linkIcon = BitIconName.Link;
+    private string copyLinkMessage = "Copy link";
     private async Task CopyLinkToClipboard()
     {
         var currentUrl = NavigationManager.Uri;
         currentUrl = currentUrl.Contains("#") ? currentUrl.Substring(0, currentUrl.IndexOf("#")) : currentUrl;
         var exampleUrl = $"{currentUrl}#{Id}";
         await JSRuntime.CopyToClipboard(exampleUrl);
+
+        linkIcon = BitIconName.CheckMark;
+        copyLinkMessage = "Link copied!";
+        isLinkCopied = true;
+
+        StateHasChanged();
+
+        await Task.Delay(1000);
+        isLinkCopied = false;
+        linkIcon = BitIconName.Link;
+        copyLinkMessage = "Copy link";
+
+        StateHasChanged();
     }
 }
