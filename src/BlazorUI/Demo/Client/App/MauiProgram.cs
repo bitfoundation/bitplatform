ddbdd65cd021095ecde171a78e5985ee85ffad28
Mohammad Hossein Rastegarinia <mh.rastegari@outlook.com>
2023-09-28 18:13:02 +0330

    feat(templates): remove AppCenter config for Mac in the BitBlazorUI demo project #5374 (#5375)

--- src/BlazorUI/Demo/Client/App/MauiProgram.cs
+++ src/BlazorUI/Demo/Client/App/MauiProgram.cs
@@ -10,7 +10,10 @@ public static class MauiProgram
     {
         try
         {
-#if Android || iOS || Mac || Windows
+            // For MacCatalyst there are still some issues with AppCenter
+            // https://github.com/microsoft/appcenter-sdk-dotnet/issues/1755
+            // https://github.com/microsoft/appcenter-sdk-dotnet/issues/1702
+#if Android || iOS || Windows
             string appSecret = OperatingSystem.IsWindows() ? "a206e212-6427-414f-b4f3-83fa5eec4f1d"
                              : OperatingSystem.IsAndroid() ? "c87802e3-0fa5-4938-b539-086b06d40726" : "f76345b1-9069-4477-afbe-a2be2a2ed46d";
             Microsoft.AppCenter.AppCenter.Start(appSecret, typeof(Microsoft.AppCenter.Crashes.Crashes));
@@ -52,7 +55,7 @@ public static class MauiProgram
         }
         catch (Exception ex)
         {
-#if Android || iOS || Mac || Windows
+#if Android || iOS || Windows
             Microsoft.AppCenter.Crashes.Crashes.TrackError(ex);
 #endif
             throw;
