bcddc6c5d0247c69a53bf8f78f0ee322e4b26d36
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-09-28 16:37:33 +0330

    Remove MacCatalyst AppCenter #5374

--- src/BlazorUI/Demo/Client/App/MauiProgram.cs
+++ src/BlazorUI/Demo/Client/App/MauiProgram.cs
@@ -10,7 +10,7 @@ public static class MauiProgram
     {
         try
         {
-#if Android || iOS || Mac || Windows
+#if Android || iOS || Windows
             string appSecret = OperatingSystem.IsWindows() ? "a206e212-6427-414f-b4f3-83fa5eec4f1d"
                              : OperatingSystem.IsAndroid() ? "c87802e3-0fa5-4938-b539-086b06d40726" : "f76345b1-9069-4477-afbe-a2be2a2ed46d";
             Microsoft.AppCenter.AppCenter.Start(appSecret, typeof(Microsoft.AppCenter.Crashes.Crashes));
@@ -52,7 +52,7 @@ public static class MauiProgram
         }
         catch (Exception ex)
         {
-#if Android || iOS || Mac || Windows
+#if Android || iOS || Windows
             Microsoft.AppCenter.Crashes.Crashes.TrackError(ex);
 #endif
             throw;
