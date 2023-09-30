94a2a1f7724e6d723220fc0aa4cabeb70b0a83dd
Saleh Yusefnejad <msynk@outlook.com>
2023-09-30 19:19:45 +0330

    add Inline mode to BitSpinButton #5412

--- src/BlazorUI/Bit.BlazorUI/Components/Inputs/SpinButton/BitSpinButtonMode.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Inputs/SpinButton/BitSpinButtonMode.cs
@@ -3,9 +3,14 @@
 public enum BitSpinButtonMode
 {
     /// <summary>
-    /// Spinning buttons render as a stack at the end of the input.
+    /// Spinning buttons render as a compact stack at the end of the input.
     /// </summary>
-    Stack,
+    Compact,
+
+    /// <summary>
+    /// Spinning buttons render inlined at the end of the input.
+    /// </summary>
+    Inline,
 
     /// <summary>
     /// Spinning buttons render at the start and end of the input.
