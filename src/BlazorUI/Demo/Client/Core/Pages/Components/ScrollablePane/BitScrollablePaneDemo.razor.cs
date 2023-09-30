90c035112d62c4e6c24ebfe86ebc8375abe0ca06
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-09-30 18:12:53 +0330

    Improve demo

--- src/BlazorUI/Demo/Client/Core/Pages/Components/ScrollablePane/BitScrollablePaneDemo.razor.cs
+++ src/BlazorUI/Demo/Client/Core/Pages/Components/ScrollablePane/BitScrollablePaneDemo.razor.cs
@@ -127,41 +127,7 @@ public partial class BitScrollablePaneDemo
 
 
     private readonly string example1RazorCode = @"
-<style>
-    .custom-scroll-container {
-        display: flex;
-        white-space: nowrap;
-    }
-</style>
-
-<BitScrollablePane Height=""16rem"">
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sit
-        amet, vulputate in leo. Maecenas vulputate congue sapien eu tincidunt. Etiam eu sem turpis. Fusce tempor
-        sagittis nunc, ut interdum ipsum vestibulum non. Proin dolor elit, aliquam eget tincidunt non, vestibulum ut
-        turpis. In hac habitasse platea dictumst. In a odio eget enim porttitor maximus. Aliquam nulla nibh,
-        ullamcorper aliquam placerat eu, viverra et dui. Phasellus ex lectus, maximus in mollis ac, luctus vel eros.
-        Vivamus ultrices, turpis sed malesuada gravida, eros ipsum venenatis elit, et volutpat eros dui et ante.
-        Quisque ultricies mi nec leo ultricies mollis. Vivamus egestas volutpat lacinia. Quisque pharetra eleifend
-        efficitur.
-    </p>
-    <p>
-        Mauris at nunc eget lectus lobortis facilisis et eget magna. Vestibulum venenatis augue sapien, rhoncus
-        faucibus magna semper eget. Proin rutrum libero sagittis sapien aliquet auctor. Suspendisse tristique a
-        magna at facilisis. Duis rhoncus feugiat magna in rutrum. Suspendisse semper, dolor et vestibulum lacinia,
-        nunc felis malesuada ex, nec hendrerit justo ex et massa. Quisque quis mollis nulla. Nam commodo est ornare,
-        rhoncus odio eu, pharetra tellus. Nunc sed velit mi.
-    </p>
-    <p>
-        Sed condimentum ultricies turpis convallis pharetra. Sed sagittis quam pharetra luctus porttitor. Cras vel
-        consequat lectus. Sed nec fringilla urna, a aliquet libero. Aenean sed nisl purus. Vivamus vulputate felis
-        et odio efficitur suscipit. Ut volutpat dictum lectus, ac rutrum massa accumsan at. Sed pharetra auctor
-        finibus. In augue libero, commodo vitae nisi non, sagittis convallis ante. Phasellus malesuada eleifend
-        mollis. Curabitur ultricies leo ac metus venenatis elementum.
-    </p>
-</BitScrollablePane>
-
-<BitScrollablePane Height=""5rem"" Class=""custom-scroll-container"">
+<BitScrollablePane Style=""height: 22rem;"">
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sit
         amet, vulputate in leo. Maecenas vulputate congue sapien eu tincidunt. Etiam eu sem turpis. Fusce tempor
@@ -189,43 +155,6 @@ public partial class BitScrollablePaneDemo
 </BitScrollablePane>";
 
     private readonly string example2RazorCode = @"
-<style>
-    .vertical-scroll-item {
-        height: 2.75rem;
-        margin: 0.5rem 0.5rem;
-        padding: 0.5rem 1.25rem;
-        background-color: #f2f2f2;
-    }
-</style>
-                    
-<BitScrollablePane @bind-ScrollbarVisibility=""@scrollbarVisibility"" Height=""19rem;"">
-    @for (int i = 0; i < itemsCount; i++)
-    {
-        var index = i;
-        <div class=""vertical-scroll-item"">@index</div>
-    }
-</BitScrollablePane>
-
-<BitSpinButton Min=""0"" @bind-Value=""@itemsCount"" Label=""Items count"" Style=""max-width: 19rem"" />
-                    
-<BitChoiceGroup @bind-Value=""scrollbarVisibility"" Label=""Scrollbar visibility"" TItem=""BitChoiceGroupOption<BitScrollbarVisibility>"" TValue=""BitScrollbarVisibility"">
-    <BitChoiceGroupOption Text=""Auto"" Value=""BitScrollbarVisibility.Auto"" />
-    <BitChoiceGroupOption Text=""Hidden"" Value=""BitScrollbarVisibility.Hidden"" />
-    <BitChoiceGroupOption Text=""Scroll"" Value=""BitScrollbarVisibility.Scroll"" />
-</BitChoiceGroup>";
-    private readonly string example2CsharpCode = @"
-private double itemsCount = 25;
-private BitScrollbarVisibility scrollbarVisibility;
-";
-
-    private readonly string example3RazorCode = @"
-<style>
-    .custom-scroll-container {
-        display: flex;
-        white-space: nowrap;
-    }
-</style>
-                    
 <BitScrollablePane Height=""16rem"">
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sit
@@ -253,7 +182,7 @@ private BitScrollbarVisibility scrollbarVisibility;
     </p>
 </BitScrollablePane>
 
-<BitScrollablePane Class=""custom-scroll-container"" Width=""50%"">
+<BitScrollablePane Width=""50%"" Style=""white-space: nowrap;"">
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sit
         amet, vulputate in leo. Maecenas vulputate congue sapien eu tincidunt. Etiam eu sem turpis. Fusce tempor
@@ -280,7 +209,7 @@ private BitScrollbarVisibility scrollbarVisibility;
     </p>
 </BitScrollablePane>
 
-<BitScrollablePane Width=""256px"" Height=""10em"">
+<BitScrollablePane Width=""256px"" Height=""352px"">
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sit
         amet, vulputate in leo. Maecenas vulputate congue sapien eu tincidunt. Etiam eu sem turpis. Fusce tempor
@@ -306,6 +235,36 @@ private BitScrollbarVisibility scrollbarVisibility;
         mollis. Curabitur ultricies leo ac metus venenatis elementum.
     </p>
 </BitScrollablePane>";
+
+    private readonly string example3RazorCode = @"
+<style>
+    .vertical-scroll-item {
+        height: 2.75rem;
+        margin: 0.5rem 0.5rem;
+        padding: 0.5rem 1.25rem;
+        background-color: #f2f2f2;
+    }
+</style>
+                    
+<BitScrollablePane @bind-ScrollbarVisibility=""@scrollbarVisibility"" Height=""19rem;"">
+    @for (int i = 0; i < itemsCount; i++)
+    {
+        var index = i;
+        <div class=""vertical-scroll-item"">@index</div>
+    }
+</BitScrollablePane>
+
+<BitSpinButton Min=""0"" @bind-Value=""@itemsCount"" Label=""Items count"" Style=""max-width: 19rem"" />
+                    
+<BitChoiceGroup @bind-Value=""scrollbarVisibility"" Label=""Scrollbar visibility"" TItem=""BitChoiceGroupOption<BitScrollbarVisibility>"" TValue=""BitScrollbarVisibility"">
+    <BitChoiceGroupOption Text=""Auto"" Value=""BitScrollbarVisibility.Auto"" />
+    <BitChoiceGroupOption Text=""Hidden"" Value=""BitScrollbarVisibility.Hidden"" />
+    <BitChoiceGroupOption Text=""Scroll"" Value=""BitScrollbarVisibility.Scroll"" />
+</BitChoiceGroup>";
+    private readonly string example3CsharpCode = @"
+private double itemsCount = 25;
+private BitScrollbarVisibility scrollbarVisibility;
+";
 
     private readonly string example4RazorCode = @"
 <style>
