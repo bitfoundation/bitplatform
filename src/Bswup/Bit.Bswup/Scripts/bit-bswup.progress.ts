045238feb8794a9d22562017ae38c5b9608b2791
Saleh Yusefnejad <msynk@outlook.com>
2023-10-11 21:18:59 +0330

    feat(prerelease): v-6.1.0-pre-01 #5512 (#5532)

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bswup.progress version'] = '6.0.0';
+﻿window['bit-bswup.progress version'] = '6.1.0-pre-01';
 
 ; (function () {
     (window as any).startBswupProgress = (autoReload: boolean,
