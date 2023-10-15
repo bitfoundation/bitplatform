4617f8eb7b4ec88035b0f6a40ad249a101f3ebf0
Saleh Yusefnejad <msynk@outlook.com>
2023-10-15 12:42:13 +0330

    feat(blazorui): improve BitOtpInput component #5561 (#5574)

--- src/BlazorUI/Demo/Client/Core/Pages/Components/Inputs/OtpInput/ValidationOtpInputModel.cs
+++ src/BlazorUI/Demo/Client/Core/Pages/Components/Inputs/OtpInput/ValidationOtpInputModel.cs
@@ -2,7 +2,7 @@
 
 public class ValidationOtpInputModel
 {
-    [Required(ErrorMessage = "Is required.")]
+    [Required(ErrorMessage = "The OTP value is required.")]
     [MinLength(6, ErrorMessage = "Minimum length is 6.")]
     public string OtpValue { get; set; }
 }
