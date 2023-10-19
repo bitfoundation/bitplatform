346d5a761909c60b1dd1adf370896e26f1c1c5ca
Mohammad Hossein Rastegarinia <mh.rastegari@outlook.com>
2023-10-20 01:20:25 +0330

    feat(templates): remove transparent titlebar of WinUI #5695 (#5697)

--- src/BlazorUI/Demo/Client/Core/App.razor.cs
+++ src/BlazorUI/Demo/Client/Core/App.razor.cs
@@ -77,10 +77,11 @@ public partial class App
         var cssVariables = new Dictionary<string, string>();
         var statusBarHeight = _bitDeviceCoordinator.GetStatusBarHeight();
 
-        if ((OperatingSystem.IsIOS() || OperatingSystem.IsAndroid()) && OperatingSystem.IsMacCatalyst() is false)
+        if (OperatingSystem.IsMacCatalyst() is false)
         {
             //For iOS this is handled in css using safe-area env() variables
-            //For Android there's an issue with keyboard in fullscreen mode
+            //For Android there's an issue with keyboard in fullscreen mode. more info: https://github.com/bitfoundation/bitplatform/issues/5626
+            //For Windows there's an issue with TitleBar. more info: https://github.com/bitfoundation/bitplatform/issues/5695
             statusBarHeight = 0;
         }
         
