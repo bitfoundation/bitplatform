fa62299df54ff1db003d58f55e50bdbac485f7bc
RupakGhosh4865 <rupak4865@gmail.com>
2023-10-24 13:46:24 +0530

    remove the line of codes

--- src/Templates/BlazorDual/Bit.BlazorDual/src/BlazorDual.Shared/Infra/CultureInfoManager.cs
+++ src/Templates/BlazorDual/Bit.BlazorDual/src/BlazorDual.Shared/Infra/CultureInfoManager.cs
@@ -51,51 +51,13 @@ public class CultureInfoManager
     /// <summary>
     /// This is an example to demonstrate the way you can customize application culture
     /// </summary>
-    public static CultureInfo CustomizeCultureInfoForFaCulture(CultureInfo cultureInfo)
-    {
-        cultureInfo.DateTimeFormat.MonthNames = new[]
-        {
-            "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند", ""
-        };
-
-        cultureInfo.DateTimeFormat.AbbreviatedMonthNames = new[]
-        {
-            "فرور", "ارد", "خرد", "تیر", "مرد", "شهر", "مهر", "آبا", "آذر", "دی", "بهم", "اسف", ""
-        };
-
-        cultureInfo.DateTimeFormat.MonthGenitiveNames = cultureInfo.DateTimeFormat.MonthNames;
-        cultureInfo.DateTimeFormat.AbbreviatedMonthGenitiveNames = cultureInfo.DateTimeFormat.AbbreviatedMonthNames;
-        cultureInfo.DateTimeFormat.DayNames = new[]
-        {
-            "یکشنبه", "دوشنبه", "ﺳﻪشنبه", "چهارشنبه", "پنجشنبه", "جمعه", "شنبه"
-        };
-
-        cultureInfo.DateTimeFormat.AbbreviatedDayNames = new[]
-        {
-            "ی", "د", "س", "چ", "پ", "ج", "ش"
-        };
-
-        cultureInfo.DateTimeFormat.ShortestDayNames = new[]
-        {
-            "ی", "د", "س", "چ", "پ", "ج", "ش"
-        };
+    
 
         cultureInfo.DateTimeFormat.AMDesignator = "ق.ظ";
         cultureInfo.DateTimeFormat.PMDesignator = "ب.ظ";
         cultureInfo.DateTimeFormat.ShortDatePattern = "yyyy/MM/dd";
-        cultureInfo.DateTimeFormat.FirstDayOfWeek = DayOfWeek.Saturday;
-
-        var cultureData = _cultureDataField.GetValue(cultureInfo.TextInfo);
-
-        _iReadingLayoutField.SetValue(cultureData, 1 /*rtl*/); // this affects cultureInfo.TextInfo.IsRightToLeft
-
-        if (cultureInfo.DateTimeFormat.Calendar is not PersianCalendar)
-        {
-            cultureInfo.DateTimeFormat.Calendar = new PersianCalendar();
-        }
-
-        return cultureInfo;
-    }
+       
+    
 
     private static readonly FieldInfo _cultureDataField = typeof(TextInfo).GetField("_cultureData", BindingFlags.NonPublic | BindingFlags.Instance)!;
 
