0b08fcbd7ca43c218d24a13718533c3caaa13fe6
Saleh Yusefnejad <msynk@outlook.com>
2023-10-27 18:20:55 +0330

    feat(prerelease): v-7.1.0-pre-03 #5792 (#5854)

--- src/Templates/BlazorDual/Bit.BlazorDual/src/BlazorDual.Web/wwwroot/service-worker.js
+++ src/Templates/BlazorDual/Bit.BlazorDual/src/BlazorDual.Web/wwwroot/service-worker.js
@@ -1,4 +1,4 @@
-// bit version: 7.1.0-pre-02
+// bit version: 7.1.0-pre-03
 // https://github.com/bitfoundation/bitplatform/tree/develop/src/Bswup
 
 // Make sure to apply all changes you make here to the service-worker.published.js file too (if required).
