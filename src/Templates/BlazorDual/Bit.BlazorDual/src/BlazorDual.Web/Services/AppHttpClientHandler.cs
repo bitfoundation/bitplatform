d067fb564d804e9d7be61f16578dc691fc205f8c
Yaser Moradi <ysmoradi@outlook.com>
2023-10-28 07:35:41 +0000

    feat(templates): add disconnect detection support to core projects of the project templates #5817 (#5819)

--- src/Templates/BlazorDual/Bit.BlazorDual/src/BlazorDual.Web/Services/AppHttpClientHandler.cs
+++ src/Templates/BlazorDual/Bit.BlazorDual/src/BlazorDual.Web/Services/AppHttpClientHandler.cs
@@ -24,36 +24,48 @@ public partial class AppHttpClientHandler : HttpClientHandler
         request.Headers.Add("Cookie", $".AspNetCore.Culture={cultureCookie}");
 #endif
 
-        var response = await base.SendAsync(request, cancellationToken);
+        bool serverCommunicationSuccess = false;
 
-        if (response.StatusCode is HttpStatusCode.Unauthorized)
+        try
         {
-            throw new UnauthorizedException();
-        }
+            var response = await base.SendAsync(request, cancellationToken);
 
-        if (response.IsSuccessStatusCode is false && response.Content.Headers.ContentType?.MediaType?.Contains("application/json", StringComparison.InvariantCultureIgnoreCase) is true)
-        {
-            if (response.Headers.TryGetValues("Request-ID", out IEnumerable<string>? values) && values is not null && values.Any())
+            serverCommunicationSuccess = true;
+
+            if (response.StatusCode is HttpStatusCode.Unauthorized)
             {
-                RestErrorInfo restError = (await response!.Content.ReadFromJsonAsync(AppJsonContext.Default.RestErrorInfo, cancellationToken))!;
-
-                Type exceptionType = typeof(RestErrorInfo).Assembly.GetType(restError.ExceptionType!) ?? typeof(UnknownException);
-
-                var args = new List<object?> { typeof(KnownException).IsAssignableFrom(exceptionType) ? new LocalizedString(restError.Key!, restError.Message!) : restError.Message! };
-
-                if (exceptionType == typeof(ResourceValidationException))
-                {
-                    args.Add(restError.Payload);
-                }
-
-                Exception exp = (Exception)Activator.CreateInstance(exceptionType, args.ToArray())!;
-
-                throw exp;
+                throw new UnauthorizedException();
             }
+
+            if (response.IsSuccessStatusCode is false && response.Content.Headers.ContentType?.MediaType?.Contains("application/json", StringComparison.InvariantCultureIgnoreCase) is true)
+            {
+                if (response.Headers.TryGetValues("Request-ID", out IEnumerable<string>? values) && values is not null && values.Any())
+                {
+                    RestErrorInfo restError = (await response!.Content.ReadFromJsonAsync(AppJsonContext.Default.RestErrorInfo, cancellationToken))!;
+
+                    Type exceptionType = typeof(RestErrorInfo).Assembly.GetType(restError.ExceptionType!) ?? typeof(UnknownException);
+
+                    var args = new List<object?> { typeof(KnownException).IsAssignableFrom(exceptionType) ? new LocalizedString(restError.Key!, restError.Message!) : restError.Message! };
+
+                    if (exceptionType == typeof(ResourceValidationException))
+                    {
+                        args.Add(restError.Payload);
+                    }
+
+                    Exception exp = (Exception)Activator.CreateInstance(exceptionType, args.ToArray())!;
+
+                    throw exp;
+                }
+            }
+
+            response.EnsureSuccessStatusCode();
+
+            return response;
         }
-
-        response.EnsureSuccessStatusCode();
-
-        return response;
+        catch (Exception exp) when ((exp is HttpRequestException && serverCommunicationSuccess is false)
+            || exp is TaskCanceledException tcExp && tcExp.InnerException is TimeoutException)
+        {
+            throw new RestException(nameof(AppStrings.UnableToConnectToServer), exp);
+        }
     }
 }
