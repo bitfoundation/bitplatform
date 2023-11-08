2a6b01b39b9dfc3b87484cd798045bd30bf5c8d3
Saleh Yusefnejad <msynk@outlook.com>
2023-11-08 10:47:25 +0330

    release v-7.1.0 #5970

--- src/Bup/Bit.Bup/Scripts/bit-bup.progress.ts
+++ src/Bup/Bit.Bup/Scripts/bit-bup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bup.progress version'] = '7.1.0-pre-05';
+﻿window['bit-bup.progress version'] = '7.1.0';
 
 ; (function () {
     (window as any).startBupProgress = (showLogs: boolean, showAssets: boolean, appContainerSelector: string, hideApp: boolean, autoHide: boolean) => {
