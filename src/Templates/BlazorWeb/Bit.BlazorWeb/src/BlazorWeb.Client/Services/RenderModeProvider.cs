9bb916ba24bf7c84ba8c3b3c298762a4f55bf45d
Yaser Moradi <ysmoradi@outlook.com>
2023-11-12 14:18:15 +0100

    fix(templates): resolve naming conflict between BlazorWeb name and BlazorWebAssembly sdk #6002 (#6005)

--- src/Templates/BlazorWeb/Bit.BlazorWeb/src/BlazorWeb.Client/Services/RenderModeProvider.cs
+++ src/Templates/BlazorWeb/Bit.BlazorWeb/src/BlazorWeb.Client/Services/RenderModeProvider.cs
@@ -5,11 +5,11 @@ namespace BlazorWeb.Client.Services;
 public static class RenderModeProvider
 {
     static IComponentRenderMode PrerenderEnabledAuto = RenderMode.InteractiveAuto;
-    static IComponentRenderMode PrerenderEnabledBlazorWebAssembly = RenderMode.InteractiveWebAssembly;
+    static IComponentRenderMode PrerenderEnabledBlazorWasm = RenderMode.InteractiveWebAssembly;
     static IComponentRenderMode PrerenderEnabledBlazorServer = RenderMode.InteractiveServer;
 
     static IComponentRenderMode Auto = new InteractiveAutoRenderMode(prerender: false);
-    static IComponentRenderMode BlazorWebAssembly = new InteractiveWebAssemblyRenderMode(prerender: false);
+    static IComponentRenderMode BlazorWasm = new InteractiveWebAssemblyRenderMode(prerender: false);
     static IComponentRenderMode BlazorServer = new InteractiveServerRenderMode(prerender: false);
 
     // PrerenderOnly: In order to have prerender only mode, simply remove @rendermode usages from App.razor
