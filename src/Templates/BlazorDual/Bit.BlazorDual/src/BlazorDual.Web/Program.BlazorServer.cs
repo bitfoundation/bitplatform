fb829b43ea9fb358084909265b0c0d26c11b9c12
ysmoradi <ysmoradi@outlook.com>
2023-11-12 02:21:16 +0100

    fix

--- src/Templates/BlazorDual/Bit.BlazorDual/src/BlazorDual.Web/Program.BlazorServer.cs
+++ src/Templates/BlazorDual/Bit.BlazorDual/src/BlazorDual.Web/Program.BlazorServer.cs
@@ -9,7 +9,7 @@ public partial class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Configuration.AddJsonStream(typeof(MainLayout).Assembly.GetManifestResourceStream("BlazorDual.Web.appsettings.json")!);
+        builder.Configuration.AddClientConfigurations();
 
         Startup.Services.Add(builder.Services, builder.Configuration);
 
