d3a8095f748629c04a2f998e0e9bfbda6e553c94
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2023-11-20 21:16:32 +0330

    feat(blazorui): add browse public api to BitFileUpload #6079 (#6083)

--- src/BlazorUI/Bit.BlazorUI/Components/FileUpload/BitFileUpload.razor.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/FileUpload/BitFileUpload.razor.cs
@@ -315,6 +315,17 @@ public partial class BitFileUpload : IDisposable
         IsRemoving = false;
     }
 
+    /// <summary>
+    /// Open a file selection dialog
+    /// </summary>
+    /// <returns></returns>
+    public async Task Browse()
+    {
+        if (IsEnabled is false) return;
+
+        await _js.Browse(inputFileElement);
+    }
+
     protected override Task OnInitializedAsync()
     {
         InputId = $"FileUpload-{UniqueId}-input";
