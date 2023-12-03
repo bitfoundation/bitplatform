7fb8e95a6f8c9d9c8a49440806c99b2a2b243963
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-12-03 17:11:22 +0330

    Replace CSS aproach with C#

--- src/BlazorUI/Bit.BlazorUI/Components/Tooltip/BitTooltip.razor.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Tooltip/BitTooltip.razor.cs
@@ -1,9 +1,25 @@
-﻿namespace Bit.BlazorUI;
+﻿using System.Text;
+
+namespace Bit.BlazorUI;
 
 public partial class BitTooltip
 {
+    private bool IsShownHasBeenSet;
+
+    private bool isShown;
     private BitTooltipPosition tooltipPosition = BitTooltipPosition.Top;
 
+
+
+    /// <summary>
+    /// The content inside of tooltip tag, It can be Any custom tag or a text
+    /// </summary>
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// Alias of ChildContent.
+    /// </summary>
+    [Parameter] public RenderFragment? Anchor { get; set; }
 
     /// <summary>
     /// The position of tooltip around its anchor
@@ -22,42 +38,122 @@ public partial class BitTooltip
     }
 
     /// <summary>
-    /// The text of tooltip to show
-    /// </summary>
-    [Parameter] public string? Text { get; set; }
-
-    /// <summary>
     /// The content of tooltip, It can be any custom tag or a text
     /// </summary>
     [Parameter] public RenderFragment? Template { get; set; }
 
     /// <summary>
-    /// The content of inside of tooltip, It can be Any custom tag or a text
+    /// The text of tooltip to show
     /// </summary>
-    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public string? Text { get; set; }
 
     /// <summary>
-    /// Alias of ChildContent.
+    /// Determines on which events the tooltip will act
     /// </summary>
-    [Parameter] public RenderFragment? Anchor { get; set; }
+    [Parameter] public bool ShowOnHover { get; set; } = true;
+
+    /// <summary>
+    /// Determines on which events the tooltip will act
+    /// </summary>
+    [Parameter] public bool ShowOnFocus { get; set; } = true;
+
+    /// <summary>
+    /// Determines on which events the tooltip will act
+    /// </summary>
+    [Parameter] public bool ShowOnClick { get; set; }
+
+    /// <summary>
+    /// The visible state of the Tooltip.
+    /// </summary>
+    [Parameter]
+    public bool IsShown
+    {
+        get => isShown;
+        set
+        {
+            if (value == isShown) return;
+
+            isShown = value;
+            _ = IsShownChanged.InvokeAsync(isShown);
+        }
+    }
+
+    [Parameter] public EventCallback<bool> IsShownChanged { get; set; }
+
 
     protected override string RootElementClass => "bit-ttp";
 
 
-    private string GetTooltipPositionClasses() => Position switch
+
+    private async Task HandleMouseEnter()
     {
-        BitTooltipPosition.Top => "bit-ttp-top",
-        BitTooltipPosition.TopLeft => "bit-ttp-tlf",
-        BitTooltipPosition.TopRight => "bit-ttp-trg",
-        BitTooltipPosition.RightTop => "bit-ttp-rtp",
-        BitTooltipPosition.Right => "bit-ttp-rgt",
-        BitTooltipPosition.RightBottom => "bit-ttp-rbm",
-        BitTooltipPosition.BottomRight => "bit-ttp-brg",
-        BitTooltipPosition.Bottom => "bit-ttp-btm",
-        BitTooltipPosition.BottomLeft => "bit-ttp-blf",
-        BitTooltipPosition.LeftBottom => "bit-ttp-lbm",
-        BitTooltipPosition.Left => "bit-ttp-lft",
-        BitTooltipPosition.LeftTop => "bit-ttp-ltp",
-        _ => "bit-ttp-top",
-    };
+        if (IsShownHasBeenSet && IsShownChanged.HasDelegate is false) return;
+
+        if (ShowOnHover)
+        {
+            IsShown = true;
+        }
+    }
+
+    private async Task HandleMouseLeave()
+    {
+        if (IsShownHasBeenSet && IsShownChanged.HasDelegate is false) return;
+        if (ShowOnHover == false) return;
+
+        IsShown = false;
+    }
+
+    private async Task HandleFocusIn()
+    {
+        if (IsShownHasBeenSet && IsShownChanged.HasDelegate is false) return;
+
+        if (ShowOnFocus)
+        {
+            IsShown = true;
+        }
+    }
+
+    private async Task HandleFocusOut()
+    {
+        if (IsShownHasBeenSet && IsShownChanged.HasDelegate is false) return;
+        if (ShowOnFocus == false) return;
+
+        IsShown = false;
+    }
+
+    private async Task HandleMouseUp()
+    {
+        if (IsShownHasBeenSet && IsShownChanged.HasDelegate is false) return;
+
+        if (ShowOnClick)
+        {
+            IsShown = !IsShown;
+        }
+    }
+
+    private string GetTooltipClasses()
+    {
+        StringBuilder className = new StringBuilder();
+
+        className.Append(IsShown ? "bit-ttp-vis" : string.Empty);
+
+        className.Append(' ').Append(Position switch
+        {
+            BitTooltipPosition.Top => "bit-ttp-top",
+            BitTooltipPosition.TopLeft => "bit-ttp-tlf",
+            BitTooltipPosition.TopRight => "bit-ttp-trg",
+            BitTooltipPosition.RightTop => "bit-ttp-rtp",
+            BitTooltipPosition.Right => "bit-ttp-rgt",
+            BitTooltipPosition.RightBottom => "bit-ttp-rbm",
+            BitTooltipPosition.BottomRight => "bit-ttp-brg",
+            BitTooltipPosition.Bottom => "bit-ttp-btm",
+            BitTooltipPosition.BottomLeft => "bit-ttp-blf",
+            BitTooltipPosition.LeftBottom => "bit-ttp-lbm",
+            BitTooltipPosition.Left => "bit-ttp-lft",
+            BitTooltipPosition.LeftTop => "bit-ttp-ltp",
+            _ => "bit-ttp-top"
+        });
+
+        return className.ToString();
+    }
 }
