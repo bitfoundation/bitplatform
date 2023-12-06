cf7602fbd46368af23e524dc65f8d074600d6d6e
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-12-06 13:20:23 +0330

    Complete the members of the ButilKeys class #6231

--- src/Butil/Bit.Butil/Publics/Events/ButilKeys.cs
+++ src/Butil/Bit.Butil/Publics/Events/ButilKeys.cs
@@ -2,6 +2,143 @@
 
 public static class ButilKeys
 {
+    // Letters
     public const string A = "A";
     public const string B = "B";
+    public const string C = "C";
+    public const string D = "D";
+    public const string E = "E";
+    public const string F = "F";
+    public const string G = "G";
+    public const string H = "H";
+    public const string I = "I";
+    public const string J = "J";
+    public const string K = "K";
+    public const string L = "L";
+    public const string M = "M";
+    public const string N = "N";
+    public const string O = "O";
+    public const string P = "P";
+    public const string Q = "Q";
+    public const string R = "R";
+    public const string S = "S";
+    public const string T = "T";
+    public const string U = "U";
+    public const string V = "V";
+    public const string W = "W";
+    public const string X = "X";
+    public const string Y = "Y";
+    public const string Z = "Z";
+
+    // Numbers
+    public const string Num0 = "0";
+    public const string Num1 = "1";
+    public const string Num2 = "2";
+    public const string Num3 = "3";
+    public const string Num4 = "4";
+    public const string Num5 = "5";
+    public const string Num6 = "6";
+    public const string Num7 = "7";
+    public const string Num8 = "8";
+    public const string Num9 = "9";
+
+    public const string NumLock = "NumLock";
+    public const string NumPad0 = "NumPad0";
+    public const string NumPad1 = "NumPad1";
+    public const string NumPad2 = "NumPad2";
+    public const string NumPad3 = "NumPad3";
+    public const string NumPad4 = "NumPad4";
+    public const string NumPad5 = "NumPad5";
+    public const string NumPad6 = "NumPad6";
+    public const string NumPad7 = "NumPad7";
+    public const string NumPad8 = "NumPad8";
+    public const string NumPad9 = "NumPad9";
+    public const string NumPadAdd = "NumPadAdd";
+    public const string NumPadSubtract = "NumPadSubtract";
+    public const string NumPadMultiply = "NumPadMultiply";
+    public const string NumPadDivide = "NumPadDivide";
+    public const string NumPadDecimal = "NumPadDecimal";
+    public const string NumPadEnter = "NumPadEnter";
+
+    // Symbols
+    public const string Exclamation = "!";
+    public const string At = "@";
+    public const string Pound = "#";
+    public const string Dollar = "$";
+    public const string Percent = "%";
+    public const string Caret = "^";
+    public const string Ampersand = "&";
+    public const string Asterisk = "*";
+    public const string OpenParenthesis = "(";
+    public const string CloseParenthesis = ")";
+    public const string Underscore = "_";
+    public const string Plus = "+";
+    public const string Minus = "-";
+    public const string EqualsTo = "=";
+    public const string OpenBrace = "{";
+    public const string CloseBrace = "}";
+    public const string OpenBracket = "[";
+    public const string CloseBracket = "]";
+    public const string Pipe = "|";
+    public const string Backslash = "\\";
+    public const string Colon = ":";
+    public const string Semicolon = ";";
+    public const string DoubleQuote = "\"";
+    public const string SingleQuote = "'";
+    public const string LessThan = "<";
+    public const string GreaterThan = ">";
+    public const string Comma = ",";
+    public const string Period = ".";
+    public const string QuestionMark = "?";
+    public const string Slash = "/";
+    public const string Space = " ";
+
+    // Function keys
+    public const string Esc = "Escape";
+    public const string F1 = "F1";
+    public const string F2 = "F2";
+    public const string F3 = "F3";
+    public const string F4 = "F4";
+    public const string F5 = "F5";
+    public const string F6 = "F6";
+    public const string F7 = "F7";
+    public const string F8 = "F8";
+    public const string F9 = "F9";
+    public const string F10 = "F10";
+    public const string F11 = "F11";
+    public const string F12 = "F12";
+
+    public const string Insert = "Insert";
+    public const string Delete = "Delete";
+    public const string Home = "Home";
+    public const string End = "End";
+    public const string PageUp = "PageUp";
+    public const string PageDown = "PageDown";
+
+    public const string ArrowUp = "UpArrow";
+    public const string ArrowDown = "DownArrow";
+    public const string ArrowLeft = "LeftArrow";
+    public const string ArrowRight = "RightArrow";
+
+    public const string Backspace = "Backspace";
+    public const string Tab = "Tab";
+    public const string Enter = "Enter";
+    public const string Shift = "Shift";
+    public const string Ctrl = "Ctrl";
+    public const string Alt = "Alt";
+    public const string CapsLock = "CapsLock";
+    public const string Windows = "Windows";
+
+    public const string PrintScreen = "PrintScreen";
+    public const string ScrollLock = "ScrollLock";
+    public const string PauseBreak = "PauseBreak";
+
+    // Media keys
+    public const string VolumeMute = "VolumeMute";
+    public const string VolumeUp = "VolumeUp";
+    public const string VolumeDown = "VolumeDown";
+    public const string MediaPlayPause = "MediaPlayPause";
+    public const string MediaStop = "MediaStop";
+    public const string MediaPreviousTrack = "MediaPreviousTrack";
+    public const string MediaNextTrack = "MediaNextTrack";
 }
