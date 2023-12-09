d5ba85f40269852a7adeb8af0044abd9d3f92131
Mohammad Hossein Rastegarinia <mh.rastegari@outlook.com>
2023-12-09 20:29:42 +0330

    feat(blazorui): add BitShimmer component #6250 (#6251)

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Shimmer/BitShimmerClassStyles.cs
@@ -0,0 +1,24 @@
+﻿namespace Bit.BlazorUI;
+
+public class BitShimmerClassStyles
+{
+    /// <summary>
+    /// Custom CSS classes/styles for the root element of the BitShimmer.
+    /// </summary>
+    public string? Root { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the content of the BitShimmer.
+    /// </summary>
+    public string? Content { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the shimmer wrapper of the BitShimmer.
+    /// </summary>
+    public string? ShimmerWrapper { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the shimmer of the BitShimmer.
+    /// </summary>
+    public string? Shimmer { get; set; }
+}
