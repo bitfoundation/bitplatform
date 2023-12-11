cd2cda5f963cc5f6f8d25355add3d0ad18875077
Yaser Moradi <ysmoradi@outlook.com>
2023-12-11 03:39:06 +0100

    fix

---
+++ src/SourceGenerators/Bit.SourceGenerators/HttpClientProxy/HttpClientProxySourceGenerator.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace Bit.SourceGenerators;
+
+[Generator]
+public class HttpClientProxySourceGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        context.RegisterForSyntaxNotifications(() => new HttpClientProxySyntaxReceiver());
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        if (context.SyntaxContextReceiver is not HttpClientProxySyntaxReceiver receiver)
+        {
+            return;
+        }
+
+        StringBuilder generatedClasses = new();
+
+        foreach (var iController in receiver.IControllers)
+        {
+            StringBuilder generatedMethods = new();
+
+            foreach (var action in iController.Actions)
+            {
+                string parameters = string.Join(", ", action.Parameters.Select(p => $"{p.Type.ToDisplayString()} {p.Name}"));
+
+                var hasQueryString = action.Url.Contains('?');
+
+                generatedMethods.AppendLine($@"
+        public async {action.ReturnType.ToDisplayString()} {action.Method.Name}({parameters})
+        {{
+            {$@"var url = $""{action.Url}"";"}
+            var dynamicQS = GetDynamicQueryString();
+            if (dynamicQS is not null)
+            {{
+                url += {(action.Url.Contains('?') ? "'&'" : "'?'")} + dynamicQS;
+            }}
+            {(action.DoesReturnSomething ? $@"return (await prerenderStateService.GetValue(url, async () =>
+            {{" : string.Empty)}
+                using var request = new HttpRequestMessage(HttpMethod.{action.HttpMethod}, url);
+                {(action.BodyParameter is not null ? $@"request.Content = JsonContent.Create({action.BodyParameter.Name}, options.GetTypeInfo<{action.BodyParameter.Type.ToDisplayString()}>());" : string.Empty)}
+                using var response = await httpClient.SendAsync(request{(action.HasCancellationToken ? ", cancellationToken" : string.Empty)});
+                {(action.DoesReturnSomething ? ($"return await response.Content.ReadFromJsonAsync(options.GetTypeInfo<{action.ReturnType.GetUnderlyingType().ToDisplayString()}>(){(action.HasCancellationToken ? ", cancellationToken" : string.Empty)});}}));") : string.Empty)}
+        }}
+");
+            }
+
+            generatedClasses.AppendLine($@"
+    internal class {iController.ClassName}(HttpClient httpClient, JsonSerializerOptions options, IPrerenderStateService prerenderStateService) : AppControllerBase, {iController.Symbol.ToDisplayString()}
+    {{
+        {generatedMethods}
+    }}");
+        }
+
+        StringBuilder finalSource = new(@$"
+using System.Text.Json;
+using System.Web;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+[global::System.CodeDom.Compiler.GeneratedCode(""Bit.SourceGenerators"",""{BitSourceGeneratorUtil.GetPackageVersion()}"")]
+[global::System.Diagnostics.DebuggerNonUserCode]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+internal static class IHttpClientServiceCollectionExtensions
+{{
+    public static void AddTypedHttpClients(this IServiceCollection services)
+    {{
+{string.Join(Environment.NewLine, receiver.IControllers.Select(i => $"        services.TryAddTransient<{i.Symbol.ToDisplayString()}, {i.ClassName}>();"))}
+    }}
+
+    internal class AppControllerBase
+    {{
+        Dictionary<string, object?> queryString = [];
+
+        public void AddQueryString(string key, object? value)
+        {{
+            queryString.Add(key, value);    
+        }}
+
+        public void AddQueryString(Dictionary<string, object?> queryString)
+        {{
+            foreach (var key in queryString.Keys)
+            {{
+                AddQueryString(key, queryString[key]);
+            }}
+        }}
+
+        protected string? GetDynamicQueryString()
+        {{
+            if (queryString is not {{ Count: > 0 }})
+                return null;
+
+            var collection = HttpUtility.ParseQueryString(string.Empty);
+
+            foreach (var key in queryString.Keys)
+            {{
+                collection.Add(key, queryString[key]?.ToString());
+            }}
+
+            queryString.Clear();
+
+            return collection.ToString();
+        }}
+    }}
+
+{generatedClasses}
+
+}}
+");
+
+        File.WriteAllText(@$"C:\Workspace\temp\{Guid.NewGuid()}.txt", finalSource.ToString());
+        context.AddSource($"HttpClientProxy.cs", finalSource.ToString());
+    }
+}
---
+++ src/SourceGenerators/Bit.SourceGenerators/HttpClientProxy/IController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
+
+namespace Bit.SourceGenerators;
+
+public class IController
+{
+    public string Name { get; set; } = default!;
+
+    public string ClassName { get; set; } = default!;
+
+    public ITypeSymbol Symbol { get; set; } = default!;
+
+    public InterfaceDeclarationSyntax Syntax { get; set; } = default!;
+
+    public List<ControllerAction> Actions { get; set; } = [];
+}
+
+public class ControllerAction
+{
+    public IMethodSymbol Method { get; set; } = default!;
+
+    public ITypeSymbol ReturnType { get; set; } = default!;
+
+    public bool DoesReturnSomething => ReturnType.ToDisplayString() is not "System.Threading.Tasks.Task" or "System.Threading.Tasks.ValueTask";
+
+    public string HttpMethod { get; set; } = default!;
+
+    public string Url { get; set; } = default!;
+
+    public List<ActionParameter> Parameters { get; set; } = [];
+
+    public ActionParameter? BodyParameter { get; set; }
+
+    public bool HasCancellationToken { get; set; }
+}
+
+public class ActionParameter
+{
+    public string Name { get; set; } = default!;
+
+    public ITypeSymbol Type { get; set; } = default!;
+}
+
