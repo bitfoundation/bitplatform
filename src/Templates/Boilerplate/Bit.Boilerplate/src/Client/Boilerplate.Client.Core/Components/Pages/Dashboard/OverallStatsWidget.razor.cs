ff73af374c06bf30146299f1f99f7b1eca5fe997
Yaser Moradi <ysmoradi@outlook.com>
2023-12-11 09:22:19 +0100

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Dashboard/OverallStatsWidget.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Dashboard/OverallStatsWidget.razor.cs
@@ -1,4 +1,4 @@
-﻿using Boilerplate.Client.Core.Controllers.Product;
+﻿using Boilerplate.Client.Core.Controllers.Dashboard;
 using Boilerplate.Shared.Dtos.Dashboard;
 
 namespace Boilerplate.Client.Core.Components.Pages.Dashboard;
--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Dashboard/ProductsPercentageWidget.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Dashboard/ProductsPercentageWidget.razor.cs
@@ -1,4 +1,4 @@
-﻿using Boilerplate.Client.Core.Controllers.Product;
+﻿using Boilerplate.Client.Core.Controllers.Dashboard;
 
 namespace Boilerplate.Client.Core.Components.Pages.Dashboard;
 
