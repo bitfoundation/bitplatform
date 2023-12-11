eece2071bcc04dbb1299425f6ab2be8ffe780217
Saleh Yusefnejad <msynk@outlook.com>
2023-12-11 15:35:19 +0330

    feat(butil): improve the Element class of the Butil #6268 (#6285)

--- src/Butil/Bit.Butil/Publics/Console.cs
+++ src/Butil/Bit.Butil/Publics/Console.cs
@@ -6,117 +6,71 @@ namespace Bit.Butil;
 public class Console(IJSRuntime js)
 {
     public async Task Assert(bool? condition, params object?[]? args)
-    {
-        await js.ConsoleAssert(condition, args);
-    }
+        => await js.ConsoleAssert(condition, args);
 
     public async Task Clear()
-    {
-        await js.ConsoleClear();
-    }
+        => await js.ConsoleClear();
 
     public async Task Count(string? label)
-    {
-        await js.ConsoleCount(label);
-    }
+        => await js.ConsoleCount(label);
 
     public async Task CountReset(string? label)
-    {
-        await js.ConsoleCountReset(label);
-    }
+        => await js.ConsoleCountReset(label);
 
     public async Task Debug(params object?[]? args)
-    {
-        await js.ConsoleDebug(args);
-    }
+        => await js.ConsoleDebug(args);
 
     public async Task Dir(object? item, object? options)
-    {
-        await js.ConsoleDir(item, options);
-    }
+        => await js.ConsoleDir(item, options);
 
     public async Task Dirxml(params object?[]? args)
-    {
-        await js.ConsoleDirxml(args);
-    }
+        => await js.ConsoleDirxml(args);
 
     public async Task Error(params object?[]? args)
-    {
-        await js.ConsoleError(args);
-    }
+        => await js.ConsoleError(args);
 
     public async Task Group(params object?[]? args)
-    {
-        await js.ConsoleGroup(args);
-    }
+        => await js.ConsoleGroup(args);
 
     public async Task GroupCollapsed(params object?[]? args)
-    {
-        await js.ConsoleGroupCollapsed(args);
-    }
+        => await js.ConsoleGroupCollapsed(args);
 
     public async Task GroupEnd()
-    {
-        await js.ConsoleGroupEnd();
-    }
+        => await js.ConsoleGroupEnd();
 
     public async Task Info(params object?[]? args)
-    {
-        await js.ConsoleInfo(args);
-    }
+        => await js.ConsoleInfo(args);
 
     public async Task Log(params object?[]? args)
-    {
-        await js.ConsoleLog(args);
-    }
+        => await js.ConsoleLog(args);
 
     public async Task Memory()
-    {
-        await js.ConsoleMemory();
-    }
+        => await js.ConsoleMemory();
 
     public async Task Profile()
-    {
-        await js.ConsoleProfile();
-    }
+        => await js.ConsoleProfile();
 
     public async Task ProfileEnd()
-    {
-        await js.ConsoleProfileEnd();
-    }
+        => await js.ConsoleProfileEnd();
 
     public async Task Table(object? data, object? properties)
-    {
-        await js.ConsoleTable(data, properties);
-    }
+        => await js.ConsoleTable(data, properties);
 
     public async Task Time(string? label)
-    {
-        await js.ConsoleTime(label);
-    }
+        => await js.ConsoleTime(label);
 
     public async Task TimeEnd(string? label)
-    {
-        await js.ConsoleTimeEnd(label);
-    }
+        => await js.ConsoleTimeEnd(label);
 
     public async Task TimeLog(string? label, params object?[]? args)
-    {
-        await js.ConsoleTimeLog(label, args);
-    }
+        => await js.ConsoleTimeLog(label, args);
 
     public async Task TimeStamp(string? label)
-    {
-        await js.ConsoleTimeStamp(label);
-    }
+        => await js.ConsoleTimeStamp(label);
 
     public async Task Trace(params object?[]? args)
-    {
-        await js.ConsoleTrace(args);
-    }
+        => await js.ConsoleTrace(args);
 
     public async Task Warn(params object?[]? args)
-    {
-        await js.ConsoleWarn(args);
-    }
+        => await js.ConsoleWarn(args);
 }
--- src/Butil/Bit.Butil/Scripts/element.ts
+++ src/Butil/Bit.Butil/Scripts/element.ts
@@ -6,15 +6,35 @@ var BitButil = BitButil || {};
         getAttribute,
         getAttributeNames,
         getBoundingClientRect,
+        hasAttribute,
+        hasAttributes,
+        hasPointerCapture,
+        matches,
         releasePointerCapture,
         remove,
         removeAttribute,
         requestFullScreen,
+        requestPointerLock,
         scroll,
         scrollBy,
         scrollIntoView,
         setAttribute,
-        setPointerCapture
+        setPointerCapture,
+        toggleAttribute,
+        getAccessKey, setAccessKey,
+        getClassName, setClassName,
+        clientHeight,
+        clientLeft,
+        clientTop,
+        clientWidth,
+        getId, setId,
+        getInnerHTML, setInnerHTML,
+        getOuterHTML, setOuterHTML,
+        scrollHeight,
+        scrollLeft,
+        scrollTop,
+        scrollWidth,
+        tagName
     };
 
     function blur(element: HTMLElement) {
@@ -33,6 +53,22 @@ var BitButil = BitButil || {};
         return element.getBoundingClientRect();
     }
 
+    function hasAttribute(element: HTMLElement, name: string) {
+        return element.hasAttribute(name);
+    }
+
+    function hasAttributes(element: HTMLElement) {
+        return element.hasAttributes();
+    }
+
+    function hasPointerCapture(element: HTMLElement, pointerId: number) {
+        return element.hasPointerCapture(pointerId);
+    }
+
+    function matches(element: HTMLElement, selectors: string) {
+        return element.matches(selectors);
+    }
+
     function releasePointerCapture(element: HTMLElement, pointerId: number) {
         element.releasePointerCapture(pointerId);
     }
@@ -47,6 +83,10 @@ var BitButil = BitButil || {};
 
     function requestFullScreen(element: HTMLElement, options?: FullscreenOptions) {
         return element.requestFullscreen(options);
+    }
+
+    function requestPointerLock(element: HTMLElement) {
+        return element.requestPointerLock();
     }
 
     function scroll(element: HTMLElement, options?: ScrollToOptions, x?: number, y?: number) {
@@ -77,4 +117,83 @@ var BitButil = BitButil || {};
         element.setPointerCapture(pointerId);
     }
 
+    function toggleAttribute(element: HTMLElement, name: string, force?: boolean) {
+        return element.toggleAttribute(name, force);
+    }
+
+    function getAccessKey(element: HTMLElement) {
+        return element.accessKey;
+    }
+
+    function setAccessKey(element: HTMLElement, key: string) {
+        element.accessKey = key;
+    }
+
+    function getClassName(element: HTMLElement) {
+        return element.className;
+    }
+
+    function setClassName(element: HTMLElement, className: string) {
+        element.className = className;
+    }
+
+    function clientHeight(element: HTMLElement) {
+        return element.clientHeight;
+    }
+
+    function clientLeft(element: HTMLElement) {
+        return element.clientLeft;
+    }
+
+    function clientTop(element: HTMLElement) {
+        return element.clientTop;
+    }
+
+    function clientWidth(element: HTMLElement) {
+        return element.clientWidth;
+    }
+
+    function getId(element: HTMLElement) {
+        return element.id;
+    }
+
+    function setId(element: HTMLElement, id: string) {
+        element.id = id;
+    }
+
+    function getInnerHTML(element: HTMLElement) {
+        return element.innerHTML;
+    }
+
+    function setInnerHTML(element: HTMLElement, innerHTML: string) {
+        element.innerHTML = innerHTML;
+    }
+
+    function getOuterHTML(element: HTMLElement) {
+        return element.outerHTML;
+    }
+
+    function setOuterHTML(element: HTMLElement, outerHTML: string) {
+        element.outerHTML = outerHTML;
+    }
+
+    function scrollHeight(element: HTMLElement) {
+        return element.scrollHeight;
+    }
+
+    function scrollLeft(element: HTMLElement) {
+        return element.scrollLeft;
+    }
+
+    function scrollTop(element: HTMLElement) {
+        return element.scrollTop;
+    }
+
+    function scrollWidth(element: HTMLElement) {
+        return element.scrollWidth;
+    }
+
+    function tagName(element: HTMLElement) {
+        return element.tagName;
+    }
 }(BitButil));
