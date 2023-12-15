cd416bfd685a5d1900e3b593a58c3717c9b7ed47
Yaser Moradi <ysmoradi@outlook.com>
2023-12-15 02:00:58 +0100

    Add support for win 7+ exe to bit boilerplate

---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.WinExe/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using Boilerplate.Client.WinExe.Services;
+
+namespace Boilerplate.Client.WinExe;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        AppRenderMode.IsBlazorHybrid = true;
+        var services = new ServiceCollection();
+        ConfigurationBuilder configurationBuilder = new();
+        configurationBuilder.AddClientConfigurations();
+        var configuration = configurationBuilder.Build();
+        services.AddTransient<IConfiguration>(sp => configuration);
+        Uri.TryCreate(configuration.GetApiServerAddress(), UriKind.Absolute, out var apiServerAddress);
+        services.AddTransient(sp =>
+        {
+            var handler = sp.GetRequiredKeyedService<HttpMessageHandler>("DefaultMessageHandler");
+            HttpClient httpClient = new(handler)
+            {
+                BaseAddress = apiServerAddress
+            };
+            return httpClient;
+        });
+        services.AddWpfBlazorWebView();
+        if (BuildConfiguration.IsDebug())
+        {
+            services.AddBlazorWebViewDeveloperTools();
+        }
+        services.AddTransient<IBitDeviceCoordinator, WindowsDeviceCoordinator>();
+        services.AddTransient<IExceptionHandler, WindowsExceptionHandler>();
+        services.AddClientSharedServices();
+        Resources.Add("services", services.BuildServiceProvider());
+        InitializeComponent();
+    }
+}
