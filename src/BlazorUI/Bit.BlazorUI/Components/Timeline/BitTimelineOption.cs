afcc3f56dce8ff67abf27641a58d95540f15d6a9
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-12-18 22:55:28 +0330

    Add BitTimeline component

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Timeline/BitTimelineOption.cs
@@ -0,0 +1,104 @@
+﻿namespace Bit.BlazorUI;
+
+public partial class BitTimelineOption : ComponentBase, IDisposable
+{
+    private bool _disposed;
+
+
+    [CascadingParameter] protected BitTimeline<BitTimelineOption> Parent { get; set; } = default!;
+
+
+    /// <summary>
+    /// The custom CSS classes of the option.
+    /// </summary>
+    [Parameter] public string? Class { get; set; }
+
+    /// <summary>
+    /// The custom template for the item's dot.
+    /// </summary>
+    [Parameter] public RenderFragment<BitTimelineOption>? DotTemplate { get; set; }
+
+    /// <summary>
+    /// Hides the item's dot.
+    /// </summary>
+    [Parameter] public bool HideDot { get; set; }
+
+    /// <summary>
+    /// Name of an icon to render in the option.
+    /// </summary>
+    [Parameter] public string? IconName { get; set; }
+
+    /// <summary>
+    /// Whether or not the option is enabled.
+    /// </summary>
+    [Parameter] public bool IsEnabled { get; set; } = true;
+
+    /// <summary>
+    /// A unique value to use as a key of the option
+    /// </summary>
+    [Parameter] public string? Key { get; set; }
+
+    /// <summary>
+    /// Click event handler of the option.
+    /// </summary>
+    [Parameter] public EventCallback<BitTimelineOption> OnClick { get; set; }
+
+    /// <summary>
+    /// The primary content of the item.
+    /// </summary>
+    [Parameter] public RenderFragment<BitTimelineItem>? PrimaryContent { get; set; }
+
+    /// <summary>
+    /// The primary text of the item.
+    /// </summary>
+    [Parameter] public string? PrimaryText { get; set; }
+
+    /// <summary>
+    /// Reverses the item direction.
+    /// </summary>
+    [Parameter] public bool Reversed { get; set; }
+
+    /// <summary>
+    /// The secondary content of the item.
+    /// </summary>
+    [Parameter] public RenderFragment<BitTimelineItem>? SecondaryContent { get; set; }
+
+    /// <summary>
+    /// The secondary text of the item.
+    /// </summary>
+    [Parameter] public string? SecondaryText { get; set; }
+
+    /// <summary>
+    /// The custom value for the style attribute of the option.
+    /// </summary>
+    [Parameter] public string? Style { get; set; }
+
+    /// <summary>
+    /// The custom template for the option.
+    /// </summary>
+    [Parameter] public RenderFragment<BitTimelineOption>? Template { get; set; }
+
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        Parent.RegisterOption(this);
+
+        await base.OnInitializedAsync();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing is false || _disposed) return;
+
+        Parent.UnregisterOption(this);
+
+        _disposed = true;
+    }
+}
