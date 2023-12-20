b707910a37568a5d9a4af9c15b40f737d1fe8d07
Saleh Yusefnejad <msynk@outlook.com>
2023-12-20 13:41:03 +0330

    feat(butil): add missing members of Document class in Butil #6350 (#6353)

---
+++ src/Butil/Bit.Butil/Publics/Document/CompatMode.cs
@@ -0,0 +1,14 @@
+﻿namespace Bit.Butil;
+
+public enum CompatMode
+{
+    /// <summary>
+    /// The document is in quirks mode.
+    /// </summary>
+    BackCompat,
+
+    /// <summary>
+    /// The document is in no-quirks (also known as "standards") mode or limited-quirks (also known as "almost standards") mode.
+    /// </summary>
+    CSS1Compat
+}
