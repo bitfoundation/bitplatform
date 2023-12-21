261f7ccef02501e29bbaaf1495e6e5cb3eb0426d
Saleh Yusefnejad <msynk@outlook.com>
2023-12-22 01:07:34 +0330

    1st commit

---
+++ src/Butil/Bit.Butil/Publics/ElementReferenceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+
+namespace Bit.Butil;
+
+public static class ElementReferenceExtensions
+{
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "get_JSRuntime")]
+    extern static IJSRuntime JSRuntimeGetter(WebElementReferenceContext context);
+
+    private static IJSRuntime GetJSRuntime(ElementReference elementReference)
+    {
+        var context = (elementReference.Context as WebElementReferenceContext) ?? throw new InvalidOperationException("ElementReference has not been configured correctly.");
+        return JSRuntimeGetter(context);
+    }
+
+
+    public static ValueTask Blur(this ElementReference element)
+    {
+        return GetJSRuntime(element).InvokeVoidAsync("BitButil.element.blur", element);
+    }
+
+    public static ValueTask<Rect> GetBoundingClientRect(this ElementReference element)
+    {
+        //_ = element.FocusAsync();
+        return GetJSRuntime(element).InvokeAsync<Rect>("BitButil.element.getBoundingClientRect", element);
+    }
+}
