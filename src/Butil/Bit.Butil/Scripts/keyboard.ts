5718793f1f8407b0b5cbc6c77d24fa2016f3182e
Saleh Yusefnejad <msynk@outlook.com>
2023-12-25 16:08:43 +0330

    fix(butil): resolve issues of key modifier handling in Butil #6398 (#6401)

--- src/Butil/Bit.Butil/Scripts/keyboard.ts
+++ src/Butil/Bit.Butil/Scripts/keyboard.ts
@@ -11,11 +11,11 @@ var BitButil = BitButil || {};
     function add(methodName, listenerId, code, alt, ctrl, meta, shift, preventDefault, stopPropagation, repeat) {
         const handler = e => {
             if (e.code !== code) return;
-            
-            if (!alt && e.altKey) return;
-            if (!ctrl && e.ctrlKey) return;
-            if (!meta && e.metaKey) return;
-            if (!shift && e.shiftKey) return;
+
+            if ((!alt && e.altKey) || (alt && !e.altKey)) return;
+            if ((!ctrl && e.ctrlKey) || (ctrl && !e.ctrlKey)) return;
+            if ((!meta && e.metaKey) || (meta && !e.metaKey)) return;
+            if ((!shift && e.shiftKey) || (shift && !e.shiftKey)) return;
 
             if (!repeat && e.repeat) return;
 
