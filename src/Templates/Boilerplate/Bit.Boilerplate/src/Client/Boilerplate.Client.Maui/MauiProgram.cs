1f4ad0a2bb86d0e5f5b2273a7182bf23fdf06608
ysmoradi <ysmoradi@outlook.com>
2023-12-25 05:03:27 +0100

    Add iOS universal links support to bit boilerplate project template

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Maui/MauiProgram.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Maui/MauiProgram.cs
@@ -1,7 +1,6 @@
 ﻿//-:cnd:noEmit
-
-using System.Reflection;
-using Boilerplate.Client.Maui.Services;
+using Boilerplate.Client.Core;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace Boilerplate.Client.Maui;
 
@@ -39,6 +38,44 @@ public static class MauiProgram
         });
 
         services.AddClientMauiServices();
+
+        builder.ConfigureLifecycleEvents(lifecycle =>
+        {
+#if iOS || Mac
+            lifecycle.AddiOS(ios =>
+            {
+                bool HandleAppLink(Foundation.NSUserActivity? userActivity)
+                {
+                    if (userActivity is not null && userActivity.ActivityType == Foundation.NSUserActivityType.BrowsingWeb && userActivity.WebPageUrl is not null)
+                    {
+                        var url = $"{userActivity.WebPageUrl.Path}?{userActivity.WebPageUrl.Query}";
+
+                        var _ = Routes.OpenUniversalLink(url);
+
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                ios.FinishedLaunching((app, data)
+                    => HandleAppLink(app.UserActivity));
+
+                ios.ContinueUserActivity((app, userActivity, handler)
+                    => HandleAppLink(userActivity));
+
+                if (OperatingSystem.IsIOSVersionAtLeast(13) || OperatingSystem.IsMacCatalystVersionAtLeast(13))
+                {
+                    ios.SceneWillConnect((scene, sceneSession, sceneConnectionOptions)
+                        => HandleAppLink(sceneConnectionOptions.UserActivities.ToArray()
+                            .FirstOrDefault(a => a.ActivityType == Foundation.NSUserActivityType.BrowsingWeb)));
+
+                    ios.SceneContinueUserActivity((scene, userActivity)
+                        => HandleAppLink(userActivity));
+                }
+            });
+#endif
+        });
 
         var mauiApp = builder.Build();
 
--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Maui/Platforms/Android/MainActivity.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Maui/Platforms/Android/MainActivity.cs
@@ -2,13 +2,14 @@
 using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Boilerplate.Client.Core;
 using Java.Net;
 
 namespace Boilerplate.Client.Maui.Platforms.Android;
 
 [IntentFilter([Intent.ActionView],
                         DataScheme = "https",
-                        DataHosts = ["todo.bitplatform.dev", "adminpanel.bitplatform.dev"],
+                        DataHost = "bp.bitplatform.dev",
                         DataPathPrefix = "/",
                         AutoVerify = true,
                         Categories = [Intent.ActionView, Intent.CategoryDefault, Intent.CategoryBrowsable])]
@@ -22,9 +23,9 @@ public class MainActivity : MauiAppCompatActivity
         base.OnCreate(savedInstanceState);
 
         var url = Intent?.DataString;
-        if (url is not null && App.Current!.MainPage is NavigationPage navPage && navPage.CurrentPage is MainPage mainPage)
+        if (string.IsNullOrWhiteSpace(url) is false)
         {
-            mainPage.BlazorWebView.StartPath = new URL(url).File ?? "/";
+            var _ = Routes.OpenUniversalLink(new URL(url).File ?? "/");
         }
     }
 
@@ -36,8 +37,7 @@ public class MainActivity : MauiAppCompatActivity
         var url = intent.DataString;
         if (action is Intent.ActionView && string.IsNullOrWhiteSpace(url) is false)
         {
-            var pubSubService = IPlatformApplication.Current!.Services.GetRequiredService<IPubSubService>();
-            pubSubService.Publish(PubSubMessages.NAVIGATE, new URL(url).File ?? "/");
+            var _ = Routes.OpenUniversalLink(new URL(url).File ?? "/");
         }
     }
 }
