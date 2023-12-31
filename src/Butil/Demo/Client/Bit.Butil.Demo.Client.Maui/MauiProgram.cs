26cdd691e33f6fd6721bfe73d2e2306cfb985124
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2023-12-31 21:47:59 +0330

    Remove extra resources

--- src/Butil/Demo/Client/Bit.Butil.Demo.Client.Maui/MauiProgram.cs
+++ src/Butil/Demo/Client/Bit.Butil.Demo.Client.Maui/MauiProgram.cs
@@ -6,12 +6,7 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            });
+        builder.UseMauiApp<App>();
 
         builder.Services.AddMauiBlazorWebView();
 
