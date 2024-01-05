14999900df10ea046925d1f9c6f3226c3405b8c2
Yaser Moradi <ysmoradi@outlook.com>
2024-01-05 15:03:11 +0100

    feat(templates): use Butil's storage feature in boilerplate project template #6476 (#6477)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/BrowserStorageService.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/BrowserStorageService.cs
@@ -2,27 +2,35 @@
 
 public partial class BrowserStorageService : IStorageService
 {
-    [AutoInject] private IJSRuntime jsRuntime;
+    [AutoInject] private SessionStorage sessionStorage;
+    [AutoInject] private LocalStorage localStorage;
 
     public async ValueTask<string?> GetItem(string key)
     {
-        return await jsRuntime.InvokeAsync<string?>("window.localStorage.getItem", key) ??
-            await jsRuntime.InvokeAsync<string?>("window.sessionStorage.getItem", key);
+        return await localStorage.GetItem(key) ??
+            await sessionStorage.GetItem(key);
     }
 
     public async ValueTask RemoveItem(string key)
     {
-        await jsRuntime.InvokeAsync<string?>("window.localStorage.removeItem", key);
-        await jsRuntime.InvokeAsync<string?>("window.sessionStorage.removeItem", key);
+        await localStorage.RemoveItem(key);
+        await sessionStorage.RemoveItem(key);
     }
 
     public async ValueTask SetItem(string key, string? value, bool persistent = true)
     {
-        await jsRuntime.InvokeAsync<string?>($"window.{(persistent ? "localStorage" : "sessionStorage")}.setItem", key, value);
+        if (persistent)
+        {
+            await localStorage.SetItem(key, value);
+        }
+        else
+        {
+            await sessionStorage.SetItem(key, value);
+        }
     }
 
     public async ValueTask<bool> IsPersistent(string key)
     {
-        return (await jsRuntime.InvokeAsync<string?>("window.localStorage.getItem", key)) is not null;
+        return (await localStorage.GetItem(key)) is not null;
     }
 }
