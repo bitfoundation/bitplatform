12a025bbd7adc81d10fb22042461e9cb2273f7cd
Yaser Moradi <ysmoradi@outlook.com>
2024-01-09 14:46:40 +0100

    fix

--- src/Besql/Demo/Bit.Besql.Demo.Client/Data/WeatherForecastConfiguration.cs
+++ src/Besql/Demo/Bit.Besql.Demo.Client/Data/WeatherForecastConfiguration.cs
@@ -14,21 +14,21 @@ public class WeatherForecastConfiguration : IEntityTypeConfiguration<WeatherFore
             .HasData([new()
                     {
                         Id = 1,
-                        Date = new DateTimeOffset(2024, 1, 1, 10, 10, 10, TimeSpan.Zero),
+                        Date = new DateTimeOffset(2023, 1, 1, 10, 10, 10, TimeSpan.Zero),
                         TemperatureC = 30,
                         Summary = "Hot"
                     },
                     new()
                     {
                         Id = 2,
-                        Date = new DateTimeOffset(2024, 1, 2, 10, 10, 10, TimeSpan.Zero),
+                        Date = new DateTimeOffset(2023, 1, 2, 10, 10, 10, TimeSpan.Zero),
                         TemperatureC = 20,
                         Summary = "Normal"
                     },
                     new()
                     {
                         Id = 3,
-                        Date = new DateTimeOffset(2024, 1, 3, 10, 10, 10, TimeSpan.Zero),
+                        Date = new DateTimeOffset(2023, 1, 3, 10, 10, 10, TimeSpan.Zero),
                         TemperatureC = 10,
                         Summary = "Cold"
                     }]);
