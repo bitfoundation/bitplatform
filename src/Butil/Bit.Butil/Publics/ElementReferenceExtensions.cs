0fcca5b996c9265b41651a34e939e5e47319a46e
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2024-01-11 21:25:12 +0330

    Update ElementReferenceExtention Scroll

--- src/Butil/Bit.Butil/Publics/ElementReferenceExtensions.cs
+++ src/Butil/Bit.Butil/Publics/ElementReferenceExtensions.cs
@@ -63,8 +63,8 @@ public static class ElementReferenceExtensions
 
     public static async ValueTask Scroll(this ElementReference element, ScrollToOptions? options)
         => await GetJSRuntime(element).ElementScroll(element, options, null, null);
-    public static async ValueTask Scroll(this ElementReference element, ScrollToOptions? options, double? x, double? y)
-        => await GetJSRuntime(element).ElementScroll(element, options, x, y);
+    public static async ValueTask Scroll(this ElementReference element, double? x, double? y)
+        => await GetJSRuntime(element).ElementScroll(element, null, x, y);
 
     public static async ValueTask ScrollBy(this ElementReference element, ScrollToOptions? options)
         => await GetJSRuntime(element).ElementScrollBy(element, options, null, null);
