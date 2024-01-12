c5fc9216947a314546d8004dddc7758c35691a5a
Mohammad Hossein Rastegarinia <mh.rastegari@outlook.com>
2024-01-12 09:20:14 +0330

    fix(butil): resolve issues of RequestFullScreen method of Element class in Butil #6559 (#6560)

--- src/Butil/Bit.Butil/Publics/Element/FullscreenOptions.cs
+++ src/Butil/Bit.Butil/Publics/Element/FullscreenOptions.cs
@@ -3,4 +3,19 @@
 public class FullScreenOptions
 {
     public FullScreenNavigationUI? NavigationUI { get; set; }
+
+    public object ToJsObject()
+    {
+        var navigationUI = NavigationUI switch
+        {
+            FullScreenNavigationUI.Hide => "hide",
+            FullScreenNavigationUI.Show => "show",
+            _ => "auto",
+        };
+
+        return new
+        {
+            NavigationUI = navigationUI,
+        };
+    }
 }
