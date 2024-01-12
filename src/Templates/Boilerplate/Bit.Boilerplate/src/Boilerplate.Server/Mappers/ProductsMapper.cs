98b915a89cc5e63ef611059d5cfd2a81f69a0c2b
Yaser Moradi <ysmoradi@outlook.com>
2024-01-12 23:38:32 +0100

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Boilerplate.Server/Mappers/ProductsMapper.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Boilerplate.Server/Mappers/ProductsMapper.cs
@@ -11,6 +11,13 @@ namespace Boilerplate.Server.Mappers;
 public static partial class ProductsMapper
 {
     public static partial IQueryable<ProductDto> Project(this IQueryable<Product> query);
+
+    [MapProperty(nameof(@Product.Category.Name), nameof(ProductDto.CategoryName))]
+    // In reality, the utilization of MapProperty is unnecessary in this context.
+    // This is because the 'Product' model already possesses a 'Category' property, and the 'Category' property, in turn,
+    // includes a 'Name' property. By concatenating these properties, we naturally obtain 'CategoryName,'
+    // thereby leveraging automatic functionality through mapperly conventions.
+    // Nevertheless, we employ MapProperty in this instance to illustrate its usage
     public static partial ProductDto Map(this Product source);
     public static partial Product Map(this ProductDto source);
     public static partial void Patch(this ProductDto source, Product destination);
