d83cea42bb1f821b1d34b9948ae538753bbcdc53
Yaser Moradi <ysmoradi@outlook.com>
2024-01-20 14:36:21 +0100

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/Clean.bat
+++ src/Templates/Boilerplate/Bit.Boilerplate/Clean.bat
@@ -3,13 +3,13 @@
 :: The commands in this script are specifically designed for Windows.
 
 :: Deletes CSS, JS, and source map files that are not tracked in Git.
-powershell -Command "[string]$trackedFiles = git ls-files; Get-ChildItem -Hidden -Include *.css,*.min.css,*.js,*.min.js,*.map -Recurse | ForEach-Object { if ($trackedFiles -NotMatch $_.Name) { Remove-Item -Recurse -Path $_ -Confirm:$false -Force }}"
+powershell -Command "[string]$trackedFiles = git ls-files; Get-ChildItem -Force -Include *.css,*.min.css,*.js,*.min.js,*.map -Recurse | ForEach-Object { if ($trackedFiles -NotMatch $_.Name) { Remove-Item -Recurse -Path $_ -Confirm:$false -Force }}"
 
 :: Runs the dotnet clean command for each .csproj file.
-powershell -Command "Get-ChildItem -Hidden -Include *.csproj -Recurse | ForEach-Object { dotnet clean $_.FullName }"
+powershell -Command "Get-ChildItem -Force -Include *.csproj -Recurse | ForEach-Object { dotnet clean $_.FullName }"
 
 :: Deletes the specified files and folders.
-powershell -Command "Get-ChildItem -Hidden -Include *.csproj.user,Resources.designer.cs,bin,obj,node_modules,Packages,TestResults,AppPackages,.meteor -Recurse | ForEach-Object { Remove-Item -Recurse -Path $_ -Confirm:$false -Force }"
+powershell -Command "Get-ChildItem -Force -Include *.csproj.user,Resources.designer.cs,bin,obj,node_modules,Packages,TestResults,AppPackages,.meteor -Recurse | ForEach-Object { Remove-Item -Recurse -Path $_ -Confirm:$false -Force }"
 FOR /d /r . %%d IN (.vs) DO @IF EXIST "%%d" rd /s /q "%%d"
 
 :: Deletes empty directories.
