0873f906311c5c12607d9e10bce6194d4b5f49c4
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2024-01-20 17:39:48 +0330

    Improve Butil docs in Platform website #6637

---
+++ src/Websites/Platform/src/Bit.Websites.Platform.Client/Pages/Butil/Butil06KeyboardPage.razor.cs
@@ -0,0 +1,31 @@
+﻿using Bit.Butil;
+
+namespace Bit.Websites.Platform.Client.Pages.Butil;
+
+public partial class Butil06KeyboardPage
+{
+    private BitSearchBox searchBox = default!;
+
+
+    protected override async Task OnInitAsync()
+    {
+        await keyboard.Add(ButilKeyCodes.KeyF, () => _ = searchBox.FocusInput(), ButilModifiers.Ctrl);
+    }
+
+
+    private string addExampleCode =
+@"@inject Bit.Butil.Document document
+
+<div>Press Ctrl+F to focus on search box</div>
+
+<BitSearchBox @ref=""searchBox"" />
+
+@code {
+    private BitSearchBox searchBox = default!;
+
+    protected override async Task OnInitAsync()
+    {
+        await keyboard.Add(ButilKeyCodes.KeyF, () => _ = searchBox.FocusInput(), ButilModifiers.Ctrl);
+    }
+}";
+}
