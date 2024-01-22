edf214ca4d8bb2d3b08d4fc9ea42e1edea389c67
Saleh Yusefnejad <msynk@outlook.com>
2024-01-22 21:21:48 +0330

    feat(prerelease): v-8.7.3-pre-02 #6682 (#6683)

--- src/Bup/Bit.Bup/Scripts/bit-bup.progress.ts
+++ src/Bup/Bit.Bup/Scripts/bit-bup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bup.progress version'] = '8.7.3-pre-01';
+﻿window['bit-bup.progress version'] = '8.7.3-pre-02';
 
 ; (function () {
     (window as any).startBupProgress = (showLogs: boolean, showAssets: boolean, appContainerSelector: string, hideApp: boolean, autoHide: boolean) => {
