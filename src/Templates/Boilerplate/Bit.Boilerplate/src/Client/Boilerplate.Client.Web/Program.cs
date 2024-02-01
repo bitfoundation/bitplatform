92e1c80a72ba46220701115952287acfd1c1b6de
Yaser Moradi <ysmoradi@outlook.com>
2024-02-01 23:30:43 +0100

    Show clear error to developer if trying to run web app through server project while BlazorWebAssemblyStandalone is true.

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Web/Program.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Web/Program.cs
@@ -32,4 +32,13 @@ if (AppRenderMode.MultilingualEnabled)
     host.Services.GetRequiredService<CultureInfoManager>().SetCurrentCulture(culture);
 }
 
-await host.RunAsync();
+try
+{
+    await host.RunAsync();
+}
+catch (JSException exp) when (exp.Message is "Error: Could not find any element matching selector '#app-container'.")
+{
+#if BlazorWebAssemblyStandalone
+    await Console.Error.WriteLineAsync("Either run/publish Client/Client.Web project or set BlazorWebAssemblyStandalone to false.");
+#endif
+}
