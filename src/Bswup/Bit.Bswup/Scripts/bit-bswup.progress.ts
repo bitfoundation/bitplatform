90e0eaad63e42ced30983b90cc529908f6d6cf24
Yaser Moradi <ysmoradi@outlook.com>
2024-02-14 15:41:41 +0100

    prerelease v-8.7.6-pre-03

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bswup.progress version'] = '8.7.6-pre-02';
+﻿window['bit-bswup.progress version'] = '8.7.6-pre-03';
 
 ; (function () {
     (window as any).startBswupProgress = (autoReload: boolean,
--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw.ts
@@ -1,4 +1,4 @@
-﻿self['bit-bswup.sw version'] = '8.7.6-pre-02';
+﻿self['bit-bswup.sw version'] = '8.7.6-pre-03';
 
 interface Window {
     clients: any
