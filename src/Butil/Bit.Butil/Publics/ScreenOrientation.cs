6cb40a6f9e845cbc0b53dca283fadfc6e804494d
Saleh Yusefnejad <msynk@outlook.com>
2024-02-20 14:11:25 +0330

    add events to ScreenOrientation of Butil #6889

--- src/Butil/Bit.Butil/Publics/ScreenOrientation.cs
+++ src/Butil/Bit.Butil/Publics/ScreenOrientation.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.JSInterop;
 
 namespace Bit.Butil;
@@ -8,8 +12,10 @@ namespace Bit.Butil;
 /// <br/>
 /// More info: <see href="https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation">https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation</see>
 /// </summary>
-public class ScreenOrientation(IJSRuntime js)
+public class ScreenOrientation(IJSRuntime js): IAsyncDisposable
 {
+    private readonly ConcurrentDictionary<Guid, Action<OrientationState>> _handlers = new();
+
     /// <summary>
     /// Returns the document's current orientation type.
     /// <br/>
@@ -68,4 +74,71 @@ public class ScreenOrientation(IJSRuntime js)
     /// </summary>
     public async Task Unlock()
         => await js.InvokeVoidAsync("Bit.Butil.screenOrientation.unlock");
+
+    /// <summary>
+    /// The change event of the ScreenOrientation interface fires when the orientation of the 
+    /// screen has changed, for example when a user rotates their mobile phone.
+    /// <br/>
+    /// <see href="https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/change_event">https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/change_event</see>
+    /// </summary>
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(ScreenOrientationListenersManager))]
+    public async ValueTask<Guid> AddChange(Action<OrientationState> handler)
+    {
+        var listenerId = ScreenOrientationListenersManager.AddListener(handler);
+        _handlers.TryAdd(listenerId, handler);
+
+        await js.InvokeVoidAsync("BitButil.screenOrientation.addChange", ScreenOrientationListenersManager.InvokeMethodName, listenerId);
+
+        return listenerId;
+    }
+    /// <summary>
+    /// The change event of the ScreenOrientation interface fires when the orientation of the 
+    /// screen has changed, for example when a user rotates their mobile phone.
+    /// <br/>
+    /// <see href="https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/change_event">https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/change_event</see>
+    /// </summary>
+    public async Task<Guid[]> RemoveChange(Action<OrientationState> handler)
+    {
+        var ids = ScreenOrientationListenersManager.RemoveListener(handler);
+
+        await RemoveChange(ids);
+
+        return ids;
+    }
+    /// <summary>
+    /// The change event of the ScreenOrientation interface fires when the orientation of the 
+    /// screen has changed, for example when a user rotates their mobile phone.
+    /// <br/>
+    /// <see href="https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/change_event">https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/change_event</see>
+    /// </summary>
+    public async ValueTask RemoveChange(Guid id)
+    {
+        ScreenOrientationListenersManager.RemoveListeners([id]);
+
+        await RemoveChange([id]);
+    }
+    private async ValueTask RemoveChange(Guid[] ids)
+    {
+        foreach (var id in ids)
+        {
+            _handlers.TryRemove(id, out _);
+        }
+
+        await js.InvokeVoidAsync("BitButil.screenOrientation.removeChange", ids);
+    }
+    public async ValueTask RemoveAllChanges()
+    {
+        var ids = _handlers.Select(h => h.Key).ToArray();
+
+        _handlers.Clear();
+
+        ScreenOrientationListenersManager.RemoveListeners(ids);
+
+        await js.InvokeVoidAsync("BitButil.screenOrientation.removeChange", ids);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await RemoveAllChanges();
+    }
 }
