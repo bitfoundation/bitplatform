77f5809ad275cc682d8835befb1df7eaaaacb2c1
Saleh Yusefnejad <msynk@outlook.com>
2024-02-21 00:03:26 +0330

    feat(butil): add ScreenOrientation demo page #6891 (#6897)

--- src/Butil/Bit.Butil/Publics/ScreenOrientation.cs
+++ src/Butil/Bit.Butil/Publics/ScreenOrientation.cs
@@ -23,7 +23,7 @@ public class ScreenOrientation(IJSRuntime js) : IAsyncDisposable
     /// </summary>
     public async Task<ScreenOrientationType> GetOrientationType()
     {
-        var type = await js.InvokeAsync<string>("Bit.Butil.screenOrientation.type");
+        var type = await js.InvokeAsync<string>("BitButil.screenOrientation.type");
 
         return type switch
         {
@@ -41,7 +41,7 @@ public class ScreenOrientation(IJSRuntime js) : IAsyncDisposable
     /// <see href="https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/angle">https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/angle</see>
     /// </summary>
     public async Task<ushort> GetAngle()
-        => await js.InvokeAsync<ushort>("Bit.Butil.screenOrientation.angle");
+        => await js.InvokeAsync<ushort>("BitButil.screenOrientation.angle");
 
     /// <summary>
     /// Locks the orientation of the containing document to the specified orientation.
@@ -64,7 +64,7 @@ public class ScreenOrientation(IJSRuntime js) : IAsyncDisposable
             _ => "any"
         };
 
-        await js.InvokeVoidAsync("Bit.Butil.screenOrientation.lock", type);
+        await js.InvokeVoidAsync("BitButil.screenOrientation.lock", type);
     }
 
     /// <summary>
@@ -73,7 +73,7 @@ public class ScreenOrientation(IJSRuntime js) : IAsyncDisposable
     /// <see href="https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/unlock">https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/unlock</see>
     /// </summary>
     public async Task Unlock()
-        => await js.InvokeVoidAsync("Bit.Butil.screenOrientation.unlock");
+        => await js.InvokeVoidAsync("BitButil.screenOrientation.unlock");
 
     /// <summary>
     /// The change event of the ScreenOrientation interface fires when the orientation of the 
