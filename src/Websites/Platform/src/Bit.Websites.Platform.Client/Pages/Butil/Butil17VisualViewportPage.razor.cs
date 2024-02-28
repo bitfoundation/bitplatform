5f273435a976df6a9005d7060c944df6eb460155
Mohammad Hossein Rastegarinia <mh.rastegari@outlook.com>
2024-02-28 14:36:17 +0330

    feat(websites): complete VisualViewport docs on Platform website #6973 (#6996)

--- src/Websites/Platform/src/Bit.Websites.Platform.Client/Pages/Butil/Butil17VisualViewportPage.razor.cs
+++ src/Websites/Platform/src/Bit.Websites.Platform.Client/Pages/Butil/Butil17VisualViewportPage.razor.cs
@@ -4,9 +4,51 @@ public partial class Butil17VisualViewportPage
 {
     private double offsetLeft;
 
+    private double offsetTop;
+    
+    private double pageLeft;
+
+    private double pageTop;
+    
+    private double width;
+    
+    private double height;
+    
+    private double scale;
+
+
     private async Task GetOffsetLeft()
     {
         offsetLeft = await visualViewport.GetOffsetLeft();
+    }
+    private async Task GetOffsetTop()
+    {
+        offsetTop = await visualViewport.GetOffsetTop();
+    }
+
+    private async Task GetPageLeft()
+    {
+        pageLeft = await visualViewport.GetPageLeft();
+    }
+
+    private async Task GetPageTop()
+    {
+        pageTop = await visualViewport.GetPageTop();
+    }
+
+    private async Task GetWidth()
+    {
+        width = await visualViewport.GetWidth();
+    }
+
+    private async Task GetHeight()
+    {
+        height = await visualViewport.GetHeight();
+    }
+
+    private async Task GetScale()
+    {
+        scale = await visualViewport.GetScale();
     }
 
 
@@ -23,6 +65,96 @@ public partial class Butil17VisualViewportPage
     private async Task GetOffsetLeft()
     {
         offsetLeft = await visualViewport.GetOffsetLeft();
+    }
+}";
+    private string getOffsetTopExampleCode =
+@"@inject Bit.Butil.VisualViewport visualViewport
+
+<BitButton OnClick=""@GetOffsetTop"">GetOffsetTop</BitButton>
+
+<div>OffsetTop: @offsetTop</div>
+
+@code {
+    private double offsetTop;
+
+    private async Task GetOffsetTop()
+    {
+        offsetTop = await visualViewport.GetOffsetTop();
+    }
+}";
+    private string getPageLeftExampleCode =
+@"@inject Bit.Butil.VisualViewport visualViewport
+
+<BitButton OnClick=""@GetPageLeft"">GetPageLeft</BitButton>
+
+<div>pageLeft: @pageLeft</div>
+
+@code {
+    private double pageLeft;
+
+    private async Task GetPageLeft()
+    {
+        pageLeft = await visualViewport.GetPageLeft();
+    }
+}";
+    private string getPageTopExampleCode =
+@"@inject Bit.Butil.VisualViewport visualViewport
+
+<BitButton OnClick=""@GetPageTop"">GetPageTop</BitButton>
+
+<div>pageTop: @pageTop</div>
+
+@code {
+    private double pageTop;
+
+    private async Task GetPageTop()
+    {
+        pageTop = await visualViewport.GetPageTop();
+    }
+}";
+    private string getWidthExampleCode =
+@"@inject Bit.Butil.VisualViewport visualViewport
+
+<BitButton OnClick=""@GetWidth"">GetWidth</BitButton>
+
+<div>Width: @width</div>
+
+@code {
+    private double width;
+
+    private async Task GetWidth()
+    {
+        width = await visualViewport.GetWidth();
+    }
+}";
+    private string getHeightExampleCode =
+@"@inject Bit.Butil.VisualViewport visualViewport
+
+<BitButton OnClick=""@GetHeight"">GetHeight</BitButton>
+
+<div>Height: @height</div>
+
+@code {
+    private double height;
+
+    private async Task GetHeight()
+    {
+        height = await visualViewport.GetHeight();
+    }
+}";
+    private string getScaleExampleCode =
+@"@inject Bit.Butil.VisualViewport visualViewport
+
+<BitButton OnClick=""@GetScale"">GetScale</BitButton>
+
+<div>Height: @height</div>
+
+@code {
+    private double height;
+
+    private async Task GetScale()
+    {
+        height = await visualViewport.GetScale();
     }
 }";
 }
