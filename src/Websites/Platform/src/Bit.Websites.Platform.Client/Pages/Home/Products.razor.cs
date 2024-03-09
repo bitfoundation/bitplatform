8b901fe9c2c475debf384fa384a0470dcad6d332
Saleh Yusefnejad <msynk@outlook.com>
2024-03-09 15:49:55 +0330

    Improve product section of home page of Platform website #7073

---
+++ src/Websites/Platform/src/Bit.Websites.Platform.Client/Pages/Home/Products.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Bit.Websites.Platform.Client.Pages.Home;
+
+public partial class Products
+{
+    private bool _boilerplateExpanded = true;
+    private bool _butilExpanded = false;
+    private bool _bswupExpanded = false;
+    private bool _besqlExpanded = false;
+    private bool _blazoruiExpanded = false;
+}
---
+++ src/Websites/Platform/src/Bit.Websites.Platform.Client/Pages/Home/Products1.razor.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Bit.Websites.Platform.Client.Pages.Home;
+
+public partial class Products1
+{
+    private class BitProduct
+    {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string Url { get; set; } = default!;
+    }
+
+    private List<BitProduct> _allProducts =
+    [
+        new() { Name = "Boilerplate", Description = "Feature-rich .NET project template", Url = Urls.Templates },
+        new() { Name = "Butil", Description = "Blazor Utilities for JavaScript", Url = Urls.Butil },
+        new() { Name = "Bswup", Description = "Blazor Service Worker Update Progress", Url = Urls.Bswup },
+        new() { Name = "Besql", Description = "Blazor Entity Framework Sqlite", Url = Urls.Besql },
+        new() { Name = "BlazorUI", Description = "Native Blazor UI components", Url = Urls.BlazorUI },
+    ];
+
+    private BitProduct _selectedProduct = default!;
+
+    protected override Task OnInitAsync()
+    {
+        _selectedProduct = _allProducts[0];
+        return base.OnInitAsync();
+    }
+
+
+}
