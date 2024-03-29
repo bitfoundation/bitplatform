3b9180cfd8415340b458e6bd7a22792d4da1badd
Saleh Yusefnejad <msynk@outlook.com>
2024-03-29 14:04:36 +0330

    feat(blazorui): add cascading dir parameter #7268 (#7269)

--- src/BlazorUI/Bit.BlazorUI/Components/BitComponentBase.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/BitComponentBase.cs
@@ -2,19 +2,19 @@
 
 public abstract partial class BitComponentBase : ComponentBase
 {
+    private BitDir? dir;
     private string? style;
     private string? @class;
     private bool isEnabled = true;
-    private BitDir? dir;
     private BitVisibility visibility;
-
-
     private Guid _uniqueId = Guid.NewGuid();
+
 
 
     protected bool Rendered { get; private set; }
 
     protected string _Id => Id ?? _uniqueId.ToString();
+
 
 
     /// <summary>
@@ -26,6 +26,11 @@ public abstract partial class BitComponentBase : ComponentBase
     /// The ElementReference of the root element.
     /// </summary>
     public ElementReference RootElement { get; internal set; }
+
+
+
+    [CascadingParameter] protected BitDir? CascadingDir { get; set; }
+
 
 
     /// <summary>
@@ -55,7 +60,7 @@ public abstract partial class BitComponentBase : ComponentBase
     [Parameter]
     public BitDir? Dir
     {
-        get => dir;
+        get => dir ?? CascadingDir;
         set
         {
             if (dir == value) return;
@@ -133,6 +138,11 @@ public abstract partial class BitComponentBase : ComponentBase
         {
             switch (parameter.Key)
             {
+                case nameof(CascadingDir):
+                    CascadingDir = (BitDir?)parameter.Value;
+                    parametersDictionary.Remove(parameter.Key);
+                    break;
+
                 case nameof(AriaLabel):
                     AriaLabel = (string?)parameter.Value;
                     parametersDictionary.Remove(parameter.Key);
@@ -144,7 +154,7 @@ public abstract partial class BitComponentBase : ComponentBase
                     break;
 
                 case nameof(Dir):
-                    Dir = (BitDir)parameter.Value;
+                    Dir = (BitDir?)parameter.Value;
                     parametersDictionary.Remove(parameter.Key);
                     break;
 
