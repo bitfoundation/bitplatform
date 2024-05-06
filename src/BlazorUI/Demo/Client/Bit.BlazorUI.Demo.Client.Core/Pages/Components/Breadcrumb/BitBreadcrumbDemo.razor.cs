fb86a27179bb909c6a74430118b4a63df22bb117
Mohammad Hossein Rastegarinia <mh.rastegari@outlook.com>
2024-05-06 16:02:58 +0330

    feat(blazorui): add Key and OnClick parameters to BitBreadCrumb items #7459 (#7460)

--- src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Breadcrumb/BitBreadcrumbDemo.razor.cs
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Breadcrumb/BitBreadcrumbDemo.razor.cs
@@ -169,6 +169,12 @@ public partial class BitBreadcrumbDemo
             {
                new()
                {
+                   Name = "Key",
+                   Type = "string?",
+                   Description = "A unique value to use as a key of the breadcrumb item.",
+               },
+               new()
+               {
                    Name = "Text",
                    Type = "string?",
                    Description = "Text to display in the breadcrumb item.",
@@ -177,7 +183,7 @@ public partial class BitBreadcrumbDemo
                {
                    Name = "Href",
                    Type = "string?",
-                   Description = "URL to navigate to when this breadcrumb item is clicked. If provided, the breadcrumb will be rendered as a link.",
+                   Description = "URL to navigate to when the breadcrumb item is clicked. If provided, the breadcrumb will be rendered as a link.",
                },
                new()
                {
@@ -204,6 +210,12 @@ public partial class BitBreadcrumbDemo
                    DefaultValue = "true",
                    Description = "Whether an item is enabled or not.",
                },
+               new()
+               {
+                   Name = "OnClick",
+                   Type = "Action<BitBreadcrumbItem>?",
+                   Description = "Click event handler of the breadcrumb item.",
+               }
             }
         },
         new()
@@ -214,41 +226,53 @@ public partial class BitBreadcrumbDemo
             {
                new()
                {
+                   Name = "Key",
+                   Type = "string?",
+                   Description = "A unique value to use as a key of the breadcrumb option.",
+               },
+               new()
+               {
                    Name = "Text",
                    Type = "string?",
-                   Description = "Text to display in the breadcrumb item.",
+                   Description = "Text to display in the breadcrumb option.",
                },
                new()
                {
                    Name = "Href",
                    Type = "string?",
-                   Description = "URL to navigate to when this breadcrumb item is clicked. If provided, the breadcrumb will be rendered as a link.",
+                   Description = "URL to navigate to when the breadcrumb option is clicked. If provided, the breadcrumb will be rendered as a link.",
                },
                new()
                {
                    Name = "Class",
                    Type = "string?",
-                   Description = "CSS class attribute for breadcrumb item.",
+                   Description = "CSS class attribute for breadcrumb option.",
                },
                new()
                {
                    Name = "Style",
                    Type = "string?",
-                   Description = "Style attribute for breadcrumb item.",
+                   Description = "Style attribute for breadcrumb option.",
                },
                new()
                {
                    Name = "IsSelected",
                    Type = "bool",
-                   Description = "Display the item as the selected item.",
+                   Description = "Display the breadcrumb option as the selected option.",
                },
                new()
                {
                    Name = "IsEnabled",
                    Type = "bool",
                    DefaultValue = "true",
-                   Description = "Whether an item is enabled or not.",
+                   Description = "Whether an option is enabled or not.",
                },
+               new()
+               {
+                   Name = "OnClick",
+                   Type = "EventCallback<BitBreadcrumbOption>",
+                   Description = "Click event handler of the breadcrumb option.",
+               }
             }
         },
     };
