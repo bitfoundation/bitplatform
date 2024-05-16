7be5d84465d091635fe5a6a6de75d3ea3d64e3a7
ysmoradi <ysmoradi@outlook.com>
2024-05-16 18:38:45 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/NoopLocalHttpServer.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/NoopLocalHttpServer.cs
@@ -3,7 +3,7 @@ namespace Boilerplate.Client.Core.Services;
 
 public class NoopLocalHttpServer : ILocalHttpServer
 {
-    public int Port { get; } => -1;
+    public int Port => -1;
 
     public Task Start() => Task.CompletedTask;
 }
