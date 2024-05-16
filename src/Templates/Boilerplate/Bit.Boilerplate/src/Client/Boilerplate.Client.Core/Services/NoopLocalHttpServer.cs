6711a48cd0017206f16df935a8b0b84ed8875c0f
ysmoradi <ysmoradi@outlook.com>
2024-05-16 19:05:02 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/NoopLocalHttpServer.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/NoopLocalHttpServer.cs
@@ -1,6 +1,9 @@
-﻿
-namespace Boilerplate.Client.Core.Services;
+﻿namespace Boilerplate.Client.Core.Services;
 
+/// <summary>
+/// Currently only Windows clients have local http server to have better support for social login.
+/// In other platforms, we don't have local http server and we use a noop implementation.
+/// </summary>
 public class NoopLocalHttpServer : ILocalHttpServer
 {
     public int Port => -1;
