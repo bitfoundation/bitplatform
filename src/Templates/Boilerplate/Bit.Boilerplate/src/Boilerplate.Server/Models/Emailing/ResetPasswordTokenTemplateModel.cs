03de51284a1c5ace8cd6023abe504249572bc742
Saleh Yusefnejad <msynk@outlook.com>
2024-05-21 17:14:21 +0330

    feat(templates): update UI of ResetPassword process in Boilerplate #7550 (#7560)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Boilerplate.Server/Models/Emailing/ResetPasswordTokenTemplateModel.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Boilerplate.Server/Models/Emailing/ResetPasswordTokenTemplateModel.cs
@@ -3,6 +3,6 @@
 public class ResetPasswordTokenTemplateModel
 {
     public string? DisplayName { get; set; }
-
     public required string Token { get; set; }
+    public required Uri Link { get; set; }
 }
