0319d72ac399480393bc4d88fbdbe65191b537b4
ysmoradi <ysmoradi@outlook.com>
2024-05-21 19:25:14 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Boilerplate.Server/Extensions/SignInManagerExtensions.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Boilerplate.Server/Extensions/SignInManagerExtensions.cs
@@ -6,30 +6,20 @@ public static class SignInManagerExtensions
 {
     public static async Task<SignInResult> OtpSignInAsync(this SignInManager<User> signInManager, User user, string otp)
     {
-        SignInResult result;
         var userManager = signInManager.UserManager;
 
-        if (await userManager.IsLockedOutAsync(user))
-        {
-            result = SignInResult.LockedOut;
-        }
-        else
-        {
-            bool tokenIsValid = await userManager.VerifyUserTokenAsync(user!, TokenOptions.DefaultPhoneProvider, $"Otp,Date:{user.OtpRequestedOn}", otp!);
+        if (await userManager.IsLockedOutAsync(user)) return SignInResult.LockedOut;
 
-            if (tokenIsValid is false)
-            {
-                result = SignInResult.Failed;
-                await userManager.AccessFailedAsync(user);
-            }
-            else
-            {
-                await userManager.UpdateSecurityStampAsync(user);
-                await signInManager.SignInWithClaimsAsync(user!, isPersistent: false, [new Claim("amr", "otp")]);
-                result = user.TwoFactorEnabled ? SignInResult.TwoFactorRequired : SignInResult.Success;
-            }
+        bool tokenIsValid = await userManager.VerifyUserTokenAsync(user!, TokenOptions.DefaultPhoneProvider, $"Otp,Date:{user.OtpRequestedOn}", otp!);
+
+        if (tokenIsValid is false)
+        {
+            await userManager.AccessFailedAsync(user);
+            return SignInResult.Failed;
         }
 
-        return result;
+        await userManager.UpdateSecurityStampAsync(user);
+        await signInManager.SignInWithClaimsAsync(user!, isPersistent: false, [new Claim("amr", "otp")]);
+        return user.TwoFactorEnabled ? SignInResult.TwoFactorRequired : SignInResult.Success;
     }
 }
