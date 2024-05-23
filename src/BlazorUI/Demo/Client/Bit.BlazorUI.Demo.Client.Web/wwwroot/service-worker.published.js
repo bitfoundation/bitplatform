abb364620ef4db22b023af3221094decdcb43e08
Saleh Yusefnejad <msynk@outlook.com>
2024-05-23 16:22:25 +0330

    feat(prerelease): v-8.8.2-pre-03 #7491 (#7584)

--- src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Web/wwwroot/service-worker.published.js
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Web/wwwroot/service-worker.published.js
@@ -1,4 +1,4 @@
-// bit version: 8.8.2-pre-02
+// bit version: 8.8.2-pre-03
 // https://github.com/bitfoundation/bitplatform/tree/develop/src/Bswup
 
 self.assetsInclude = [];
--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bswup.progress version'] = '8.8.2-pre-02';
+﻿window['bit-bswup.progress version'] = '8.8.2-pre-03';
 
 ; (function () {
     (window as any).startBswupProgress = (autoReload: boolean,
