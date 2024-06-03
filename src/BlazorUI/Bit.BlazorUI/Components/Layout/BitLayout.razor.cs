eba37c97accb912aa919d168be09d512aa6846ea
Saleh Yusefnejad <msynk@outlook.com>
2024-06-03 12:40:20 +0330

    fist commit

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Layout/BitLayout.razor.cs
@@ -0,0 +1,44 @@
+﻿namespace Bit.BlazorUI;
+
+public partial class BitLayout
+{
+    private bool isRequired;
+
+    /// <summary>
+    /// The content of label, It can be Any custom tag or a text
+    /// </summary>
+    [Parameter] public RenderFragment? Header { get; set; }
+
+    /// <summary>
+    /// The content of label, It can be Any custom tag or a text
+    /// </summary>
+    [Parameter] public RenderFragment? Main { get; set; }
+
+    /// <summary>
+    /// The content of label, It can be Any custom tag or a text
+    /// </summary>
+    [Parameter] public RenderFragment? Footer { get; set; }
+
+
+    /// <summary>
+    /// Whether the associated field is required or not, it shows a star above of it
+    /// </summary>
+    [Parameter]
+    public bool IsRequired
+    {
+        get => isRequired;
+        set
+        {
+            isRequired = value;
+            ClassBuilder.Reset();
+        }
+    }
+
+
+    protected override string RootElementClass => "bit-lbl";
+
+    protected override void RegisterCssClasses()
+    {
+        ClassBuilder.Register(() => IsRequired ? $"{RootElementClass}-req" : string.Empty);
+    }
+}
