fb7a2341c5249c080e8d42caad50193b487308bd
ysmoradi <ysmoradi@outlook.com>
2024-06-04 11:56:32 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/HttpMessageHandlers/AuthDelegatingHandler.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/HttpMessageHandlers/AuthDelegatingHandler.cs
@@ -20,32 +20,29 @@ public class AuthDelegatingHandler(IAuthTokenProvider tokenProvider, IServicePro
         {
             return await base.SendAsync(request, cancellationToken);
         }
-        catch (Exception _) when ((_ is ForbiddenException or UnauthorizedException) &&
-                                  tokenProvider.IsInitialized &&
-                                  request.RequestUri?.LocalPath?.Contains("api/Identity/Refresh", StringComparison.InvariantCultureIgnoreCase) is false)
+        catch (KnownException _) when (_ is ForbiddenException or UnauthorizedException)
         {
             // Let's update the access token by refreshing it when a refresh token is available.
             // Following this procedure, the newly acquired access token may now include the necessary roles or claims.
 
+            if (tokenProvider.IsInitialized is false ||
+               request.RequestUri?.LocalPath?.Contains("api/Identity/Refresh", StringComparison.InvariantCultureIgnoreCase) is true /* To prevent refresh token loop */) throw;
+
             var authManager = serviceProvider.GetRequiredService<AuthenticationManager>();
             var refresh_token = await storageService.GetItem("refresh_token");
 
-            if (refresh_token is not null)
-            {
-                // In the AuthenticationStateProvider, the access_token is refreshed using the refresh_token (if available).
-                await authManager.RefreshToken();
+            if (refresh_token is null) throw;
 
-                var access_token = await tokenProvider.GetAccessTokenAsync();
+            // In the AuthenticationStateProvider, the access_token is refreshed using the refresh_token (if available).
+            await authManager.RefreshToken();
 
-                if (string.IsNullOrEmpty(access_token) is false)
-                {
-                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", access_token);
+            var access_token = await tokenProvider.GetAccessTokenAsync();
 
-                    return await base.SendAsync(request, cancellationToken);
-                }
-            }
+            if (string.IsNullOrEmpty(access_token)) throw;
 
-            throw;
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", access_token);
+
+            return await base.SendAsync(request, cancellationToken);
         }
     }
 }
