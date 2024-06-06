8ea2891b8a9b0a356c04e8ce013f8d77271aacce
Yaser Moradi <ysmoradi@outlook.com>
2024-06-06 16:59:41 +0200

    fix(templates): resolve Boilerplate without multilingual feature issue in Blazor WASM #7718 (#7724)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Boilerplate.Shared/Services/CultureInfoManager.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Boilerplate.Shared/Services/CultureInfoManager.cs
@@ -4,9 +4,9 @@ namespace Boilerplate.Shared.Services;
 
 public class CultureInfoManager
 {
-    public static CultureInfo DefaultCulture { get; } = CreateCultureInfo("en-US");
+    public static CultureInfo DefaultCulture => CreateCultureInfo("en-US");
 
-    public static (string DisplayName, CultureInfo Culture)[] SupportedCultures { get; } =
+    public static (string DisplayName, CultureInfo Culture)[] SupportedCultures =>
     [
         ("English US", CreateCultureInfo("en-US")),
         ("English UK", CreateCultureInfo("en-GB")),
@@ -37,7 +37,7 @@ public class CultureInfoManager
     /// <summary>
     /// This is an example to demonstrate the way you can customize application culture
     /// </summary>
-    public static CultureInfo CustomizeCultureInfoForFaCulture(CultureInfo cultureInfo)
+    private static CultureInfo CustomizeCultureInfoForFaCulture(CultureInfo cultureInfo)
     {
         cultureInfo.DateTimeFormat.AMDesignator = "ق.ظ";
         cultureInfo.DateTimeFormat.PMDesignator = "ب.ظ";
