ee7d822347fd90abd35fffd4861219166fe7343e
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2024-06-08 00:45:34 +0330

    feat(blazorui): add Classes/Styles to BitCircularTimePicker #7725 (#7726)

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Inputs/TimePickers/CircularTimePicker/BitCircularTimePickerClassStyles.cs
@@ -0,0 +1,149 @@
+﻿namespace Bit.BlazorUI;
+
+public class BitCircularTimePickerClassStyles
+{
+    /// <summary>
+    /// Custom CSS classes/styles for the root element of the BitCircularTimePicker.
+    /// </summary>
+    public string? Root { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the label of the BitCircularTimePicker.
+    /// </summary>
+    public string? Label { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the input wrapper of the BitCircularTimePicker.
+    /// </summary>
+    public string? InputWrapper { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the input container of the BitCircularTimePicker.
+    /// </summary>
+    public string? InputContainer { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the input of the BitCircularTimePicker.
+    /// </summary>
+    public string? Input { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the icon of the BitCircularTimePicker.
+    /// </summary>
+    public string? Icon { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the overlay of the BitCircularTimePicker.
+    /// </summary>
+    public string? Overlay { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the callout of the BitCircularTimePicker.
+    /// </summary>
+    public string? Callout { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the callout container of the BitCircularTimePicker.
+    /// </summary>
+    public string? CalloutContainer { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the toolbar of the BitCircularTimePicker.
+    /// </summary>
+    public string? Toolbar { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the hour and minute container of the BitCircularTimePicker.
+    /// </summary>
+    public string? HourMinuteContainer { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the hour button of the BitCircularTimePicker.
+    /// </summary>
+    public string? HourButton { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the minute button of the BitCircularTimePicker.
+    /// </summary>
+    public string? MinuteButton { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the hour minute separator of the BitCircularTimePicker.
+    /// </summary>
+    public string? HourMinuteSeparator { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the hour/minute text of the BitCircularTimePicker.
+    /// </summary>
+    public string? HourMinuteText { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the AM/PM container of the BitCircularTimePicker.
+    /// </summary>
+    public string? AmPmContainer { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the AM button of the BitCircularTimePicker.
+    /// </summary>
+    public string? AmButton { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the PM button of the BitCircularTimePicker.
+    /// </summary>
+    public string? PmButton { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the selected buttons of the BitCircularTimePicker.
+    /// </summary>
+    public string? SelectedButtons { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the clock container of the BitCircularTimePicker.
+    /// </summary>
+    public string? ClockContainer { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the clock face of the BitCircularTimePicker.
+    /// </summary>
+    public string? ClockFace { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the clock pin of the BitCircularTimePicker.
+    /// </summary>
+    public string? ClockPin { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the clock number of the BitCircularTimePicker.
+    /// </summary>
+    public string? ClockNumber { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the clock selected number of the BitCircularTimePicker.
+    /// </summary>
+    public string? ClockSelectedNumber { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the clock pointer of the BitCircularTimePicker.
+    /// </summary>
+    public string? ClockPointer { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the clock pointer thumb of the BitCircularTimePicker.
+    /// </summary>
+    public string? ClockPointerThumb { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the clock pointer thumb minute of the BitCircularTimePicker.
+    /// </summary>
+    public string? ClockPointerThumbMinute { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the close button of the BitCircularTimePicker.
+    /// </summary>
+    public string? CloseButton { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the close button icon of the BitCircularTimePicker.
+    /// </summary>
+    public string? CloseButtonIcon { get; set; }
+}
