7710c46d21d95f704d7efbebac8539ce85871b2f
Yaser Moradi <ysmoradi@outlook.com>
2024-06-13 13:25:08 +0200

    feat(dependencies): Use Msbuild incremental build to run BeforeBuildTasks target #7774 (#7778)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Boilerplate.Shared/Extensions/TupleExtensions.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Boilerplate.Shared/Extensions/TupleExtensions.cs
@@ -41,7 +41,7 @@ public static class TupleExtensions
         async Task<(T1, T2)> UnifyTasks()
         {
             var (task1, task2) = tuple;
-            await Task.WhenAll(task1, task2);
+            await Task.WhenAll([task1, task2]);
             return (task1.Result, task2.Result);
         }
 
@@ -53,7 +53,7 @@ public static class TupleExtensions
         async Task<(T1, T2, T3)> UnifyTasks()
         {
             var (task1, task2, task3) = tuple;
-            await Task.WhenAll(task1, task2, task3);
+            await Task.WhenAll([task1, task2, task3]);
             return (task1.Result, task2.Result, task3.Result);
         }
 
@@ -65,7 +65,7 @@ public static class TupleExtensions
         async Task<(T1, T2, T3, T4)> UnifyTasks()
         {
             var (task1, task2, task3, task4) = tuple;
-            await Task.WhenAll(task1, task2, task3, task4);
+            await Task.WhenAll([task1, task2, task3, task4]);
             return (task1.Result, task2.Result, task3.Result, task4.Result);
         }
 
@@ -77,7 +77,7 @@ public static class TupleExtensions
         async Task<(T1, T2, T3, T4, T5)> UnifyTasks()
         {
             var (task1, task2, task3, task4, task5) = tuple;
-            await Task.WhenAll(task1, task2, task3, task4, task5);
+            await Task.WhenAll([task1, task2, task3, task4, task5]);
             return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result);
         }
 
@@ -89,7 +89,7 @@ public static class TupleExtensions
         async Task<(T1, T2, T3, T4, T5, T6)> UnifyTasks()
         {
             var (task1, task2, task3, task4, task5, task6) = tuple;
-            await Task.WhenAll(task1, task2, task3, task4, task5, task6);
+            await Task.WhenAll([task1, task2, task3, task4, task5, task6]);
             return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result);
         }
 
@@ -101,7 +101,7 @@ public static class TupleExtensions
         async Task<(T1, T2, T3, T4, T5, T6, T7)> UnifyTasks()
         {
             var (task1, task2, task3, task4, task5, task6, task7) = tuple;
-            await Task.WhenAll(task1, task2, task3, task4, task5, task6, task7);
+            await Task.WhenAll([task1, task2, task3, task4, task5, task6, task7]);
             return (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result, task7.Result);
         }
 
