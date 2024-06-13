efcdf59ea64aacf7260db9c9985a029d1b946a49
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2024-06-13 13:03:49 +0330

    Add Copy to clipboard button to code boxes in the docs of the Platform website #6238

---
+++ src/Websites/Platform/src/Bit.Websites.Platform.Client/Pages/CodeBox.razor.cs
@@ -0,0 +1,37 @@
+﻿using Bit.Butil;
+
+namespace Bit.Websites.Platform.Client.Pages;
+
+public partial class CodeBox
+{
+    private bool isCodeCopied = false;
+    private string codeIcon = BitIconName.Copy;
+    private string copyCodeMessage = "Copy code";
+
+
+    [AutoInject] private Clipboard clipboard = default!;
+
+
+    [Parameter] public string? Code { get; set; }
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+
+
+    private async Task CopyCodeToClipboard()
+    {
+        await clipboard.WriteText(Code!);
+
+        codeIcon = BitIconName.CheckMark;
+        copyCodeMessage = "Code copied!";
+        isCodeCopied = true;
+
+        StateHasChanged();
+
+        await Task.Delay(1000);
+        isCodeCopied = false;
+        codeIcon = BitIconName.Copy;
+        copyCodeMessage = "Copy code";
+
+        StateHasChanged();
+    }
+}
