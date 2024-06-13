542f9ec07b1c22159f09fed77b2d1cd602a4bc0a
Saleh Yusefnejad <msynk@outlook.com>
2024-06-13 18:40:10 +0330

    add BitLayout component (#7505)

--- src/BlazorUI/Bit.BlazorUI/Components/Layout/BitLayout.razor.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Layout/BitLayout.razor.cs
@@ -3,31 +3,65 @@
 public partial class BitLayout
 {
     /// <summary>
-    /// The content of label, It can be Any custom tag or a text
+    /// Custom CSS classes for different parts of the BitLayout.
+    /// </summary>
+    [Parameter] public BitLayoutClassStyles? Classes { get; set; }
+
+    /// <summary>
+    /// Enables fixed positioning of the header at the top of the viewport.
+    /// </summary>
+    [Parameter] public bool FixedHeader { get; set; }
+
+    /// <summary>
+    /// Enables fixed positioning of the footer at the bottom of the viewport.
+    /// </summary>
+    [Parameter] public bool FixedFooter { get; set; }
+
+    /// <summary>
+    /// The content of the header section.
     /// </summary>
     [Parameter] public RenderFragment? Header { get; set; }
 
+    /// <summary>
+    /// The height of the header to calculate heights and paddings.
+    /// </summary>
     [Parameter] public int HeaderHeight { get; set; }
 
     /// <summary>
-    /// The content of label, It can be Any custom tag or a text
+    /// The content of the main section.
     /// </summary>
     [Parameter] public RenderFragment? Main { get; set; }
 
     /// <summary>
-    /// The content of label, It can be Any custom tag or a text
+    /// The content of the footer section.
     /// </summary>
     [Parameter] public RenderFragment? Footer { get; set; }
-    
+
+    /// <summary>
+    /// The height of the footer to calculate heights and paddings.
+    /// </summary>
     [Parameter] public int FooterHeight { get; set; }
 
+    /// <summary>
+    /// The height of the status bar on mobile devices to calculate heights and paddings.
+    /// </summary>
     [Parameter] public int StatusBarHeight { get; set; }
+
+    /// <summary>
+    /// Custom CSS styles for different parts of the BitLayout.
+    /// </summary>
+    [Parameter] public BitLayoutClassStyles? Styles { get; set; }
 
 
     protected override string RootElementClass => "bit-lyt";
 
     protected override void RegisterCssClasses()
     {
+        ClassBuilder.Register(() => Classes?.Root);
+    }
 
+    protected override void RegisterCssStyles()
+    {
+        StyleBuilder.Register(() => Styles?.Root);
     }
 }
