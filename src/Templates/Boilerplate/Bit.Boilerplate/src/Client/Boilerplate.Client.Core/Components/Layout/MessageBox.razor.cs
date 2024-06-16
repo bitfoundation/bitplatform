0aef0f8cd57f5206de844698e7f35736014af72a
Saleh Yusefnejad <msynk@outlook.com>
2024-06-16 18:59:00 +0330

    improve MessageBoxService to correctly show all messages in Boilerplate #7515

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/MessageBox.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/MessageBox.razor.cs
@@ -8,16 +8,19 @@ public partial class MessageBox
     private Action? unsubscribe;
     private bool disposed = false;
 
-    private TaskCompletionSource<object?>? tcs;
+    private TaskCompletionSource<bool>? tcs;
 
     protected override Task OnInitAsync()
     {
         unsubscribe = PubSubService.Subscribe(PubSubMessages.SHOW_MESSAGE, async args =>
         {
-            (var message, string title, TaskCompletionSource<object?> tcs) = ((string message, string title, TaskCompletionSource<object?> tcs))args!;
-            await (this.tcs?.Task ?? Task.CompletedTask);
-            this.tcs = tcs;
-            await ShowMessageBox(message, title);
+            var data = (MessageBoxData)args!;
+
+            await (tcs?.Task ?? Task.CompletedTask);
+
+            tcs = data.TaskCompletionSource;
+
+            await ShowMessageBox(data.Message, data.Title);
         });
 
         return base.OnInitAsync();
@@ -38,14 +41,14 @@ public partial class MessageBox
     private async Task OnCloseClick()
     {
         isOpen = false;
-        tcs?.SetResult(null);
+        tcs?.SetResult(false);
         tcs = null;
     }
 
     private async Task OnOkClick()
     {
         isOpen = false;
-        tcs?.SetResult(null);
+        tcs?.SetResult(true);
         tcs = null;
     }
 
@@ -55,8 +58,9 @@ public partial class MessageBox
 
         if (disposed || disposing is false) return;
 
-        tcs?.TrySetResult(null);
+        tcs?.TrySetResult(false);
         tcs = null;
+
         unsubscribe?.Invoke();
 
         disposed = true;
