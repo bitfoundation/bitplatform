5ae0a293b90cb1d7fe912cd829760aa8bc481cea
Saleh Yusefnejad <msynk@outlook.com>
2024-06-28 18:41:05 +0330

    rename target to length

--- src/BlazorUI/Bit.BlazorUI/Utils/BitShortId.cs
+++ src/BlazorUI/Bit.BlazorUI/Utils/BitShortId.cs
@@ -3,7 +3,7 @@
 internal class BitShortId
 {
     private static readonly string _chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-    private static readonly int target = _chars.Length;
+    private static readonly int length = _chars.Length;
 
     public static string NewId() => ToString(Math.Abs(BitConverter.ToInt32(Guid.NewGuid().ToByteArray())));
 
@@ -13,8 +13,8 @@ internal class BitShortId
 
         do
         {
-            result = _chars[value % target] + result;
-            value /= target;
+            result = _chars[value % length] + result;
+            value /= length;
         } while (value > 0);
 
         return result;
