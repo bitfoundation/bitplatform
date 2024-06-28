3419a25dfc00555776b8933e4821dc6e49e76063
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2024-06-28 13:36:24 +0330

    feat(blazorui): remove bit-icon-- css class when BitIcon has no IconName #7897 (#7898)

--- src/BlazorUI/Bit.BlazorUI/Components/Utilities/Icon/BitIcon.razor.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Utilities/Icon/BitIcon.razor.cs
@@ -2,10 +2,29 @@
 
 public partial class BitIcon
 {
+    private string? iconName;
+
     /// <summary>
     /// The icon name for the icon shown in the button
     /// </summary>
-    [Parameter] public string? IconName { get; set; }
+    [Parameter] 
+    public string? IconName
+    {
+        get => iconName; 
+        set
+        {
+            if (iconName == value) return;
+
+            iconName = value;
+
+            ClassBuilder.Reset();
+        }
+    }
 
     protected override string RootElementClass => "bit-ico";
+
+    protected override void RegisterCssClasses()
+    {
+        ClassBuilder.Register(() => IconName.HasValue() ? $"bit-icon bit-icon--{IconName}" : string.Empty);
+    }
 }
