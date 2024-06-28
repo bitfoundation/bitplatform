4a4fd226a9e90c266c13626562caff195d07cbff
Saleh Yusefnejad <msynk@outlook.com>
2024-06-28 20:03:26 +0330

    feat(blazorui): use a short unique string for id in BitComponentBase #7894 (#7901)

---
+++ src/BlazorUI/Bit.BlazorUI/Utils/BitShortId.cs
@@ -0,0 +1,22 @@
+﻿namespace Bit.BlazorUI;
+
+internal class BitShortId
+{
+    private static readonly string _chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    private static readonly int _length = _chars.Length;
+
+    public static string NewId() => ToString(Math.Abs(BitConverter.ToInt32(Guid.NewGuid().ToByteArray())));
+
+    public static string ToString(int value)
+    {
+        var result = string.Empty;
+
+        do
+        {
+            result = _chars[value % _length] + result;
+            value /= _length;
+        } while (value > 0);
+
+        return result;
+    }
+}
