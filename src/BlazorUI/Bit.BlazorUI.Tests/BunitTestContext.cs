b06d1b090e9a02d2f9296a55b4ca45ac242dae25
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2024-07-04 13:44:14 +0330

    Fix issue

--- src/BlazorUI/Bit.BlazorUI.Tests/BunitTestContext.cs
+++ src/BlazorUI/Bit.BlazorUI.Tests/BunitTestContext.cs
@@ -10,6 +10,8 @@ public abstract class BunitTestContext : IDisposable
 {
     protected Bunit.TestContext Context;
 
+    protected readonly string IdRegexPattern = @"[a-z0-9]{5,6}";
+
     public ITestRenderer Renderer => Context?.Renderer ?? throw new InvalidOperationException("MSTest has not started executing tests yet");
 
     public TestServiceProvider Services => Context?.Services ?? throw new InvalidOperationException("MSTest has not started executing tests yet");
