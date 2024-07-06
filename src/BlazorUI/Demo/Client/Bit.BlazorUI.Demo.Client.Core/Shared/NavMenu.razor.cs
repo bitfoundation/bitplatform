b1e88b3555276d3982bc9c47346d0f3fd052042b
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2024-07-06 16:34:33 +0330

    Add BitSplitter Component #7962

--- src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Shared/NavMenu.razor.cs
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Shared/NavMenu.razor.cs
@@ -125,6 +125,7 @@ public partial class NavMenu : IDisposable
                 new() { Text = "Modal", Url = "/components/modal" },
                 new() { Text = "Panel", Url = "/components/panel" },
                 new() { Text = "ScrollablePane", Url = "/components/scrollablepane", Description = "ScrollView" },
+                new() { Text = "Splitter", Url = "/components/splitter" },
                 new() { Text = "Tooltip", Url = "/components/tooltip" },
             ],
         },
