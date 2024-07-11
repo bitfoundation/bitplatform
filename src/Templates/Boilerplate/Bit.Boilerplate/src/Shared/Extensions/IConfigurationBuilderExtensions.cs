c53835ec675522af514595b5292a51927086aff2
Yaser Moradi <ysmoradi@outlook.com>
2024-07-11 19:33:33 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Shared/Extensions/IConfigurationBuilderExtensions.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Shared/Extensions/IConfigurationBuilderExtensions.cs
@@ -7,18 +7,20 @@ public static class IConfigurationBuilderExtensions
 {
     public static void AddSharedConfigurations(this IConfigurationBuilder builder)
     {
-        List<Stream?> appsettings = [];
+        List<Stream?> appSettingSources = [];
 
         var sharedAssembly = Assembly.Load("Boilerplate.Shared");
 
-        appsettings.Add(sharedAssembly.GetManifestResourceStream("Boilerplate.Shared.appsettings.json"));
-        appsettings.Add(sharedAssembly.GetManifestResourceStream($"Boilerplate.Shared.appsettings.{AppEnvironment.Current}.json"));
+        appSettingSources.Add(sharedAssembly.GetManifestResourceStream("Boilerplate.Shared.appsettings.json"));
+        appSettingSources.Add(sharedAssembly.GetManifestResourceStream($"Boilerplate.Shared.appsettings.{AppEnvironment.Current}.json"));
 
-        var clonedConfigurations = builder.Sources.ToArray(); // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/configuration#default-application-configuration-sources
+        var originalSources = builder.Sources.ToArray(); // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/configuration#default-application-configuration-sources
         if (builder.Sources.IsReadOnly is false)
+        {
             builder.Sources.Clear();
+        }
 
-        builder.Sources.AddRange(appsettings.Where(appsetting => appsetting is not null).Select(appsetting => new JsonStreamConfigurationSource { Stream = appsetting! }));
-        builder.Sources.AddRange(clonedConfigurations);
+        builder.Sources.AddRange(appSettingSources.Where(appsetting => appsetting is not null).Select(appsetting => new JsonStreamConfigurationSource { Stream = appsetting! }));
+        builder.Sources.AddRange(originalSources);
     }
 }
