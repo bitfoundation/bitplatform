030112a6e142b345f77b1c483852e2a43d8f3cf3
Mohammad Hosein Rastegarinia <mh.rastegari@outlook.com>
2024-07-17 21:51:46 +0330

    Replace @page directives with Route attributes #8016

---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Urls.cs
@@ -0,0 +1,36 @@
+﻿namespace Boilerplate.Client.Core;
+
+public static class Urls
+{
+    public const string HomePage = "";
+
+    public const string NotAuthorizedPage = "not-authorized";
+
+    public const string NotFoundPage = "not-found";
+
+    public const string TermsPage = "terms";
+
+    public const string AddOrEditCategoryPage = "add-edit-category/{Id:int?}";
+
+    public const string CategoriesPage = "categories";
+
+    public const string DashboardPage = "dashboard";
+
+    public const string ProfilePage = "profile";
+
+    public const string ConfirmPage = "confirm";
+
+    public const string ForgotPasswordPage = "forgot-password";
+
+    public const string ResetPasswordPage = "reset-password";
+
+    public const string SignInPage = "sign-in";
+
+    public const string SignUpPage = "sign-up";
+
+    public const string OfflineEditProfilePage = "offline-edit-profile";
+
+    public const string ProductsPage = "products";
+
+    public const string TodoPage = "todo";
+}
