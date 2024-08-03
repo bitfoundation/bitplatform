bed8e76226f8796ce1b9575528ba35c119ef38d6
Saleh Yusefnejad <msynk@outlook.com>
2024-08-03 16:33:19 +0330

    update Boilerplate for new version of BlazorUI #8203

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Categories/AddOrEditCategoryPage.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Categories/AddOrEditCategoryPage.razor.cs
@@ -14,7 +14,7 @@ public partial class AddOrEditCategoryPage
     private bool isSaving;
     private string? saveMessage;
     private bool isColorPickerOpen;
-    private BitSeverity saveMessageSeverity;
+    private BitColor saveMessageColor;
     private CategoryDto category = new();
 
     protected override async Task OnInitAsync()
@@ -69,14 +69,14 @@ public partial class AddOrEditCategoryPage
         }
         catch (ResourceValidationException e)
         {
-            saveMessageSeverity = BitSeverity.Error;
+            saveMessageColor = BitColor.Error;
 
             saveMessage = string.Join(Environment.NewLine, e.Payload.Details.SelectMany(d => d.Errors).Select(e => e.Message));
         }
         catch (KnownException e)
         {
             saveMessage = e.Message;
-            saveMessageSeverity = BitSeverity.Error;
+            saveMessageColor = BitColor.Error;
         }
         finally
         {
