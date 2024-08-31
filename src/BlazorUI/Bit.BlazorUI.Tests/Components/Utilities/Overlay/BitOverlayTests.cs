dbd7cbefe3a499405cd326b086c85854caf29cf6
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2024-08-31 09:17:39 +0330

    feat(blazorui): improve tests of BitOverlay #8400 (#8458)

--- src/BlazorUI/Bit.BlazorUI.Tests/Components/Utilities/Overlay/BitOverlayTests.cs
+++ src/BlazorUI/Bit.BlazorUI.Tests/Components/Utilities/Overlay/BitOverlayTests.cs
@@ -6,94 +6,369 @@ namespace Bit.BlazorUI.Tests.Components.Utilities.Overlay;
 [TestClass]
 public class BitOverlayTests : BunitTestContext
 {
-
-    [TestMethod]
-    public void BitOverlayInitialTest()
+    [DataTestMethod]
+    public void BitOverlayShouldRenderExpectedElement()
     {
-        var com = RenderComponent<BitOverlay>();
-        var element = com.Find(".bit-ovl");
+        var component = RenderComponent<BitOverlay>();
 
-        Assert.IsFalse(element.ClassList.Contains("bit-ovl-vis")); //shouldn't be visible when rendered
-
+        component.MarkupMatches(@"<div class=""bit-ovl"" id:ignore></div>");
     }
 
-    [TestMethod]
-    public void BitOverlayIsVisibleTest()
+    [DataTestMethod,
+        DataRow(true),
+        DataRow(false)
+    ]
+    public void BitOverlayShouldRespectIsEnabled(bool isEnabled)
     {
-        var isVisible = true;
-        var com = RenderComponent<BitOverlay>(parameters =>
+        var component = RenderComponent<BitOverlay>(parameters =>
         {
-            parameters.Bind(p => p.IsVisible, isVisible, value => isVisible = value);
+            parameters.Add(p => p.IsEnabled, isEnabled);
         });
 
-        var element = com.Find(".bit-ovl");
+        var cssClass = isEnabled ? null : " bit-dis";
 
-        Assert.IsTrue(element.ClassList.Contains("bit-ovl-vis"));
+        component.MarkupMatches(@$"<div class=""bit-ovl{cssClass}"" id:ignore></div>");
     }
 
-    [TestMethod]
-    public void BitOverlayAutoCloseTest()
+    [DataTestMethod]
+    public void BitOverlayShouldRespectIsEnabledChangingAfterRender()
     {
-        var isVisible = true;
-        var com = RenderComponent<BitOverlay>(parameters =>
+        var component = RenderComponent<BitOverlay>();
+
+        component.MarkupMatches(@"<div class=""bit-ovl"" id:ignore></div>");
+
+        component.SetParametersAndRender(parameters =>
         {
-            parameters.Bind(p => p.IsVisible, isVisible, value => isVisible = value);
+            parameters.Add(p => p.IsEnabled, false);
         });
 
-        var element = com.Find(".bit-ovl");
+        component.MarkupMatches(@"<div class=""bit-ovl bit-dis"" id:ignore></div>");
+    }
 
+    [DataTestMethod,
+        DataRow("font-size: 14px; color: red;"),
+        DataRow("padding: 1rem;"),
+        DataRow(null)
+    ]
+    public void BitOverlayShouldRespectStyle(string style)
+    {
+        var component = RenderComponent<BitOverlay>(parameters =>
+        {
+            parameters.Add(p => p.Style, style);
+        });
+
+        if (style.HasValue())
+        {
+            component.MarkupMatches(@$"<div style=""{style}"" class=""bit-ovl"" id:ignore></div>");
+        }
+        else
+        {
+            component.MarkupMatches(@"<div class=""bit-ovl"" id:ignore></div>");
+        }
+    }
+
+    [DataTestMethod]
+    public void BitOverlayShouldRespectStyleChangingAfterRender()
+    {
+        var component = RenderComponent<BitOverlay>();
+
+        component.MarkupMatches(@"<div class=""bit-ovl"" id:ignore></div>");
+
+        var style = "padding: 1rem;";
+        component.SetParametersAndRender(parameters =>
+        {
+            parameters.Add(p => p.Style, style);
+        });
+
+        component.MarkupMatches(@$"<div style=""{style}"" class=""bit-ovl"" id:ignore></div>");
+    }
+
+    [DataTestMethod,
+        DataRow("test-class"),
+        DataRow(null)
+    ]
+    public void BitOverlayShouldRespectClass(string @class)
+    {
+        var component = RenderComponent<BitOverlay>(parameters =>
+        {
+            parameters.Add(p => p.Class, @class);
+        });
+
+        var cssClass = @class.HasValue() ? $" {@class}" : null;
+
+        component.MarkupMatches(@$"<div class=""bit-ovl{cssClass}"" id:ignore></div>");
+    }
+
+    [DataTestMethod]
+    public void BitOverlayShouldRespectClassChangingAfterRender()
+    {
+        var component = RenderComponent<BitOverlay>();
+
+        component.MarkupMatches(@"<div class=""bit-ovl"" id:ignore></div>");
+
+        var cssClass = "test-class";
+
+        component.SetParametersAndRender(parameters =>
+        {
+            parameters.Add(p => p.Class, cssClass);
+        });
+
+        component.MarkupMatches(@$"<div class=""bit-ovl {cssClass}"" id:ignore></div>");
+    }
+
+    [DataTestMethod,
+        DataRow("test-id"),
+        DataRow(null)
+    ]
+    public void BitOverlayShouldRespectId(string id)
+    {
+        var component = RenderComponent<BitOverlay>(parameters =>
+        {
+            parameters.Add(p => p.Id, id);
+        });
+
+        var expectedId = id.HasValue() ? id : component.Instance.UniqueId.ToString();
+
+        component.MarkupMatches(@$"<div id=""{expectedId}"" class=""bit-ovl""></div>");
+    }
+
+    [DataTestMethod,
+        DataRow(BitDir.Rtl),
+        DataRow(BitDir.Ltr),
+        DataRow(BitDir.Auto),
+        DataRow(null)
+    ]
+    public void BitOverlayShouldRespectDir(BitDir? dir)
+    {
+        var component = RenderComponent<BitOverlay>(parameters =>
+        {
+            parameters.Add(p => p.Dir, dir);
+        });
+
+        if (dir.HasValue)
+        {
+            var cssClass = dir is BitDir.Rtl ? " bit-rtl" : null;
+            component.MarkupMatches(@$"<div dir=""{dir.Value.ToString().ToLower()}"" class=""bit-ovl{cssClass}"" id:ignore></div>");
+        }
+        else
+        {
+            component.MarkupMatches(@"<div class=""bit-ovl"" id:ignore></div>");
+        }
+    }
+
+    [DataTestMethod]
+    public void BitOverlayShouldRespectDirChangingAfterRender()
+    {
+        var component = RenderComponent<BitOverlay>();
+
+        component.MarkupMatches(@"<div class=""bit-ovl"" id:ignore></div>");
+
+        component.SetParametersAndRender(parameters =>
+        {
+            parameters.Add(p => p.Dir, BitDir.Ltr);
+        });
+
+        component.MarkupMatches(@"<div dir=""ltr"" class=""bit-ovl"" id:ignore></div>");
+    }
+
+    [DataTestMethod,
+        DataRow(BitVisibility.Visible),
+        DataRow(BitVisibility.Collapsed),
+        DataRow(BitVisibility.Hidden)
+    ]
+    public void BitOverlayShouldRespectVisibility(BitVisibility visibility)
+    {
+        var component = RenderComponent<BitOverlay>(parameters =>
+        {
+            parameters.Add(p => p.Visibility, visibility);
+        });
+
+        var styleAttribute = visibility switch
+        {
+            BitVisibility.Hidden => @"style=""visibility: hidden;""",
+            BitVisibility.Collapsed => @"style=""display: none;""",
+            _ => null
+        };
+
+        component.MarkupMatches(@$"<div {styleAttribute} class=""bit-ovl"" id:ignore></div>");
+    }
+
+    [DataTestMethod]
+    public void BitOverlayShouldRespectVisibilityChangingAfterRender()
+    {
+        var component = RenderComponent<BitOverlay>();
+
+        component.MarkupMatches(@"<div class=""bit-ovl"" id:ignore></div>");
+
+        component.SetParametersAndRender(parameters =>
+        {
+            parameters.Add(p => p.Visibility, BitVisibility.Collapsed);
+        });
+
+        component.MarkupMatches(@$"<div style=""display: none;"" class=""bit-ovl"" id:ignore></div>");
+    }
+
+    [DataTestMethod,
+        DataRow("Bit Blazor UI"),
+        DataRow("<span>Bit Blazor UI</span>"),
+        DataRow(null)
+    ]
+    public void BitOverlayShouldRespectChildContent(string childContent)
+    {
+        var component = RenderComponent<BitOverlay>(parameters =>
+        {
+            if (childContent is not null)
+            {
+                parameters.AddChildContent(childContent);
+            }
+        });
+
+        component.MarkupMatches(@$"<div class=""bit-ovl"" id:ignore>{childContent}</div>");
+    }
+
+    [DataTestMethod]
+    public void BitOverlayShouldRespectHtmlAttributes()
+    {
+        var component = RenderComponent<BitOverlayHtmlAttributesTest>();
+
+        component.MarkupMatches(@$"<div data-val-test=""bit"" class=""bit-ovl"" id:ignore>I'm an overlay</div>");
+    }
+
+    [DataTestMethod,
+        DataRow(true),
+        DataRow(false)
+    ]
+    public void BitOverlayShouldRespectNoAutoClose(bool noAutoClose)
+    {
+        var isVisible = true;
+        var component = RenderComponent<BitOverlay>(parameters =>
+        {
+            parameters.Bind(p => p.IsVisible, isVisible, currentValue => isVisible = currentValue);
+            parameters.Add(p => p.NoAutoClose, noAutoClose);
+        });
+
+        component.MarkupMatches(@"<div class=""bit-ovl bit-ovl-vis"" id:ignore></div>");
+
+        Assert.IsTrue(isVisible);
+
+        var element = component.Find(".bit-ovl");
         element.Click();
 
-        Assert.IsFalse(element.ClassList.Contains("bit-ovl-vis"));
+        var cssClass = noAutoClose ? " bit-ovl-vis" : null;
+
+        component.MarkupMatches(@$"<div class=""bit-ovl{cssClass}"" id:ignore></div>");
+
+        if (noAutoClose)
+        {
+            Assert.IsTrue(isVisible);
+        }
+        else
+        {
+            Assert.IsFalse(isVisible);
+        }
     }
 
-    [TestMethod]
-    public void BitOverlayDisabledAutoCloseTest()
+    [DataTestMethod,
+        DataRow(true),
+        DataRow(false)
+    ]
+    public void BitOverlayShouldRespectAutoToggleScroll(bool autoToggleScroll)
     {
         var isVisible = true;
-        var com = RenderComponent<BitOverlay>(parameters =>
+        var component = RenderComponent<BitOverlay>(parameters =>
         {
             parameters.Bind(p => p.IsVisible, isVisible, value => isVisible = value);
-            parameters.Add(p => p.NoAutoClose, true);
+            parameters.Add(p => p.AutoToggleScroll, autoToggleScroll);
         });
 
-        var element = com.Find(".bit-ovl");
-
+        var element = component.Find(".bit-ovl");
         element.Click();
 
-        Assert.IsTrue(element.ClassList.Contains("bit-ovl-vis"));
+        if (autoToggleScroll)
+        {
+            //AutoToggleScroll is false by default so it should invoke "BitBlazorUI.Overlay.toggleScroll" once and then once again on closing component
+            Context.JSInterop.VerifyInvoke("BitBlazorUI.Overlay.toggleScroll", 2);
+        }
+        else
+        {
+            Context.JSInterop.VerifyNotInvoke("BitBlazorUI.Overlay.toggleScroll");
+        }
     }
 
-    [TestMethod]
-    public void BitOverlayAbsolutePositionTest()
+    [DataTestMethod,
+        DataRow(true),
+        DataRow(false)
+    ]
+    public void BitOverlayShouldRespectAbsolutePosition(bool absolutePosition)
     {
-        var com = RenderComponent<BitOverlay>(parameters =>
+        var component = RenderComponent<BitOverlay>(parameters =>
+        {
+            parameters.Add(p => p.AbsolutePosition, absolutePosition);
+        });
+
+        var cssClass = absolutePosition ? " bit-ovl-abs" : null;
+
+        component.MarkupMatches(@$"<div class=""bit-ovl{cssClass}"" id:ignore></div>");
+    }
+
+    [DataTestMethod]
+    public void BitOverlayShouldRespectAbsolutePositionChangingAfterRender()
+    {
+        var component = RenderComponent<BitOverlay>();
+
+        component.MarkupMatches(@"<div class=""bit-ovl"" id:ignore></div>");
+
+        component.SetParametersAndRender(parameters =>
         {
             parameters.Add(p => p.AbsolutePosition, true);
         });
 
-        var element = com.Find(".bit-ovl");
-
-        Assert.IsTrue(element.ClassList.Contains("bit-ovl-abs"));
+        component.MarkupMatches(@$"<div class=""bit-ovl bit-ovl-abs"" id:ignore></div>");
     }
 
-    [TestMethod]
-    public void BitOverlayAutoToggleScrollTest()
+    [DataTestMethod,
+        DataRow(true),
+        DataRow(false)
+    ]
+    public void BitOverlayShouldRespectIsVisible(bool isVisible)
     {
-        Context.JSInterop.Mode = JSRuntimeMode.Loose;
-
-        var isVisible = true;
-        var com = RenderComponent<BitOverlay>(parameters =>
+        var isVisibleBind = isVisible;
+        var component = RenderComponent<BitOverlay>(parameters =>
         {
-            parameters.Bind(p => p.IsVisible, isVisible, value => isVisible = value);
-            parameters.Add(p => p.AutoToggleScroll, true);
+            parameters.Bind(p => p.IsVisible, isVisibleBind, value => isVisibleBind = value);
         });
 
-        var element = com.Find(".bit-ovl");
+        var cssClass = isVisible ? " bit-ovl-vis" : null;
+
+        component.MarkupMatches(@$"<div class=""bit-ovl{cssClass}"" id:ignore></div>");
+
+        Assert.AreEqual(isVisible, isVisibleBind);
+
+        var element = component.Find(".bit-ovl");
         element.Click();
 
-        //AutoToggleScroll is false by default so it should invoke "BitBlazorUI.Overlay.toggleScroll" once and then once again on closing component
-        Context.JSInterop.VerifyInvoke("BitBlazorUI.Overlay.toggleScroll", 2);
+        component.MarkupMatches(@"<div class=""bit-ovl"" id:ignore></div>");
+
+        Assert.IsFalse(isVisibleBind);
     }
 
+    [DataTestMethod]
+    public void BitOverlayShouldRespectIsVisibleChangingAfterRender()
+    {
+        var isVisible = false;
+        var component = RenderComponent<BitOverlay>(parameters =>
+        {
+            parameters.Bind(p => p.IsVisible, isVisible, value => isVisible = value);
+        });
+
+        component.MarkupMatches(@"<div class=""bit-ovl"" id:ignore></div>");
+
+        isVisible = true;
+        component.SetParametersAndRender(parameters =>
+        {
+            parameters.Bind(p => p.IsVisible, isVisible, value => isVisible = value);
+        });
+
+        component.MarkupMatches(@$"<div class=""bit-ovl bit-ovl-vis"" id:ignore></div>");
+    }
 }
