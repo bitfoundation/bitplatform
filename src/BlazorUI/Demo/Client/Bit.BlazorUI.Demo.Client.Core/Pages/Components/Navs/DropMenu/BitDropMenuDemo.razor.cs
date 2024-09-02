7d27b9b097d5825b60776072213b6ae29662f5dc
Saleh Yusefnejad <msynk@outlook.com>
2024-09-02 14:11:37 +0330

    feat(blazorui): add DropMenu component #8460 (#8474)

---
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Navs/DropMenu/BitDropMenuDemo.razor.cs
@@ -0,0 +1,150 @@
+﻿namespace Bit.BlazorUI.Demo.Client.Core.Pages.Components.Navs.DropMenu;
+
+public partial class BitDropMenuDemo
+{
+    private readonly List<ComponentParameter> componentParameters =
+    [
+        new()
+        {
+            Name = "Body",
+            Type = "RenderFragment?",
+            DefaultValue = "null",
+            Description = "Alias of the ChildContent."
+        },
+        new()
+        {
+            Name = "ChevronDownIcon",
+            Type = "string?",
+            DefaultValue = "null",
+            Description = "The icon name of the chevron down part of the drop menu."
+        },
+        new()
+        {
+            Name = "ChildContent",
+            Type = "RenderFragment?",
+            DefaultValue = "null",
+            Description = "The content of the callout of the drop menu."
+        },
+        new()
+        {
+            Name = "Classes",
+            Type = "BitDropMenuClassStyles?",
+            DefaultValue = "null",
+            Description = "Custom CSS classes for different parts of the drop menu.",
+            LinkType = LinkType.Link,
+            Href = "#class-styles"
+        },
+        new()
+        {
+            Name = "IconName",
+            Type = "string?",
+            DefaultValue = "null",
+            Description = "The icon to show inside the header of the drop menu."
+        },
+        new()
+        {
+            Name = "IsOpen",
+            Type = "bool",
+            DefaultValue = "false",
+            Description = "Determines the opening state of the callout of the drop menu."
+        },
+        new()
+        {
+            Name = "OnClick",
+            Type = "EventCallback",
+            DefaultValue = "",
+            Description = "The callback is called when the drop menu is clicked."
+        },
+        new()
+        {
+            Name = "Styles",
+            Type = "BitDropMenuClassStyles?",
+            DefaultValue = "null",
+            Description = "Custom CSS styles for different parts of the drop menu.",
+            LinkType = LinkType.Link,
+            Href = "#class-styles"
+        },
+        new()
+        {
+            Name = "Template",
+            Type = "RenderFragment?",
+            DefaultValue = "null",
+            Description = "The custom content to render inside the header of the drop menu."
+        },
+        new()
+        {
+            Name = "Text",
+            Type = "string?",
+            DefaultValue = "null",
+            Description = "The text to show inside the header of the drop menu."
+        },
+        new()
+        {
+            Name = "Transparent",
+            Type = "bool",
+            DefaultValue = "false",
+            Description = "Makes the background of the header of the drop menu transparent."
+        },
+    ];
+
+    private readonly List<ComponentSubClass> componentSubClasses =
+    [
+        new()
+        {
+            Id = "class-styles",
+            Title = "BitDropMenuClassStyles",
+            Parameters =
+            [
+                new()
+                {
+                    Name = "Root",
+                    Type = "string?",
+                    DefaultValue = "null",
+                    Description = "Custom CSS classes/styles for the root element of the BitDropMenu."
+                },
+                new()
+                {
+                    Name = "Opened",
+                    Type = "string?",
+                    DefaultValue = "null",
+                    Description = "Custom CSS classes/styles for the opened callout state of the BitDropMenu."
+                },
+                new()
+                {
+                    Name = "Icon",
+                    Type = "string?",
+                    DefaultValue = "null",
+                    Description = "Custom CSS classes/styles for the icon of the BitDropMenu."
+                },
+                new()
+                {
+                    Name = "Text",
+                    Type = "string?",
+                    DefaultValue = "null",
+                    Description = "Custom CSS classes/styles for the text of the BitDropMenu."
+                },
+                new()
+                {
+                    Name = "ChevronDown",
+                    Type = "string?",
+                    DefaultValue = "null",
+                    Description = "Custom CSS classes/styles for the chevron-down icon of the BitDropMenu."
+                },
+                new()
+                {
+                    Name = "Overlay",
+                    Type = "string?",
+                    DefaultValue = "null",
+                    Description = "Custom CSS classes/styles for the overlay of the BitDropMenu."
+                },
+                new()
+                {
+                    Name = "Callout",
+                    Type = "string?",
+                    DefaultValue = "null",
+                    Description = "Custom CSS classes/styles for the callout of the BitDropMenu."
+                },
+            ]
+        }
+    ];
+}
---
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Navs/DropMenu/BitDropMenuDemo.razor.samples.cs
@@ -0,0 +1,13 @@
+﻿namespace Bit.BlazorUI.Demo.Client.Core.Pages.Components.Navs.DropMenu;
+
+public partial class BitDropMenuDemo
+{
+    private readonly string example1RazorCode = @"
+<BitDropMenu Text=""Drop me"">
+    <BitStack Gap=""1rem"" Style=""padding:0.5rem"">
+        <BitText Variant=""BitTextVariant.Subtitle1"">This is the content</BitText>
+        <BitButton>Click me</BitButton>
+        <BitToggle>Toggle me</BitToggle>
+    </BitStack>
+</BitDropMenu>";
+}
