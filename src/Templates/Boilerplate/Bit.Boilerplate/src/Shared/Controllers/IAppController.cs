e3a0aa628b321f31647fa923c62f2a7e30b69ff5
ysmoradi <ysmoradi@outlook.com>
2024-09-03 16:28:29 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Shared/Controllers/IAppController.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Shared/Controllers/IAppController.cs
@@ -2,6 +2,7 @@
 
 public interface IAppController
 {
+    void AddQueryString(string existingQueryString) { }
     void AddQueryString(string key, object? value) { }
     void AddQueryStrings(Dictionary<string, object?> queryString) { }
 }
