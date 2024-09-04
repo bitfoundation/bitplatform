7bf5f4250615240e1fe0714bdfa9510180541169
Saleh Yusefnejad <msynk@outlook.com>
2024-09-04 09:12:56 +0330

    feat(prerelease): v-8.11.0-pre-06 #8494 (#8495)

--- src/Bup/Bit.Bup/Scripts/bit-bup.ts
+++ src/Bup/Bit.Bup/Scripts/bit-bup.ts
@@ -1,5 +1,5 @@
 ﻿var BitBup = BitBup || {};
-BitBup.version = window['bit-bup version'] = '8.11.0-pre-05';
+BitBup.version = window['bit-bup version'] = '8.11.0-pre-06';
 
 declare const Blazor: any;
 
