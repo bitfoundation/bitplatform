a3d9ea2da8f88ef89e499f7e68ef5e79da7ba556
ysmoradi <ysmoradi@outlook.com>
2024-09-14 21:42:14 +0200

    Refactor Boilerplate Identity (#8630)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Mappers/IdentityMapper.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Mappers/IdentityMapper.cs
@@ -12,7 +12,5 @@ public static partial class IdentityMapper
 {
     public static partial UserDto Map(this User source);
     public static partial void Patch(this EditUserDto source, User destination);
-
-    // https://mapperly.riok.app/docs/configuration/before-after-map/
     public static partial UserSessionDto Map(this UserSession source);
 }
