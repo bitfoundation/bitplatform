e1fd51ba4c531d3adcb56b602f28dbf2b407f68c
ysmoradi <ysmoradi@outlook.com>
2024-09-16 20:15:47 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Web/wwwroot/service-worker.js
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Web/wwwroot/service-worker.js
@@ -15,7 +15,7 @@ self.externalAssets = [
         "url": "/"
     },
     {
-        url: "_framework/blazor.web.js?v=8.0.8"
+        url: "_framework/blazor.web.js?v=9.0.0-rc.1.24452.1"
     }
 ];
 
