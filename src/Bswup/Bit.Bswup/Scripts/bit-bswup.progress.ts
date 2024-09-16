c6cb0bafdc89767efc938b8600363a3877d6452b
Saleh Yusefnejad <msynk@outlook.com>
2024-09-16 14:07:41 +0330

    feat(release): v-8.11.0 #8577 (#8585)

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bswup.progress version'] = '8.11.0-pre-09';
+﻿window['bit-bswup.progress version'] = '8.11.0';
 
 ; (function () {
     (window as any).startBswupProgress = (autoReload: boolean,
--- src/Bup/Bit.Bup/Scripts/bit-bup.progress.ts
+++ src/Bup/Bit.Bup/Scripts/bit-bup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bup.progress version'] = '8.11.0-pre-09';
+﻿window['bit-bup.progress version'] = '8.11.0';
 
 ; (function () {
     (window as any).startBupProgress = (showLogs: boolean, showAssets: boolean, appContainerSelector: string, hideApp: boolean, autoHide: boolean) => {
