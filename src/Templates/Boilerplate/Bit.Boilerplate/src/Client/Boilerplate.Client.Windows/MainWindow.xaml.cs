9e08c52f11215b589b0652fb64f3a7cd303cc000
ysmoradi <ysmoradi@outlook.com>
2024-09-19 18:55:41 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Windows/MainWindow.xaml.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Windows/MainWindow.xaml.cs
@@ -13,7 +13,9 @@ public partial class MainWindow
         AppWebView.Services = services.BuildServiceProvider();
         if (CultureInfoManager.MultilingualEnabled)
         {
-            AppWebView.Services.GetRequiredService<CultureInfoManager>().SetCurrentCulture(App.Current.Properties["Culture"]?.ToString() ?? CultureInfo.CurrentUICulture.Name);
+            AppWebView.Services.GetRequiredService<CultureInfoManager>().SetCurrentCulture(
+                App.Current.Properties["Culture"]?.ToString() ?? // 1- User settings
+                CultureInfo.CurrentUICulture.Name); // 2- OS Settings
         }
         AppWebView.Services.GetRequiredService<IPubSubService>().Subscribe(PubSubMessages.CULTURE_CHANGED, async culture =>
         {
@@ -22,7 +24,7 @@ public partial class MainWindow
         });
         AppWebView.Loaded += async delegate
         {
-            AppWebView.WebView.DefaultBackgroundColor = ColorTranslator.FromHtml(Background.ToString());
+            AppWebView.WebView.DefaultBackgroundColor = ColorTranslator.FromHtml(App.Current.Resources["PrimaryBgColor"].ToString()!);
             await AppWebView.WebView.EnsureCoreWebView2Async();
             var settings = AppWebView.WebView.CoreWebView2.Settings;
             if (AppEnvironment.IsDev() is false)
