52e209012add94f5f40906ee96e8c8ee335ad326
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2024-09-22 17:39:40 +0330

    Rename TimelineActionItem class to Event in BitTimeline demo page (#8721)

--- src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Lists/Timeline/Event.cs
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Lists/Timeline/Event.cs
@@ -1,12 +1,12 @@
 ﻿namespace Bit.BlazorUI.Demo.Client.Core.Pages.Components.Lists.Timeline;
 
-public class TimelineActionItem
+public class Event
 {
     public string? Class { get; set; }
 
-    public RenderFragment<TimelineActionItem>? DotContent { get; set; }
+    public RenderFragment<Event>? DotContent { get; set; }
 
-    public RenderFragment<TimelineActionItem>? FirstContent { get; set; }
+    public RenderFragment<Event>? FirstContent { get; set; }
 
     public string? FirstText { get; set; }
 
@@ -16,7 +16,7 @@ public class TimelineActionItem
 
     public bool Reversed { get; set; }
 
-    public RenderFragment<TimelineActionItem>? SecondContent { get; set; }
+    public RenderFragment<Event>? SecondContent { get; set; }
 
     public string? SecondText { get; set; }
 
--- src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Lists/Timeline/_BitTimelineCustomDemo.razor.cs
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Lists/Timeline/_BitTimelineCustomDemo.razor.cs
@@ -1,8 +1,10 @@
-﻿namespace Bit.BlazorUI.Demo.Client.Core.Pages.Components.Lists.Timeline;
+﻿using Microsoft.Extensions.Logging;
+
+namespace Bit.BlazorUI.Demo.Client.Core.Pages.Components.Lists.Timeline;
 
 public partial class _BitTimelineCustomDemo
 {
-    BitTimelineNameSelectors<TimelineActionItem> nameSelectors = new()
+    BitTimelineNameSelectors<Event> nameSelectors = new()
     {
         PrimaryText = { Selector = i => i.FirstText },
         SecondaryText = { Selector = i => i.SecondText },
@@ -13,41 +15,41 @@ public partial class _BitTimelineCustomDemo
         SecondaryContent = { Selector = i => i.SecondContent },
     };
 
-    private List<TimelineActionItem> basicCustoms =
+    private List<Event> basicCustoms =
     [
         new() { FirstText = "Custom 1" },
         new() { FirstText = "Custom 2", SecondText = "Custom 2 Secondary" },
         new() { FirstText = "Custom 3" }
     ];
 
-    private List<TimelineActionItem> disabledCustoms =
+    private List<Event> disabledCustoms =
     [
         new() { FirstText = "Custom 1" },
         new() { FirstText = "Custom 2", SecondText = "Custom 2 Secondary", Disabled = true },
         new() { FirstText = "Custom 3" }
     ];
 
-    private List<TimelineActionItem> iconCustoms =
+    private List<Event> iconCustoms =
     [
         new() { FirstText = "Custom 1", Icon = BitIconName.Add },
         new() { FirstText = "Custom 2", Icon = BitIconName.Edit, SecondText = "Custom 2 Secondary", Disabled = true },
         new() { FirstText = "Custom 3", Icon = BitIconName.Delete }
     ];
 
-    private List<TimelineActionItem> reversedCustoms =
+    private List<Event> reversedCustoms =
     [
         new() { FirstText = "Custom 1" },
         new() { FirstText = "Custom 2", Reversed = true },
         new() { FirstText = "Custom 3" }
     ];
 
-    private List<TimelineActionItem> styleClassCustoms =
+    private List<Event> styleClassCustoms =
     [
         new() { FirstText = "Styled", Style = "color: dodgerblue;", Icon = BitIconName.Brush },
         new() { FirstText = "Classed", Class = "custom-item", Icon = BitIconName.FormatPainter }
     ];
 
-    private List<TimelineActionItem> basicRtlCustoms =
+    private List<Event> basicRtlCustoms =
     [
         new() { FirstText = "گزینه ۱" },
         new() { FirstText = "گزینه ۲", SecondText = "گزینه ۲ ثانویه" },
