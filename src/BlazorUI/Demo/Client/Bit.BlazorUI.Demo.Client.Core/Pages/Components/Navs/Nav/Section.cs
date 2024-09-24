86af473f032b7ce8cfc076cb56e9a75eedfdc6c3
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2024-09-24 23:26:38 +0330

    Rename BitPlatformMenu class to Section in BitNav demo page (#8720)

--- src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Navs/Nav/Section.cs
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Navs/Nav/Section.cs
@@ -1,13 +1,13 @@
 ﻿namespace Bit.BlazorUI.Demo.Client.Core.Pages.Components.Navs.Nav;
 
-public class BitPlatformMenu
+public class Section
 {
     public string Text { get; set; } = string.Empty;
     public string? Icon { get; set; }
     public string? Url { get; set; }
     public bool IsEnabled { get; set; } = true;
     public bool IsExpanded { get; set; }
-    public List<BitPlatformMenu> Links { get; set; } = [];
+    public List<Section> Links { get; set; } = [];
     public string? Comment { get; set; }
-    public RenderFragment<BitPlatformMenu>? Template { get; set; }
+    public RenderFragment<Section>? Template { get; set; }
 }
