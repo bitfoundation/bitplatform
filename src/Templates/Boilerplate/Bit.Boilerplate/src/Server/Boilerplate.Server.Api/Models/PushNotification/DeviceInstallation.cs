e9c153dc8c7455c873a3818a40fbd54f76175970
ysmoradi <ysmoradi@outlook.com>
2024-09-30 12:58:54 +0200

    initial commit

---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Models/PushNotification/DeviceInstallation.cs
@@ -0,0 +1,15 @@
+﻿namespace Boilerplate.Server.Api.Models.PushNotification;
+
+public partial class DeviceInstallation
+{
+    [Required]
+    public string? InstallationId { get; set; }
+
+    [Required]
+    public string? Platform { get; set; }
+
+    [Required]
+    public string? PushChannel { get; set; }
+
+    public IList<string> Tags { get; set; } = [];
+}
---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Models/PushNotification/NotificationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Boilerplate.Server.Api.Models.PushNotification;
+
+public partial class NotificationRequest
+{
+    public string? Text { get; set; }
+    public string? Action { get; set; }
+    public string[] Tags { get; set; } = [];
+    public bool Silent { get; set; }
+}
