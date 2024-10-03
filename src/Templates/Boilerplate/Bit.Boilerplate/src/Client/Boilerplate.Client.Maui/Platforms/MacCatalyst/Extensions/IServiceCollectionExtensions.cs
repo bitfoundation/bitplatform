74ee7a5c1b9a0ca99247b31dd8b423bd38e3691b
ysmoradi <ysmoradi@outlook.com>
2024-10-03 20:50:44 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Maui/Platforms/MacCatalyst/Extensions/IServiceCollectionExtensions.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Maui/Platforms/MacCatalyst/Extensions/IServiceCollectionExtensions.cs
@@ -1,10 +1,15 @@
-﻿namespace Microsoft.Extensions.DependencyInjection;
+﻿using Boilerplate.Client.Maui.Platforms.MacCatalyst.Services;
+
+namespace Microsoft.Extensions.DependencyInjection;
 
 public static partial class IServiceCollectionExtensions
 {
     public static IServiceCollection AddClientMauiProjectMacCatalystServices(this IServiceCollection services)
     {
         // Services being registered here can get injected in Maui/macOS.
+
+        services.TryAddSessioned<IPushNotificationService, MacCatalystPushNotificationService>();
+
 
         return services;
     }
