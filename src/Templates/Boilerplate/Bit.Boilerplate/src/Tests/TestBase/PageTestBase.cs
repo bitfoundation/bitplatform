50d01f259c4692cf0e1d61d09cc40e091ca83b1b
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-10-09 14:45:46 +0330

    Add playwright video record for failed tests

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Tests/TestBase/PageTestBase.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Tests/TestBase/PageTestBase.cs
@@ -27,9 +27,30 @@ public partial class PageTestBase : PageTest
     [TestCleanup]
     public async ValueTask CleanupTestServer()
     {
+        await Context.CloseAsync();
+        if (TestContext.CurrentTestOutcome is not UnitTestOutcome.Failed)
+        {
+            var directory = GetVideoDirectory(TestContext);
+            if (Directory.Exists(directory))
+                Directory.Delete(directory, true);
+        }
+
         if (TestServer is not null)
         {
             await TestServer.DisposeAsync();
         }
+    }
+
+    public override BrowserNewContextOptions ContextOptions()
+    {
+        var options = base.ContextOptions();
+        options.RecordVideoDir = GetVideoDirectory(TestContext);
+        return options;
+    }
+
+    private static string GetVideoDirectory(TestContext testContext)
+    {
+        var testMethodFullName = $"{testContext.FullyQualifiedTestClassName}.{testContext.TestName}";
+        return Path.Combine(testContext.TestResultsDirectory!, "..", "..", "Videos", testMethodFullName);
     }
 }
