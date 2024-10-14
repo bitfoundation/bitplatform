cb22269228cfb21b1b7c3592d9757abd091bc626
Saleh Yusefnejad <msynk@outlook.com>
2024-10-14 15:09:40 +0330

    implement new design of home page of Boilerplate #8890

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/HomePage.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/HomePage.razor.cs
@@ -4,4 +4,7 @@ public partial class HomePage
 {
     protected override string? Title => Localizer[nameof(AppStrings.Home)];
     protected override string? Subtitle => string.Empty;
+
+
+    [CascadingParameter] private BitDir? currentDir { get; set; }
 }
