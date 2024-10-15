15d6eff7c0ecbf270c2401b9710ae1e464dd7e21
Saleh Yusefnejad <msynk@outlook.com>
2024-10-15 15:23:48 +0330

    revert II

--- src/BlazorUI/Bit.BlazorUI/Components/Notifications/SnackBar/BitSnackBarItem.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Notifications/SnackBar/BitSnackBarItem.cs
@@ -3,12 +3,12 @@
 internal class BitSnackBarItem
 {
     public string Title { get; set; } = default!;
-
+    
     public string? Body { get; set; }
-
+    
     public BitColor? Color { get; set; }
-
+    
     public string? CssClass { get; set; }
-
+    
     public string? CssStyle { get; set; }
 }
