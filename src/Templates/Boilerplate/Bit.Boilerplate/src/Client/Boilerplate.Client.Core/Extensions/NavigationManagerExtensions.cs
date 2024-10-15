8b96cb1f81559673eb41ed67e0db492c429f5961
ysmoradi <ysmoradi@outlook.com>
2024-10-15 15:36:38 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Extensions/NavigationManagerExtensions.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Extensions/NavigationManagerExtensions.cs
@@ -6,27 +6,11 @@ public static partial class NavigationManagerExtensions
 {
     public static string GetUriWithoutQueryParameter(this NavigationManager navigationManager, string key)
     {
-        var url = navigationManager.Uri;
-
-        var uri = new Uri(url);
-
-        // this gets all the query string key value pairs as a collection
-        var newQueryString = HttpUtility.ParseQueryString(uri.Query);
-
-        // this removes the key if exists
-        newQueryString.Remove(key);
-
-        // this gets the page path from root without QueryString
-        string pagePathWithoutQueryString = uri.GetLeftPart(UriPartial.Path);
-
-        return newQueryString.Count > 0
-            ? string.Format("{0}?{1}", pagePathWithoutQueryString, newQueryString)
-            : pagePathWithoutQueryString;
+        return new Uri(navigationManager.Uri).GetWithoutQueryParameter(key);
     }
 
     public static string GetUriPath(this NavigationManager navigationManager)
     {
-        var uriBuilder = new UriBuilder(new Uri(navigationManager.Uri).GetWithoutCulture()) { Query = string.Empty, Fragment = string.Empty };
-        return uriBuilder.Path;
+        return new Uri(navigationManager.Uri).GetPath();
     }
 }
