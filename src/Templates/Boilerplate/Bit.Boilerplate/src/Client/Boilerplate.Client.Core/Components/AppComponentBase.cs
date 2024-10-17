eb5fec84ae287e7ab8389532cf02a3de694cb442
ysmoradi <ysmoradi@outlook.com>
2024-10-17 20:02:21 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/AppComponentBase.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/AppComponentBase.cs
@@ -3,7 +3,6 @@
 public partial class AppComponentBase : ComponentBase, IAsyncDisposable
 {
     [CascadingParameter] public Task<AuthenticationState> AuthenticationStateTask { get; set; } = default!;
-    [Parameter] public string? culture { get; set; }
 
     [AutoInject] protected IJSRuntime JSRuntime = default!;
 
@@ -98,16 +97,6 @@ public partial class AppComponentBase : ComponentBase, IAsyncDisposable
         {
             try
             {
-                if (string.IsNullOrEmpty(culture) is false)
-                {
-                    if (CultureInfoManager.MultilingualEnabled is false || CultureInfoManager.SupportedCultures.Any(sc => string.Equals(sc.Culture.Name, culture, StringComparison.InvariantCultureIgnoreCase)) is false)
-                    {
-                        // Because Blazor router doesn't support regex, the '/{culture?}/' captures some irrelevant routes
-                        // such as non existing routes like /some-invalid-url, we need to make sure that the first segment of the route is a valid culture name.
-                        NavigationManager.NavigateTo($"{Urls.NotFoundPage}?url={NavigationManager.ToBaseRelativePath(NavigationManager.Uri)}");
-                    }
-                }
-
                 await OnAfterFirstRenderAsync();
             }
             catch (Exception exp)
