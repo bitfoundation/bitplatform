a5aad3309666a1fbcfcec8a1efdbc71f2411b5a9
Saleh Yusefnejad <msynk@outlook.com>
2024-10-17 09:02:35 +0330

    implement new design of dashboard page of Boilerplate #8932

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Controllers/Dashboard/DashboardController.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Controllers/Dashboard/DashboardController.cs
@@ -15,40 +15,43 @@ public partial class DashboardController : AppControllerBase, IDashboardControll
 
         result.TotalProducts = await DbContext.Products.CountAsync(cancellationToken);
         result.Last30DaysProductCount = await DbContext.Products.CountAsync(p => p.CreatedOn > last30DaysDate, cancellationToken);
+
         result.TotalCategories = await DbContext.Categories.CountAsync(cancellationToken);
+        result.CategoriesWithProductCount = await DbContext.Categories.CountAsync(c => c.Products.Count > 0, cancellationToken);
 
         return result;
     }
 
     [HttpGet]
-    public IQueryable<ProductsCountPerCategoryResponseDto> GetProductsCountPerCategoryStats()
+    public async Task<IQueryable<ProductsCountPerCategoryResponseDto>> GetProductsCountPerCategoryStats()
     {
+        await Task.Delay(2000);
+
         return DbContext.Categories
-            .Select(c => new ProductsCountPerCategoryResponseDto()
-            {
-                CategoryName = c.Name,
-                CategoryColor = c.Color,
-                ProductCount = c.Products!.Count()
-            });
+                        .Select(c => new ProductsCountPerCategoryResponseDto()
+                        {
+                            CategoryName = c.Name,
+                            CategoryColor = c.Color,
+                            ProductCount = c.Products!.Count()
+                        });
     }
 
 
     [HttpGet]
     public async Task<List<ProductPercentagePerCategoryResponseDto>> GetProductsPercentagePerCategoryStats(CancellationToken cancellationToken)
     {
+        await Task.Delay(1000);
+
         var productsTotalCount = await DbContext.Products.CountAsync(cancellationToken);
 
-        if (productsTotalCount == 0)
-        {
-            return [];
-        }
+        if (productsTotalCount == 0) return [];
 
         return await DbContext.Categories
-             .Select(c => new ProductPercentagePerCategoryResponseDto()
-             {
-                 CategoryName = c!.Name,
-                 CategoryColor = c.Color,
-                 ProductPercentage = (float)decimal.Divide(c.Products!.Count(), productsTotalCount) * 100
-             }).ToListAsync(cancellationToken);
+                              .Select(c => new ProductPercentagePerCategoryResponseDto()
+                              {
+                                  CategoryName = c!.Name,
+                                  CategoryColor = c.Color,
+                                  ProductPercentage = (float)decimal.Divide(c.Products!.Count(), productsTotalCount) * 100
+                              }).ToListAsync(cancellationToken);
     }
 }
