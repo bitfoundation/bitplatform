c200f9939640c845fce81449e297b70aa019f092
Saleh Yusefnejad <msynk@outlook.com>
2024-10-19 17:59:53 +0330

    refactor structure

---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/AuthorizedHeader.razor.cs
@@ -0,0 +1,33 @@
+﻿namespace Boilerplate.Client.Core.Components.Layout;
+
+public partial class AuthorizedHeader : AppComponentBase
+{
+    private string? pageTitle;
+    private string? pageSubtitle;
+    private Action unsubscribePageTitleChanged = default!;
+
+
+    protected override async Task OnInitAsync()
+    {
+        unsubscribePageTitleChanged = PubSubService.Subscribe(PubSubMessages.PAGE_TITLE_CHANGED, async payload =>
+        {
+            (pageTitle, pageSubtitle) = (ValueTuple<string?, string?>)payload!;
+
+            StateHasChanged();
+        });
+    }
+
+
+    private void OpenNavPanel()
+    {
+        PubSubService.Publish(PubSubMessages.OPEN_NAV_PANEL);
+    }
+
+
+    protected override async ValueTask DisposeAsync(bool disposing)
+    {
+        await base.DisposeAsync(disposing);
+
+        unsubscribePageTitleChanged?.Invoke();
+    }
+}
---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Authorized/Dashboard/OverallStatusCard.razor.cs
@@ -0,0 +1,9 @@
+﻿namespace Boilerplate.Client.Core.Components.Pages.Authorized.Dashboard;
+
+public partial class OverallStatusCard
+{
+    [Parameter] public bool Loading { get; set; }
+    [Parameter] public int Value { get; set; }
+    [Parameter] public string? Label { get; set; }
+    [Parameter] public string? IconName { get; set; }
+}
