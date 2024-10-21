bc7f3e144b6bc5847301e2d921355b3aa7718b3a
Yaser Moradi <ysmoradi@outlook.com>
2024-10-21 18:33:58 +0200

    fix(templates): resolve issues of sessions section of settings page of Boilerplate #8972 (#8973)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Authorized/Settings/SessionsSection.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Authorized/Settings/SessionsSection.razor.cs
@@ -10,7 +10,6 @@ public partial class SessionsSection
     private UserSessionDto? currentSession;
     private IEnumerable<UserSessionDto> otherSessions = [];
 
-
     [AutoInject] private IUserController userController = default!;
 
 
@@ -63,9 +62,11 @@ public partial class SessionsSection
 
     private static string GetImageUrl(string? device)
     {
-        var d = device?.ToLower() ?? "";
+        if (string.IsNullOrEmpty(device)) return "unknown.png";
 
-        if (d.Contains("windows")) return "windows.png";
+        var d = device.ToLowerInvariant();
+
+        if (d.Contains("windows") || d.Contains("win32")) return "windows.png";
 
         if (d.Contains("android")) return "android.png";
 
