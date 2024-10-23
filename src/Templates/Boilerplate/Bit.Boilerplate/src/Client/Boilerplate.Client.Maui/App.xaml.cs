a60d11712435d4d9bc5b1ef9f78f15c54d7bd72e
Yaser Moradi <ysmoradi@outlook.com>
2024-10-23 17:07:24 +0200

    fix(templates): resolve small issues in boilerplate #8994 (#8995)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Maui/App.xaml.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Maui/App.xaml.cs
@@ -1,7 +1,4 @@
 ﻿//+:cnd:noEmit
-using Maui.AppStores;
-using System.Runtime.InteropServices;
-
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 namespace Boilerplate.Client.Maui;
@@ -11,7 +8,6 @@ public partial class App
     private readonly Page mainPage;
     private readonly IExceptionHandler exceptionHandler;
     private readonly IBitDeviceCoordinator deviceCoordinator;
-    private readonly IStorageService storageService;
 
     public App(MainPage mainPage,
         IExceptionHandler exceptionHandler,
@@ -20,7 +16,6 @@ public partial class App
     {
         this.exceptionHandler = exceptionHandler;
         this.deviceCoordinator = deviceCoordinator;
-        this.storageService = storageService;
         this.mainPage = new NavigationPage(mainPage);
 
         InitializeComponent();
@@ -38,38 +33,10 @@ public partial class App
             base.OnStart();
 
             await deviceCoordinator.ApplyTheme(AppInfo.Current.RequestedTheme is AppTheme.Dark);
-
-            await CheckForUpdates();
         }
         catch (Exception exp)
         {
             exceptionHandler.Handle(exp);
         }
-    }
-
-    private async Task CheckForUpdates()
-    {
-        await Task.Delay(TimeSpan.FromSeconds(3)); // No rush to check for updates.
-
-        try
-        {
-            if (await AppStoreInfo.Current.IsUsingLatestVersionAsync() is false)
-            {
-                if (await storageService.GetItem($"{AppInfo.Version}_UpdateFromVersionIsRequested") is not "true")
-                {
-                    await storageService.SetItem($"{AppInfo.Version}_UpdateFromVersionIsRequested", "true");
-
-                    // It's an opportune moment to request an update. (:
-                    // https://github.com/oscoreio/Maui.AppStoreInfo
-                    if (await App.Current!.Windows.First().Page!.DisplayAlert(AppStrings.NewVersionIsAvailable, AppStrings.UpdateToNewVersion, AppStrings.Yes, AppStrings.No) is true)
-                    {
-                        await AppStoreInfo.Current.OpenApplicationInStoreAsync();
-                    }
-                }
-            }
-        }
-        catch (InvalidOperationException) when ((AppPlatform.IsIOS || AppPlatform.IsMacOS) && AppEnvironment.IsDev()) { }
-        catch (FileNotFoundException) { }
-        catch (COMException) { }
     }
 }
