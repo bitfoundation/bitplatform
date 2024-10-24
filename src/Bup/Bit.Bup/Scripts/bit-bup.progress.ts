c1396bafa9d22d2dcc8514d58fb8a8f694305100
Saleh Yusefnejad <msynk@outlook.com>
2024-10-24 17:20:03 +0330

    feat(prerelease): v-8.12.0-pre-06 #8988 (#9009)

--- src/Bup/Bit.Bup/Scripts/bit-bup.progress.ts
+++ src/Bup/Bit.Bup/Scripts/bit-bup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bup.progress version'] = '8.12.0-pre-05';
+﻿window['bit-bup.progress version'] = '8.12.0-pre-06';
 
 ; (function () {
     (window as any).startBupProgress = (showLogs: boolean, showAssets: boolean, appContainerSelector: string, hideApp: boolean, autoHide: boolean) => {
--- src/Butil/Bit.Butil/Scripts/butil.ts
+++ src/Butil/Bit.Butil/Scripts/butil.ts
@@ -1,2 +1,2 @@
 var BitButil = BitButil || {};
-BitButil.version = window['bit-butil version'] = '8.12.0-pre-05';
+BitButil.version = window['bit-butil version'] = '8.12.0-pre-06';
