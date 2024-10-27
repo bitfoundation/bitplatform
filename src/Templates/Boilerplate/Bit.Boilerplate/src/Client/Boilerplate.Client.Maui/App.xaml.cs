3cb548e66fd8c6a14d66013ae347deb3845d9a66
Yaser Moradi <ysmoradi@outlook.com>
2024-10-27 09:57:25 +0100

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Maui/App.xaml.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Maui/App.xaml.cs
@@ -42,6 +42,8 @@ public partial class App
             {
                 await App.Current!.Windows.First().Page!.DisplayAlert("Boilerplate", localizer[nameof(AppStrings.UpdateWebViewThroughGooglePlay)], localizer[nameof(AppStrings.Ok)]);
                 await Launcher.OpenAsync($"https://play.google.com/store/apps/details?id={Android.Webkit.WebView.CurrentWebViewPackage.PackageName}");
+                // Download for Android Emulator x86 + x86_64: https://www.apkmirror.com/apk/google-inc/android-system-webview/variant-%7B%22arches_slug%22%3A%5B%22x86%22%2C%22x86_64%22%5D%2C%22minapi_slug%22%3A%22minapi-26%22%7D/
+                // Download for Android Device arm64-v8a + arm-v7a: https://www.apkmirror.com/apk/google-inc/android-system-webview/variant-%7B%22arches_slug%22%3A%5B%22arm64-v8a%22%2C%22armeabi-v7a%22%5D%2C%22minapi_slug%22%3A%22minapi-26%22%7D/
             }
 #endif
         }
