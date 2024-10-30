66b5abfaf64044e52004f5cb414d976b53441ddd
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-10-31 03:20:07 +0330

    Extract video recording feature to a sperate class

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Tests/PageTests/PageTestBase.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Tests/PageTests/PageTestBase.cs
@@ -44,13 +44,7 @@ public abstract partial class PageTestBase : PageTest
     [TestCleanup]
     public async ValueTask CleanupTestServer()
     {
-        await Context.CloseAsync();
-        if (TestContext.CurrentTestOutcome is not UnitTestOutcome.Failed)
-        {
-            var directory = GetVideoDirectory(TestContext);
-            if (Directory.Exists(directory))
-                Directory.Delete(directory, true);
-        }
+        await this.FinalizeVideoRecording();
 
         if (TestServer is not null)
         {
@@ -61,7 +55,7 @@ public abstract partial class PageTestBase : PageTest
     public override BrowserNewContextOptions ContextOptions()
     {
         var options = base.ContextOptions();
-        options.RecordVideoDir = GetVideoDirectory(TestContext);
+        options.EnableVideoRecording(TestContext);
 
         var currentTestMethod = GetType().GetMethod(TestContext.TestName!);
 
@@ -82,27 +76,6 @@ public abstract partial class PageTestBase : PageTest
         }
 
         return options;
-    }
-
-    private string GetVideoDirectory(TestContext testContext)
-    {
-        var testMethodDsiplayName = GetTestMethodDisplayName();
-        char[] invalidChars = [.. Path.GetInvalidPathChars(), .. Path.GetInvalidFileNameChars(), ')', '"', '<', '>', '|', '*', '?', '\r', '\n'];
-        testMethodDsiplayName = new string(testMethodDsiplayName.Where(ch => !invalidChars.Contains(ch)).Select(ch => ch is '(' or ',' ? '_' : ch).ToArray());
-        var testMethodFullName = $"{testContext.FullyQualifiedTestClassName}.{testMethodDsiplayName}";
-        var dir = Path.Combine(testContext.TestResultsDirectory!, "..", "..", "Videos", testMethodFullName);
-        return Path.GetFullPath(dir);
-    }
-
-    private string GetTestMethodDisplayName()
-    {
-        var testContextType = TestContext.GetType();
-        var testMethodField = testContextType.GetField("_testMethod", BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new InvalidOperationException("Field '_testMethod' not found.");
-        var testMethod = testMethodField.GetValue(TestContext) ?? throw new InvalidOperationException("Field '_testMethod' is null.");
-        var testMethodType = testMethod.GetType();
-        var displayNameProperty = testMethodType.GetProperty("DisplayName", BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new InvalidOperationException("Property 'DisplayName' not found.");
-        var displayName = displayNameProperty.GetValue(testMethod) ?? throw new InvalidOperationException("Field 'DisplayName' is null.");
-        return (string)displayName;
     }
 
     private string?[]? GetTestMethodArguments()
