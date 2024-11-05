61a4ae01ccca82e71bd5cb92dd181a1c3b1ba7e4
Saleh Yusefnejad <msynk@outlook.com>
2024-11-05 19:28:13 +0330

    fix missing members

--- src/BlazorUI/Bit.BlazorUI.Extras/Components/PdfReader/@types/display/editor/alt_text.d.ts
+++ src/BlazorUI/Bit.BlazorUI.Extras/Components/PdfReader/@types/display/editor/alt_text.d.ts
@@ -19,9 +19,12 @@ declare class AltText {
     /**
      * Set the alt text data.
      */
-    set data({ altText, decorative, guessedText, textWithDisclaimer, cancel, }: {
+    set data({ altText, decorative, guessedText, textWithDisclaimer, cancel }: {
         altText: null;
         decorative: boolean;
+        guessedText: any;
+        textWithDisclaimer: any;
+        cancel: any;
     });
     get data(): {
         altText: null;
