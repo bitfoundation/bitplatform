39eef8f0167f914d0463e51304dc493c1e7221a8
Saleh Yusefnejad <msynk@outlook.com>
2024-11-07 23:10:25 +0330

    add comments for jsbridge

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/JsBridge.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/JsBridge.razor.cs
@@ -2,6 +2,11 @@
 
 public partial class JsBridge
 {
+    /// <summary>
+    /// at the rendering time of this component (the component is added to the `RootLayout`)
+    /// it registers an instance of the `DotNetObjectReference` into the js code (look at the `app.ts` file), 
+    /// so we can later use it to call .net methods.
+    /// </summary>
     protected override async Task OnAfterFirstRenderAsync()
     {
         var dotnetObj = DotNetObjectReference.Create(this);
@@ -12,6 +17,9 @@ public partial class JsBridge
     }
 
 
+    /// <summary>
+    /// you can add any other method like this to complete the bridge between js and .net code.
+    /// </summary>
     [JSInvokable(nameof(ShowDiagnostic))]
     public async Task ShowDiagnostic()
     {
