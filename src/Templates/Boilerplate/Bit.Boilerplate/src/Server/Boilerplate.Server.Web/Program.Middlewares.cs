91580dae3330f2c9fdfc021d8521fc579ec34bc7
Yaser Moradi <ysmoradi@outlook.com>
2024-11-08 17:02:33 +0100

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Web/Program.Middlewares.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Web/Program.Middlewares.cs
@@ -75,7 +75,7 @@ public static partial class Program
                 if (context.Request.Query.Any(q => string.Equals(q.Key, "v", StringComparison.InvariantCultureIgnoreCase)) &&
                     env.WebRootFileProvider.GetFileInfo(context.Request.Path).Exists)
                 {
-                    context.Request.Headers.AcceptEncoding = new(["zstd", "br"]);
+                    context.Request.Headers.AcceptEncoding = new(["br"]);
                     context.Response.OnStarting(async () =>
                     {
                         context.Response.GetTypedHeaders().CacheControl = new()
