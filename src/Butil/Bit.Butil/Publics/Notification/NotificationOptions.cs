8b1f911294e5ab1aaa68a9aa01aa247aa8fe4cfb
Saleh Yusefnejad <msynk@outlook.com>
2024-11-09 14:41:19 +0330

    add Notification api #9158

---
+++ src/Butil/Bit.Butil/Publics/Notification/NotificationOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Bit.Butil;
+
+public class NotificationOptions
+{
+    public string? Tag { get; set; }
+    public string? Body { get; set; }
+    public object? Data { get; set; }
+    public string? Icon { get; set; }
+    public string? Lang { get; set; }
+    public bool? Silent { get; set; }
+    public string? Badge { get; set; }
+    public bool RequireInteraction { get; set; }
+    public NotificationDirection Dir { get; set; }
+}
