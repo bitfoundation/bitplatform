a0e4487581376fa4c5e1e4c52989e61503629643
Saleh Yusefnejad <msynk@outlook.com>
2024-11-10 14:45:24 +0330

    prerelease v-8.12.0-pre-12 #9166

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
@@ -1,5 +1,5 @@
 ﻿const BitBswup = {} as any;
-BitBswup.version = window['bit-bswup version'] = '8.12.0-pre-11';
+BitBswup.version = window['bit-bswup version'] = '8.12.0-pre-12';
 
 declare const Blazor: any;
 
