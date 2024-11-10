a0e1ec5f4cf26c3f0ac541de88ec572135e4221e
Saleh Yusefnejad <msynk@outlook.com>
2024-11-10 22:44:07 +0330

    add missing IsSupported api to Butil Notification #9173

--- src/Butil/Bit.Butil/Scripts/notification.ts
+++ src/Butil/Bit.Butil/Scripts/notification.ts
@@ -2,10 +2,15 @@ var BitButil = BitButil || {};
 
 (function (butil: any) {
     butil.notification = {
+        isSupported,
         show,
         getPermission,
         requestPermission
     };
+
+    function isSupported() {
+        return ('Notification' in window);
+    }
 
     function show(title: string, options?: NotificationOptions) {
         const notification = new Notification(title, options);
