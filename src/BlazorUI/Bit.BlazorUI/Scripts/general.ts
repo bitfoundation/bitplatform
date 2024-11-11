1cc3e88995f0fc6a2c3b2e11f1c7b25436353066
Saleh Yusefnejad <msynk@outlook.com>
2024-11-11 12:01:02 +0330

    feat(prerelease): v-8.12.0-pre-13 #9180 (#9182)

--- src/BlazorUI/Bit.BlazorUI/Scripts/general.ts
+++ src/BlazorUI/Bit.BlazorUI/Scripts/general.ts
@@ -1,4 +1,4 @@
-﻿(BitBlazorUI as any).version = (window as any)['bit-blazorui version'] = '8.12.0-pre-12';
+﻿(BitBlazorUI as any).version = (window as any)['bit-blazorui version'] = '8.12.0-pre-13';
 
 interface DotNetObject {
     invokeMethod<T>(methodIdentifier: string, ...args: any[]): T;
