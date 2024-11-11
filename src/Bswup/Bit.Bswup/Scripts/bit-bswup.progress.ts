c7e92e17b3d329a43b8902eab3e1f39d40ed8f1a
Saleh Yusefnejad <msynk@outlook.com>
2024-11-11 11:49:41 +0330

    prerelease v-8.12.0-pre-13 #9180

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bswup.progress version'] = '8.12.0-pre-12';
+﻿window['bit-bswup.progress version'] = '8.12.0-pre-13';
 
 ; (function () {
     (window as any).startBswupProgress = (autoReload: boolean,
