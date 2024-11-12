4455d29b78e49c62571874a20237733cb9d7303e
Yaser Moradi <ysmoradi@outlook.com>
2024-11-12 17:26:52 +0100

    Check access token exp claim before using it in Boilerplate (#9186)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/AuthenticationManager.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/AuthenticationManager.cs
@@ -1,11 +1,15 @@
-﻿using System.Text;
-using Boilerplate.Shared.Dtos.Identity;
+﻿using Boilerplate.Shared.Dtos.Identity;
 using Boilerplate.Shared.Controllers.Identity;
 
 namespace Boilerplate.Client.Core.Services;
 
 public partial class AuthenticationManager : AuthenticationStateProvider
 {
+    /// <summary>
+    /// To prevent multiple simultaneous refresh token requests.
+    /// </summary>
+    private readonly SemaphoreSlim semaphore = new(1, maxCount: 1);
+
     [AutoInject] private Cookie cookie = default!;
     [AutoInject] private IJSRuntime jsRuntime = default!;
     [AutoInject] private IStorageService storageService = default!;
@@ -14,7 +18,6 @@ public partial class AuthenticationManager : AuthenticationStateProvider
     [AutoInject] private IPrerenderStateService prerenderStateService;
     [AutoInject] private IExceptionHandler exceptionHandler = default!;
     [AutoInject] private IIdentityController identityController = default!;
-    [AutoInject] private JsonSerializerOptions jsonSerializerOptions = default!;
 
     /// <summary>
     /// Sign in and return whether the user requires two-factor authentication.
@@ -76,45 +79,51 @@ public partial class AuthenticationManager : AuthenticationStateProvider
         try
         {
             var access_token = await prerenderStateService.GetValue(() => tokenProvider.GetAccessToken());
+            var claimsPrinciple = IAuthTokenProvider.ParseAccessToken(access_token);
 
             bool inPrerenderSession = AppPlatform.IsBlazorHybrid is false && jsRuntime.IsInitialized() is false;
 
-            if (string.IsNullOrEmpty(access_token) && inPrerenderSession is false)
+            if (claimsPrinciple.IsAuthenticated() is false && inPrerenderSession is false)
             {
-                string? refresh_token = await storageService.GetItem("refresh_token");
-
-                if (string.IsNullOrEmpty(refresh_token) is false)
+                try
                 {
-                    // We refresh the access_token to ensure a seamless user experience, preventing unnecessary 'NotAuthorized' page redirects and improving overall UX.
-                    // This method is triggered after 401 and 403 server responses in AuthDelegationHandler,
-                    // as well as when accessing pages without the required permissions in NotAuthorizedPage, ensuring that any recent claims granted to the user are promptly reflected.
+                    await semaphore.WaitAsync();
+                    claimsPrinciple = IAuthTokenProvider.ParseAccessToken(await tokenProvider.GetAccessToken());
+                    if (claimsPrinciple.IsAuthenticated() is false) // Check again after acquiring the lock.
+                    {
+                        string? refresh_token = await storageService.GetItem("refresh_token");
 
-                    try
-                    {
-                        var refreshTokenResponse = await identityController.Refresh(new() { RefreshToken = refresh_token }, CancellationToken.None);
-                        await StoreTokens(refreshTokenResponse!);
-                        access_token = refreshTokenResponse!.AccessToken;
+                        if (string.IsNullOrEmpty(refresh_token) is false)
+                        {
+                            // We refresh the access_token to ensure a seamless user experience, preventing unnecessary 'NotAuthorized' page redirects and improving overall UX.
+                            // This method is triggered after 401 and 403 server responses in AuthDelegationHandler,
+                            // as well as when accessing pages without the required permissions in NotAuthorizedPage, ensuring that any recent claims granted to the user are promptly reflected.
+
+                            try
+                            {
+                                var refreshTokenResponse = await identityController.Refresh(new() { RefreshToken = refresh_token }, CancellationToken.None);
+                                await StoreTokens(refreshTokenResponse!);
+                                claimsPrinciple = IAuthTokenProvider.ParseAccessToken(refreshTokenResponse!.AccessToken);
+                            }
+                            catch (UnauthorizedException) // refresh_token is either invalid or expired.
+                            {
+                                await storageService.RemoveItem("refresh_token");
+                            }
+                        }
                     }
-                    catch (UnauthorizedException) // refresh_token is either invalid or expired.
-                    {
-                        await storageService.RemoveItem("refresh_token");
-                    }
+                }
+                finally
+                {
+                    semaphore.Release();
                 }
             }
 
-            if (string.IsNullOrEmpty(access_token))
-            {
-                return NotSignedIn();
-            }
-
-            var identity = new ClaimsIdentity(claims: ParseTokenClaims(access_token), authenticationType: "Bearer", nameType: "name", roleType: "role");
-
-            return new AuthenticationState(new ClaimsPrincipal(identity));
+            return new AuthenticationState(claimsPrinciple);
         }
         catch (Exception exp)
         {
             exceptionHandler.Handle(exp); // Do not throw exceptions in GetAuthenticationStateAsync. This will fault CascadingAuthenticationState's state unless NotifyAuthenticationStateChanged is called again.
-            return NotSignedIn();
+            return new AuthenticationState(IAuthTokenProvider.Anonymous());
         }
     }
 
@@ -140,68 +149,5 @@ public partial class AuthenticationManager : AuthenticationStateProvider
                 Secure = AppEnvironment.IsDev() is false
             });
         }
-    }
-
-    private static AuthenticationState NotSignedIn()
-    {
-        return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
-    }
-
-    private IEnumerable<Claim> ParseTokenClaims(string access_token)
-    {
-        var parsedClaims = ParseJwt(access_token);
-
-        var claims = new List<Claim>();
-        foreach (var keyValue in parsedClaims)
-        {
-            if (keyValue.Value.ValueKind == JsonValueKind.Array)
-            {
-                foreach (var element in keyValue.Value.EnumerateArray())
-                {
-                    claims.Add(new Claim(keyValue.Key, element.ToString() ?? string.Empty));
-                }
-            }
-            else
-            {
-                claims.Add(new Claim(keyValue.Key, keyValue.Value.ToString() ?? string.Empty));
-            }
-        }
-
-        return claims;
-    }
-
-    private Dictionary<string, JsonElement> ParseJwt(string access_token)
-    {
-        // Split the token to get the payload
-        string base64UrlPayload = access_token.Split('.')[1];
-
-        // Convert the payload from Base64Url format to Base64
-        string base64Payload = ConvertBase64UrlToBase64(base64UrlPayload);
-
-        // Decode the Base64 string to get a JSON string
-        string jsonPayload = Encoding.UTF8.GetString(Convert.FromBase64String(base64Payload));
-
-        // Deserialize the JSON string to a dictionary
-        var claims = JsonSerializer.Deserialize(jsonPayload, jsonSerializerOptions.GetTypeInfo<Dictionary<string, JsonElement>>())!;
-
-        return claims;
-    }
-
-    private static string ConvertBase64UrlToBase64(string base64Url)
-    {
-        base64Url = base64Url.Replace('-', '+').Replace('_', '/');
-
-        // Adjust base64Url string length for padding
-        switch (base64Url.Length % 4)
-        {
-            case 2:
-                base64Url += "==";
-                break;
-            case 3:
-                base64Url += "=";
-                break;
-        }
-
-        return base64Url;
     }
 }
