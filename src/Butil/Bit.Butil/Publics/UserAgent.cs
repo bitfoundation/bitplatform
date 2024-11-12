bfaf07d9cd5473b745693713510bc693538d5e29
Saleh Yusefnejad <msynk@outlook.com>
2024-11-12 23:22:23 +0330

    feat(butil): add UserAgent api to Butil #9204 (#9208)

---
+++ src/Butil/Bit.Butil/Publics/UserAgent.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Bit.Butil.Publics.UserAgent;
+using Microsoft.JSInterop;
+
+namespace Bit.Butil;
+
+/// <summary>
+/// This service is used to detect the user agent information such as the Operating System, browser or web-view, versions and properties.
+/// </summary>
+public class UserAgent(IJSRuntime js)
+{
+    /// <summary>
+    /// Extracts the user agent properties from the browser or web-view.
+    /// </summary>
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(UserAgentProperties))]
+    public async ValueTask<UserAgentProperties> Extract()
+    {
+        return await js.InvokeAsync<UserAgentProperties>("BitButil.userAgent.extract");
+    }
+}
