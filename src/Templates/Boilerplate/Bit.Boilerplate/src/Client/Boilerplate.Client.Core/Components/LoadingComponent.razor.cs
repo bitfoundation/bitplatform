465ff518913e41c31c6fe5932df96162e4025615
Saleh Yusefnejad <msynk@outlook.com>
2024-11-13 23:51:37 +0330

    add zindex

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/LoadingComponent.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/LoadingComponent.razor.cs
@@ -1,8 +1,21 @@
-﻿namespace Boilerplate.Client.Core.Components;
+﻿
+namespace Boilerplate.Client.Core.Components;
 
 public partial class LoadingComponent
 {
     [Parameter] public string Color { get; set; } = "#123456";
 
     [Parameter] public bool FullScreen { get; set; }
+
+
+    private int zIndex = -1;
+    protected override Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            zIndex = 999999;
+            StateHasChanged();
+        }
+        return base.OnAfterRenderAsync(firstRender);
+    }
 }
