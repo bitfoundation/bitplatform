edc2305933d4d8d1ebd0fbfa0a29966fc5fbbfb7
Yaser Moradi <ysmoradi@outlook.com>
2024-11-15 12:09:33 +0100

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Shared/Dtos/AppJsonContext.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Shared/Dtos/AppJsonContext.cs
@@ -8,8 +8,8 @@ using Boilerplate.Shared.Dtos.Products;
 //#endif
 //#if (notification == true)
 using Boilerplate.Shared.Dtos.PushNotification;
-using Boilerplate.Shared.Dtos.Home;
 //#endif
+using Boilerplate.Shared.Dtos.Home;
 
 namespace Boilerplate.Shared.Dtos;
 
