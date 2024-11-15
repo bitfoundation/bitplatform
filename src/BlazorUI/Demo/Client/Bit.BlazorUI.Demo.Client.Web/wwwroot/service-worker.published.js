36fce5af1681960b4ae6952e8dab284314acaedb
Saleh Yusefnejad <msynk@outlook.com>
2024-11-16 01:07:45 +0330

    feat(prerelease): v-9.0.0-pre-01 #9216 (#9244)

--- src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Web/wwwroot/service-worker.published.js
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Web/wwwroot/service-worker.published.js
@@ -1,4 +1,4 @@
-// bit version: 8.12.0
+// bit version: 9.0.0-pre-01
 // https://github.com/bitfoundation/bitplatform/tree/develop/src/Bswup
 
 self.assetsInclude = [];
--- src/Butil/Bit.Butil/Scripts/butil.ts
+++ src/Butil/Bit.Butil/Scripts/butil.ts
@@ -1,2 +1,2 @@
 var BitButil = BitButil || {};
-BitButil.version = window['bit-butil version'] = '8.12.0';
+BitButil.version = window['bit-butil version'] = '9.0.0-pre-01';
