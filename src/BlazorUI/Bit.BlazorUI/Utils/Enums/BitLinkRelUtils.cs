fec4a78b1faeccacf90faee2f97c20bf11dc9ce5
Saleh Yusefnejad <msynk@outlook.com>
2024-11-16 13:33:55 +0330

    improve AllRels

--- src/BlazorUI/Bit.BlazorUI/Utils/Enums/BitLinkRelUtils.cs
+++ src/BlazorUI/Bit.BlazorUI/Utils/Enums/BitLinkRelUtils.cs
@@ -2,7 +2,7 @@
 
 internal class BitLinkRelUtils
 {
-    internal static IEnumerable<BitLinkRel> AllRels = Enum.GetValues<BitLinkRel>().Cast<BitLinkRel>();
+    internal static BitLinkRel[] AllRels = Enum.GetValues<BitLinkRel>();
 
     internal static string GetRels(BitLinkRel rel)
     {
