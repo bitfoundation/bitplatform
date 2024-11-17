f684286a85bb760a263803d2300c253059293d73
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-11-17 19:26:50 +0330

    Add tests for Pre-Rendering + WaitForHydrationToComplete mechanism

---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Tests/PageTests/BlazorWebAssemblyPreRenderingTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Boilerplate.Tests.PageTests.BlazorWebAssembly.PreRendering;
+
+[TestClass]
+public partial class IdentityPagesTests : BlazorWebAssembly.IdentityPagesTests
+{
+    public override bool PreRenderEnabled => true;
+}
+
+[TestClass]
+public partial class LocalizationTests : BlazorWebAssembly.LocalizationTests
+{
+    public override bool PreRenderEnabled => true;
+}
