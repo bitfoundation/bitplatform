502ec46aed791b847fc9c20e20b625920542a4c3
Saleh Yusefnejad <msynk@outlook.com>
2024-11-18 21:34:23 +0330

    feat(release): v-9.0.0 #9217 (#9279)

--- src/Besql/Bit.Besql/wwwroot/bit-besql.js
+++ src/Besql/Bit.Besql/wwwroot/bit-besql.js
@@ -1,5 +1,5 @@
 var BitBesql = BitBesql || {};
-BitBesql.version = window['bit-besql version'] = '9.0.0-pre-02';
+BitBesql.version = window['bit-besql version'] = '9.0.0';
 
 async function synchronizeDbWithCache(file) {
 
--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bswup.progress version'] = '9.0.0-pre-02';
+﻿window['bit-bswup.progress version'] = '9.0.0';
 
 ; (function () {
     (window as any).startBswupProgress = (autoReload: boolean,
