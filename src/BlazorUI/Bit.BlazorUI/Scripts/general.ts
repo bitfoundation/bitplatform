3c0aabf2586a3124e2d927859e3d34d9305829db
Saleh Yusefnejad <msynk@outlook.com>
2024-11-20 15:33:27 +0330

    feat(release): v-9.0.1 #9284 (#9296)

--- src/BlazorUI/Bit.BlazorUI/Scripts/general.ts
+++ src/BlazorUI/Bit.BlazorUI/Scripts/general.ts
@@ -1,4 +1,4 @@
-﻿(BitBlazorUI as any).version = (window as any)['bit-blazorui version'] = '9.0.0';
+﻿(BitBlazorUI as any).version = (window as any)['bit-blazorui version'] = '9.0.1';
 
 interface DotNetObject {
     invokeMethod<T>(methodIdentifier: string, ...args: any[]): T;
--- src/Bswup/Bit.Bswup.Demo/wwwroot/service-worker.js
+++ src/Bswup/Bit.Bswup.Demo/wwwroot/service-worker.js
@@ -1,4 +1,4 @@
-// bit version: 9.0.0
+// bit version: 9.0.1
 
 self.assetsExclude = [/\.scp\.css$/, /weather\.json$/];
 self.caseInsensitiveUrl = true;
