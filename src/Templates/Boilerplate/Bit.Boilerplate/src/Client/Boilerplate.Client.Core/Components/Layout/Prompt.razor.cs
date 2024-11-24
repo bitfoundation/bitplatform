bedd9ef04979d996c8ce7c179be8c555f95a284d
Saleh Yusefnejad <msynk@outlook.com>
2024-11-24 18:18:55 +0330

    feat(templates): Add ModalService to Boilerplate #9323 (#9324)

---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/Prompt.razor.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Boilerplate.Client.Core.Components.Layout;
+
+public partial class Prompt
+{
+    private string? value;
+
+    [Parameter] public string? Body { get; set; }
+    [Parameter] public Action<string?>? OnOk { get; set; }
+
+    private void OnOkClick()
+    {
+        OnOk?.Invoke(value);
+    }
+}
--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Extensions/IClientCoreServiceCollectionExtensions.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Extensions/IClientCoreServiceCollectionExtensions.cs
@@ -39,7 +39,9 @@ public static partial class IClientCoreServiceCollectionExtensions
         // Defining them as singletons would result in them being shared across all users in Blazor Server and during pre-rendering.
         // To address this, we use the AddSessioned extension method.
         // AddSessioned applies AddSingleton in BlazorHybrid and AddScoped in Blazor WebAssembly and Blazor Server, ensuring correct service lifetimes for each environment.
+        services.AddSessioned<ModalService>();
         services.AddSessioned<PubSubService>();
+        services.AddSessioned<PromptService>();
         services.AddSessioned<SnackBarService>();
         services.AddSessioned<MessageBoxService>();
         services.AddSessioned<ILocalHttpServer, NoopLocalHttpServer>();
