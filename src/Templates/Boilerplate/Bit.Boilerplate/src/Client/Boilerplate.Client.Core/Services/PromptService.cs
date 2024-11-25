4c97d238b10fed6b805e8fe0adcda77ab0acb178
ysmoradi <ysmoradi@outlook.com>
2024-11-25 14:06:19 +0100

    Add nonInterrupting param and OTP input support
    Introduced a `nonInterrupting` parameter to the exception handling mechanism, allowing for different handling strategies such as showing a snackbar instead of a message box. Updated `IExceptionHandler` interface and various handler implementations to support this parameter.
    
    Enhanced the `Prompt` component to support an OTP input field, controlled by the new `OtpInput` parameter. Updated `PromptService` to pass this parameter to the `Prompt` component.
    
    Styled `EditForm` elements in `ChangeEmailSection.razor` and `ChangePhoneNumberSection.razor` with a `max-width` class.
    
    Simplified logic in `SessionsSection.razor` for setting `ImageInitials` and `CoinColor`.
    
    Updated `AuthManager` to use the `nonInterrupting` parameter for exception handling and to support OTP input in the prompt service.

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/PromptService.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/PromptService.cs
@@ -4,12 +4,13 @@ public partial class PromptService
 {
     [AutoInject] private ModalService modalService = default!;
 
-    public Task<string?> Show(string message, string title = "")
+    public Task<string?> Show(string message, string title = "", bool otpInput = false)
     {
         TaskCompletionSource<string?> tcs = new();
         Dictionary<string, object> parameters = new()
         {
             { nameof(Prompt.Body), message },
+            { nameof(Prompt.OtpInput), otpInput },
             { nameof(Prompt.OnOk), (string value) => { tcs.SetResult(value); modalService.Close(); } }
         };
         modalService.Show<Prompt>(parameters, title).ContinueWith(async task =>
