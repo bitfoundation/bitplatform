c962fe9669ee3d0ae904f73774702af84bb59fe1
Saleh Yusefnejad <msynk@outlook.com>
2024-11-28 16:10:33 +0330

    1st commit

---
+++ src/BlazorUI/Bit.BlazorUI.Extras/Components/NavPanel/BitNavPanel.razor.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Bit.BlazorUI;
+
+public partial class BitNavPanel : IDisposable
+{
+    private bool _disposed;
+
+    public void Dispose()
+    {
+        if (_disposed) return;
+
+        _disposed = true;
+    }
+}
---
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Extras/NavPanel/BitNavPanelDemo.razor.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Bit.BlazorUI.Demo.Client.Core.Pages.Components.Extras.NavPanel;
+
+public partial class BitNavPanelDemo
+{
+    private readonly List<ComponentParameter> componentParameters =
+    [
+         new()
+         {
+            Name = "CanvasClass",
+            Type = "string?",
+            DefaultValue = "null",
+            Description = "The CSS class of the canvas element(s).",
+         },
+    ];
+
+    private readonly List<ComponentSubClass> componentSubClasses =
+    [
+        new()
+         {
+            Id = "pdf-reader-config",
+            Title = "BitPdfReaderConfig",
+            Parameters=
+            [
+                new()
+                {
+                    Name = "Id",
+                    Type = "string",
+                    DefaultValue = "Guid.NewGuid().ToString()",
+                    Description = "The id of the pdf reader instance and its canvas element(s).",
+                },
+            ]
+        }
+    ];
+
+
+    
+    private readonly string example1RazorCode = @"
+";
+    private readonly string example1CsharpCode = @"
+";
+}
