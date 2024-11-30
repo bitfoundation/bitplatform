3f79cc16a045d0ebad86de03e15f2e9eaeac6a6c
Saleh Yusefnejad <msynk@outlook.com>
2024-12-01 00:10:41 +0330

    add singleton settings to service collection extension

--- src/BlazorUI/Bit.BlazorUI.Extras/IServiceCollectionExtensions.cs
+++ src/BlazorUI/Bit.BlazorUI.Extras/IServiceCollectionExtensions.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Bit.BlazorUI;
 
 public static class IServiceCollectionExtensions
 {
-    public static IServiceCollection AddBitBlazorUIExtrasServices(this IServiceCollection services)
+    public static IServiceCollection AddBitBlazorUIExtrasServices(this IServiceCollection services, bool singleton = false)
     {
-        services.AddScoped<BitModalService>();
+        if (singleton)
+        {
+            services.TryAddSingleton<BitModalService>();
+        }
+        else
+        {
+            services.TryAddScoped<BitModalService>();
+        }
 
         return services;
     }
