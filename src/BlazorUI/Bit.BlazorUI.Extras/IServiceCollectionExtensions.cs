ef6c0652ba7484efefe31c5d473cc5fc714bea4d
Saleh Yusefnejad <msynk@outlook.com>
2024-12-02 20:15:50 +0330

    add comments

--- src/BlazorUI/Bit.BlazorUI.Extras/IServiceCollectionExtensions.cs
+++ src/BlazorUI/Bit.BlazorUI.Extras/IServiceCollectionExtensions.cs
@@ -5,6 +5,12 @@ namespace Bit.BlazorUI;
 
 public static class IServiceCollectionExtensions
 {
+    /// <summary>
+    /// Registers required services of the Extras package of the BitBlazorUI components.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="trySingleton">Tries to register the services as singleton, but only for the services can be singleton (e.g. the services that do not use IJSRuntime).</param>
+    /// <returns></returns>
     public static IServiceCollection AddBitBlazorUIExtrasServices(this IServiceCollection services, bool trySingleton = false)
     {
         if (trySingleton)
