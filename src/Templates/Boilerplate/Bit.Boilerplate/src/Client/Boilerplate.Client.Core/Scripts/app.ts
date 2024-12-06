fabc5b370dfdf9cd136dce5f3187a4e2bfbd1abd
Saleh Yusefnejad <msynk@outlook.com>
2024-12-06 08:45:14 +0330

    improve app.ts

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Scripts/app.ts
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Scripts/app.ts
@@ -1,20 +1,25 @@
 ﻿//+:cnd:noEmit
+
+interface DotNetObject {
+    invokeMethod<T>(methodIdentifier: string, ...args: any[]): T;
+    invokeMethodAsync<T>(methodIdentifier: string, ...args: any[]): Promise<T>;
+    dispose(): void;
+}
+
 class App {
+    // For additional details, see the JsBridge.cs file.
     private static jsBridgeObj: DotNetObject;
 
     public static registerJsBridge(dotnetObj: DotNetObject) {
-        // For additional details, see the JsBridge.cs file.
         App.jsBridgeObj = dotnetObj;
-        window.addEventListener('message', e => {
-            // Enable publishing messages from JavaScript's `window.postMessage` to the C# `PubSubService`.
-            if (e.data.key === 'PUBLISH_MESSAGE') {
-                App.jsBridgeObj?.invokeMethodAsync('PublishMessage', e.data.message, e.data.payload);
-            }
-        });
     }
 
     public static showDiagnostic() {
         return App.jsBridgeObj?.invokeMethodAsync('ShowDiagnostic');
+    }
+
+    public static publishMessage(message: string, payload: any) {
+        return App.jsBridgeObj?.invokeMethodAsync('PublishMessage', message, payload);
     }
 
     public static applyBodyElementClasses(cssClasses: string[], cssVariables: any): void {
@@ -54,45 +59,59 @@ class App {
         if (pushManager == null) return null;
 
         let subscription = await pushManager.getSubscription();
+
         if (subscription == null) {
             subscription = await pushManager.subscribe({
                 userVisibleOnly: true,
                 applicationServerKey: vapidPublicKey
             });
         }
+
         const pushChannel = subscription.toJSON();
         const p256dh = pushChannel.keys!['p256dh'];
         const auth = pushChannel.keys!['auth'];
-        return { deviceId: `${p256dh}-${auth}`, platform: 'browser', p256dh: p256dh, auth: auth, endpoint: pushChannel.endpoint };
+
+        return {
+            deviceId: `${p256dh}-${auth}`,
+            platform: 'browser',
+            p256dh: p256dh,
+            auth: auth,
+            endpoint: pushChannel.endpoint
+        };
     };
     //#endif
 }
 
-declare class BitTheme { static init(options: any): void; };
+window.addEventListener('message', handleMessage);
+window.addEventListener('load', handleLoad);
+window.addEventListener('resize', setCssWindowSizes);
 
-interface DotNetObject {
-    invokeMethod<T>(methodIdentifier: string, ...args: any[]): T;
-    invokeMethodAsync<T>(methodIdentifier: string, ...args: any[]): Promise<T>;
-    dispose(): void;
+function handleMessage(e: MessageEvent) {
+    // Enable publishing messages from JavaScript's `window.postMessage` to the C# `PubSubService`.
+    if (e.data.key === 'PUBLISH_MESSAGE') {
+        App.publishMessage(e.data.message, e.data.payload);
+    }
 }
 
-window.addEventListener('load', () => {
+function handleLoad() {
     setCssWindowSizes();
+
     if (window.opener != null) {
         // The IExternalNavigationService is responsible for opening pages in a new window,
         // such as during social sign-in flows. Once the external navigation is complete,
         // and the user is redirected back to the newly opened window,
         // the following code ensures that the original window is notified of where it should navigate next.
         window.opener.postMessage({ key: 'PUBLISH_MESSAGE', message: 'NAVIGATE_TO', payload: window.location.href });
-        window.close();
+        setTimeout(() => window.close(), 100);
     }
-});
-window.addEventListener('resize', setCssWindowSizes);
+}
 
 function setCssWindowSizes() {
     document.documentElement.style.setProperty('--win-width', `${window.innerWidth}px`);
     document.documentElement.style.setProperty('--win-height', `${window.innerHeight}px`);
 }
+
+declare class BitTheme { static init(options: any): void; };
 
 BitTheme.init({
     system: true,
