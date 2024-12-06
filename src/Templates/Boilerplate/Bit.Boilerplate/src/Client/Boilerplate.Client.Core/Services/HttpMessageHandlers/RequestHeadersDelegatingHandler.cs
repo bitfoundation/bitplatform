1b876ddf854198ca7f6c52d83ff2cf6afeff633e
ysmoradi <ysmoradi@outlook.com>
2024-12-06 17:18:20 +0100

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/HttpMessageHandlers/RequestHeadersDelegatingHandler.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/HttpMessageHandlers/RequestHeadersDelegatingHandler.cs
@@ -3,13 +3,17 @@ using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 namespace Boilerplate.Client.Core.Services.HttpMessageHandlers;
 
-public partial class RequestHeadersDelegatingHandler(HttpMessageHandler handler)
+public partial class RequestHeadersDelegatingHandler(ITelemetryContext telemetryContext, HttpMessageHandler handler)
     : DelegatingHandler(handler)
 {
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         request.SetBrowserRequestCredentials(BrowserRequestCredentials.Omit);
         request.SetBrowserResponseStreamingEnabled(true);
+        if (request.Headers.UserAgent.Any() is false)
+        {
+            var a = request.Headers.UserAgent.TryParseAdd(telemetryContext.UserAgent ?? telemetryContext.Platform);
+        }
 
         if (CultureInfoManager.MultilingualEnabled && string.IsNullOrEmpty(CultureInfo.CurrentUICulture.Name) is false)
         {
