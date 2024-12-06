18ead050271622a24fa5a1d24d18ec9e6b159f6b
Yaser Moradi <ysmoradi@outlook.com>
2024-12-06 07:44:24 +0100

    fix(besql): resolve Bit Besql memory leak issues #9403 (#9404)

---
+++ src/Besql/Bit.Besql/BesqlDbContextInterceptor.cs
@@ -0,0 +1,62 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Bit.Besql;
+
+public class BesqlDbContextInterceptor(IBesqlStorage storage) : IDbCommandInterceptor, ISingletonInterceptor
+{
+    public async ValueTask<DbDataReader> ReaderExecutedAsync(
+        DbCommand command,
+        CommandExecutedEventData eventData,
+        DbDataReader result,
+        CancellationToken cancellationToken = default)
+    {
+        if (IsTargetedCommand(command.CommandText))
+        {
+            _ = Sync(eventData.Context!.Database.GetDbConnection().DataSource).ConfigureAwait(false);
+        }
+
+        return result;
+    }
+
+    public async ValueTask<int> NonQueryExecutedAsync(
+        DbCommand command,
+        CommandExecutedEventData eventData,
+        int result,
+        CancellationToken cancellationToken)
+    {
+        if (IsTargetedCommand(command.CommandText))
+        {
+            _ = Sync(eventData.Context!.Database.GetDbConnection().DataSource).ConfigureAwait(false);
+        }
+
+        return result;
+    }
+
+    public async ValueTask<object?> ScalarExecutedAsync(
+        DbCommand command,
+        CommandExecutedEventData eventData,
+        object? result,
+        CancellationToken cancellationToken = default)
+    {
+        if (IsTargetedCommand(command.CommandText))
+        {
+            _ = Sync(eventData.Context!.Database.GetDbConnection().DataSource).ConfigureAwait(false);
+        }
+        return result;
+    }
+
+    private bool IsTargetedCommand(string sql)
+    {
+        var keywords = new[] { "INSERT", "UPDATE", "DELETE", "CREATE", "ALTER", "DROP" };
+        return keywords.Any(k => sql.Contains(k, StringComparison.OrdinalIgnoreCase));
+    }
+
+    private async Task Sync(string dataSource)
+    {
+        var fileName = dataSource.Trim('/');
+        await Task.Yield();
+        await storage.Persist(fileName).ConfigureAwait(false);
+    }
+}
