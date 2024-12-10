4c1e1617eac11f59aabf79cec223a0f5fbceda2e
Saleh Yusefnejad <msynk@outlook.com>
2024-12-10 16:38:01 +0330

    fix review comments

--- src/BlazorUI/Bit.BlazorUI/Components/Surfaces/Panel/BitPanel.ts
+++ src/BlazorUI/Bit.BlazorUI/Components/Surfaces/Panel/BitPanel.ts
@@ -33,7 +33,7 @@
             };
 
             const onMove = async (e: TouchEvent | PointerEvent): Promise<void> => {
-                if (startX === -1 && startY === -1) return;
+                if (startX === -1 || startY === -1) return;
 
                 diffX = getX(e) - startX;
                 diffY = getY(e) - startY;
@@ -79,6 +79,8 @@
             };
 
             const onEnd = async (e: TouchEvent | PointerEvent): Promise<void> => {
+                if (startX === -1 || startY === -1) return;
+
                 startX = startY = -1;
 
                 element.style.transitionDuration = '';
