33e8aa1191a83b4f57951960fd89c176fc6b94eb
Saleh Yusefnejad <msynk@outlook.com>
2024-12-12 21:03:29 +0330

    fix stuff

--- src/BlazorUI/Bit.BlazorUI.Extras/Components/ProPanel/BitProPanelClassStyles.cs
+++ src/BlazorUI/Bit.BlazorUI.Extras/Components/ProPanel/BitProPanelClassStyles.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Bit.BlazorUI;
+﻿namespace Bit.BlazorUI;
 
 public class BitProPanelClassStyles : BitPanelClassStyles
 {
--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/HomePage.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/HomePage.razor.cs
@@ -19,7 +19,6 @@ public partial class HomePage
 
     protected override async Task OnInitAsync()
     {
-        var name = typeof(BitButton).AssemblyQualifiedName;
         await base.OnInitAsync();
 
         // If required, you should typically manage the authorization header for external APIs in **AuthDelegatingHandler.cs**
