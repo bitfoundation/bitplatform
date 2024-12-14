85a6c4acb3c98fc2becc15d30e5a614a78a8c93a
Saleh Yusefnejad <msynk@outlook.com>
2024-12-14 23:30:59 +0330

    release v-9.1.0 #9483

--- src/Besql/Bit.Besql/wwwroot/bit-besql.js
+++ src/Besql/Bit.Besql/wwwroot/bit-besql.js
@@ -1,5 +1,5 @@
 ﻿var BitBesql = window.BitBesql || {};
-BitBesql.version = window['bit-besql version'] = '9.1.0-pre-13';
+BitBesql.version = window['bit-besql version'] = '9.1.0';
 
 BitBesql.init = async function init(fileName) {
     const sqliteFilePath = `/${fileName}`;
--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw.ts
@@ -1,4 +1,4 @@
-﻿self['bit-bswup.sw version'] = '9.1.0-pre-13';
+﻿self['bit-bswup.sw version'] = '9.1.0';
 
 interface Window {
     clients: any
