0699bb2949b696b7a8e9003edeef1ad3d82bf5b6
Saleh Yusefnejad <msynk@outlook.com>
2024-12-15 20:18:46 +0330

    release v-9.1.1 #9497

--- src/Bswup/Bit.Bswup.Demo/wwwroot/service-worker.published.js
+++ src/Bswup/Bit.Bswup.Demo/wwwroot/service-worker.published.js
@@ -1,4 +1,4 @@
-// bit version: 9.1.1-pre-01
+// bit version: 9.1.1
 
 self.assetsExclude = [/\.scp\.css$/, /weather\.json$/];
 self.caseInsensitiveUrl = true;
--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
@@ -1,5 +1,5 @@
 ﻿const BitBswup = {} as any;
-BitBswup.version = window['bit-bswup version'] = '9.1.1-pre-01';
+BitBswup.version = window['bit-bswup version'] = '9.1.1';
 
 declare const Blazor: any;
 
