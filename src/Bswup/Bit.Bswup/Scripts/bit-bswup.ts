9a3fcb42a0c2a93255055b46b7fc74a9a265af0a
Saleh Yusefnejad <msynk@outlook.com>
2025-01-03 15:49:17 +0330

    feat(prerelease): v-9.2.1-pre-01 #9608 (#9609)

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
@@ -1,5 +1,5 @@
 ﻿const BitBswup = {} as any;
-BitBswup.version = window['bit-bswup version'] = '9.2.0';
+BitBswup.version = window['bit-bswup version'] = '9.2.1-pre-01';
 
 declare const Blazor: any;
 
