1a03f0fe6e23ee7a05b836c41e8a1f022430bbdc
Saleh Yusefnejad <msynk@outlook.com>
2025-01-17 22:54:28 +0330

    complete implementation

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/AppErrorBoundary.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/AppErrorBoundary.razor.cs
@@ -1,32 +1,22 @@
-﻿//-:cnd:noEmit
+﻿namespace Boilerplate.Client.Core.Components.Layout;
 
-namespace Boilerplate.Client.Core.Components.Layout;
-
-/// <summary>
-/// https://learn.microsoft.com/en-us/aspnet/core/blazor/fundamentals/handle-errors
-/// </summary>
 public partial class AppErrorBoundary
 {
-    private bool showException;
+    private BitErrorBoundary boundaryRef = default!;
+
+
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
 
     [AutoInject] private PubSubService pubSubService = default!;
     [AutoInject] private IExceptionHandler exceptionHandler = default!;
     [AutoInject] private NavigationManager navigationManager = default!;
     [AutoInject] private IStringLocalizer<AppStrings> localizer = default!;
 
-    protected override void OnInit()
-    {
-        showException = AppEnvironment.IsDev();
-    }
-
-    private async Task OnErrorAsync(Exception exception)
-    {
-        exceptionHandler.Handle(exception);
-    }
 
     private async Task ShowDiagnostic()
     {
-        //Recover();
+        boundaryRef.Recover();
         await Task.Yield();
         pubSubService.Publish(ClientPubSubMessages.SHOW_DIAGNOSTIC_MODAL);
     }
