f200dab2210467fc35786375da1a870f731b2cbd
Yaser Moradi <ysmoradi@outlook.com>
2025-01-23 14:49:39 +0100

    feat(besql): add dbcontext initialization hook #9720 (#9721)

--- src/Besql/Bit.Besql/BesqlPooledDbContextFactory.cs
+++ src/Besql/Bit.Besql/BesqlPooledDbContextFactory.cs
@@ -5,18 +5,18 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Bit.Besql;
 
-public class BesqlPooledDbContextFactory<TDbContext> : PooledDbContextFactory<TDbContext>
+public class BesqlPooledDbContextFactory<TDbContext> : PooledDbContextFactoryBase<TDbContext>
     where TDbContext : DbContext
 {
     private readonly string _fileName;
     private readonly IBesqlStorage _storage;
     private readonly string _connectionString;
-    private readonly TaskCompletionSource _initTcs = new();
 
     public BesqlPooledDbContextFactory(
         IBesqlStorage storage,
-        DbContextOptions<TDbContext> options)
-        : base(options)
+        DbContextOptions<TDbContext> options,
+        Func<IServiceProvider, TDbContext, Task> dbContextInitializer)
+        : base(options, dbContextInitializer)
     {
         _connectionString = options.Extensions
                 .OfType<RelationalOptionsExtension>()
@@ -28,33 +28,17 @@ public class BesqlPooledDbContextFactory<TDbContext> : PooledDbContextFactory<TD
         }["Data Source"].ToString()!.Trim('/');
 
         _storage = storage;
-        _ = InitAsync();
     }
 
-    public override async Task<TDbContext> CreateDbContextAsync(CancellationToken cancellationToken = default)
+    protected override async Task InitializeDbContext()
     {
-        await _initTcs.Task.ConfigureAwait(false);
+        await _storage.Init(_fileName).ConfigureAwait(false);
+        await using var connection = new SqliteConnection(_connectionString);
+        await connection.OpenAsync().ConfigureAwait(false);
+        await using var command = connection.CreateCommand();
+        command.CommandText = "PRAGMA synchronous = FULL;";
+        await command.ExecuteNonQueryAsync().ConfigureAwait(false);
 
-        var ctx = await base.CreateDbContextAsync(cancellationToken).ConfigureAwait(false);
-
-        return ctx;
-    }
-
-    private async Task InitAsync()
-    {
-        try
-        {
-            await _storage.Init(_fileName).ConfigureAwait(false);
-            await using var connection = new SqliteConnection(_connectionString);
-            await connection.OpenAsync().ConfigureAwait(false);
-            await using var command = connection.CreateCommand();
-            command.CommandText = "PRAGMA synchronous = FULL;";
-            await command.ExecuteNonQueryAsync().ConfigureAwait(false);
-            _initTcs.SetResult();
-        }
-        catch (Exception exp)
-        {
-            _initTcs.SetException(exp);
-        }
+        await base.InitializeDbContext().ConfigureAwait(false);
     }
 }
---
+++ src/Besql/Bit.Besql/PooledDbContextFactoryBase.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Bit.Besql;
+
+public class PooledDbContextFactoryBase<TDbContext>(DbContextOptions<TDbContext> options,
+        Func<IServiceProvider, TDbContext, Task> dbContextInitializer) : PooledDbContextFactory<TDbContext>(options)
+    where TDbContext : DbContext
+{
+    private TaskCompletionSource? dbContextInitializerTcs;
+
+    public override async Task<TDbContext> CreateDbContextAsync(CancellationToken cancellationToken = default)
+    {
+        if (dbContextInitializerTcs is null)
+        {
+            await StartRunningDbContextInitializer();
+        }
+
+        await dbContextInitializerTcs!.Task.ConfigureAwait(false);
+
+        return await base.CreateDbContextAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    private async Task StartRunningDbContextInitializer()
+    {
+        if (dbContextInitializerTcs is not null)
+            return;
+
+        dbContextInitializerTcs = new();
+
+        try
+        {
+            await InitializeDbContext().ConfigureAwait(false);
+            dbContextInitializerTcs.SetResult();
+        }
+        catch (Exception ex)
+        {
+            dbContextInitializerTcs.SetException(ex);
+        }
+    }
+
+    protected virtual async Task InitializeDbContext()
+    {
+        if (dbContextInitializer is not null)
+        {
+            await using var dbContext = await base.CreateDbContextAsync().ConfigureAwait(false);
+            await dbContextInitializer(dbContext.GetService<IServiceProvider>(), dbContext).ConfigureAwait(false);
+        }
+    }
+}
