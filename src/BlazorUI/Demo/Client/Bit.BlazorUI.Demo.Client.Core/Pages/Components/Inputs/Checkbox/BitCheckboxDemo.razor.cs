52f22404220a92bc269e957de83016ab00db2461
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2025-02-06 11:23:07 +0330

    feat(blazorui): add Size and Color parameters to BitCheckbox #9746 (#9769)

--- src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Inputs/Checkbox/BitCheckboxDemo.razor.cs
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Inputs/Checkbox/BitCheckboxDemo.razor.cs
@@ -64,6 +64,15 @@ public partial class BitCheckboxDemo
         },
         new()
         {
+            Name = "Color",
+            Type = "BitColor?",
+            DefaultValue = "null",
+            Description = "The general color of the checkbox.",
+            LinkType = LinkType.Link,
+            Href = "#color-enum",
+        },
+        new()
+        {
             Name = "DefaultIndeterminate",
             Type = "bool?",
             DefaultValue = "null",
@@ -116,6 +125,15 @@ public partial class BitCheckboxDemo
             Type = "bool",
             DefaultValue = "false",
             Description = "Reverses the label and checkbox location."
+        },
+        new()
+        {
+            Name = "Size",
+            Type = "BitSize?",
+            DefaultValue = "null",
+            Description = "The size of the checkbox.",
+            LinkType = LinkType.Link,
+            Href = "#size-enum",
         },
         new()
         {
@@ -189,6 +207,175 @@ public partial class BitCheckboxDemo
         }
     ];
 
+    private readonly List<ComponentSubEnum> componentSubEnums =
+    [
+        new()
+        {
+            Id = "variant-enum",
+            Name = "BitVariant",
+            Description = "Determines the variant of the content that controls the rendered style of the corresponding element(s).",
+            Items =
+            [
+                new()
+                {
+                    Name= "Fill",
+                    Description="Fill styled variant.",
+                    Value="0",
+                },
+                new()
+                {
+                    Name= "Outline",
+                    Description="Outline styled variant.",
+                    Value="1",
+                },
+                new()
+                {
+                    Name= "Text",
+                    Description="Text styled variant.",
+                    Value="2",
+                }
+            ]
+        },
+        new()
+        {
+            Id = "color-enum",
+            Name = "BitColor",
+            Description = "Defines the general colors available in the bit BlazorUI.",
+            Items =
+            [
+                new()
+                {
+                    Name= "Primary",
+                    Description="Info Primary general color.",
+                    Value="0",
+                },
+                new()
+                {
+                    Name= "Secondary",
+                    Description="Secondary general color.",
+                    Value="1",
+                },
+                new()
+                {
+                    Name= "Tertiary",
+                    Description="Tertiary general color.",
+                    Value="2",
+                },
+                new()
+                {
+                    Name= "Info",
+                    Description="Info general color.",
+                    Value="3",
+                },
+                new()
+                {
+                    Name= "Success",
+                    Description="Success general color.",
+                    Value="4",
+                },
+                new()
+                {
+                    Name= "Warning",
+                    Description="Warning general color.",
+                    Value="5",
+                },
+                new()
+                {
+                    Name= "SevereWarning",
+                    Description="SevereWarning general color.",
+                    Value="6",
+                },
+                new()
+                {
+                    Name= "Error",
+                    Description="Error general color.",
+                    Value="7",
+                },
+                new()
+                {
+                    Name= "PrimaryBackground",
+                    Description="Primary background color.",
+                    Value="8",
+                },
+                new()
+                {
+                    Name= "SecondaryBackground",
+                    Description="Secondary background color.",
+                    Value="9",
+                },
+                new()
+                {
+                    Name= "TertiaryBackground",
+                    Description="Tertiary background color.",
+                    Value="10",
+                },
+                new()
+                {
+                    Name= "PrimaryForeground",
+                    Description="Primary foreground color.",
+                    Value="11",
+                },
+                new()
+                {
+                    Name= "SecondaryForeground",
+                    Description="Secondary foreground color.",
+                    Value="12",
+                },
+                new()
+                {
+                    Name= "TertiaryForeground",
+                    Description="Tertiary foreground color.",
+                    Value="13",
+                },
+                new()
+                {
+                    Name= "PrimaryBorder",
+                    Description="Primary border color.",
+                    Value="14",
+                },
+                new()
+                {
+                    Name= "SecondaryBorder",
+                    Description="Secondary border color.",
+                    Value="15",
+                },
+                new()
+                {
+                    Name= "TertiaryBorder",
+                    Description="Tertiary border color.",
+                    Value="16",
+                }
+            ]
+        },
+        new()
+        {
+            Id = "size-enum",
+            Name = "BitSize",
+            Description = "",
+            Items =
+            [
+                new()
+                {
+                    Name= "Small",
+                    Description="The small size checkbox.",
+                    Value="0",
+                },
+                new()
+                {
+                    Name= "Medium",
+                    Description="The medium size checkbox.",
+                    Value="1",
+                },
+                new()
+                {
+                    Name= "Large",
+                    Description="The large size checkbox.",
+                    Value="2",
+                }
+            ]
+        }
+    ];
+
 
 
     private bool oneWayValue;
@@ -216,168 +403,4 @@ public partial class BitCheckboxDemo
     {
         SuccessMessage = string.Empty;
     }
-
-
-    private readonly string example1RazorCode = @"
-<BitCheckbox Label=""Basic checkbox"" />
-<BitCheckbox Label=""Disable checkbox"" IsEnabled=""false"" />
-<BitCheckbox Label=""Disable checked checkbox"" IsEnabled=""false"" Value=""true"" />";
-
-    private readonly string example2RazorCode = @"
-<BitCheckbox Label=""Custom check icon"" CheckIconName=""@BitIconName.Heart"" />
-<BitCheckbox Label=""Disabled custom check icon"" CheckIconName=""@BitIconName.WavingHand"" Value=""true"" IsEnabled=""false"" />";
-
-    private readonly string example3RazorCode = @"
-<BitCheckbox Label=""Reversed"" Reversed />
-<BitCheckbox Label=""Reversed - Disabled"" Reversed IsEnabled=""false"" />
-<BitCheckbox Label=""Reversed - Disable Checked"" Reversed IsEnabled=""false"" Value=""true"" />";
-
-    private readonly string example4RazorCode = @"
-<BitCheckbox>
-    <LabelTemplate>
-        <BitTag Color=""BitColor.Success"">Label Template</BitTag>
-    </LabelTemplate>
-</BitCheckbox>";
-
-    private readonly string example5RazorCode = @"
-<BitCheckbox Label=""Indeterminate checkbox"" Indeterminate />
-<BitCheckbox Label=""Disabled indeterminate checkbox"" Indeterminate IsEnabled=""false"" />";
-
-    private readonly string example6RazorCode = @"
-<BitCheckbox Label=""One-way checked (Fixed)"" Value=""true"" />
-
-<BitCheckbox Label=""One-way"" Value=""oneWayValue"" />
-<BitToggleButton @bind-IsChecked=""oneWayValue"" Text=""Toggle"" />
-
-<BitCheckbox Label=""Two-way controlled checkbox"" @bind-Value=""twoWayValue"" />
-<BitToggleButton @bind-IsChecked=""twoWayValue"" Text=""Toggle"" />
-
-
-<BitCheckbox Label=""One-way indeterminate (Fixed)"" Indeterminate />
-
-<BitCheckbox Label=""One-way indeterminate"" Indeterminate=""oneWayIndeterminate"" />
-<BitToggleButton @bind-IsChecked=""oneWayIndeterminate"" Text=""Toggle"" />
-
-<BitCheckbox Label=""Two-way indeterminate"" @bind-Indeterminate=""twoWayIndeterminate"" />
-<BitToggleButton @bind-IsChecked=""twoWayIndeterminate"" Text=""Toggle"" />";
-    private readonly string example6CsharpCode = @"
-private bool oneWayValue;
-private bool twoWayValue;
-private bool oneWayIndeterminate = true;
-private bool twoWayIndeterminate = true;";
-
-    private readonly string example7RazorCode = @"
-<BitCheckbox @bind-Value=""customCheckboxValue"">
-    <BitIcon Style=""border:1px solid gray;width:22px;height:22px""
-             IconName=""@(customCheckboxValue ? BitIconName.Accept : null)"" />
-    <span>Custom basic checkbox</span>
-</BitCheckbox>
-
-
-<BitCheckbox @bind-Value=""customContentValue"" @bind-Indeterminate=""customContentIndeterminate"">
-    <BitIcon Style=""border:1px solid gray;width:22px;height:22px""
-             IconName=""@(customContentIndeterminate ? BitIconName.Fingerprint : (customContentValue ? BitIconName.Accept : null))"" />
-    <span>Custom indeterminate checkbox</span>
-</BitCheckbox>
-<BitButton OnClick=""() => customContentIndeterminate = true"">Make Indeterminate</BitButton>";
-    private readonly string example7CsharpCode = @"
-private bool customCheckboxValue;
-private bool customContentValue;
-private bool customContentIndeterminate = true;";
-
-    private readonly string example8RazorCode = @"
-<style>
-    .custom-class {
-        padding: 0.5rem;
-        border-radius: 0.125rem;
-        background-color: #d3d3d347;
-        border: 1px solid dodgerblue;
-    }
-
-
-    .custom-label {
-        font-weight: bold;
-        color: lightseagreen;
-    }
-
-    .custom-icon {
-        color: lightseagreen
-    }
-
-    .custom-box {
-        border-radius: 0.2rem;
-        border-color: lightseagreen;
-    }
-
-    .custom-checked .custom-icon {
-        color: white
-    }
-
-    .custom-checked:hover .custom-icon {
-        color: whitesmoke;
-    }
-
-    .custom-checked .custom-box {
-        background-color: lightseagreen;
-    }
-
-    .custom-checked:hover .custom-box {
-        border-color: mediumseagreen;
-    }
-</style>
-
-
-<BitCheckbox Label=""Styled checkbox"" Style=""color: dodgerblue; text-shadow: lightskyblue 0 0 1rem;"" />
-
-<BitCheckbox Label=""Classed checkbox"" Class=""custom-class"" />
-
-
-<BitCheckbox Label=""Styles"" 
-             Styles=""@(new() { Checked = ""--check-color: deeppink; --icon-color: white;"", 
-                               Label = ""color: var(--check-color);"",
-                               Box = ""border-radius: 50%; border-color: var(--check-color); background-color: var(--check-color);"",
-                               Icon = ""color: var(--icon-color);"" })"" />
-
-<BitCheckbox Label=""Classes"" 
-             Classes=""@(new() { Checked = ""custom-checked"",
-                                Icon = ""custom-icon"",
-                                Label=""custom-label"",
-                                Box=""custom-box"" })"" />";
-
-    private readonly string example9RazorCode = @"
-<style>
-    .validation-message {
-        color: red;
-        font-size: 0.75rem;
-    }
-</style>
-
-<EditForm Model=""validationModel""
-          OnValidSubmit=""HandleValidSubmit""
-          OnInvalidSubmit=""HandleInvalidSubmit"">
-    <DataAnnotationsValidator />
-    <BitCheckbox Label=""I agree with the terms and conditions.""
-                 @bind-Value=""validationModel.TermsAgreement"" />
-    <ValidationMessage For=""@(() => validationModel.TermsAgreement)"" />
-
-    <BitButton ButtonType=""BitButtonType.Submit"">Submit</BitButton>
-</EditForm>";
-    private readonly string example9CsharpCode = @"
-private BitCheckboxValidationModel validationModel = new();
-
-public class BitCheckboxValidationModel
-{
-    [Range(typeof(bool), ""true"", ""true"", ErrorMessage = ""You must agree to the terms and conditions."")]
-    public bool TermsAgreement { get; set; }
-}
-
-private async Task HandleValidSubmit() { }
-
-private void HandleInvalidSubmit() { }";
-
-    private readonly string example10RazorCode = @"
-<BitCheckbox Dir=""BitDir.Rtl"" Label=""چکباکس راست به چپ"" />
-<BitCheckbox Dir=""BitDir.Rtl"" Label=""چکباکس غیرفعال"" IsEnabled=""false"" />
-<BitCheckbox Dir=""BitDir.Rtl"" Label=""چکباکس غیرفعال چک شده"" IsEnabled=""false"" Value=""true"" />";
-
 }
