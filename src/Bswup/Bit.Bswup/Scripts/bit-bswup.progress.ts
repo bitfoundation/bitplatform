efc4038881cfcaa7d2d19a6f534b14783d1431b7
Saleh Yusefnejad <msynk@outlook.com>
2025-02-08 13:47:10 +0330

    prerelease v-9.4.1-pre-01 #9832

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bswup.progress version'] = '9.4.0';
+﻿window['bit-bswup.progress version'] = '9.4.1-pre-01';
 
 ; (function () {
     (window as any).startBswupProgress = (autoReload: boolean,
--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Web/wwwroot/service-worker.published.js
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Web/wwwroot/service-worker.published.js
@@ -1,5 +1,5 @@
 ﻿//+:cnd:noEmit
-// bit version: 9.4.0
+// bit version: 9.4.1-pre-01
 // https://github.com/bitfoundation/bitplatform/tree/develop/src/Bswup
 
 //#if (notification == true)
