9031cf258323cbaa4819384616b8154300227646
Saleh Yusefnejad <msynk@outlook.com>
2025-02-13 22:58:57 +0330

    fix review comments II

--- src/BlazorUI/Bit.BlazorUI.Tests/Components/Inputs/Dropdown/BitDropdownTests.cs
+++ src/BlazorUI/Bit.BlazorUI.Tests/Components/Inputs/Dropdown/BitDropdownTests.cs
@@ -4,7 +4,6 @@ using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bunit;
-using Microsoft.AspNetCore.Components;
 
 namespace Bit.BlazorUI.Tests.Components.Inputs.Dropdown;
 
