4b2c774e75a3441189511593ec929658a6a0aed2
ysmoradi <ysmoradi@outlook.com>
2025-02-16 14:49:31 +0100

    Fix Bit.SourceGenerators issue with interfaces containg properties wit attributes (#9916)

--- src/SourceGenerators/Bit.SourceGenerators/HttpClientProxy/HttpClientProxySyntaxReceiver.cs
+++ src/SourceGenerators/Bit.SourceGenerators/HttpClientProxy/HttpClientProxySyntaxReceiver.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using DoLess.UriTemplates;
+﻿using System;
 using System.Web;
+using System.Linq;
+using System.Collections.Generic;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using DoLess.UriTemplates;
 
 namespace Bit.SourceGenerators;
 
@@ -13,81 +14,88 @@ public class HttpClientProxySyntaxReceiver : ISyntaxContextReceiver
 
     public void OnVisitSyntaxNode(GeneratorSyntaxContext syntaxNode)
     {
-        if (syntaxNode.Node is InterfaceDeclarationSyntax interfaceDeclarationSyntax
+        try
+        {
+            if (syntaxNode.Node is InterfaceDeclarationSyntax interfaceDeclarationSyntax
                                                   && interfaceDeclarationSyntax.BaseList is not null
                                                                                    && interfaceDeclarationSyntax.BaseList.Types.Any(t => t.Type.ToString() == "IAppController"))
-        {
-            var model = syntaxNode.SemanticModel.Compilation.GetSemanticModel(interfaceDeclarationSyntax.SyntaxTree);
-            var controllerSymbol = (ITypeSymbol)model.GetDeclaredSymbol(interfaceDeclarationSyntax)!;
-            bool isController = controllerSymbol.IsIController();
-
-            if (isController == true)
             {
-                var controllerName = controllerSymbol.Name[1..].Replace("Controller", string.Empty);
+                var model = syntaxNode.SemanticModel.Compilation.GetSemanticModel(interfaceDeclarationSyntax.SyntaxTree);
+                var controllerSymbol = (ITypeSymbol)model.GetDeclaredSymbol(interfaceDeclarationSyntax)!;
+                bool isController = controllerSymbol.IsIController();
 
-                var route = controllerSymbol
-                    .GetAttributes()
-                    .FirstOrDefault(a => a.AttributeClass?.Name.StartsWith("Route") is true)?
-                    .ConstructorArguments
-                    .FirstOrDefault()
-                    .Value?
-                    .ToString()
-                    ?.Replace("[controller]", controllerName) ?? string.Empty;
-
-                var actions = controllerSymbol.GetMembers()
-                        .OfType<IMethodSymbol>()
-                        .Where(m => m.MethodKind == MethodKind.Ordinary)
-                        .Select(m => new ControllerAction
-                        {
-                            Method = m,
-                            ReturnType = m.ReturnType,
-                            HttpMethod = m.GetHttpMethod(),
-                            Url = m.Name,
-                            Parameters = m.Parameters.Select(y => new ActionParameter
-                            {
-                                Name = y.Name,
-                                Type = y.Type
-                            }).ToList()
-                        }).ToList();
-
-                foreach (var action in actions)
+                if (isController == true)
                 {
-                    var actionSpecificRoute = action.Method
+                    var controllerName = controllerSymbol.Name[1..].Replace("Controller", string.Empty);
+
+                    var route = controllerSymbol
                         .GetAttributes()
                         .FirstOrDefault(a => a.AttributeClass?.Name.StartsWith("Route") is true)?
                         .ConstructorArguments
                         .FirstOrDefault()
                         .Value?
                         .ToString()
-                        ?.Replace("[controller]", controllerName)
-                        ?.Replace("~/", string.Empty); // https://stackoverflow.com/a/34712201
+                        ?.Replace("[controller]", controllerName) ?? string.Empty;
 
-                    var uriTemplate = UriTemplate.For($"{actionSpecificRoute ?? route}{action.Method.GetAttributes()
-                        .FirstOrDefault(a => a.AttributeClass?.Name.StartsWith("Http") is true)?
-                        .ConstructorArguments.FirstOrDefault().Value?.ToString()}".Replace("[action]", action.Method.Name));
+                    var actions = controllerSymbol.GetMembers()
+                            .OfType<IMethodSymbol>()
+                            .Where(m => m.MethodKind == MethodKind.Ordinary)
+                            .Select(m => new ControllerAction
+                            {
+                                Method = m,
+                                ReturnType = m.ReturnType,
+                                HttpMethod = m.GetHttpMethod(),
+                                Url = m.Name,
+                                Parameters = m.Parameters.Select(y => new ActionParameter
+                                {
+                                    Name = y.Name,
+                                    Type = y.Type
+                                }).ToList()
+                            }).ToList();
 
-                    foreach (var parameter in action.Parameters)
+                    foreach (var action in actions)
                     {
-                        uriTemplate.WithParameter(parameter.Name, $"{{{parameter.Name}}}");
+                        var actionSpecificRoute = action.Method
+                            .GetAttributes()
+                            .FirstOrDefault(a => a.AttributeClass?.Name.StartsWith("Route") is true)?
+                            .ConstructorArguments
+                            .FirstOrDefault()
+                            .Value?
+                            .ToString()
+                            ?.Replace("[controller]", controllerName)
+                            ?.Replace("~/", string.Empty); // https://stackoverflow.com/a/34712201
+
+                        var uriTemplate = UriTemplate.For($"{actionSpecificRoute ?? route}{action.Method.GetAttributes()
+                            .FirstOrDefault(a => a.AttributeClass?.Name.StartsWith("Http") is true)?
+                            .ConstructorArguments.FirstOrDefault().Value?.ToString()}".Replace("[action]", action.Method.Name));
+
+                        foreach (var parameter in action.Parameters)
+                        {
+                            uriTemplate.WithParameter(parameter.Name, $"{{{parameter.Name}}}");
+                        }
+
+                        string url = HttpUtility.UrlDecode(uriTemplate.ExpandToString()).TrimEnd('/');
+
+                        // if there is a parameter that is not a cancellation token and is not in the route template, then it is the body parameter
+                        action.BodyParameter = action.Parameters.FirstOrDefault(p => p.Type.ToDisplayString() is not "System.Threading.CancellationToken" && url.Contains($"{{{p.Name}}}") is false);
+                        action.CancellationTokenParameterName = action.Parameters.FirstOrDefault(p => p.Type.ToDisplayString() == "System.Threading.CancellationToken")?.Name;
+                        action.Url = url;
                     }
 
-                    string url = HttpUtility.UrlDecode(uriTemplate.ExpandToString()).TrimEnd('/');
-
-                    // if there is a parameter that is not a cancellation token and is not in the route template, then it is the body parameter
-                    action.BodyParameter = action.Parameters.FirstOrDefault(p => p.Type.ToDisplayString() is not "System.Threading.CancellationToken" && url.Contains($"{{{p.Name}}}") is false);
-                    action.CancellationTokenParameterName = action.Parameters.FirstOrDefault(p => p.Type.ToDisplayString() == "System.Threading.CancellationToken")?.Name;
-                    action.Url = url;
+                    IControllers.Add(new IController
+                    {
+                        Actions = actions,
+                        Name = controllerName,
+                        ClassName = controllerSymbol.Name[1..],
+                        Symbol = controllerSymbol,
+                        Syntax = interfaceDeclarationSyntax
+                    });
                 }
-
-                IControllers.Add(new IController
-                {
-                    Actions = actions,
-                    Name = controllerName,
-                    ClassName = controllerSymbol.Name[1..],
-                    Symbol = controllerSymbol,
-                    Syntax = interfaceDeclarationSyntax
-                });
             }
+        }
+        catch (Exception exp)
+        {
+            throw new InvalidOperationException($"Error processing {syntaxNode.Node.SyntaxTree.FilePath}: {exp}", exp);
         }
     }
 }
