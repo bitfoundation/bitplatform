b93491f584f3b0f992bd2612ee62ead562d17f66
Saleh Yusefnejad <msynk@outlook.com>
2025-02-26 01:12:43 +0330

    feat(blazorui): add BitMediaQuery component #10037 (#10044)

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Utilities/MediaQuery/BitScreenQuery.cs
@@ -0,0 +1,87 @@
+﻿namespace Bit.BlazorUI;
+
+/// <summary>
+/// The predefined screen media queries in the bit BlazorUI.
+/// </summary>
+public enum BitScreenQuery
+{
+    /// <summary>
+    /// @media screen and (max-width: 600px)
+    /// </summary>
+    Xs,
+
+    /// <summary>
+    /// @media screen and (min-width: 601px) and (max-width: 960px)
+    /// </summary>
+    Sm,
+
+    /// <summary>
+    /// @media screen and (min-width: 961px) and (max-width: 1280px)
+    /// </summary>
+    Md,
+
+    /// <summary>
+    /// @media screen and (min-width: 1281px) and (max-width: 1920px)
+    /// </summary>
+    Lg,
+
+    /// <summary>
+    /// @media screen and (min-width: 1921px) and (max-width: 2560px)
+    /// </summary>
+    Xl,
+
+    /// <summary>
+    /// @media screen and (min-width: 2561px)
+    /// </summary>
+    Xxl,
+
+    /// <summary>
+    /// @media screen and (max-width: 600px)
+    /// </summary>
+    LtSm,
+
+    /// <summary>
+    /// @media screen and (max-width: 960px)
+    /// </summary>
+    LtMd,
+
+    /// <summary>
+    /// @media screen and (max-width: 1280px)
+    /// </summary>
+    LtLg,
+
+    /// <summary>
+    /// @media screen and (max-width: 1920px)
+    /// </summary>
+    LtXl,
+
+    /// <summary>
+    /// @media screen and (max-width: 2560px)
+    /// </summary>
+    LtXxl,
+
+    /// <summary>
+    /// @media screen and (min-width: 601px)
+    /// </summary>
+    GtXs,
+
+    /// <summary>
+    /// @media screen and (min-width: 961px)
+    /// </summary>
+    GtSm,
+
+    /// <summary>
+    /// @media screen and (min-width: 1281px)
+    /// </summary>
+    GtMd,
+
+    /// <summary>
+    /// @media screen and (min-width: 1921px)
+    /// </summary>
+    GtLg,
+
+    /// <summary>
+    /// @media screen and (min-width: 2561px)
+    /// </summary>
+    GtXl
+}
---
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Utilities/MediaQuery/BitMediaQueryDemo.razor.cs
@@ -0,0 +1,27 @@
+﻿namespace Bit.BlazorUI.Demo.Client.Core.Pages.Components.Utilities.MediaQuery;
+
+public partial class BitMediaQueryDemo
+{
+    private readonly List<ComponentParameter> componentParameters =
+    [
+        new()
+        {
+            Name = "ChildContent",
+            Type = "RenderFragment?",
+            DefaultValue = "null",
+            Description = "The content of the element.",
+        },
+        new()
+        {
+            Name = "Query",
+            Type = "BitScreenQuery?",
+            DefaultValue = "null",
+            Description = "Specifies the media query to match for the child content.",
+        }
+    ];
+
+
+
+    private string example1RazorCode = @"
+<BitMediaQuery Query=""BitScreenQuery.Sm"">This is SM!</BitMediaQuery>";
+}
