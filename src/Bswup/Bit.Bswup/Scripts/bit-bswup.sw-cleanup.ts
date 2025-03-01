5a9339de20df19b75ece0dd90c19f54552405f1d
Saleh Yusefnejad <msynk@outlook.com>
2025-03-01 09:53:30 +0330

    add bswup cleanup

---
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw-cleanup.ts
@@ -0,0 +1,20 @@
+self['bit-bswup.sw-cleanup version'] = '9.5.1';
+
+self.addEventListener('install', e => e.waitUntil(handleInstall(e)));
+
+async function handleInstall(e) {
+    await removeBswup();
+}
+
+async function removeBswup() {
+    const cacheKeys = await caches.keys();
+    const cachePromises = cacheKeys.filter(key => key.startsWith('bit-bswup') || key.startsWith('blazor-resources')).map(key => caches.delete(key));
+    await Promise.all(cachePromises);
+
+    self.skipWaiting().then(() => self.clients
+        .matchAll({ includeUncontrolled: true })
+        .then(clients => (clients || []).forEach(client => {
+            client.postMessage('UNREGISTER');
+            setTimeout(() => client.postMessage('WAITING_SKIPPED'), 1000);
+        })));
+}
