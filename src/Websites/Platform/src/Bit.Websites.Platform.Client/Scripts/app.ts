c34a6d4f09e67c1d567b68c4e8ebf552c6024552
Saleh Yusefnejad <msynk@outlook.com>
2025-03-03 01:44:09 +0330

    feat(websites): improve demos page of Platform website #10115 (#10116)

--- src/Websites/Platform/src/Bit.Websites.Platform.Client/Scripts/app.ts
+++ src/Websites/Platform/src/Bit.Websites.Platform.Client/Scripts/app.ts
@@ -22,9 +22,11 @@ BitTheme.init({
         if (newTheme === 'dark') {
             document.body.classList.add('bit-platform-dark-theme');
             document.body.classList.remove('bit-platform-light-theme');
+            document.querySelector("meta[name=theme-color]")?.setAttribute('content', '#060E2D');
         } else {
             document.body.classList.add('bit-platform-light-theme');
             document.body.classList.remove('bit-platform-dark-theme');
+            document.querySelector("meta[name=theme-color]")?.setAttribute('content', '#FFFFFF');
         }
     }
 });
