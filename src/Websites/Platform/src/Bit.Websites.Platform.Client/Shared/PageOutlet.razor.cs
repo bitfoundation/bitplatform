beaa040cab40969de9c8daa91bc2a4f5f2c0e16b
Saleh Yusefnejad <msynk@outlook.com>
2025-03-02 21:19:41 +0330

    improve demos page of Platform website #10115

--- src/Websites/Platform/src/Bit.Websites.Platform.Client/Shared/PageOutlet.razor.cs
+++ src/Websites/Platform/src/Bit.Websites.Platform.Client/Shared/PageOutlet.razor.cs
@@ -5,5 +5,5 @@ public partial class PageOutlet
     [Parameter] public string? Title { get; set; }
     [Parameter] public string? Description { get; set; }
     [Parameter] public string? Url { get; set; }
-    [Parameter] public string ImageUrl { get; set; } = "https://bitplatform.dev/images/og-image.svg";
+    [Parameter] public string ImageUrl { get; set; } = "https://bitplatform.dev/images/og-image.webp";
 }
