57f0f909dbd213d79c22113f74ce69b8fba1d06a
Saleh Yusefnejad <msynk@outlook.com>
2025-03-02 08:23:14 +0330

    add images and complete demos page

--- src/Websites/Platform/src/Bit.Websites.Platform.Client/Shared/Urls.cs
+++ src/Websites/Platform/src/Bit.Websites.Platform.Client/Shared/Urls.cs
@@ -29,17 +29,23 @@ public static class Urls
 
     public const string BoilerplateNuget = "https://www.nuget.org/packages/Bit.Boilerplate/";
 
+    public const string SalesSpa = "https://sales.bitplatform.dev/";
+    public const string SalesWindows = "https://windows-sales.bitplatform.dev/SalesModule.Client.Windows-win-Setup.exe";
+
+    public const string BlazorUiPwa = "https://blazorui.bitplatform.dev/";
+    public const string BlazorUiGooglePlay = "https://play.google.com/store/apps/details?id=com.bitplatform.BlazorUI.Demo";
+    public const string BlazorUiAppleStore = "https://apps.apple.com/app/bit-blazor-ui/id6450401404";
+    public const string BlazorUiWindows = "https://windows-components.bitplatform.dev/Bit.BlazorUI.Demo.Client.Windows-win-Setup.exe";
+
+    public const string AdminPanelPwa = "https://adminpanel.bitplatform.dev/";
+    public const string AdminPanelGooglePlay = "https://play.google.com/store/apps/details?id=com.bitplatform.AdminPanel.Template";
+    public const string AdminPanelAppleStore = "https://apps.apple.com/app/bit-adminpanel/id6450611349";
+    public const string AdminPanelWindows = "https://windows-admin.bitplatform.dev/AdminPanel.Client.Windows-win-Setup.exe";
+
     public const string TodoTemplatePwaPrerendered = "https://todo.bitplatform.dev/";
     public const string TodoTemplateGooglePlay = "https://play.google.com/store/apps/details?id=com.bitplatform.Todo.Template";
-    public const string TodoTemplateAppleStore = "https://apps.apple.com/us/app/bit-todotemplate/id6450611072";
+    public const string TodoTemplateAppleStore = "https://apps.apple.com/app/bit-todotemplate/id6450611072";
     public const string TodoTemplateWindows = "https://windows-todo.bitplatform.dev/TodoSample.Client.Windows-win-Setup.exe";
-
-    public const string BlazorWebTemplate = "https://www.nuget.org/packages/Bit.BlazorWeb/";
-
-    public const string AdminPanelMultiLingual = "https://adminpanel.bitplatform.dev/";
-    public const string AdminPanelGooglePlay = "https://play.google.com/store/apps/details?id=com.bitplatform.AdminPanel.Template";
-    public const string AdminPanelAppleStore = "https://apps.apple.com/us/app/bit-adminpanel/id6450611349";
-    public const string AdminPanelWindows = "https://windows-admin.bitplatform.dev/AdminPanel.Client.Windows-win-Setup.exe";
 
     public const string BlazorUI = "https://blazorui.bitplatform.dev/";
 
