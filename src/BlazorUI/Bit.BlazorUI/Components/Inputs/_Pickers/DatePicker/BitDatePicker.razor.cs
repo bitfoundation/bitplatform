8806377f6369c0a5c0b045abe16e17ff4b1b57e8
Mohammad Aminsafaei <m.aminsafaei@outlook.com>
2025-03-02 12:32:56 +0330

    Clean code

--- src/BlazorUI/Bit.BlazorUI/Components/Inputs/_Pickers/DatePicker/BitDatePicker.razor.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Inputs/_Pickers/DatePicker/BitDatePicker.razor.cs
@@ -739,7 +739,6 @@ public partial class BitDatePicker : BitInputBase<DateTimeOffset?>
         if (selectedMonth < _currentMonth && _currentMonth == 12 && isNotInCurrentMonth && _currentDay < 15)
         {
             _currentYear++;
-            
         }
 
         //The number of days displayed in the picker is about 34 days, and if the selected day is greater than 15, it means that the previous month has been selected in previous year.
