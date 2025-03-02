5c8734b4b62a5e987eeb88e78e0120c233f693d2
ysmoradi <ysmoradi@outlook.com>
2025-03-02 12:06:29 +0100

    fix

---
+++ src/Websites/Careers/src/Bit.Websites.Careers.Client/wwwroot/service-worker.published.js
@@ -0,0 +1,28 @@
+// bit version: 9.6.0-pre-02
+// https://github.com/bitfoundation/bitplatform/tree/develop/src/Bswup
+
+self.assetsInclude = [];
+self.assetsExclude = [
+    /bit\.blazorui\.fluent\.css$/,
+    /bit\.blazorui\.fluent-dark\.css$/,
+    /bit\.blazorui\.fluent-light\.css$/
+];
+self.externalAssets = [
+    {
+        "url": "_framework/blazor.web.js"
+    },
+    {
+        "url": "Bit.Websites.Carrers.Server.styles.css"
+    },
+    {
+        "url": "Bit.Websites.Carrers.Client.bundle.scp.css"
+    }
+];
+
+self.serverHandledUrls = [
+    /\/api\//
+];
+
+self.prerenderMode = 'always';
+
+self.importScripts('_content/Bit.Bswup/bit-bswup.sw.js');
---
+++ src/Websites/Sales/src/Bit.Websites.Sales.Client/wwwroot/service-worker.js
@@ -0,0 +1,8 @@
+// In development, always fetch from the network and do not enable offline support.
+// This is because caching would make development more difficult (changes would not
+// be reflected on the first load after each change).
+self.addEventListener('fetch', () => { });
+
+self.clients
+    .matchAll({ includeUncontrolled: true })
+    .then(clients => (clients || []).forEach(client => client.postMessage('START_BLAZOR')));
