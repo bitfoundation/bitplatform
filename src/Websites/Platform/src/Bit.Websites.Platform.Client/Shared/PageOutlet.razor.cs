731a7c368da1cc0b476830a0729d9e687a5c778b
Yaser Moradi <ysmoradi@outlook.com>
2025-03-05 20:07:56 +0100

    feat(websites): improve demos page of Platform website #10165 (#10178)

--- src/Websites/Platform/src/Bit.Websites.Platform.Client/Shared/PageOutlet.razor.cs
+++ src/Websites/Platform/src/Bit.Websites.Platform.Client/Shared/PageOutlet.razor.cs
@@ -6,4 +6,6 @@ public partial class PageOutlet
     [Parameter] public string? Description { get; set; }
     [Parameter] public string? Url { get; set; }
     [Parameter] public string ImageUrl { get; set; } = "https://bitplatform.dev/images/og-image.webp";
+
+    [Parameter] public RenderFragment? ChildContent { get; set; }
 }
