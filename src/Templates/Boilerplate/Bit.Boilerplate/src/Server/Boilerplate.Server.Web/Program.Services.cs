b3d076c9f3ceb4d7fee7f0949c3205ea38255fd6
ysmoradi <ysmoradi@outlook.com>
2025-03-07 09:16:57 +0100

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Web/Program.Services.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Web/Program.Services.cs
@@ -107,8 +107,9 @@ public static partial class Program
         var configuration = builder.Configuration;
 
         services.AddTransient<IAntiforgery, NoOpAntiforgery>();
-        services.AddScoped<IAuthTokenProvider, ServerSideAuthTokenProvider>();
         services.AddTransient<IPrerenderStateService, WebServerPrerenderStateService>();
+        services.AddScoped<IExceptionHandler, WebServerExceptionHandler>();
+        services.AddScoped<IAuthTokenProvider, ServerSideAuthTokenProvider>();
         services.AddScoped(sp =>
         {
             // This HTTP client is utilized during pre-rendering and within Blazor Auto/Server sessions for API calls. 
