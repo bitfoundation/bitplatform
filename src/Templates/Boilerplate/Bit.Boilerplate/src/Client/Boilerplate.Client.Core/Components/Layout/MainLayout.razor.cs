48af0c46dab53a0f2f2f883382f0a16ce1292cbe
ysmoradi <ysmoradi@outlook.com>
2025-03-09 11:17:24 +0100

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/MainLayout.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/MainLayout.razor.cs
@@ -42,7 +42,7 @@ public partial class MainLayout : IAsyncDisposable
         try
         {
             var inPrerenderSession = jsRuntime.IsInitialized() is false;
-            isOnline = await prerenderStateService.GetValue(async () => isOnline ?? inPrerenderSession);
+            isOnline = await prerenderStateService.GetValue<bool?>(nameof(isOnline), async () => isOnline ?? inPrerenderSession is true ? true : null);
             // During pre-rendering, if any API calls are made, the `isOnline` value will be set 
             // using PubSub's `ClientPubSubMessages.IS_ONLINE_CHANGED`, depending on the success 
             // or failure of the API call. However, if a pre-rendered page has no HTTP API call 
