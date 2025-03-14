640c6bb60f0cff82591ce01c14873b4d7d6a0998
Saleh Yusefnejad <msynk@outlook.com>
2025-03-14 21:32:00 +0330

    prerelease v-9.6.0-pre-11 #10257

--- src/Besql/Bit.Besql/wwwroot/bit-besql.js
+++ src/Besql/Bit.Besql/wwwroot/bit-besql.js
@@ -1,5 +1,5 @@
 ﻿var BitBesql = window.BitBesql || {};
-BitBesql.version = window['bit-besql version'] = '9.6.0-pre-10';
+BitBesql.version = window['bit-besql version'] = '9.6.0-pre-11';
 
 BitBesql.persist = async function persist(fileName) {
 
