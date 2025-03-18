76e1aeb849a6bd605dcacbe983aa1655127c8e66
Saleh Yusefnejad <msynk@outlook.com>
2025-03-18 10:52:58 +0330

    feat(blazorui): add BitMarkdownEditor component #10230 (#10268)

---
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Pages/Components/Extras/MarkdownEditor/BitMarkdownEditorDemo.razor.cs
@@ -0,0 +1,35 @@
+﻿namespace Bit.BlazorUI.Demo.Client.Core.Pages.Components.Extras.MarkdownEditor;
+
+public partial class BitMarkdownEditorDemo
+{
+    private readonly List<ComponentParameter> componentParameters =
+    [
+    ];
+
+
+
+    private BitMarkdownEditor editorRef = default!;
+    private string? value;
+    private async Task GetValue()
+    {
+        value = await editorRef.GetValue();
+    }
+
+
+
+    private readonly string example1RazorCode = @"
+<BitButton OnClick=""GetValue"">Get Value</BitButton>
+<div style=""margin-top:1rem;display:flex;flex-direction:row;gap:1rem;height:300px"">
+    <BitMarkdownEditor @ref=""editorRef"" />
+    <pre style=""width:100%"">
+        @value
+    </pre>
+</div>";
+    private readonly string example1CsharpCode = @"
+private BitMarkdownEditor editorRef = default!;
+private string? value;
+private async Task GetValue()
+{
+    value = await editorRef.GetValue();
+}";
+}
