2b5c11efabd1b0b293634dacdba9445e9afe9ac4
Saleh Yusefnejad <msynk@outlook.com>
2025-03-26 19:53:39 +0330

    feat(release): v-9.6.0 #10301 (#10314)

--- src/Bswup/Bit.Bswup.NewDemo/Bit.Bswup.NewDemo.Client/wwwroot/service-worker.published.js
+++ src/Bswup/Bit.Bswup.NewDemo/Bit.Bswup.NewDemo.Client/wwwroot/service-worker.published.js
@@ -1,4 +1,4 @@
-﻿// bit version: 9.6.0-pre-14
+﻿// bit version: 9.6.0
 
 self.assetsInclude = [];
 self.assetsExclude = [
--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw.ts
@@ -1,4 +1,4 @@
-﻿self['bit-bswup.sw version'] = '9.6.0-pre-14';
+﻿self['bit-bswup.sw version'] = '9.6.0';
 
 interface Window {
     clients: any
