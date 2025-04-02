352127f0482c5810f98f2a2be68cc854608beb8a
ysmoradi <ysmoradi@outlook.com>
2025-04-02 13:37:14 +0200

    refactor identity

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/IdentityHeader.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/IdentityHeader.razor.cs
@@ -4,11 +4,10 @@ namespace Boilerplate.Client.Core.Components.Layout;
 
 public partial class IdentityHeader : AppComponentBase
 {
-    private string? backLinkPayload;
-    private Action unsubscribeUpdateBackLink = default!;
     private BitDropdownItem<string>[] cultures = default!;
 
 
+    [AutoInject] private History history = default!;
     [AutoInject] private ThemeService themeService = default!;
     [AutoInject] private CultureService cultureService = default!;
 
@@ -19,13 +18,6 @@ public partial class IdentityHeader : AppComponentBase
 
     protected override async Task OnInitAsync()
     {
-        unsubscribeUpdateBackLink = PubSubService.Subscribe(ClientPubSubMessages.UPDATE_IDENTITY_HEADER_BACK_LINK, async payload =>
-        {
-            backLinkPayload = (string?)payload;
-
-            await InvokeAsync(StateHasChanged);
-        });
-
         if (CultureInfoManager.MultilingualEnabled)
         {
             cultures = CultureInfoManager.SupportedCultures
@@ -38,7 +30,7 @@ public partial class IdentityHeader : AppComponentBase
 
     private async Task HandleBackLinkClick()
     {
-        PubSubService.Publish(ClientPubSubMessages.IDENTITY_HEADER_BACK_LINK_CLICKED, backLinkPayload);
+        await history.GoBack();
     }
 
     private async Task ToggleTheme()
@@ -49,13 +41,5 @@ public partial class IdentityHeader : AppComponentBase
     private async Task OnCultureChanged(string? cultureName)
     {
         await cultureService.ChangeCulture(cultureName);
-    }
-
-
-    protected override async ValueTask DisposeAsync(bool disposing)
-    {
-        unsubscribeUpdateBackLink?.Invoke();
-
-        await base.DisposeAsync(disposing);
     }
 }
--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Identity/SignIn/OtpPanel.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Identity/SignIn/OtpPanel.razor.cs
@@ -11,4 +11,6 @@ public partial class OtpPanel
     [Parameter] public EventCallback<string?> OnSignIn { get; set; }
 
     [Parameter] public EventCallback OnResendOtp { get; set; }
+
+    [Parameter] public EventCallback ShowSignInPanel { get; set; }
 }
