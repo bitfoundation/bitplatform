bc9c4d6da2688d2390e9bb0720c8e219fa860e98
Saleh Yusefnejad <msynk@outlook.com>
2025-04-12 22:26:43 +0330

    feat(blazorui): add AutoScroll and ScrollToEnd to BitScrollablePane #10417 (#10422)

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Surfaces/ScrollablePane/BitScrollablePane.ts
@@ -0,0 +1,9 @@
+﻿namespace BitBlazorUI {
+    export class ScrollablePane {
+        public static scrollToEnd(element: HTMLInputElement) {
+            if (!element) return;
+
+            element.scrollTo(element.scrollWidth, element.scrollHeight);
+        }
+    }
+}
