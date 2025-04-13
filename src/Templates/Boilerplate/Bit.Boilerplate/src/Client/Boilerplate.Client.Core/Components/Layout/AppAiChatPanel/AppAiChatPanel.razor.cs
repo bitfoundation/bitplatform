7eb141ade1a3b520f4e5bf3230aec0e0f78ee2ee
Saleh Yusefnejad <msynk@outlook.com>
2025-04-13 14:58:54 +0330

    fix last response counter issues

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/AppAiChatPanel/AppAiChatPanel.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/AppAiChatPanel/AppAiChatPanel.razor.cs
@@ -112,7 +112,10 @@ public partial class AppAiChatPanel
             }
             else
             {
-                lastAssistantMessage.Content += response;
+                if ((responseCounter + 1) == (chatMessages.Count - 1) / 2)
+                {
+                    lastAssistantMessage.Content += response;
+                }
             }
 
             StateHasChanged();
