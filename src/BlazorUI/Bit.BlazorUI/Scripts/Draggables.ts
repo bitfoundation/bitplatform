f1172269a503cad7785fa4454792a872baea4353
Saleh Yusefnejad <msynk@outlook.com>
2025-04-13 07:28:08 +0330

    1st commit

---
+++ src/BlazorUI/Bit.BlazorUI/Scripts/Draggables.ts
@@ -0,0 +1,81 @@
+﻿namespace BitBlazorUI {
+    export class Draggables {
+        private static _abortControllers: { [key: string]: AbortController } = {};
+
+        public static setupDragDrop(id: string, selector: string) {
+            Draggables.removeDragDrop(id, selector);
+
+            const ac = new AbortController();
+            Draggables._abortControllers[id] = ac;
+
+            const element = document.getElementById(id)! as HTMLElement;
+            const dragElement = document.querySelector(selector)! as HTMLElement;
+
+            let x = 0;
+            let y = 0;
+
+            listeners['pointerdown'] = handlePointerDown;
+            dragElement.addEventListener('pointerdown', handlePointerDown);
+            dragElement.style.cursor = 'move';
+            dragElement.classList.add('bit-mdl-nta');
+
+            function handlePointerDown(e: PointerEvent) {
+                //e.preventDefault();
+
+                x = e.clientX;
+                y = e.clientY;
+
+                const { width } = element.getBoundingClientRect();
+                element.style.width = `${width}px`;
+
+                document.addEventListener('pointermove', handlePointerMove);
+                listeners['pointermove'] = handlePointerMove;
+
+                document.addEventListener('pointerup', handlePointerUp);
+                //document.addEventListener('pointerout', handlePointerUp);
+                //document.addEventListener('pointerleave', handlePointerUp);
+                listeners['pointerup'] = handlePointerUp;
+            }
+
+            function handlePointerMove(e: PointerEvent) {
+                e.preventDefault();
+
+                element.style.left = `${element.offsetLeft - (x - e.clientX)}px`;
+                element.style.top = `${element.offsetTop - (y - e.clientY)}px`;
+
+                x = e.clientX;
+                y = e.clientY;
+            }
+
+            function handlePointerUp() {
+                document.removeEventListener('pointermove', handlePointerMove);
+
+                document.removeEventListener('pointerup', handlePointerUp);
+                //document.removeEventListener('pointerout', handlePointerUp);
+                //document.removeEventListener('pointerleave', handlePointerUp);
+            }
+        }
+
+        public static removeDragDrop(id: string, dragElementSelector: string) {
+            const listeners = Modal._dragDropListeners[id];
+            if (!listeners) return;
+
+            const dragElement = document.querySelector(dragElementSelector)! as HTMLElement;
+
+            dragElement.removeEventListener('pointerdown', listeners['pointerdown']);
+            dragElement.style.cursor = '';
+            dragElement.classList.remove('bit-mdl-nta');
+
+            document.removeEventListener('pointermove', listeners['pointermove']);
+
+            document.removeEventListener('pointerup', listeners['pointerup']);
+            //document.removeEventListener('pointerout', listeners['pointerup']);
+            //document.removeEventListener('pointerleave', listeners['pointerup']);
+
+            delete listeners['pointerdown'];
+            delete listeners['pointermove'];
+            delete listeners['pointerup'];
+            delete Modal._dragDropListeners[id];
+        }
+    }
+}
