3c198ae40bc8083fce17f3f85f5f4e42bab99b76
Saleh Yusefnejad <msynk@outlook.com>
2025-04-17 13:20:01 +0330

    feat(blazorui): improve BitMediaQuery component #10461 (#10469)

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Utilities/MediaQuery/BitMediaQueryJsRuntimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Bit.BlazorUI;
+
+internal static class BitMediaQueryJsRuntimeExtensions
+{
+    internal static ValueTask BitMediaQuerySetup(this IJSRuntime jsRuntime, 
+                                                      string id, 
+                                                      string query, 
+                                                      DotNetObjectReference<BitMediaQuery>? dotnetObj)
+    {
+        return jsRuntime.InvokeVoid("BitBlazorUI.MediaQuery.setup", id, query, dotnetObj);
+    }
+
+    internal static ValueTask BitMediaQueryDispose(this IJSRuntime jsRuntime, string id)
+    {
+        return jsRuntime.InvokeVoid("BitBlazorUI.MediaQuery.dispose", id);
+    }
+}
