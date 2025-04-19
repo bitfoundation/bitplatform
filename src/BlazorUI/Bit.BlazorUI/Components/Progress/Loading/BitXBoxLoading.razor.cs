85d6cd517c7b25c6476ac39ee72f0ee14beab3c7
Saleh Yusefnejad <msynk@outlook.com>
2025-04-19 12:43:19 +0330

    add some important missing loadings to BlazorUI #10467

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Progress/Loading/BitXBoxLoading.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace Bit.BlazorUI;
+
+public partial class BitXBoxLoading : BitLoadingBase
+{
+    protected override string RootElementClass => "bit-ldn-xbx";
+
+    protected override void RegisterCssStyles()
+    {
+        base.RegisterCssStyles();
+
+        StyleBuilder.Register(() => $"--bit-ldn-xbx-3:{Convert(3)}px");
+        StyleBuilder.Register(() => $"--bit-ldn-xbx-25f:{Convert(0.25 * OriginalSize)}px");
+        StyleBuilder.Register(() => $"--bit-ldn-xbx-50f:{Convert(0.50 * OriginalSize)}px");
+        StyleBuilder.Register(() => $"--bit-ldn-xbx-125f:{Convert(0.125 * OriginalSize)}px");
+        StyleBuilder.Register(() => $"--bit-ldn-xbx-75f:{Convert(0.75 * OriginalSize)}px");
+    }
+}
