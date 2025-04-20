6f24900f6213bc6e73be424d96aca42a9218ab8c
Saleh Yusefnejad <msynk@outlook.com>
2025-04-20 12:30:42 +0330

    fix issues of SignOut confirm modal in Boilerplate #10504

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/SignOutConfirmDialog.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Layout/SignOutConfirmDialog.razor.cs
@@ -1,7 +1,10 @@
-namespace Boilerplate.Client.Core.Components.Layout;
+﻿namespace Boilerplate.Client.Core.Components.Layout;
 
 public partial class SignOutConfirmDialog
 {
+    private bool isSigningOut;
+
+
     [Parameter] public bool IsOpen { get; set; }
 
     [Parameter] public EventCallback<bool> IsOpenChanged { get; set; }
@@ -9,13 +12,26 @@ public partial class SignOutConfirmDialog
 
     private async Task CloseModal()
     {
+        if (isSigningOut) return;
+
         IsOpen = false;
         await IsOpenChanged.InvokeAsync(false);
     }
 
     private async Task SignOut()
     {
-        await AuthManager.SignOut(CurrentCancellationToken);
+        if (isSigningOut) return;
+
+        try
+        {
+            isSigningOut = true;
+
+            await AuthManager.SignOut(CurrentCancellationToken);
+        }
+        finally
+        {
+            isSigningOut = false;
+        }
 
         await CloseModal();
     }
