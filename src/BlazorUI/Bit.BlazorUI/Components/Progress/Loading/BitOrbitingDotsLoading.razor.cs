dec485f9acf81bd262069eef8dcc8cc7e202c348
Saleh Yusefnejad <msynk@outlook.com>
2025-04-21 09:52:18 +0330

    add RollingSquare

--- src/BlazorUI/Bit.BlazorUI/Components/Progress/Loading/BitOrbitingDotsLoading.razor.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Progress/Loading/BitOrbitingDotsLoading.razor.cs
@@ -8,7 +8,6 @@ public partial class BitOrbitingDotsLoading : BitLoadingBase
     {
         base.RegisterCssStyles();
 
-        StyleBuilder.Register(() => $"--bit-ldn-ord-6:{Convert(6)}px");
         StyleBuilder.Register(() => $"--bit-ldn-ord-25:{Convert(25)}px");
     }
 }
---
+++ src/BlazorUI/Bit.BlazorUI/Components/Progress/Loading/BitRollingSquareLoading.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace Bit.BlazorUI;
+
+public partial class BitRollingSquareLoading : BitLoadingBase
+{
+    protected override string RootElementClass => "bit-ldn-rsq";
+
+    protected override void RegisterCssStyles()
+    {
+        base.RegisterCssStyles();
+
+        StyleBuilder.Register(() => $"--bit-ldn-rsq-4:{Convert(4)}px");
+        StyleBuilder.Register(() => $"--bit-ldn-rsq-20:{Convert(20)}px");
+    }
+}
