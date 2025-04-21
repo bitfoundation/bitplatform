d3ac714a989b0b2001536e44500884699473054e
Saleh Yusefnejad <msynk@outlook.com>
2025-04-21 09:58:30 +0330

    feat(blazorui): add some important missing loadings to BlazorUI #10467 (#10488)

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Progress/Loading/BitSlickBarsLoading.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace Bit.BlazorUI;
+
+public partial class BitSlickBarsLoading : BitLoadingBase
+{
+    protected override string RootElementClass => "bit-ldn-sbr";
+
+    protected override void RegisterCssStyles()
+    {
+        base.RegisterCssStyles();
+
+        StyleBuilder.Register(() => $"--bit-ldn-sbr-2:{Convert(2)}px");
+        StyleBuilder.Register(() => $"--bit-ldn-sbr-8:{Convert(8)}px");
+    }
+}
