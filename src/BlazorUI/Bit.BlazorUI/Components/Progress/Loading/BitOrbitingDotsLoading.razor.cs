1401d5df8a02e0158fa9188046667bfc1ec9b5c1
Saleh Yusefnejad <msynk@outlook.com>
2025-04-21 09:18:30 +0330

    add OrbitingDots loading

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Progress/Loading/BitOrbitingDotsLoading.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace Bit.BlazorUI;
+
+public partial class BitOrbitingDotsLoading : BitLoadingBase
+{
+    protected override string RootElementClass => "bit-ldn-ord";
+
+    protected override void RegisterCssStyles()
+    {
+        base.RegisterCssStyles();
+
+        StyleBuilder.Register(() => $"--bit-ldn-ord-6:{Convert(6)}px");
+        StyleBuilder.Register(() => $"--bit-ldn-ord-25:{Convert(25)}px");
+    }
+}
