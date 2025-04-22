ac7a22da4a41c5208cbe4dabaf492d06c641ef25
Saleh Yusefnejad <msynk@outlook.com>
2025-04-22 13:44:26 +0330

    release v-9.7.0 #10496

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw-cleanup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw-cleanup.ts
@@ -1,4 +1,4 @@
-self['bit-bswup.sw-cleanup version'] = '9.7.0-pre-12';
+self['bit-bswup.sw-cleanup version'] = '9.7.0';
 
 self.addEventListener('install', e => e.waitUntil(removeBswup()));
 
--- src/Butil/Bit.Butil/Scripts/butil.ts
+++ src/Butil/Bit.Butil/Scripts/butil.ts
@@ -1,2 +1,2 @@
 var BitButil = BitButil || {};
-BitButil.version = window['bit-butil version'] = '9.7.0-pre-12';
+BitButil.version = window['bit-butil version'] = '9.7.0';
