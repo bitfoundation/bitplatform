a68ab5c8fcb5f03b17fc2ea30e2e40a3a45d6008
Saleh Yusefnejad <msynk@outlook.com>
2025-04-22 09:55:51 +0330

    feat(blazorui): improve BitFlag component #10517 (#10518)

--- src/BlazorUI/Bit.BlazorUI.Extras/Components/Flag/BitFlag.razor.cs
+++ src/BlazorUI/Bit.BlazorUI.Extras/Components/Flag/BitFlag.razor.cs
@@ -6,10 +6,28 @@
 public partial class BitFlag : BitComponentBase
 {
     /// <summary>
+    /// The dialing code of the country.
+    /// </summary>
+    [Parameter, ResetStyleBuilder]
+    public string? Code { get; set; }
+
+    /// <summary>
     /// The country to render the flag.
     /// </summary>
     [Parameter, ResetStyleBuilder]
     public BitCountry? Country { get; set; }
+
+    /// <summary>
+    /// The ISO 3166-1 alpha-2 code of the country.
+    /// </summary>
+    [Parameter, ResetStyleBuilder]
+    public string? Iso2 { get; set; }
+
+    /// <summary>
+    /// The ISO 3166-1 alpha-3 code of the country.
+    /// </summary>
+    [Parameter, ResetStyleBuilder]
+    public string? Iso3 { get; set; }
 
     /// <summary>
     /// The tooltip value of the flag element.
@@ -22,8 +40,30 @@ public partial class BitFlag : BitComponentBase
 
     protected override void RegisterCssStyles()
     {
-        StyleBuilder.Register(() => Country is null
-                                    ? string.Empty
-                                    : $"background-image:url('_content/Bit.BlazorUI.Extras/flags/{Country?.Iso2}-flat-16.webp')");
+        StyleBuilder.Register(() =>
+        {
+            var iso2 = FindIso2();
+            return iso2.HasValue() ? $"background-image:url('_content/Bit.BlazorUI.Extras/flags/{iso2!.ToLower()}-flat-16.webp')" : string.Empty;
+        });
+    }
+
+
+    private string? FindIso2()
+    {
+        if (Country is not null) return Country.Iso2;
+
+        if (Iso2.HasValue()) return Iso2;
+
+        if (Iso3.HasValue())
+        {
+            return BitCountries.All.FirstOrDefault(c => c.Iso3.Equals(Iso3, StringComparison.InvariantCultureIgnoreCase))?.Iso2;
+        }
+
+        if (Code.HasValue())
+        {
+            return BitCountries.All.FirstOrDefault(c => c.Code.Equals(Code, StringComparison.InvariantCultureIgnoreCase))?.Iso2;
+        }
+
+        return null;
     }
 }
