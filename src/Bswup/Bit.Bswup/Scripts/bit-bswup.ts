86f82442c0453b36b4c0ea375ba94fc43e938af6
Saleh Yusefnejad <msynk@outlook.com>
2025-05-03 18:59:21 +0330

    prerelease v-9.7.3-pre-02 #10624

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
@@ -1,5 +1,5 @@
 ﻿var BitBswup = BitBswup || {};
-BitBswup.version = window['bit-bswup version'] = '9.7.3-pre-01';
+BitBswup.version = window['bit-bswup version'] = '9.7.3-pre-02';
 
 declare const Blazor: any;
 
--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Web/wwwroot/service-worker.published.js
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Web/wwwroot/service-worker.published.js
@@ -1,5 +1,5 @@
 //+:cnd:noEmit
-// bit version: 9.7.3-pre-01
+// bit version: 9.7.3-pre-02
 // https://github.com/bitfoundation/bitplatform/tree/develop/src/Bswup
 
 //#if (notification == true)
