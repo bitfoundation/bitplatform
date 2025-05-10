d35e99e5f96c42be9a7759a2bce1b8a424e04beb
ysmoradi <ysmoradi@outlook.com>
2025-05-10 15:31:50 +0200

    improve push notification subscription management in Boilerplate (#10717)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Controllers/PushNotification/PushNotificationController.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Controllers/PushNotification/PushNotificationController.cs
@@ -1,4 +1,4 @@
-//-:cnd:noEmit
+﻿//-:cnd:noEmit
 using Boilerplate.Server.Api.Services;
 using Boilerplate.Shared.Dtos.PushNotification;
 using Boilerplate.Shared.Controllers.PushNotification;
@@ -14,6 +14,14 @@ public partial class PushNotificationController : AppControllerBase, IPushNotifi
     [HttpPost]
     public async Task Subscribe([Required] PushNotificationSubscriptionDto subscription, CancellationToken cancellationToken)
     {
+        if (User.IsAuthenticated() is false && Request.Headers.Authorization.Any() is true)
+        {
+            // PushNotificationController allows anonymous notification subscriptions. However, if an Authorization is included
+            // and the user is not authenticated, it indicates the client has sent an invalid or expired access token.
+            // In this scenario, we should refresh the access token and attempt to re-send the api request.
+            throw new UnauthorizedException();
+        }
+
         await pushNotificationService.Subscribe(subscription, cancellationToken);
     }
 
