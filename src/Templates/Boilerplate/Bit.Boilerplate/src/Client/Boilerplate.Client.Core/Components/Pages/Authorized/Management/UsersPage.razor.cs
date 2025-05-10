fb04f55e5d85f528f714bccb72634b47943caeb2
Saleh Yusefnejad <msynk@outlook.com>
2025-05-10 16:45:09 +0330

    1st commit

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Authorized/Management/UsersPage.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Authorized/Management/UsersPage.razor.cs
@@ -3,5 +3,15 @@ namespace Boilerplate.Client.Core.Components.Pages.Authorized.Management;
 
 public partial class UsersPage
 {
+    private string? newUserEmail;
+    private string? newUserFullName;
+    private string? newUserPhoneNumber;
+    private bool isLoadingSessions = true;
+    private List<BitNavItem> allSessions = [];
 
+
+    private async Task AddUser()
+    {
+
+    }
 }
