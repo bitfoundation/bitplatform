03c138c8d64af3523a4d28111c103ec72604f7b5
Yaser Moradi <ysmoradi@outlook.com>
2025-05-10 18:02:34 +0200

    feat(templates): add quick sign-in feature to Boilerplate #10719 (#10720)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Extensions/UserManagerExtensions.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Extensions/UserManagerExtensions.cs
@@ -37,16 +37,38 @@ public static partial class UserManagerExtensions
         return userManager.Users.FirstOrDefaultAsync(u => u.PhoneNumber == phoneNumber);
     }
 
-    public static async Task CreateUserWithDemoRole(this UserManager<User> userManager, User userToAdd, string password)
+    public static async Task<User> CreateUserWithDemoRole(this UserManager<User> userManager, IdentityRequestDto request, string? password = null)
     {
+        return await userManager.CreateUserWithDemoRole(new User
+        {
+            Email = request.Email,
+            PhoneNumber = request.PhoneNumber,
+            UserName = request.UserName
+        }, password);
+    }
+
+    public static async Task<User> CreateUserWithDemoRole(this UserManager<User> userManager, User userToAdd, string? password = null)
+    {
+        if (string.IsNullOrEmpty(password))
+        {
+            password = Guid.NewGuid().ToString("N"); // Users can reset their password later.
+        }
+
+        if (string.IsNullOrEmpty(userToAdd.UserName))
+        {
+            userToAdd.UserName = userToAdd.Email ?? userToAdd.PhoneNumber ?? Guid.NewGuid().ToString("N");
+        }
+
         var result = await userManager.CreateAsync(userToAdd, password);
 
         if (result.Succeeded is false)
             throw new ResourceValidationException(result.Errors.Select(e => new LocalizedString(e.Code, e.Description)).ToArray());
 
+        result = await userManager.AddToRoleAsync(userToAdd, AppRoles.Demo);
+
         if (result.Succeeded is false)
             throw new ResourceValidationException(result.Errors.Select(e => new LocalizedString(e.Code, e.Description)).ToArray());
 
-        result = await userManager.AddToRoleAsync(userToAdd, AppRoles.Demo);
+        return userToAdd;
     }
 }
