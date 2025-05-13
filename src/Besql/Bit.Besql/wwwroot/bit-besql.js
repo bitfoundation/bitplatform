cff0d7e7c8cf43805e092931ba27b82c7fd77111
Saleh Yusefnejad <msynk@outlook.com>
2025-05-13 10:08:08 +0200

    feat(prerelease): v-9.7.4-pre-04 #10750 (#10755)

--- src/Besql/Bit.Besql/wwwroot/bit-besql.js
+++ src/Besql/Bit.Besql/wwwroot/bit-besql.js
@@ -1,5 +1,5 @@
 ﻿var BitBesql = window.BitBesql || {};
-BitBesql.version = window['bit-besql version'] = '9.7.4-pre-03';
+BitBesql.version = window['bit-besql version'] = '9.7.4-pre-04';
 
 BitBesql.persist = async function persist(fileName) {
 
