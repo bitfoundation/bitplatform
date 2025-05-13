b9cbeee0fc468eb72c479adcc53929d68d399cad
Saleh Yusefnejad <msynk@outlook.com>
2025-05-13 12:53:15 +0330

    release v-9.7.4 #10756

--- src/Bswup/FullDemo/Client/wwwroot/service-worker.published.js
+++ src/Bswup/FullDemo/Client/wwwroot/service-worker.published.js
@@ -1,4 +1,4 @@
-// bit version: 9.7.4-pre-04
+// bit version: 9.7.4
 
 self.assetsInclude = [];
 self.assetsExclude = [/\.scp\.css$/, /weather\.json$/];
--- src/Butil/Bit.Butil/Scripts/butil.ts
+++ src/Butil/Bit.Butil/Scripts/butil.ts
@@ -1,2 +1,2 @@
 var BitButil = BitButil || {};
-BitButil.version = window['bit-butil version'] = '9.7.4-pre-04';
+BitButil.version = window['bit-butil version'] = '9.7.4';
