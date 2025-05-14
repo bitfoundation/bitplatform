0aefeec307af0af63c8cdc1da541fc16e95dfcc5
Saleh Yusefnejad <msynk@outlook.com>
2025-05-14 08:24:10 +0200

    feat(prerelease): v-9.8.0-pre-01 #10769 (#10770)

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
@@ -1,5 +1,5 @@
 ﻿var BitBswup = BitBswup || {};
-BitBswup.version = window['bit-bswup version'] = '9.7.4';
+BitBswup.version = window['bit-bswup version'] = '9.8.0-pre-01';
 
 BitBswup.checkForUpdate = async () => {
     if (!('serviceWorker' in navigator)) {
--- src/Butil/Bit.Butil/Scripts/butil.ts
+++ src/Butil/Bit.Butil/Scripts/butil.ts
@@ -1,2 +1,2 @@
 var BitButil = BitButil || {};
-BitButil.version = window['bit-butil version'] = '9.7.4';
+BitButil.version = window['bit-butil version'] = '9.8.0-pre-01';
