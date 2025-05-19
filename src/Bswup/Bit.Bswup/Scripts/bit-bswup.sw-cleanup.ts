88a31ae27da2472306d80ade2d52f2a4a77b7c95
Saleh Yusefnejad <msynk@outlook.com>
2025-05-19 11:51:28 +0330

    prerelease v-9.8.0-pre-04 #10811

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw-cleanup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw-cleanup.ts
@@ -1,4 +1,4 @@
-self['bit-bswup.sw-cleanup version'] = '9.8.0-pre-03';
+self['bit-bswup.sw-cleanup version'] = '9.8.0-pre-04';
 
 self.addEventListener('install', e => e.waitUntil(removeBswup()));
 
