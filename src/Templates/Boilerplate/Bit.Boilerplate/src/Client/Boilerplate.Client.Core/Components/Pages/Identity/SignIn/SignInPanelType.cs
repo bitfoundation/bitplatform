b335bee4d9ef41d5bf6c4c1b1f80e48ce829b05f
Saleh Yusefnejad <msynk@outlook.com>
2025-06-12 11:17:18 +0330

    feat(templates): improve sign-in panel and modal in Boilerplate #10969 (#10973)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Identity/SignIn/SignInPanelType.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Pages/Identity/SignIn/SignInPanelType.cs
@@ -1,8 +1,22 @@
 ﻿namespace Boilerplate.Client.Core.Components.Pages.Identity.SignIn;
 
-public enum SignInPanelType // Check out SignInModalService for more details
+/// <summary>
+/// The layout type of the SignIn panel UI. Check out <see cref="SignInModalService"/> for more details
+/// </summary>
+public enum SignInPanelType
 {
-    Full, // Shows email,phone and password fields alongside with sign-in, send otp and sign-up buttons.
-    PasswordOnly, // Shows email,phone and password fields alongside with sign-in button only.
-    OtpOnly // Shows email,phone and send otp button only.
+    /// <summary>
+    /// Shows email, phone and password fields alongside with sign-in, send otp and sign-up buttons.
+    /// </summary>
+    Full,
+
+    /// <summary>
+    /// Shows email, phone and password fields alongside with sign-in button.
+    /// </summary>
+    Password,
+
+    /// <summary>
+    /// Shows email, phone and send otp button.
+    /// </summary>
+    Otp
 }
