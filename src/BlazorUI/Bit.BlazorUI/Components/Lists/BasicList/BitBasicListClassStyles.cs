7db581675596321db9a7f1b2466fdd9169e4e716
Saleh Yusefnejad <msynk@outlook.com>
2025-06-27 20:35:13 +0330

    add Classes and Styles parameters to BitBasicList #11019

---
+++ src/BlazorUI/Bit.BlazorUI/Components/Lists/BasicList/BitBasicListClassStyles.cs
@@ -0,0 +1,19 @@
+﻿namespace Bit.BlazorUI;
+
+public class BitBasicListClassStyles
+{
+    /// <summary>
+    /// Custom CSS classes/styles for the root element of the list.
+    /// </summary>
+    public string? Root { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the LoadMore button of the list.
+    /// </summary>
+    public string? LoadMoreButton { get; set; }
+
+    /// <summary>
+    /// Custom CSS classes/styles for the LoadMore text of the list.
+    /// </summary>
+    public string? LoadMoreText { get; set; }
+}
