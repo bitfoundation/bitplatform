7496798bc407d6305e10dfaeb8f9c6fbd3da9551
ysmoradi <ysmoradi@outlook.com>
2025-07-07 21:19:55 +0200

    improve bit Boilerplate aspire configuration (#11072)

---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.AppHost/Extensions/IDistributedApplicationBuilderExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Aspire.Hosting.ApplicationModel;
+
+namespace Aspire.Hosting;
+
+public static class IDistributedApplicationBuilderExtensions
+{
+    /// <summary>
+    /// https://github.com/davidfowl/aspire-ai-chat-demo/blob/main/AIChat.AppHost/DashboardExtensions.cs
+    /// </summary>
+    public static void AddAspireDashboard(this IDistributedApplicationBuilder builder)
+    {
+        if (builder.ExecutionContext.IsPublishMode)
+        {
+            // The name aspire-dashboard is special cased and excluded from the default
+            var dashboard = builder.AddContainer("dashboard", "mcr.microsoft.com/dotnet/nightly/aspire-dashboard")
+                   .WithHttpEndpoint(targetPort: 18888)
+                   .WithHttpEndpoint(name: "otlp", targetPort: 18889);
+
+            builder.Eventing.Subscribe<BeforeStartEvent>((e, ct) =>
+            {
+                // We loop over all resources and set the OTLP endpoint to the dashboard
+                // we should make WithOtlpExporter() add an annotation so we can detect this
+                // automatically in the future.
+                foreach (var r in e.Model.Resources.OfType<IResourceWithEnvironment>())
+                {
+                    if (r == dashboard.Resource)
+                    {
+                        continue;
+                    }
+
+                    builder.CreateResourceBuilder(r).WithEnvironment(c =>
+                    {
+                        c.EnvironmentVariables["OTEL_EXPORTER_OTLP_ENDPOINT"] = dashboard.GetEndpoint("otlp");
+                        c.EnvironmentVariables["OTEL_EXPORTER_OTLP_PROTOCOL"] = "grpc";
+                        c.EnvironmentVariables["OTEL_SERVICE_NAME"] = r.Name;
+                    });
+                }
+
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
