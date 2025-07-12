71df90af7b75da5392a2f51dcff9f5d6ada8ba27
Saleh Yusefnejad <msynk@outlook.com>
2025-07-12 20:49:21 +0330

    feat(templates): apply improvements to new prodcut alt text feature of Boilerplate #11099 (#11100)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Common/ProductImage.razor.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Components/Common/ProductImage.razor.cs
@@ -5,6 +5,7 @@ public partial class ProductImage
     [CascadingParameter] public AppThemeType? CurrentTheme { get; set; }
 
     [Parameter] public string? Src { get; set; }
+    [Parameter] public string? Alt { get; set; }
     [Parameter] public string? Width { get; set; }
     [Parameter] public string? Class { get; set; }
 }
