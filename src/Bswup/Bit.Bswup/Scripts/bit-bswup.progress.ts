0794f208a63ff7566d2ddee99e45fd06d94455fc
Saleh Yusefnejad <msynk@outlook.com>
2025-07-21 23:02:51 +0330

    prerelease v-9.11.0-pre-04 #11148

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.progress.ts
@@ -1,4 +1,4 @@
-﻿window['bit-bswup.progress version'] = '9.11.0-pre-03';
+﻿window['bit-bswup.progress version'] = '9.11.0-pre-04';
 
 (function () {
     const _config: IBswupProgressConfigs = {};
--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
@@ -1,5 +1,5 @@
 ﻿var BitBswup = BitBswup || {};
-BitBswup.version = window['bit-bswup version'] = '9.11.0-pre-03';
+BitBswup.version = window['bit-bswup version'] = '9.11.0-pre-04';
 
 (function () {
     const bitBswupScript = document.currentScript;
