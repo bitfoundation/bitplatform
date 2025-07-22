3a2d1817943724c429e091692cb227ae1c85fe63
ysmoradi <ysmoradi@outlook.com>
2025-07-22 16:36:41 +0200

    fix

---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Scripts/bswup.ts
@@ -0,0 +1,33 @@
+/**
+ * Checks for and applies updates if available.
+ * Called by `WebAppUpdateService.cs` when the user clicks the app version in `AppShell.razor`
+ * or when `ForceUpdateSnackbar.razor` appears after a forced update.
+ */
+async function tryUpdatingPwa(autoReload?: boolean) {
+    const bswup = (window as any).BitBswup; // https://bitplatform.dev/bswup
+    const bswupProgress = (window as any).BitBswupProgress;
+
+    if (await bswup.skipWaiting()) return;
+
+    bswupProgress.config({ autoReload: autoReload ?? true });
+    bswup.checkForUpdate();
+}
+
+// To minimize user-facing force updates, the following code attempts to update the PWA ASAP.
+window.addEventListener('beforeunload', () => tryUpdatingPwa(true));
+
+let appIsInBackground = false; // App is in the background if visibilityState is 'hidden' for 20 seconds.
+let visibilityChangeTimeout: number;
+window.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'hidden') {
+        visibilityChangeTimeout = window.setTimeout(() => {
+            appIsInBackground = true;
+        }, 20 * 1000 /* 20 seconds */);
+    } else {
+        clearTimeout(visibilityChangeTimeout);
+        appIsInBackground = false;
+    }
+});
+
+// This would download the updates, but reloads the app only if it's in the background.
+setInterval(() => tryUpdatingPwa(appIsInBackground), 60 * 1000 /* 1 minute */);
