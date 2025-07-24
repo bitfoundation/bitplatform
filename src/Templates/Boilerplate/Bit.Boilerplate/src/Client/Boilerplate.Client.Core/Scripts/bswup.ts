ba03cdf701dae925677db3f1a63ee4ed92577a66
Yaser Moradi <ysmoradi@outlook.com>
2025-07-24 12:40:58 +0200

    feat(templates): improve bit Boilerplate bswup check app update #11171 (#11172)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Scripts/bswup.ts
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Scripts/bswup.ts
@@ -18,15 +18,19 @@ async function tryUpdatePwa(autoReload: boolean) {
 
 bswup?.skipWaiting(); // Use new service worker if available.
 
-// Check for updates every minute.
-let lastRunInForeground = new Date().getTime();
+// Detects if the app was in the background for over 2 minutes. Since setInterval usually pauses in the background on modern browsers and runs immediately upon resuming,
+// a lastRunInForeground older than 2 minutes indicates the app was likely not focused.
+let counter = 0;
+let lastTimeAppWasInForeground = new Date().getTime();
 setInterval(() => {
     const now = new Date().getTime();
-    const resuming = now - lastRunInForeground > 60 * 2 * 1000;
-    if (document.visibilityState === 'visible') {
-        lastRunInForeground = now;
+    const resuming = now - lastTimeAppWasInForeground > 60 * 2 * 1000;
+    counter++;
+    if (counter % 60 === 0 /*Every 60 seconds*/ || resuming) {
+        counter = 0;
+        tryUpdatePwa(resuming);
     }
-    // Detects if the app was in the background for over 2 minutes. Since setInterval usually pauses in the background on modern browsers and runs immediately upon resuming,
-    // a lastRunInForeground older than 2 minutes indicates the app was likely not focused.
-    tryUpdatePwa(resuming);
-}, 60 * 1000);
+    if (document.visibilityState === 'visible') {
+        lastTimeAppWasInForeground = now;
+    }
+}, 1000);
