9b0fb1e0ec60632fb9b2f9ba8c48df9431c70fee
ysmoradi <ysmoradi@outlook.com>
2025-07-29 16:41:21 +0200

    Revert "fix"
    This reverts commit 0ab0154886abc2e4621fe63ec1a92d2efe18f3bf.

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Scripts/bswup.ts
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Scripts/bswup.ts
@@ -8,9 +8,6 @@ const bswup = (window as any).BitBswup; // https://bitplatform.dev/bswup
 async function tryUpdatePwa(autoReload: boolean) {
     const bswupProgress = (window as any).BitBswupProgress;
 
-    if (bswupProgress == null)
-        return;
-
     if (autoReload) {
         if (await bswup.skipWaiting()) return; // Use new service worker if available and reload the page.
     }
@@ -19,24 +16,21 @@ async function tryUpdatePwa(autoReload: boolean) {
     bswup.checkForUpdate();
 }
 
-if (bswup != null) {
+bswup?.skipWaiting(); // Use new service worker if available.
 
-    bswup.skipWaiting(); // Use new service worker if available.
-
-    // Detects if the app was in the background for over 2 minutes. Since setInterval usually pauses in the background on modern browsers and runs immediately upon resuming,
-    // a lastRunInForeground older than 2 minutes indicates the app was likely not focused.
-    let counter = 0;
-    let lastTimeAppWasInForeground = new Date().getTime();
-    setInterval(() => {
-        const now = new Date().getTime();
-        const resuming = now - lastTimeAppWasInForeground > 60 * 2 * 1000;
-        counter++;
-        if (counter % 60 === 0 /*Every 60 seconds*/ || resuming) {
-            counter = 0;
-            tryUpdatePwa(resuming);
-        }
-        if (document.visibilityState === 'visible') {
-            lastTimeAppWasInForeground = now;
-        }
-    }, 1000);
-}
+// Detects if the app was in the background for over 2 minutes. Since setInterval usually pauses in the background on modern browsers and runs immediately upon resuming,
+// a lastRunInForeground older than 2 minutes indicates the app was likely not focused.
+let counter = 0;
+let lastTimeAppWasInForeground = new Date().getTime();
+setInterval(() => {
+    const now = new Date().getTime();
+    const resuming = now - lastTimeAppWasInForeground > 60 * 2 * 1000;
+    counter++;
+    if (counter % 60 === 0 /*Every 60 seconds*/ || resuming) {
+        counter = 0;
+        tryUpdatePwa(resuming);
+    }
+    if (document.visibilityState === 'visible') {
+        lastTimeAppWasInForeground = now;
+    }
+}, 1000);
