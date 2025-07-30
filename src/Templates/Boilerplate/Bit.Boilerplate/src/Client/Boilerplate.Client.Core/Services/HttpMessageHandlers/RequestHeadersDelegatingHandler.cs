2a8b597e4d3960a7a1e408b3b7da896ddc363f31
ysmoradi <ysmoradi@outlook.com>
2025-07-30 16:43:51 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/HttpMessageHandlers/RequestHeadersDelegatingHandler.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Services/HttpMessageHandlers/RequestHeadersDelegatingHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Http.Headers;
+using Boilerplate.Shared.Dtos.Identity;
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 namespace Boilerplate.Client.Core.Services.HttpMessageHandlers;
@@ -9,7 +10,13 @@ public partial class RequestHeadersDelegatingHandler(ITelemetryContext telemetry
 {
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        request.SetBrowserRequestCredentials(BrowserRequestCredentials.Omit);
+        // In order to have access_token as http only cookie to support pre-rendering scenarios,
+        // we need to set the request credentials to include to api's that return access token in the response.
+        // In other actions, include only result into double sending the access token in the request headers unnecessarily. (Authorization and Cookie headers).
+        var responseType = request.Options.GetValueOrDefault(RequestOptionNames.ResponseType, null) as Type;
+        var includeCredentials = responseType == typeof(SignInResponseDto) || responseType == typeof(TokenResponseDto);
+        request.SetBrowserRequestCredentials(includeCredentials ? BrowserRequestCredentials.Include : BrowserRequestCredentials.Omit);
+
         request.SetBrowserResponseStreamingEnabled(true);
 
         request.Version = HttpVersion.Version20;
