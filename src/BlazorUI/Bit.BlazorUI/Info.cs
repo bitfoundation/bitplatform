2724b882a8177a5f0e43052d63258ee7ddc6b518
Saleh Yusefnejad <msynk@outlook.com>
2025-08-30 01:03:11 +0330

    feat(blazorui): add built-in cache busting feature to BlazorUI #11307 (#11309)

---
+++ src/BlazorUI/Bit.BlazorUI/Info.cs
@@ -0,0 +1,12 @@
+﻿namespace Bit.BlazorUI;
+
+/// <summary>
+/// Provides information about the bit BlazorUI library.
+/// </summary>
+public static class Info
+{
+    /// <summary>
+    /// The current version string of bit BlazorUI.
+    /// </summary>
+    public static string Version { get; } = "9.11.4-pre-01";
+}
