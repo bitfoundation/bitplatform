d8fc9d3d3ca74955b0157a2989f7aab397303639
Saleh Yusefnejad <msynk@outlook.com>
2025-09-08 15:53:32 +0330

    feat(websites): apply improvements to AI chat panel on Platform website #11353 (#11356)

--- src/Websites/Platform/src/Bit.Websites.Platform.Client/Shared/AppAiChatPanel.razor.cs
+++ src/Websites/Platform/src/Bit.Websites.Platform.Client/Shared/AppAiChatPanel.razor.cs
@@ -8,8 +8,6 @@ namespace Bit.Websites.Platform.Client.Shared;
 
 public partial class AppAiChatPanel
 {
-    [CascadingParameter] public BitDir? CurrentDir { get; set; }
-
     [AutoInject] private HubConnection hubConnection = default!;
 
 
