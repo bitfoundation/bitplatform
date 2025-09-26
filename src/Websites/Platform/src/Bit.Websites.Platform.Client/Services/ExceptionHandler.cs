c38881323646b22a3badb51f5ad3d5af8a1f61fa
Yaser Moradi <ysmoradi@outlook.com>
2025-09-26 14:38:08 +0200

    feat(websites): write platform website exception details in console #11443 (#11444)

--- src/Websites/Platform/src/Bit.Websites.Platform.Client/Services/ExceptionHandler.cs
+++ src/Websites/Platform/src/Bit.Websites.Platform.Client/Services/ExceptionHandler.cs
@@ -8,10 +8,10 @@ public partial class ExceptionHandler : IExceptionHandler
 
     public void Handle(Exception exception, IDictionary<string, object?>? parameters = null)
     {
-#if DEBUG
         string exceptionMessage = (exception as KnownException)?.Message ?? exception.ToString();
-        _ = messageBoxService.Show(exceptionMessage, "Error");
         _ = Console.Out.WriteLineAsync(exceptionMessage);
+#if DEBUG
+        _ = messageBoxService.Show(exceptionMessage, "Error");
         Debugger.Break();
 #else
         if (exception is KnownException knownException)
