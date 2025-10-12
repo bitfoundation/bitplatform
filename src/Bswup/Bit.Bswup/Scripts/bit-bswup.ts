92610bf05771dca60ddb4ddb1d01586677f09bea
ysmoradi <ysmoradi@outlook.com>
2025-10-12 15:48:05 +0200

    Prerelease v-10.0.0-pre-03 (#11478)

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
@@ -1,5 +1,5 @@
 ﻿var BitBswup = BitBswup || {};
-BitBswup.version = window['bit-bswup version'] = '10.0.0-pre-02';
+BitBswup.version = window['bit-bswup version'] = '10.0.0-pre-03';
 
 (function () {
     const bitBswupScript = document.currentScript;
