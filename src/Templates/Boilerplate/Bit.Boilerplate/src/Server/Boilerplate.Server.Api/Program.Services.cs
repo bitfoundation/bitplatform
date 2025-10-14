a19abbd0b9287bb0f606bf4a1bef279e295be27b
ysmoradi <ysmoradi@outlook.com>
2025-10-14 14:31:53 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Program.Services.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Program.Services.cs
@@ -261,10 +261,10 @@ public static partial class Program
             return;
             //#endif
             //#if (database == "SqlServer")
-            var mssqlConnectionStringBuilder = new SqliteConnectionStringBuilder(configuration.GetRequiredConnectionString("mssqldb"));
+            var mssqlConnectionStringBuilder = new Microsoft.Data.SqlClient.SqlConnectionStringBuilder(configuration.GetRequiredConnectionString("mssqldb"));
             if (env.IsDevelopment())
             {
-                mssqlConnectionStringBuilder["TrustServerCertificate"] = true;
+                mssqlConnectionStringBuilder.TrustServerCertificate = true;
             }
             options.UseSqlServer(mssqlConnectionStringBuilder.ConnectionString, dbOptions =>
             {
