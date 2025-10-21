1a921b704fac959e64b9d3b84678ebb06416ce6b
msynk <msynk@outlook.com>
2025-10-21 12:01:23 +0330

    prerelease v-10.0.0-pre-05 #11498

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw-cleanup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw-cleanup.ts
@@ -1,4 +1,4 @@
-self['bit-bswup.sw-cleanup version'] = '10.0.0-pre-04';
+self['bit-bswup.sw-cleanup version'] = '10.0.0-pre-05';
 
 self.addEventListener('install', e => e.waitUntil(removeBswup()));
 
