572d2250ab023dc5a8fa64f864b3e2158e3222c7
ysmoradi <ysmoradi@outlook.com>
2025-11-14 16:12:32 +0100

    fix several small issues regarding latest dependencies update (#11629)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Data/AppDbContext.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Data/AppDbContext.cs
@@ -271,7 +271,7 @@ public partial class AppDbContext(DbContextOptions<AppDbContext> options)
     // In order to enable embedding, the `pgvector` extension must be installed in your PostgreSQL.
     //#if (aspire == false)
     // The following command runs the postgreSQL container with the `pgvector` extension:
-    // docker run -d --name postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=BoilerplateDb -p 5432:5432 -v pgdata:/var/lib/postgresql/data --restart unless-stopped pgvector/pgvector:pg17
+    // docker run -d --name postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=BoilerplateDb -p 5432:5432 -v pgdata:/var/lib/postgresql --restart unless-stopped pgvector/pgvector:pg18
     //#endif
     //#elif (database == "SqlServer")
     // This requires SQL Server 2025+
