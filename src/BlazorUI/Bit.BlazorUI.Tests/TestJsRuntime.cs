e81c26947c1ce2523af64d1fedf1539be0efe4b1
Saleh Yusefnejad <msynk@outlook.com>
2025-11-14 05:14:39 +0330

    add missing tests for BitCarousel component #11606

---
+++ src/BlazorUI/Bit.BlazorUI.Tests/TestJsRuntime.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Bit.BlazorUI.Tests;
+
+public class TestJsRuntime : IJSRuntime
+{
+    public ValueTask<TValue> InvokeAsync<TValue>(string identifier, object?[]? args)
+    {
+        return new ValueTask<TValue>(default(TValue)!);
+    }
+
+    public ValueTask<TValue> InvokeAsync<TValue>(string identifier, CancellationToken cancellationToken, object?[]? args)
+    {
+        return new ValueTask<TValue>(default(TValue)!);
+    }
+}
