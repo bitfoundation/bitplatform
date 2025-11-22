6b935a3052afe54b0e105ac1d8e1d55615a83324
Saleh Yusefnejad <msynk@outlook.com>
2025-11-22 12:15:41 +0330

    feat(blazorui): add missing tests for BitCalendar #11714 (#11715)

---
+++ src/BlazorUI/Bit.BlazorUI.Tests/Components/Inputs/Calendar/BitCalendarTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bit.BlazorUI.Tests.Components.Inputs.Calendar;
+
+[TestClass]
+public class BitCalendarTests : BunitTestContext
+{
+    [TestMethod,
+        DataRow(true),
+        DataRow(false)]
+    public void BitCalendarShouldRespectIsEnabled(bool isEnabled)
+    {
+        var component = RenderComponent<BitCalendar>(parameters =>
+        {
+            parameters.Add(p => p.IsEnabled, isEnabled);
+        });
+
+        var calendar = component.Find(".bit-cal");
+
+        if (isEnabled)
+        {
+            Assert.IsFalse(calendar.ClassList.Contains("bit-dis"));
+        }
+        else
+        {
+            Assert.IsTrue(calendar.ClassList.Contains("bit-dis"));
+        }
+    }
+
+    [TestMethod]
+    public void BitCalendarShouldRenderDayCellTemplate()
+    {
+        var component = RenderComponent<BitCalendar>(parameters =>
+        {
+            parameters.Add(p => p.DayCellTemplate, (RenderFragment<DateTimeOffset>)(context =>
+            {
+                RenderFragment fragment = builder => builder.AddContent(0, $"Day-{context.Day}");
+                return fragment;
+            }));
+        });
+
+        var firstDayCell = component.Find(".bit-cal-dbt");
+
+        Assert.IsTrue(firstDayCell.TextContent.Contains("Day-"));
+    }
+
+    [TestMethod,
+        DataRow(true),
+        DataRow(false)]
+    public void BitCalendarShouldRespectShowWeekNumbers(bool showWeekNumbers)
+    {
+        var component = RenderComponent<BitCalendar>(parameters =>
+        {
+            parameters.Add(p => p.ShowWeekNumbers, showWeekNumbers);
+        });
+
+        var weekNumbers = component.FindAll(".bit-cal-wnm");
+
+        if (showWeekNumbers)
+        {
+            Assert.IsTrue(weekNumbers.Count > 0);
+        }
+        else
+        {
+            Assert.AreEqual(0, weekNumbers.Count);
+        }
+    }
+
+    [TestMethod]
+    public void BitCalendarShouldRespectGoToTodayTitle()
+    {
+        var title = "Go now";
+
+        var component = RenderComponent<BitCalendar>(parameters =>
+        {
+            parameters.Add(p => p.ShowMonthPicker, false);
+            parameters.Add(p => p.GoToTodayTitle, title);
+        });
+
+        var goToTodayButton = component.Find(".bit-cal-gtb");
+
+        Assert.AreEqual(title, goToTodayButton.GetAttribute("title"));
+    }
+
+    [TestMethod]
+    public void BitCalendarSelectingTodayShouldUpdateValue()
+    {
+        var component = RenderComponent<BitCalendar>();
+
+        Assert.IsNull(component.Instance.Value);
+
+        var todayButton = component.Find(".bit-cal-dtd");
+
+        todayButton.Click();
+
+        Assert.IsNotNull(component.Instance.Value);
+        Assert.AreEqual(DateTimeOffset.Now.Date, component.Instance.Value!.Value.Date);
+        Assert.AreEqual(TimeZoneInfo.Local.GetUtcOffset(DateTimeOffset.Now), component.Instance.Value!.Value.Offset);
+    }
+}
