5aaca72b952fdd55160a39686cd4a8f0c484b617
Saleh Yusefnejad <msynk@outlook.com>
2025-11-25 13:43:43 +0330

    feat(blazorui): develop unit tests for BitModalService #11750 (#11751)

---
+++ src/BlazorUI/Bit.BlazorUI.Tests/Components/Extras/ModalService/BitModalServiceTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bit.BlazorUI.Tests.Components.Extras.ModalService;
+
+[TestClass]
+public class BitModalServiceTests : BunitTestContext
+{
+    private BitModalService ModalService => Services.GetRequiredService<BitModalService>();
+
+    [TestInitialize]
+    public void SetupServices()
+    {
+        Services.AddSingleton<BitModalService>();
+    }
+
+    [TestMethod]
+    public async Task BitModalServiceShouldRenderModalInContainer()
+    {
+        var message = "Hello modal";
+
+        var container = RenderComponent<BitModalContainer>();
+
+        await ModalService.Show<TestModalContent>(new Dictionary<string, object>
+        {
+            { nameof(TestModalContent.Message), message }
+        });
+
+        container.WaitForAssertion(() =>
+        {
+            Assert.AreEqual(1, container.FindAll(".bit-mdl").Count);
+            Assert.IsTrue(container.Markup.Contains(message));
+        });
+    }
+
+    [TestMethod]
+    public async Task BitModalServiceShouldCloseModal()
+    {
+        var container = RenderComponent<BitModalContainer>();
+
+        var modalRef = await ModalService.Show<TestModalContent>();
+
+        container.WaitForAssertion(() =>
+        {
+            Assert.AreEqual(1, container.FindAll(".bit-mdl").Count);
+        });
+
+        modalRef.Close();
+
+        container.WaitForAssertion(() =>
+        {
+            Assert.AreEqual(0, container.FindAll(".bit-mdl").Count);
+        });
+    }
+
+    [TestMethod]
+    public async Task BitModalServiceShouldRenderPersistentModalAfterContainerInit()
+    {
+        var modalRef = await ModalService.Show<TestModalContent>(persistent: true);
+
+        var container = RenderComponent<BitModalContainer>();
+
+        container.WaitForAssertion(() =>
+        {
+            Assert.AreEqual(1, container.FindAll(".bit-mdl").Count);
+        });
+
+        Assert.IsTrue(modalRef.Persistent);
+    }
+}
---
+++ src/BlazorUI/Bit.BlazorUI.Tests/Components/Extras/ModalService/TestModalContent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Bit.BlazorUI.Tests.Components.Extras.ModalService;
+
+public class TestModalContent : ComponentBase
+{
+    [Parameter] public string Message { get; set; } = string.Empty;
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenElement(0, "p");
+        builder.AddAttribute(1, "class", "test-modal-content");
+        builder.AddContent(2, Message);
+        builder.CloseElement();
+    }
+}
