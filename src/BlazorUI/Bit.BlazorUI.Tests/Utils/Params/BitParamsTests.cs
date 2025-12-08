e119a55f01dd2ad7c10997397eab23932250ca92
Saleh Yusefnejad <msynk@outlook.com>
2025-12-09 00:55:33 +0330

    add missing tests of BitParams #11852

---
+++ src/BlazorUI/Bit.BlazorUI.Tests/Utils/Params/BitParamsTests.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bit.BlazorUI.Tests.Utils.Params;
+
+[TestClass]
+public class BitParamsTests : BunitTestContext
+{
+    [TestMethod]
+    public void ShouldCascadeParametersToChildren()
+    {
+        var parameters = new List<IBitComponentParams>
+        {
+            new FakeParamsA(),
+            new FakeParamsB()
+        };
+
+        var component = RenderComponent<BitParams>(builder =>
+        {
+            builder.Add(p => p.Parameters, parameters);
+            builder.AddChildContent(childBuilder =>
+            {
+                childBuilder.OpenComponent<ParamsConsumer>(0);
+                childBuilder.CloseComponent();
+            });
+        });
+
+        component.MarkupMatches("1-Hello");
+    }
+
+    [TestMethod]
+    public void ShouldProvideValuesWithNamesAndFixedFlag()
+    {
+        var param = new FakeParamsA();
+
+        var component = RenderComponent<BitParams>(builder =>
+        {
+            builder.Add(p => p.Parameters, new[] { param });
+            builder.AddChildContent("<div>content</div>");
+        });
+
+        var provider = component.FindComponent<BitCascadingValueProvider>().Instance;
+        var value = provider.Values?.Single();
+
+        Assert.IsNotNull(value);
+        Assert.AreEqual(param, value!.Value);
+        Assert.AreEqual(param.Name, value.Name);
+        Assert.IsTrue(value.IsFixed);
+    }
+
+    [TestMethod]
+    public void ShouldSkipNullParameters()
+    {
+        var component = RenderComponent<BitParams>(builder =>
+        {
+            builder.Add(p => p.Parameters, new IBitComponentParams[] { null!, new FakeParamsB() });
+            builder.AddChildContent("<span>child</span>");
+        });
+
+        var provider = component.FindComponent<BitCascadingValueProvider>().Instance;
+        var values = provider.Values?.ToList();
+
+        Assert.IsNotNull(values);
+        Assert.AreEqual(1, values!.Count);
+        Assert.AreEqual("B", values[0].Name);
+    }
+
+    [TestMethod]
+    public void ShouldRenderChildContentWhenParametersNull()
+    {
+        var component = RenderComponent<BitParams>(builder =>
+        {
+            builder.AddChildContent("<p>no-params</p>");
+        });
+
+        component.MarkupMatches("<p>no-params</p>");
+    }
+
+    [TestMethod]
+    public void ShouldRenderChildContentWhenParametersEmpty()
+    {
+        var component = RenderComponent<BitParams>(builder =>
+        {
+            builder.Add(p => p.Parameters, Enumerable.Empty<IBitComponentParams>());
+            builder.AddChildContent("<p>empty</p>");
+        });
+
+        component.MarkupMatches("<p>empty</p>");
+    }
+
+    [TestMethod]
+    public void ShouldRenderEmptyWhenNoChildAndNoParameters()
+    {
+        var component = RenderComponent<BitParams>();
+
+        component.MarkupMatches(string.Empty);
+    }
+
+    [TestMethod]
+    public void ShouldIgnoreUnknownParametersAndNotThrow()
+    {
+        var component = RenderComponent<BitParams>();
+
+        var parameters = ParameterView.FromDictionary(new Dictionary<string, object?>
+        {
+            { "Unknown", 1 },
+            { nameof(BitParams.Parameters), new[] { new FakeParamsA() } }
+        });
+
+        component.InvokeAsync(() => component.Instance.SetParametersAsync(parameters)).GetAwaiter().GetResult();
+
+        Assert.AreEqual(1, component.Instance.Parameters?.Count());
+    }
+
+    [TestMethod]
+    public void ShouldUpdateParametersOnRerender()
+    {
+        var first = new FakeParamsA();
+        var second = new FakeParamsB();
+
+        var component = RenderComponent<BitParams>(builder =>
+        {
+            builder.Add(p => p.Parameters, new IBitComponentParams[] { first });
+            builder.AddChildContent(childBuilder =>
+            {
+                childBuilder.OpenComponent<ParamsConsumer>(0);
+                childBuilder.CloseComponent();
+            });
+        });
+
+        component.SetParametersAndRender(builder => builder.Add(p => p.Parameters, new IBitComponentParams[] { second }));
+
+        component.MarkupMatches("-Hello");
+    }
+}
