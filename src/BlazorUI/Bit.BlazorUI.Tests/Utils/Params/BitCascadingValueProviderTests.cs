6c121f7a1d73e89d77c44c0f01d8fa1c3f0b1b99
Saleh Yusefnejad <msynk@outlook.com>
2025-12-09 00:28:57 +0330

    add missing tests of BitCascadingValueProvider #11850

---
+++ src/BlazorUI/Bit.BlazorUI.Tests/Utils/Params/BitCascadingValueProviderTests.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bit.BlazorUI.Tests.Utils.Params;
+
+[TestClass]
+public class BitCascadingValueProviderTests : BunitTestContext
+{
+    [TestMethod]
+    public void ShouldProvideCascadingValuesFromEnumerable()
+    {
+        var cascadingValues = new List<BitCascadingValue?>
+        {
+            null,
+            new("hi", "Greeting"),
+            new(7)
+        };
+
+        var component = RenderComponent<BitCascadingValueProvider>(parameters =>
+        {
+            parameters.Add(p => p.Values, cascadingValues!);
+            parameters.AddChildContent(builder =>
+            {
+                builder.OpenComponent<CascadingConsumer>(0);
+                builder.CloseComponent();
+            });
+        });
+
+        var consumer = component.FindComponent<CascadingConsumer>().Instance;
+
+        Assert.AreEqual(7, consumer.Number);
+        Assert.AreEqual("hi", consumer.Greeting);
+    }
+
+    [TestMethod]
+    public void ShouldProvideCascadingValuesFromValueList()
+    {
+        var cascadingValues = new BitCascadingValueList();
+        cascadingValues.Add(3, isFixed: true);
+        cascadingValues.Add("message", "Greeting");
+
+        var component = RenderComponent<BitCascadingValueProvider>(parameters =>
+        {
+            parameters.Add(p => p.ValueList, cascadingValues);
+            parameters.AddChildContent(builder =>
+            {
+                builder.OpenComponent<CascadingConsumer>(0);
+                builder.CloseComponent();
+            });
+        });
+
+        var intCascade = component.FindComponent<CascadingValue<int>>().Instance;
+        var stringCascade = component.FindComponent<CascadingValue<string>>().Instance;
+
+        Assert.AreEqual(3, intCascade.Value);
+        Assert.IsTrue(intCascade.IsFixed);
+
+        Assert.AreEqual("message", stringCascade.Value);
+        Assert.IsFalse(stringCascade.IsFixed);
+        Assert.AreEqual("Greeting", stringCascade.Name);
+    }
+
+    [TestMethod]
+    public void ShouldRenderChildContentWhenNoValuesAreProvided()
+    {
+        var childContentRendered = false;
+
+        var component = RenderComponent<BitCascadingValueProvider>(parameters =>
+        {
+            parameters.AddChildContent(builder =>
+            {
+                childContentRendered = true;
+                builder.AddContent(0, "plain-child");
+            });
+        });
+
+        Assert.IsTrue(childContentRendered);
+        component.MarkupMatches("plain-child");
+    }
+
+    [TestMethod]
+    public void ShouldRenderEmptyWhenNothingProvided()
+    {
+        var component = RenderComponent<BitCascadingValueProvider>();
+
+        component.MarkupMatches(string.Empty);
+    }
+
+    [TestMethod]
+    public void ShouldPreferValuesParameterOverValueList()
+    {
+        var values = new List<BitCascadingValue> { new("from-values", "Greeting") };
+        var valueList = new BitCascadingValueList
+        {
+            { "from-value-list", "Greeting" }
+        };
+
+        var component = RenderComponent<BitCascadingValueProvider>(parameters =>
+        {
+            parameters.Add(p => p.Values, values);
+            parameters.Add(p => p.ValueList, valueList);
+            parameters.AddChildContent(builder =>
+            {
+                builder.OpenComponent<CascadingConsumer>(0);
+                builder.CloseComponent();
+            });
+        });
+
+        var consumer = component.FindComponent<CascadingConsumer>().Instance;
+
+        Assert.AreEqual("from-values", consumer.Greeting);
+    }
+
+    [TestMethod]
+    public void ShouldSkipNullEntriesAndStillCascadeOthers()
+    {
+        var cascadingValues = new List<BitCascadingValue?>
+        {
+            new(5),
+            null,
+            new("hello", "Greeting")
+        };
+
+        var component = RenderComponent<BitCascadingValueProvider>(parameters =>
+        {
+            parameters.Add(p => p.Values, cascadingValues!);
+            parameters.AddChildContent(builder =>
+            {
+                builder.OpenComponent<CascadingConsumer>(0);
+                builder.CloseComponent();
+            });
+        });
+
+        var consumer = component.FindComponent<CascadingConsumer>().Instance;
+
+        Assert.AreEqual(5, consumer.Number);
+        Assert.AreEqual("hello", consumer.Greeting);
+    }
+
+    [TestMethod]
+    public void ShouldRespectOrderWhenDuplicateNamesExist()
+    {
+        var cascadingValues = new List<BitCascadingValue?>
+        {
+            new("first", "Greeting"),
+            new("second", "Greeting")
+        };
+
+        var component = RenderComponent<BitCascadingValueProvider>(parameters =>
+        {
+            parameters.Add(p => p.Values, cascadingValues!);
+            parameters.AddChildContent(builder =>
+            {
+                builder.OpenComponent<CascadingConsumer>(0);
+                builder.CloseComponent();
+            });
+        });
+
+        var consumer = component.FindComponent<CascadingConsumer>().Instance;
+
+        Assert.AreEqual("second", consumer.Greeting);
+    }
+
+    [TestMethod]
+    public void ShouldAllowNullValuesWhenTypeProvided()
+    {
+        var cascadingValues = new BitCascadingValueList();
+        cascadingValues.Add<string?>(null, "Greeting");
+        cascadingValues.Add<int?>(null);
+
+        var component = RenderComponent<BitCascadingValueProvider>(parameters =>
+        {
+            parameters.Add(p => p.ValueList, cascadingValues);
+            parameters.AddChildContent(builder =>
+            {
+                builder.OpenComponent<NullableConsumer>(0);
+                builder.CloseComponent();
+            });
+        });
+
+        var stringCascade = component.FindComponent<CascadingValue<string>>().Instance;
+        var intCascade = component.FindComponent<CascadingValue<int?>>().Instance;
+        var consumer = component.FindComponent<NullableConsumer>().Instance;
+
+        Assert.IsNull(stringCascade.Value);
+        Assert.IsNull(intCascade.Value);
+        Assert.AreEqual("Greeting", stringCascade.Name);
+        Assert.IsNull(consumer.NullableNumber);
+        component.MarkupMatches("-");
+    }
+
+    [TestMethod]
+    public void ShouldHandleManyCascadingValues()
+    {
+        var cascadingValues = Enumerable.Range(0, 50).Select(i => new BitCascadingValue(i)).ToList();
+
+        var component = RenderComponent<BitCascadingValueProvider>(parameters =>
+        {
+            parameters.Add(p => p.Values, cascadingValues);
+            parameters.AddChildContent(builder =>
+            {
+                builder.OpenComponent<CascadingConsumer>(0);
+                builder.CloseComponent();
+            });
+        });
+
+        var consumer = component.FindComponent<CascadingConsumer>().Instance;
+
+        Assert.AreEqual(49, consumer.Number);
+    }
+}
