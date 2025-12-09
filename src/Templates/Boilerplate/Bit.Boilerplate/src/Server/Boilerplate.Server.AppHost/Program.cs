37a71ce0db11b5ffb7866588d34e8bc50d9d0a13
ysmoradi <ysmoradi@outlook.com>
2025-12-09 16:24:03 +0100

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.AppHost/Program.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.AppHost/Program.cs
@@ -5,46 +5,45 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Check out appsettings.Development.json for credentials/passwords settings.
 
-////#if (database == "SqlServer")
-//var sqlDatabase = builder.AddSqlServer("sqlserver")
-//        .WithDbGate(config => config.WithDataVolume())
-//        .WithDataVolume()
-//        .WithImage("mssql/server", "2025-latest")
-//        .AddDatabase("mssqldb"); // Sql server 2025 supports embedded vector search.
+//#if (database == "SqlServer")
+var sqlDatabase = builder.AddSqlServer("sqlserver")
+        .WithDbGate(config => config.WithDataVolume())
+        .WithDataVolume()
+        .WithImage("mssql/server", "2025-latest")
+        .AddDatabase("mssqldb"); // Sql server 2025 supports embedded vector search.
 
-////#elif (database == "PostgreSql")
-//var postgresDatabase = builder.AddPostgres("postgresserver")
-//        .WithPgAdmin(config => config.WithVolume("/var/lib/pgadmin/Boilerplate/data"))
-//        .WithV18DataVolume()
-//        .WithImage("pgvector/pgvector", "pg18") // pgvector supports embedded vector search.
-//        .AddDatabase("postgresdb");
+//#elif (database == "PostgreSql")
+var postgresDatabase = builder.AddPostgres("postgresserver")
+        .WithPgAdmin(config => config.WithVolume("/var/lib/pgadmin/Boilerplate/data"))
+        .WithV18DataVolume()
+        .WithImage("pgvector/pgvector", "pg18") // pgvector supports embedded vector search.
+        .AddDatabase("postgresdb");
 
-////#elif (database == "MySql")
-//var mySqlDatabase = builder.AddMySql("mysqlserver")
-//        .WithPhpMyAdmin(config => config.WithVolume("/var/lib/phpMyAdmin/Boilerplate/data"))
-//        .WithDataVolume()
-//        .AddDatabase("mysqldb");
-////#elif (database == "Sqlite")
+//#elif (database == "MySql")
+var mySqlDatabase = builder.AddMySql("mysqlserver")
+        .WithPhpMyAdmin(config => config.WithVolume("/var/lib/phpMyAdmin/Boilerplate/data"))
+        .WithDataVolume()
+        .AddDatabase("mysqldb");
+//#elif (database == "Sqlite")
 var sqlite = builder.AddSqlite("sqlite", databaseFileName: "BoilerplateDb.db")
     .WithSqliteWeb(config => config.WithVolume("/var/lib/sqliteweb/Boilerplate/data"));
-////#endif
-////#if (filesStorage == "AzureBlobStorage")
-//var azureBlobStorage = builder.AddAzureStorage("storage")
-//        .RunAsEmulator(azurite =>
-//        {
-//            azurite
-//                .WithDataVolume();
-//        })
-//        .AddBlobs("azureblobstorage");
+//#endif
+//#if (filesStorage == "AzureBlobStorage")
+var azureBlobStorage = builder.AddAzureStorage("storage")
+        .RunAsEmulator(azurite =>
+        {
+            azurite
+                .WithDataVolume();
+        })
+        .AddBlobs("azureblobstorage");
 
-////#elif (filesStorage == "S3")
-//var s3Storage = builder.AddMinioContainer("s3")
-//    .WithDataVolume();
-////#endif
+//#elif (filesStorage == "S3")
+var s3Storage = builder.AddMinioContainer("s3")
+    .WithDataVolume();
+//#endif
 
 // https://aspire.dev/integrations/security/keycloak/
 var keycloak = builder.AddKeycloak("keycloak", 8080)
-    .WithLifetime(ContainerLifetime.Persistent)
     .WithDataVolume()
     .WithRealmImport("./Realms");
 
@@ -71,35 +70,35 @@ if (builder.Environment.IsDevelopment())
 
 serverWebProject.WithReference(serverApiProject);
 //#if (database == "SqlServer")
-//serverApiProject.WithReference(sqlDatabase).WaitFor(sqlDatabase);
-////#elif (database == "PostgreSql")
-//serverApiProject.WithReference(postgresDatabase).WaitFor(postgresDatabase);
-////#elif (database == "MySql")
-//serverApiProject.WithReference(mySqlDatabase).WaitFor(mySqlDatabase);
-////#elif (database == "Sqlite")
-//serverApiProject.WithReference(sqlite).WaitFor(sqlite);
-////#endif
-////#if (filesStorage == "AzureBlobStorage")
-//serverApiProject.WithReference(azureBlobStorage);
-////#elif (filesStorage == "S3")
-//serverApiProject.WithReference(s3Storage);
+serverApiProject.WithReference(sqlDatabase).WaitFor(sqlDatabase);
+//#elif (database == "PostgreSql")
+serverApiProject.WithReference(postgresDatabase).WaitFor(postgresDatabase);
+//#elif (database == "MySql")
+serverApiProject.WithReference(mySqlDatabase).WaitFor(mySqlDatabase);
+//#elif (database == "Sqlite")
+serverApiProject.WithReference(sqlite).WaitFor(sqlite);
+//#endif
+//#if (filesStorage == "AzureBlobStorage")
+serverApiProject.WithReference(azureBlobStorage);
+//#elif (filesStorage == "S3")
+serverApiProject.WithReference(s3Storage);
 //#endif
 serverApiProject.WithReference(keycloak);
 //#else
 
-////#if (database == "SqlServer")
-//serverWebProject.WithReference(sqlDatabase).WaitFor(sqlDatabase);
-////#elif (database == "PostgreSql")
-//serverWebProject.WithReference(postgresDatabase).WaitFor(postgresDatabase);
-////#elif (database == "MySql")
-//serverWebProject.WithReference(mySqlDatabase).WaitFor(mySqlDatabase);
-////#elif (database == "Sqlite")
+//#if (database == "SqlServer")
+serverWebProject.WithReference(sqlDatabase).WaitFor(sqlDatabase);
+//#elif (database == "PostgreSql")
+serverWebProject.WithReference(postgresDatabase).WaitFor(postgresDatabase);
+//#elif (database == "MySql")
+serverWebProject.WithReference(mySqlDatabase).WaitFor(mySqlDatabase);
+//#elif (database == "Sqlite")
 serverWebProject.WithReference(sqlite).WaitFor(sqlite);
-////#endif
-////#if (filesStorage == "AzureBlobStorage")
-//serverWebProject.WithReference(azureBlobStorage);
-////#elif (filesStorage == "S3")
-//serverWebProject.WithReference(s3Storage);
+//#endif
+//#if (filesStorage == "AzureBlobStorage")
+serverWebProject.WithReference(azureBlobStorage);
+//#elif (filesStorage == "S3")
+serverWebProject.WithReference(s3Storage);
 //#endif
 serverWebProject.WithReference(keycloak);
 //#endif
