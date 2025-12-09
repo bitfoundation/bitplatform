f458bc43fe5a4885445858670b4fc14a1a1d5092
Saleh Yusefnejad <msynk@outlook.com>
2025-12-09 09:58:32 +0330

    feat(blazorui): add missing tests of BitCascadingValueProvider #11850 (#11851)

---
+++ src/BlazorUI/Bit.BlazorUI.Tests/Utils/Params/NullableConsumer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Bit.BlazorUI.Tests.Utils.Params;
+
+public sealed class NullableConsumer : ComponentBase
+{
+    [CascadingParameter] public int? NullableNumber { get; set; }
+
+    [CascadingParameter(Name = "Greeting")] public string? Greeting { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.AddContent(0, $"{NullableNumber}-{Greeting}");
+    }
+}
