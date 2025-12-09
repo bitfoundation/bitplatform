c464005ee36a0017b854b87ce54e29802fd40272
Saleh Yusefnejad <msynk@outlook.com>
2025-12-09 10:00:03 +0330

    feat(blazorui): add missing tests of BitParams #11852 (#11853)

---
+++ src/BlazorUI/Bit.BlazorUI.Tests/Utils/Params/ParamsConsumer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Bit.BlazorUI.Tests.Utils.Params;
+
+public sealed class ParamsConsumer : ComponentBase
+{
+    [CascadingParameter(Name = "A")] public FakeParamsA? A { get; set; }
+
+    [CascadingParameter(Name = "B")] public FakeParamsB? B { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.AddContent(0, $"{A?.Value}-{B?.Text}");
+    }
+}
