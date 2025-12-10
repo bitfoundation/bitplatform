a54204184764e25aef3dd733550380d7a2a12891
Saleh Yusefnejad <msynk@outlook.com>
2025-12-10 22:34:10 +0330

    resovle review comments

--- src/BlazorUI/Bit.BlazorUI/Components/Utilities/Text/BitTextParams.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Utilities/Text/BitTextParams.cs
@@ -1,6 +1,4 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace Bit.BlazorUI;
+﻿namespace Bit.BlazorUI;
 
 /// <summary>
 /// The parameters for <see cref="BitText"/> component.
