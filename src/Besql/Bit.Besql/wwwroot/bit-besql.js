98d867b6a1db6e7e1e749158e6f812c2a55dc157
Saleh Yusefnejad <msynk@outlook.com>
2025-12-11 12:46:40 +0330

    feat(prerelease): v-10.2.1-pre-01 #11862 (#11869)

--- src/Besql/Bit.Besql/wwwroot/bit-besql.js
+++ src/Besql/Bit.Besql/wwwroot/bit-besql.js
@@ -1,5 +1,5 @@
 var BitBesql = window.BitBesql || {};
-BitBesql.version = window['bit-besql version'] = '10.2.0';
+BitBesql.version = window['bit-besql version'] = '10.2.1-pre-01';
 
 BitBesql.persist = async function besqlPersist(fileName) {
 
--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
@@ -1,5 +1,5 @@
 ﻿var BitBswup = BitBswup || {};
-BitBswup.version = window['bit-bswup version'] = '10.2.0';
+BitBswup.version = window['bit-bswup version'] = '10.2.1-pre-01';
 
 (function () {
     const bitBswupScript = document.currentScript;
