39637aed07f02eea34e049710cf9ceb97f812a2a
ysmoradi <ysmoradi@outlook.com>
2026-01-18 20:35:46 +0100

    prioritize Azure SignalR over SignalR Redis backplane if azure SignalR connection string is provided in bit Boilerplate (#11978)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/SignalR/AppHub.Chatbot.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/SignalR/AppHub.Chatbot.cs
@@ -13,6 +13,19 @@ public partial class AppHub
     private static readonly UpDownCounter<long> ongoingConversationsCount = Meter.Current.CreateUpDownCounter<long>("appHub.ongoing_conversations_count", "Number of ongoing conversations in the chatbot hub.");
 
     /// <summary>
+    /// This method is accepting stream of user messages and returning stream of string charecters of AI chatbot responses.
+    /// The basic implementation idea is brought from here: https://learn.microsoft.com/en-us/aspnet/core/signalr/streaming?view=aspnetcore-10.0
+    /// Accepting stream of user messages drastically reduces the latency instead of sending different requests for each user message.
+    /// 
+    /// During this process, if a new user message arrives, the ongoing message processing is cancelled and the new message is processed,
+    /// and server is able to send commands such as navigate to specific page, change theme etc to the client as well,
+    /// which means as AI chatbot behind this hub is able to guide user through the application and a lot more interactive experience can be built.
+    /// Using SignalR's `InvokeAsync` method, the AI chatbot would know if the command is successfully executed on client side or not, because it's not a fire-and-forget operation.
+    /// 
+    /// By accepting stream of user messages, we can store `chatMessages` field inside `AppChatbot` for current conversation only,
+    /// which is **scalable** without forcing client to send the whole chat history each time or storing chat history on redis or database.
+    /// 
+    /// The above 3 reasons are the main motivations of this design/implementation using SignalR instead of using SSE or other techniques.
     /// Checkout <see cref="AppChatbot"/> for more details.
     /// </summary>
     [HubMethodName(SharedAppMessages.StartChat)]
