90150687e066c739cbcc99b196fc00ab485514eb
Yas Moradi <ysmoradi@outlook.com>
2026-01-22 19:04:21 +0100

    feat(templates): improve distributed lock factory #11997 (#11998)

---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Infrastructure/Services/DistributedLockFactory.cs
@@ -0,0 +1,9 @@
+﻿using Medallion.Threading;
+
+namespace Boilerplate.Server.Api.Infrastructure.Services;
+
+/// <summary>
+/// Creates distributed lock by given lock key.
+/// It can be any implementation of IDistributedLock such as Redis, SqlServer, PostgreSQL, etc.
+/// </summary>
+public delegate IDistributedLock DistributedLockFactory(string lockKey);
