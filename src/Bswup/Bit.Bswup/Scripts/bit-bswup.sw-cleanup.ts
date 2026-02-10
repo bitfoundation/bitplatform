0c809e94f2b236dfb1a81c7ae9239d0ceb264492
Saleh Yusefnejad <msynk@outlook.com>
2026-02-10 11:50:46 +0330

    feat(prerelease): v-10.4.2-pre-02 #12067 (#12069)

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw-cleanup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.sw-cleanup.ts
@@ -1,4 +1,4 @@
-self['bit-bswup.sw-cleanup version'] = '10.4.2-pre-01';
+self['bit-bswup.sw-cleanup version'] = '10.4.2-pre-02';
 
 self.addEventListener('install', e => e.waitUntil(removeBswup()));
 
--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
@@ -1,5 +1,5 @@
 var BitBswup = BitBswup || {};
-BitBswup.version = window['bit-bswup version'] = '10.4.2-pre-01';
+BitBswup.version = window['bit-bswup version'] = '10.4.2-pre-02';
 
 (function () {
     const bitBswupScript = document.currentScript;
