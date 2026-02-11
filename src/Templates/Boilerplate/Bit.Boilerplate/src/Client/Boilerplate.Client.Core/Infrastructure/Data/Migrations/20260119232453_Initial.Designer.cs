98d6bfb9887e92bc4b33fbe10dd344ad22c4b0f3
ysmoradi <ysmoradi@outlook.com>
2026-02-11 12:55:32 +0100

    update project dependencies (#12074)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Infrastructure/Data/Migrations/20260119232453_Initial.Designer.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Infrastructure/Data/Migrations/20260119232453_Initial.Designer.cs
@@ -17,7 +17,7 @@ partial class Initial
     protected override void BuildTargetModel(ModelBuilder modelBuilder)
     {
 #pragma warning disable 612, 618
-        modelBuilder.HasAnnotation("ProductVersion", "10.0.2");
+        modelBuilder.HasAnnotation("ProductVersion", "10.0.3");
 
         modelBuilder.Entity("Boilerplate.Shared.Dtos.Todo.TodoItemDto", b =>
             {
