d174eee6fb72eb3c51ba902c34e2846235a407dd
Yas Moradi <ysmoradi@outlook.com>
2026-03-01 23:45:11 +0100

    feat(templates): unify system text json serializer options in bit Boilerplate #12142 (#12143)

---
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Infrastructure/Extensions/JsonSerializerOptionsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using FluentEmail.Core;
+using Boilerplate.Shared.Features.Identity;
+using Boilerplate.Server.Api.Infrastructure.Services;
+
+namespace System.Text.Json;
+
+public static class JsonSerializerOptionsExtensions
+{
+    public static void ApplyDefaultOptions(this JsonSerializerOptions options)
+    {
+        // Changes in this file should be applied on AppJsonContext.cs, IdentityJsonContext.cs and ServerJsonContext.cs as well,
+        // so source generators can use the same options.
+
+        options.AllowTrailingCommas = true;
+        options.PropertyNameCaseInsensitive = true;
+        options.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+        options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+
+        options.TypeInfoResolverChain.AddRange([AppJsonContext.Default,
+            IdentityJsonContext.Default,
+            ServerJsonContext.Default]);
+    }
+}
