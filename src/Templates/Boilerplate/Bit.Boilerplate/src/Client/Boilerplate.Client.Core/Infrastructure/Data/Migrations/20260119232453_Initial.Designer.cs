bf84aa49a50b47a238291a366962b64eb5636be5
ysmoradi <ysmoradi@outlook.com>
2026-03-10 22:34:59 +0100

    update project dependencies (#12165)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Infrastructure/Data/Migrations/20260119232453_Initial.Designer.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Infrastructure/Data/Migrations/20260119232453_Initial.Designer.cs
@@ -17,7 +17,7 @@ partial class Initial
     protected override void BuildTargetModel(ModelBuilder modelBuilder)
     {
 #pragma warning disable 612, 618
-        modelBuilder.HasAnnotation("ProductVersion", "10.0.3");
+        modelBuilder.HasAnnotation("ProductVersion", "10.0.4");
 
         modelBuilder.Entity("Boilerplate.Shared.Dtos.Todo.TodoItemDto", b =>
             {
--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Infrastructure/Data/Migrations/AppOfflineDbContextModelSnapshot.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Infrastructure/Data/Migrations/AppOfflineDbContextModelSnapshot.cs
@@ -14,7 +14,7 @@ partial class AppOfflineDbContextModelSnapshot : ModelSnapshot
     protected override void BuildModel(ModelBuilder modelBuilder)
     {
 #pragma warning disable 612, 618
-        modelBuilder.HasAnnotation("ProductVersion", "10.0.3");
+        modelBuilder.HasAnnotation("ProductVersion", "10.0.4");
 
         modelBuilder.Entity("Boilerplate.Shared.Dtos.Todo.TodoItemDto", b =>
             {
