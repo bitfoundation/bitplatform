2598c6ace8e32682455840a6cd28405f878eccbb
Yas Moradi <yas_moradi@outlook.com>
2026-03-24 22:42:45 +0100

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Program.Services.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Api/Program.Services.cs
@@ -161,7 +161,7 @@ public static partial class Program
             {
                 if (string.IsNullOrEmpty(appSettings.AdsPushVapid.PublicKey))
                     throw new InvalidOperationException("VAPID public key is required");
-                if (string.IsNullOrEmpty(appSettings.AdsPushVapid.Subject) is false)
+                if (string.IsNullOrEmpty(appSettings.AdsPushVapid.Subject))
                     throw new InvalidOperationException("VAPID subject is required"); // While it would work on Android, Windows, Linux, Apple requires subject, so we enforce it for all platforms to avoid confusion and potential issues.
 
                 adsPushSenderBuilder = adsPushSenderBuilder.ConfigureVapid(appSettings.AdsPushVapid, sp.GetRequiredService<IHttpClientFactory>().CreateClient("Vapid"));
