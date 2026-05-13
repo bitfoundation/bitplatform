32b3f689e6e22608d539aa162d9f067e1c48ffeb
Yas Moradi <yas_moradi@outlook.com>
2026-05-13 11:11:07 +0200

    update project dependencies (#12325)

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Infrastructure/Data/Migrations/AppOfflineDbContextModelSnapshot.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Client/Boilerplate.Client.Core/Infrastructure/Data/Migrations/AppOfflineDbContextModelSnapshot.cs
@@ -14,7 +14,7 @@ partial class AppOfflineDbContextModelSnapshot : ModelSnapshot
     protected override void BuildModel(ModelBuilder modelBuilder)
     {
 #pragma warning disable 612, 618
-        modelBuilder.HasAnnotation("ProductVersion", "10.0.7");
+        modelBuilder.HasAnnotation("ProductVersion", "10.0.8");
 
         modelBuilder.Entity("Boilerplate.Shared.Dtos.Todo.TodoItemDto", b =>
             {
