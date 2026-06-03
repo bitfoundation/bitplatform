8ec89eb2b9dbced89c44dfe79de86aa63c1a22cc
Saleh Yusefnejad <msynk@outlook.com>
2026-06-03 19:46:57 +0330

    revert unrelated changes

--- src/BlazorUI/Bit.BlazorUI/Components/Surfaces/ScrollablePane/BitScrollablePaneJsRuntimeExtensions.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Surfaces/ScrollablePane/BitScrollablePaneJsRuntimeExtensions.cs
@@ -2,10 +2,8 @@
 
 internal static class BitScrollablePaneJsRuntimeExtensions
 {
-    // ScrollablePane.scrollToEnd is a synchronous, null-guarded DOM call (see BitScrollablePane.ts),
-    // so it is safe to run through FastInvokeVoid (synchronous on in-process runtimes, async fallback otherwise).
     internal static ValueTask BitScrollablePaneScrollToEnd(this IJSRuntime jsRuntime, ElementReference element)
     {
-        return jsRuntime.FastInvokeVoid("BitBlazorUI.ScrollablePane.scrollToEnd", element);
+        return jsRuntime.InvokeVoid("BitBlazorUI.ScrollablePane.scrollToEnd", element);
     }
 }
