ee171e73824fd9af903277857ab47fc14413cd8f
msynk <msynk@outlook.com>
2026-06-09 12:02:20 +0330

    resolve review comments

--- src/BlazorUI/Tests/Bit.BlazorUI.Tests/Components/Inputs/SearchBox/BitSearchBoxTests.cs
+++ src/BlazorUI/Tests/Bit.BlazorUI.Tests/Components/Inputs/SearchBox/BitSearchBoxTests.cs
@@ -291,14 +291,19 @@ public class BitSearchBoxTests : BunitTestContext
         Context.JSInterop.Mode = JSRuntimeMode.Loose;
 
         string? receivedSearchTerm = null;
+        int receivedTake = 0;
 
         var component = RenderComponent<BitSearchBox>(p =>
         {
             p.Add(x => x.Immediate, true);
+            p.Add(x => x.MinSuggestTriggerChars, 3);
             p.Add(x => x.SuggestItemsProvider, (BitSearchBoxSuggestItemsProviderRequest req) =>
             {
                 receivedSearchTerm = req.SearchTerm;
-                return ValueTask.FromResult<IEnumerable<string>>(new List<string> { "apple", "application" });
+                receivedTake = req.Take;
+                return req.SearchTerm == "app"
+                    ? ValueTask.FromResult<IEnumerable<string>>(new List<string> { "apple", "application" })
+                    : ValueTask.FromResult<IEnumerable<string>>(new List<string> { "banana" });
             });
         });
 
@@ -308,6 +313,17 @@ public class BitSearchBoxTests : BunitTestContext
         var items = component.FindAll(".bit-srb-itm");
 
         Assert.AreEqual("app", receivedSearchTerm);
+        Assert.AreEqual(5, receivedTake);
         Assert.AreEqual(2, items.Count);
+        Assert.AreEqual("apple", items[0].TextContent.Trim());
+        Assert.AreEqual("application", items[1].TextContent.Trim());
+
+        input.Input("ban");
+
+        items = component.FindAll(".bit-srb-itm");
+
+        Assert.AreEqual("ban", receivedSearchTerm);
+        Assert.AreEqual(1, items.Count);
+        Assert.AreEqual("banana", items[0].TextContent.Trim());
     }
 }
