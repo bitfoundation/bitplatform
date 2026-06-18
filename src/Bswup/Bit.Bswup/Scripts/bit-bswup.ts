357bd5df5fc117d113d5ff0e9ffa1b3947123ee1
msynk <msynk@outlook.com>
2026-06-18 13:37:05 +0330

    prerelease v-10.5.0-pre-02 #12471

--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
@@ -1,5 +1,5 @@
 var BitBswup = BitBswup || {};
-BitBswup.version = window['bit-bswup version'] = '10.5.0-pre-01';
+BitBswup.version = window['bit-bswup version'] = '10.5.0-pre-02';
 
 (function () {
     const bitBswupScript = document.currentScript;
