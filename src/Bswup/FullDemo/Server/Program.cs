3e6197afcdaf0c91db378922d9a5724fc90dfbd1
Saleh Yusefnejad <msynk@outlook.com>
2026-06-30 07:04:09 +0330

    improve FullDemo

--- src/Bswup/FullDemo/Server/Program.cs
+++ src/Bswup/FullDemo/Server/Program.cs
@@ -1,20 +1,53 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.IO.Compression;
+using Bit.Bswup.FullDemo.Server.Components;
+using Microsoft.AspNetCore.ResponseCompression;
 
-#if DEBUG
-if (OperatingSystem.IsWindows())
-{
-    builder.WebHost.UseUrls("https://localhost:5021", "http://localhost:5020", "https://*:5021", "http://*:5020");
-}
-else
-{
-    builder.WebHost.UseUrls("https://localhost:5021", "http://localhost:5020");
-}
-#endif
+var builder = WebApplication.CreateBuilder(args);
 
-Bit.Bswup.FullDemo.Server.Startup.Services.Add(builder.Services);
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddCors();
+builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.EnableForHttps = true;
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(["application/octet-stream"]);
+    opts.Providers.Add<BrotliCompressionProvider>();
+    opts.Providers.Add<GzipCompressionProvider>();
+})
+    .Configure<BrotliCompressionProviderOptions>(opt => opt.Level = CompressionLevel.Fastest)
+    .Configure<GzipCompressionProviderOptions>(opt => opt.Level = CompressionLevel.Fastest);
 
 var app = builder.Build();
 
-Bit.Bswup.FullDemo.Server.Startup.Middlewares.Use(app, builder.Environment);
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseResponseCompression();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+app.MapStaticAssets();
+app.UseAntiforgery();
+
+app.MapControllers();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Bit.Bswup.FullDemo.Client._Imports).Assembly);
 
 app.Run();
