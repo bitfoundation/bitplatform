4e5f169f551fd20902bd8255fd220591e1febf52
msynk <msynk@outlook.com>
2026-07-07 15:23:44 +0330

    add new TextShimmer extra component #12598

--- src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Shared/MainLayout.razor.NavItems.cs
+++ src/BlazorUI/Demo/Client/Bit.BlazorUI.Demo.Client.Core/Shared/MainLayout.razor.NavItems.cs
@@ -175,6 +175,7 @@ public partial class MainLayout
                 new() { Text = "ProModalService", Url = "/components/promodalservice" },
                 new() { Text = "ProPanel", Url = "/components/propanel" },
                 new() { Text = "RichTextEditor", Url = "/components/richtexteditor" },
+                new() { Text = "TextShimmer", Url = "/components/textshimmer", Description = "Shimmer, AI" },
             ]
         },
         new() { Text = "Iconography", Url = "/iconography" },
