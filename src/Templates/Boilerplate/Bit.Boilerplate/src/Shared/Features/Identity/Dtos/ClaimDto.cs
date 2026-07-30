a554595c66c6e636a2d21db6ec0c14b5e4dd3aa7
yasmoradi <ysmoradi@outlook.com>
2026-07-30 16:12:58 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Shared/Features/Identity/Dtos/ClaimDto.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Shared/Features/Identity/Dtos/ClaimDto.cs
@@ -6,32 +6,4 @@ public partial class ClaimDto
     public string? ClaimType { get; set; }
 
     public string? ClaimValue { get; set; }
-
-    public static bool operator ==(ClaimDto? @this, ClaimDto? that)
-    {
-        if (@this is null && that is null) return true;
-        if (@this is null || that is null) return false;
-        return @this.ClaimType == that.ClaimType
-             && @this.ClaimValue == that.ClaimValue;
-    }
-
-    public static bool operator !=(ClaimDto? @this, ClaimDto? that)
-    {
-        return !(@this == that);
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return Equals(obj as ClaimDto);
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(ClaimType, ClaimValue);
-    }
-
-    public override string ToString()
-    {
-        return $"{ClaimType}={ClaimValue}";
-    }
 }
