50f88dca8c288bb2714ff74114122d92a2617554
Saleh Yusefnejad <msynk@outlook.com>
2026-08-04 09:55:34 +0330

    feat(prerelease): v-10.6.0-pre-01 #12808 (#12814)

--- src/BlazorUI/Bit.BlazorUI/Scripts/general.ts
+++ src/BlazorUI/Bit.BlazorUI/Scripts/general.ts
@@ -1,4 +1,4 @@
-﻿(BitBlazorUI as any).version = (window as any)['bit-blazorui version'] = '10.5.0';
+﻿(BitBlazorUI as any).version = (window as any)['bit-blazorui version'] = '10.6.0-pre-01';
 
 interface DotNetObject {
     invokeMethod<T>(methodIdentifier: string, ...args: any[]): T;
--- src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
+++ src/Bswup/Bit.Bswup/Scripts/bit-bswup.ts
@@ -1,5 +1,5 @@
 var BitBswup = BitBswup || {};
-BitBswup.version = window['bit-bswup version'] = '10.5.0';
+BitBswup.version = window['bit-bswup version'] = '10.6.0-pre-01';
 
 (function () {
     const bitBswupScript = document.currentScript;
