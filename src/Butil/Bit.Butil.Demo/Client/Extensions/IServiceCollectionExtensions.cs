3acc499fec30334a41476e5db2b9a5aea0bf1247
Saleh Yusefnejad <msynk@outlook.com>
2026-08-12 15:07:55 +0330

    improve Butil demo website #12894

--- src/Butil/Bit.Butil.Demo/Client/Extensions/IServiceCollectionExtensions.cs
+++ src/Butil/Bit.Butil.Demo/Client/Extensions/IServiceCollectionExtensions.cs
@@ -15,6 +15,7 @@ public static class IServiceCollectionExtensions
     {
         services.AddBitButilServices();
         services.AddScoped<ThemeService>();
+        services.AddScoped<PageOutline>();
 
         return services;
     }
