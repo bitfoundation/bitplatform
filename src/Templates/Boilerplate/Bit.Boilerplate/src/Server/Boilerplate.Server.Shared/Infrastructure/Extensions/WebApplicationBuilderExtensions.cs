4951f51feef7e71fa033e8fcb17f891bad4d1fc2
yasmoradi <ysmoradi@outlook.com>
2026-08-28 17:52:47 +0200

    fix

--- src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Shared/Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
+++ src/Templates/Boilerplate/Bit.Boilerplate/src/Server/Boilerplate.Server.Shared/Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
@@ -50,7 +50,7 @@ public static class WebApplicationBuilderExtensions
                     var policyBuilder = policy.AddPolicy<AppResponseCachePolicy>();
                 }, excludeDefaultPolicy: true);
             });
-            if (settings.ResponseCaching?.EnableCdnEdgeCaching is true || settings.ResponseCaching?.EnableOutputCaching is true)
+            if (settings.ResponseCaching?.EnableCdnEdgeCaching is not false || settings.ResponseCaching?.EnableOutputCaching is not false)
             {
                 services.AddSingleton<AspNetCore.Antiforgery.IAntiforgery, SharedResponseCacheCompatibleAntiforgery>();
             }
