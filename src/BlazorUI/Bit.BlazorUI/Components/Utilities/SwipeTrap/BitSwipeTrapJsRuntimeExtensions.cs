f0292df70d6ff85ff6adb6b647a05a6bb6c0629e
Saleh Yusefnejad <msynk@outlook.com>
2026-09-03 18:01:14 +0330

    feat(blazorui): apply BitSwipeTrap improvements #13109 (#13132)

--- src/BlazorUI/Bit.BlazorUI/Components/Utilities/SwipeTrap/BitSwipeTrapJsRuntimeExtensions.cs
+++ src/BlazorUI/Bit.BlazorUI/Components/Utilities/SwipeTrap/BitSwipeTrapJsRuntimeExtensions.cs
@@ -6,12 +6,15 @@ internal static class BitSwipeTrapJsRuntimeExtensions
                                                      string id,
                                                      ElementReference element,
                                                      decimal trigger,
+                                                     decimal triggerVelocity,
                                                      decimal threshold,
                                                      int throttle,
                                                      BitSwipeOrientation orientationLock,
+                                                     bool touchOnly,
+                                                     string? skipSelector,
                                                      DotNetObjectReference<BitSwipeTrap>? dotnetObjectReference)
     {
-        return js.InvokeVoid("BitBlazorUI.SwipeTrap.setup", id, element, trigger, threshold, throttle, orientationLock, dotnetObjectReference);
+        return js.InvokeVoid("BitBlazorUI.SwipeTrap.setup", id, element, trigger, triggerVelocity, threshold, throttle, orientationLock, touchOnly, skipSelector, dotnetObjectReference);
     }
 
     internal static ValueTask BitSwipeTrapDispose(this IJSRuntime jsRuntime, string id)
